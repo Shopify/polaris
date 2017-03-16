7a3b926ce0d74cff61f46e8f5dc4a1dca462ca6e
Ismail Syed <isyed867@gmail.com>
2017-03-16 10:39:01 -0400

    Add commonjs2 as a supported external module context (#20)
    * Add commonjs2 as a supported external module context
    
    * Group commonjs externals

--- config/webpack/config.js
+++ config/webpack/config.js
@@ -41,11 +41,13 @@ module.exports = function createWebpackConfig() {
     externals: {
       react: {
         commonjs: 'react',
+        commonjs2: 'react',
         amd: 'react',
         root: 'React',
       },
       'react-dom': {
         commonjs: 'react-dom',
+        commonjs2: 'react',
         amd: 'react-dom',
         root: 'ReactDOM',
       },
