d9cbe26ce3fdad16b895e6dec6754cc2d294707d
Ismail Syed <isyed867@gmail.com>
2017-03-31 12:00:37 -0400

    Add test for lib build (#54)
    * Add test to verify lib build
    
    * Move beforeAll() & afterAll() into build block
    
    * Add jest env to eslintConfig
    
    * :fire: beforeAll() & afterAll() :fire:

--- tests/build.test.js
+++ tests/build.test.js
@@ -1,13 +1,18 @@
-
-/* global describe it expect */
 const execSync = require('child_process').execSync;
 const fs = require('fs-extra');
 
-
 describe('build', () => {
-  it('should generate CDN files', () => {
+  it('should generate CDN files in ./build', () => {
     execSync('yarn run build');
     expect(fs.existsSync('./build/quilt.js')).toBe(true);
     expect(fs.existsSync('./build/quilt.css')).toBe(true);
+    execSync('yarn run clean');
+  });
+
+  it('should generate lib files in ./', () => {
+    execSync('yarn run build');
+    expect(fs.existsSync('./index.js')).toBe(true);
+    expect(fs.existsSync('./components/index.js')).toBe(true);
+    execSync('yarn run clean');
   });
 });
