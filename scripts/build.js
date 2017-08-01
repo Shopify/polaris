91195c4a92c7cef3882d5abe561aa663a0e62ecd
Dominic McPhee <dominic.mcphee@gmail.com>
2017-08-01 13:52:40 -0400

    Add component docs to npm package build

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,5 @@
 import {execSync} from 'child_process';
-import {ensureDirSync, writeFileSync} from 'fs-extra';
+import {ensureDirSync, writeFileSync, readFileSync} from 'fs-extra';
 import {join, resolve as resolvePath} from 'path';
 import {rollup} from 'rollup';
 import {cp} from 'shelljs';
@@ -7,12 +7,14 @@ import copyfiles from 'copyfiles';
 
 import createRollupConfig from '../config/rollup';
 import generateSassBuild from './sass-build';
+import packageJSON from '../package.json';
 
 const root = resolvePath(__dirname, '..');
 const build = resolvePath(root, 'build');
 const buildEsnext = resolvePath(root, 'build-esnext');
 const finalEsnext = resolvePath(root, 'esnext');
 
+const docs = resolvePath(root, './docs');
 const intermediateBuild = resolvePath(root, './build-intermediate');
 const mainEntry = resolvePath(intermediateBuild, './index.js');
 const embeddedEntry = resolvePath(intermediateBuild, './embedded/index.js');
@@ -22,6 +24,22 @@ execSync(`./node_modules/.bin/tsc --outDir ${intermediateBuild}`, {
 });
 
 writeFileSync(resolvePath(root, 'embedded.d.ts'), "export * from './types/embedded';\n");
+
+const srcReadme = resolvePath(root, './src/components/README.md');
+const destinationReadme = resolvePath(docs, './components/README.md');
+
+copy(['./src/**/*.md', docs], { up: 1 })
+  .then(() => {
+    writeFileSync(
+      destinationReadme,
+      readFileSync(srcReadme, 'utf8').replace(/\{\{VERSION\}\}/g, packageJSON.version)
+    );
+  })
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    process.exit(1);
+  });
 
 copy(['./src/**/*.{scss,svg,png,jpg,jpeg}', intermediateBuild], {up: 1})
   .then(() => {
