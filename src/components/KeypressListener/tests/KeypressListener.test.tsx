7b2758fe106e5f6779ba40954431373ba2c66184
Mallory Allen <mallory.allen@shopify.com>
2017-08-04 13:20:09 -0400

    ✅ add tests for KeypressListener

---
+++ src/components/KeypressListener/tests/KeypressListener.test.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import {mount} from 'enzyme';
+import {noop} from '@shopify/javascript-utilities/other';
+
+import KeypressListener from '../KeypressListener';
+import {Keys} from '../../../types';
+
+interface HandlerMap {
+  [eventName: string]: (event: any) => void,
+}
+
+const originalAddEventListener = document.addEventListener;
+const originalRemoveEventListener = document.removeEventListener;
+const listenerMap: HandlerMap = {};
+
+describe('<KeypressListener />', () => {
+  beforeEach(() => {
+    document.addEventListener = jest.fn((event, cb) => {
+      listenerMap[event] = cb;
+    });
+
+    document.removeEventListener = jest.fn((event) => {
+      listenerMap[event] = noop;
+    });
+  });
+
+  afterEach(() => {
+    document.addEventListener = originalAddEventListener;
+    document.removeEventListener = originalRemoveEventListener;
+  });
+
+  it('attaches a listener for the given key on mount', () => {
+    const spy = jest.fn();
+
+    mount(
+      <KeypressListener
+        handler={spy}
+        keyCode={Keys.ESCAPE}
+      />,
+    );
+
+    listenerMap.keyup({keyCode: Keys.ESCAPE});
+    listenerMap.keyup({keyCode: Keys.ENTER});
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('removes listener for the given key on unmount', () => {
+    const spy = jest.fn();
+
+    mount(
+      <KeypressListener
+        handler={spy}
+        keyCode={Keys.ESCAPE}
+      />,
+    ).unmount();
+
+    listenerMap.keyup({keyCode: Keys.ESCAPE});
+    expect(spy).not.toBeCalled();
+  });
+});
