00ea5008a72cd85cb364cd364568a3f1fd8481ca
Andrew Musgrave <andrew.musgrave@shopify.com>
2018-03-19 11:10:00 -0400

    Added props and tests related to preventDefault

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,16 +2,31 @@ import * as React from 'react';
 import {autobind} from '@shopify/javascript-utilities/decorators';
 
 export type AutoComplete = 'off' | 'on';
+export type Enctype = 'application/x-www-form-urlencoded' | 'multipart/form-data' | 'text/plain';
+export type Method = 'post' | 'get' | 'action';
+export type Target = '_blank' | '_self' | '_parent' | '_top' | string;
 
 export interface Props {
+  /** Space seperated list of character encodings */
+  acceptCharset?: string,
+  /** Where to send form-data on submittal */
+  action?: string,
   /** Grants the broswer the ability to autocomplete input elements */
   autoComplete?: AutoComplete,
   /** The content to display inside the form. */
   children?: React.ReactNode,
+  /** Media type when submiting content to server */
+  encType?: Enctype,
+  /** Method used to submit form */
+  method?: Method,
   /** A unique name for the form */
   name?: string,
   /** Wheather or not form is validated when submitting */
   noValidate?: boolean,
+  /** Blocks the default form action */
+  preventDefault?: boolean,
+  /** Where to display response after form submittal */
+  target?: Target,
   /** Callback when for is submitted */
   onSubmit(): void,
 }
@@ -19,17 +34,27 @@ export interface Props {
 export default class Form extends React.PureComponent<Props> {
   render() {
     const {
+      acceptCharset,
+      action,
       autoComplete,
       children,
+      encType,
+      method,
       name,
       noValidate,
+      target,
     } = this.props;
 
     return (
       <form
+        acceptCharset={acceptCharset}
+        action={action}
         autoComplete={autoComplete}
+        encType={encType}
+        method={method}
         name={name}
         noValidate={noValidate}
+        target={target}
         onSubmit={this.handleSubmit}
       >
         {children}
@@ -39,7 +64,14 @@ export default class Form extends React.PureComponent<Props> {
 
   @autobind
   private handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    const {
+      preventDefault = true,
+      onSubmit,
+    } = this.props;
+
+    if (!preventDefault) { return; }
+
     event.preventDefault();
-    this.props.onSubmit();
+    onSubmit();
   }
 }
--- src/components/Form/tests/Form.test.tsx
+++ src/components/Form/tests/Form.test.tsx
@@ -7,27 +7,25 @@ const autoComplete = 'off';
 const name = 'form-name';
 const noValidate = true;
 
+const acceptCharset = 'UTF-8';
+const action = 'shopifyapi.com';
+const encType = 'text/plain';
+const method = 'get';
+const preventDefault = false;
+const target = '_blank';
+
 describe('<Form />', () => {
-  describe('onSubmit', () => {
-    it('is called when the form is submitted', () => {
-      const spy = jest.fn();
-      const wrapper = mount(<Form onSubmit={spy} />);
-      wrapper.simulate('submit');
-
-      expect(spy).toHaveBeenCalled();
+  describe('acceptCharset', () => {
+    it('sets the acceptCharset attribute when provided', () => {
+      const wrapper = mount(<Form acceptCharset={acceptCharset} onSubmit={noop} />);
+      expect(wrapper.prop('acceptCharset')).toBe(acceptCharset);
     });
+  });
 
-    it('submits the form with preventDefault', () => {
-      const onSubmitSpy = jest.fn();
-      const preventDefaultSpy = jest.fn();
-
-      const wrapper = mount(<Form onSubmit={onSubmitSpy} />);
-
-      wrapper.simulate('submit', {
-        preventDefault: preventDefaultSpy,
-      });
-
-      expect(preventDefaultSpy).toHaveBeenCalledTimes(1);
+  describe('action', () => {
+    it('sets the action attribute when provided', () => {
+      const wrapper = mount(<Form action={action} onSubmit={noop} />);
+      expect(wrapper.prop('action')).toBe(action);
     });
   });
 
@@ -35,6 +33,20 @@ describe('<Form />', () => {
     it('sets the autocomplete attribute when provided', () => {
       const wrapper = mount(<Form autoComplete={autoComplete} onSubmit={noop} />);
       expect(wrapper.prop('autoComplete')).toBe(autoComplete);
+    });
+  });
+
+  describe('encType', () => {
+    it('sets the encType attribute when provided', () => {
+      const wrapper = mount(<Form encType={encType} onSubmit={noop} />);
+      expect(wrapper.prop('encType')).toBe(encType);
+    });
+  });
+
+  describe('method', () => {
+    it('sets the method attribute when provided', () => {
+      const wrapper = mount(<Form method={method} onSubmit={noop} />);
+      expect(wrapper.prop('method')).toBe(method);
     });
   });
 
@@ -46,9 +58,68 @@ describe('<Form />', () => {
   });
 
   describe('noValidate', () => {
-    it('sets the nocalidate attribute when provided', () => {
+    it('sets the noValidate attribute when provided', () => {
       const wrapper = mount(<Form noValidate={noValidate} onSubmit={noop} />);
       expect(wrapper.prop('noValidate')).toBe(noValidate);
+    });
+  });
+
+  describe('onSubmit', () => {
+    it('is called when the form is submitted', () => {
+      const spy = jest.fn();
+      const wrapper = mount(<Form onSubmit={spy} />);
+      wrapper.simulate('submit');
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('preventDefault', () => {
+    it('sets the preventDefault attribute when provided', () => {
+      const onSubmitSpy = jest.fn();
+      const preventDefaultSpy = jest.fn();
+
+      const wrapper = mount(<Form preventDefault={preventDefault} onSubmit={onSubmitSpy} />);
+
+      wrapper.simulate('submit', {
+        preventDefault: preventDefaultSpy,
+      });
+
+      expect(preventDefaultSpy).not.toHaveBeenCalled();
+    });
+
+
+    it('submits the form with preventDefault when the prop is not provided', () => {
+      const onSubmitSpy = jest.fn();
+      const preventDefaultSpy = jest.fn();
+
+      const wrapper = mount(<Form onSubmit={onSubmitSpy} />);
+
+      wrapper.simulate('submit', {
+        preventDefault: preventDefaultSpy,
+      });
+
+      expect(preventDefaultSpy).toHaveBeenCalled();
+    });
+
+    it('submits the form with preventDefault when the prop is set to true', () => {
+      const onSubmitSpy = jest.fn();
+      const preventDefaultSpy = jest.fn();
+
+      const wrapper = mount(<Form preventDefault onSubmit={onSubmitSpy} />);
+
+      wrapper.simulate('submit', {
+        preventDefault: preventDefaultSpy,
+      });
+
+      expect(preventDefaultSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('target', () => {
+    it('sets the target attribute when provided', () => {
+      const wrapper = mount(<Form target={target} onSubmit={noop} />);
+      expect(wrapper.prop('target')).toBe(target);
     });
   });
 });
