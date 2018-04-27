0841afda5e9cf14b8357c351c8a3269ff913dbf0
Andrew Musgrave <andrew.musgrave@shopify.com>
2018-04-27 13:05:38 -0400

    Renamed test to run properly

--- src/components/List/tests/List.test.tsx
+++ src/components/List/tests/List.test.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
-import {shallowWithAppProvider} from '../../../../tests/utilities';
+import {mountWithAppProvider} from '../../../../tests/utilities';
 import List from '..';
 
 describe('<List />', () => {
   it('sets the list type to ul when is a bullet list', () => {
-    const list = shallowWithAppProvider(<List type="bullet">test</List>);
+    const list = mountWithAppProvider(<List type="bullet">test</List>);
     expect(list.find('ul').exists()).toBeTruthy();
   });
 
   it('sets the list type to ul when no type is provided', () => {
-    const list = shallowWithAppProvider(<List>test</List>);
+    const list = mountWithAppProvider(<List>test</List>);
     expect(list.find('ul').exists()).toBeTruthy();
   });
 
   it('sets the list type to ol when is a number list', () => {
-    const list = shallowWithAppProvider(<List type="number">test</List>);
+    const list = mountWithAppProvider(<List type="number">test</List>);
     expect(list.find('ol').exists()).toBeTruthy();
   });
 });
