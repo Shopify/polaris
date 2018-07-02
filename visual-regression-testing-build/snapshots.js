7ffc1075295baf7c9814d47d581b158b0911bc17
amrocha <andre.rocha@shopify.com>
2018-07-02 13:56:20 -0700

    Switch to puppeteer instead of wdio wdio makes a of assumptions about the shape of your tests that dont really work for what we're trying to do this ends up making parallelization very complicated to add puppeteer on the other hand is very easy to work asynchronously, so it fits our needs pretty well

--- visual-regression-testing-build/snapshots.js
+++ visual-regression-testing-build/snapshots.js
@@ -1,17 +1,62 @@
-/* global browser, $ */
-/* eslint-disable no-console */
+const puppeteer = require('puppeteer');
+const {Percy, FileSystemAssetLoader} = require('@percy/puppeteer');
 
-describe('Take snapshots', () => {
-  it('Snapshots', () => {
-    browser.url('/');
-    const urlElements = $('a');
-    urlElements.waitForExist(5000);
-    const urls = browser.getAttribute('a', 'href');
-    urls.map((path) => {
+(async () => {
+  const percy = new Percy({
+    loaders: [
+      new FileSystemAssetLoader({
+        buildDir: './visual-regression-testing-build/assets',
+        mountPath: '/assets',
+      }),
+    ],
+  });
+
+  const browsers = [
+    {
+      browser: await puppeteer.launch(),
+      taken: new Promise((resolve) => {
+        resolve();
+      }),
+    },
+    {
+      browser: await puppeteer.launch(),
+      taken: new Promise((resolve) => {
+        resolve();
+      }),
+    },
+  ];
+
+  await browsers.forEach(async (instance) => {
+    instance.page = await instance.browser.newPage();
+  });
+
+  let browserIndex = 0;
+
+  await percy.startBuild();
+
+  // Launch the browser and visit example.com
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000');
+  let urls = await page.evaluate(() =>
+    [...document.querySelectorAll('a')].map((element) =>
+      element.getAttribute('href'),),);
+
+  urls = urls.filter((url, index) => !url.includes('embedded'));
+
+  urls.map((path) => {
+    const currentBrowser = browsers[browserIndex % 2];
+    browserIndex++;
+    currentBrowser.taken = currentBrowser.taken.then(async () => {
       console.log('Snapshotting ', path);
-      browser.url(path);
-      browser.pause(1000);
-      browser.percySnapshot(path);
+      await currentBrowser.page.goto(`http://localhost:3000${path}`);
+      return currentBrowser.page.screenshot({path: 'idk.png'});
+      // return percy.snapshot(`Snapshot of ${path}`, currentBrowser.page);
     });
   });
-});
+
+  await Promise.all(browsers.map((instance) => instance.taken));
+  await percy.finalizeBuild();
+  await browser.close();
+  await Promise.all(browsers.map((instance) => instance.browser.close()));
+})();
