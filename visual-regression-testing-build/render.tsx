de2822b1d750bc015af9c358c37b77f15980800b
Kaelig <kaelig.deloumeauprigent@shopify.com>
2018-07-02 18:05:30 -0700

    Use helmet to render meta tags

--- visual-regression-testing-build/render.tsx
+++ visual-regression-testing-build/render.tsx
@@ -2,17 +2,21 @@
 import * as React from 'react';
 import {renderToString} from 'react-dom/server';
 import HTML, {DOCTYPE} from '@shopify/react-html';
+import Helmet from 'react-helmet';
 
 export default function render(scripts, data) {
   return (
     DOCTYPE +
     renderToString(
       <HTML scripts={scripts} data={data}>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no"
-        />
+        <Helmet>
+          <meta charSet="utf-8" />
+          {/* Same viewport meta as in the admin: */}
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no"
+          />
+        </Helmet>
       </HTML>,
     )
   );
