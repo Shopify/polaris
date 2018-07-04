7e425c5ded68c00c57ee4a835eb24928bdefe9a2
Andrew Musgrave <andrew.musgrave@shopify.com>
2018-07-04 10:40:49 -0400

    Add tooltip and keypresses

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react';
 import {classNames} from '@shopify/react-utilities';
-
-import {withAppProvider, WithAppProviderProps} from '../AppProvider';
+import {
+  withAppProvider,
+  WithAppProviderProps,
+} from '../../components/AppProvider';
 import Icon from '../Icon';
 import UnstyledLink from '../UnstyledLink';
+import ToolTip from '../Tooltip';
+import KeypressListener from '../KeypressListener';
+import {Keys} from '../../types';
 import {handleMouseUpByBlurring} from '../../utilities/focus';
 
 import * as styles from './Pagination.scss';
 
 export interface PaginationDescriptor {
+  /** Keyboard shortcuts for the next button */
+  nextKeys?: Keys[];
+  /** Keyboard shortcuts for the previous button */
+  previousKeys?: Keys[];
+  /** Tooltip for the next button */
+  nextTooltip?: string;
+  /** Tooltip for the previous button */
+  previousTooltip?: string;
   /** The URL of the next page */
   nextURL?: string;
   /** The URL of the previous page */
@@ -39,6 +52,10 @@ function Pagination({
   previousURL,
   onNext,
   onPrevious,
+  nextTooltip,
+  previousTooltip,
+  nextKeys,
+  previousKeys,
   plain,
   accessibilityLabel,
   polaris: {intl},
@@ -59,6 +76,7 @@ function Pagination({
       url={previousURL}
       onMouseUp={handleMouseUpByBlurring}
       aria-label={intl.translate('Polaris.Pagination.previous')}
+      id="previousURL"
     >
       <Icon source="arrowLeft" />
     </UnstyledLink>
@@ -80,6 +98,7 @@ function Pagination({
       url={nextURL}
       onMouseUp={handleMouseUpByBlurring}
       aria-label={intl.translate('Polaris.Pagination.next')}
+      id="nextURL"
     >
       <Icon source="arrowRight" />
     </UnstyledLink>
@@ -95,12 +114,63 @@ function Pagination({
     </button>
   );
 
+  const constructedPrevious = previousTooltip ? (
+    <ToolTip content={previousTooltip}>{previousButton}</ToolTip>
+  ) : (
+    previousButton
+  );
+
+  const constructedNext = nextTooltip ? (
+    <ToolTip content={nextTooltip}>{nextButton}</ToolTip>
+  ) : (
+    nextButton
+  );
+
+  const previousButtonEvents =
+    previousKeys &&
+    (previousURL || onPrevious) &&
+    previousKeys.map((key, idx) => (
+      <KeypressListener
+        key={idx}
+        keyCode={key}
+        handler={
+          previousURL
+            ? clickPaginationLink('previousURL')
+            : (onPrevious as () => void)
+        }
+      />
+    ));
+
+  const nextButtonEvents =
+    nextKeys &&
+    (nextURL || onNext) &&
+    nextKeys.map((key, idx) => (
+      <KeypressListener
+        key={idx}
+        keyCode={key}
+        handler={
+          nextURL ? clickPaginationLink('nextURL') : (onNext as () => void)
+        }
+      />
+    ));
+
   return (
     <nav className={className} aria-label={label}>
-      {previousButton}
-      {nextButton}
+      {previousButtonEvents}
+      {constructedPrevious}
+      {nextButtonEvents}
+      {constructedNext}
     </nav>
   );
+}
+
+function clickPaginationLink(id: string) {
+  return () => {
+    const link = document.getElementById(id);
+    if (link) {
+      link.click();
+    }
+  };
 }
 
 export default withAppProvider<Props>()(Pagination);
