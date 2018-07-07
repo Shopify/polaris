34b4aae0b8c27c478cd5213c78943d9d0629015d
Tim Layton <tim.layton@shopify.com>
2018-07-07 10:47:18 -0700

    add tests for easdk calls

--- src/components/Loading/tests/Loading.test.tsx
+++ src/components/Loading/tests/Loading.test.tsx
@@ -6,20 +6,33 @@ import Loading from '../Loading';
 
 describe('<Loading />', () => {
   it('starts loading on mount', () => {
-    const {frame} = mountWithContext(<Loading />);
+    const {frame} = mountWithFrame(<Loading />);
     expect(frame.startLoading).toHaveBeenCalled();
   });
 
   it('stops loading on unmount', () => {
-    const {loading, frame} = mountWithContext(<Loading />);
+    const {loading, frame} = mountWithFrame(<Loading />);
     expect(frame.stopLoading).not.toHaveBeenCalled();
 
     loading.unmount();
     expect(frame.stopLoading).toHaveBeenCalled();
   });
+
+  it('starts easdk loading on mount', () => {
+    const {easdk} = mountWithEasdk(<Loading />);
+    expect(easdk.startLoading).toHaveBeenCalled();
+  });
+
+  it('stops easdk loading on unmount', () => {
+    const {loading, easdk} = mountWithEasdk(<Loading />);
+    expect(easdk.stopLoading).not.toHaveBeenCalled();
+
+    loading.unmount();
+    expect(easdk.stopLoading).toHaveBeenCalled();
+  });
 });
 
-function mountWithContext(element: React.ReactElement<any>) {
+function mountWithFrame(element: React.ReactElement<any>) {
   const frame = {startLoading: jest.fn(), stopLoading: jest.fn()};
   const loading = mountWithAppProvider(element, {
     context: {frame},
@@ -27,4 +40,14 @@ function mountWithContext(element: React.ReactElement<any>) {
   });
 
   return {loading, frame};
+}
+
+function mountWithEasdk(element: React.ReactElement<any>) {
+  const easdk = {startLoading: jest.fn(), stopLoading: jest.fn()};
+  const loading = mountWithAppProvider(element, {
+    context: {frame: {}, easdk},
+    childContextTypes: {frame: PropTypes.any},
+  });
+
+  return {loading, easdk};
 }
