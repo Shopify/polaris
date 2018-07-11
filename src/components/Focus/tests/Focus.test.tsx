725004cdf641ed117e8881b1fe305a97452438d0
Andrew Musgrave <andrew.musgrave@shopify.com>
2018-07-11 18:11:35 -0400

    Add tests

---
+++ src/components/Focus/tests/Focus.test.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import {mountWithAppProvider} from '../../../../tests/utilities';
+import Focus from '../Focus';
+
+describe('<Focus />', () => {
+  let requestAnimationFrameSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    requestAnimationFrameSpy = jest.spyOn(window, 'requestAnimationFrame');
+    requestAnimationFrameSpy.mockImplementation((cb) => cb());
+  });
+
+  afterEach(() => {
+    requestAnimationFrameSpy.mockRestore();
+  });
+
+  it('mounts', () => {
+    const focus = mountWithAppProvider(
+      <Focus>
+        <div />
+      </Focus>,
+    );
+
+    expect(focus.exists()).toBe(true);
+  });
+
+  it('will not focus any element if none are natively focusable', () => {
+    mountWithAppProvider(
+      <Focus>
+        <div>
+          <span />
+        </div>,
+      </Focus>,
+    );
+
+    expect(document.body).toBe(document.activeElement);
+  });
+
+  it('will focus first focusable node', () => {
+    const focus = mountWithAppProvider(
+      <Focus>
+        <div>
+          <input />
+        </div>,
+      </Focus>,
+    );
+
+    const input = focus.find('input').getDOMNode();
+    expect(input).toBe(document.activeElement);
+  });
+});
