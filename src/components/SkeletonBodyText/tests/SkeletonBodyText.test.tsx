878e0dfb677285505e249d662956e5d93d282ba9
Solona Armstrong <solona.armstrong@shopify.com>
2018-09-04 22:50:20 -0700

    Add tests for skeleton page display and body

---
+++ src/components/SkeletonBodyText/tests/SkeletonBodyText.test.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {mountWithAppProvider} from '../../../../tests/utilities';
+import SkeletonBodyText from '../SkeletonBodyText';
+
+describe('<SkeletonBodyText />', () => {
+  it('renders the amount of lines provided', () => {
+    const skeletonBodyText = mountWithAppProvider(
+      <SkeletonBodyText lines={2} />,
+    );
+    expect(
+      skeletonBodyText
+        .find('div')
+        .first()
+        .children().length,
+    ).toBe(2);
+  });
+
+  it('renders 3 lines if none are provided', () => {
+    const skeletonBodyText = mountWithAppProvider(<SkeletonBodyText />);
+    expect(
+      skeletonBodyText
+        .find('div')
+        .first()
+        .children().length,
+    ).toBe(3);
+  });
+});
