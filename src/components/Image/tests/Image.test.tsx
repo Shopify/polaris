2a5d3d5c609b176de0e634a6bd19b98ad400bb82
Solona Armstrong <solona.armstrong@shopify.com>
2018-09-08 19:12:39 -0700

    Declare component and use beforeAll

--- src/components/Image/tests/Image.test.tsx
+++ src/components/Image/tests/Image.test.tsx
@@ -4,9 +4,15 @@ import Image from '..';
 
 describe('<Image />', () => {
   describe('accessibilityLabel', () => {
-    const src =
-      'https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg';
-    const image = mountWithAppProvider(<Image alt="alt text" source={src} />);
+    let src: string;
+    let image: any;
+
+    beforeAll(() => {
+      src =
+        'https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg';
+      image = mountWithAppProvider(<Image alt="alt text" source={src} />);
+    });
+
     it('renders the src', () => {
       expect(image.find('img').prop('src')).toBe(src);
     });
