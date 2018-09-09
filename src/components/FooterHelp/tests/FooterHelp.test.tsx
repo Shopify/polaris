00ea5d0978ea7318c6d03be7144261c0fb57cb72
Solona Armstrong <solona.armstrong@shopify.com>
2018-09-08 19:09:38 -0700

    Declare component and use beforeAll

--- src/components/FooterHelp/tests/FooterHelp.test.tsx
+++ src/components/FooterHelp/tests/FooterHelp.test.tsx
@@ -4,8 +4,13 @@ import FooterHelp from '..';
 import Icon from '../../Icon';
 
 describe('<FooterHelp />', () => {
-  const children = 'Learn more about fulfilling orders';
-  const footerHelp = mountWithAppProvider(<FooterHelp>{children}</FooterHelp>);
+  let children: string;
+  let footerHelp: any;
+
+  beforeAll(() => {
+    children = 'Learn more about fulfilling orders';
+    footerHelp = mountWithAppProvider(<FooterHelp>{children}</FooterHelp>);
+  });
 
   it('renders its children', () => {
     expect(footerHelp.contains(children)).toBe(true);
