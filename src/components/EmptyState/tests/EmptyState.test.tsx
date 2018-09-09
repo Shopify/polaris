982b963e27a17926cfa5ac9bd943a349b3341b0f
Solona Armstrong <solona.armstrong@shopify.com>
2018-09-08 18:56:28 -0700

    Declare props in beforeAll

--- src/components/EmptyState/tests/EmptyState.test.tsx
+++ src/components/EmptyState/tests/EmptyState.test.tsx
@@ -6,22 +6,27 @@ import Image from '../../Image';
 import DisplayText from '../../DisplayText';
 
 describe('<EmptyState />', () => {
-  const imgSrc =
-    'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+  let imgSrc: string;
+  let emptyState: any;
 
-  const emptyState = mountWithAppProvider(
-    <EmptyState
-      heading="Manage your inventory transfers"
-      action={{content: 'Add transfer'}}
-      image={imgSrc}
-      secondaryAction={{
-        content: 'Learn more',
-        url: 'https://help.shopify.com',
-      }}
-    >
-      <p>Track and receive your incoming inventory from suppliers.</p>
-    </EmptyState>,
-  );
+  beforeAll(() => {
+    imgSrc =
+      'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
+    emptyState = mountWithAppProvider(
+      <EmptyState
+        heading="Manage your inventory transfers"
+        action={{content: 'Add transfer'}}
+        image={imgSrc}
+        secondaryAction={{
+          content: 'Learn more',
+          url: 'https://help.shopify.com',
+        }}
+      >
+        <p>Track and receive your incoming inventory from suppliers.</p>
+      </EmptyState>,
+    );
+  });
 
   it('renders a button with the action content', () => {
     expect(emptyState.find('button').contains('Add transfer')).toBe(true);
