0460fee7998001f87d001415588c5baef808bc53
Koen Vendrik <k.vendrik@gmail.com>
2018-10-22 14:56:28 -0400

    adds script to transform absolute imports to relative ones

---
+++ scripts/ts-imports-transform.js
@@ -0,0 +1,41 @@
+import {resolve, relative, dirname} from 'path';
+import {readFileSync, readdirSync, writeFileSync} from 'fs';
+import {sync as globSync} from 'glob';
+import {compilerOptions} from '../tsconfig.json';
+
+const ROOT_PATH = resolve(__dirname, '..');
+const BASE_PATH = resolve(ROOT_PATH, compilerOptions.baseUrl);
+const TYPES_PATH = resolve(ROOT_PATH, compilerOptions.declarationDir);
+
+const typeFiles = globSync(`${TYPES_PATH}/**/*.d.ts`);
+const moduleImportOptionsRegex = getModuleImportOptionsRegex();
+const moduleImportRegex = new RegExp(
+  `import [^f]+ from '(${moduleImportOptionsRegex}(/[^']+)?)';`,
+  'g',
+);
+
+for (const typeFile of typeFiles) {
+  const contents = readFileSync(typeFile, 'utf-8');
+  const newContents = replaceAbsoluteImports(typeFile, contents);
+  writeFileSync(typeFile, newContents);
+}
+
+function replaceAbsoluteImports(path, contents) {
+  return contents.replace(moduleImportRegex, (match, importPath) =>
+    match.replace(importPath, absoluteToRelativeImportPath(path, importPath)),
+  );
+}
+
+function absoluteToRelativeImportPath(filePath, importPath) {
+  return relative(dirname(filePath), `${TYPES_PATH}/${importPath}`);
+}
+
+function getModuleImportOptionsRegex() {
+  const moduleImportOptions = readdirSync(BASE_PATH);
+  const moduleRegexOptions = moduleImportOptions
+    .map((moduleName) =>
+      moduleName.replace(/\.tsx?/, '').replace(/([.-])/g, '\\$1'),
+    )
+    .join('|');
+  return `(${moduleRegexOptions})`;
+}
