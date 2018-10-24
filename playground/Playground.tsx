de1a5e15cbad1615fc9d3150f729be8e0f41dddc
Koen Vendrik <k.vendrik@gmail.com>
2018-10-24 11:53:19 -0400

    rm debug code

--- playground/Playground.tsx
+++ playground/Playground.tsx
@@ -7,21 +7,7 @@ export default class Playground extends React.Component<never, State> {
   render() {
     return (
       <AppProvider>
-        <Page
-          breadcrumbs={[{content: 'Products', url: '/products'}]}
-          title="Jar With Lock-Lid"
-          primaryAction={{content: 'Save', disabled: true}}
-          secondaryActions={[
-            {content: 'Duplicate'},
-            {content: 'View on your store'},
-          ]}
-          pagination={{
-            hasPrevious: true,
-            hasNext: true,
-          }}
-        >
-          <p>Page content</p>
-        </Page>
+        <Page title="Playground" />
       </AppProvider>
     );
   }
