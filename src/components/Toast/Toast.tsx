989f0595017bb0388078a69dfd592b7859befaa8
Tim Layton <tim.layton@shopify.com>
2018-10-29 16:25:36 -0700

    update app bridge to 0.8.0

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {createUniqueIDFactory} from '@shopify/javascript-utilities/other';
-import {Flash as AppBridgeToast} from '@shopify/app-bridge/actions';
+import {Toast as AppBridgeToast} from '@shopify/app-bridge/actions';
 
 import {FrameContext, frameContextTypes} from '../types';
 import {withAppProvider, WithAppProviderProps} from '..';
@@ -30,7 +30,7 @@ export class Toast extends React.PureComponent<ComposedProps, never> {
   context: FrameContext;
 
   private id = createId();
-  private appBridgeToast: AppBridgeToast.Flash | undefined;
+  private appBridgeToast: AppBridgeToast.Toast | undefined;
 
   componentDidMount() {
     const {context, id, props} = this;
