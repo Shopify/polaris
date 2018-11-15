bfadbaa661d914062affd6f2e625c6dcf2a54387
Andrew Musgrave <andrew.musgrave@shopify.com>
2018-11-15 15:40:59 -0500

    Add test

--- src/components/EventListener/tests/EventListener.test.tsx
+++ src/components/EventListener/tests/EventListener.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {mountWithAppProvider} from 'test-utilities';
+import {noop} from '@shopify/javascript-utilities/other';
 import EventListener from '../EventListener';
 
 describe('<EventListener />', () => {
@@ -13,6 +14,16 @@ describe('<EventListener />', () => {
   it('does not call handler when a different event is fired', () => {
     const spy = jest.fn();
     mountWithAppProvider(<EventListener event="click" handler={spy} />);
+    window.dispatchEvent(new Event('resize'));
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('removes listener on update', () => {
+    const spy = jest.fn();
+    const eventListener = mountWithAppProvider(
+      <EventListener event="resize" handler={spy} />,
+    );
+    eventListener.setProps({event: 'scroll', handler: noop});
     window.dispatchEvent(new Event('resize'));
     expect(spy).not.toHaveBeenCalled();
   });
