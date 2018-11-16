79cdb28dde1d6898677dbc6d872f50f74817b290
Daniel Leroux <dleroux@users.noreply.github.com>
2018-11-16 16:15:52 -0500

    Revert "Update Avatar and Image component to handle onError with images."

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {classNames, variationName} from '@shopify/react-utilities/styles';
-import {autobind} from '@shopify/javascript-utilities/decorators';
 
 import {withAppProvider, WithAppProviderProps} from '../AppProvider';
 import Image from '../Image';
@@ -29,131 +28,89 @@ export interface Props {
   initials?: string;
   /** Whether the avatar is for a customer */
   customer?: boolean;
-  /** URL of the avatar image which falls back to initials if the image fails to load */
+  /** URL of the avatar image */
   source?: string;
   /** Accessible label for the avatar image */
   accessibilityLabel?: string;
 }
 
-export interface State {
-  hasError: boolean;
-  hasLoaded: boolean;
-}
-
 export type CombinedProps = Props & WithAppProviderProps;
 
-export class Avatar extends React.PureComponent<CombinedProps, State> {
-  state: State = {
-    hasError: false,
-    hasLoaded: false,
-  };
+function Avatar({
+  name,
+  source,
+  initials,
+  customer,
+  size = 'medium',
+  accessibilityLabel,
+  polaris: {intl},
+}: CombinedProps) {
+  const nameString = name || initials;
 
-  componentDidUpdate({source: previousSource}: Props) {
-    const {source} = this.props;
-    const {hasError, hasLoaded} = this.state;
-    if (previousSource === source || (!hasError && !hasLoaded)) {
-      return;
-    }
-    // eslint-disable-next-line react/no-did-update-set-state
-    this.setState({hasError: false, hasLoaded: false});
+  let finalSource: string | undefined;
+  let label: string | undefined;
+
+  if (accessibilityLabel) {
+    label = accessibilityLabel;
+  } else if (name) {
+    label = name;
+  } else if (initials) {
+    const splitInitials = initials.split('').join(' ');
+    label = intl.translate('Polaris.Avatar.labelWithInitials', {
+      initials: splitInitials,
+    });
+  } else {
+    label = intl.translate('Polaris.Avatar.label');
   }
 
-  render() {
-    const {
-      name,
-      source,
-      initials,
-      customer,
-      size = 'medium',
-      accessibilityLabel,
-      polaris: {intl},
-    } = this.props;
-
-    const {hasError, hasLoaded} = this.state;
-
-    const hasImage = (source || customer) && !hasError;
-
-    const nameString = name || initials;
-
-    let finalSource: string | undefined;
-    let label: string | undefined;
-
-    if (accessibilityLabel) {
-      label = accessibilityLabel;
-    } else if (name) {
-      label = name;
-    } else if (initials) {
-      const splitInitials = initials.split('').join(' ');
-      label = intl.translate('Polaris.Avatar.labelWithInitials', {
-        initials: splitInitials,
-      });
-    } else {
-      label = intl.translate('Polaris.Avatar.label');
-    }
-
-    if (source) {
-      finalSource = source;
-    } else if (customer) {
-      finalSource = customerPlaceholder(nameString);
-    }
-
-    const className = classNames(
-      styles.Avatar,
-      styles[variationName('style', styleClass(nameString))],
-      size && styles[variationName('size', size)],
-      hasImage && !hasLoaded && styles.hidden,
-      hasImage && styles.hasImage,
-    );
-
-    const imageMarkUp = finalSource ? (
-      <Image
-        className={styles.Image}
-        source={finalSource}
-        alt=""
-        role="presentation"
-        onLoad={this.handleLoad}
-        onError={this.handleError}
-      />
-    ) : null;
-
-    // Use `dominant-baseline: central` instead of `dy` when Edge supports it.
-    const verticalOffset = '0.35em';
-
-    const initialsMarkup =
-      initials && !hasImage ? (
-        <span className={styles.Initials}>
-          <svg className={styles.Svg} viewBox="0 0 48 48">
-            <text
-              x="50%"
-              y="50%"
-              dy={verticalOffset}
-              fill="currentColor"
-              fontSize="26"
-              textAnchor="middle"
-            >
-              {initials}
-            </text>
-          </svg>
-        </span>
-      ) : null;
-
-    return (
-      <span aria-label={label} role="img" className={className}>
-        {initialsMarkup}
-        {imageMarkUp}
-      </span>
-    );
+  if (source) {
+    finalSource = source;
+  } else if (customer) {
+    finalSource = customerPlaceholder(nameString);
   }
 
-  @autobind
-  handleError() {
-    this.setState({hasError: true, hasLoaded: false});
-  }
+  const className = classNames(
+    styles.Avatar,
+    styles[variationName('style', styleClass(nameString))],
+    size && styles[variationName('size', size)],
+    finalSource && styles.hasImage,
+  );
 
-  @autobind
-  handleLoad() {
-    this.setState({hasLoaded: true, hasError: false});
-  }
+  const imageMarkUp = finalSource ? (
+    <Image
+      className={styles.Image}
+      source={finalSource}
+      alt=""
+      role="presentation"
+    />
+  ) : null;
+
+  // Use `dominant-baseline: central` instead of `dy` when Edge supports it.
+  const verticalOffset = '0.35em';
+
+  const initialsMarkup = initials ? (
+    <span className={styles.Initials}>
+      <svg className={styles.Svg} viewBox="0 0 48 48">
+        <text
+          x="50%"
+          y="50%"
+          dy={verticalOffset}
+          fill="currentColor"
+          fontSize="26"
+          textAnchor="middle"
+        >
+          {initials}
+        </text>
+      </svg>
+    </span>
+  ) : null;
+
+  return (
+    <span aria-label={label} role="img" className={className}>
+      {initialsMarkup}
+      {imageMarkUp}
+    </span>
+  );
 }
 
 function styleClass(name?: string) {
--- src/components/Image/tests/Image.test.tsx
+++ src/components/Image/tests/Image.test.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react';
-import {mountWithAppProvider, trigger} from 'test-utilities';
+import {mountWithAppProvider} from 'test-utilities';
 import Image from '../Image';
 
 describe('<Image />', () => {
-  describe('img attributes', () => {
+  describe('accessibilityLabel', () => {
     let src: string;
     let image: any;
 
     beforeAll(() => {
       src =
         'https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg';
-      image = mountWithAppProvider(
-        <Image alt="alt text" source={src} crossOrigin="Anonymous" />,
-      );
+      image = mountWithAppProvider(<Image alt="alt text" source={src} />);
     });
 
     it('renders the src', () => {
@@ -21,34 +19,6 @@ describe('<Image />', () => {
 
     it('renders the alt text', () => {
       expect(image.find('img').prop('alt')).toBe('alt text');
-    });
-
-    it('renders the crossOrigin', () => {
-      expect(image.find('img').prop('crossOrigin')).toBe('Anonymous');
-    });
-  });
-
-  describe('onError', () => {
-    it('calls the onError callback when the image onError is triggered', () => {
-      const spy = jest.fn();
-      const image = mountWithAppProvider(
-        <Image alt="alt text" source="404" onError={spy} />,
-      );
-
-      trigger(image.find('img'), 'onError');
-      expect(spy).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('onLoad', () => {
-    it('calls the onLoad callback when the image on onLoad is triggered', () => {
-      const spy = jest.fn();
-      const image = mountWithAppProvider(
-        <Image alt="alt text" source="/path/to/image" onLoad={spy} />,
-      );
-
-      trigger(image.find('img'), 'onLoad');
-      expect(spy).toHaveBeenCalledTimes(1);
     });
   });
 });
