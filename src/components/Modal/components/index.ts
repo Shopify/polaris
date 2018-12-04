865f6c6c0e159f789a7ef59f204a67a6920ff436
Koen Vendrik <k.vendrik@gmail.com>
2018-12-04 14:40:39 -0500

    fixes close button issue

--- src/components/Modal/components/index.ts
+++ src/components/Modal/components/index.ts
@@ -2,3 +2,4 @@ export {default as Dialog, DialogProps} from './Dialog';
 export {default as Footer, FooterProps} from './Footer';
 export {default as Header, HeaderProps} from './Header';
 export {default as Section, SectionProps} from './Section';
+export {default as CloseButton, CloseButtonProps} from './CloseButton';
