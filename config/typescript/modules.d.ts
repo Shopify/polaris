7fc71af199f29aa65338f2c37cc1497d664a6bfc
Ben Scott <ben.scott@shopify.com>
2018-12-10 14:17:59 -0800

    Remove workaround for MediaQueryList typings

--- config/typescript/modules.d.ts
+++
@@ -1,9 +0,0 @@
-// The version of jest-dom-mocks we use references this interface which  was
-// built into Typescript but then removed in TypeScript 3.1.0 as per
-// https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#some-vendor-specific-types-are-removed-from-libdts
-// This can be removed once we update to use a version of jest-dom-mocks that
-// includes https://github.com/Shopify/quilt/pull/426, which removes the
-// reference to this interface
-interface MediaQueryListListener {
-  (mql: MediaQueryList): void;
-}
