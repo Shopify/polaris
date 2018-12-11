Shopify/polaris

commit 266f1297e15ac9b41737627507f738c4ccebcdf3

Final remove cyclical types (#698)
* Fix ContextualSaveBar circular deps
* Fix ToastProps circular deps
* Move Frame related types into Frame Component
* Remove dependency-cruiser as eslint's no-cycle rule takes care of circular import hunting

src/components/index.ts
@@ -60,10 +60,7 @@ export {default as ColorPicker, Props as ColorPickerProps} from './ColorPicker';
 
 export {default as Connected, Props as ConnectedProps} from './Connected';
 
-export {
-  default as ContextualSaveBar,
-  ContextualSaveBarProps,
-} from './ContextualSaveBar';
+export {default as ContextualSaveBar} from './ContextualSaveBar';
 
 export {
   default as DataTable,
@@ -118,7 +115,11 @@ export {default as Form, Props as FormProps} from './Form';
 
 export {default as FormLayout, Props as FormLayoutProps} from './FormLayout';
 
-export {default as Frame, Props as FrameProps} from './Frame';
+export {
+  default as Frame,
+  Props as FrameProps,
+  DEFAULT_TOAST_DURATION,
+} from './Frame';
 
 export {default as Heading, Props as HeadingProps} from './Heading';
 
@@ -240,11 +241,7 @@ export {
   Context as ThemeProviderContext,
 } from './ThemeProvider';
 
-export {
-  default as Toast,
-  Props as ToastProps,
-  DEFAULT_TOAST_DURATION,
-} from './Toast';
+export {default as Toast} from './Toast';
 
 export {default as Tooltip, Props as TooltipProps} from './Tooltip';
 

src/components/ContextualSaveBar/index.ts
@@ -1,4 +1,3 @@
 import ContextualSaveBar from './ContextualSaveBar';
 
 export default ContextualSaveBar;
-export {Props as ContextualSaveBarProps} from './ContextualSaveBar';

src/components/Frame/components/ContextualSaveBar/index.ts
@@ -1,4 +1,3 @@
 import ContextualSaveBar from './ContextualSaveBar';
 
-export {Props} from './ContextualSaveBar';
 export default ContextualSaveBar;

src/components/Frame/components/Toast/index.ts
@@ -1,4 +1,1 @@
-import Toast from './Toast';
-
-export {Props} from './Toast';
-export default Toast;
+export {default, DEFAULT_TOAST_DURATION} from './Toast';

src/components/Frame/components/index.ts
@@ -1,10 +1,7 @@
-export {default as Toast, Props as ToastProps} from './Toast';
+export {default as Toast, DEFAULT_TOAST_DURATION} from './Toast';
 export {
   default as ToastManager,
   Props as ToastManagerProps,
 } from './ToastManager';
 export {default as Loading, Props as LoadingProps} from './Loading';
-export {
-  default as ContextualSaveBar,
-  Props as ContextualSaveBarProps,
-} from './ContextualSaveBar';
+export {default as ContextualSaveBar} from './ContextualSaveBar';

src/components/Frame/index.ts
@@ -1,4 +1,14 @@
 import Frame from './Frame';
 
 export {Props} from './Frame';
+
+export {DEFAULT_TOAST_DURATION} from './components';
+
+export {
+  ContextualSaveBarProps,
+  FrameContext,
+  frameContextTypes,
+  ToastProps,
+} from './types';
+
 export default Frame;

src/components/Toast/index.ts
@@ -1,4 +1,3 @@
 import Toast from './Toast';
 
-export {Props, DEFAULT_TOAST_DURATION} from './Toast';
 export default Toast;

.eslintrc
@@ -28,7 +28,6 @@
         "allowBlockStart": false
       }
     ],
-    "import/no-cycle": "off",
     "import/no-named-as-default": "off",
     "react/button-has-type": "off",
     "react/no-array-index-key": "off",

commit af8c2ed04f5593b5259850c1bbaf49bc26e350ac

Export ContextualSaveBarProps from src/components/index
266f1297 accidentally removed this import as part of refactoring

src/components/index.ts
@@ -135,6 +135,7 @@ export {default as FormLayout, Props as FormLayoutProps} from './FormLayout';
 export {
   default as Frame,
   Props as FrameProps,
+  ContextualSaveBarProps,
   ToastProps,
   DEFAULT_TOAST_DURATION,
 } from './Frame';