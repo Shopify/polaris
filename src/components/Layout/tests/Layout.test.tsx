codebkp/polaris-react

Adds evenWidth prop to layout component

src/components/Layout/tests/Layout.test.tsx
@@ -42,6 +42,12 @@ describe('<Layout />', () => {
 
       expect(section.prop('secondary')).toBe(true);
     });
+
+    it('renders an even width section', () => {
+      const section = mountWithAppProvider(<Layout.Section evenWidth />);
+
+      expect(section.prop('evenWidth')).toBe(true);
+    });
   });
 
   describe('<Layout.AnnotatedSection />', () => {

Shopify/polaris

src/components/Layout/tests/Layout.test.tsx
@@ -42,6 +42,18 @@ describe('<Layout />', () => {
 
       expect(section.prop('secondary')).toBe(true);
     });
+
+    it('renders a half width section', () => {
+      const section = mountWithAppProvider(<Layout.Section oneHalf />);
+
+      expect(section.prop('oneHalf')).toBe(true);
+    });
+
+    it('renders a third width section', () => {
+      const section = mountWithAppProvider(<Layout.Section oneThird />);
+
+      expect(section.prop('oneThird')).toBe(true);
+    });
   });
 
   describe('<Layout.AnnotatedSection />', () => {