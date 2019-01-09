dbb763a988c1e53b4b11a7096ae89ada105fecb4
Dan Rosenthal <dan.rosenthal@shopify.com>
2019-01-09 14:22:23 -0500

    add tests for default props

--- src/components/RangeSlider/tests/RangeSlider.test.tsx
+++ src/components/RangeSlider/tests/RangeSlider.test.tsx
@@ -3,29 +3,130 @@ import {noop} from '@shopify/javascript-utilities/other';
 import {mountWithAppProvider} from 'test-utilities';
 import RangeSlider from '../RangeSlider';
 import {DualThumb, SingleThumb} from '../components';
+import {DEFAULT_RANGE_SLIDER_PROPS} from '../utilities';
 
-const mockProps = {
-  id: 'MyRangeSlider',
+const mockRangeSliderProps = {
   label: 'RangeSlider',
-  min: 10,
-  max: 20,
-  step: 0.5,
   onChange: noop,
 };
 
 describe('<RangeSlider />', () => {
-  it('renders a dual thumb if value is a tuple', () => {
-    const element = mountWithAppProvider(
-      <RangeSlider value={[0, 25]} {...mockProps} />,
-    );
+  describe('<DualThumb />', () => {
+    it('renders a dual thumb if value is a tuple', () => {
+      const element = mountWithAppProvider(
+        <RangeSlider value={[0, 25]} {...mockRangeSliderProps} />,
+      );
 
-    expect(element.find(DualThumb)).toHaveLength(1);
+      expect(element.find(DualThumb)).toHaveLength(1);
+    });
+
+    it('passes default props to dual thumb', () => {
+      const element = mountWithAppProvider(
+        <RangeSlider value={[0, 25]} {...mockRangeSliderProps} />,
+      );
+
+      const elementProps = element.find(DualThumb).props();
+
+      expect({
+        min: elementProps.min,
+        max: elementProps.max,
+        step: elementProps.step,
+      }).toEqual({
+        min: DEFAULT_RANGE_SLIDER_PROPS.min,
+        max: DEFAULT_RANGE_SLIDER_PROPS.max,
+        step: DEFAULT_RANGE_SLIDER_PROPS.step,
+      });
+    });
+
+    it('passes overrides to default props to dual thumb', () => {
+      const overrideProps = {
+        id: 'MyRangeSlider',
+        min: 99,
+        max: 999,
+        step: 9,
+      };
+
+      const element = mountWithAppProvider(
+        <RangeSlider
+          value={[0, 25]}
+          {...mockRangeSliderProps}
+          {...overrideProps}
+        />,
+      );
+
+      const elementProps = element.find(DualThumb).props();
+
+      expect({
+        min: elementProps.min,
+        max: elementProps.max,
+        step: elementProps.step,
+        id: elementProps.id,
+      }).toEqual(overrideProps);
+    });
+
+    it('passes an id to dual thumb', () => {
+      const element = mountWithAppProvider(
+        <RangeSlider value={[0, 25]} {...mockRangeSliderProps} />,
+      );
+
+      expect(element.find(DualThumb).props().id).not.toBeNull();
+    });
   });
 
-  it('renders a single thumb if value is a single number', () => {
-    const element = mountWithAppProvider(
-      <RangeSlider value={25} {...mockProps} />,
-    );
-    expect(element.find(SingleThumb)).toHaveLength(1);
+  describe('<SingleThumb />', () => {
+    it('renders a single thumb if value is a single number', () => {
+      const element = mountWithAppProvider(
+        <RangeSlider value={25} {...mockRangeSliderProps} />,
+      );
+      expect(element.find(SingleThumb)).toHaveLength(1);
+    });
+
+    it('passes default props to single thumb', () => {
+      const element = mountWithAppProvider(
+        <RangeSlider value={25} {...mockRangeSliderProps} />,
+      );
+
+      const elementProps = element.find(SingleThumb).props();
+
+      expect({
+        min: elementProps.min,
+        max: elementProps.max,
+        step: elementProps.step,
+      }).toEqual({
+        min: DEFAULT_RANGE_SLIDER_PROPS.min,
+        max: DEFAULT_RANGE_SLIDER_PROPS.max,
+        step: DEFAULT_RANGE_SLIDER_PROPS.step,
+      });
+    });
+
+    it('passes an id to single thumb', () => {
+      const element = mountWithAppProvider(
+        <RangeSlider value={25} {...mockRangeSliderProps} />,
+      );
+
+      expect(element.find(SingleThumb).props().id).not.toBeNull();
+    });
+
+    it('passes overrides to default props to single thumb', () => {
+      const overrideProps = {
+        id: 'MyRangeSlider',
+        min: 99,
+        max: 999,
+        step: 9,
+      };
+
+      const element = mountWithAppProvider(
+        <RangeSlider value={25} {...mockRangeSliderProps} {...overrideProps} />,
+      );
+
+      const elementProps = element.find(SingleThumb).props();
+
+      expect({
+        min: elementProps.min,
+        max: elementProps.max,
+        step: elementProps.step,
+        id: elementProps.id,
+      }).toEqual(overrideProps);
+    });
   });
 });
