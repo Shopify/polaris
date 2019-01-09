3733e43e3c84d2c2ef775cfc6d4d7034b9478aa3
Dan Rosenthal <dan.rosenthal@shopify.com>
2019-01-09 14:22:23 -0500

    add tests for default props

--- src/components/RangeSlider/utilities/index.ts
+++ src/components/RangeSlider/utilities/index.ts
@@ -1,2 +1,7 @@
 export const CSS_VAR_PREFIX = '--Polaris-RangeSlider-';
+export const DEFAULT_RANGE_SLIDER_PROPS = {
+  min: 0,
+  max: 100,
+  step: 1,
+};
 export {default as invertNumber} from './invertNumber';
