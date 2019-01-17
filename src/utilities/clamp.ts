upschmidtcreek/polaris-react

Clamp single thumb value

src/utilities/clamp.ts
@@ -1,0 +1,5 @@
+export default function clamp(number: number, lower: number, upper: number) {
+  if (number < lower) return lower;
+  if (number > upper) return upper;
+  return number;
+}

Shopify/polaris

src/utilities/clamp.ts
@@ -1,0 +1,5 @@
+export default function clamp(number: number, min: number, max: number) {
+  if (number < min) return min;
+  if (number > max) return max;
+  return number;
+}