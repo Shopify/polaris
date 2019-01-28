df62241a8815c19aca1461ba2ae7b7037b33496a
Koen Vendrik <k.vendrik@gmail.com>
2019-01-28 13:48:56 -0500

    adds explaination and changelog entry

--- src/components/TopBar/components/UserMenu/context/Consumer.ts
+++ src/components/TopBar/components/UserMenu/context/Consumer.ts
@@ -1,3 +1,5 @@
+// Variable prefixed with underscore to prevent unused variable error.
+// More info here: https://github.com/Shopify/polaris-react/pull/938
 import * as _React from 'react';
 import UserMenuContext from './context';
 
