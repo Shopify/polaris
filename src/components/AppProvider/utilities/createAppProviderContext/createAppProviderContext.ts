f1b1030531c1d166a4ede1eb86eb89f2be217cd4
Hanna Chen <hanna.chen@shopify.com>
2019-02-05 01:08:33 -0500

    <AppProvider>: allow App Bridge to handle server side response before defaulting to `undefined`

--- src/components/AppProvider/utilities/createAppProviderContext/createAppProviderContext.ts
+++ src/components/AppProvider/utilities/createAppProviderContext/createAppProviderContext.ts
@@ -1,6 +1,5 @@
 import {noop} from '@shopify/javascript-utilities/other';
 import createApp, {getShopOrigin} from '@shopify/app-bridge';
-import {isServer} from '@shopify/react-utilities/target';
 import {AppProviderProps, Context} from '../../types';
 import {StickyManager} from '../withSticky';
 import ScrollLockManager from '../ScrollLockManager';
@@ -13,25 +12,6 @@ export interface CreateAppProviderContext extends AppProviderProps {
   subscribe?(callback: () => void): void;
   unsubscribe?(callback: () => void): void;
 }
-
-const serverAppBridge = {
-  dispatch<A>() {
-    return {} as A;
-  },
-  error() {
-    return noop;
-  },
-  featuresAvailable() {
-    return new Promise(noop);
-  },
-  getState() {
-    return new Promise(noop);
-  },
-  localOrigin: '',
-  subscribe() {
-    return noop;
-  },
-};
 
 export default function createAppProviderContext({
   i18n,
@@ -46,18 +26,13 @@ export default function createAppProviderContext({
 }: CreateAppProviderContext = {}): Context {
   const intl = new Intl(i18n);
   const link = new Link(linkComponent);
-
-  let appBridge;
-
-  if (apiKey) {
-    appBridge = isServer
-      ? serverAppBridge
-      : createApp({
-          apiKey,
-          shopOrigin: shopOrigin || getShopOrigin(),
-          forceRedirect,
-        });
-  }
+  const appBridge = apiKey
+    ? createApp({
+        apiKey,
+        shopOrigin: shopOrigin || getShopOrigin(),
+        forceRedirect,
+      })
+    : undefined;
 
   return {
     polaris: {
