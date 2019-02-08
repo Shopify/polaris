9ef6bebfb48b073b1997e3c8ca2d06f687845f6e
Tim Layton <tmlayton@users.noreply.github.com>
2019-02-08 14:36:09 -0800

    Revert "Update Card.Section to accept ReactNode as title (#781)"
    This reverts commit 9687934f7e5da3fdfdd9bf1b81e1513d3db44c9d.

--- src/components/Card/components/Section/Section.tsx
+++ src/components/Card/components/Section/Section.tsx
@@ -6,29 +6,28 @@ import Subheading from '../../../Subheading';
 import styles from '../../Card.scss';
 
 export interface Props {
-  title?: React.ReactNode;
+  title?: string;
   children?: React.ReactNode;
   subdued?: boolean;
   fullWidth?: boolean;
 }
 
 export default function Section({children, title, subdued, fullWidth}: Props) {
+  const headerContent = title ? (
+    <div className={styles.SectionHeader}>
+      <Subheading>{title}</Subheading>
+    </div>
+  ) : null;
+
   const className = classNames(
     styles.Section,
     subdued && styles['Section-subdued'],
     fullWidth && styles['Section-fullWidth'],
   );
 
-  let headerMarkup = null;
-  if (title) {
-    const headerContent =
-      typeof title === 'string' ? <Subheading>{title}</Subheading> : title;
-    headerMarkup = <div className={styles.SectionHeader}>{headerContent}</div>;
-  }
-
   return (
     <div className={className}>
-      {headerMarkup}
+      {headerContent}
       {children}
     </div>
   );
