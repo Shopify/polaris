49ad69e5a9fee3945745688ccdf0f77886936b52
Koen Vendrik <k.vendrik@gmail.com>
2019-02-11 16:09:15 -0500

    resets playground

--- playground/Playground.tsx
+++ playground/Playground.tsx
@@ -1,51 +1,14 @@
 import * as React from 'react';
-import {TopBar, AppProvider, Frame, TextField} from '../src';
+import {Page} from '../src';
 
 interface State {}
 
-const theme = {
-  colors: {
-    topBar: {
-      background: '#357997',
-    },
-  },
-  logo: {
-    width: 124,
-    topBarSource:
-      'https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-color.svg?6215648040070010999',
-    url: 'http://jadedpixel.com',
-    accessibilityLabel: 'Jaded Pixel',
-    contextualSaveBarSource:
-      'https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-gray.svg?6215648040070010999',
-  },
-};
-
 export default class Playground extends React.Component<{}, State> {
   render() {
-    const topBar = (
-      <TopBar
-        userMenu={
-          <TopBar.UserMenu
-            initials="JD"
-            name="John Doe"
-            open
-            actions={[
-              {
-                items: [
-                  {
-                    content: 'Hello',
-                  },
-                ],
-              },
-            ]}
-          />
-        }
-      />
-    );
     return (
-      <AppProvider theme={theme}>
-        <Frame topBar={topBar} />
-      </AppProvider>
+      <Page title="Playground">
+        {/* Add the code you want to test in here */}
+      </Page>
     );
   }
 }
