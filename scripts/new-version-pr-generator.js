de01b0f84893b91e7d405c5c1464644b9ae40911
Alex Page <alex@alexpage.com.au>
2019-02-20 16:40:05 -0500

    Fixing up messy flow

--- scripts/new-version-pr-generator.js
+++ scripts/new-version-pr-generator.js
@@ -56,8 +56,6 @@ const polarisBotEmail = 'shopify-polaris-bot@users.noreply.github.com';
 const polarisBotToken = secrets.github['shopify-polaris'];
 const shopifyPolarisBotGitOverride = `GIT_COMMITTER_NAME='${polarisBotName}' GIT_COMMITTER_EMAIL='${polarisBotEmail}'`;
 
-const gitPolarisStyleguideDirectoryOverride = `--git-dir ${polarisStyleguide}/.git --work-tree=${polarisStyleguide}`;
-
 // Pull request template
 const updateBody = `
 ## Version ${releaseVersion} of @shopify/polaris just got published!
@@ -92,6 +90,18 @@ If tests fail, you may have to troubleshoot the problem locally.
 </details>
 `.trim();
 
+// Message that is logged on a failed update
+const failedUpdateMessage = `It seems your automatic branch creation for polaris-styleguide failed. This can be due to many reasons. To resolve this follow these steps and replace [release-version] with the version you are publishing:
+
+1. Checkout "polaris-styleguide"
+2. Get the latest version of master "git checkout master && git pull"
+3. Create a new branch based on the version that failed "git checkout -b update-polaris-v[release-version]"
+4. Update "shopify/polaris" by running "npx yarn upgrade @shopify/polaris@[release-version]"
+5. Add the changed files to git by running "git add package.json yarn.lock"
+6. Commit the changes with "git commit -m 'Update @shopify/polaris to [release-version]'
+7. Push the changes with "git push origin update-polaris-v[release-version]"
+8. Create a new pull request for the branch in "polaris-styleguide"`;
+
 const updatePostObject = {
   title: `Update @shopify/polaris to ${releaseVersion} 🚀`,
   body: updateBody,
@@ -99,95 +109,67 @@ const updatePostObject = {
   base: baseBranch,
 };
 
-ExecSyncQueue(
-  ['polaris-styleguide', 'polaris-web'],
-  [
-    // Notify polaris-styleguide to upgrade @shopify/polaris to the latest version
-    // So that polaris.shopify.com gets updated fairly quickly
-    [
-      `git clone --branch ${baseBranch} --single-branch https://${polarisBotToken}@github.com/Shopify/polaris-styleguide.git ${id}`,
-      execOpts,
-    ],
-    // Used to make git operations in polarisStyleguide dir instead of current working dir
-    [
-      `git ${gitPolarisStyleguideDirectoryOverride} checkout -b update-polaris-${releaseVersion}`,
-      execOpts,
-    ],
-    // Ensure all @shopify/* packages are pulled from the public registry
-    ['npm config set @shopify:registry https://registry.npmjs.org', execOpts],
-    [
-      `npx yarn@${YARN_VERSION} upgrade @shopify/polaris@${releaseVersion.replace(
-        'v',
-        '',
-      )} --no-progress --ignore-engines`,
-      {cwd: id},
-    ],
-    [
-      `git ${gitPolarisStyleguideDirectoryOverride} add package.json yarn.lock`,
-      execOpts,
-    ],
-    [
-      `${shopifyPolarisBotGitOverride} git ${gitPolarisStyleguideDirectoryOverride} commit -m "Update @shopify/polaris to ${releaseVersion}" --author "${polarisBotName} <${polarisBotEmail}>" -m "${releaseVersion}" --allow-empty`,
-      ,
-      execOpts,
-    ],
-    [
-      `git ${gitPolarisStyleguideDirectoryOverride} push origin update-polaris-${releaseVersion}`,
-      execOpts,
-    ],
-    [
-      `curl -d '${JSON.stringify(
-        updatePostObject,
-      )}' -X POST https://api.github.com/repos/shopify/polaris-styleguide/pulls?access_token=${polarisBotToken}`,
-      execOpts,
-    ],
-  ],
-);
-
 /**
- * ExecSyncQueue - Queue tasks to be ran parallel
+ * BulkExecute - Run a bunch of execSync
  *
- * @param {*} ids      - The unique ids of the tasks
- * @param {*} commands - The commands to be ran
+ * @param {array}  tasks   - List of tasks to run
+ * @param {object} options - Specific options for execSync
  */
-function execSyncQueue(ids, commands, options) {
-  // Create the tasks for each repository
-  const repositoryTasks = ids.map((id) => {
-    return new Promise((resolve, reject) => {
-      try {
-        commands.forEach((command) => {
-          execSync(command, options);
-          console.log(id);
-        });
-
-        console.log(
-          `Done: a pull request was opened at https://github.com/shopify/${id}/pulls'`,
-        );
-
-        resolve();
-      } catch (error) {
-        reject(error);
-      }
-    });
+function BulkExecute(tasks, options) {
+  tasks.forEach((task) => {
+    execSync(task.command, options ? options : {});
   });
-
-  // Wait for all of the tasks to complete
-  Promise.all(repositoryTasks)
-    .then('Pull requests created')
-    .catch((error) => console.error(error));
 }
 
-// Try and run update polaris using retry default settings, 3 attempts, 10second delay
-// retry(
-//   updatePolaris,
-//   `It seems your automatic branch creation for polaris-styleguide failed. This can be due to many reasons. To resolve this follow these steps and replace [release-version] with the version you are publishing:
+// Each of the repositories to create pull requests updating polaris
+const repositories = ['polaris-styleguide', 'polaris-web'];
 
-// 1. Checkout "polaris-styleguide"
-// 2. Get the latest version of master "git checkout master && git pull"
-// 3. Create a new branch based on the version that failed "git checkout -b update-polaris-v[release-version]"
-// 4. Update "shopify/polaris" by running "npx yarn upgrade @shopify/polaris@[release-version]"
-// 5. Add the changed files to git by running "git add package.json yarn.lock"
-// 6. Commit the changes with "git commit -m 'Update @shopify/polaris to [release-version]'
-// 7. Push the changes with "git push origin update-polaris-v[release-version]"
-// 8. Create a new pull request for the branch in "polaris-styleguide"`,
-// );
+// Iterate through the repositories
+const tasks = repositories.map((repository) => {
+  // Create a new promise for each repository
+  return new Promise((resolve, reject) => {
+    try {
+      const repositoryDirectory = resolve(sandbox, repository);
+      const gitPolarisRepositoryOverride = `--git-dir ${repository}/.git --work-tree=${repository}`;
+
+      // Check out the project and apply npm config
+      execSync(
+        `git ${gitPolarisRepositoryOverride} checkout -b update-polaris-${releaseVersion}`,
+        execOpts,
+      );
+
+      // Create a bunch of tasks
+      const createPullRequestTasks = BulkExecute(
+        [
+          'npm config set @shopify:registry https://registry.npmjs.org',
+          `npx yarn@${YARN_VERSION} upgrade @shopify/polaris@${releaseVersion.replace(
+            'v',
+            '',
+          )} --no-progress --ignore-engines``git add package.json yarn.lock`,
+          `${shopifyPolarisBotGitOverride} git commit -m "Update @shopify/polaris to ${releaseVersion}" --author "${polarisBotName} <${polarisBotEmail}>" -m "${releaseVersion}" --allow-empty`,
+          `git push origin update-polaris-${releaseVersion}`,
+          `curl -d '${JSON.stringify(
+            updatePostObject,
+          )}' -X POST https://api.github.com/repos/shopify/${repository}/pulls?access_token=${polarisBotToken}`,
+        ],
+        {cwd: repositoryDirectory},
+      );
+
+      // Try and run update polaris using retry default settings, 3 attempts, 10second delay
+      retry(createPullRequestTasks, failedUpdateMessage);
+
+      resolve(repository);
+    } catch (error) {
+      reject(error);
+    }
+  });
+});
+
+// Wait for all of the tasks to complete, run repositories parellel
+Promise.all(tasks)
+  .then((repository) =>
+    console.log(
+      `Done: Pull request made for ${releaseVersion} in ${repository}`,
+    ),
+  )
+  .catch((error) => console.error(error));
