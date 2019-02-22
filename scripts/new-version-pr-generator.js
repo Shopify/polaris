ca46cf67c20d274de0bfc65cc2c734cddc8b3ee1
Alex Page <alex@alexpage.com.au>
2019-02-22 10:29:03 -0500

    Fixing issues found in review

--- scripts/new-version-pr-generator.js
+++ scripts/new-version-pr-generator.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-console */
-
 const {execSync} = require('child_process');
 const {readFileSync} = require('fs');
 const pathResolve = require('path').resolve;
-const {mkdir, rm} = require('shelljs');
+const {mkdir} = require('shelljs');
 const yaml = require('js-yaml');
 const semver = require('semver');
 
@@ -11,31 +10,17 @@ const {version: PACKAGE_VERSION} = require('../package.json');
 const secrets = require('../secrets.json');
 const Retry = require('./utilities/retry');
 
+const repositories = ['polaris-styleguide', 'web'];
+
 const YARN_VERSION = yaml
   .safeLoad(readFileSync(pathResolve(__dirname, '..', 'dev.yml'), 'utf8'))
   .up.find((obj) => Object.keys(obj).includes('node')).node.yarn;
 
-// File paths and directories
 const root = pathResolve(__dirname, '../');
 const sandbox = pathResolve(root, 'sandbox');
 
-// Create the sandbox directory
-rm('-rf', sandbox);
-mkdir(sandbox);
-
-// Current release version
 const releaseVersion = `v${PACKAGE_VERSION}`;
 
-// Compute the base branch on the repository (default: master)
-// Example: will open a PR against the v4 branch if the
-// version found in package.json is `4.0.0-beta.x`
-
-// Check if a version is a major pre-release:
-// isMajorPrerelease('2.0.0') => false
-// isMajorPrerelease('3.0.1-beta') => false
-// isMajorPrerelease('1.1.0-alpha') => false
-// isMajorPrerelease('2.0.0-beta.1') => true
-// isMajorPrerelease('3.0.0-alpha') => true
 function isMajorPrerelease(version) {
   return (
     semver.prerelease(version) &&
@@ -48,51 +33,16 @@ const baseBranch = isMajorPrerelease(PACKAGE_VERSION)
   ? `v${semver.major(PACKAGE_VERSION)}`
   : 'master';
 
-// Polaris bot settings
 const polarisBotName = 'Shopify Polaris Bot';
 const polarisBotEmail = 'shopify-polaris-bot@users.noreply.github.com';
 const polarisBotToken = secrets.github['shopify-polaris'];
 const shopifyPolarisBotGitOverride = `GIT_COMMITTER_NAME='${polarisBotName}' GIT_COMMITTER_EMAIL='${polarisBotEmail}'`;
 
-/**
- * BulkExecute - Run a bunch of execSync one at a time
- *
- * @param {array}  tasks   - List of tasks to run
- * @param {object} options - Specific options for execSync
- */
-function BulkExecute(tasks, options) {
-  tasks.forEach((task) => {
-    execSync(task.command, options ? options : {});
-  });
-}
+function pullRequestTemplate(repository, version) {
+  return `
+## Version ${version} of @shopify/polaris just got published!
 
-// Each of the repositories to create pull requests updating polaris
-const repositories = ['polaris-styleguide', 'web'];
-
-// Iterate through the repositories
-const tasks = repositories.map((repository) => {
-  // Create a new promise for each repository
-  return new Promise((resolve, reject) => {
-    try {
-      const repositoryDirectory = pathResolve(sandbox, repository);
-
-      // Message that is logged on a failed update
-      const failedUpdateMessage = `It seems your automatic branch creation for ${repository} failed. This can be due to many reasons. To resolve this follow these steps and replace [release-version] with the version you are publishing:
-
-      1. Checkout "${repository}"
-      2. Get the latest version of master "git checkout master && git pull"
-      3. Create a new branch based on the version that failed "git checkout -b update-polaris-v[release-version]"
-      4. Update "shopify/polaris" by running "npx yarn upgrade @shopify/polaris@[release-version]"
-      5. Add the changed files to git by running "git add package.json yarn.lock"
-      6. Commit the changes with "git commit -m 'Update @shopify/polaris to [release-version]'
-      7. Push the changes with "git push origin update-polaris-v[release-version]"
-      8. Create a new pull request for the branch in "${repository}"`;
-
-      // Pull request template
-      const updateBody = `
-## Version ${releaseVersion} of @shopify/polaris just got published!
-
-See what’s new: https://github.com/Shopify/polaris-react/releases/tag/${releaseVersion}
+See what’s new: https://github.com/Shopify/polaris-react/releases/tag/${version}
 
 cc @kaelig @dfmcphee @amrocha @BPScott
 
@@ -103,74 +53,92 @@ cc @kaelig @dfmcphee @amrocha @BPScott
 
 If tests fail, you may have to troubleshoot the problem locally.
 
-1. Checkout the \`update-polaris-${releaseVersion}\` branch:
+1. Checkout the \`update-polaris-${version}\` branch:
     \`\`\`bash
     dev cd ${repository}
     git fetch
-    git checkout update-polaris-${releaseVersion}
+    git checkout update-polaris-${version}
     dev up
     \`\`\`
 1. Apply changes/fixes
 1. Commit
-1. Push your work to the \`update-polaris-${releaseVersion}\` branch:
+1. Push your work to the \`update-polaris-${version}\` branch:
     \`\`\`bash
-    git push origin update-polaris-${releaseVersion}
+    git push origin update-polaris-${version}
     \`\`\`
 1. Repeat until the build & tests go ✅
 1. 🎩
 1. :shipit:
 </details>
 `.trim();
+}
 
-      const updatePostObject = {
-        title: `Update @shopify/polaris to ${releaseVersion} 🚀`,
-        body: updateBody,
-        head: `update-polaris-${releaseVersion}`,
-        base: baseBranch,
-      };
+function failedUpdateMessage(repository, version) {
+  return `It seems your automatic branch creation for ${repository} failed. This can be due to many reasons. To resolve this follow these steps:
 
-      // Clone the repository to the repository directory
-      execSync(
-        `git clone --branch ${baseBranch} --single-branch https://${polarisBotToken}@github.com/Shopify/${repository}.git`,
-        {
-          cwd: sandbox,
-          stdio: 'inherit',
-        },
+  1. Checkout "${repository}"
+  2. Get the latest version of master "git checkout master && git pull"
+  3. Create a new branch based on the version that failed "git checkout -b update-polaris-v${version}"
+  4. Update "shopify/polaris" by running "npx yarn upgrade @shopify/polaris@${version}"
+  5. Add the changed files to git by running "git add package.json yarn.lock"
+  6. Commit the changes with "git commit -m 'Update @shopify/polaris to ${version}'
+  7. Push the changes with "git push origin update-polaris-v${version}"
+  8. Create a new pull request for the branch in "${repository}"`;
+}
+
+mkdir(sandbox);
+
+const tasks = repositories.map((repository) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const repositoryDirectory = pathResolve(sandbox, repository);
+
+      // Clone the repositories to the sandbox directory
+      Retry(
+        execSync(
+          `git clone --branch ${baseBranch} --single-branch https://${polarisBotToken}@github.com/Shopify/${repository}.git`,
+          {
+            cwd: sandbox,
+            stdio: 'inherit',
+          },
+        ),
       );
 
-      // Create a bunch of tasks
-      const commands = BulkExecute(
-        [
-          `git checkout -b update-polaris-${releaseVersion}`,
-          'npm config set @shopify:registry https://registry.npmjs.org',
-          `npx yarn@${YARN_VERSION} upgrade @shopify/polaris@${releaseVersion.replace(
-            'v',
-            '',
-          )} --no-progress --ignore-engines``git add package.json yarn.lock`,
-          `${shopifyPolarisBotGitOverride} git commit -m "Update @shopify/polaris to ${releaseVersion}" --author "${polarisBotName} <${polarisBotEmail}>" -m "${releaseVersion}" --allow-empty`,
-          `git push origin update-polaris-${releaseVersion}`,
-          `curl -d '${JSON.stringify(
-            updatePostObject,
-          )}' -X POST https://api.github.com/repos/shopify/${repository}/pulls?access_token=${polarisBotToken}`,
-        ],
-        {
-          cwd: repositoryDirectory,
-          stdio: 'inherit',
-        },
-      );
+      const commands = [
+        `git checkout -b update-polaris-${releaseVersion}`,
+        'npm config set @shopify:registry https://registry.npmjs.org',
+        `npx yarn@${YARN_VERSION} upgrade @shopify/polaris@${releaseVersion.replace(
+          'v',
+          '',
+        )} --no-progress --ignore-engines``git add package.json yarn.lock`,
+        `${shopifyPolarisBotGitOverride} git commit -m "Update @shopify/polaris to ${releaseVersion}" --author "${polarisBotName} <${polarisBotEmail}>" -m "${releaseVersion}" --allow-empty`,
+        `git push origin update-polaris-${releaseVersion}`,
+        `curl -d '${JSON.stringify({
+          title: `Update @shopify/polaris to ${releaseVersion} 🚀`,
+          body: pullRequestTemplate(repository, releaseVersion),
+          head: `update-polaris-${releaseVersion}`,
+          base: baseBranch,
+        })}' -X POST https://api.github.com/repos/shopify/${repository}/pulls?access_token=${polarisBotToken}`,
+      ];
 
-      // Try and run update polaris using retry default settings
-      // 3 attempts maximum with a 10 second delay between each attempt
-      Retry(commands, failedUpdateMessage);
+      // Run the commands in the cloned repository directories
+      commands.forEach((command) => {
+        Retry(
+          execSync(command, {
+            cwd: repositoryDirectory,
+            stdio: 'inherit',
+          }),
+        );
+      });
 
       resolve(repository);
     } catch (error) {
+      console.error(failedUpdateMessage(repository, releaseVersion));
       reject(error);
     }
   });
 });
 
-// Wait for all of the tasks to complete, run repositories parellel
 Promise.all(tasks)
   .then((repository) =>
     console.log(
--- scripts/utilities/retry.js
+++ scripts/utilities/retry.js
@@ -1,45 +1,19 @@
-/* eslint-disable no-console */
-
-/**
- * Retry - Retry a function on failure
- *
- * @param {function} Run          - The function to retry on failure
- * @param {string}   errorMessage - Additional information when the limit is reached
- * @param {number}   attempts     - The total number of attempts
- * @param {number}   limit        - The maximum number of attempts
- * @param {number}   delay        - The seconds to wait between attempts
- */
-const Retry = function(Run, errorMessage, attempts = 0, limit = 3, delay = 10) {
-  // Check that there is a function to try
-  if (typeof Run !== 'function') {
-    console.error('Retry must have a function to run and retry on failure');
-    return;
+function Retry(
+  functionToTry,
+  maxAttempts = 3,
+  delay = 1000,
+  currentAttempt = 0,
+) {
+  if (currentAttempt >= maxAttempts) {
+    throw new Error();
   }
-
-  let currentAttempts = attempts;
-
-  // Try the function
   try {
-    // Check if we are at maximum attempts
-    if (attempts < limit) {
-      currentAttempts += 1;
-
-      // Try the function
-      Run();
-    }
-    // Catch any errors
+    functionToTry();
   } catch (error) {
-    // If we are at maximum
-    if (attempts === limit) {
-      const message = errorMessage ? `${error}\n\n${errorMessage}` : error;
-      console.error(message);
-    } else {
-      // Otherwise try again after a delay
-      setTimeout(() => {
-        Retry(Run, errorMessage, currentAttempts, limit, delay);
-      }, delay * 1000);
-    }
+    setTimeout(() => {
+      Retry(functionToTry, currentAttempt + 1, maxAttempts, delay);
+    }, delay);
   }
-};
+}
 
 module.exports.default = Retry;
