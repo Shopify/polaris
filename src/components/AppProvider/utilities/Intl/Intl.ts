e365b71842675c39896a4e03d43c1a6d8d919b52
Andrew Musgrave <andrew.musgrave@shopify.com>
2019-02-28 18:43:36 -0500

    changelog

--- src/components/AppProvider/utilities/Intl/Intl.ts
+++ src/components/AppProvider/utilities/Intl/Intl.ts
@@ -1,6 +1,6 @@
 import merge from 'lodash/merge';
 import {autobind} from '@shopify/javascript-utilities/decorators';
-import get from '../../../../utilities/get';
+import {get} from '../../../../utilities/get';
 import defaultTranslation from '../../../../locales';
 import {
   TranslationDictionary,
