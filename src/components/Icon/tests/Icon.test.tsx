165840de13045162004a40afa1d05cf67c7c11a0
Tim Layton <tmlayton@users.noreply.github.com>
2019-03-04 08:45:36 -0800

    Update src/components/Icon/tests/Icon.test.tsx
    Co-Authored-By: amrocha <amrocha@users.noreply.github.com>

--- src/components/Icon/tests/Icon.test.tsx
+++ src/components/Icon/tests/Icon.test.tsx
@@ -67,7 +67,7 @@ describe('<Icon />', () => {
     it('spits out a console warning when rendering an untrusted SVG', () => {
       const spy = jest.spyOn(global.console, 'warn');
       const svg =
-        "<svg><path d='M17 9h-6V3a1 1 0 1 0-2 0v6H3a1 1 0 1 0 0 2h6v6a1 1 0 1 0 2 0v-6h6a1 1 0 1 0 0-2'  fill-rule='evenodd'/></svg>";
+        "<svg><path d="M10 10"/></svg>";
       shallowWithAppProvider(<Icon source={svg} untrusted />);
       expect(spy).toHaveBeenCalled();
     });
