67fbbaabb607ed5a4b083ffe2bb286a0b5e68ee5
Alex Page <alex@alexpage.com.au>
2019-03-18 09:02:37 -0400

    Add error message to retry

--- scripts/utilities/retry.js
+++ scripts/utilities/retry.js
@@ -1,13 +1,15 @@
 function retry(functionToTry, maxAttempts = 3, delay = 1000) {
-  if (maxAttempts <= 0) {
-    throw new Error();
-  }
   try {
     functionToTry();
   } catch (error) {
-    setTimeout(() => {
-      retry(functionToTry, maxAttempts - 1, delay);
-    }, delay);
+    const remainingAttempts = maxAttempts - 1;
+    if (remainingAttempts <= 0) {
+      throw new Error(error);
+    } else {
+      setTimeout(() => {
+        retry(functionToTry, remainingAttempts, delay);
+      }, delay);
+    }
   }
 }
 
