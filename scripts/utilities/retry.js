ca78856d4c84fc9cda8f598af198e7309ccb0795
Alex Page <alex@alexpage.com.au>
2019-03-18 09:52:40 -0400

    Stop nested error objects

--- scripts/utilities/retry.js
+++ scripts/utilities/retry.js
@@ -4,7 +4,7 @@ function retry(functionToTry, maxAttempts = 3, delay = 1000) {
   } catch (error) {
     const remainingAttempts = maxAttempts - 1;
     if (remainingAttempts <= 0) {
-      throw new Error(error);
+      throw error;
     } else {
       setTimeout(() => {
         retry(functionToTry, remainingAttempts, delay);
