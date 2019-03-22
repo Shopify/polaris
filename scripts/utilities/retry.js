9bb9005a8e70de29f2def18561ba0167bb143b94
Alex Page <alex@alexpage.com.au>
2019-03-22 10:50:52 -0400

    Remove test script

--- scripts/utilities/retry.js
+++ scripts/utilities/retry.js
@@ -19,26 +19,4 @@ function retry(functionToTry, maxAttempts = 3, delay = 1000) {
   });
 }
 
-(async () => {
-  const commands = [
-    `git checkout -b update-polaris-`,
-    `npx yarn@ upgrade @shopify/polaris@ --no-progress --ignore-engines`,
-    `git add package.json yarn.lock`,
-    `git commit -m "Update @shopify/polaris to" --author --allow-empty`,
-    `git push origin update-polaris-`,
-    `curl -d '`,
-  ];
-
-  const runCommand = async (command) => {
-    await retry(() => {
-      console.log(command);
-      throw new Error('noo');
-    });
-  };
-
-  for (const command of commands) {
-    await runCommand(command);
-  }
-})();
-
 module.exports = retry;
