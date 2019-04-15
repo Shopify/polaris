be1f8fe77bdd94cd75303607ff911508dcf5bf2a
Koen Vendrik <k.vendrik@gmail.com>
2019-04-15 08:29:25 +0200

    adds playroom

---
+++ playroom.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  components: './src/components',
+  outputPath: './dist/playroom',
+  title: 'Polaris',
+  widths: [320, 375, 768, 1024],
+  port: 9000,
+  openBrowser: true,
+  exampleCode: `
+    // Check out https://polaris.shopify.com/components/get-started for more info
+  `,
+};
