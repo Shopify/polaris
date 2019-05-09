58d4a75a33a7b281e2c2302490c9ce9f4aa68b27
Andrew Musgrave <andrew.musgrave@shopify.com>
2019-05-09 18:38:54 -0400

    Remove dropzone context from index

--- src/components/DropZone/index.ts
+++ src/components/DropZone/index.ts
@@ -3,5 +3,4 @@ import DropZone from './DropZone';
 export * from './types';
 
 export {Props} from './DropZone';
-export {default as DropZoneContext, DropZoneContextType} from './context';
 export default DropZone;
