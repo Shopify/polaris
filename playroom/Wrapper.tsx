ed8919cb09ccb3683a218557f799a659bee39973
Koen Vendrik <k.vendrik@gmail.com>
2019-05-24 14:57:17 -0400

    adds basic data generation

--- playroom/Wrapper.tsx
+++ playroom/Wrapper.tsx
@@ -6,6 +6,19 @@ interface Props {
   children: React.ReactNode;
 }
 
+function customers(amount: number) {
+  return Array.from({length: amount}, (_, index) => {
+    return {
+      id: index,
+      url: `#/customers/${index}`,
+      name: faker.name.findName(),
+      location: `${faker.address.city()}, ${faker.address.country()}`,
+    };
+  });
+}
+
+window.data = {customers};
+
 export default function PlayroomAppProvider({children}: Props) {
   return <AppProvider>{children}</AppProvider>;
 }
