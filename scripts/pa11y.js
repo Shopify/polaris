6f4003388b359ccdbd29b911006c59e64c932324
amrocha <andre.rocha@shopify.com>
2019-05-24 10:43:58 -0700

    Remove outdated comment

--- scripts/pa11y.js
+++ scripts/pa11y.js
@@ -156,7 +156,6 @@ The following issues were discovered and need to be fixed before this code can b
   }
 
   if (results.length || remainingIssues) {
-    // This is temporarily returning 0 while we ensure pa11y doesn't throw any false positives
     process.exit(1);
   }
   process.exit(0);
