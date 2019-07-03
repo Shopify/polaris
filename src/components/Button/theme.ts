5c97a68ac576d12f5a10c639b104fcd5c5faf296
Dan Rosenthal <dan.rosenthal@shopify.com>
2019-07-03 15:55:03 -0400

    more strictly type component theme api and take arrays of colors optionally

--- src/components/Button/theme.ts
+++ src/components/Button/theme.ts
@@ -1,22 +1,29 @@
 import {CSSProperties} from 'react';
 
-interface ComponentThemeProperties {
-  [key: string]: {
-    [key: string]: string;
-  };
-}
+type StateProperties = 'default' | 'hover' | 'focus' | 'active' | 'disabled';
+type StyleProperties =
+  | 'text'
+  | 'icon'
+  | 'background'
+  | 'border'
+  | 'gradient'
+  | 'shadow';
+
+type ComponentThemeProperties = {
+  [Theme in StateProperties]?: {[Style in StyleProperties]?: string | string[]}
+};
 
 const baseComponentTheme: ComponentThemeProperties = {
   default: {
     text: '--polaris-surface-28',
     icon: '--polaris-surface-27',
     background: '--polaris-surface-0',
-    backgroundSecondary: '--polaris-surface-1',
+    gradient: ['--polaris-surface-0', '--polaris-surface-1'],
     border: '--polaris-surface-8',
   },
   hover: {
-    backgroundPrimary: '--polaris-surface-1',
-    backgroundSecondary: '--polaris-surface-2',
+    background: '--polaris-surface-1',
+    gradient: ['--polaris-surface-1', '--polaris-surface-2'],
     border: '--polaris-surface-8',
   },
   focus: {
@@ -24,17 +31,19 @@ const baseComponentTheme: ComponentThemeProperties = {
     shadow: '--polaris-brand',
   },
   active: {
-    backgroundPrimary: '--polaris-surface-1',
-    backgroundSecondary: '--polaris-surface-1',
+    background: '--polaris-surface-1',
+    gradient: ['--polaris-surface-1', '--polaris-surface-1'],
     border: '--polaris-surface-4',
-    shadowPrimary: '--polaris-surface-opposingOpacified2',
-    shadowSecondary: '--polaris-surface-opposingOpacified3',
+    shadow: [
+      '--polaris-surface-opposingOpacified2',
+      '--polaris-surface-opposingOpacified3',
+    ],
   },
   disabled: {
     text: '--polaris-surface-20',
     icon: '--polaris-surface-20',
-    backgroundPrimary: '--polaris-surface-1',
-    backgroundSecondary: '--polaris-surface-1',
+    background: '--polaris-surface-1',
+    gradient: ['--polaris-surface-1', '--polaris-surface-1'],
   },
 };
 
@@ -42,33 +51,32 @@ const primaryComponentTheme: ComponentThemeProperties = {
   default: {
     text: '--polaris-surface-0',
     icon: '--polaris-surface-27',
-    backgroundPrimary: '--polaris-brand-lightened1',
-    backgroundSecondary: '--polaris-brand-darkened1',
+    background: '--polaris-brand-lightened1',
+    gradient: ['--polaris-brand-lightened1', '--polaris-brand-darkened1'],
     border: '--polaris-brand-darkened2',
     shadow: '--polaris-brand-lightened1',
   },
   hover: {
-    backgroundPrimary: '--polaris-brand',
-    backgroundSecondary: '--polaris-brand-darkened1',
+    background: '--polaris-brand',
+    gradient: ['--polaris-brand', '--polaris-brand-darkened1'],
     border: '--polaris-brand-darkened2',
     text: '--polaris-surface-0',
   },
   focus: {
     border: '--polaris-brand-darkened5',
-    shadowPrimary: '--polaris-brand-lightened1',
-    shadowSecondary: '--polaris-brand-darkened5',
+    shadow: ['--polaris-brand-lightened1', '--polaris-brand-darkened5'],
   },
   active: {
-    backgroundPrimary: '--polaris-brand-darkened2',
-    backgroundSecondary: '--polaris-brand-darkened2',
+    background: '--polaris-brand-darkened2',
+    gradient: ['--polaris-brand-darkened2', '--polaris-brand-darkened2'],
     border: '--polaris-brand-darkened3',
     shadow: '--polaris-brand-darkened3',
   },
   disabled: {
     text: '--polaris-surface-0',
     icon: '--polaris-surface-0',
-    backgroundPrimary: '--polaris-brand-lightened5',
-    backgroundSecondary: '--polaris-brand-lightened5',
+    background: '--polaris-brand-lightened5',
+    gradient: ['--polaris-brand-lightened5', '--polaris-brand-lightened5'],
     border: '--polaris-brand-lightened4',
   },
 };
@@ -77,33 +85,35 @@ const destructiveComponentTheme: ComponentThemeProperties = {
   default: {
     text: '--polaris-surface-0',
     icon: '--polaris-surface-27',
-    backgroundPrimary: '--polaris-negative-lightened1',
-    backgroundSecondary: '--polaris-negative-darkened1',
+    background: '--polaris-negative-lightened1',
+    gradient: ['--polaris-negative-lightened1', '--polaris-negative-darkened1'],
     border: '--polaris-negative-darkened2',
     shadow: '--polaris-negative-lightened1',
   },
   hover: {
-    backgroundPrimary: '--polaris-negative',
-    backgroundSecondary: '--polaris-negative-darkened1',
+    background: '--polaris-negative',
+    gradient: ['--polaris-negative', '--polaris-negative-darkened1'],
     border: '--polaris-negative-darkened2',
     text: '--polaris-surface-0',
   },
   focus: {
     border: '--polaris-negative-darkened5',
-    shadowPrimary: '--polaris-negative-lightened1',
-    shadowSecondary: '--polaris-negative-darkened5',
+    shadow: ['--polaris-negative-lightened1', '--polaris-negative-darkened5'],
   },
   active: {
-    backgroundPrimary: '--polaris-negative-darkened2',
-    backgroundSecondary: '--polaris-negative-darkened2',
+    background: '--polaris-negative-darkened2',
+    gradient: ['--polaris-negative-darkened2', '--polaris-negative-darkened2'],
     border: '--polaris-negative-darkened3',
     shadow: '--polaris-negative-darkened3',
   },
   disabled: {
     text: '--polaris-surface-0',
     icon: '--polaris-surface-0',
-    backgroundPrimary: '--polaris-negative-lightened5',
-    backgroundSecondary: '--polaris-negative-lightened5',
+    background: '--polaris-negative-lightened5',
+    gradient: [
+      '--polaris-negative-lightened5',
+      '--polaris-negative-lightened5',
+    ],
     border: '--polaris-negative-lightened4',
   },
 };
@@ -114,15 +124,15 @@ const outlineComponentTheme: ComponentThemeProperties = {
     border: '--polaris-surface-opposingOpacified4',
   },
   hover: {
-    backgroundPrimary: '--polaris-surface-opposingOpacified05',
+    background: '--polaris-surface-opposingOpacified05',
     border: '--polaris-surface-opposingOpacified4',
   },
   focus: {
     border: '--polaris-surface-opposingOpacified8',
-    shadowPrimary: '--polaris-surface-opposingOpacified8',
+    shadow: '--polaris-surface-opposingOpacified8',
   },
   active: {
-    backgroundPrimary: '--polaris-surface-opposingOpacified1',
+    background: '--polaris-surface-opposingOpacified1',
   },
   disabled: {
     text: '--polaris-surface-20',
@@ -138,15 +148,15 @@ const outlineDestructiveComponentTheme: ComponentThemeProperties = {
     icon: '--polaris-negative-darkened5',
   },
   hover: {
-    backgroundPrimary: '--polaris-negative-opacified05',
+    background: '--polaris-negative-opacified05',
     border: '--polaris-negative-opacified4',
   },
   focus: {
     border: '--polaris-negative-opacified8',
-    shadowPrimary: '--polaris-negative-opacified8',
+    shadow: '--polaris-negative-opacified8',
   },
   active: {
-    backgroundPrimary: '--polaris-negative-opacified1',
+    background: '--polaris-negative-opacified1',
   },
   disabled: {
     text: '--polaris-surface-20',
@@ -180,20 +190,37 @@ const plainDestructiveComponentTheme: ComponentThemeProperties = {
 function reduceTheme(theme: ComponentThemeProperties): CSSProperties {
   return Object.entries(theme)
     .reduce((childAccumulator, childCurrent) => {
+      const maybeChildCurrent = childCurrent[1] != null ? childCurrent[1] : [];
       return [
         ...childAccumulator,
-        ...Object.entries(childCurrent[1]).reduce(
+        ...Object.entries(maybeChildCurrent).reduce(
           (propertyAccumulator, propertyCurrent) => {
-            return [
-              ...propertyAccumulator,
-              ...[
-                {
-                  [`--${childCurrent[0]}-${propertyCurrent[0]}`]: `var(${
-                    propertyCurrent[1]
-                  })`,
-                },
-              ],
-            ];
+            if (typeof propertyCurrent[1] === 'string') {
+              return [
+                ...propertyAccumulator,
+                ...[
+                  {
+                    [`--${childCurrent[0]}-${propertyCurrent[0]}`]: `var(${
+                      propertyCurrent[1]
+                    })`,
+                  },
+                ],
+              ];
+            } else {
+              const maybePropertyCurrent =
+                propertyCurrent[1] != null ? propertyCurrent[1] : [];
+              return [
+                ...propertyAccumulator,
+                ...[
+                  ...maybePropertyCurrent.map((property, index) => {
+                    return {
+                      [`--${childCurrent[0]}-${propertyCurrent[0]}-${index +
+                        1}`]: `var(${property})`,
+                    };
+                  }),
+                ],
+              ];
+            }
           },
           [],
         ),
