10f3ed51b32e4d2b122d25756e362d8372455bf8
Ben Scott <ben.scott@shopify.com>
2019-07-02 17:42:54 -0700

    Distribute compiled sass in the styles folder
    This helps protect us against people overriding our variables / mixins /
    functions and thus causing problematic output.
    
    Do the sass build as part of the rollup styles plugin instead of a
    separate step. This saves us having to generate a file in the rollup
    plugin that we then pass on to a second step.
    
    Merge src/styles/global into a single file, it's pretty small and it's
    easier to deal with a single file

--- config/rollup/plugins/styles.js
+++ config/rollup/plugins/styles.js
@@ -1,10 +1,18 @@
-const {promisify} = require('util');
-const {resolve: resolvePath, dirname} = require('path');
+const util = require('util');
+const path = require('path');
 const postcss = require('postcss');
-const {ensureDir, readFileSync, writeFile} = require('fs-extra');
+const {
+  ensureDir,
+  outputFile,
+  readFileSync,
+  writeFile,
+  createWriteStream,
+} = require('fs-extra');
+const glob = require('glob');
 const nodeSass = require('node-sass');
 const {createFilter} = require('rollup-pluginutils');
 const cssnano = require('cssnano');
+const archiver = require('archiver');
 
 const cssModulesExtractImports = require('postcss-modules-extract-imports');
 const cssModulesLocalByDefault = require('postcss-modules-local-by-default');
@@ -15,7 +23,7 @@ const postcssShopify = require('postcss-shopify');
 
 const generateScopedName = require('../namespaced-classname');
 
-const renderSass = promisify(nodeSass.render);
+const renderSass = util.promisify(nodeSass.render);
 
 module.exports = function styles(options = {}) {
   const filter = createFilter(
@@ -24,7 +32,9 @@ module.exports = function styles(options = {}) {
   );
 
   const {output, includePaths = [], includeAlways = []} = options;
-  const cssAndTokensByFile = {};
+  const cssByFile = {};
+
+  let inputRoot;
 
   const includeAlwaysSource = includeAlways
     .map((resource) => readFileSync(resource, 'utf8'))
@@ -37,8 +47,8 @@ module.exports = function styles(options = {}) {
     cssModulesScope({generateScopedName}),
     new Parser({
       fetch(to, from) {
-        const fromDirectoryPath = dirname(from);
-        const toPath = resolvePath(fromDirectoryPath, to);
+        const fromDirectoryPath = path.dirname(from);
+        const toPath = path.resolve(fromDirectoryPath, to);
         const source = readFileSync(toPath, 'utf8');
         return getPostCSSOutput(processor, source, toPath);
       },
@@ -49,6 +59,10 @@ module.exports = function styles(options = {}) {
   return {
     name: 'shopify-styles',
 
+    options({input}) {
+      inputRoot = path.dirname(input);
+    },
+
     async transform(source, id) {
       if (!filter(id)) {
         return null;
@@ -56,12 +70,12 @@ module.exports = function styles(options = {}) {
 
       const sassOutput = await renderSass({
         data: `${includeAlwaysSource}\n${source}`,
-        includePaths: includePaths.concat(dirname(id)),
+        includePaths: includePaths.concat(path.dirname(id)),
       }).then((result) => result.css.toString());
 
       const postCssOutput = await getPostCSSOutput(processor, sassOutput, id);
 
-      cssAndTokensByFile[id] = postCssOutput;
+      cssByFile[id] = postCssOutput.css;
 
       const properties = JSON.stringify(postCssOutput.tokens, null, 2);
       return `export default ${properties};`;
@@ -83,7 +97,7 @@ module.exports = function styles(options = {}) {
       // whatever transform gets resolved first appears first. The contents of
       // the css file should use the order in which scss files were referenced
       // in the compiled javascript file.
-      const styleIds = Object.keys(cssAndTokensByFile);
+      const styleIds = Object.keys(cssByFile);
       const includedStyleIds = Array.from(
         Object.values(bundles).reduce((memo, bundle) => {
           Object.keys(bundle.modules).forEach((moduleName) => {
@@ -95,32 +109,136 @@ module.exports = function styles(options = {}) {
         }, new Set()),
       );
 
-      const {cssArray, tokensByFile} = includedStyleIds.reduce(
-        (memo, id) => {
-          memo.cssArray.push(cssAndTokensByFile[id].css);
-          memo.tokensByFile[id] = cssAndTokensByFile[id].tokens;
-          return memo;
-        },
-        {cssArray: [], tokensByFile: {}},
-      );
-      const css = cssArray.join('\n\n');
-      const tokens = `${JSON.stringify(tokensByFile, null, 2)}\n`;
-      const minifiedCss = (await cssnano.process(css, {
-        from: generateOptions.file,
-      })).css;
+      const orderedCssByFile = includedStyleIds.reduce((memo, id) => {
+        return memo.set(id, cssByFile[id]);
+      }, new Map());
 
-      await ensureDir(dirname(output));
+      const css = Array.from(orderedCssByFile.values()).join('\n\n');
+
+      await ensureDir(path.dirname(output));
       await Promise.all([
         writeFile(output, css),
-        writeFile(`${output.slice(0, -4)}.min.css`, minifiedCss),
-        writeFile(`${output.slice(0, -4)}.tokens.json`, tokens),
+        generateMinifiedCss(output, css),
+        generateSass(inputRoot, path.dirname(output), orderedCssByFile),
       ]);
     },
   };
 };
 
-function getPostCSSOutput(processor, source, path) {
+function getPostCSSOutput(processor, source, fromPath) {
   return processor
-    .process(source, {from: path})
+    .process(source, {from: fromPath})
     .then(({css, root: {tokens}}) => ({css, tokens}));
+}
+
+/**
+ * Generate a minified css file next to the inout file
+ */
+function generateMinifiedCss(sourceFilePath, css) {
+  const outputPath = `${sourceFilePath.slice(0, -4)}.min.css`;
+
+  return cssnano
+    .process(css, {from: sourceFilePath, to: outputPath})
+    .then((result) => writeFile(outputPath, result.css));
+}
+
+/**
+ * The Sass build - the styles folder.
+ * We precompile our source Sass to avoid possibility that people may
+ * accidentally overwrite some of our Sass variables / mixins / functions.
+ * The only Sass feature we use in the built output is the importing of partials
+ * to generate fewer entrypoints.
+ *
+ * - global.scss: Standard css that must be included by all applications. Per
+ *   component styles relies on these base styles.
+ * - components.scss and the components folder: A suite of the compiled css for
+ *  every component
+ * - foundation.scss, shared.scss and the foundation and shared folders: Our
+ *   public Sass API.
+ */
+async function generateSass(inputFolder, outputFolder, cssByFile) {
+  // Copy contents of $inputFolder/styles/shared.scss and $inputFolder/styles/foundation.scss
+  // and the foundation, shared and polaris-tokens folders into into build/styles
+  // We need to transform the contents of the files as some of them contain
+  // `:global` css modules definitions that we want to strip out
+  const stripGlobalRegex = /:global\s*\(([^)]+)\)|:global\s*{\s*([^}]+)\s*}\s*/g;
+  await Promise.all(
+    glob.sync(`styles/**/*.scss`, {cwd: inputFolder}).map((filePath) => {
+      const file = readFileSync(`${inputFolder}/${filePath}`, 'utf8').replace(
+        stripGlobalRegex,
+        '$1$2',
+      );
+      return outputFile(`${outputFolder}/${filePath}`, file);
+    }),
+  );
+
+  // For each file that was referened in the build, create a copy of the compiled
+  // ouput
+  const componentsCssFilenames = [];
+  cssByFile.forEach(async (compiledCss, filename) => {
+    if (filename.startsWith(`${inputFolder}/styles`)) {
+      await outputFile(
+        filename.replace(inputFolder, outputFolder),
+        compiledCss,
+      );
+    }
+
+    // For every referenced file that lives in the components folder:
+    // - copy it into the `styles/components` folder
+    // - make a note of the filename to use as an import
+    if (filename.startsWith(`${inputFolder}/components`)) {
+      componentsCssFilenames.push(filename.replace(inputFolder, '.'));
+
+      await outputFile(
+        filename.replace(inputFolder, `${outputFolder}/styles`),
+        compiledCss,
+      );
+    }
+  });
+
+  // Generate styles/components.scss
+  // Contains imports for all the individual components that we collected above
+  const componentsScssContents = componentsCssFilenames
+    .map((filename) => `@import "${filename.replace(/\.scss$/, '')}";`)
+    .join('\n');
+
+  // Generate polaris.scss
+  const polarisScssContent = `@import 'styles/foundation';
+@import 'styles/shared';
+@import 'styles/global';
+@import 'styles/components';
+`;
+
+  await Promise.all([
+    outputFile(
+      `${outputFolder}/styles/components.scss`,
+      componentsScssContents,
+    ),
+    outputFile(`${outputFolder}/styles.scss`, polarisScssContent),
+  ]);
+
+  // Generate Sass.zip
+  return generateSassZip(outputFolder, outputFolder);
+}
+
+// see https://archiverjs.com
+function generateSassZip(sourceDir, destinationDir) {
+  return new Promise((resolve, reject) => {
+    const output = createWriteStream(`${destinationDir}/Sass.zip`);
+    const archive = archiver('zip', {store: true});
+
+    output.on('close', () => {
+      // eslint-disable-next-line no-console
+      console.log(`Sass zip complete: ${archive.pointer()} total bytes`);
+      resolve();
+    });
+
+    archive.on('error', (err) => {
+      reject(err);
+    });
+
+    archive.pipe(output);
+    archive.glob(`**/*.scss`, {cwd: sourceDir});
+    archive.finalize();
+  });
 }
