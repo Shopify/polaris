9bc0f67f64c512691fdad48f193c9f8988847308
Andrew Musgrave <andrew.musgrave@shopify.com>
2019-07-15 17:35:31 -0400

    Hide collapsible content when exited

--- src/components/Collapsible/Collapsible.tsx
+++ src/components/Collapsible/Collapsible.tsx
@@ -17,12 +17,23 @@ export interface Props {
   children?: React.ReactNode;
 }
 
+enum TransitionStatus {
+  Entering = 'entering',
+  Entered = 'entered',
+  Exiting = 'exiting',
+  Exited = 'exited',
+}
+
 const CSS_VAR_COLLAPSIBLE_HEIGHT = '--polaris-collapsible-height';
 const CSS_VAR_COLLAPSIBLE_TRANSITION_DURATION =
   '--polaris-collapsible-transition-duration';
 
 export default function Collapsible({id, open, children}: Props) {
   const [height, setHeight] = useState<number | null>(null);
+  const [transitionStatus, setTransitionStatus] = useState(
+    open ? TransitionStatus.Entering : TransitionStatus.Exited,
+  );
+  const isMounted = useRef(false);
   const node = useRef<HTMLDivElement>(null);
 
   const handleResize = useCallback(() => {
@@ -30,6 +41,29 @@ export default function Collapsible({id, open, children}: Props) {
 
     setHeight(node.current.scrollHeight);
   }, []);
+
+  useEffect(
+    () => {
+      if (!isMounted.current) return;
+      transitionStatus === TransitionStatus.Entering &&
+        changeTransitionStatus(TransitionStatus.Entered);
+
+      transitionStatus === TransitionStatus.Exiting &&
+        setTimeout(() => {
+          changeTransitionStatus(TransitionStatus.Exited);
+        }, durationSlow);
+    },
+    [transitionStatus],
+  );
+
+  useEffect(
+    () => {
+      if (!isMounted.current) return;
+      open && changeTransitionStatus(TransitionStatus.Entered);
+      !open && changeTransitionStatus(TransitionStatus.Exiting);
+    },
+    [open],
+  );
 
   useEffect(
     () => {
@@ -64,11 +98,27 @@ export default function Collapsible({id, open, children}: Props) {
     [height],
   );
 
+  // Hooks always execute in the same sequence. It's
+  // important this hooks runs after the animation effects
+  useEffect(() => {
+    isMounted.current = true;
+  }, []);
+
   const wrapperClassName = classNames(styles.Collapsible, open && styles.open);
+
+  const content =
+    transitionStatus === TransitionStatus.Exited && !open ? null : children;
 
   return (
     <div id={id} aria-hidden={!open} className={wrapperClassName} ref={node}>
-      <div>{children}</div>
+      <div>{content}</div>
     </div>
   );
+
+  function changeTransitionStatus(transitionStatus: TransitionStatus) {
+    setTransitionStatus(transitionStatus);
+    // Forcing a reflow to enable the animation
+    if (transitionStatus === TransitionStatus.Entering)
+      node.current && node.current.getBoundingClientRect();
+  }
 }
