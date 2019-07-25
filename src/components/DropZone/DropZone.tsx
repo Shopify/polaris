wschwanke/polaris-react

Allow fixed height on Dropzone

src/components/DropZone/DropZone.tsx
@@ -21,7 +21,7 @@ import {FileUpload, Provider} from './components';
 import {dragDrop, alertCircle} from './icons';
 
 import {fileAccepted, getDataTransferFiles} from './utils';
-import {DropZoneContext} from './types';
+import {DropZoneContext, Size} from './types';
 
 import styles from './DropZone.scss';
 
@@ -29,13 +29,15 @@ export type Type = 'file' | 'image';
 
 export interface State {
   id: string;
-  size: string;
+  height: Size;
+  width: Size;
   type?: string;
   error?: boolean;
   dragging: boolean;
   overlayText?: string;
   errorOverlayText?: string;
   numFiles: number;
+  measuring: boolean;
 }
 
 export interface Props {
@@ -108,7 +110,6 @@ export interface Props {
 export type CombinedProps = Props & WithAppProviderProps;
 
 const getUniqueID = createUniqueIDFactory('DropZone');
-
 export class DropZone extends React.Component<CombinedProps, State> {
   public static FileUpload: typeof FileUpload = FileUpload;
   public static defaultProps: Partial<CombinedProps> = {
@@ -170,19 +171,23 @@ export class DropZone extends React.Component<CombinedProps, State> {
     this.state = {
       type,
       id: props.id || getUniqueID(),
-      size: 'extraLarge',
+      height: Size.ExtraLarge,
       dragging: false,
       error: false,
       overlayText: translate(`Polaris.DropZone.overlayText${suffix}`),
       errorOverlayText: translate(`Polaris.DropZone.errorOverlayText${suffix}`),
       numFiles: 0,
+      width: Size.ExtraLarge,
+      measuring: true,
     };
   }
 
   get getContext(): DropZoneContext {
+    const {width, height, type = 'file'} = this.state;
     return {
-      size: this.state.size,
-      type: this.state.type || 'file',
+      width,
+      height,
+      type,
     };
   }
 
@@ -191,9 +196,11 @@ export class DropZone extends React.Component<CombinedProps, State> {
       id,
       dragging,
       error,
-      size,
+      height,
+      width,
       overlayText,
       errorOverlayText,
+      measuring,
     } = this.state;
     const {
       label,
@@ -223,11 +230,28 @@ export class DropZone extends React.Component<CombinedProps, State> {
       styles.DropZone,
       outline && styles.hasOutline,
       (active || dragging) && styles.isDragging,
+      measuring && styles.isMeasuring,
       error && styles.hasError,
-      size && size === 'extraLarge' && styles.sizeExtraLarge,
-      size && size === 'large' && styles.sizeLarge,
-      size && size === 'medium' && styles.sizeMedium,
-      size && size === 'small' && styles.sizeSmall,
+      height === Size.ExtraLarge && styles.sizeExtraLarge,
+      height === Size.Large && styles.sizeLarge,
+      height === Size.Medium && styles.sizeMedium,
+      height === Size.Small && styles.sizeSmall,
+    );
+
+    const extraLargeDropZoneSize =
+      width === Size.ExtraLarge && height !== Size.Small;
+
+    const mediumLargeDropZoneSize =
+      (width === Size.Medium || width === Size.Large) && height !== Size.Small;
+
+    const dragOverlayDisplayText = extraLargeDropZoneSize && (
+      <DisplayText size="small" element="p">
+        {overlayText}
+      </DisplayText>
+    );
+
+    const dragOverlayCaption = mediumLargeDropZoneSize && (
+      <Caption>{overlayText}</Caption>
     );
 
     const dragOverlay =
@@ -235,49 +259,48 @@ export class DropZone extends React.Component<CombinedProps, State> {
         <div className={styles.Overlay}>
           <Stack vertical spacing="tight">
             <Icon source={dragDrop} color="indigo" />
-            {size === 'extraLarge' && (
-              <DisplayText size="small" element="p">
-                {overlayText}
-              </DisplayText>
-            )}
-            {(size === 'medium' || size === 'large') && (
-              <Caption>{overlayText}</Caption>
-            )}
+            {dragOverlayDisplayText}
+            {dragOverlayCaption}
           </Stack>
         </div>
       ) : null;
+
+    const dragErrorOverlayDisplayText = extraLargeDropZoneSize && (
+      <DisplayText size="small" element="p">
+        {errorOverlayText}
+      </DisplayText>
+    );
+
+    const dragErrorOverlayCaption = mediumLargeDropZoneSize && (
+      <Caption>{errorOverlayText}</Caption>
+    );
 
     const dragErrorOverlay =
       dragging && error ? (
         <div className={styles.Overlay}>
           <Stack vertical spacing="tight">
             <Icon source={alertCircle} color="red" />
-            {size === 'extraLarge' && (
-              <DisplayText size="small" element="p">
-                {errorOverlayText}
-              </DisplayText>
-            )}
-            {(size === 'medium' || size === 'large') && (
-              <Caption>{errorOverlayText}</Caption>
-            )}
+            {dragErrorOverlayDisplayText}
+            {dragErrorOverlayCaption}
           </Stack>
         </div>
       ) : null;
 
     const dropZoneMarkup = (
-      <div
-        ref={this.setNode}
-        className={classes}
-        aria-disabled={disabled}
-        onClick={this.handleClick}
-        onDragStart={handleDragStart}
-      >
-        {dragOverlay}
-        {dragErrorOverlay}
-        <div className={styles.Container}>{children}</div>
-        <VisuallyHidden>
-          <input {...inputAttributes} />
-        </VisuallyHidden>
+      <div ref={this.setNode} className={styles.DropZoneWrapper}>
+        <div
+          className={classes}
+          aria-disabled={disabled}
+          onClick={this.handleClick}
+          onDragStart={handleDragStart}
+        >
+          {dragOverlay}
+          {dragErrorOverlay}
+          <div className={styles.Container}>{children}</div>
+          <VisuallyHidden>
+            <input {...inputAttributes} />
+          </VisuallyHidden>
+        </div>
       </div>
     );
 
@@ -356,24 +379,34 @@ export class DropZone extends React.Component<CombinedProps, State> {
   }
 
   @autobind
+  private setWrapperSize(size: string, node: HTMLElement) {
+    let wrapperSize;
+    const getSize = size === 'height' ? 'height' : 'width';
+    const wrapper = node.getBoundingClientRect()[getSize];
+
+    if (wrapper < Size.Small) {
+      wrapperSize = Size.Small;
+    } else if (wrapper < Size.Medium) {
+      wrapperSize = Size.Medium;
+    } else if (wrapper < Size.Large) {
+      wrapperSize = Size.Large;
+    } else {
+      wrapperSize = Size.ExtraLarge;
+    }
+    return wrapperSize;
+  }
+
+  @autobind
   @debounce(50, {trailing: true})
   private adjustSize() {
     if (!this.node) {
       return;
     }
 
-    let size = 'extraLarge';
-    const width = this.node.getBoundingClientRect().width;
+    const height = this.setWrapperSize('height', this.node);
+    const width = this.setWrapperSize('width', this.node);
 
-    if (width < 100) {
-      size = 'small';
-    } else if (width < 160) {
-      size = 'medium';
-    } else if (width < 300) {
-      size = 'large';
-    }
-
-    this.setState({size});
+    this.setState({height, width, measuring: false});
   }
 
   @autobind

src/components/DropZone/components/FileUpload/FileUpload.tsx
@@ -15,7 +15,7 @@ import TextStyle from '../../../TextStyle';
 import withContext from '../../../WithContext';
 import withRef from '../../../WithRef';
 
-import {DropZoneContext} from '../../types';
+import {DropZoneContext, Size} from '../../types';
 import {dragDrop} from '../../icons';
 import {fileUpload, imageUpload} from '../../images';
 
@@ -77,71 +77,72 @@ export class FileUpload extends React.Component<CombinedProps, State> {
   }
 
   render() {
+    const fileUploadMarkup = this.renderFileUploadMarkup();
+
+    return <div className={styles.FileUpload}>{fileUploadMarkup}</div>;
+  }
+
+  private renderFileUploadMarkup() {
     const {
-      context: {size, type},
+      context: {width, height, type},
     } = this.props;
+
     const {actionTitle, actionHint} = this.state;
     const imageClasses = classNames(
       styles.Image,
-      size && size === 'extraLarge' && styles.sizeExtraLarge,
-      size && size === 'large' && styles.sizeLarge,
+      width === Size.ExtraLarge && styles.sizeExtraLarge,
+      width === Size.Large && styles.sizeLarge,
     );
 
-    const extraLargeView =
-      size === 'extraLarge' ? (
-        <Stack vertical>
-          {type === 'file' && (
-            <img className={imageClasses} src={fileUpload} alt="" />
-          )}
-          {type === 'image' && (
-            <img className={imageClasses} src={imageUpload} alt="" />
-          )}
-          <Button>{actionTitle}</Button>
-          <TextStyle variation="subdued">{actionHint}</TextStyle>
-        </Stack>
-      ) : null;
+    const fileType = type === 'file';
+    const imageType = type === 'image';
 
-    const largeView =
-      size === 'large' ? (
-        <Stack vertical spacing="tight">
-          {type === 'file' && (
-            <img className={imageClasses} src={fileUpload} alt="" />
-          )}
-          {type === 'image' && (
-            <img className={imageClasses} src={imageUpload} alt="" />
-          )}
-          <Button size="slim">{actionTitle}</Button>
-          <Caption>
+    const size = Math.min(width, height);
+    switch (size) {
+      case Size.ExtraLarge:
+        return (
+          <Stack vertical>
+            {fileType && (
+              <img className={imageClasses} src={fileUpload} alt="" />
+            )}
+            {imageType && (
+              <img className={imageClasses} src={imageUpload} alt="" />
+            )}
+            <Button>{actionTitle}</Button>
             <TextStyle variation="subdued">{actionHint}</TextStyle>
-          </Caption>
-        </Stack>
-      ) : null;
-
-    const mediumView =
-      size === 'medium' ? (
-        <Stack vertical spacing="tight">
-          <Link>{actionTitle}</Link>
-          <Caption>
-            <TextStyle variation="subdued">{actionHint}</TextStyle>
-          </Caption>
-        </Stack>
-      ) : null;
-
-    const smallView =
-      size === 'small' ? (
-        <Stack vertical spacing="tight">
-          <Icon source={dragDrop} color="inkLightest" />
-        </Stack>
-      ) : null;
-
-    return (
-      <div className={styles.FileUpload}>
-        {smallView}
-        {mediumView}
-        {largeView}
-        {extraLargeView}
-      </div>
-    );
+          </Stack>
+        );
+      case Size.Large:
+        return (
+          <Stack vertical spacing="tight">
+            {fileType && (
+              <img className={imageClasses} src={fileUpload} alt="" />
+            )}
+            {imageType && (
+              <img className={imageClasses} src={imageUpload} alt="" />
+            )}
+            <Button size="slim">{actionTitle}</Button>
+            <Caption>
+              <TextStyle variation="subdued">{actionHint}</TextStyle>
+            </Caption>
+          </Stack>
+        );
+      case Size.Medium:
+        return (
+          <Stack vertical spacing="tight">
+            <Link>{actionTitle}</Link>
+            <Caption>
+              <TextStyle variation="subdued">{actionHint}</TextStyle>
+            </Caption>
+          </Stack>
+        );
+      case Size.Small:
+        return (
+          <Stack vertical spacing="tight">
+            <Icon source={dragDrop} color="inkLightest" />
+          </Stack>
+        );
+    }
   }
 }
 

Shopify/polaris

src/components/DropZone/DropZone.tsx
@@ -23,7 +23,7 @@ import {withAppProvider, WithAppProviderProps} from '../AppProvider';
 import {FileUpload, Provider} from './components';
 
 import {fileAccepted, getDataTransferFiles} from './utils';
-import {DropZoneContext} from './types';
+import {DropZoneContext, Size} from './types';
 
 import styles from './DropZone.scss';
 
@@ -31,13 +31,15 @@ export type Type = 'file' | 'image';
 
 export interface State {
   id: string;
-  size: string;
+  height: Size;
+  width: Size;
   type?: string;
   error?: boolean;
   dragging: boolean;
   overlayText?: string;
   errorOverlayText?: string;
   numFiles: number;
+  measuring: boolean;
 }
 
 export interface Props {
@@ -163,18 +165,10 @@ class DropZone extends React.Component<CombinedProps, State> {
         return;
       }
 
-      let size = 'extraLarge';
-      const width = this.node.current.getBoundingClientRect().width;
+      const height = this.setWrapperSize('height', this.node);
+      const width = this.setWrapperSize('width', this.node);
 
-      if (width < 100) {
-        size = 'small';
-      } else if (width < 160) {
-        size = 'medium';
-      } else if (width < 300) {
-        size = 'large';
-      }
-
-      this.setState({size});
+      this.setState({height, width, measuring: false});
     },
     50,
     {trailing: true},
@@ -195,19 +189,23 @@ class DropZone extends React.Component<CombinedProps, State> {
     this.state = {
       type,
       id: props.id || getUniqueID(),
-      size: 'extraLarge',
+      height: Size.ExtraLarge,
       dragging: false,
       error: false,
       overlayText: translate(`Polaris.DropZone.overlayText${suffix}`),
       errorOverlayText: translate(`Polaris.DropZone.errorOverlayText${suffix}`),
       numFiles: 0,
+      width: Size.ExtraLarge,
+      measuring: true,
     };
   }
 
   get getContext(): DropZoneContext {
+    const {width, height, type = 'file'} = this.state;
     return {
-      size: this.state.size,
-      type: this.state.type || 'file',
+      width,
+      height,
+      type,
     };
   }
 
@@ -220,9 +218,11 @@ class DropZone extends React.Component<CombinedProps, State> {
       id,
       dragging,
       error,
-      size,
+      height,
+      width,
       overlayText,
       errorOverlayText,
+      measuring,
     } = this.state;
     const {
       label,
@@ -244,6 +244,7 @@ class DropZone extends React.Component<CombinedProps, State> {
       disabled,
       type: 'file',
       multiple: allowMultiple,
+      name: id,
       ref: this.fileInputNode,
       onChange: this.handleDrop,
       autoComplete: 'off',
@@ -253,11 +254,28 @@ class DropZone extends React.Component<CombinedProps, State> {
       styles.DropZone,
       outline && styles.hasOutline,
       (active || dragging) && styles.isDragging,
+      measuring && styles.isMeasuring,
       error && styles.hasError,
-      size && size === 'extraLarge' && styles.sizeExtraLarge,
-      size && size === 'large' && styles.sizeLarge,
-      size && size === 'medium' && styles.sizeMedium,
-      size && size === 'small' && styles.sizeSmall,
+      height === Size.ExtraLarge && styles.sizeExtraLarge,
+      height === Size.Large && styles.sizeLarge,
+      height === Size.Medium && styles.sizeMedium,
+      height === Size.Small && styles.sizeSmall,
+    );
+
+    const extraLargeDropZoneSize =
+      width === Size.ExtraLarge && height !== Size.Small;
+
+    const mediumLargeDropZoneSize =
+      (width === Size.Medium || width === Size.Large) && height !== Size.Small;
+
+    const dragOverlayDisplayText = extraLargeDropZoneSize && (
+      <DisplayText size="small" element="p">
+        {overlayText}
+      </DisplayText>
+    );
+
+    const dragOverlayCaption = mediumLargeDropZoneSize && (
+      <Caption>{overlayText}</Caption>
     );
 
     const dragOverlay =
@@ -265,31 +283,29 @@ class DropZone extends React.Component<CombinedProps, State> {
         <div className={styles.Overlay}>
           <Stack vertical spacing="tight">
             <Icon source={DragDropMajorMonotone} color="indigo" />
-            {size === 'extraLarge' && (
-              <DisplayText size="small" element="p">
-                {overlayText}
-              </DisplayText>
-            )}
-            {(size === 'medium' || size === 'large') && (
-              <Caption>{overlayText}</Caption>
-            )}
+            {dragOverlayDisplayText}
+            {dragOverlayCaption}
           </Stack>
         </div>
       ) : null;
+
+    const dragErrorOverlayDisplayText = extraLargeDropZoneSize && (
+      <DisplayText size="small" element="p">
+        {errorOverlayText}
+      </DisplayText>
+    );
+
+    const dragErrorOverlayCaption = mediumLargeDropZoneSize && (
+      <Caption>{errorOverlayText}</Caption>
+    );
 
     const dragErrorOverlay =
       dragging && error ? (
         <div className={styles.Overlay}>
           <Stack vertical spacing="tight">
             <Icon source={CircleAlertMajorMonotone} color="red" />
-            {size === 'extraLarge' && (
-              <DisplayText size="small" element="p">
-                {errorOverlayText}
-              </DisplayText>
-            )}
-            {(size === 'medium' || size === 'large') && (
-              <Caption>{errorOverlayText}</Caption>
-            )}
+            {dragErrorOverlayDisplayText}
+            {dragErrorOverlayCaption}
           </Stack>
         </div>
       ) : null;
@@ -298,31 +314,39 @@ class DropZone extends React.Component<CombinedProps, State> {
       ? label
       : intl.translate('Polaris.DropZone.FileUpload.label');
     const labelHiddenValue = label ? labelHidden : true;
+    const dropZoneMarkup = (
+      <div ref={this.node} className={styles.DropZoneWrapper}>
+        <div
+          className={classes}
+          aria-disabled={disabled}
+          onClick={this.handleClick}
+          onDragStart={handleDragStart}
+        >
+          {dragOverlay}
+          {dragErrorOverlay}
+          <div className={styles.Container}>{children}</div>
+          <VisuallyHidden>
+            <input {...inputAttributes} />
+          </VisuallyHidden>
+        </div>
+      </div>
+    );
+
+    const labelledDropzoneMarkup = label ? (
+      <Labelled
+        id={id}
+        label={labelValue}
+        action={labelAction}
+        labelHidden={labelHiddenValue}
+      >
+        {dropZoneMarkup}
+      </Labelled>
+    ) : (
+      dropZoneMarkup
+    );
 
     return (
-      <Provider value={this.getContext}>
-        <Labelled
-          id={id}
-          label={labelValue}
-          action={labelAction}
-          labelHidden={labelHiddenValue}
-        >
-          <div
-            ref={this.node}
-            className={classes}
-            aria-disabled={disabled}
-            onClick={this.handleClick}
-            onDragStart={handleDragStart}
-          >
-            {dragOverlay}
-            {dragErrorOverlay}
-            <div className={styles.Container}>{children}</div>
-            <VisuallyHidden>
-              <input {...inputAttributes} />
-            </VisuallyHidden>
-          </div>
-        </Labelled>
-      </Provider>
+      <Provider value={this.getContext}>{labelledDropzoneMarkup}</Provider>
     );
   }
 
@@ -380,6 +404,26 @@ class DropZone extends React.Component<CombinedProps, State> {
     }
 
     this.fileInputNode.current.click();
+  };
+
+  private setWrapperSize = (
+    size: string,
+    node: React.RefObject<HTMLDivElement>,
+  ) => {
+    let wrapperSize = Size.ExtraLarge;
+
+    if (node.current) {
+      const getSize = size === 'height' ? 'height' : 'width';
+      const wrapper = node.current.getBoundingClientRect()[getSize];
+      if (wrapper < Size.Small) {
+        wrapperSize = Size.Small;
+      } else if (wrapper < Size.Medium) {
+        wrapperSize = Size.Medium;
+      } else if (wrapper < Size.Large) {
+        wrapperSize = Size.Large;
+      }
+    }
+    return wrapperSize;
   };
 
   private getValidatedFiles = (files: File[] | DataTransferItem[]) => {

src/components/DropZone/components/FileUpload/FileUpload.tsx
@@ -16,7 +16,7 @@ import TextStyle from '../../../TextStyle';
 import withContext from '../../../WithContext';
 import withRef from '../../../WithRef';
 
-import {DropZoneContext} from '../../types';
+import {DropZoneContext, Size} from '../../types';
 import {fileUpload, imageUpload} from '../../images';
 
 import {Consumer} from '../Context';
@@ -91,71 +91,72 @@ class FileUpload extends React.Component<CombinedProps, State> {
   }
 
   render() {
+    const fileUploadMarkup = this.getFileUploadMarkup();
+
+    return <div className={styles.FileUpload}>{fileUploadMarkup}</div>;
+  }
+
+  private getFileUploadMarkup() {
     const {
-      context: {size, type},
+      context: {width, height, type},
     } = this.props;
+
     const {actionTitle, actionHint} = this.state;
     const imageClasses = classNames(
       styles.Image,
-      size && size === 'extraLarge' && styles.sizeExtraLarge,
-      size && size === 'large' && styles.sizeLarge,
+      width === Size.ExtraLarge && styles.sizeExtraLarge,
+      width === Size.Large && styles.sizeLarge,
     );
 
-    const extraLargeView =
-      size === 'extraLarge' ? (
-        <Stack vertical>
-          {type === 'file' && (
-            <img className={imageClasses} src={fileUpload} alt="" />
-          )}
-          {type === 'image' && (
-            <img className={imageClasses} src={imageUpload} alt="" />
-          )}
-          <Button>{actionTitle}</Button>
-          <TextStyle variation="subdued">{actionHint}</TextStyle>
-        </Stack>
-      ) : null;
+    const fileType = type === 'file';
+    const imageType = type === 'image';
 
-    const largeView =
-      size === 'large' ? (
-        <Stack vertical spacing="tight">
-          {type === 'file' && (
-            <img className={imageClasses} src={fileUpload} alt="" />
-          )}
-          {type === 'image' && (
-            <img className={imageClasses} src={imageUpload} alt="" />
-          )}
-          <Button size="slim">{actionTitle}</Button>
-          <Caption>
+    const size = Math.min(width, height);
+    switch (size) {
+      case Size.ExtraLarge:
+        return (
+          <Stack vertical>
+            {fileType && (
+              <img className={imageClasses} src={fileUpload} alt="" />
+            )}
+            {imageType && (
+              <img className={imageClasses} src={imageUpload} alt="" />
+            )}
+            <Button>{actionTitle}</Button>
             <TextStyle variation="subdued">{actionHint}</TextStyle>
-          </Caption>
-        </Stack>
-      ) : null;
-
-    const mediumView =
-      size === 'medium' ? (
-        <Stack vertical spacing="tight">
-          <Link>{actionTitle}</Link>
-          <Caption>
-            <TextStyle variation="subdued">{actionHint}</TextStyle>
-          </Caption>
-        </Stack>
-      ) : null;
-
-    const smallView =
-      size === 'small' ? (
-        <Stack vertical spacing="tight">
-          <Icon source={DragDropMajorMonotone} color="inkLightest" />
-        </Stack>
-      ) : null;
-
-    return (
-      <div className={styles.FileUpload}>
-        {smallView}
-        {mediumView}
-        {largeView}
-        {extraLargeView}
-      </div>
-    );
+          </Stack>
+        );
+      case Size.Large:
+        return (
+          <Stack vertical spacing="tight">
+            {fileType && (
+              <img className={imageClasses} src={fileUpload} alt="" />
+            )}
+            {imageType && (
+              <img className={imageClasses} src={imageUpload} alt="" />
+            )}
+            <Button size="slim">{actionTitle}</Button>
+            <Caption>
+              <TextStyle variation="subdued">{actionHint}</TextStyle>
+            </Caption>
+          </Stack>
+        );
+      case Size.Medium:
+        return (
+          <Stack vertical spacing="tight">
+            <Link>{actionTitle}</Link>
+            <Caption>
+              <TextStyle variation="subdued">{actionHint}</TextStyle>
+            </Caption>
+          </Stack>
+        );
+      case Size.Small:
+        return (
+          <Stack vertical spacing="tight">
+            <Icon source={DragDropMajorMonotone} color="inkLightest" />
+          </Stack>
+        );
+    }
   }
 }
 