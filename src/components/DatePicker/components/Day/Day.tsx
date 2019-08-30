709dea2245b0efebb4a107b56c6b634fd21d758f
amrocha <andre.rocha@shopify.com>
2019-08-30 16:51:53 -0700

    Hookify Day

--- src/components/DatePicker/components/Day/Day.tsx
+++ src/components/DatePicker/components/Day/Day.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import {Months, isSameDay} from '@shopify/javascript-utilities/dates';
 import {classNames} from '../../../../utilities/css';
-import {
-  withAppProvider,
-  WithAppProviderProps,
-} from '../../../../utilities/with-app-provider';
+import {useI18n} from '../../../../utilities/i18n';
 
 import styles from '../../DatePicker.scss';
 
@@ -16,84 +13,72 @@ export interface DayProps {
   inHoveringRange?: boolean;
   disabled?: boolean;
   onClick?(day: Date): void;
-  onHover?(day: Date): void;
-  onFocus?(day: Date): void;
+  onHover?(day?: Date): void;
+  onFocus?(day?: Date): void;
 }
 
-type CombinedProps = DayProps & WithAppProviderProps;
+export function Day({
+  day,
+  focused,
+  onClick,
+  onHover = noop,
+  onFocus = noop,
+  selected,
+  inRange,
+  inHoveringRange,
+  disabled,
+}: DayProps) {
+  const i18n = useI18n();
+  const dayNode = useRef<HTMLButtonElement>(null);
 
-class Day extends React.PureComponent<CombinedProps, never> {
-  private dayNode: HTMLElement | null = null;
+  useEffect(
+    () => {
+      if (focused && dayNode.current) {
+        dayNode.current.focus();
+      }
+    },
+    [focused],
+  );
 
-  componentDidUpdate(prevProps: CombinedProps) {
-    if (!prevProps.focused && this.props.focused && this.dayNode) {
-      this.dayNode.focus();
-    }
+  if (!day) {
+    return <div className={styles.EmptyDay} onMouseOver={() => onHover(day)} />;
   }
+  const handleClick = onClick && !disabled ? onClick.bind(null, day) : noop;
+  const today = isSameDay(new Date(), day);
+  const className = classNames(
+    styles.Day,
+    selected && styles['Day-selected'],
+    disabled && styles['Day-disabled'],
+    today && styles['Day-today'],
+    (inRange || inHoveringRange) && !disabled && styles['Day-inRange'],
+  );
+  const date = day.getDate();
+  const tabIndex =
+    (focused || selected || today || date === 1) && !disabled ? 0 : -1;
+  const ariaLabel = [
+    `${today ? i18n.translate('Polaris.DatePicker.today') : ''}`,
+    `${Months[day.getMonth()]} `,
+    `${date} `,
+    `${day.getFullYear()}`,
+  ].join('');
 
-  render() {
-    const {
-      day,
-      focused,
-      onClick,
-      onHover = noop,
-      onFocus = noop,
-      selected,
-      inRange,
-      inHoveringRange,
-      disabled,
-      polaris: {intl},
-    } = this.props;
-
-    const handleHover = onHover.bind(null, day);
-    if (!day) {
-      return <div className={styles.EmptyDay} onMouseOver={handleHover} />;
-    }
-    const handleClick = onClick && !disabled ? onClick.bind(null, day) : noop;
-    const today = isSameDay(new Date(), day);
-    const className = classNames(
-      styles.Day,
-      selected && styles['Day-selected'],
-      disabled && styles['Day-disabled'],
-      today && styles['Day-today'],
-      (inRange || inHoveringRange) && !disabled && styles['Day-inRange'],
-    );
-    const date = day.getDate();
-    const tabIndex =
-      (focused || selected || today || date === 1) && !disabled ? 0 : -1;
-    const ariaLabel = [
-      `${today ? intl.translate('Polaris.DatePicker.today') : ''}`,
-      `${Months[day.getMonth()]} `,
-      `${date} `,
-      `${day.getFullYear()}`,
-    ].join('');
-
-    return (
-      <button
-        onFocus={onFocus.bind(null, day)}
-        type="button"
-        ref={this.setNode}
-        tabIndex={tabIndex}
-        className={className}
-        onMouseOver={handleHover}
-        onClick={handleClick}
-        aria-label={ariaLabel}
-        aria-selected={selected}
-        aria-disabled={disabled}
-        role="gridcell"
-      >
-        {date}
-      </button>
-    );
-  }
-
-  private setNode = (node: HTMLElement | null) => {
-    this.dayNode = node;
-  };
+  return (
+    <button
+      onFocus={() => onFocus(day)}
+      type="button"
+      ref={dayNode}
+      tabIndex={tabIndex}
+      className={className}
+      onMouseOver={() => onHover(day)}
+      onClick={handleClick}
+      aria-label={ariaLabel}
+      aria-selected={selected}
+      aria-disabled={disabled}
+      role="gridcell"
+    >
+      {date}
+    </button>
+  );
 }
-
-// Use named export once withAppProvider is refactored away
-// eslint-disable-next-line import/no-default-export
-export default withAppProvider<DayProps>()(Day);
 
 function noop() {}
