8cb411355b96252243d1367969ff3d03bc353724
Kaelig <kaelig.deloumeauprigent@shopify.com>
2019-09-10 23:00:22 -0700

    Plug data from git status and improve listing

--- scripts/observer/index.tsx
+++ scripts/observer/index.tsx
@@ -1,68 +1,56 @@
-import React from 'react';
+import path from 'path';
+import React, {useState, useEffect} from 'react';
 import {Box, Text, Color, render} from 'ink';
+import sortBy from 'lodash/sortBy';
 import {getGitStagedFiles, getDependencies} from '../../treebuilder';
 
-(async function() {
-  const stagedFiles = (await getGitStagedFiles('src/')) as string[];
-  console.log(stagedFiles);
-  const data = await getDependencies(
-    'src/**/*.tsx',
-    'src/**/*.test.tsx,src/**/index.ts',
-    stagedFiles,
-  );
-  console.log(data);
-})();
+const excludedFileNames = (fileName) =>
+  !fileName.includes('test') &&
+  !fileName.includes('types') &&
+  !fileName.endsWith('index.ts') &&
+  !fileName.endsWith('utils.tsx');
 
-const data = [
-  {
-    path: 'src/components/Button/',
-    filename: 'Button.tsx',
-    componentsAffected: [
-      {path: 'src/components/ButtonGroup/', filename: 'ButtonGroup.tsx'},
-      {path: 'src/components/CalloutCard/', filename: 'CalloutCard.tsx'},
-      {path: 'src/components/Caption/', filename: 'Caption.tsx'},
-      {path: 'src/components/TopBar/', filename: 'TopBar.tsx'},
-    ],
-  },
-  {
-    path: 'src/components/Button/',
-    filename: 'Button.scss',
-    componentsAffected: [
-      {path: 'src/components/ButtonGroup/', filename: 'ButtonGroup.tsx'},
-      {path: 'src/components/Caption/', filename: 'Caption.tsx'},
-      {path: 'src/components/Button/', filename: 'Button.tsx'},
-    ],
-  },
-  {
-    path: 'src/components/Checkbox/',
-    filename: 'Checkbox.tsx',
-    componentsAffected: [
-      {
-        path: 'src/components/ContextualSaveBar/',
-        filename: 'ContextualSaveBar.tsx',
-      },
-      {
-        path: 'src/components/EmptySearchResult/',
-        filename: 'EmptySearchResult.tsx',
-      },
-      {path: 'src/components/Labelled/', filename: 'Labelled.tsx'},
-      {path: 'src/components/Button/', filename: 'Button.tsx'},
-    ],
-  },
-];
+const formatDependencies = (dependencies) =>
+  dependencies
+    .filter(({fileName}) => excludedFileNames(fileName))
+    .map((dependency) => ({
+      path: `${path.dirname(dependency.fileName)}/`,
+      filename: path.basename(dependency.fileName),
+      dependencies: sortBy(
+        dependency.dependencies.filter(excludedFileNames).map((dependency) => ({
+          path: `${path.dirname(dependency)}/`,
+          filename: path.basename(dependency),
+          componentName: path
+            .dirname(dependency)
+            .replace('src/components/', '')
+            .split('/')[0],
+        })),
+        ['path', 'filename'],
+      ),
+    }));
 
-const Component = ({path, filename, componentsAffected}) => (
+const Component = ({path, filename, dependencies}) => (
   <Box marginBottom={1} flexDirection="column">
     <Box paddingLeft={2}>
       {filename.endsWith('tsx') ? '🧩  ' : '💅 '}
-      <Text underline>
-        <Color dim>{path}</Color>
-        <Text bold>{filename}</Text>
+      <Text bold>
+        <Color greenBright>
+          {path}
+          {filename}
+        </Color>
       </Text>
     </Box>
-    <Box marginLeft={4}>Affected files: {componentsAffected.length}</Box>
-    {componentsAffected.map(({path, filename}) => (
+    <Box marginLeft={4}>
+      <Box width={23}>Component name</Box>
+      File potentially affected (total: {dependencies.length})
+    </Box>
+    {dependencies.map(({path, filename, componentName}) => (
       <Box marginLeft={4} key={path + filename}>
+        <Box width={23}>
+          <Color dim>{'<'}</Color>
+          <Color>{componentName}</Color>
+          <Color dim>{' />'}</Color>
+        </Box>
         <Text>
           <Color dim>{path}</Color>
           {filename}
@@ -74,12 +62,12 @@ const Component = ({path, filename, componentsAffected}) => (
 
 const Components = ({components}) => (
   <React.Fragment>
-    {components.map(({path, filename, componentsAffected}) => (
+    {components.map(({path, filename, dependencies}) => (
       <Component
         key={path + filename}
         path={path}
         filename={filename}
-        componentsAffected={componentsAffected}
+        dependencies={dependencies}
       />
     ))}
   </React.Fragment>
@@ -87,50 +75,69 @@ const Components = ({components}) => (
 
 const Summary = ({
   componentsModified,
-  componentsAffected,
+  dependencies,
 }: {
   componentsModified: number;
-  componentsAffected: number;
+  dependencies: number;
 }) => (
   <Box flexDirection="column">
     <Box>
-      <Box width={22}>
-        <Text>Components modified:</Text>
+      <Box width={30}>
+        <Text>Files modified:</Text>
       </Box>
       {componentsModified}
     </Box>
     <Box>
-      <Box width={22}>
-        <Text>Components affected:</Text>
+      <Box width={30}>
+        <Text>Files potentially affected:</Text>
       </Box>
-      {componentsAffected}
+      {dependencies}
     </Box>
   </Box>
 );
 
-render(
-  <React.Fragment>
-    <Box marginBottom={1}>
-      💡 tip: command + click file paths to open them in your text editor
-    </Box>
-    <Components components={data} />
-    <Summary
-      componentsModified={data.length}
-      componentsAffected={
-        new Map([
-          ...data.map(({path, filename}) => [path, path + filename]),
-          ...data.reduce(
-            (val, curr) =>
-              val.concat(
-                curr.componentsAffected.map(({path}) => [
-                  path,
-                  curr.path + curr.filename,
-                ]),
-              ),
-            [],
-          ),
-        ]).size
-      }
-    />
-  </React.Fragment>,
-);
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const stagedFiles = (await getGitStagedFiles('src/')) as string[];
+      const dependencies = await getDependencies(
+        'src/**/*.tsx',
+        '*.test.tsx',
+        stagedFiles,
+      );
+      setData(formatDependencies(dependencies));
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Box marginBottom={1}>
+        💡 tip: command + click file paths to open them in your text editor
+      </Box>
+      <Components components={data} />
+      <Summary
+        componentsModified={data.length}
+        dependencies={
+          new Map([
+            ...data.map(({path, filename}) => [path, path + filename]),
+            ...data.reduce(
+              (val, curr) =>
+                val.concat(
+                  curr.dependencies.map(({path}) => [
+                    path,
+                    curr.path + curr.filename,
+                  ]),
+                ),
+              [],
+            ),
+          ]).size
+        }
+      />
+    </React.Fragment>
+  );
+};
+
+render(<App />);
