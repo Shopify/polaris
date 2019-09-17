sijad/polaris-react

remove app bridge

src/components/AppProvider/AppProvider.tsx
@@ -7,11 +7,6 @@ import {
   ScrollLockManagerContext,
 } from '../../utilities/scroll-lock-manager';
 import {
-  createAppBridge,
-  AppBridgeContext,
-  AppBridgeOptions,
-} from '../../utilities/app-bridge';
-import {
   StickyManager,
   StickyManagerContext,
 } from '../../utilities/sticky-manager';
@@ -19,11 +14,10 @@ import {LinkContext, LinkLikeComponent} from '../../utilities/link';
 
 interface State {
   intl: I18n;
-  appBridge: ReturnType<typeof createAppBridge>;
   link: LinkLikeComponent | undefined;
 }
 
-export interface Props extends AppBridgeOptions {
+export interface Props {
   /** A locale object or array of locale objects that overrides default translations */
   i18n: TranslationDictionary | TranslationDictionary[];
   /** A custom component to use for all links used by Polaris components */
@@ -40,13 +34,12 @@ export default class AppProvider extends React.Component<Props, State> {
     super(props);
     this.stickyManager = new StickyManager();
     this.scrollLockManager = new ScrollLockManager();
-    const {i18n, apiKey, shopOrigin, forceRedirect, linkComponent} = this.props;
+    const {i18n, linkComponent} = this.props;
 
     // eslint-disable-next-line react/state-in-constructor
     this.state = {
       link: linkComponent,
       intl: new I18n(i18n),
-      appBridge: createAppBridge({shopOrigin, apiKey, forceRedirect}),
     };
   }
 
@@ -59,19 +52,10 @@ export default class AppProvider extends React.Component<Props, State> {
   componentDidUpdate({
     i18n: prevI18n,
     linkComponent: prevLinkComponent,
-    apiKey: prevApiKey,
-    shopOrigin: prevShopOrigin,
-    forceRedirect: prevForceRedirect,
   }: Props) {
-    const {i18n, linkComponent, apiKey, shopOrigin, forceRedirect} = this.props;
+    const {i18n, linkComponent} = this.props;
 
-    if (
-      i18n === prevI18n &&
-      linkComponent === prevLinkComponent &&
-      apiKey === prevApiKey &&
-      shopOrigin === prevShopOrigin &&
-      forceRedirect === prevForceRedirect
-    ) {
+    if (i18n === prevI18n && linkComponent === prevLinkComponent) {
       return;
     }
 
@@ -79,25 +63,22 @@ export default class AppProvider extends React.Component<Props, State> {
     this.setState({
       link: linkComponent,
       intl: new I18n(i18n),
-      appBridge: createAppBridge({shopOrigin, apiKey, forceRedirect}),
     });
   }
 
   render() {
     const {theme = {logo: null}, children} = this.props;
-    const {intl, appBridge, link} = this.state;
+    const {intl, link} = this.state;
 
     return (
       <I18nContext.Provider value={intl}>
         <ScrollLockManagerContext.Provider value={this.scrollLockManager}>
           <StickyManagerContext.Provider value={this.stickyManager}>
-            <AppBridgeContext.Provider value={appBridge}>
-              <LinkContext.Provider value={link}>
-                <ThemeProvider theme={theme}>
-                  {React.Children.only(children)}
-                </ThemeProvider>
-              </LinkContext.Provider>
-            </AppBridgeContext.Provider>
+            <LinkContext.Provider value={link}>
+              <ThemeProvider theme={theme}>
+                {React.Children.only(children)}
+              </ThemeProvider>
+            </LinkContext.Provider>
           </StickyManagerContext.Provider>
         </ScrollLockManagerContext.Provider>
       </I18nContext.Provider>

Shopify/polaris

src/components/AppProvider/AppProvider.tsx
@@ -7,11 +7,6 @@ import {
   ScrollLockManagerContext,
 } from '../../utilities/scroll-lock-manager';
 import {
-  createAppBridge,
-  AppBridgeContext,
-  AppBridgeOptions,
-} from '../../utilities/app-bridge';
-import {
   StickyManager,
   StickyManagerContext,
 } from '../../utilities/sticky-manager';
@@ -24,11 +19,10 @@ import {
 
 interface State {
   intl: I18n;
-  appBridge: ReturnType<typeof createAppBridge>;
   link: LinkLikeComponent | undefined;
 }
 
-export interface AppProviderProps extends AppBridgeOptions {
+export interface AppProviderProps {
   /** A locale object or array of locale objects that overrides default translations */
   i18n: TranslationDictionary | TranslationDictionary[];
   /** A custom component to use for all links used by Polaris components */
@@ -50,13 +44,12 @@ export class AppProvider extends React.Component<AppProviderProps, State> {
     this.scrollLockManager = new ScrollLockManager();
     this.uniqueIdFactory = new UniqueIdFactory(globalIdGeneratorFactory);
 
-    const {i18n, apiKey, shopOrigin, forceRedirect, linkComponent} = this.props;
+    const {i18n, linkComponent} = this.props;
 
     // eslint-disable-next-line react/state-in-constructor
     this.state = {
       link: linkComponent,
       intl: new I18n(i18n),
-      appBridge: createAppBridge({shopOrigin, apiKey, forceRedirect}),
     };
   }
 
@@ -69,19 +62,10 @@ export class AppProvider extends React.Component<AppProviderProps, State> {
   componentDidUpdate({
     i18n: prevI18n,
     linkComponent: prevLinkComponent,
-    apiKey: prevApiKey,
-    shopOrigin: prevShopOrigin,
-    forceRedirect: prevForceRedirect,
   }: AppProviderProps) {
-    const {i18n, linkComponent, apiKey, shopOrigin, forceRedirect} = this.props;
+    const {i18n, linkComponent} = this.props;
 
-    if (
-      i18n === prevI18n &&
-      linkComponent === prevLinkComponent &&
-      apiKey === prevApiKey &&
-      shopOrigin === prevShopOrigin &&
-      forceRedirect === prevForceRedirect
-    ) {
+    if (i18n === prevI18n && linkComponent === prevLinkComponent) {
       return;
     }
 
@@ -89,24 +73,21 @@ export class AppProvider extends React.Component<AppProviderProps, State> {
     this.setState({
       link: linkComponent,
       intl: new I18n(i18n),
-      appBridge: createAppBridge({shopOrigin, apiKey, forceRedirect}),
     });
   }
 
   render() {
     const {theme = {logo: null}, children} = this.props;
-    const {intl, appBridge, link} = this.state;
+    const {intl, link} = this.state;
 
     return (
       <I18nContext.Provider value={intl}>
         <ScrollLockManagerContext.Provider value={this.scrollLockManager}>
           <StickyManagerContext.Provider value={this.stickyManager}>
             <UniqueIdFactoryContext.Provider value={this.uniqueIdFactory}>
-              <AppBridgeContext.Provider value={appBridge}>
-                <LinkContext.Provider value={link}>
-                  <ThemeProvider theme={theme}>{children}</ThemeProvider>
-                </LinkContext.Provider>
-              </AppBridgeContext.Provider>
+              <LinkContext.Provider value={link}>
+                <ThemeProvider theme={theme}>{children}</ThemeProvider>
+              </LinkContext.Provider>
             </UniqueIdFactoryContext.Provider>
           </StickyManagerContext.Provider>
         </ScrollLockManagerContext.Provider>