92be77cbafc8f20571a0f7ef79ec975f15f2c98b
Tim Layton <tim.layton@shopify.com>
2019-09-27 13:31:14 -0700

    add test case for throwing

--- src/utilities/features/tests/hooks.test.tsx
+++ src/utilities/features/tests/hooks.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {mountWithApp} from 'test-utilities';
+import {mount, mountWithApp} from 'test-utilities';
 import {useFeatures} from '../hooks';
 
 function Component() {
@@ -10,10 +10,25 @@ function Component() {
 }
 
 describe('useFeatures', () => {
+  let consoleErrorSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    consoleErrorSpy.mockRestore();
+  });
+
   it('returns context', () => {
     const component = mountWithApp(<Component />, {
       features: {foo: true},
     });
     expect(component).toContainReactComponent('div');
+  });
+
+  it('throws an error if context is not set', () => {
+    const attemptMount = () => mount(<Component />);
+    expect(attemptMount).toThrow('No Features were provided.');
   });
 });
