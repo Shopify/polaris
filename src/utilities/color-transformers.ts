e9d6f8eb3bfb48d1e915d37b3d8e94989a964552
Ruairi Hackett <ruairi.hackett@shopify.com>
2019-10-11 12:43:52 +0100

    Change default value in rgbStringToHex

--- src/utilities/color-transformers.ts
+++ src/utilities/color-transformers.ts
@@ -347,9 +347,12 @@ export function nameToHex(value: string) {
 }
 
 export function rgbStringToHex(value: string) {
-  const rgb =
-    normalizeColorString(value).match(RGB_STRING_TO_HEX_REGEX) ||
-    'rgb(0, 0, 0)';
+  const rgb = normalizeColorString(value).match(RGB_STRING_TO_HEX_REGEX) || [
+    'rgb(0,0,0)',
+    '0',
+    '0',
+    '0',
+  ];
   return rgbToHex({
     red: parseInt(rgb[1], 10),
     green: parseInt(rgb[2], 10),
