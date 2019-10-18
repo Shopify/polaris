955e43096b9c41b400a69e00cb9d6972f98b7a34
Ben Scott <ben.scott@shopify.com>
2019-10-18 14:00:10 -0700

    PR Feedback

--- scripts/utilities/retry.js
+++ scripts/utilities/retry.js
@@ -3,7 +3,8 @@ function sleep(ms) {
 }
 
 function retry(functionToTry, maxAttempts = 3, delay = 1000) {
-  // We should fix this but I've not got the time at the moment
+  // TODO work out how to keep eslint happy
+  // SEE https://eslint.org/docs/rules/no-async-promise-executor
   // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve, reject) => {
     let remainingAttempts = maxAttempts;
