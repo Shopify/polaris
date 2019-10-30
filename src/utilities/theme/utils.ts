1eee99bc8655c7037f85fec6efc46f0c7f0806c5
Dan Rosenthal <dan.rosenthal@shopify.com>
2019-10-30 08:34:22 -0400

    Revert "[Color system] Apply color system to text field #2295"
    This reverts commit f576cfd74c61ad3225fa52c4c11e0fca21a52ef6.

--- src/utilities/theme/utils.ts
+++ src/utilities/theme/utils.ts
@@ -305,10 +305,6 @@ function overrides() {
     [toCssCustomPropertySyntax('borderCriticalDisabled')]: `${rem(
       '1px',
     )} solid var(--p-critical-divider-disabled)`,
-    [toCssCustomPropertySyntax('textFieldSpinnerOffset')]: rem('2px'),
-    [toCssCustomPropertySyntax('textFieldFocusRingOffset')]: rem('-4px'),
-    [toCssCustomPropertySyntax('textFieldFocusRingBorderRadius')]: rem('7px'),
-    [toCssCustomPropertySyntax('focusRingStroke')]: rem('2px'),
   };
 }
 
