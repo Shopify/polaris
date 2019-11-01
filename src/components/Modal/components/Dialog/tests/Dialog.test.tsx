36088ce912e18f9a2d336b47edaa473f84ab9be7
Darrel-Day Guerrero <darrelday.guerrero@shopify.com>
2019-11-01 16:14:38 -0400

    Test aria-modal attribute to Modal

--- src/components/Modal/components/Dialog/tests/Dialog.test.tsx
+++ src/components/Modal/components/Dialog/tests/Dialog.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {animationFrame} from '@shopify/jest-dom-mocks';
 // eslint-disable-next-line no-restricted-imports
 import {trigger, mountWithAppProvider} from 'test-utilities/legacy';
-import {KeypressListener} from 'components';
+import {KeypressListener, TrapFocus} from 'components';
 import {Dialog} from '../Dialog';
 
 describe('<Dialog>', () => {
@@ -33,5 +33,14 @@ describe('<Dialog>', () => {
 
     trigger(dialog.find('FadeUp'), 'onEntered');
     expect(dialog.find('FadeUp').prop('onEntered')).toHaveBeenCalledTimes(1);
+  });
+
+  it('sets the aria-modal attribute to true', () => {
+    const trapFocus = mountWithAppProvider(
+      <Dialog labelledBy="test" onClose={jest.fn()} in>
+        something
+      </Dialog>,
+    ).find('div.Modal');
+    expect(trapFocus.prop('aria-modal')).toBe(true);
   });
 });
