0c2f8b6928c2844fb078116983a546cd28accac2
Marten Bjork <marten.bjork@shopify.com>
2019-11-15 19:10:26 -0500

    Disable linting for uppercase export

--- src/utilities/theme/index.ts
+++ src/utilities/theme/index.ts
@@ -9,5 +9,6 @@ export {UNSTABLE_Color} from './color-adjustments';
 export {
   buildCustomProperties,
   buildThemeContext,
+  // eslint-disable-next-line @typescript-eslint/camelcase
   buildColors as UNSTABLE_buildColors,
 } from './utils';
