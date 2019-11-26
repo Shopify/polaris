31d31407276bfe0be76690aaa037f3184f3a7ac9
Tim Layton <tim.layton@shopify.com>
2019-11-26 15:00:26 -0800

    chipping away

--- src/utilities/theme/role-variants.ts
+++ src/utilities/theme/role-variants.ts
@@ -145,152 +145,231 @@ export const roleVariants: RoleVariants = {
     },
     {
       name: 'iconDisabledOnSurface',
+      description: 'For use as the fill color of disabled neutral icons.',
       light: {lightness: 68},
       dark: {lightness: 75},
     },
     {
       name: 'iconSubduedOnSurface',
+      description: 'For use as the fill color of subdued neutral icons.',
       light: {lightness: 59.8},
       dark: {lightness: 62},
     },
     {
       name: 'textOnSurface',
+      description: 'For use as a neutral text color.',
       light: {lightness: 13},
       dark: {lightness: 100},
     },
     {
       name: 'textDisabledOnSurface',
+      description: 'For use as a disabled neutral text color.',
       light: {lightness: 61.3},
       dark: {lightness: 50},
     },
     {
       name: 'textSubduedOnSurface',
+      description: 'For use as a subdued neutral text color.',
       light: {lightness: 38},
       dark: {lightness: 62},
     },
   ],
   interactive: [
     {
+      name: 'interactive',
+      description:
+        'While use directly in our components is discouraged, the base variant is unmodified from the original role input color.',
+      light: {},
+      dark: {},
+    },
+    {
       name: 'interactiveAction',
+      description: 'Used for links and plain buttons.',
       light: {lightness: 44},
       dark: {lightness: 65},
     },
     {
       name: 'interactiveActionDisabled',
+      description: 'Used for disabled links and plain buttons.',
       light: {lightness: 58},
       dark: {lightness: 42},
     },
     {
       name: 'interactiveActionHovered',
+      description: 'Used for hovered links and plain buttons.',
       light: {lightness: 37},
       dark: {lightness: 70},
     },
     {
       name: 'interactiveActionSubdued',
+      description: 'Used for subdued links and plain buttons.',
       light: {lightness: 51},
       dark: {lightness: 49},
     },
     {
       name: 'interactiveActionPressed',
+      description: 'Used for pressed links and plain buttons.',
       light: {lightness: 31},
       dark: {lightness: 75},
     },
     {
       name: 'interactiveFocus',
+      description: 'For use in the focus ring on interactive elements.',
       light: {lightness: 58},
       dark: {lightness: 42},
     },
     {
       name: 'interactiveSelected',
+      description:
+        'For use as a surface color in selected interactive elements, in components such as option list and resource list.',
       light: {lightness: 96},
       dark: {lightness: 4},
     },
     {
       name: 'interactiveSelectedHovered',
+      description:
+        'For use as a surface color in selected interactive elements that are hovered, in components such as option list and resource list.',
       light: {lightness: 89},
       dark: {lightness: 11},
     },
     {
       name: 'interactiveSelectedPressed',
+      description:
+        'For use as a surface color in selected interactive elements that are pressed, in components such as option list and resource list.',
       light: {lightness: 82},
       dark: {lightness: 18},
     },
   ],
   neutral: [
     {
-      name: 'neutralActionDisabled',
-      light: {lightness: 94},
-      dark: {lightness: 13},
+      name: 'neutral',
+      description:
+        'While use directly in our components is discouraged, the base variant is unmodified from the original role input color.',
+      light: {},
+      dark: {},
     },
     {
       name: 'neutralAction',
+      description:
+        'Used for secondary buttons and tertiary buttons, as well as in form elements as a background color and pontentially other neutral surfaces.',
       light: {lightness: 93},
       dark: {lightness: 22},
     },
     {
+      name: 'neutralActionDisabled',
+      description: 'Used as a disabled state for secondary buttons',
+      light: {lightness: 94},
+      dark: {lightness: 13},
+    },
+    {
       name: 'neutralActionHovered',
+      description: 'Used as a hovered state for secondary buttons',
       light: {lightness: 90},
       dark: {lightness: 25},
     },
     {
       name: 'neutralActionPressed',
+      description: 'Used as a pressed state for secondary buttons',
       light: {lightness: 87},
       dark: {lightness: 39},
     },
   ],
   branded: [
     {
+      name: 'branded',
+      description:
+        'While use directly in our components is discouraged, the base variant is unmodified from the original role input color.',
+      light: {},
+      dark: {},
+    },
+    {
       name: 'brandedAction',
+      description:
+        'Used as the background color for primary actions, and as the fill color for icons and the text color in navigation and tabs to communicate interaction states.',
       light: {lightness: 47.3},
       dark: {lightness: 47.3},
     },
     {
       name: 'brandedActionDisabled',
+      description:
+        'Used as the background color for disabled primary actions, and as the fill color for icons and the text color in navigation and tabs to communicate interaction states.',
       light: {lightness: 32},
       dark: {lightness: 32},
     },
     {
       name: 'brandedActionHovered',
+      description:
+        'Used as the background color for hovered primary actions, and as the fill color for icons and the text color in navigation and tabs to communicate interaction states.',
       light: {lightness: 42.3},
       dark: {lightness: 55},
     },
     {
       name: 'brandedActionPressed',
+      description:
+        'Used as the background color for pressed primary actions, and as the fill color for icons and the text color in navigation and tabs to communicate interaction states.',
       light: {lightness: 37.3},
       dark: {lightness: 60},
     },
     {
       name: 'iconOnBranded',
+      description:
+        'For use as a fill color for icons on primary actions. Not for use in icons on navigation and tabs.',
       light: {lightness: 98},
       dark: {lightness: 98},
     },
     {
       name: 'iconSubduedOnBranded',
+      description:
+        'For use as a fill color for icons on subdued primary actions. Not for use in icons on navigation and tabs.',
       light: {lightness: 88},
       dark: {lightness: 88},
     },
     {
+      name: 'iconDisabledOnBranded',
+      description:
+        'For use as a fill color for icons on disabled primary actions. Not for use in icons on navigation and tabs.',
+      light: {lightness: 78},
+      dark: {lightness: 78},
+    },
+    {
       name: 'textOnBranded',
+      description:
+        'For use as a text color on primary actions. Not for use in text on navigation and tabs.',
       light: {lightness: 100},
       dark: {lightness: 100},
     },
     {
       name: 'textSubduedOnBranded',
+      description:
+        'For use as a text color on subdued primary actions. Not for use in text on navigation and tabs.',
+      light: {lightness: 90},
+      dark: {lightness: 90},
+    },
+    {
+      name: 'textDisabledOnBranded',
+      description:
+        'For use as a text color on disabled primary actions. Not for use in text on navigation and tabs.',
       light: {lightness: 90},
       dark: {lightness: 90},
     },
     {
       name: 'brandedSelected',
+      description:
+        'Used as a surface color to indicate selected interactive states in navigation and tabs.',
       light: {lightness: 95, saturation: 30},
       dark: {lightness: 5, saturation: 30},
     },
     {
       name: 'brandedSelectedHovered',
+      description:
+        'Used as a surface color to indicate selected interactive states that are hovered in navigation and tabs.',
       light: {lightness: 81, saturation: 22},
       dark: {lightness: 19, saturation: 22},
     },
     {
       name: 'brandedSelectedPressed',
+      description:
+        'Used as a surface color to indicate selected interactive states that are pressed in navigation and tabs.',
       light: {lightness: 74, saturation: 22},
       dark: {lightness: 26, saturation: 22},
     },
