d8ba2fc778b243a009bf61d0098b5e9818f0bd28
Sajjad Hashemian <wolaws@gmail.com>
2019-11-27 22:52:37 +0330

    add more tests

--- src/components/Frame/components/Loading/tests/Loading.test.tsx
+++ src/components/Frame/components/Loading/tests/Loading.test.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import {mountWithAppProvider} from 'test-utilities/legacy';
+import {animationFrame} from '@shopify/jest-dom-mocks';
+import {act} from 'react-dom/test-utils';
 import {Loading} from '../Loading';
 
 describe('<Loading />', () => {
-  const loading = mountWithAppProvider(<Loading />);
-
-  it('mounts', () => {
-    expect(loading.exists()).toBe(true);
+  beforeEach(() => {
+    animationFrame.mock();
   });
 
-  it('unmounts safely', () => {
+  afterEach(() => {
+    animationFrame.restore();
+  });
+
+  it('increases over time', () => {
+    const loading = mountWithAppProvider(<Loading />);
+
+    for (let i = 0; i <= 100; i++) {
+      act(() => animationFrame.runFrame());
+    }
+
+    loading.update();
+
+    expect(loading.find('.Level').prop('aria-valuenow')).toBe(26);
+  });
+
+  it('cancels the animationFrame on unmount', () => {
+    const cancelAnimationFrameSpy = jest.spyOn(window, 'cancelAnimationFrame');
+    const loading = mountWithAppProvider(<Loading />);
+
     expect(() => {
       loading.unmount();
     }).not.toThrow();
+
+    expect(cancelAnimationFrameSpy).toHaveBeenCalledTimes(1);
   });
 });
