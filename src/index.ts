dd136758cc5b40ff426116c240625d5038553f15
Dan Rosenthal <dan.rosenthal@shopify.com>
2019-12-12 15:03:15 -0500

    format

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ export * from './types';
 
 export * from './components';
 
-export { ScrollLockManagerContext as _SECRET_INTERNAL_SCROLL_LOCK_MANAGER_CONTEXT } from './utilities/scroll-lock-manager';
-export { WithinContentContext as _SECRET_INTERNAL_WITHIN_CONTENT_CONTEXT } from './utilities/within-content-context';
+export {ScrollLockManagerContext as _SECRET_INTERNAL_SCROLL_LOCK_MANAGER_CONTEXT} from './utilities/scroll-lock-manager';
+export {WithinContentContext as _SECRET_INTERNAL_WITHIN_CONTENT_CONTEXT} from './utilities/within-content-context';
 
-export { AppBridgeContext } from './utilities/app-bridge';
+export {AppBridgeContext} from './utilities/app-bridge';
 
 /* eslint-disable @typescript-eslint/camelcase */
 export {
