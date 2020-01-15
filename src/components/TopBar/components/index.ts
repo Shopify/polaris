1510092b88c190ac389f5f6e9bae329b19c5de03
mitchmaps <42747959+mitchmaps@users.noreply.github.com>
2020-01-15 11:22:47 -0500

    [TopBar.Search] Fix search dismissal overlay (#2440)
    Co-authored-by: Joshua Allen <32902385+joshuajay@users.noreply.github.com>

--- src/components/TopBar/components/index.ts
+++ src/components/TopBar/components/index.ts
@@ -2,6 +2,8 @@ export {Search, SearchProps} from './Search';
 
 export {SearchField, SearchFieldProps} from './SearchField';
 
+export {SearchDismissOverlay} from './SearchDismissOverlay';
+
 export {UserMenu, UserMenuProps} from './UserMenu';
 
 export {Menu, MenuProps} from './Menu';
