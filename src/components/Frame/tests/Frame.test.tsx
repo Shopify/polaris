a88bac5080467b94599b8faf900904ddf9a9bfd6
Kyle Durand <kyle.durand@shopify.com>
2020-02-04 13:10:18 -0500

    High contrast

--- src/components/Frame/tests/Frame.test.tsx
+++ src/components/Frame/tests/Frame.test.tsx
@@ -303,6 +303,21 @@ describe('<Frame />', () => {
       });
     });
 
+    it('renders a ContextualSaveBar with a newDesignLanguage class when newDesignLanguage is true', () => {
+      const frame = mountWithApp(
+        <Frame topBar={<div />}>
+          <PolarisLoading />
+        </Frame>,
+        {
+          features: {newDesignLanguage: true},
+        },
+      );
+      expect(frame).toContainReactComponent('div', {
+        className:
+          'ContextualSaveBar ContextualSaveBar-newDesignLanguage startFadeUp',
+      });
+    });
+
     it('renders a GlobalRibbon container with a newDesignLanguage class when newDesignLanguage is true', () => {
       const frame = mountWithApp(
         <Frame globalRibbon={<div />}>
