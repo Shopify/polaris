4e08bb05835e3ddd0e1cf263dda8b7b4f7047d35
Kyle Durand <kyledurand@users.noreply.github.com>
2020-02-05 14:54:49 -0500

    [Avatar] Add styled placeholder when initials are blank (#2693)
    * Add style to placeholder when initials are blank
    
    * Simplify svg

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -114,22 +114,24 @@ export function Avatar({
   // Use `dominant-baseline: central` instead of `dy` when Edge supports it.
   const verticalOffset = '0.35em';
 
-  const avatarBody = customer ? (
-    <g fill="currentColor" fillRule="nonzero">
-      <path d="M8.28 27.5A14.95 14.95 0 0120 21.8c4.76 0 8.97 2.24 11.72 5.7a14.02 14.02 0 01-8.25 5.91 14.82 14.82 0 01-6.94 0 14.02 14.02 0 01-8.25-5.9zM13.99 12.78a6.02 6.02 0 1112.03 0 6.02 6.02 0 01-12.03 0z" />
-    </g>
-  ) : (
-    <text
-      x="50%"
-      y="50%"
-      dy={verticalOffset}
-      fill="currentColor"
-      fontSize="20"
-      textAnchor="middle"
-    >
-      {initials}
-    </text>
-  );
+  const avatarBody =
+    customer || !initials ? (
+      <path
+        fill="currentColor"
+        d="M8.28 27.5A14.95 14.95 0 0120 21.8c4.76 0 8.97 2.24 11.72 5.7a14.02 14.02 0 01-8.25 5.91 14.82 14.82 0 01-6.94 0 14.02 14.02 0 01-8.25-5.9zM13.99 12.78a6.02 6.02 0 1112.03 0 6.02 6.02 0 01-12.03 0z"
+      />
+    ) : (
+      <text
+        x="50%"
+        y="50%"
+        dy={verticalOffset}
+        fill="currentColor"
+        fontSize="20"
+        textAnchor="middle"
+      >
+        {initials}
+      </text>
+    );
 
   const svgMarkup = !hasImage ? (
     <span className={styles.Initials}>
