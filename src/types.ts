dd48d35514a90f70df996aa99956981d8ef13717
Dan Rosenthal <dan.rosenthal@shopify.com>
2020-02-26 11:06:35 -0500

    update icon component to consume new design langauge

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,19 @@
+export type NewDesignLanguageColor =
+  | 'base'
+  | 'disabled'
+  | 'hovered'
+  | 'pressed'
+  | 'subdued'
+  | 'critical'
+  | 'warning'
+  | 'highlight'
+  | 'interactive'
+  | 'success'
+  | 'primary'
+  | 'primaryDisabled'
+  | 'primaryHovered'
+  | 'primaryPressed';
+
 export type Color =
   | 'white'
   | 'black'
@@ -45,7 +61,7 @@ export interface IconProps {
   /** The SVG contents to display in the icon (icons should fit in a 20 × 20 pixel viewBox) */
   source: IconSource;
   /** Set the color for the SVG fill */
-  color?: Color;
+  color?: Color | NewDesignLanguageColor;
   /** Show a backdrop behind the icon */
   backdrop?: boolean;
   /** Descriptive text to be read to screenreaders */
