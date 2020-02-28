5b0f8289e6c0689ce79ca3cf5ef94c8b6a05847e
Kyle Durand <kyle.durand@shopify.com>
2020-02-28 10:15:50 -0500

    Add animation to navigation

--- src/components/Navigation/components/Item/components/Secondary/Secondary.tsx
+++ src/components/Navigation/components/Item/components/Secondary/Secondary.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 import {useUniqueId} from '../../../../../../utilities/unique-id';
 import {Collapsible} from '../../../../../Collapsible';
-
 import styles from '../../../../Navigation.scss';
+import {useFeatures} from '../../../../../../utilities/features';
+import {Tokens} from '../../../../../../utilities/theme';
 
 interface SecondaryProps {
   expanded: boolean;
@@ -12,8 +13,15 @@ interface SecondaryProps {
 
 export function Secondary({children, expanded}: SecondaryProps) {
   const id = useUniqueId('SecondaryNavigation');
+  const {newDesignLanguage = false} = useFeatures();
+
+  const transitionProperties = {
+    duration: newDesignLanguage ? Tokens.duration150 : '0ms',
+    timingFunction: newDesignLanguage ? Tokens.ease : 'none',
+  };
+
   return (
-    <Collapsible id={id} open={expanded}>
+    <Collapsible id={id} open={expanded} transition={transitionProperties}>
       <ul className={styles.List}>{children}</ul>
     </Collapsible>
   );
