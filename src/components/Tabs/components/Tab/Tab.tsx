vanoMekantsishvili/polaris-react

Apply the design system to Tabs

src/components/Tabs/components/Tab/Tab.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useRef} from 'react';
 import {focusFirstFocusableNode} from '@shopify/javascript-utilities/focus';
 
 import {classNames} from '../../../../utilities/css';
+import {useFeatures} from '../../../../utilities/features';
 import {UnstyledLink} from '../../../UnstyledLink';
 import {handleMouseUpByBlurring} from '../../../../utilities/focus';
 
@@ -35,6 +36,7 @@ export function Tab({
   const wasSelected = useRef(selected);
   const panelFocused = useRef(false);
   const node = useRef<HTMLLIElement | null>(null);
+  const {newDesignLanguage} = useFeatures();
 
   // A tab can start selected when it is moved from the disclosure dropdown
   // into the main list, so we need to send focus from the tab to the panel
@@ -69,6 +71,10 @@ export function Tab({
   const handleClick = onClick && onClick.bind(null, id);
 
   const className = classNames(styles.Tab, selected && styles['Tab-selected']);
+  const selectedUnderlineClassName = classNames(
+    styles.tabUnderline,
+    selected && styles.selectedTabUnderline,
+  );
 
   let tabIndex: 0 | -1;
 
@@ -79,6 +85,11 @@ export function Tab({
   } else {
     tabIndex = -1;
   }
+
+  const tabTitleClassNames = classNames(
+    styles.Title,
+    newDesignLanguage && styles.newDesignLanguage,
+  );
 
   const markup = url ? (
     <UnstyledLink
@@ -93,7 +104,7 @@ export function Tab({
       aria-label={accessibilityLabel}
       onMouseUp={handleMouseUpByBlurring}
     >
-      <span className={styles.Title}>{children}</span>
+      <span className={tabTitleClassNames}>{children}</span>
     </UnstyledLink>
   ) : (
     <button
@@ -108,7 +119,8 @@ export function Tab({
       aria-label={accessibilityLabel}
       onMouseUp={handleMouseUpByBlurring}
     >
-      <span className={styles.Title}>{children}</span>
+      <span className={tabTitleClassNames}>{children}</span>
+      {newDesignLanguage && <div className={selectedUnderlineClassName} />}
     </button>
   );
 

Shopify/polaris

src/components/Tabs/components/Tab/Tab.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useRef} from 'react';
 import {focusFirstFocusableNode} from '@shopify/javascript-utilities/focus';
 
 import {classNames} from '../../../../utilities/css';
+import {useFeatures} from '../../../../utilities/features';
 import {UnstyledLink} from '../../../UnstyledLink';
 import {handleMouseUpByBlurring} from '../../../../utilities/focus';
 
@@ -35,6 +36,7 @@ export function Tab({
   const wasSelected = useRef(selected);
   const panelFocused = useRef(false);
   const node = useRef<HTMLLIElement | null>(null);
+  const {newDesignLanguage} = useFeatures();
 
   // A tab can start selected when it is moved from the disclosure dropdown
   // into the main list, so we need to send focus from the tab to the panel
@@ -80,6 +82,17 @@ export function Tab({
     tabIndex = -1;
   }
 
+  const tabContainerClassNames = classNames(
+    styles.TabContainer,
+    selected && styles.Underline,
+    newDesignLanguage && styles.newDesignLanguage,
+  );
+
+  const tabTitleClassNames = classNames(
+    styles.Title,
+    newDesignLanguage && styles.newDesignLanguage,
+  );
+
   const markup = url ? (
     <UnstyledLink
       id={id}
@@ -93,7 +106,7 @@ export function Tab({
       aria-label={accessibilityLabel}
       onMouseUp={handleMouseUpByBlurring}
     >
-      <span className={styles.Title}>{children}</span>
+      <span className={tabTitleClassNames}>{children}</span>
     </UnstyledLink>
   ) : (
     <button
@@ -108,12 +121,12 @@ export function Tab({
       aria-label={accessibilityLabel}
       onMouseUp={handleMouseUpByBlurring}
     >
-      <span className={styles.Title}>{children}</span>
+      <span className={tabTitleClassNames}>{children}</span>
     </button>
   );
 
   return (
-    <li className={styles.TabContainer} ref={node}>
+    <li className={tabContainerClassNames} ref={node}>
       {markup}
     </li>
   );