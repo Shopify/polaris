285aaf882039f041d22c9e7a816bcc34868690de
Ruairi Hackett <ruairi.hackett@shopify.com>
2020-03-19 14:51:11 +0000

    Change export to *

--- src/components/ColorPicker/components/index.ts
+++ src/components/ColorPicker/components/index.ts
@@ -1,9 +1,9 @@
-export {AlphaField, AlphaFieldProps} from './AlphaField';
+export * from './AlphaField';
 
-export {AlphaPicker, AlphaPickerProps} from './AlphaPicker';
+export * from './AlphaPicker';
 
 export * from './HuePicker';
 
-export {Slidable, SlidableProps, Position} from './Slidable';
+export * from './Slidable';
 
-export {TextPicker, TextPickerProps} from './TextPicker';
+export * from './TextPicker';
