1d04440411aed280d59dfc3bc682051e774b0cda
Dan Rosenthal <dan.rosenthal@shopify.com>
2020-03-19 20:52:32 -0400

    Revert "[Navigation] Design language animation (#2796)"
    This reverts commit 20e6ff1d527ad22e358f74de4963a89522f74e2a.

--- src/components/Navigation/components/Item/components/Secondary/Secondary.tsx
+++ src/components/Navigation/components/Item/components/Secondary/Secondary.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 import {useUniqueId} from '../../../../../../utilities/unique-id';
 import {Collapsible} from '../../../../../Collapsible';
+
 import styles from '../../../../Navigation.scss';
-import {useFeatures} from '../../../../../../utilities/features';
-import {Tokens} from '../../../../../../utilities/theme';
 
 interface SecondaryProps {
   expanded: boolean;
@@ -13,15 +12,8 @@ interface SecondaryProps {
 
 export function Secondary({children, expanded}: SecondaryProps) {
   const id = useUniqueId('SecondaryNavigation');
-  const {newDesignLanguage = false} = useFeatures();
-
-  const transitionProperties = {
-    duration: newDesignLanguage ? Tokens.duration150 : '0ms',
-    timingFunction: newDesignLanguage ? Tokens.ease : 'none',
-  };
-
   return (
-    <Collapsible id={id} open={expanded} transition={transitionProperties}>
+    <Collapsible id={id} open={expanded}>
       <ul className={styles.List}>{children}</ul>
     </Collapsible>
   );
