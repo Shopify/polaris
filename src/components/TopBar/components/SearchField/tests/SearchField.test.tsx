bc91bedb59a7f72f5e15263827b5f8f682c87fd7
Alex Page <alex@alexpage.com.au>
2020-04-02 19:14:03 -0400

    Add test for showFocusBorder

--- src/components/TopBar/components/SearchField/tests/SearchField.test.tsx
+++ src/components/TopBar/components/SearchField/tests/SearchField.test.tsx
@@ -131,6 +131,16 @@ describe('<TextField />', () => {
     });
   });
 
+  it('showFocusBorder', () => {
+    const textField = mountWithAppProvider(
+      <SearchField value="" onChange={noop} showFocusBorder />,
+    );
+
+    expect(textField.find('div').last().prop('className')).toBe(
+      'Backdrop BackdropShowFocusBorder',
+    );
+  });
+
   describe('newDesignLanguage', () => {
     it('does not render a container with newDesignLanguage className by default', () => {
       const textField = mountWithApp(
