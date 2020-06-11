ad52d38b6fd069adf15c15b1136440067754e576
Kyle Durand <kyle.durand@shopify.com>
2020-06-11 10:11:53 -0400

    Add spacing to empty state within content

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -16,12 +16,11 @@ export interface EmptyStateProps {
   /** The empty state heading */
   heading?: string;
   /** The path to the image to display */
+  /** The image should have ~40px of white space above when empty state is used within a card, modal, or navigation component */
   image: string;
   /** The path to the image to display on large screens */
   largeImage?: string;
-  /**
-   * Whether or not to limit the image to the size of its container on large screens.
-   */
+  /** Whether or not to limit the image to the size of its container on large screens */
   imageContained?: boolean;
   /** Whether or not the content should span the full width of its container  */
   fullWidth?: boolean;
