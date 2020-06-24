734e48b3bda3bb293aeb52cb72ba099a98f637b8
Ben Black <ben.black@shopify.com>
2020-06-24 09:52:19 -0700

    Use Quilt Address to display JA address

--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import AddressFormatter from '@shopify/address';
 
 import {ThemeProvider} from '../ThemeProvider';
 import {Card} from '../Card';
@@ -8,42 +9,68 @@ import {Stack} from '../Stack';
 import {Button} from '../Button';
 import {TextStyle} from '../TextStyle';
 
-export interface OrderDetailsProps {}
+export function OrderDetails() {
+  const [address, setAddress] = useState('');
 
-export class OrderDetails extends React.PureComponent<OrderDetailsProps> {
-  render() {
-    return (
-      <ThemeProvider
-        theme={{
-          colorScheme: 'dark',
-          colors: {
-            surface: '#020c1d',
-            secondary: '#103560',
-            interactive: '#009973',
-          },
-        }}
-      >
-        <Card title="Order details">
-          <Card.Section title="Unfulfilled">
-            <Stack alignment="center" distribution="equalSpacing">
-              <Stack alignment="center">
-                <Thumbnail
-                  source="https://burst.shopifycdn.com/photos/black-leather-choker-necklace_373x@2x.jpg"
-                  alt="Black choker necklace"
-                />
-                <TextStyle variation="positive">Enamel pin</TextStyle>
-              </Stack>
-              <span>$9.99</span>
+  useEffect(() => {
+    const address = {
+      company: 'Shopify',
+      firstName: '恵子',
+      lastName: '田中',
+      address1: '八重洲1-5-3',
+      address2: '',
+      city: '目黒区',
+      province: 'JP-13',
+      zip: '100-8994',
+      country: 'JP',
+      phone: '',
+    };
+
+    const addressFormatter = new AddressFormatter('ja');
+
+    (async () => {
+      try {
+        const formattedAddress = await addressFormatter.format(address);
+        setAddress(formattedAddress.join(','));
+      } catch {
+        // Do nothing for now
+      }
+    })();
+  });
+
+  return (
+    <ThemeProvider
+      theme={{
+        colorScheme: 'dark',
+        colors: {
+          surface: '#020c1d',
+          secondary: '#103560',
+          interactive: '#009973',
+        },
+      }}
+    >
+      <Card title="Order details">
+        <Card.Section title="Unfulfilled">
+          <Stack alignment="center" distribution="equalSpacing">
+            <Stack alignment="center">
+              <Thumbnail
+                source="https://burst.shopifycdn.com/photos/black-leather-choker-necklace_373x@2x.jpg"
+                alt="Black choker necklace"
+              />
+              <TextStyle variation="positive">Enamel pin</TextStyle>
             </Stack>
-          </Card.Section>
-          <Card.Section>
-            <Stack distribution="equalSpacing" alignment="center">
-              <TextContainer>Buy postage and ship 2 items</TextContainer>
-              <Button>Continue</Button>
-            </Stack>
-          </Card.Section>
-        </Card>
-      </ThemeProvider>
-    );
-  }
+            <span>$9.99</span>
+          </Stack>
+        </Card.Section>
+        <Card.Section>
+          <Stack distribution="equalSpacing" alignment="center">
+            <TextContainer>
+              Buy postage and ship 2 items to {address}
+            </TextContainer>
+            <Button>Continue</Button>
+          </Stack>
+        </Card.Section>
+      </Card>
+    </ThemeProvider>
+  );
 }
