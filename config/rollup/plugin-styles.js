6fa82aeb10874e78bc45bb71980bd1ad4e8fea61
Ben Scott <ben.scott@shopify.com>
2020-07-02 12:09:13 -0700

    Back to .es.js as .mjs messes with transitive imports

--- config/rollup/plugin-styles.js
+++ config/rollup/plugin-styles.js
@@ -67,7 +67,7 @@ export function styles({
   function generateBundleStandalone(rollup, generateOptions, bundle) {
     // generateBundle gets called once per call to bundle.write(). We call
     // that twice - one for the cjs build (index.js), one for the esm build
-    // (index.mjs). We only want to do perform this logic once though
+    // (index.es.js). We only want to do perform this logic once though
     if (!generateOptions.file.endsWith('/index.js')) {
       return;
     }
