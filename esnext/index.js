800af2f4052c6bb46b1a475c63cccde44b96eded
Ben Scott <ben.scott@shopify.com>
2020-07-06 15:05:26 -0700

    Better esnext comment

--- esnext/index.js
+++ esnext/index.js
@@ -1,6 +1,7 @@
 // Our esnext build lives in dist/esnext.
-// However currently sewing-kit expects the esnext build to live in the esnext
-// folder and there's no way to tell it otherwise currently.
-// So have this little redirect file for the sake of keeping SK happy until we
-// have a better mechanism defining the esnext entrypoint
+// However currently sewing-kit expects the esnext's entrypoint to be this file
+// thanks to a hardcoded alias:
+// https://github.com/Shopify/sewing-kit/blob/9911ec817ebd21384f549187a481f6b624cf7182/packages/sewing-kit/src/tools/webpack/config/resolve.ts#L45-L50.
+// Once that alias is removed then sewing-kit will use the `sewing-kit:esnext`
+// key in our package.json as the entrypoint and this file can be removed.
 export * from '../dist/esnext/index.ts.esnext';
