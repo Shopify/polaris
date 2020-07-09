58af9313932cb4373ea893e69e251b92466a5ab5
Ben Scott <ben.scott@shopify.com>
2020-07-08 19:01:55 -0700

    Inline nodeContainsDescendant usage
    See:
    - https://github.com/Shopify/javascript-utilities/blob/master/src/dom.ts

--- src/components/Popover/components/PopoverOverlay/PopoverOverlay.tsx
+++ src/components/Popover/components/PopoverOverlay/PopoverOverlay.tsx
@@ -1,5 +1,4 @@
 import React, {createRef} from 'react';
-import {nodeContainsDescendant} from '@shopify/javascript-utilities/dom';
 import {durationBase} from '@shopify/polaris-tokens';
 
 import {classNames} from '../../../../utilities/css';
@@ -286,4 +285,24 @@ function renderPopoverContent(
     return childrenArray;
   }
   return wrapWithComponent(childrenArray, Pane, props);
+}
+
+export function nodeContainsDescendant(
+  rootNode: HTMLElement,
+  descendant: HTMLElement,
+): boolean {
+  if (rootNode === descendant) {
+    return true;
+  }
+
+  let parent = descendant.parentNode;
+
+  while (parent != null) {
+    if (parent === rootNode) {
+      return true;
+    }
+    parent = parent.parentNode;
+  }
+
+  return false;
 }
