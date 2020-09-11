cccadf41b815c2d046c0afc61fcbfe41d5e050dd
Kyle Durand <kyle.durand@shopify.com>
2020-09-11 10:31:21 -0400

    Update depressed button styles

--- src/utilities/theme/tokens.ts
+++ src/utilities/theme/tokens.ts
@@ -13,7 +13,7 @@ export const Tokens = {
   topBarShadow: '0 2px 2px -1px var(--p-shadow-from-direct-light)',
   buttonDropShadow: '0 1px 0 rgba(0, 0, 0, 0.05)',
   buttonInnerShadow: 'inset 0 -1px 0 rgba(0, 0, 0, 0.2)',
-  buttonPressedInnerShadow: 'inset 0 1px 0 rgba(0, 0, 0, 0.05)',
+  buttonPressedInnerShadow: 'inset 0 1px 0 rgba(0, 0, 0, 0.15)',
 
   // Overrides
   overrideNone: 'none',
