b6944dfbadfcc2be21be69d6f09a985b83b3b373
Alex Page <alex@alexpage.com.au>
2020-09-22 20:48:47 -0400

    Add ndl to action list

--- src/components/ActionList/components/Section/Section.tsx
+++ src/components/ActionList/components/Section/Section.tsx
@@ -6,6 +6,8 @@ import type {
   ActionListSection,
 } from '../../../../types';
 import styles from '../../ActionList.scss';
+import {useFeatures} from '../../../../utilities/features';
+import {classNames} from '../../../../utilities/css';
 
 export interface SectionProps {
   /** Section of action items */
@@ -51,6 +53,12 @@ export function Section({
 
   const className = section.title ? undefined : styles['Section-withoutTitle'];
 
+  const {newDesignLanguage} = useFeatures();
+  const actionsClassName = classNames(
+    styles.Actions,
+    newDesignLanguage && styles.newDesignLanguage,
+  );
+
   const titleMarkup = section.title ? (
     <p className={styles.Title}>{section.title}</p>
   ) : null;
@@ -60,7 +68,7 @@ export function Section({
   const sectionMarkup = (
     <div className={className}>
       {titleMarkup}
-      <ul className={styles.Actions} role={sectionRole}>
+      <ul className={actionsClassName} role={sectionRole}>
         {actionMarkup}
       </ul>
     </div>
