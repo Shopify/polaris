459fce35d65f8d798c32aa2f79f1d26e1e49d366
Avin Chadee <achadee15@gmail.com>
2020-10-09 10:15:03 +1100

    add type to types

--- src/utilities/theme/types.ts
+++ src/utilities/theme/types.ts
@@ -3,6 +3,10 @@ import type {Config} from '@shopify/polaris-tokens/dist-modern/types';
 export interface ThemeLogo {
   /** Provides a path for a logo used on a dark background */
   topBarSource?: string;
+
+  /** Provides a path for a logo used on the mobile background */
+  navigationSource?: string;
+
   /** Provides a path for a logo used on a light background */
   contextualSaveBarSource?: string;
   /** Destination the merchant will navigate to when clicking the logo */
