ef6e0207e6dffe91f5661ead32ca24a5524a5a2b
Kyle Durand <kyle.durand@shopify.com>
2020-10-09 13:22:25 -0400

    fix action menu lint / test

--- src/components/ActionMenu/components/MenuGroup/MenuGroup.tsx
+++ src/components/ActionMenu/components/MenuGroup/MenuGroup.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
+import React, {useCallback} from 'react';
 
 import type {MenuGroupDescriptor} from '../../../../types';
 import {useFeatures} from '../../../../utilities/features';
