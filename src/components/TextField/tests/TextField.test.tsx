b4adaec510341fc84d67f17300cb536384efec1f
Elio Hasrouni <elio.hasrouni@shopify.com>
2020-10-20 08:34:53 -0400

    Fixed a linting issue, added onChange to test

--- src/components/TextField/tests/TextField.test.tsx
+++ src/components/TextField/tests/TextField.test.tsx
@@ -705,12 +705,14 @@ describe('<TextField />', () => {
       });
 
       it('removes spinner buttons when type is number and step is 0', () => {
+        const spy = jest.fn();
         const element = mountWithAppProvider(
           <TextField
             id="MyNumberField"
             label="NumberField"
             type="number"
             step={0}
+            onChange={spy}
           />,
         );
         expect(element.find(Spinner)).toHaveLength(0);
