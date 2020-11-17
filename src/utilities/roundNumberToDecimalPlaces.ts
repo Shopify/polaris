87221e8f2493c54729ad6852d80a379163e60e01
Mark Dimitry <mark.dimitry@shopify.com>
2020-11-17 09:46:58 -0500

    remove trivially inferred type annotations

--- src/utilities/roundNumberToDecimalPlaces.ts
+++ src/utilities/roundNumberToDecimalPlaces.ts
@@ -1,9 +1,7 @@
 // precise rounding https://www.jacklmoore.com/notes/rounding-in-javascript/
 export function roundNumberToDecimalPlaces(value: number, decimals: number) {
-  const exponent: number = Number(`${value}e${decimals}`);
+  const exponent = Number(`${value}e${decimals}`);
   const roundedExponent = Math.round(exponent);
-  const numberWithDecimalPlaces: number = Number(
-    `${roundedExponent}e-${decimals}`,
-  );
+  const numberWithDecimalPlaces = Number(`${roundedExponent}e-${decimals}`);
   return numberWithDecimalPlaces;
 }
