0a2a17560862768cdd7656f7f74f52790359829d
Mark Dimitry <mark.dimitry@shopify.com>
2020-11-17 08:57:59 -0500

    push eslint ignore

--- src/utilities/roundNumberToDecimalPlaces.ts
+++ src/utilities/roundNumberToDecimalPlaces.ts
@@ -1,5 +1,6 @@
 // precise rounding https://www.jacklmoore.com/notes/rounding-in-javascript/
 export function roundNumberToDecimalPlaces(value: number, decimals: number) {
   // @ts-ignore - string concatenation represents exponential number notation
+  // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
   return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
