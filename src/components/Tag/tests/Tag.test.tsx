6f21167b720a0b30246febcbe51adaebb7bf429c
Alex Page <alex@alexpage.com.au>
2020-12-22 07:49:05 -0800

    Update Tag.test.tsx

--- src/components/Tag/tests/Tag.test.tsx
+++ src/components/Tag/tests/Tag.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 // eslint-disable-next-line no-restricted-imports
 import {mountWithAppProvider} from 'test-utilities/legacy';
-import {mountWithApp} from 'test-utilities';
 
 import {Tag} from '../Tag';
 
