e710f1fe061dd27b60b4460f6dc56869307e381a
Jackie <jackie.ellis@shopify.com>
2021-01-08 10:25:57 -0700

    [Tabs] Fix aria-controls related a11y error (#3777)
    * Update the Tabs component to prevent an a11y error

--- UNRELEASED.md
+++ UNRELEASED.md
@@ -2,6 +2,8 @@
 
 Use [the changelog guidelines](https://git.io/polaris-changelog-guidelines) to format new entries. 💜
 
+- Updated `Tabs` to prevent `aria-valid-attr-value` a11y error ([#3777](https://github.com/Shopify/polaris-react/pull/3777))
+
 ### Breaking changes
 
 ### Enhancements
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -246,8 +246,9 @@ class TabsInner extends PureComponent<CombinedProps, State> {
 
   // eslint-disable-next-line @shopify/react-no-multiple-render-methods
   private renderTabMarkup = (tab: TabDescriptor, index: number) => {
-    const {selected} = this.props;
+    const {selected, children} = this.props;
     const {tabToFocus} = this.state;
+    const tabPanelID = tab.panelID || `${tab.id}-panel`;
 
     return (
       <Tab
@@ -257,7 +258,7 @@ class TabsInner extends PureComponent<CombinedProps, State> {
         focused={index === tabToFocus}
         selected={index === selected}
         onClick={this.handleTabClick}
-        panelID={tab.panelID || `${tab.id}-panel`}
+        panelID={children ? tabPanelID : undefined}
         accessibilityLabel={tab.accessibilityLabel}
         url={tab.url}
       >
