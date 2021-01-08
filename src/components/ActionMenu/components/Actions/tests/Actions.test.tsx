456c76003025ac0f138f9a67908d4fdb1d6e007c
Kyle Durand <kyledurand@users.noreply.github.com>
2021-01-08 14:51:35 -0500

    [Page Actions] Re-render without measuring when actions / groups change (#3641)
    * Re-render without measuring when hasMeasured and actions lengths are equal
    
    * Add some tests
    
    * Move re-render to new callback
    
    * Allow two measures and add page tests
    
    * update changelog

--- src/components/ActionMenu/components/Actions/tests/Actions.test.tsx
+++ src/components/ActionMenu/components/Actions/tests/Actions.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {ActionMenuProps, ActionMenu} from 'index';
 // eslint-disable-next-line no-restricted-imports
 import {mountWithAppProvider} from 'test-utilities/legacy';
+import {mountWithApp} from 'test-utilities';
 
-import {MenuAction, RollupActions} from '../..';
+import {
+  Actions,
+  MenuAction,
+  MenuGroup,
+  RollupActions,
+  SecondaryAction,
+} from '../..';
 
 describe('<Actions />', () => {
   const mockProps: ActionMenuProps = {
@@ -34,7 +41,7 @@ describe('<Actions />', () => {
     );
   });
 
-  it('renders actions in their initial order when no indexes are set', () => {
+  it('renders actions in their initial order', () => {
     const actionsBeforeOverriddenOrder: ActionMenuProps['actions'] = [
       {content: 'mock content 0'},
       {content: 'mock content 1'},
@@ -53,6 +60,127 @@ describe('<Actions />', () => {
 
     wrapper.find(MenuAction).forEach((action, index) => {
       expect(action.props()).toMatchObject(expectedOrderAfterOverride[index]);
+    });
+  });
+
+  describe('Actions', () => {
+    it('renders SecondaryActions when the new design language is true', () => {
+      const actionsBeforeOverriddenOrder: ActionMenuProps['actions'] = [
+        {content: 'mock content 0'},
+        {content: 'mock content 1'},
+        {content: 'mock content 2'},
+      ];
+
+      const wrapper = mountWithApp(
+        <ActionMenu actions={actionsBeforeOverriddenOrder} />,
+        {features: {newDesignLanguage: true}},
+      );
+
+      expect(wrapper.findAll(SecondaryAction)).toHaveLength(3);
+    });
+
+    it('renders a MenuGroup when the new design language is true', () => {
+      const wrapper = mountWithApp(
+        <ActionMenu groups={[{title: 'group', actions: []}]} />,
+        {
+          features: {newDesignLanguage: true},
+        },
+      );
+
+      expect(wrapper.findAll(MenuGroup)).toHaveLength(1);
+    });
+
+    it('updates actions when they change', () => {
+      function ActionsWithToggle() {
+        const initialActions: ActionMenuProps['actions'] = [
+          {content: 'initial'},
+        ];
+
+        const [actions, setActions] = useState(initialActions);
+        const handleActivatorClick = useCallback(
+          () => setActions([{content: 'updated'}]),
+          [],
+        );
+
+        return (
+          <>
+            <button onClick={handleActivatorClick}>Activator</button>
+            <Actions actions={actions} />
+          </>
+        );
+      }
+
+      const wrapper = mountWithApp(<ActionsWithToggle />, {
+        features: {newDesignLanguage: true},
+      });
+
+      wrapper.find('button')!.trigger('onClick');
+      expect(wrapper).toContainReactComponent(SecondaryAction, {
+        children: 'updated',
+      });
+    });
+
+    it('updates groups when they change', () => {
+      function ActionsWithToggle() {
+        const initialGroups: ActionMenuProps['groups'] = [
+          {title: 'initial', actions: [{content: 'initial'}]},
+        ];
+
+        const [groups, setGroups] = useState(initialGroups);
+        const handleActivatorClick = useCallback(
+          () =>
+            setGroups([{title: 'updated', actions: [{content: 'updated'}]}]),
+          [],
+        );
+
+        return (
+          <>
+            <button onClick={handleActivatorClick}>Activator</button>
+            <Actions groups={groups} />
+          </>
+        );
+      }
+
+      const wrapper = mountWithApp(<ActionsWithToggle />, {
+        features: {newDesignLanguage: true},
+      });
+
+      wrapper.find('button')!.trigger('onClick');
+      expect(wrapper).toContainReactComponent(MenuGroup, {
+        title: 'updated',
+        actions: [{content: 'updated'}],
+      });
+    });
+
+    it('updates actions when their lengths change', () => {
+      function ActionsWithToggle() {
+        const initialActions: ActionMenuProps['actions'] = [
+          {content: 'initial'},
+        ];
+
+        const [actions, setActions] = useState(initialActions);
+        const handleActivatorClick = useCallback(
+          () => setActions([{content: 'updated 0'}, {content: 'updated 1'}]),
+          [],
+        );
+
+        return (
+          <>
+            <button onClick={handleActivatorClick}>Activator</button>
+            <Actions actions={actions} />
+          </>
+        );
+      }
+
+      const wrapper = mountWithApp(<ActionsWithToggle />, {
+        features: {newDesignLanguage: true},
+      });
+
+      expect(wrapper).toContainReactComponentTimes(SecondaryAction, 1);
+
+      wrapper.find('button')!.trigger('onClick');
+
+      expect(wrapper).toContainReactComponentTimes(SecondaryAction, 2);
     });
   });
 });
