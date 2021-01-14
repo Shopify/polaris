8d0a8b056879f0b7ce9d50829608a65ef249af8d
Kyle Durand <kyledurand@users.noreply.github.com>
2021-01-14 17:26:26 -0500

    [Breadcrumbs] Remove new DL (#3836)
    Co-authored-by: Alex Page <alex@alexpage.com.au>

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,12 +1,10 @@
-import React, {PureComponent} from 'react';
-import {ChevronLeftMinor, ArrowLeftMinor} from '@shopify/polaris-icons';
+import React from 'react';
+import {ArrowLeftMinor} from '@shopify/polaris-icons';
 
 import {Icon} from '../Icon';
 import {UnstyledLink} from '../UnstyledLink';
 import type {CallbackAction, LinkAction} from '../../types';
 import {handleMouseUpByBlurring} from '../../utilities/focus';
-import {FeaturesContext} from '../../utilities/features';
-import {classNames} from '../../utilities/css';
 import {VisuallyHidden} from '../VisuallyHidden';
 
 import styles from './Breadcrumbs.scss';
@@ -16,64 +14,46 @@ export interface BreadcrumbsProps {
   breadcrumbs: (CallbackAction | LinkAction)[];
 }
 
-export class Breadcrumbs extends PureComponent<BreadcrumbsProps, never> {
-  static contextType = FeaturesContext;
-  context!: React.ContextType<typeof FeaturesContext>;
-
-  render() {
-    const {newDesignLanguage} = this.context || {};
-    const {breadcrumbs} = this.props;
-    const breadcrumb = breadcrumbs[breadcrumbs.length - 1];
-    if (breadcrumb == null) {
-      return null;
-    }
-
-    const {content} = breadcrumb;
-
-    const contentMarkup = (
-      <span className={styles.ContentWrapper}>
-        <span className={styles.Icon}>
-          <Icon
-            source={newDesignLanguage ? ArrowLeftMinor : ChevronLeftMinor}
-          />
-        </span>
-        {newDesignLanguage ? (
-          <VisuallyHidden>{content}</VisuallyHidden>
-        ) : (
-          <span className={styles.Content}>{content}</span>
-        )}
-      </span>
-    );
-
-    const breadcrumbClassNames = classNames(
-      styles.Breadcrumb,
-      newDesignLanguage && styles.newDesignLanguage,
-    );
-
-    const breadcrumbMarkup =
-      'url' in breadcrumb ? (
-        <UnstyledLink
-          key={content}
-          url={breadcrumb.url}
-          className={breadcrumbClassNames}
-          onMouseUp={handleMouseUpByBlurring}
-          aria-label={breadcrumb.accessibilityLabel}
-        >
-          {contentMarkup}
-        </UnstyledLink>
-      ) : (
-        <button
-          key={content}
-          className={breadcrumbClassNames}
-          onClick={breadcrumb.onAction}
-          onMouseUp={handleMouseUpByBlurring}
-          type="button"
-          aria-label={breadcrumb.accessibilityLabel}
-        >
-          {contentMarkup}
-        </button>
-      );
-
-    return <nav role="navigation">{breadcrumbMarkup}</nav>;
+export function Breadcrumbs({breadcrumbs}: BreadcrumbsProps) {
+  const breadcrumb = breadcrumbs[breadcrumbs.length - 1];
+  if (breadcrumb == null) {
+    return null;
   }
+
+  const {content} = breadcrumb;
+
+  const contentMarkup = (
+    <span className={styles.ContentWrapper}>
+      <span className={styles.Icon}>
+        <Icon source={ArrowLeftMinor} />
+      </span>
+      <VisuallyHidden>{content}</VisuallyHidden>
+    </span>
+  );
+
+  const breadcrumbMarkup =
+    'url' in breadcrumb ? (
+      <UnstyledLink
+        key={content}
+        url={breadcrumb.url}
+        className={styles.Breadcrumb}
+        onMouseUp={handleMouseUpByBlurring}
+        aria-label={breadcrumb.accessibilityLabel}
+      >
+        {contentMarkup}
+      </UnstyledLink>
+    ) : (
+      <button
+        key={content}
+        className={styles.Breadcrumb}
+        onClick={breadcrumb.onAction}
+        onMouseUp={handleMouseUpByBlurring}
+        type="button"
+        aria-label={breadcrumb.accessibilityLabel}
+      >
+        {contentMarkup}
+      </button>
+    );
+
+  return <nav role="navigation">{breadcrumbMarkup}</nav>;
 }
