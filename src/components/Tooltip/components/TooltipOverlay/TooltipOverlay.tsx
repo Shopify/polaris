27b27b58eded1a15f028b9068fed7e7be3a469d7
Alex Page <alex@alexpage.com.au>
2021-01-19 10:40:11 -0800

    Move sass file

--- src/components/Tooltip/components/TooltipOverlay/TooltipOverlay.tsx
+++ src/components/Tooltip/components/TooltipOverlay/TooltipOverlay.tsx
@@ -7,7 +7,8 @@ import {
   PositionedOverlay,
 } from '../../../PositionedOverlay';
 import {useI18n} from '../../../../utilities/i18n';
-import styles from '../../Tooltip.scss';
+
+import styles from './TooltipOverlay.scss';
 
 export interface TooltipOverlayProps {
   id: string;
@@ -48,7 +49,7 @@ export function TooltipOverlay({
     const {measuring, desiredHeight, positioning} = overlayDetails;
 
     const containerClassName = classNames(
-      styles.Tooltip,
+      styles.TooltipOverlay,
       measuring && styles.measuring,
       positioning === 'above' && styles.positionedAbove,
     );
