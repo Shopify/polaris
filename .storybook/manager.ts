dbe996f272b4c0d980fecbfb90c071328be64254
Kyle Durand <kyle.durand@shopify.com>
2021-01-27 15:06:53 -0500

    Matchy match the admin

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -2,6 +2,13 @@ import {addons} from '@storybook/addons';
 import {create} from '@storybook/theming';
 import tokens from '@shopify/polaris-tokens';
 
+const colors = {
+  primary: '#008060',
+  secondary: '#6d7175',
+  background: '#f6f6f7',
+  border: '#202123',
+};
+
 addons.setConfig({
   panelPosition: 'right',
   theme: create({
@@ -10,23 +17,26 @@ addons.setConfig({
     brandUrl: '/',
     brandImage:
       'https://cdn.shopify.com/shopifycloud/brochure/assets/brand-assets/shopify-logo-primary-logo-456baa801ee66a0a435671082365958316831c9960c480451dd0330bcdae304f.svg',
+    appBg: colors.background,
+    appContentBg: colors.background,
     appBorderRadius: 4,
-    colorPrimary: '#009671',
-    colorSecondary: '#36a3ff',
+
+    colorPrimary: colors.primary,
+    colorSecondary: colors.secondary,
 
     // // Typography
-    textColor: tokens.colorWhite,
-    textInverseColor: tokens.colorBlack,
+    textColor: tokens.colorInkLight,
+    textInverseColor: tokens.colorWhite,
     fontBase: 'ShopifySans, Helvetica, Arial, Lucida Grande, sans-serif',
 
-    // // Toolbar default and active colors
-    barTextColor: tokens.colorWhite,
-    barSelectedColor: '#009671',
-    barBg: '#202123',
+    // Toolbar default and active colors
+    barTextColor: tokens.colorInkLight,
+    barSelectedColor: colors.primary,
+    barBg: colors.background,
 
-    // // Form colors
-    inputBg: '#202123',
-    inputBorder: '#202123',
+    // // Form colors'
+    inputBg: colors.background,
+    inputBorder: colors.background,
     inputTextColor: tokens.colorWhite,
     inputBorderRadius: 4,
   }),
