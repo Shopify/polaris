508b554b40c7f9e3ccb8adc0c717026f3350bae0
Emma Boardman <emma.boardman@shopify.com>
2021-05-06 11:41:43 +0100

    bump to react-hooks 1.13.0, improve keypress perf

--- src/components/KeypressListener/KeypressListener.tsx
+++ src/components/KeypressListener/KeypressListener.tsx
@@ -1,4 +1,5 @@
-import {useEffect} from 'react';
+import {useCallback, useEffect, useRef} from 'react';
+import {useIsomorphicLayoutEffect} from '@shopify/react-hooks';
 
 import type {Key} from '../../types';
 
@@ -15,18 +16,27 @@ export function KeypressListener({
   handler,
   keyEvent = 'keyup',
 }: KeypressListenerProps) {
-  const handleKeyEvent = (event: KeyboardEvent) => {
-    if (event.keyCode === keyCode) {
-      handler(event);
-    }
-  };
+  const handlerRef = useRef(handler);
+
+  const handleKeyEvent = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.keyCode === keyCode) {
+        handlerRef.current(event);
+      }
+    },
+    [handler, keyCode],
+  );
+
+  useIsomorphicLayoutEffect(() => {
+    handlerRef.current = handler;
+  }, [handler]);
 
   useEffect(() => {
     document.addEventListener(keyEvent, handleKeyEvent);
     return () => {
       document.removeEventListener(keyEvent, handleKeyEvent);
     };
-  });
+  }, [keyEvent, handleKeyEvent]);
 
   return null;
 }
