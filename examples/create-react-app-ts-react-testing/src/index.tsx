d2d25def2b73b28b33c79b079d5df2ba40734bdd
Alex Page <alex@alexpage.com.au>
2021-05-17 18:22:44 -0700

    Remove serviceworker and bump dependencies

--- examples/create-react-app-ts-react-testing/src/index.tsx
+++ examples/create-react-app-ts-react-testing/src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {AppProvider} from '@shopify/polaris';
 import enTranslations from '@shopify/polaris/locales/en.json';
+
 import '@shopify/polaris/dist/styles.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {App} from './App';
 
 ReactDOM.render(
   <AppProvider i18n={enTranslations}>
@@ -12,8 +12,3 @@ ReactDOM.render(
   </AppProvider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
