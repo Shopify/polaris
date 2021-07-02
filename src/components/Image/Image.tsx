1c072da2ebcba57dce0be3ea29d78d2343e7a744
Jinesh Shah <jineshshah36@gmail.com>
2021-07-01 20:46:36 -0400

    6.6.0-fork.2

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -12,6 +12,7 @@ export interface ImageProps extends React.HTMLProps<HTMLImageElement> {
   source: string;
   crossOrigin?: CrossOrigin;
   sourceSet?: SourceSet[];
+  loading?: 'lazy' | 'eager';
   onLoad?(): void;
   onError?(): void;
 }
--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ReactNode, useState} from 'react';
 
 import {classNames, variationName} from '../../utilities/css';
 import {Image} from '../Image';
@@ -16,22 +16,45 @@ export interface ThumbnailProps {
   size?: Size;
   /** URL for the image */
   source: string | React.SFC<React.SVGProps<SVGSVGElement>>;
+  /** Fallback for the image */
+  fallback?: React.SFC<React.SVGProps<SVGSVGElement>>;
   /** Alt text for the thumbnail image */
   alt: string;
+  /** Loading */
+  loading?: 'lazy' | 'eager';
 }
 
-export function Thumbnail({source, alt, size = 'medium'}: ThumbnailProps) {
+export function Thumbnail({
+  source,
+  fallback,
+  alt,
+  size = 'medium',
+  loading,
+}: ThumbnailProps) {
+  const [errored, setErrored] = useState(false);
+
   const className = classNames(
     styles.Thumbnail,
     size && styles[variationName('size', size)],
   );
 
-  const content =
-    typeof source === 'string' ? (
-      <Image alt={alt} source={source} />
-    ) : (
-      <Icon accessibilityLabel={alt} source={source} />
-    );
+  let content: ReactNode;
+  if (typeof source === 'string') {
+    if (errored && fallback) {
+      content = <Icon accessibilityLabel={alt} source={fallback} />;
+    } else {
+      content = (
+        <Image
+          alt={alt}
+          source={source}
+          loading={loading}
+          onError={() => setErrored(true)}
+        />
+      );
+    }
+  } else {
+    content = <Icon accessibilityLabel={alt} source={source} />;
+  }
 
   return <span className={className}>{content}</span>;
 }
