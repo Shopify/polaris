97bcd0b996e63cd7174a0df7e47b011ab5fd1daa
Richard Todd <richard.todd@shopify.com>
2021-07-12 17:12:34 -0400

    Test modernization- avatar, backdrop, badge, banner

--- src/components/Avatar/tests/Avatar.test.tsx
+++ src/components/Avatar/tests/Avatar.test.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
-// eslint-disable-next-line no-restricted-imports
-import {mountWithAppProvider} from 'test-utilities/legacy';
 import {mountWithApp} from 'test-utilities';
 import {Avatar, Image} from 'components';
 
 describe('<Avatar />', () => {
   describe('intials', () => {
-    it('renders intials if the image is not provided', () => {
-      const avatar = mountWithAppProvider(<Avatar initials="DL" />);
-      expect(avatar.find('span[role="img"] span svg')).toHaveLength(1);
+    it('renders initials if the image is not provided', () => {
+      const avatar = mountWithApp(<Avatar initials="DL" />);
+
+      expect(avatar).toContainReactComponent('span', {
+        className: 'Initials',
+      });
+      expect(avatar).toContainReactText('DL');
+      expect(avatar).not.toContainReactComponent(Image);
     });
   });
 
   describe('source', () => {
     it('renders an Image component with the Avatar source if one is provided', () => {
       const src = 'image/path/';
-      const avatar = mountWithAppProvider(<Avatar source={src} />);
-      const image = avatar.find(Image);
-      expect(image.prop('source')).toBe(src);
+      const avatar = mountWithApp(<Avatar source={src} />);
+      expect(avatar).toContainReactComponent(Image, {source: src});
     });
 
     it('safely updates', () => {
       const src = 'image/path/';
-      const avatar = mountWithAppProvider(<Avatar source={src} />);
+      const avatar = mountWithApp(<Avatar source={src} />);
       expect(() => {
         avatar.setProps({source: 'image/new/path'});
       }).not.toThrow();
@@ -31,14 +33,14 @@ describe('<Avatar />', () => {
 
   describe('customer', () => {
     it('renders an inline svg', () => {
-      const avatar = mountWithAppProvider(<Avatar customer />);
-      expect(avatar.find('svg').exists()).toBe(true);
+      const avatar = mountWithApp(<Avatar customer />);
+      expect(avatar).toContainReactComponentTimes('svg', 1);
     });
 
     it('does not render a customer Avatar if a source is provided', () => {
       const src = 'image/path/';
-      const avatar = mountWithAppProvider(<Avatar customer source={src} />);
-      expect(avatar.find('svg').exists()).toBe(false);
+      const avatar = mountWithApp(<Avatar customer source={src} />);
+      expect(avatar).not.toContainReactComponent('svg');
     });
   });
 
@@ -62,8 +64,8 @@ describe('<Avatar />', () => {
     });
 
     it('renders an inline svg if initials are blank', () => {
-      const avatar = mountWithAppProvider(<Avatar initials="" />);
-      expect(avatar.find('svg').exists()).toBe(true);
+      const avatar = mountWithApp(<Avatar initials="" />);
+      expect(avatar).toContainReactComponentTimes('svg', 1);
     });
   });
 
@@ -110,21 +112,19 @@ describe('<Avatar />', () => {
 
   describe('accessibilityLabel', () => {
     it('is passed to the aria-label', () => {
-      const avatar = mountWithAppProvider(
-        <Avatar accessibilityLabel="Hello World" />,
-      );
-      expect(avatar.find('span').first().prop('aria-label')).toBe(
-        'Hello World',
-      );
+      const avatar = mountWithApp(<Avatar accessibilityLabel="Hello World" />);
+      expect(avatar).toContainReactComponent('span', {
+        'aria-label': 'Hello World',
+      });
     });
   });
 
   describe('name', () => {
     it('is passed to the aria-label', () => {
-      const avatar = mountWithAppProvider(<Avatar name="Hello World" />);
-      expect(avatar.find('span').first().prop('aria-label')).toBe(
-        'Hello World',
-      );
+      const avatar = mountWithApp(<Avatar name="Hello World" />);
+      expect(avatar).toContainReactComponent('span', {
+        'aria-label': 'Hello World',
+      });
     });
   });
 });
