278f6eb8590f968fc46f4a8ba49a8dcd8b37d60b
Jinesh Shah <jineshshah36@gmail.com>
2021-07-13 11:22:15 -0400

    6.6.0-fork.6

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,10 @@ export {
 
 export {ScrollLockManagerContext as _SECRET_INTERNAL_SCROLL_LOCK_MANAGER_CONTEXT} from './utilities/scroll-lock-manager';
 export {WithinContentContext as _SECRET_INTERNAL_WITHIN_CONTENT_CONTEXT} from './utilities/within-content-context';
-export {useIndexResourceState} from './utilities/use-index-resource-state';
+export {
+  useIndexResourceState,
+  SelectionType,
+} from './utilities/use-index-resource-state';
 
 export {
   toCssCustomPropertySyntax as UNSTABLE_toCssCustomPropertySyntax,
