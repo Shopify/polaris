4b6baf48e6d3227ffbb9e59dd9dd4b29572861e4
Karen Pinzás Morrongiello <45268098+kpinzas-sh@users.noreply.github.com>
2021-07-19 22:31:17 -0400

    Add frame tests for components

--- UNRELEASED.md
+++ UNRELEASED.md
@@ -25,5 +25,6 @@ Use [the changelog guidelines](https://git.io/polaris-changelog-guidelines) to f
 - Modernized tests for AccountConnection, ActionList components([#4316](https://github.com/Shopify/polaris-react/pull/4316))
 - Modernized tests for ActionMenu and its subcomponents ([#4318](https://github.com/Shopify/polaris-react/pull/4318))
 - Modernized tests for Loading-List-Item-Label components([#4321](https://github.com/Shopify/polaris-react/pull/4321))
+- Modernizes test for DiscardConfirmationModal, ContextualSaveBar, Loading, Toast, ToastManager, Frame (from Frame components) ([#4324](https://github.com/Shopify/polaris-react/pull/4324))
 
 ### Deprecations
--- src/components/Frame/components/ContextualSaveBar/components/DiscardConfirmationModal/tests/DiscardConfirmationModal.test.tsx
+++ src/components/Frame/components/ContextualSaveBar/components/DiscardConfirmationModal/tests/DiscardConfirmationModal.test.tsx
@@ -1,47 +1,54 @@
 import React from 'react';
-// eslint-disable-next-line no-restricted-imports
-import {mountWithAppProvider, trigger} from 'test-utilities/legacy';
+import {mountWithApp} from 'test-utilities';
 import {Modal} from 'components';
 
 import {DiscardConfirmationModal} from '../DiscardConfirmationModal';
 
 describe('<DiscardConfirmationModal />', () => {
   it('passes its open prop value to the Modal', () => {
-    const discardConfirmationModalOpen = mountWithAppProvider(
+    const discardConfirmationModalOpen = mountWithApp(
       <DiscardConfirmationModal open onDiscard={noop} onCancel={noop} />,
     );
-    expect(discardConfirmationModalOpen.find(Modal).prop('open')).toBe(true);
+    expect(discardConfirmationModalOpen).toContainReactComponent(Modal, {
+      open: true,
+    });
 
-    const discardConfirmationModalClosed = mountWithAppProvider(
+    const discardConfirmationModalClosed = mountWithApp(
       <DiscardConfirmationModal
         open={false}
         onDiscard={noop}
         onCancel={noop}
       />,
     );
-    expect(discardConfirmationModalClosed.find(Modal).prop('open')).toBe(false);
+
+    expect(discardConfirmationModalClosed).toContainReactComponent(Modal, {
+      open: false,
+    });
   });
 
   it('calls onDiscard when primaryAction is triggered', () => {
     const spy = jest.fn();
-    const discardConfirmationModal = mountWithAppProvider(
+    const discardConfirmationModal = mountWithApp(
       <DiscardConfirmationModal open onDiscard={spy} onCancel={noop} />,
     );
 
-    trigger(discardConfirmationModal.find(Modal), 'primaryAction.onAction');
+    discardConfirmationModal
+      .find(Modal)
+      ?.triggerKeypath('primaryAction.onAction');
+
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('calls onCancel when secondaryAction is triggered', () => {
     const spy = jest.fn();
-    const discardConfirmationModal = mountWithAppProvider(
+    const discardConfirmationModal = mountWithApp(
       <DiscardConfirmationModal open onDiscard={noop} onCancel={spy} />,
     );
 
-    trigger(
-      discardConfirmationModal.find(Modal),
-      'secondaryActions.0.onAction',
-    );
+    discardConfirmationModal
+      .find(Modal)
+      ?.triggerKeypath('secondaryActions.0.onAction');
+
     expect(spy).toHaveBeenCalledTimes(1);
   });
 });
