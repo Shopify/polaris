decimal-inc/polaris-react

Adding multi sections support to Autocomplete component

src/components/Autocomplete/Autocomplete.tsx
@@ -1,19 +1,24 @@
 import React, {useMemo, useCallback} from 'react';
+import type {
+  ActionListItemDescriptor,
+  Descriptor,
+  OptionDescriptor,
+} from 'types';
 
-import type {ActionListItemDescriptor} from '../../types';
-import type {OptionDescriptor} from '../OptionList';
 import type {PopoverProps} from '../Popover';
+import {isSection} from '../../utilities/options';
 import {useI18n} from '../../utilities/i18n';
 import {ComboBox} from '../ComboBox';
 import {ListBox} from '../ListBox';
 
-import {MappedOption, MappedAction} from './components';
+import {MappedAction, MappedOption} from './components';
+import styles from './Autocomplete.scss';
 
 export interface AutocompleteProps {
   /** A unique identifier for the Autocomplete */
   id?: string;
   /** Collection of options to be listed */
-  options: OptionDescriptor[];
+  options: Descriptor[];
   /** The selected options */
   selected: string[];
   /** The text field component attached to the list of options */
@@ -63,9 +68,47 @@ export const Autocomplete: React.FunctionComponent<AutocompleteProps> & {
 
   const optionsMarkup = useMemo(() => {
     const conditionalOptions = loading && !willLoadMoreResults ? [] : options;
+
+    if (isSection(conditionalOptions)) {
+      const noOptionsAvailable = conditionalOptions.every(
+        ({options}) => options.length === 0,
+      );
+
+      if (noOptionsAvailable) {
+        return null;
+      }
+
+      const optionsMarkup = conditionalOptions.map(({options, title}) => {
+        if (options.length === 0) {
+          return null;
+        }
+
+        const optionMarkup = options.map((opt) => (
+          <MappedOption
+            {...opt}
+            key={opt.id || opt.value}
+            selected={selected.includes(opt.value)}
+            singleSelection={!allowMultiple}
+          />
+        ));
+
+        return (
+          <ListBox.Section
+            divider={false}
+            title={<ListBox.Header>{title}</ListBox.Header>}
+            key={title}
+          >
+            {optionMarkup}
+          </ListBox.Section>
+        );
+      });
+
+      return <div className={styles.SectionWrapper}>{optionsMarkup}</div>;
+    }
+
     const optionList =
       conditionalOptions.length > 0
-        ? conditionalOptions.map((option) => (
+        ? (conditionalOptions as OptionDescriptor[]).map((option) => (
             <MappedOption
               {...option}
               key={option.id || option.value}

src/types.ts
@@ -1,3 +1,29 @@
+import type {AvatarProps, IconProps, ThumbnailProps} from 'components';
+
+export interface OptionDescriptor {
+  /** Value of the option */
+  value: string;
+  /** Display label for the option */
+  label: React.ReactNode;
+  /** Whether the option is disabled or not */
+  disabled?: boolean;
+  /** Whether the option is active or not */
+  active?: boolean;
+  /** Unique identifier for the option */
+  id?: string;
+  /** Media to display to the left of the option content */
+  media?: React.ReactElement<IconProps | ThumbnailProps | AvatarProps>;
+}
+
+export interface SectionDescriptor {
+  /** Collection of options within the section */
+  options: OptionDescriptor[];
+  /** Section title */
+  title?: string;
+}
+
+export type Descriptor = OptionDescriptor | SectionDescriptor;
+
 export type IconSource =
   | React.SFC<React.SVGProps<SVGSVGElement>>
   | 'placeholder'

Shopify/polaris

src/components/Autocomplete/Autocomplete.tsx
@@ -1,19 +1,24 @@
 import React, {useMemo, useCallback} from 'react';
+import type {
+  ActionListItemDescriptor,
+  OptionDescriptor,
+  SectionDescriptor,
+} from 'types';
 
-import type {ActionListItemDescriptor} from '../../types';
-import type {OptionDescriptor} from '../OptionList';
 import type {PopoverProps} from '../Popover';
+import {isSection} from '../../utilities/options';
 import {useI18n} from '../../utilities/i18n';
 import {ComboBox} from '../ComboBox';
 import {ListBox} from '../ListBox';
 
-import {MappedOption, MappedAction} from './components';
+import {MappedAction, MappedOption} from './components';
+import styles from './Autocomplete.scss';
 
 export interface AutocompleteProps {
   /** A unique identifier for the Autocomplete */
   id?: string;
   /** Collection of options to be listed */
-  options: OptionDescriptor[];
+  options: SectionDescriptor[] | OptionDescriptor[];
   /** The selected options */
   selected: string[];
   /** The text field component attached to the list of options */
@@ -61,18 +66,56 @@ export const Autocomplete: React.FunctionComponent<AutocompleteProps> & {
 }: AutocompleteProps) {
   const i18n = useI18n();
 
+  const buildMappedOptionFromOption = useCallback(
+    (options: OptionDescriptor[]) => {
+      return options.map((option) => (
+        <MappedOption
+          {...option}
+          key={option.id || option.value}
+          selected={selected.includes(option.value)}
+          singleSelection={!allowMultiple}
+        />
+      ));
+    },
+    [selected, allowMultiple],
+  );
+
   const optionsMarkup = useMemo(() => {
     const conditionalOptions = loading && !willLoadMoreResults ? [] : options;
+
+    if (isSection(conditionalOptions)) {
+      const noOptionsAvailable = conditionalOptions.every(
+        ({options}) => options.length === 0,
+      );
+
+      if (noOptionsAvailable) {
+        return null;
+      }
+
+      const optionsMarkup = conditionalOptions.map(({options, title}) => {
+        if (options.length === 0) {
+          return null;
+        }
+
+        const optionMarkup = buildMappedOptionFromOption(options);
+
+        return (
+          <ListBox.Section
+            divider={false}
+            title={<ListBox.Header>{title}</ListBox.Header>}
+            key={title}
+          >
+            {optionMarkup}
+          </ListBox.Section>
+        );
+      });
+
+      return <div className={styles.SectionWrapper}>{optionsMarkup}</div>;
+    }
+
     const optionList =
       conditionalOptions.length > 0
-        ? conditionalOptions.map((option) => (
-            <MappedOption
-              {...option}
-              key={option.id || option.value}
-              selected={selected.includes(option.value)}
-              singleSelection={!allowMultiple}
-            />
-          ))
+        ? buildMappedOptionFromOption(conditionalOptions)
         : null;
 
     if (listTitle) {
@@ -92,8 +135,7 @@ export const Autocomplete: React.FunctionComponent<AutocompleteProps> & {
     loading,
     options,
     willLoadMoreResults,
-    allowMultiple,
-    selected,
+    buildMappedOptionFromOption,
   ]);
 
   const loadingMarkup = loading ? (

src/types.ts
@@ -1,3 +1,29 @@
+import type {AvatarProps, IconProps, ThumbnailProps} from 'components';
+
+export interface OptionDescriptor {
+  /** Value of the option */
+  value: string;
+  /** Display label for the option */
+  label: React.ReactNode;
+  /** Whether the option is disabled or not */
+  disabled?: boolean;
+  /** Whether the option is active or not */
+  active?: boolean;
+  /** Unique identifier for the option */
+  id?: string;
+  /** Media to display to the left of the option content */
+  media?: React.ReactElement<IconProps | ThumbnailProps | AvatarProps>;
+}
+
+export interface SectionDescriptor {
+  /** Collection of options within the section */
+  options: OptionDescriptor[];
+  /** Section title */
+  title?: string;
+}
+
+export type Descriptor = SectionDescriptor | OptionDescriptor;
+
 export type IconSource =
   | React.SFC<React.SVGProps<SVGSVGElement>>
   | 'placeholder'