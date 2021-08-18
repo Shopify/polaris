3f019153abc15eafd07c4e42440174dafd8894be
Ben Scott <ben.scott@shopify.com>
2021-08-18 16:07:47 -0700

    more poking at ignores, only jest transform to datauri

--- config/jest-transform-image.js
+++ config/jest-transform-image.js
@@ -1,16 +1,8 @@
 const {basename} = require('path');
 
-const ICON_REGEX = /icons\/.*\.svg$/;
-
 module.exports = {
   process(src, filename) {
-    if (ICON_REGEX.test(filename)) {
-      return `const React = require('react');
-      module.exports = () => React.createElement("svg", {"data-src": ${JSON.stringify(
-        basename(filename),
-      )}});`;
-    }
-
-    return `module.exports = ${JSON.stringify(basename(filename))};`;
+    const stringifedBasename = JSON.stringify(basename(filename));
+    return `module.exports = ${stringifedBasename};`;
   },
 };
