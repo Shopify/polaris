e54a81ee02892d0466e6d54572abbf7b8d8effd9
Ben Scott <227292+BPScott@users.noreply.github.com>
2021-08-19 14:33:05 -0700

    Migrate to use sewing-kit-next for lint and test runs (#4402)
    Under the hood, we've updated from jest 25 to 27.
    We also update from babel 7.12 to 7.15, and @shopify/babel-plugin now
    uses the latest version.
    
    Currently skn does not provide out-the-box config jest transformers
    for scss and image files, so create them ourselves for now.

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,3 @@
-module.exports = function (api) {
-  // When building (using rollup) or running storybook (using babel-loader) we
-  // want to compile for the web otherwise compile for node usage (within jest)
-  const isWeb = api.caller((caller = {}) => {
-    return ['babel-loader', '@rollup/plugin-babel'].includes(caller.name);
-  });
-
-  // Our esnext rollup build will want to override the browserslist we pass in
-  // instead of using the default as defined in our package.json
-  const browsers = api.caller((caller = {}) => caller.browserslistOverride);
-
-  const runtimePreset = isWeb
-    ? [
-        '@shopify/babel-preset/web',
-        {modules: 'auto', typescript: true, browsers},
-      ]
-    : ['@shopify/babel-preset/node', {modules: 'auto', typescript: true}];
-
-  return {
-    presets: [runtimePreset, ['@shopify/babel-preset/react']],
-  };
+module.exports = {
+  presets: [['@shopify/babel-preset', {typescript: true, react: true}]],
 };
--- sewing-kit.config.ts
+++ sewing-kit.config.ts
@@ -1,34 +1,77 @@
-import {join} from 'path';
-
-import {ConfigurationCallback, Plugins} from '@shopify/sewing-kit';
+import {
+  createPackage,
+  Runtime,
+  createComposedWorkspacePlugin,
+  createComposedProjectPlugin,
+  createProjectPlugin,
+} from '@sewing-kit/core';
+import {babel} from '@sewing-kit/plugin-babel';
+import {workspaceTypeScript} from '@sewing-kit/plugin-typescript';
+import {eslint} from '@sewing-kit/plugin-eslint';
+import {stylelint} from '@sewing-kit/plugin-stylelint';
+import {prettier} from '@sewing-kit/plugin-prettier';
+import {jest} from '@sewing-kit/plugin-jest';
 
 // eslint-disable-next-line import/no-default-export
-export default function sewingKitConfig(
-  plugins: Plugins,
-): ReturnType<ConfigurationCallback> {
-  return {
-    name: 'polaris',
-    library: true,
-    plugins: [
-      plugins.jest((config) => {
-        config.roots = [
-          join(__dirname, 'src'),
-          join(__dirname, 'tests'),
-          join(__dirname, 'scripts'),
-        ];
+export default createPackage((pkg) => {
+  pkg.runtimes(Runtime.Node, Runtime.Browser);
+  pkg.entry({root: './src/index'});
+  pkg.use(libraryPackagePlugin(), libaryWorkspacePlugin());
+});
 
-        // Code coverage
-        config.collectCoverageFrom = [
-          'src/**/*.{ts,tsx}',
-          'scripts/**/*.{ts,tsx}',
-          '!src/test-utilities/**/*.*',
-          '!src/**/index.{ts,tsx}',
-          '!src/**/*.d.ts',
-          '!src/**/*.test.{ts,tsx}',
-        ];
+function libraryPackagePlugin() {
+  return createComposedProjectPlugin('PolarisPackage', [
+    // this needs to be set here as the current setup cannot
+    // find the babel.config.js file at the root of the project
+    babel({
+      config: {
+        presets: [['@shopify/babel-preset', {typescript: true, react: true}]],
+        configFile: false,
+      },
+    }),
+    jestAdjustmentsPlugin(),
+  ]);
+}
 
-        return config;
-      }),
-    ],
-  };
+function libaryWorkspacePlugin() {
+  return createComposedWorkspacePlugin('PolarisWorkspace', [
+    jest(),
+    eslint(),
+    stylelint({files: '**/*.scss'}),
+    prettier({files: '**/*.{md,json,yaml,yml}'}),
+    workspaceTypeScript(),
+  ]);
+}
+
+function jestAdjustmentsPlugin() {
+  return createProjectPlugin('PolarisJest', ({tasks: {test}}) => {
+    test.hook(({hooks}) => {
+      hooks.configure.hook((configuration) => {
+        configuration.jestEnvironment?.hook(() => 'jsdom');
+
+        // Aliases for root-level imports
+        // These do not work in rollup builds, so perhaps we shouldn't configure
+        // them to work here either
+        configuration.jestModuleMapper?.hook((moduleMapper) => {
+          moduleMapper['^(components|test-utilities|types|utilities)(.*)'] =
+            '<rootDir>/src/$1$2';
+
+          return moduleMapper;
+        });
+
+        // Ignore tests in the examples folder
+        configuration.jestConfig?.hook((config) => ({
+          ...config,
+          testPathIgnorePatterns: ['/node_modules/', '<rootDir>/examples/'],
+        }));
+
+        // Novel file types - scss and images
+        configuration.jestTransforms?.hook((transforms) => ({
+          ...transforms,
+          '\\.s?css$': require.resolve('./config/jest-transform-style'),
+          '\\.svg$': require.resolve('./config/jest-transform-image'),
+        }));
+      });
+    });
+  });
 }
