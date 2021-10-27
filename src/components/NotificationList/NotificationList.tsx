b4f522ade7850dd4077cf7e4752abba92e158a4d
Carlos Matallín Civera <carlos.matallin@shopify.com>
2021-10-27 11:53:14 -0400

    Notifications sections

--- src/components/NotificationList/NotificationList.tsx
+++ src/components/NotificationList/NotificationList.tsx
@@ -22,7 +22,7 @@ export function NotificationList({
   sections = [],
   actionRole,
   onActionAnyItem,
-}: ActionListProps) {
+}: NotificationListProps) {
   let finalSections: ActionListSection[] = [];
 
   if (items) {
