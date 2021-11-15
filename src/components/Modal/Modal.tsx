71545366798aaa6d37598109ed097a4c7614cd86
Alex Page <hi@alexpage.dev>
2021-11-15 07:57:18 -0800

    Update src/components/Modal/Modal.tsx
    Co-authored-by: Kyle Durand <kyledurand@users.noreply.github.com>

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -121,7 +121,7 @@ export const Modal: React.FunctionComponent<ModalProps> & {
       if (iframe && iframe.contentWindow) {
         try {
           setIframeHeight(iframe.contentWindow.document.body.scrollHeight);
-        } catch (err) {
+        } catch (_error) {
           setIframeHeight(DEFAULT_IFRAME_CONTENT_HEIGHT);
         }
       }
