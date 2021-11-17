ed5df48b8f8248aecde9b715fd5e73308b291c58
Temitayo Oyelowo <temitayo.oyelowo@shopify.com>
2021-11-17 10:12:34 -0500

    reset Playground.tsx

--- playground/Playground.tsx
+++ playground/Playground.tsx
@@ -4,17 +4,8 @@ import {Page} from '../src';
 
 export function Playground() {
   return (
-    // <Page title="Playground">
-    //   {/* Add the code you want to test in here */}
-    // </Page>
-    <Page
-      title="My Page"
-      primaryAction={{content: 'Edit', url: '#Edit'}}
-      secondaryActions={[
-        {content: 'Delete', destructive: true, url: '#Delete'},
-      ]}
-    >
-      Some page content
+    <Page title="Playground">
+      {/* Add the code you want to test in here */}
     </Page>
   );
 }
