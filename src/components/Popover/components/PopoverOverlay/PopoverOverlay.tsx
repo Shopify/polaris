b9b275a915dfea4361163e1e1b59031a3db32aed
Aaron Casanova <aaron.casanova@shopify.com>
2021-11-17 17:20:27 -0800

    fix: Update PopoverOverlay to address inverted color-scheme issue

--- src/components/Popover/components/PopoverOverlay/PopoverOverlay.tsx
+++ src/components/Popover/components/PopoverOverlay/PopoverOverlay.tsx
@@ -1,10 +1,6 @@
 import React, {PureComponent, Children, createRef} from 'react';
 import {durationFast, durationBase} from '@shopify/polaris-tokens';
 
-import {
-  CustomProperties,
-  CustomPropertiesProps,
-} from '../../../CustomProperties';
 import {findFirstFocusableNode} from '../../../../utilities/focus';
 import {classNames} from '../../../../utilities/css';
 import {
@@ -54,7 +50,6 @@ export interface PopoverOverlayProps {
   fixed?: boolean;
   hideOnPrint?: boolean;
   onClose(source: PopoverCloseSource): void;
-  colorScheme?: CustomPropertiesProps['colorScheme'];
   autofocusTarget?: PopoverAutofocusTarget;
 }
 
@@ -215,7 +210,6 @@ export class PopoverOverlay extends PureComponent<PopoverOverlayProps, State> {
       fullHeight,
       fluidContent,
       hideOnPrint,
-      colorScheme,
       autofocusTarget,
     } = this.props;
 
@@ -258,9 +252,9 @@ export class PopoverOverlay extends PureComponent<PopoverOverlayProps, State> {
           tabIndex={0}
           onFocus={this.handleFocusFirstItem}
         />
-        <CustomProperties colorScheme={colorScheme}>
+        <div>
           <div className={styles.Wrapper}>{content}</div>
-        </CustomProperties>
+        </div>
         <div
           className={styles.FocusTracker}
           // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
