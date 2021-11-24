2e532e0de363792e61bc60cd57e22271e6f82657
Alex Page <hi@alexpage.dev>
2021-11-24 15:50:21 -0800

    Remove legacy tokens from .storybook/manager.js (#4729)

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,5 @@
 import {addons} from '@storybook/addons';
 import {create} from '@storybook/theming';
-import {colorSkyLight, colorInk} from '@shopify/polaris-tokens';
 
 addons.setConfig({
   panelPosition: 'bottom',
@@ -10,10 +9,5 @@ addons.setConfig({
     brandUrl: '/',
     brandImage: null,
     appBorderRadius: 0,
-    appBg: colorSkyLight,
-    contentBg: colorSkyLight,
-    textColor: colorInk,
-    // TODO more pretty brand colors?
-    // SEE https://github.com/storybooks/storybook/blob/next/docs/src/pages/configurations/theming/index.md
   }),
 });
