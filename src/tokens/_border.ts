f7ec665438060e05dabf72f38b812f31931df574
Laura Griffee <laura.griffee@shopify.com>
2021-11-29 15:57:23 -0500

    Create tokens to replace border-radius function

--- src/tokens/_border.ts
+++ src/tokens/_border.ts
@@ -12,6 +12,8 @@ export const border: TokenGroup = {
   'banner-border-warning':
     'inset 0 0.1rem 0 0 var(--p-border-warning-subdued), inset 0 0 0 0.1rem var(--p-border-warning-subdued)',
   'border-radius-base': '0.4rem',
+  'border-radius-small': '3px',
+  'border-radius-large': '6px',
   'border-radius-full': '50%',
   'border-radius-slim': '0.2rem',
   'border-radius-wide': '0.8rem',
