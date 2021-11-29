0cb99c281fc3285bea11a87cb0aaa85a87d97079
Kyle Durand <kyle.durand@shopify.com>
2021-11-29 14:28:59 -0500

    Update set-root-property.ts
    Co-Authored-By: Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>

--- src/utilities/set-root-property.ts
+++ src/utilities/set-root-property.ts
@@ -1,13 +1,10 @@
 export function setRootProperty(
   name: string,
   value: string,
-  node: Element | null,
+  node?: HTMLElement,
 ) {
-  if (document == null) {
-    return;
-  }
+  if (!document) return;
 
-  const styleNode =
-    node && node instanceof HTMLElement ? node : document.documentElement;
-  styleNode && styleNode.style.setProperty(name, value);
+  const styleNode = node ? node : document.documentElement;
+  styleNode.style.setProperty(name, value);
 }
