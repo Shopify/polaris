bde9ae167c6e31424805e7d43bfd859bd35757d8
Laura Griffee <laura.griffee@shopify.com>
2021-11-30 10:41:15 -0500

    Remove border-radius() tokens

--- src/tokens/_border.ts
+++ src/tokens/_border.ts
@@ -2,8 +2,6 @@ import type {TokenGroup} from './tokens';
 
 export const border: TokenGroup = {
   'border-radius-base': '0.4rem',
-  'border-radius-small': '3px',
-  'border-radius-large': '6px',
   'border-radius-full': '50%',
   'border-radius-slim': '0.2rem',
   'border-radius-wide': '0.8rem',
