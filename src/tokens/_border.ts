b0fdc5d462fb03b92fcc0c381d5c64c12aff36e1
Laura Griffee <laura.griffee@shopify.com>
2021-11-30 13:29:36 -0500

    Remove unused token value

--- src/tokens/_border.ts
+++ src/tokens/_border.ts
@@ -1,7 +1,6 @@
 import type {TokenGroup} from './tokens';
 
 export const border: TokenGroup = {
-  'border-radius-slim': '0.2rem',
   'border-radius-base': '0.4rem',
   'border-radius-wide': '0.8rem',
   'border-radius-full': '50%',
