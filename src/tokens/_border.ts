cd058e84c02f2cd8b613692edbbc8888fb0a1f55
Laura Griffee <laura.griffee@shopify.com>
2021-11-30 13:28:48 -0500

    Reorder token values

--- src/tokens/_border.ts
+++ src/tokens/_border.ts
@@ -1,8 +1,8 @@
 import type {TokenGroup} from './tokens';
 
 export const border: TokenGroup = {
-  'border-radius-base': '0.4rem',
-  'border-radius-full': '50%',
   'border-radius-slim': '0.2rem',
+  'border-radius-base': '0.4rem',
   'border-radius-wide': '0.8rem',
+  'border-radius-full': '50%',
 };
