bbb68a97e8b7f13d1a054d45ccd85c33c13e74d6
Laura Griffee <laura.griffee@shopify.com>
2021-12-01 15:00:38 -0500

    Rework and rename tokens
    Co-Authored-By: Kyle Durand <6844391+kyledurand@users.noreply.github.com>

--- src/tokens/_shape.ts
+++ src/tokens/_shape.ts
@@ -1,7 +1,11 @@
 import type {TokenGroup} from './tokens';
 
 export const shape: TokenGroup = {
-  'border-radius-base': '0.4rem',
-  'border-radius-wide': '0.8rem',
-  'border-radius-full': '50%',
+  'border-radius-1': '0.2rem',
+  'border-radius-2': '0.4rem',
+  'border-radius-3': '0.8rem',
+  'border-radius-4': '1.2rem',
+  'border-radius-5': '1.6rem',
+  'border-radius-6': '2rem',
+  'border-radius-7': '50%',
 };
