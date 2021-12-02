2e32548a0cb54c1a86fa9ef46aafacfaf92418b8
Laura Griffee <laura.griffee@shopify.com>
2021-12-02 15:21:41 -0500

    Update the values of ease-in and ease-out

--- src/tokens/_motion.ts
+++ src/tokens/_motion.ts
@@ -13,7 +13,7 @@ export const motion: TokenGroup = {
   'duration-450': '450ms',
   'duration-500': '500ms',
   ease: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
-  'ease-in': 'cubic-bezier(0.36, 0, 1, 1)',
-  'ease-out': 'cubic-bezier(0, 0, 0.42, 1)',
+  'ease-in': 'cubic-bezier(0.42, 0, 1, 1)',
+  'ease-out': 'cubic-bezier(0, 0, 0.58, 1)',
   'ease-excite': 'cubic-bezier(0.18, 0.67, 0.6, 1.22)',
 };
