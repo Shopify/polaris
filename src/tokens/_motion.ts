528fb8590aada792958fe63763b381cedc4971ca
Laura Griffee <laura.griffee@shopify.com>
2021-12-02 15:25:03 -0500

    Add ease-in-out and linear tokens

--- src/tokens/_motion.ts
+++ src/tokens/_motion.ts
@@ -15,5 +15,7 @@ export const motion: TokenGroup = {
   ease: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
   'ease-in': 'cubic-bezier(0.42, 0, 1, 1)',
   'ease-out': 'cubic-bezier(0, 0, 0.58, 1)',
+  'ease-in-out': 'cubic-bezier(0.42, 0, 0.58, 1)',
+  linear: 'cubic-bezier(0, 0, 1, 1)',
   'ease-excite': 'cubic-bezier(0.18, 0.67, 0.6, 1.22)',
 };
