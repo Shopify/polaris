85f405e0e3c2aea321c8d43b528ce155824f3faf
Laura Griffee <laura@mailzone.com>
2021-12-02 13:26:54 -0500

    Add duration token values between 0 and 500 with 50ms increments (Duration Tokens Phase 1) (#4781)
    * Add missing duration values between 0 and 500 with 50ms increments
    
    * Replace any hardcoded values with new tokens
    
    * Update README.md
    
    * Remove unnecessary motion legacy tokens
    
    * Update UNRELEASED-v8.md
    
    * Update UNRELEASED-v8.md
    
    Co-authored-by: Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
    
    Co-authored-by: Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>

--- UNRELEASED-v8.md
+++ UNRELEASED-v8.md
@@ -33,6 +33,8 @@ Use [the changelog guidelines](/documentation/Versioning%20and%20changelog.md) t
 
 ### Enhancements
 
+- Added duration token values between 0 and 500 with 50ms increments ([#4781](https://github.com/Shopify/polaris-react/pull/4781))
+
 ### Bug fixes
 
 ### Documentation
--- src/tokens/_motion.ts
+++ src/tokens/_motion.ts
@@ -1,11 +1,16 @@
 import type {TokenGroup} from './tokens';
 
 export const motion: TokenGroup = {
+  'duration-0': '0ms',
+  'duration-50': '50ms',
   'duration-100': '100ms',
   'duration-150': '150ms',
   'duration-200': '200ms',
+  'duration-250': '250ms',
   'duration-300': '300ms',
+  'duration-350': '350ms',
   'duration-400': '400ms',
+  'duration-450': '450ms',
   'duration-500': '500ms',
   ease: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
   'ease-in': 'cubic-bezier(0.36, 0, 1, 1)',
