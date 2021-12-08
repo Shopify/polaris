a5eaf30bc48e7e5708690d315ccfc023e1554d09
Dominic McPhee <dominic.mcphee@shopify.com>
2021-12-08 12:55:30 -0500

    Better selection

--- scripts/storybook-a11y-test.js
+++ scripts/storybook-a11y-test.js
@@ -17,8 +17,6 @@ const pMap__default = /* #__PURE__*/ _interopDefaultLegacy(pMap);
 const chalk__default = /* #__PURE__*/ _interopDefaultLegacy(chalk);
 
 /* eslint-disable no-console */
-// Until https://github.com/dequelabs/axe-core/pull/3161 is fixed
-// type A11yParametersElement = A11yParameters['element'] | NodeList;
 const FORMATTING_SPACER = '    ';
 
 function getBrowser() {
@@ -63,6 +61,30 @@ async function getCurrentStoryIds({ iframePath, skippedStoryIds = [] }) {
   return stories.reduce(removeSkippedStories(skippedStoryIds), []);
 }
 
+function nodeToSelector(elem) {
+  const { tagName, id, className, parentNode } = elem;
+  if (tagName === 'HTML') return 'HTML';
+  let str = tagName;
+  str += id === '' ? '' : `#${id}`;
+
+  if (className) {
+    const classes = className.split(/\s/); // eslint-disable-next-line @typescript-eslint/prefer-for-of
+
+    for (let i = 0; i < classes.length; i++) {
+      str += `.${classes[i]}`;
+    }
+  }
+
+  let childIndex = 1;
+
+  for (let e = elem; e.previousElementSibling; e = e.previousElementSibling) {
+    childIndex += 1;
+  }
+
+  str += `:nth-child(${childIndex})`;
+  return `${nodeToSelector(parentNode)} > ${str}`;
+}
+
 function formatFailureNodes(nodes) {
   return `${FORMATTING_SPACER}${nodes
     .map((node) => node.html)
@@ -93,6 +115,26 @@ function isAutocompleteNope(violation) {
   return isAutocompleteAttribute && hasNope;
 }
 
+function getElement() {
+  const storyRoot = document.getElementById('story-root');
+  return storyRoot ? storyRoot.childNodes : document.getElementById('root');
+} // Returns story parameters or default ones.
+// Originally inspired by:
+// https://github.com/storybookjs/storybook/blob/78c580eac4c91231b5966116492e34de0a0df66f/addons/a11y/src/a11yRunner.ts#L69-L80
+
+function getA11yParams(storyId) {
+  const { parameters } = window.__STORYBOOK_STORY_STORE__.fromId(storyId);
+
+  return (
+    parameters.a11y || {
+      config: {},
+      options: {
+        restoreScroll: true,
+      },
+    }
+  );
+}
+
 function testPage(iframePath, browser, timeout, disableAnimation) {
   return async function (id) {
     console.log(` - ${id}`);
@@ -117,8 +159,15 @@ function testPage(iframePath, browser, timeout, disableAnimation) {
         });
       }
 
-      const results = await new puppeteer$1.AxePuppeteer(page) // .configure(storyA11yParams)
-        .include('#root')
+      const a11yParams = getA11yParams(id);
+      const config = a11yParams.config ? a11yParams.config : {};
+      const options = a11yParams.options ? a11yParams.options : {};
+      const element = getElement();
+      const elementSelector = nodeToSelector(element);
+      const results = await new puppeteer$1.AxePuppeteer(page)
+        .include(elementSelector)
+        .configure(config)
+        .options(options)
         .analyze(); // const result: axeCore.AxeResults = await page.evaluate(
       //   async (id: StoryId) => {
       //     // Implementation matching
