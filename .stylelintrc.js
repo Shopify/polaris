0b7a56c942f6995271890d766a2cd680cf44d283
Aaron Casanova <aaron.casanova@shopify.com>
2021-12-08 10:34:36 -0800

    fix: Move Stylelint config to .stylelintrc.js

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const {tokenList} = require('../../build/cjs/token-list');
+const {tokenList} = require('./build/cjs/token-list');
 
 /**
  * Allowed custom property names in Polaris component styles.
@@ -8,7 +8,10 @@ const {tokenList} = require('../../build/cjs/token-list');
 const polarisComponentCustomProperties = /--pc-.+/;
 
 module.exports = {
-  plugins: [path.join(__dirname, '../plugins/custom-properties-allowed-list')],
+  extends: ['@shopify/stylelint-plugin/prettier'],
+  plugins: [
+    path.join(__dirname, './stylelint/plugins/custom-properties-allowed-list'),
+  ],
   rules: {
     '@shopify/custom-properties-allowed-list': {
       allowedProperties: [
