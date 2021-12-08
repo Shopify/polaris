468ebeceed5ea3f40243ebdea271a4c6fe3d7ceb
Kyle Durand <kyledurand@users.noreply.github.com>
2021-12-08 18:09:56 -0500

    Use rems in tokens (#4808)
    * Use rems in tokens
    
    * revert settings.json change

--- src/tokens/_shape.ts
+++ src/tokens/_shape.ts
@@ -1,11 +1,12 @@
 import type {TokenGroup} from './tokens';
+import {rem} from './utilities';
 
 export const shape: TokenGroup = {
-  'border-radius-05': '2px',
-  'border-radius-1': '4px',
-  'border-radius-2': '8px',
-  'border-radius-3': '12px',
-  'border-radius-4': '16px',
-  'border-radius-5': '20px',
+  'border-radius-05': rem('2px'),
+  'border-radius-1': rem('4px'),
+  'border-radius-2': rem('8px'),
+  'border-radius-3': rem('12px'),
+  'border-radius-4': rem('16px'),
+  'border-radius-5': rem('20px'),
   'border-radius-6': '50%',
 };
