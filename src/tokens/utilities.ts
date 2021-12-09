c4f3d2ea9b01550ba9255f08ab22b463adbf322e
Aaron Casanova <aaron.casanova@shopify.com>
2021-12-09 12:09:07 -0800

    fix: Update rem util to only transform valid px units

--- src/tokens/utilities.ts
+++ src/tokens/utilities.ts
@@ -2,14 +2,18 @@ import type {TokenGroup} from './tokens';
 
 const BASE_FONT_SIZE = 16;
 
-export function rem(unit: string) {
-  return unit.endsWith('px')
-    ? `${parseInt(unit, 10) / BASE_FONT_SIZE}rem`
-    : unit;
+function rem(value: string) {
+  return value.replace(
+    // https://regex101.com/r/RBL7EE/1
+    /\d+(?:\.\d+|\d*)px/g,
+    (px: string) => `${parseInt(px, 10) / BASE_FONT_SIZE}rem`,
+  );
 }
 
 export function tokensToRems(tokenGroup: TokenGroup): TokenGroup {
   return Object.fromEntries(
-    Object.entries(tokenGroup).map(([token, value]) => [token, rem(value)]),
+    Object.entries(tokenGroup).map(([token, value]) => {
+      return [token, rem(value)];
+    }),
   );
 }
