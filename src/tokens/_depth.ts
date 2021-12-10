8ed2ec6b5dd6ca47d6b36b52dd3f5b87babdec82
Laura Griffee <laura.griffee@shopify.com>
2021-12-10 09:46:14 -0500

    Remove --p-shadow-from-dim-light legacy token

--- src/tokens/_depth.ts
+++ src/tokens/_depth.ts
@@ -6,7 +6,7 @@ export const depth: TokenGroup = {
   'popover-shadow':
     '-1px 0px 20px rgba(23, 24, 24, 0.05), 0px 1px 5px rgba(0, 0, 0, 0.15)',
   'modal-shadow':
-    '0px 26px 80px var(--p-shadow-from-dim-light), 0px 0px 1px var(--p-shadow-from-dim-light)',
+    '0px 26px 80px rgba(0, 0, 0, 0.2), 0px 0px 1px rgba(0, 0, 0, 0.2)',
   'top-bar-shadow': '0 2px 2px -1px rgba(0, 0, 0, 0.15)',
   'button-drop-shadow': '0 1px 0 rgba(0, 0, 0, 0.05)',
   'button-inner-shadow': 'inset 0 -1px 0 rgba(0, 0, 0, 0.2)',
