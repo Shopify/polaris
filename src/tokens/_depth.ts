d003ac2536006df2d513fe0d46d7985d119ae6ea
Laura Griffee <laura.griffee@shopify.com>
2021-12-10 10:08:09 -0500

    Add tokens to replace shadow() values

--- src/tokens/_depth.ts
+++ src/tokens/_depth.ts
@@ -1,6 +1,14 @@
 import type {TokenGroup} from './tokens';
 
 export const depth: TokenGroup = {
+  'shadow-transparent': '0 0 0 0 transparent',
+  'shadow-faint': '0 1px 0 0 rgba(22, 29, 37, 0.05)',
+  'shadow-base':
+    '0 0 0 1px rgba(63, 63, 68, 0.05), 0 1px 3px 0 rgba(63, 63, 68, 0.15)',
+  'shadow-deep':
+    '0 0 0 1px rgba(6, 44, 82, 0.1), 0 2px 16px rgba(33, 43, 54, 0.08)',
+  'shadow-layer':
+    '0 31px 41px 0 rgba(32, 42, 53, 0.2), 0 2px 16px 0 rgba(32, 42, 54, 0.08)',
   'card-shadow':
     '0px 0px 5px rgba(23, 24, 24, 0.05), 0px 1px 2px rgba(0, 0, 0, 0.15)',
   'popover-shadow':
