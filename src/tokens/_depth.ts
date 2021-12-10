13b09ff61736e4974418034358cd88f1bb531fbd
Laura Griffee <laura.griffee@shopify.com>
2021-12-10 10:08:52 -0500

    Remove px on 0 values in tokens

--- src/tokens/_depth.ts
+++ src/tokens/_depth.ts
@@ -10,11 +10,11 @@ export const depth: TokenGroup = {
   'shadow-layer':
     '0 31px 41px 0 rgba(32, 42, 53, 0.2), 0 2px 16px 0 rgba(32, 42, 54, 0.08)',
   'card-shadow':
-    '0px 0px 5px rgba(23, 24, 24, 0.05), 0px 1px 2px rgba(0, 0, 0, 0.15)',
+    '0 0 5px rgba(23, 24, 24, 0.05), 0 1px 2px rgba(0, 0, 0, 0.15)',
   'popover-shadow':
-    '-1px 0px 20px rgba(23, 24, 24, 0.05), 0px 1px 5px rgba(0, 0, 0, 0.15)',
+    '-1px 0 20px rgba(23, 24, 24, 0.05), 0 1px 5px rgba(0, 0, 0, 0.15)',
   'modal-shadow':
-    '0px 26px 80px rgba(0, 0, 0, 0.2), 0px 0px 1px rgba(0, 0, 0, 0.2)',
+    '0 26px 80px rgba(0, 0, 0, 0.2), 0 0px 1px rgba(0, 0, 0, 0.2)',
   'top-bar-shadow': '0 2px 2px -1px rgba(0, 0, 0, 0.15)',
   'button-drop-shadow': '0 1px 0 rgba(0, 0, 0, 0.05)',
   'button-inner-shadow': 'inset 0 -1px 0 rgba(0, 0, 0, 0.2)',
