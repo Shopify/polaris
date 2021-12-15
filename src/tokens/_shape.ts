e4e898946a4f900ffb09555c0155bbfa2aea82f3
Kyle Durand <kyle.durand@shopify.com>
2021-12-15 13:11:16 -0500

    [Maintenance] Clean up css custom properties

---
+++ src/tokens/_shape.ts
@@ -0,0 +1,14 @@
+import type {TokenGroup} from './tokens';
+
+export const shape: TokenGroup = {
+  'border-radius-05': '2px',
+  'border-radius-1': '4px',
+  'border-radius-2': '8px',
+  'border-radius-3': '12px',
+  'border-radius-4': '16px',
+  'border-radius-5': '20px',
+  'border-radius-6': '30px',
+  'border-radius-base': '3px',
+  'border-radius-large': '6px',
+  'border-radius-half': '50%',
+};
