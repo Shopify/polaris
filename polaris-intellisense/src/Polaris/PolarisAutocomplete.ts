95a6b6ae0230e0fc1ddd18ef932ccfee9546f6a9
Yuraima Estevez <yurm04@gmail.com>
2022-03-08 10:34:06 -0600

    updated to use utility func for tokens generation

--- polaris-intellisense/src/Polaris/PolarisAutocomplete.ts
+++ polaris-intellisense/src/Polaris/PolarisAutocomplete.ts
@@ -1,51 +1,45 @@
-import * as vscode from 'vscode'
-import { allTokens } from '../data/allTokens';
-
+import * as vscode from 'vscode';
+import {allTokens} from '../data/allTokens';
 
 export class PolarisAutocomplete implements vscode.CompletionItemProvider {
+  get allTokens() {
+    return allTokens;
+  }
 
-    get allTokens() {
-        return allTokens
-    }
+  async provideCompletionItems(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken,
+    context: vscode.CompletionContext,
+  ): Promise<vscode.CompletionItem[] | null | undefined> {
+    // iterate over all tokens and create completion items
+    let completionItems = allTokens.map((token) => {
+      const autocompleteValue = `var(${token})`;
+      return new vscode.CompletionItem(
+        autocompleteValue,
+        vscode.CompletionItemKind.Value,
+      );
+    });
 
-    async provideCompletionItems(
-        document: vscode.TextDocument, 
-        position: vscode.Position, 
-        token: vscode.CancellationToken, 
-        context: vscode.CompletionContext
-    ): Promise<vscode.CompletionItem[] | null | undefined> {
-        
-            // iterate over all tokens and create completion items
-            const tokenNames = Object.keys(this.allTokens)
-            let completionItems = tokenNames.map(token => {
-                const autocompleteValue = `var(--p-${token})`
-                return new vscode.CompletionItem(autocompleteValue, vscode.CompletionItemKind.Value)
-            })
+    return completionItems;
 
-            return completionItems;
+    // -------
+    // Junk to narrow completion items by css attributes
+    // const currentLine = document.getText(document.lineAt(position))
 
-        // -------
-        // Junk to narrow completion items by css attributes
-        // const currentLine = document.getText(document.lineAt(position))
-        
-        
-        // const currentLine = document.getText(document.lineAt(position).range)
-        // console.log(currentLine)
+    // const currentLine = document.getText(document.lineAt(position).range)
+    // console.log(currentLine)
 
-        // if (currentLine.includes('color')) {
-        //     console.log('found color')
-        //     // iterate over all tokens and create completion items
-        //     const tokenNames = Object.keys(this.allTokens)
-        //     let completionItems = tokenNames.map(token => {
-        //         const autocompleteValue = `var(--p-${token})`
-        //         return new vscode.CompletionItem(autocompleteValue, vscode.CompletionItemKind.Value)
-        //     })
+    // if (currentLine.includes('color')) {
+    //     console.log('found color')
+    //     // iterate over all tokens and create completion items
+    //     const tokenNames = Object.keys(this.allTokens)
+    //     let completionItems = tokenNames.map(token => {
+    //         const autocompleteValue = `var(--p-${token})`
+    //         return new vscode.CompletionItem(autocompleteValue, vscode.CompletionItemKind.Value)
+    //     })
 
-        //     return completionItems;
-        // }
-    }
-
-
-
-
+    //     return completionItems;
+    // }
+  }
 }
