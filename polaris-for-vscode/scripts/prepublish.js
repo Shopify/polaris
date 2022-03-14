13cfe7a98f34a67f5441a04e6486dd37e273d772
Yuraima Estevez <yurm04@gmail.com>
2022-03-14 15:35:55 -0500

    remove allTokens file and generate it instead

--- polaris-for-vscode/scripts/prepublish.js
+++ polaris-for-vscode/scripts/prepublish.js
@@ -3,11 +3,22 @@ const path = require('path');
 
 const getCustomPropertyNames = require('../../scripts/utilities/getCustomPropertyNames');
 
-const outDir = '../../polaris-for-vscode/src/data/allTokens.ts';
+const dirPath = path.join(__dirname, '../src/data')
+const outFile = 'allTokens.ts';
+const filePath = path.join(dirPath, outFile)
 
 const tokens = getCustomPropertyNames();
 
-fs.writeFileSync(
-  path.join(__dirname, outDir),
-  `export const allTokens = ${JSON.stringify(tokens)};`,
-);
+try {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath)
+  }
+
+  fs.writeFileSync(
+    filePath,
+    `export const allTokens = ${JSON.stringify(tokens)};`,
+  );
+} catch (err) {
+  console.error(`ERROR Could not create custom properties file "${filePath}": ${err}`)
+}
+
