4b9a6e2b0de0b8b6e1855eabd014fa2c73a6ac40
Romello Goodman <romello.goodman@shopify.com>
2022-03-16 16:23:22 -0400

    Adding files from rest of the guide

--- secretpolaris.shopify.com/next.config.js
+++ secretpolaris.shopify.com/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  rewrites: () => [
+    {
+      source: '/services/ping',
+      destination: '/api/services/ping',
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
