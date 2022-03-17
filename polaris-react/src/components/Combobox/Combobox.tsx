6350b2531a62810ab2a3bd972542614ed13d1379
Lo Kim <lo.kim@shopify.com>
2022-03-16 20:27:36 -0400

    [Combobox] Fix accessibility issue on focus and option create
    When the activator on the Combobox is focused, fix functionality to set active option as first valid option in Listbox. Also fixes bug where popover was closing when creating an option which was inconsist with behavior when selecting/deselecting an existing option.
    
    Co-authored-by: Chloe Rice <chloe.rice@shopify.com>

--- polaris-react/src/components/Combobox/Combobox.tsx
+++ polaris-react/src/components/Combobox/Combobox.tsx
@@ -17,60 +17,78 @@ import styles from './Combobox.scss';
 import {TextField} from './components';
 
 export interface ComboboxProps {
-  children?: React.ReactElement<ListboxProps> | null;
+  /** The text field component to activate the Popover */
   activator: React.ReactElement<TextFieldProps>;
+  /** Allows more than one option to be selected */
   allowMultiple?: boolean;
-  onScrolledToBottom?(): void;
+  /** The content to display inside the popover */
+  children?: React.ReactElement<ListboxProps> | null;
+  /** The preferred direction to open the popover */
   preferredPosition?: PopoverProps['preferredPosition'];
+  /** Callback fired when the bottom of the popover content is reached */
+  onScrolledToBottom?(): void;
 }
 
 export function Combobox({
-  children,
   activator,
   allowMultiple,
-  onScrolledToBottom,
+  children,
   preferredPosition = 'below',
+  onScrolledToBottom,
 }: ComboboxProps) {
   const [popoverActive, setPopoverActive] = useState(false);
   const [activeOptionId, setActiveOptionId] = useState<string>();
   const [textFieldLabelId, setTextFieldLabelId] = useState<string>();
   const [listboxId, setListboxId] = useState<string>();
   const [textFieldFocused, setTextFieldFocused] = useState<boolean>(false);
+  const [disableCloseOnSelect, setDisableCloseOnSelect] = useState(false);
   const shouldOpen = Boolean(!popoverActive && Children.count(children) > 0);
   const ref = useRef<PopoverPublicAPI | null>(null);
 
-  const onOptionSelected = useCallback(() => {
-    if (!allowMultiple) {
-      setPopoverActive(false);
-      setActiveOptionId(undefined);
-      return;
-    }
-    ref.current?.forceUpdatePosition();
-  }, [allowMultiple]);
-
   const handleClose = useCallback(() => {
-    setPopoverActive(false);
+    // only deactive popover if not creating a new option
+    if (!disableCloseOnSelect) {
+      setPopoverActive(false);
+    }
+
+    setActiveOptionId(undefined);
+  }, [disableCloseOnSelect]);
+
+  const handleOpen = useCallback(() => {
+    setPopoverActive(true);
     setActiveOptionId(undefined);
   }, []);
 
+  const onOptionSelected = useCallback(() => {
+    if (!allowMultiple) {
+      handleClose();
+      return;
+    } else {
+      setDisableCloseOnSelect(true);
+      setActiveOptionId(undefined);
+    }
+
+    ref.current?.forceUpdatePosition();
+  }, [allowMultiple, handleClose, setDisableCloseOnSelect]);
+
   const handleFocus = useCallback(() => {
     if (shouldOpen) {
-      setPopoverActive(true);
+      handleOpen();
     }
-  }, [shouldOpen]);
+  }, [shouldOpen, handleOpen]);
 
   const handleChange = useCallback(() => {
     if (shouldOpen) {
-      setPopoverActive(true);
+      handleOpen();
     }
-  }, [shouldOpen]);
+  }, [shouldOpen, handleOpen]);
 
   const handleBlur = useCallback(() => {
+    setDisableCloseOnSelect(false);
     if (popoverActive) {
-      setPopoverActive(false);
-      setActiveOptionId(undefined);
+      handleClose();
     }
-  }, [popoverActive]);
+  }, [popoverActive, handleClose, setDisableCloseOnSelect]);
 
   const textFieldContextValue: ComboboxTextFieldType = useMemo(
     () => ({
@@ -104,21 +122,21 @@ export function Combobox({
 
   const listboxContextValue: ComboboxListboxType = useMemo(
     () => ({
-      setActiveOptionId,
-      setListboxId,
       listboxId,
       textFieldLabelId,
-      onOptionSelected,
       textFieldFocused,
+      onOptionSelected,
+      setActiveOptionId,
+      setListboxId,
       onKeyToBottom: onScrolledToBottom,
     }),
     [
-      setActiveOptionId,
-      setListboxId,
       listboxId,
       textFieldLabelId,
-      onOptionSelected,
       textFieldFocused,
+      onOptionSelected,
+      setActiveOptionId,
+      setListboxId,
       onScrolledToBottom,
     ],
   );
