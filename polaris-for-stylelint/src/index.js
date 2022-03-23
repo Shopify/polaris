f81da20344d511dadb06f4ea413f9a0d6f828c08
Aaron Casanova <aaron.casanova@shopify.com>
2022-03-23 15:08:47 +0000

    feat: Polaris for Stylelint

--- polaris-for-stylelint/src/index.js
+++ polaris-for-stylelint/src/index.js
@@ -21,25 +21,17 @@ const polarisTokenCustomProperties = Array.from(
   ),
 );
 
-/**
- * Allowed custom property names in Polaris component styles.
- */
-const polarisComponentCustomProperties = /--pc-.+/;
-
 module.exports = {
-  extends: ['@shopify/stylelint-plugin/prettier'],
   plugins: ['@shopify/custom-properties-allowed-list'],
   rules: {
     '@shopify/custom-properties-allowed-list': {
       allowedProperties: [
         '--polaris-version-number',
-        polarisComponentCustomProperties,
+        // Allow anything that's not a Polaris component custom property
+        /^(?!--pc-).+/,
       ],
       allowedValues: {
-        '/.+/': [
-          polarisComponentCustomProperties,
-          ...polarisTokenCustomProperties,
-        ],
+        '/.+/': [...polarisTokenCustomProperties],
       },
     },
   },
