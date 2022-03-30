dbbfc28f142075777533fe1cc8ec01aebb87d42e
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-03-30 13:30:45 -0700

    feat: Add consumer facing custom-properties-allowed-list config

--- stylelint-polaris/index.js
+++ stylelint-polaris/index.js
@@ -1,6 +1,48 @@
+const {
+  ruleName: customPropertiesAllowedListRuleName,
+} = require('./plugins/custom-properties-allowed-list');
+
+/**
+ * Allowed Polaris token custom properties.
+ *
+ * @example ['--p-text', '--p-background']
+ */
+const polarisCustomPropertyNames = require('./data/polaris-custom-property-names');
+
+/**
+ * User defined custom property names.
+ *
+ * Determined by allowing any custom property that is not prefixed with `--p-` or `--pc-`.
+ */
+const userDefinedCustomPropertyNames = /--(?!pc?-).+/;
+
 /**
  * @type {import('stylelint').Config}
  */
-modules.exports = {
-  extends: ['../configs/shared'],
+module.exports = {
+  extends: ['./configs/shared'],
+  plugins: ['./plugins/custom-properties-allowed-list'],
+  rules: {
+    /**
+     * Custom property constraints:
+     * - Allow any user defined custom properties.
+     * - Allow `--p-*` Polaris custom properties as values and property overrides.
+     * - Disallow `--pc-` Polaris component custom properties as values and property overrides.
+     */
+    [customPropertiesAllowedListRuleName]: [
+      {
+        allowedProperties: [
+          ...polarisCustomPropertyNames,
+          userDefinedCustomPropertyNames,
+        ],
+        allowedValues: {
+          '/.+/': [
+            ...polarisCustomPropertyNames,
+            userDefinedCustomPropertyNames,
+          ],
+        },
+      },
+      {severity: 'warning'},
+    ],
+  },
 };
