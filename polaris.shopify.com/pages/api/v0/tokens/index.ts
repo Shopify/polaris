b33ddb571c7951624db920ac663e50aee2afac46
Ben Scott <227292+BPScott@users.noreply.github.com>
2022-04-05 15:41:35 -0700

    Enable linting poalris.shopify.com and fix issues (#5435)

--- polaris.shopify.com/pages/api/v0/tokens/index.ts
+++ polaris.shopify.com/pages/api/v0/tokens/index.ts
@@ -1,8 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type {NextApiRequest, NextApiResponse} from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader("content-type", "text/html");
-  res.send("Token API coming soon.");
+  res.setHeader('content-type', 'text/html');
+  res.send('Token API coming soon.');
 };
 
 export default handler;
