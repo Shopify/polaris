b322fb65f6ed6a6489b9ceaf32195dcf4a9fbeef
Sam Rose <sam.rose@shopify.com>
2022-04-14 13:08:24 -0600

    Enable CORS for the token API

--- polaris.shopify.com/next.config.js
+++ polaris.shopify.com/next.config.js
@@ -1,8 +1,30 @@
+/* eslint-disable require-await */
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/v0/tokens/:path*',
+        headers: [
+          {key: 'Access-Control-Allow-Credentials', value: 'true'},
+          {key: 'Access-Control-Allow-Origin', value: '*'},
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
 };
 
