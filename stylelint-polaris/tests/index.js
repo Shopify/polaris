0234e8a7fc5ec5dc1c6a8d3a9921be7176b85499
Alex Page <hi@alexpage.dev>
2022-04-19 09:45:00 -0700

    Update index.js

--- stylelint-polaris/tests/index.js
+++ stylelint-polaris/tests/index.js
@@ -16,32 +16,37 @@ const configs = {
   },
 };
 
-(async () => {
-  for (const [configName, config] of Object.entries(configs)) {
-    const {results} = await stylelint.lint({
-      config: {
-        extends: config.path,
-        customSyntax: 'postcss-scss',
-      },
-      files: config.files,
-      globbyOptions: {
-        cwd: __dirname,
-      },
-    });
+const testFile = async (configName, config) => {
+  const {results} = await stylelint.lint({
+    config: {
+      extends: config.path,
+      customSyntax: 'postcss-scss',
+    },
+    files: config.files,
+    globbyOptions: {
+      cwd: __dirname,
+    },
+  });
 
-    const foundFailues = results.reduce(
-      (warnings, result) => warnings + result.warnings.length,
-      0,
+  const foundFailues = results.reduce(
+    (warnings, result) => warnings + result.warnings.length,
+    0,
+  );
+
+  if (foundFailues === config.expectedFailures) {
+    console.log(
+      `✅ stylelint-polaris found the expected ${foundFailues}/${config.expectedFailures} errors in ${configName}`,
     );
-
-    if (foundFailues === config.expectedFailures) {
-      console.log(
-        `✅ stylelint-polaris found ${config.expectedFailures} errors in ${configName} config test`,
-      );
-    } else {
-      throw Error(
-        `❌ stylelint-polaris found ${foundFailues} errors. Failed to find ${config.expectedFailures} errors in the ${configName} config test`,
-      );
-    }
+  } else {
+    throw Error(
+      `❌ stylelint-polaris did not find ${foundFailues}/${config.expectedFailures} errors in the ${configName} test`,
+    );
   }
+};
+
+(async () => {
+  const tests = Object.entries(configs).map(([configName, config]) =>
+    testFile(configName, config),
+  );
+  await Promise.all(tests);
 })();
