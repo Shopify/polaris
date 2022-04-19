148abd2a78dcfcb6aa10fa28b3d01aedbc372ccb
Marten Bjork <marten.bjork@shopify.com>
2022-04-19 15:05:33 +0200

    Show all tokens in global search

--- polaris.shopify.com/src/utils/search.ts
+++ polaris.shopify.com/src/utils/search.ts
@@ -1,9 +1,15 @@
 import { Result } from "../types";
 import colorLight from "../../../polaris-react/src/tokens/token-groups/color.light.json";
+import depth from "../../../polaris-react/src/tokens/token-groups/depth.json";
+import motion from "../../../polaris-react/src/tokens/token-groups/motion.json";
+import shape from "../../../polaris-react/src/tokens/token-groups/shape.json";
+import spacing from "../../../polaris-react/src/tokens/token-groups/spacing.json";
+import typography from "../../../polaris-react/src/tokens/token-groups/typography.json";
+import zIndex from "../../../polaris-react/src/tokens/token-groups/z-index.json";
 import components from "../data/components.json";
 import icons from "../data/icons.json";
 import Fuse from "fuse.js";
-import { stripMarkdownLinks } from "./various";
+import { slugify, stripMarkdownLinks } from "./various";
 
 let allPages: Result[] = [];
 
@@ -30,6 +36,21 @@ Object.entries(colorLight).forEach(([tokenName, tokenValue]) => {
     meta: {
       colorToken: { value: tokenValue },
     },
+  });
+});
+
+// Add other tokens
+const otherTokenGroups = { depth, motion, shape, spacing, typography, zIndex };
+Object.entries(otherTokenGroups).forEach(([groupSlug, tokenGroup]) => {
+  Object.entries(tokenGroup).forEach(([tokenName, tokenValue]) => {
+    allPages.push({
+      type: "token",
+      title: `--p-${tokenName}`,
+      excerpt: "",
+      url: `/tokens/${slugify(groupSlug)}#${tokenName}`,
+      keywords: [],
+      meta: {},
+    });
   });
 });
 
