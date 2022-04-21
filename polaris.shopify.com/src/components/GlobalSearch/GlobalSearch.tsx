f7339a066e523ed31a722b98452e64b06fb62eae
Marten Bjork <marten.bjork@shopify.com>
2022-04-21 18:52:29 +0200

    Fix screen reader text

--- polaris.shopify.com/src/components/GlobalSearch/GlobalSearch.tsx
+++ polaris.shopify.com/src/components/GlobalSearch/GlobalSearch.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "../Image";
 import Link from "next/link";
 import { useState } from "react";
 import { search } from "../../utils/search";
@@ -42,99 +42,95 @@ function GlobalSearch({}: Props) {
 
   return (
     <div className={styles.GlobalSearch}>
-      <>
-        <label {...getLabelProps()} className="sr-only">
-          Choose an element:
-        </label>
-        <div {...getComboboxProps()}>
-          <input {...getInputProps()} placeholder="Search" />
-          <button
-            type="button"
-            {...getToggleButtonProps()}
-            aria-label={"toggle menu"}
-            className="sr-only"
-          >
-            &#8595;
-          </button>
-        </div>
-        <ul {...getMenuProps()} className={styles.Results}>
-          {isOpen &&
-            searchResults.map((item, index) => {
-              const previousItemHadDifferentCategory =
-                searchResults[index - 1] &&
-                searchResults[index - 1].category !== item.category;
+      <label {...getLabelProps()} className="sr-only">
+        Search
+      </label>
+      <div {...getComboboxProps()}>
+        <input {...getInputProps()} placeholder="Search" />
+        <button
+          type="button"
+          {...getToggleButtonProps()}
+          aria-label={"toggle menu"}
+          className="sr-only"
+        >
+          &#8595;
+        </button>
+      </div>
+      <ul {...getMenuProps()} className={styles.Results}>
+        {isOpen &&
+          searchResults.map((item, index) => {
+            const previousItemHadDifferentCategory =
+              searchResults[index - 1] &&
+              searchResults[index - 1].category !== item.category;
 
-              const shouldShowCategory =
-                index === 0 || previousItemHadDifferentCategory;
-              return (
-                <li
-                  key={`${item.url}`}
-                  {...getItemProps({ item, index })}
-                  className={styles.Result}
-                  data-is-active={highlightedIndex === index}
-                >
-                  {shouldShowCategory && (
-                    <p className={styles.ResultCategory}>{item.category}</p>
-                  )}
-                  <Link href={item.url} passHref>
-                    <a>
-                      {item.meta.colorToken?.value && (
-                        <div
-                          style={{
-                            width: 32,
-                            height: 32,
-                            background: item.meta.colorToken?.value,
-                            borderRadius: 100,
-                          }}
-                        ></div>
-                      )}
+            const shouldShowCategory =
+              index === 0 || previousItemHadDifferentCategory;
+            return (
+              <li
+                key={`${item.url}`}
+                {...getItemProps({ item, index })}
+                className={styles.Result}
+                data-is-active={highlightedIndex === index}
+              >
+                {shouldShowCategory && (
+                  <p className={styles.ResultCategory}>{item.category}</p>
+                )}
+                <Link href={item.url} passHref>
+                  <a>
+                    {item.meta.colorToken?.value && (
+                      <div
+                        style={{
+                          width: 32,
+                          height: 32,
+                          background: item.meta.colorToken?.value,
+                          borderRadius: 100,
+                        }}
+                      ></div>
+                    )}
 
-                      {item.meta.icon?.fileName && (
-                        <div
-                          style={{
-                            filter: `brightness(0%) saturation(0%)`,
-                            width: 32,
-                            height: 32,
-                            display: "flex",
-                            alignItems: "center",
-                          }}
-                        >
-                          <div>
-                            <Image
-                              src={`/icons/${item.meta.icon.fileName}.svg`}
-                              width={32}
-                              height={32}
-                              layout="fixed"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      )}
-
-                      <div>
-                        <p className={styles.Title}>{item.title}</p>
-                        <p className={styles.Excerpt}>{item.excerpt}</p>
-                      </div>
-
-                      {item.category === "Components" && (
-                        <div className={styles.ComponentPreview}>
+                    {item.meta.icon?.fileName && (
+                      <div
+                        style={{
+                          filter: `brightness(0%) saturation(0%)`,
+                          width: 32,
+                          height: 32,
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        <div>
                           <Image
-                            src={`/component-previews/${slugify(
-                              item.title
-                            )}.png`}
-                            width={525}
-                            height={300}
+                            src={`/icons/${item.meta.icon.fileName}.svg`}
+                            width={32}
+                            height={32}
+                            layout="fixed"
                             alt=""
                           />
                         </div>
-                      )}
-                    </a>
-                  </Link>
-                </li>
-              );
-            })}
-        </ul>
-      </>
+                      </div>
+                    )}
+
+                    <div>
+                      <p className={styles.Title}>{item.title}</p>
+                      <p className={styles.Excerpt}>{item.excerpt}</p>
+                    </div>
+
+                    {item.category === "Components" && (
+                      <div className={styles.ComponentPreview}>
+                        <Image
+                          src={`/component-previews/${slugify(item.title)}.png`}
+                          width={525}
+                          height={300}
+                          alt=""
+                        />
+                      </div>
+                    )}
+                  </a>
+                </Link>
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 }
