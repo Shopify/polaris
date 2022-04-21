d6611e6a46070c695bad1c5bfb923889cf28b116
Marten Bjork <marten.bjork@shopify.com>
2022-04-21 18:59:45 +0200

    Remove unused files

--- polaris.shopify.com/src/utils/lines.ts
+++
@@ -1,7 +0,0 @@
-import { LineConfig } from "../types";
-
-export function createLineAttribute(target: LineConfig): {
-  "data-line": string;
-} {
-  return { "data-line": JSON.stringify(target) };
-}
