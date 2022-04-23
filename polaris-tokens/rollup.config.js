ab911ae56229e5fbc2f84ec3e4813a4821565b18
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-04-23 12:45:02 -0700

    Use Rollup to build ESM, CommonJs, and Browser distributions

---
+++ polaris-tokens/rollup.config.js
@@ -0,0 +1,54 @@
+import path from 'path';
+
+import pkg from './package.json';
+
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+
+const name = 'PolarisTokens';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      format: /** @type {const} */ ('cjs'),
+      dir: path.dirname(pkg.main),
+      preserveModules: true,
+    },
+    {
+      format: /** @type {const} */ ('es'),
+      dir: path.dirname(pkg.module),
+      preserveModules: true,
+    },
+    {
+      format: /** @type {const} */ ('iife'),
+      file: pkg.browser,
+      name,
+
+      // https://rollupjs.org/guide/en/#outputglobals
+      // globals: {},
+    },
+  ],
+  plugins: [
+    // Allows node_modules resolution
+    nodeResolve({extensions}),
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    commonjs(),
+    // Compile TypeScript/JavaScript files
+    babel({
+      extensions,
+      babelHelpers: 'bundled',
+      include: ['src/**/*'],
+    }),
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies ?? {}),
+    ...Object.keys(pkg.peerDependencies ?? {}),
+  ],
+};
