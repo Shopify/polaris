0ffa81345f5b71ae797e7093be03ac2bae4d8423
Romello Goodman <romellogood@gmail.com>
2022-04-26 10:20:14 -0400

    Making the description field on TokenProperties optional (#5611)

--- polaris-tokens/src/token-groups/breakpoints.ts
+++ polaris-tokens/src/token-groups/breakpoints.ts
@@ -1,22 +1,17 @@
 export const breakpoints = {
   'breakpoints-xs': {
     value: '0px',
-    description: '',
   },
   'breakpoints-sm': {
     value: '560px',
-    description: '',
   },
   'breakpoints-md': {
     value: '768px',
-    description: '',
   },
   'breakpoints-lg': {
     value: '1008px',
-    description: '',
   },
   'breakpoints-xl': {
     value: '1776px',
-    description: '',
   },
 };
