1c9949af42b1f3a418d7d0f0188107cf1f4fab50
Sam Rose <sam.rose@shopify.com>
2022-05-04 19:17:19 -0600

    Consolidate tsconfigs to shared configurations
    Closes #5682

---
+++ config/ts/definitions/images.d.ts
@@ -0,0 +1,29 @@
+declare module '*.svg' {
+  const image: any;
+  export default image;
+}
+
+declare module '*.png' {
+  const image: string;
+  export default image;
+}
+
+declare module '*.gif' {
+  const image: string;
+  export default image;
+}
+
+declare module '*.jpeg' {
+  const image: string;
+  export default image;
+}
+
+declare module '*.jpg' {
+  const image: string;
+  export default image;
+}
+
+declare module '*.ico' {
+  const image: string;
+  export default image;
+}
---
+++ config/ts/definitions/styles.d.ts
@@ -0,0 +1,9 @@
+declare module '*.scss' {
+  const classNames: {[key: string]: string};
+  export default classNames;
+}
+
+declare module '*.css' {
+  const classNames: {[key: string]: string};
+  export default classNames;
+}
