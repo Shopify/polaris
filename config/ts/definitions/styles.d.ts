de8c3657299e18c088d09c732e04427c75a1d742
Sam Rose <sam.rose@shopify.com>
2022-05-06 11:17:54 -0600

    Use shared configs from @shopify/typescript-configs

--- config/ts/definitions/styles.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.scss' {
-  const classNames: {[key: string]: string};
-  export default classNames;
-}
-
-declare module '*.css' {
-  const classNames: {[key: string]: string};
-  export default classNames;
-}
