ceb758fc6ba001e936a5ad9e4a8da70c03678f38
Kim Hawthorne <100439213+kimhawthorne-shopify@users.noreply.github.com>
2022-05-09 13:22:25 -0400

    scaffold steps; show/hide next/prev buttons; make Step component (#5743)

--- polaris-react/src/components/InContextLearning/InContextLearning.tsx
+++ polaris-react/src/components/InContextLearning/InContextLearning.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './InContextLearning.scss';
-import {Header} from "./components"
+import {Header, Step} from "./components"
+import {Button} from '../Button';
 
-export function InContextLearning() {
-  return(
-    <div className={styles.InContextLearning}>
-      <div>
-        <Header onClose={() => {}}>Placeholder title</Header>
-      </div>
-    </div>
-  )
+interface Props {
+    children?: React.ReactElement[];
 }
+
+export function InContextLearning({children}: Props) {
+    const hasMultipleSteps = children ? children.length > 0 : false;
+    const [currentStep, setCurrentStep] = useState(0);
+
+    const showPrev = hasMultipleSteps && currentStep > 0;
+    const showNext = hasMultipleSteps && currentStep < (children ? children.length - 1 : false);
+
+    const handleNext = () => {
+        setCurrentStep((currentStep) => currentStep + 1)
+        console.warn('Next clicked');
+    }
+
+    const handlePrev = () => {
+        setCurrentStep((currentStep) => currentStep - 1)
+        console.warn('Prev clicked');
+    }
+
+    return (
+        <>
+          {children && children[currentStep]}
+          <div className={styles.InContextLearning}>
+            <div>
+              <Header onClose={() => {}}>Placeholder title</Header>
+            </div>
+          </div>
+          {showPrev && <Button onClick={handlePrev}>Prev</Button>}
+          {showNext && <Button onClick={handleNext}>Next</Button>}
+        </>
+    );
+}
+
+InContextLearning.Step = Step;
