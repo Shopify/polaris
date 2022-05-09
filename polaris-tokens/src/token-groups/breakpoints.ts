44eae044296c723e030530d0bd7425fc9cbae7da
Alex Page <hi@alexpage.dev>
2022-05-09 14:30:26 -0700

    Fix conflict

--- polaris-tokens/src/token-groups/breakpoints.ts
+++ polaris-tokens/src/token-groups/breakpoints.ts
@@ -3,15 +3,15 @@ export const breakpoints = {
     value: '0px',
   },
   'breakpoints-sm': {
-    value: '490px',
+    value: '560px',
   },
   'breakpoints-md': {
     value: '768px',
   },
   'breakpoints-lg': {
-    value: '1040px',
+    value: '1008px',
   },
   'breakpoints-xl': {
-    value: '1400px',
+    value: '1776px',
   },
 };
