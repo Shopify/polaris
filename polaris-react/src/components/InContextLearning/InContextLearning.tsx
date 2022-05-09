2ddc9ae70f319e92bbdf823e2fd98cdb7139d07c
annmariedoyle-shopify <97552718+annmariedoyle-shopify@users.noreply.github.com>
2022-05-09 16:01:35 -0400

    Add dismiss button and onDismiss prop

--- polaris-react/src/components/InContextLearning/InContextLearning.tsx
+++ polaris-react/src/components/InContextLearning/InContextLearning.tsx
@@ -1,37 +1,57 @@
 import React, {useState} from 'react';
 
-import {Step} from './components';
+import {CancelSmallMinor} from '@shopify/polaris-icons';
+
+import {useI18n} from '../../utilities/i18n';
 import {Button} from '../Button';
+import {Step} from './components';
 
 interface Props {
-    children: React.ReactElement[];
+  children: React.ReactElement[];
+  onDismiss(): void;
 }
 
-export function InContextLearning({children}: Props) {
-    const hasMultipleSteps = children.length > 0;
-    const [currentStep, setCurrentStep] = useState(0);
+export function InContextLearning({children, onDismiss}: Props) {
+  const i18n = useI18n();
 
-    const showPrev = hasMultipleSteps && currentStep > 0;
-    const showNext = hasMultipleSteps && currentStep < (children.length - 1);
+  const hasMultipleSteps = children.length > 0;
+  const [currentStep, setCurrentStep] = useState(0);
 
-    const handleNext = () => {
-        setCurrentStep((currentStep) => currentStep + 1)
-        console.warn('Next clicked');
-    }
+  const showPrev = hasMultipleSteps && currentStep > 0;
+  const showNext = hasMultipleSteps && currentStep < children.length - 1;
 
-    const handlePrev = () => {
-        setCurrentStep((currentStep) => currentStep - 1)
-        console.warn('Prev clicked');
-    }
+  const handleNext = () => {
+    setCurrentStep((currentStep) => currentStep + 1);
+    console.warn('Next clicked');
+  };
 
-    return (
-        <>
-            {children[currentStep]}
+  const handlePrev = () => {
+    setCurrentStep((currentStep) => currentStep - 1);
+    console.warn('Prev clicked');
+  };
 
-            {showPrev && <Button onClick={handlePrev}>Prev</Button>}
-            {showNext && <Button onClick={handleNext}>Next</Button>}
-        </>
-    );
+  const dismissButton = (
+    <div>
+      <Button
+        plain
+        icon={CancelSmallMinor}
+        onClick={onDismiss}
+        accessibilityLabel={i18n.translate(
+          'Polaris.InContextLearning.accessibilityLabel',
+        )}
+      />
+    </div>
+  );
+
+  return (
+    <>
+      {dismissButton}
+      {children[currentStep]}
+
+      {showPrev && <Button onClick={handlePrev}>Prev</Button>}
+      {showNext && <Button onClick={handleNext}>Next</Button>}
+    </>
+  );
 }
 
 InContextLearning.Step = Step;
