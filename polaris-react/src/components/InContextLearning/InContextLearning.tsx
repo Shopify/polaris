318372de1193df0a801059c36092a192bb094812
Chris McLean <chris.w.mclean@shopify.com>
2022-05-09 11:57:28 -0600

    added missing semicolon

--- polaris-react/src/components/InContextLearning/InContextLearning.tsx
+++ polaris-react/src/components/InContextLearning/InContextLearning.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 
 export function InContextLearning() {
   // TODO: Remove me when I have real content to render
-  return <div>InContextLearning placeholder text</div>
+  return <div>InContextLearning placeholder text</div>;
 }
