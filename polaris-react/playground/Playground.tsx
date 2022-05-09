6864da130423ce6dfd6c2625ca480e13608d3282
Kaunain Karmali <kaunain.karmali@shopify.com>
2022-05-09 14:22:18 -0400

    remove component from Playgroud

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import {InContextLearning, Page} from '../src';
+import {Page} from '../src';
 
 export function Playground() {
   return (
     <Page title="Playground">
       {/* Add the code you want to test in here */}
-      <InContextLearning />
     </Page>
   );
 }
