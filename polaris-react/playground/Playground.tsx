b4f6b705bc0651e6e3af3740f638c47efc64b6e8
Kemal Tatar <kemal.tatar@shopify.com>
2022-05-10 12:19:16 +0000

    Playground reverted

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,52 +1,11 @@
-/* eslint-disable no-alert */
 import React from 'react';
 
-import {Page, Badge, Card} from '../src';
+import {Page} from '../src';
 
 export function Playground() {
   return (
-    <Page
-      breadcrumbs={[{content: 'Products', url: '/products'}]}
-      title="3/4 inch Leather pet collar"
-      titleMetadata={<Badge status="success">Paid</Badge>}
-      subtitle="Perfect for any pet"
-      compactTitle
-      primaryAction={{content: 'Save', disabled: true}}
-      secondaryActions={[
-        {
-          content: 'Duplicate',
-          accessibilityLabel: 'Secondary action label',
-          onAction: () => alert('Duplicate action'),
-        },
-        {
-          content: 'View on your store',
-          onAction: () => alert('View on your store action'),
-        },
-      ]}
-      actionGroups={[
-        {
-          title: 'Promote',
-          onClick: (onOpen) => {
-            alert('hey');
-            onOpen();
-          },
-          actions: [
-            {
-              content: 'Share on Facebook',
-              accessibilityLabel: 'Individual action label',
-              onAction: () => alert('Share on Facebook action'),
-            },
-          ],
-        },
-      ]}
-      pagination={{
-        hasPrevious: true,
-        hasNext: true,
-      }}
-    >
-      <Card title="Credit card" sectioned>
-        <p>Credit card information</p>
-      </Card>
+    <Page title="Playground">
+      {/* Add the code you want to test in here */}
     </Page>
   );
 }
