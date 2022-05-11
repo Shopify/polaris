ae4ea907816c33510a874925314c1882d4e258ca
Alex Page <hi@alexpage.dev>
2022-05-10 19:51:24 -0700

    Remove unused utils

--- polaris-react/src/utilities/scroll-into-view.ts
+++
@@ -1,8 +0,0 @@
-export function scrollIntoView(element: HTMLElement, container: HTMLElement) {
-  requestAnimationFrame(() => {
-    if (element) {
-      const offset = element.offsetTop - container.scrollTop;
-      container.scrollBy({top: offset});
-    }
-  });
-}
