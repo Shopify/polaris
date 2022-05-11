ba6bdb3540977d6a9a0ebb444326f6f9aff5aaee
Robin Dykema <robindykema@gmail.com>
2022-05-11 14:19:44 -0500

    Style cleanup and translations (#5778)
    * Style cleanup and translations
    
    * move arrow to psuedo class

--- polaris-react/src/components/InContextLearning/components/Step.tsx
+++ polaris-react/src/components/InContextLearning/components/Step.tsx
@@ -1,36 +1,18 @@
 import React, {useContext, useEffect, useRef} from 'react';
-import { InContextLearningContext } from './InContextLearningContext';
+import {InContextLearningContext} from './InContextLearningContext';
 
 interface Props {
-  children?:React.ReactNode;
-  direction?:
-    | 'top-left'
-    | 'top-right'
-    | 'right-top'
-    | 'right-bottom'
-    | 'bottom-right'
-    | 'bottom-left'
-    | 'left-top'
-    | 'left-bottom'
-    | 'none';
+  children?: React.ReactNode;
   stepIndex: number;
 }
 
-export function Step({
-  children,
-  direction,
-  stepIndex
-}: Props) {
+export function Step({children, stepIndex}: Props) {
   const stepRef = useRef(null);
-  const {registerStep} = useContext(InContextLearningContext); 
-  
+  const {registerStep} = useContext(InContextLearningContext);
+
   useEffect(() => {
-    registerStep(stepIndex, stepRef.current, direction);
+    registerStep(stepIndex, stepRef.current);
   }, []);
 
-  return (
-    <span ref={stepRef}>
-      {children}
-    </span>
-  );
+  return <span ref={stepRef}>{children}</span>;
 }
