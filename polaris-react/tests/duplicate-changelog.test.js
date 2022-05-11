40f0ef126b1d6edea6919a580d11afde88d4b710
Ben Scott <ben.scott@shopify.com>
2022-05-11 16:19:07 -0700

    lint fixes and add test:watch command

--- polaris-react/tests/duplicate-changelog.test.js
+++ polaris-react/tests/duplicate-changelog.test.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 const fs = require('fs-extra');
 
 const rootDir = path.join(__dirname, '..');
--- polaris-react/tests/readme-update-version.test.js
+++ polaris-react/tests/readme-update-version.test.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 const fs = require('fs-extra');
 
 const {semverRegExp, readmes} = require('../scripts/utilities');
