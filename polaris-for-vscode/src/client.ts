1beffd3f3207f60704e79f3a5aced6535cad4220
Sam Rose <sam.rose@shopify.com>
2022-05-12 12:32:38 -0600

    Move VS Code client and server into src folder

--- polaris-for-vscode/src/client.ts
+++ polaris-for-vscode/src/client.ts
@@ -12,9 +12,7 @@ let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
   // The server is implemented in node
-  const serverModule = context.asAbsolutePath(
-    path.join('server', 'dist', 'server.js'),
-  );
+  const serverModule = context.asAbsolutePath(path.join('dist', 'server.js'));
   // The debug options for the server
   // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
   const debugOptions = {execArgv: ['--nolazy', '--inspect=6009']};
