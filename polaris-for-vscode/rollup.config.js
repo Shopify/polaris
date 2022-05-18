587b1ea152d7e572fddf16fb1151b8387f9fc5fd
Sam Rose <sam.rose@shopify.com>
2022-05-18 06:53:07 -0600

    Add bundler devDependencies to VS Code package.json

--- polaris-for-vscode/rollup.config.js
+++ polaris-for-vscode/rollup.config.js
@@ -2,8 +2,6 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 
-import pkg from './package.json';
-
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 /**
