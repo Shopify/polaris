4d6568792fcc13c2a225d9e9d57aa44d813d855a
Tim Layton <tmlayton@users.noreply.github.com>
2022-05-19 20:41:21 -0700

    use the hook with the component

--- polaris-react/src/components/PerformanceBenchmark/PerformanceBenchmark.tsx
+++ polaris-react/src/components/PerformanceBenchmark/PerformanceBenchmark.tsx
@@ -1,48 +1,12 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import React, {PropsWithChildren, useEffect} from 'react';
+import React, {PropsWithChildren} from 'react';
 
-import {useFeatures} from '../../utilities/features';
-import {useIsMountedRef} from '../../utilities/use-is-mounted-ref';
-import {Mark, PolarisEmoji} from '../../utilities/use-performance-benchmark';
-
-interface PerformanceBenchmarkProps {
-  name: string;
-}
+import {usePerformanceBenchmark} from '../../utilities/use-performance-benchmark';
 
 export function PerformanceBenchmark({
   name,
   children,
-}: PropsWithChildren<PerformanceBenchmarkProps>) {
-  const {enablePerformanceBenchmarking} = useFeatures();
-  if (!enablePerformanceBenchmarking) {
-    return <>{children}</>;
-  }
-
-  const {current: isMounted} = useIsMountedRef();
-
-  if (!isMounted) {
-    window.performance.mark(`${name}${Mark.MountStart}`);
-  } else {
-    window.performance.mark(`${name}${Mark.UpdateStart}`);
-  }
-
-  useEffect(() => {
-    if (!isMounted) {
-      window.performance.mark(`${name}${Mark.MountEnd}`);
-      window.performance.measure(
-        `${PolarisEmoji} ${name}`,
-        `${name}${Mark.MountStart}`,
-        `${name}${Mark.MountEnd}`,
-      );
-    } else {
-      window.performance.mark(`${name}UpdateEnd`);
-      window.performance.measure(
-        `${PolarisEmoji} ${name} (${Mark.Update})`,
-        `${name}${Mark.UpdateStart}`,
-        `${name}${Mark.UpdateEnd}`,
-      );
-    }
-  });
+}: PropsWithChildren<{name: string}>) {
+  usePerformanceBenchmark(name);
 
   return <>{children}</>;
 }
