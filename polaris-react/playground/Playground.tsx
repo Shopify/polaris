29a605a167def97d363773d31199ccedff30e2af
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-05-19 21:22:52 -0700

    feat: Improve useEventListener types

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
 import {Page} from '../src';
+import {useEventListener} from '../src/utilities/use-event-listener';
 
 export function Playground() {
+  const ref = React.useRef<HTMLParagraphElement>(null);
+
+  const [coords, setCoords] = React.useState({x: 0, y: 0});
+
+  useEventListener('mousemove', (event) =>
+    setCoords({
+      x: event.clientX,
+      y: event.clientY,
+    }),
+  );
+
+  useEventListener('click', (event) => event);
+  useEventListener('keydown', (event) => event);
+  useEventListener('copy', (event) => event);
+  useEventListener('copy', (event) => event, document);
+  useEventListener('copy', (event) => event, ref.current);
+
   return (
     <Page title="Playground">
-      {/* Add the code you want to test in here */}
+      <p ref={ref}>Mouse X: {coords.x}</p>
+      <p>Mouse Y: {coords.y}</p>
     </Page>
   );
 }
--- polaris-react/src/utilities/use-event-listener.ts
+++ polaris-react/src/utilities/use-event-listener.ts
@@ -1,27 +1,78 @@
 import {useEffect, useRef} from 'react';
 
-type ElementType = Window | Document | HTMLElement;
+import {useIsomorphicLayoutEffect} from './use-isomorphic-layout-effect';
 
-export function useEventListener(
-  event: string,
-  handler: (event: Event) => void,
-  element: ElementType = window,
+/**
+ * Acceptable target elements for `useEventListener`.
+ */
+type UseEventListenerTarget = Window | Document | HTMLElement;
+
+/**
+ * Extracts a (lib.dom.ts) EventMap for a given target element.
+ */
+type ExtractEventMap<Target> = Target extends Window
+  ? WindowEventMap
+  : HTMLElementEventMap;
+
+/**
+ * Extracts all event names for a given target element.
+ */
+type ExtractEventName<Target> = keyof ExtractEventMap<Target>;
+
+/**
+ * Extracts the `event` object for a given event type.
+ */
+type ExtractEvent<
+  Target,
+  EventName extends ExtractEventName<Target>,
+> = ExtractEventMap<Target>[EventName];
+
+/**
+ * React hook encapsulating the boilerplate logic for adding and removing event listeners.
+ */
+export function useEventListener<
+  TargetEventName extends ExtractEventName<Target>,
+  TargetEvent extends ExtractEvent<Target, TargetEventName>,
+  Target extends UseEventListenerTarget = Window,
+>(
+  eventName: TargetEventName,
+  handler: (event: TargetEvent) => void,
+  target?: null | Target,
   options?: AddEventListenerOptions,
-) {
+): void {
   const handlerRef = useRef(handler);
   const optionsRef = useRef(options);
 
-  useEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     handlerRef.current = handler;
+  }, [handler]);
+
+  useIsomorphicLayoutEffect(() => {
     optionsRef.current = options;
-  }, [handler, options]);
+  }, [options]);
 
   useEffect(() => {
-    const handler = handlerRef.current;
-    const options = optionsRef.current;
+    const targetElement = target || window;
 
-    element.addEventListener(event, handler, options);
+    if (
+      !(
+        Boolean(targetElement) &&
+        targetElement.addEventListener &&
+        typeof eventName === 'string'
+      )
+    ) {
+      return;
+    }
 
-    return () => element.removeEventListener(event, handler, options);
-  }, [event, element]);
+    const eventOptions = optionsRef.current;
+
+    const eventListener = (event: Event) =>
+      handlerRef.current(event as unknown as TargetEvent);
+
+    targetElement.addEventListener(eventName, eventListener, eventOptions);
+
+    return () => {
+      targetElement.removeEventListener(eventName, eventListener, eventOptions);
+    };
+  }, [eventName, target]);
 }
