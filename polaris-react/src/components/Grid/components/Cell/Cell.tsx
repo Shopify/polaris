db4dca7441208bf89301d06bedcf622a6997e23b
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-05-25 15:35:52 -0700

    fix: Update Columns types to match number of columns

--- polaris-react/src/components/Grid/components/Cell/Cell.tsx
+++ polaris-react/src/components/Grid/components/Cell/Cell.tsx
@@ -5,11 +5,11 @@ import styles from '../../Grid.scss';
 
 interface Columns {
   /** Number of columns the section should span on extra small screens */
-  xs?: 1 | 2;
+  xs?: 1 | 2 | 3 | 4 | 5 | 6;
   /** Number of columns the section should span on small screens */
-  sm?: 1 | 2 | 3 | 4;
+  sm?: 1 | 2 | 3 | 4 | 5 | 6;
   /** Number of columns the section should span on medium screens */
-  md?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  md?: 1 | 2 | 3 | 4 | 5 | 6;
   /** Number of columns the section should span on large screens */
   lg?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   /** Number of columns the section should span on extra large screens */
