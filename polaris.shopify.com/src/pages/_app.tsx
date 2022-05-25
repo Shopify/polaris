21cfd1dbea302e2160f4720de2f770b51153399d
Gui Santos <guilherme.santos@shopify.com>
2022-05-25 14:47:09 +0200

    feat: add meta tag to scale pages correctly

--- polaris.shopify.com/src/pages/_app.tsx
+++ polaris.shopify.com/src/pages/_app.tsx
@@ -1,30 +1,35 @@
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 // import "@shopify/polaris/build/esm/styles.css";
+import Head from "next/head";
 import "../styles/globals.scss";
 import Page from "../components/Page";
 import { useRouter } from "next/router";
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const router = useRouter();
   const isPolaris = router.asPath.startsWith("/generated-examples");
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-      <Page skipHeaderAndFooter={isPolaris}>
-        <div style={{ background: isPolaris ? "#fafafa" : "unset" }}>
-          {getLayout(<Component {...pageProps} />)}
-        </div>
-      </Page>
+    <Page skipHeaderAndFooter={isPolaris}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <div style={{ background: isPolaris ? "#fafafa" : "unset" }}>
+        {getLayout(<Component {...pageProps} />)}
+      </div>
+    </Page>
   );
 }
 
