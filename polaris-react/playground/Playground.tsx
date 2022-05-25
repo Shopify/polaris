ebb0ce7c62afd4a5d437b827104a9477bd103856
Tal Ben Shoam <tal.benshoam@shopify.com>
2022-05-25 18:08:34 +0000

    revert playground file

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -5,7 +5,6 @@ import {Button, Page} from '../src';
 export function Playground() {
   return (
     <Page title="Playground">
-      <Button onPointerDown={() => console.log('test')}>test</Button>
       {/* Add the code you want to test in here */}
     </Page>
   );
