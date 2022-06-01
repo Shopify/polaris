4797d6aa56b2cbc6af56eed021dff42ee46eb4f2
Yuraima Estevez <yurm04@gmail.com>
2022-06-01 18:35:30 -0500

    add left nav layout components

---
+++ polaris.shopify.com/src/components/LeftNavLayout/LeftNavLayout.tsx
@@ -0,0 +1,30 @@
+import { className, slugify } from "../../utils/various";
+import MaxPageWidthDiv from "../MaxPageWidthDiv";
+import Nav, { NavItem } from "../Nav/Nav";
+import styles from "./LeftNavLayout.module.scss";
+
+interface Props {
+  navItems?: NavItem[];
+  children: React.ReactNode;
+}
+
+const LeftNavLayout = ({
+  navItems,
+  children
+}: Props) => {
+  return (
+    <MaxPageWidthDiv
+      className={className(
+        styles.LeftNavLayout,
+      )}
+    >
+      <div className={styles.Nav}>
+        {navItems && <Nav navItems={navItems} />}
+      </div>
+      {children}
+    </MaxPageWidthDiv>
+  );
+}
+
+
+export default LeftNavLayout;
