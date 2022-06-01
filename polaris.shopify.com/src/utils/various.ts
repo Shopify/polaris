7408c8222bb947bf134b69c226c2ca6b574cc6e9
Marten Bjork <marten.bjork@shopify.com>
2022-06-01 14:34:44 +0200

    Remove commented code

--- polaris.shopify.com/src/utils/various.ts
+++ polaris.shopify.com/src/utils/various.ts
@@ -14,22 +14,6 @@ export const getComponentCategories = (): string[] => {
 };
 
 export const getComponentNav = (): NavItem[] => {
-  const categories = getComponentCategories();
-
-  // const navItems: NavItem[] = [
-  //   ...categories.map((category) => ({
-  //     title: category,
-  //     children: components
-  //       .filter((component) => component.frontMatter.category === category)
-  //       .map((component) => ({
-  //         title: component.frontMatter.name,
-  //         url: `/components/${slugify(category)}/${slugify(
-  //           component.frontMatter.name
-  //         )}`,
-  //       })),
-  //   })),
-  // ];
-
   const navItems: NavItem[] = [
     {
       title: "All",
