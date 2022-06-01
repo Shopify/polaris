94669ee774de8fa8ad784c67b95c63e2a10d0d22
Marten Bjork <marten.bjork@shopify.com>
2022-06-01 16:17:32 +0200

    Add some motion to the front page

--- polaris.shopify.com/src/components/HomePage/HomePage.tsx
+++ polaris.shopify.com/src/components/HomePage/HomePage.tsx
@@ -6,13 +6,29 @@ interface Props {}
 function HomePage({}: Props) {
   return (
     <div className={styles.HomePage}>
-      <MaxPageWidthDiv>
-        <h1>A Design System built for Commerce</h1>
-        <p>
-          Polaris lets you build experiences for million of merchants. Be a part
-          of changing commerce. For everyone, everywhere.
-        </p>
+      <div className={styles.Intro}>
+        <MaxPageWidthDiv>
+          <h1>A design system built for commerce</h1>
+          <p>
+            Polaris lets you build experiences for million of merchants. Be a
+            part of changing commerce. For everyone, everywhere.
+          </p>
+        </MaxPageWidthDiv>
+      </div>
 
+      <div className={styles.HeroGraphic}>
+        <div className={styles.Components}>
+          <div className={styles.Component}></div>
+          <div className={styles.Component}></div>
+          <div className={styles.Component}></div>
+          <div className={styles.Component}></div>
+          <div className={styles.Component}></div>
+          <div className={styles.Component}></div>
+          <div className={styles.Component}></div>
+        </div>
+      </div>
+
+      <MaxPageWidthDiv>
         <h2>Contributors</h2>
         {[...Array(32)].map((_, n) => (
           <div key={n}></div>
