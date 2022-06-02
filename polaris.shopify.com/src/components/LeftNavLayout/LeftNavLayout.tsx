4c32c19a41fc3a9d1c4250223072952ff1ef1c28
Yuraima Estevez <yurm04@gmail.com>
2022-06-02 09:23:11 -0500

    removing unused imports

--- polaris.shopify.com/src/components/LeftNavLayout/LeftNavLayout.tsx
+++ polaris.shopify.com/src/components/LeftNavLayout/LeftNavLayout.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useTOC } from "../../utils/hooks";
 import { className, slugify } from "../../utils/various";
-import Longform from "../Longform";
-import Markdown from "../Markdown";
 import MaxPageWidthDiv from "../MaxPageWidthDiv";
 import Nav, { NavItem } from "../Nav/Nav";
 import styles from "./LeftNavLayout.module.scss";
