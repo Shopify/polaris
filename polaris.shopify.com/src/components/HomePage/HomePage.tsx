386e8068581798b9e3c66841d4ef4f050fc583ad
Marten Bjork <marten.bjork@shopify.com>
2022-06-03 10:36:06 +0200

    Start creating some dummy components

--- polaris.shopify.com/src/components/HomePage/HomePage.tsx
+++ polaris.shopify.com/src/components/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import { className } from "../../utils/various";
 import MaxPageWidthDiv from "../MaxPageWidthDiv";
 import styles from "./HomePage.module.scss";
 
@@ -10,14 +11,60 @@ function HomePage({}: Props) {
         <MaxPageWidthDiv>
           <h1>A design system built for commerce</h1>
           <p>
-            Polaris lets you build experiences for million of merchants. Be a
+            Polaris lets you build experiences for millions of merchants. Be a
             part of changing commerce. For everyone, everywhere.
           </p>
         </MaxPageWidthDiv>
       </div>
 
       <div className={styles.HeroGraphic}>
-        <div className={styles.Components}>
+        <div className={className(styles.Set, styles.Components)}>
+          {[...Array(5)].map((_, n) => {
+            return (
+              <div
+                key={n}
+                className={className(styles.Component, styles.HomeCard)}
+              >
+                <PolarisCard>
+                  <PolarisCard.Section>Reach more shoppers</PolarisCard.Section>
+                  <PolarisCard.Section>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                  </PolarisCard.Section>
+                  <PolarisCard.Section>
+                    <PolarisButton>Primary action</PolarisButton>
+                  </PolarisCard.Section>
+                </PolarisCard>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className={className(styles.Set, styles.Tokens)}>
+          {[...Array(100)].map((_, n) => {
+            const rows = 4;
+            const columns = 20;
+            const row = Math.floor(n / columns);
+            const hues = [-1, 215, 165, 145, 50];
+            const column = n % columns;
+            const backgroundColor = `hsl(${hues[row]}deg, ${
+              hues[row] === -1 ? 0 : 70
+            }%, ${column * 5}%)`;
+            return (
+              <div
+                key={n}
+                style={{ backgroundColor }}
+                className={styles.Token}
+              ></div>
+            );
+          })}
+        </div>
+
+        <div className={styles.Icons}>
           <div className={styles.Component}></div>
           <div className={styles.Component}></div>
           <div className={styles.Component}></div>
@@ -59,5 +106,19 @@ function HomePage({}: Props) {
     </div>
   );
 }
+
+function PolarisCard({ children }: { children: React.ReactNode }) {
+  return <div className={styles.PolarisCard}>{children}</div>;
+}
+
+function PolarisCardSection({ children }: { children: React.ReactNode }) {
+  return <div className={styles.PolarisCardSection}>{children}</div>;
+}
+
+function PolarisButton({ children }: { children: React.ReactNode }) {
+  return <div className={styles.PolarisButton}>{children}</div>;
+}
+
+PolarisCard.Section = PolarisCardSection;
 
 export default HomePage;
