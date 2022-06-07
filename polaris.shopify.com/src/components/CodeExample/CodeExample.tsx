fb9bb3b40d11891ee9013911be500e5b255a450b
Marten Bjork <marten.bjork@shopify.com>
2022-06-07 23:22:46 +0200

    [polaris.shopify.com reboot] Tweak design of icons detail view (#6061)
    * Tweak design
    
    * Add better class names
    
    * Add changeset

--- polaris.shopify.com/src/components/CodeExample/CodeExample.tsx
+++ polaris.shopify.com/src/components/CodeExample/CodeExample.tsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
+import Tooltip from "../Tooltip";
+import iconCancel from "../../../public/icon-cancel.svg";
+import iconClipboard from "../../../public/icon-clipboard.svg";
 import styles from "./CodeExample.module.scss";
+import { useCopyToClipboard } from "../../utils/hooks";
 
 interface Props {
   language: "terminal" | "typescript";
@@ -7,10 +12,28 @@ interface Props {
 }
 
 function CodeExample({ title, children }: Props) {
+  const [copy, didJustCopy] = useCopyToClipboard(children);
   const lines = children.split("\n");
+
   return (
     <div className={styles.CodeExample}>
-      <div className={styles.Title}>{title}</div>
+      <div className={styles.TitleBar}>
+        <div className={styles.Title}>{title}</div>
+        <Tooltip
+          ariaLabel="Copy to clipboard"
+          placement="top"
+          renderContent={() => (
+            <div className={styles.IconToolTip}>
+              <p>{didJustCopy ? "Copied" : "Copy"}</p>
+            </div>
+          )}
+        >
+          <button type="button" onClick={copy} className={styles.CopyButton}>
+            <Image src={iconClipboard} alt="Copy" width={19} height={19} />
+          </button>
+        </Tooltip>
+      </div>
+
       <div className={styles.Code}>
         {lines.map((line, i) => (
           <div className={styles.Line} key={line}>
