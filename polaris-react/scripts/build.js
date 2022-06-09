77ff4130599b854593ecc578031c1c0d86d8562d
Alex Page <hi@alexpage.dev>
2022-06-09 12:13:19 -0700

    Update build.js

--- polaris-react/scripts/build.js
+++ polaris-react/scripts/build.js
@@ -16,9 +16,5 @@ run('yarn run downlevel-dts "./build/ts/latest" "./build/ts/3.4"');
 
 run('yarn run copyfiles "./src/**/*.md" "./build/docs" --up=1');
 
-run('yarn run downlevel-dts "build/ts/latest" "build/ts/3.4"');
-
-run('yarn run copyfiles "./src/**/*.md" "./build/docs" --up=1');
-
 // Run build validation tests to ensure build content is as we expect
 run(`node scripts/build-validate.js`);
