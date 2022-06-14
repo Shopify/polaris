5dd94597ad142686ed49fdc87225ed2760e74756
Gui Santos <guilherme.santos@shopify.com>
2022-06-14 12:19:56 +0200

    fix: forward ref of the button component

--- polaris.shopify.com/src/components/Button/Button.tsx
+++ polaris.shopify.com/src/components/Button/Button.tsx
@@ -15,22 +15,26 @@ interface LinkButtonProps extends Props, PropsWithChildren<LinkProps> {
   download?: boolean;
 }
 
-function Button({ small, pill, primary, children, ...rest }: ButtonProps) {
-  return (
-    <button
-      className={className(
-        styles.Button,
-        small && styles.small,
-        pill && styles.pill,
-        primary && styles.primary
-      )}
-      {...rest}
-      type="button"
-    >
-      {children}
-    </button>
-  );
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ small, pill, primary, children, ...rest }, ref) => {
+    return (
+      <button
+        className={className(
+          styles.Button,
+          small && styles.small,
+          pill && styles.pill,
+          primary && styles.primary
+        )}
+        {...rest}
+        type="button"
+        ref={ref}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+Button.displayName = "Button";
 
 export function LinkButton({
   small,
