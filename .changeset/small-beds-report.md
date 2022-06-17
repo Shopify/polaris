d75dff18603fffe5881b2992cd48e6af606806d3
Chloe Rice <chloerice@users.noreply.github.com>
2022-06-17 06:43:36 -0400

    [TextField] Revert #6143 and make expecting onBlur event arg optional (#6224)
    * [TextField] Reverts onFocus event arg not being optional
    
    * [TextField] Make onBlur event optional

---
+++ .changeset/small-beds-report.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris': minor
+---
+
+Reverted #6143 and made expecting `TextField` `onBlur` `event` arg optional
--- polaris-react/src/components/TextField/TextField.tsx
+++ polaris-react/src/components/TextField/TextField.tsx
@@ -159,9 +159,9 @@ interface NonMutuallyExclusiveProps {
   /** Callback fired when value is changed */
   onChange?(value: string, id: string): void;
   /** Callback fired when input is focused */
-  onFocus?: (event: React.FocusEvent) => void;
+  onFocus?: (event?: React.FocusEvent) => void;
   /** Callback fired when input is blurred */
-  onBlur?(event: React.FocusEvent): void;
+  onBlur?(event?: React.FocusEvent): void;
 }
 
 export type MutuallyExclusiveSelectionProps =
