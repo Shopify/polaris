df34e3a90bddd318db623486fe48f619207634b3
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-06-20 15:29:22 -0400

    Add ability to track page views

--- polaris.shopify.com/src/pages/_app.tsx
+++ polaris.shopify.com/src/pages/_app.tsx
@@ -1,10 +1,25 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import "../styles/globals.scss";
 import Page from "../components/Page";
+import * as ga from "../../lib/ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Page>
       <Head>
