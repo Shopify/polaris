a56c21d4df7b9aec5b20a49da5fe990e4cdae608
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-06-21 09:29:20 -0400

    Update ga env variable

--- polaris.shopify.com/src/pages/_app.tsx
+++ polaris.shopify.com/src/pages/_app.tsx
@@ -27,7 +27,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script
         async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${ga.GA_TRACKING_ID}`}
       />
       <Script
         id="gtag-init"
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            gtag('config', '${ga.GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `,
