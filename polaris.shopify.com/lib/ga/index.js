2bdb48ff996ee214aa2a525237cb6b1f665c71f5
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-06-21 09:30:50 -0400

    Update lib/ga syntax & styling

--- polaris.shopify.com/lib/ga/index.js
+++ polaris.shopify.com/lib/ga/index.js
@@ -1,9 +1,15 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
+
 export const pageview = (url) => {
-    window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-      page_path: url,
+    window.gtag('config', GA_TRACKING_ID, {
+        page_path: url,
     })
-  }
+}
   
-export const event = ({ action, params }) => {
-    window.gtag('event', action, params)
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
 }
