b4b59d2d89743473f28a1ad33c5f4dc5663349b1
Marten Bjork <marten.bjork@shopify.com>
2022-06-22 21:05:25 +0200

    Fix card hover (#6315)
    * Fix card hover
    
    * Add changeset

--- polaris.shopify.com/src/components/ThemeProvider/ThemeProvider.tsx
+++ polaris.shopify.com/src/components/ThemeProvider/ThemeProvider.tsx
@@ -66,7 +66,7 @@ const themes: {
     "--border": "1px solid var(--border-color)",
     "--card-shadow": "inset 0 0 0 1px rgb(255 255 255 / 12%)",
     "--card-shadow-hover":
-      "0 4px 7px rgba(0, 0, 0, 0.1), 0 0 0 0.5px var(--border-color)",
+      "0 4px 7px rgba(255,255,255, 0.1), inset 0 0 0 1px rgb(255 255 255 / 12%)",
     "--decorative-1": "#224700",
     "--decorative-2": "#603900",
     "--decorative-3": "#601249",
