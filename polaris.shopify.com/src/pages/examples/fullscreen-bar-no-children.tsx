1a10d61cbb10d23a77d67cf6b1e7ece4291f85bf
Marten Bjork <marten.bjork@shopify.com>
2022-06-27 17:21:27 +0200

    Fix another full screen bar example

--- polaris.shopify.com/src/pages/examples/fullscreen-bar-no-children.tsx
+++ polaris.shopify.com/src/pages/examples/fullscreen-bar-no-children.tsx
@@ -24,4 +24,8 @@ function FullscreenBarExample() {
   );
 }
 
-export default withPolarisExample(FullscreenBarExample);
+export default withPolarisExample(FullscreenBarExample, {
+  width: "full",
+  alignItems: "flex-start",
+  padding: "none",
+});
