50aa75a47902c98cbdf2b309f8a39260b5aa769f
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-06-29 12:06:52 -0700

    Remove stylelintignore in favor of ignoreFiles config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,6 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
   extends: ['@shopify/stylelint-plugin/prettier', './stylelint-polaris'],
-  ignoreFiles: [
-    'polaris-tokens/dist/**/*.{css,scss}',
-    // TODO: Remove and address stylelint errors
-    'polaris.shopify.com/**/*.{css,scss}',
-  ],
   // Disabling @shopify/stylelint-plugin/configs/core no-unknown-animations as styelint
   // is not aware of global Polaris keyframes
   // TODO: create custom plugin to ensure animation-names match Polaris keyframe names
@@ -22,5 +17,17 @@ module.exports = {
         './stylelint-polaris/configs/internal',
       ],
     },
+  ],
+  ignoreFiles: [
+    '.next/**/*.{css,scss}',
+    'node_modules/**/*.{css,scss}',
+    'dist/**/*.{css,scss}',
+    'documentation/guides/legacy-polaris-v8-public-api.scss',
+    'polaris-react/build/**/*.{css,scss}',
+    'polaris-react/build-internal/**/*.{css,scss}',
+    'polaris-tokens/dist/**/*.{css,scss}',
+    'stylelint-polaris/tests/**/*.{css,scss}',
+    // TODO: Remove and address stylelint errors
+    'polaris.shopify.com/**/*.{css,scss}',
   ],
 };
