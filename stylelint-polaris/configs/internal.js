71130808a9adb1ec39fa735e4cd590b5cae0d72c
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-06-29 13:55:31 -0700

    Update internal config to warn on media-queries-allowed-list failures

--- stylelint-polaris/configs/internal.js
+++ stylelint-polaris/configs/internal.js
@@ -45,30 +45,34 @@ module.exports = {
         ],
       },
     },
-    [mediaQueriesAllowedList]: {
-      // Allowed media types and media conditions
-      // https://www.w3.org/TR/mediaqueries-5/#media
-      allowedMedia: [
-        'print',
-        'screen',
-        '-ms-high-contrast',
-        'forced-colors',
-        '$p-breakpoints-xs-up',
-        '$p-breakpoints-xs-down',
-        '$p-breakpoints-xs-only',
-        '$p-breakpoints-sm-up',
-        '$p-breakpoints-sm-down',
-        '$p-breakpoints-sm-only',
-        '$p-breakpoints-md-up',
-        '$p-breakpoints-md-down',
-        '$p-breakpoints-md-only',
-        '$p-breakpoints-lg-up',
-        '$p-breakpoints-lg-up',
-        '$p-breakpoints-lg-down',
-        '$p-breakpoints-xl-down',
-        '$p-breakpoints-xl-only',
-        '$p-breakpoints-xl-only',
-      ],
-    },
+    [mediaQueriesAllowedList]: [
+      {
+        // Allowed media types and media conditions
+        // https://www.w3.org/TR/mediaqueries-5/#media
+        allowedMedia: [
+          'print',
+          'screen',
+          '-ms-high-contrast',
+          'forced-colors',
+          // TODO: Add utility to @shopify/polaris-tokens to getMediaConditionNames
+          '$p-breakpoints-xs-up',
+          '$p-breakpoints-xs-down',
+          '$p-breakpoints-xs-only',
+          '$p-breakpoints-sm-up',
+          '$p-breakpoints-sm-down',
+          '$p-breakpoints-sm-only',
+          '$p-breakpoints-md-up',
+          '$p-breakpoints-md-down',
+          '$p-breakpoints-md-only',
+          '$p-breakpoints-lg-up',
+          '$p-breakpoints-lg-up',
+          '$p-breakpoints-lg-down',
+          '$p-breakpoints-xl-down',
+          '$p-breakpoints-xl-only',
+          '$p-breakpoints-xl-only',
+        ],
+      },
+      {severity: 'warning'},
+    ],
   },
 };
