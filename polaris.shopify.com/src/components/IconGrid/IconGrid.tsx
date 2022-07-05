0d31c1352e73b6400e83063fa4a3f69299052643
Alex Page <hi@alexpage.dev>
2022-07-04 17:42:25 -0700

    Fix up css

--- polaris.shopify.com/src/components/IconGrid/IconGrid.tsx
+++ polaris.shopify.com/src/components/IconGrid/IconGrid.tsx
@@ -15,40 +15,38 @@ function IconGrid({ title, icons, activeIcon, query = "" }: Props) {
   return (
     <>
       {title ? <h2 className={styles.SectionHeading}>{title}</h2> : null}
-      <div className={styles.IconGrid}>
-        <ul className={styles.IconGridInner}>
-          {Object.values(icons).map(({ id, description, name }) => (
-            <li key={id}>
-              <Link
-                href={{
-                  pathname: "/icons",
-                  query: {
-                    icon: id,
-                    ...(query === "" ? {} : { q: query }),
-                  },
-                }}
-                scroll={false}
+      <ul className={styles.IconGrid}>
+        {Object.values(icons).map(({ id, description, name }) => (
+          <li key={id}>
+            <Link
+              href={{
+                pathname: "/icons",
+                query: {
+                  icon: id,
+                  ...(query === "" ? {} : { q: query }),
+                },
+              }}
+              scroll={false}
+            >
+              <a
+                className={className(
+                  styles.Icon,
+                  activeIcon === id && styles.isSelected
+                )}
               >
-                <a
-                  className={className(
-                    styles.Icon,
-                    activeIcon === id && styles.isSelected
-                  )}
-                >
-                  <Image
-                    src={`/icons/${id}.svg`}
-                    alt={description}
-                    width={20}
-                    height={20}
-                    fadeIn={false}
-                  />
-                  <p>{name}</p>
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+                <Image
+                  src={`/icons/${id}.svg`}
+                  alt={description}
+                  width={20}
+                  height={20}
+                  fadeIn={false}
+                />
+                <p>{name}</p>
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
