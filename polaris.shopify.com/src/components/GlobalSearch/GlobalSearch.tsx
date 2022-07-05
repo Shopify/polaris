764d12bf735be95065f938f4d5557d2e6ca4a1df
Marten Bjork <marten.bjork@shopify.com>
2022-07-05 10:59:16 +0200

    Better handling of whitespace in global search

--- polaris.shopify.com/src/components/GlobalSearch/GlobalSearch.tsx
+++ polaris.shopify.com/src/components/GlobalSearch/GlobalSearch.tsx
@@ -83,7 +83,7 @@ function GlobalSearch({}: Props) {
   }, []);
 
   useEffect(() => {
-    setSearchResults(search(searchTerm));
+    setSearchResults(search(searchTerm.trim()));
     setActiveDescendant(0);
     scrollToTop();
   }, [searchTerm]);
