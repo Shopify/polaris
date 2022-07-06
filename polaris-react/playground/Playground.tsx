f4f67fd14d4cbcd2a665e8e477ba17c3db0eb0f4
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-07-06 13:27:01 -0700

    chore: Revert playground demo

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 
 import {Page} from '../src';
-import {useBreakpoints} from '../src/utilities/breakpoints';
 
 export function Playground() {
-  const breakpoints = useBreakpoints();
-
   return (
     <Page title="Playground">
-      {Object.entries(breakpoints).map(([name, value]) => (
-        <div key={name} style={{background: value ? 'lightsteelblue' : 'pink'}}>
-          <p>
-            {name}: {value.toString()}
-          </p>
-        </div>
-      ))}
+      {/* Add the code you want to test in here */}
     </Page>
   );
 }
