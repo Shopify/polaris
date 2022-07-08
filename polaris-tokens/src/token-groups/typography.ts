8b354b3ef610e7ba09379ad6a906e13052cdc930
aveline <aveline@users.noreply.github.com>
2022-07-08 11:45:38 -0700

    Update type scale and font size tokens

--- polaris-tokens/src/token-groups/typography.ts
+++ polaris-tokens/src/token-groups/typography.ts
@@ -7,44 +7,29 @@ export const typography = {
     value:
       "ui-monospace, SFMono-Regular, 'SF Mono', Consolas, 'Liberation Mono', Menlo, monospace",
   },
-  'font-size-1': {
+  'font-fontsize-75': {
     value: '12px',
   },
-  'font-size-2': {
-    value: '13px',
-  },
-  'font-size-3': {
+  'font-fontsize-100': {
     value: '14px',
   },
-  'font-size-4': {
-    value: '15px',
-  },
-  'font-size-5': {
+  'font-fontsize-200': {
     value: '16px',
   },
-  'font-size-6': {
-    value: '17px',
-  },
-  'font-size-7': {
+  'font-fontsize-300': {
     value: '20px',
   },
-  'font-size-8': {
-    value: '21px',
-  },
-  'font-size-9': {
+  'font-fontsize-400': {
     value: '24px',
   },
-  'font-size-10': {
-    value: '26px',
-  },
-  'font-size-11': {
-    value: '27px',
-  },
-  'font-size-12': {
+  'font-fontsize-500': {
     value: '28px',
   },
-  'font-size-13': {
-    value: '42px',
+  'font-fontsize-600': {
+    value: '32px',
+  },
+  'font-fontsize-700': {
+    value: '40px',
   },
   'font-weight-regular': {
     value: '400',
