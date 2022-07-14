d9f116ca8afe4b0c54e0610164144593d6117b6f
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-07-14 15:47:40 -0400

    add label props

--- polaris-react/src/components/Text/Text.tsx
+++ polaris-react/src/components/Text/Text.tsx
@@ -4,7 +4,7 @@ import {classNames} from '../../utilities/css';
 
 import styles from './Text.scss';
 
-type Element = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
+type Element = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'label' | 'p' | 'span';
 
 type Variant =
   | 'displaySm'
@@ -35,6 +35,10 @@ export interface TextProps {
   color?: Color;
   /** Adjust weight of text */
   fontWeight?: FontWeight;
+  /** A unique identifier for label text */
+  id?: string;
+  /** Visual required indicator for label text*/
+  requiredIndicator?: boolean;
   /** Truncate text overflow with ellipsis */
   truncate?: boolean;
   /** Typographic style of text */
@@ -43,17 +47,28 @@ export interface TextProps {
   visuallyHidden?: boolean;
 }
 
+export function labelID(id: string, as: Element) {
+  return as === 'label' ? `${id}Label` : undefined;
+}
+
 export const Text = ({
   alignment = 'inherit',
   as,
   children,
   color,
   fontWeight = 'regular',
+  id,
+  requiredIndicator,
   truncate = false,
   variant,
   visuallyHidden = false,
 }: TextProps) => {
   const Component = as || (visuallyHidden ? 'span' : 'p');
+
+  const labelProps = {
+    ...(id &&
+      labelID(id, as) !== undefined && {id: labelID(id, as), htmlFor: id}),
+  };
 
   const className = classNames(
     styles.root,
@@ -64,7 +79,13 @@ export const Text = ({
     color && styles[color],
     truncate && styles.truncate,
     visuallyHidden && styles.visuallyHidden,
+    as == 'label' && styles['subdued'],
+    as == 'label' && requiredIndicator && styles.RequiredIndicator,
   );
 
-  return <Component className={className}>{children}</Component>;
+  return (
+    <Component {...labelProps} className={className}>
+      {children}
+    </Component>
+  );
 };
