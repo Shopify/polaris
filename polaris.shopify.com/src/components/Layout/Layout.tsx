396e18ee1a88b1d2be1d9949a59b63983b5fb75e
Marten Bjork <marten.bjork@shopify.com>
2022-07-14 12:47:12 +0200

    More design improvements

--- polaris.shopify.com/src/components/Layout/Layout.tsx
+++ polaris.shopify.com/src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TOCItem, useTOC } from "../../utils/hooks";
 import { className, slugify } from "../../utils/various";
 import Longform from "../Longform";
@@ -6,6 +6,8 @@ import Container from "../Container";
 import Nav, { NavItem } from "../Nav";
 
 import styles from "./Layout.module.scss";
+
+const HEADING_THRESHOLD = 120;
 
 interface Props {
   width?: "full" | "narrow";
@@ -23,6 +25,41 @@ function Layout({
   children,
 }: Props) {
   const [tocItems] = useTOC(children);
+  const [idOfCurrentHeading, setIdOfCurrentHeading] = useState<string>();
+
+  useEffect(() => {
+    const handler = () => {
+      const headings = document.querySelectorAll("#main h2, #main h3");
+      let currentHeading: Element | null = null;
+
+      for (let i = headings.length - 1; i >= 0; i--) {
+        const heading = headings[i];
+        const rect = heading.getBoundingClientRect();
+        currentHeading = heading;
+        if (rect.top < HEADING_THRESHOLD) {
+          break;
+        }
+      }
+
+      if (!currentHeading && headings.length > 0) {
+        currentHeading = headings[0];
+      }
+
+      if (currentHeading) {
+        const id = currentHeading.getAttribute("id");
+        if (id) {
+          setIdOfCurrentHeading(id);
+        }
+      }
+    };
+
+    handler();
+    window.addEventListener("scroll", handler);
+
+    return () => {
+      window.removeEventListener("scroll", handler);
+    };
+  }, []);
 
   return (
     <Container
@@ -36,45 +73,87 @@ function Layout({
         {navItems && <Nav navItems={navItems} />}
       </div>
 
-      <article className={styles.Main}>
-        <div className={styles.CenteredMainContent}>
+      <article className={styles.Post}>
+        <div className={styles.PostInner} id="main">
           {title && (
             <Longform>
               <h1>{title}</h1>
             </Longform>
           )}
-          <div className={styles.Post}>
-            {showTOC && <TOC items={tocItems} />}
-            <div id="main" className={styles.PostContent}>
-              {children}
-            </div>
-          </div>
+          {children}
         </div>
       </article>
+
+      {showTOC && (
+        <TOC items={tocItems} idOfCurrentHeading={idOfCurrentHeading} />
+      )}
     </Container>
   );
 }
 
-function TOC({ items }: { items: TOCItem[] }) {
+function TOC({
+  items,
+  idOfCurrentHeading,
+}: {
+  items: TOCItem[];
+  idOfCurrentHeading: string | undefined;
+}) {
   const isNested = !!items.find((item) => item.children.length > 0);
+
+  function scrollIntoView(id: string) {
+    const targetEl = document.getElementById(id);
+    if (targetEl) {
+      const top =
+        window.scrollY +
+        targetEl.getBoundingClientRect().top -
+        HEADING_THRESHOLD +
+        1;
+      window.scrollTo({ top, behavior: "smooth" });
+      return false;
+    }
+  }
 
   return (
     <div className={className(styles.TOC, isNested && styles.isNested)}>
       <ul>
-        {items.map(({ name, children }) => (
-          <li key={name}>
-            <a href={`#${slugify(name)}`}>{name}</a>
-            {children.length > 0 && (
-              <ul>
-                {children.map((child) => (
-                  <li key={child.name}>
-                    <a href={`#${slugify(child.name)}`}>{child.name}</a>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
+        {items.map(({ name, children }) => {
+          const id = slugify(name);
+          return (
+            <li key={name}>
+              <a
+                href={`#${id}`}
+                data-is-current={id === idOfCurrentHeading}
+                onClick={(evt) => {
+                  scrollIntoView(id);
+                  evt.preventDefault();
+                }}
+              >
+                {name}
+              </a>
+              {children.length > 0 && (
+                <ul>
+                  {children.map((child) => {
+                    const id = slugify(child.name);
+                    return (
+                      <li key={child.name}>
+                        <a
+                          href={`#${id}`}
+                          data-is-current={id === idOfCurrentHeading}
+                          onClick={(evt) => {
+                            scrollIntoView(id);
+                            evt.preventDefault();
+                          }}
+                        >
+                          {child.name}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
