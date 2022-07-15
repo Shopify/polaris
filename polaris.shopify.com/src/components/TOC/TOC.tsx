162b40080b85a8f5c8bef02b4e50a3bb5d45f757
Marten Bjork <marten.bjork@shopify.com>
2022-07-15 09:03:37 +0200

    Refactor

--- polaris.shopify.com/src/components/TOC/TOC.tsx
+++ polaris.shopify.com/src/components/TOC/TOC.tsx
@@ -9,10 +9,45 @@ interface Props {
   items: TOCItem[];
 }
 
+function getHeaderThreshold(): number {
+  const headerHeight = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue("--header-height");
+  const headerMargin = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue("--header-margin");
+  const headerThreshold = parseInt(headerHeight) + parseInt(headerMargin);
+  console.log({ headerHeight, headerMargin });
+  return headerThreshold;
+}
+
 function TOC({ items }: Props) {
   const isNested = !!items.find((item) => item.children.length > 0);
   const [idOfCurrentHeading, setIdOfCurrentHeading] = useState<string>();
   const temporarilyIgnoreScrolling = useRef(false);
+
+  function scrollIntoView(id: string) {
+    temporarilyIgnoreScrolling.current = true;
+    setIdOfCurrentHeading(id);
+    const headerThreshold = getHeaderThreshold();
+    const targetEl = document.getElementById(id);
+    if (targetEl) {
+      const top =
+        window.scrollY +
+        targetEl.getBoundingClientRect().top -
+        headerThreshold +
+        1;
+      window.scrollTo({ top, behavior: "smooth" });
+
+      history.pushState({}, "", `#${id}`);
+
+      setTimeout(() => {
+        temporarilyIgnoreScrolling.current = false;
+      }, 1000);
+
+      return false;
+    }
+  }
 
   const findCurrentHeading = () => {
     if (temporarilyIgnoreScrolling.current === true) {
@@ -51,28 +86,6 @@ function TOC({ items }: Props) {
     window.addEventListener("scroll", findCurrentHeading);
     return () => window.removeEventListener("scroll", findCurrentHeading);
   }, []);
-
-  function scrollIntoView(id: string) {
-    temporarilyIgnoreScrolling.current = true;
-    setIdOfCurrentHeading(id);
-    const targetEl = document.getElementById(id);
-    if (targetEl) {
-      const top =
-        window.scrollY +
-        targetEl.getBoundingClientRect().top -
-        HEADING_THRESHOLD +
-        1;
-      window.scrollTo({ top, behavior: "smooth" });
-
-      history.pushState({}, "", `#${id}`);
-
-      setTimeout(() => {
-        temporarilyIgnoreScrolling.current = false;
-      }, 1000);
-
-      return false;
-    }
-  }
 
   return (
     <div className={className(styles.TOC, isNested && styles.isNested)}>
