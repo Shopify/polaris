d26fd64e4ebe25cf285582f68c5958ba13c48bfe
Marten Bjork <marten.bjork@shopify.com>
2022-07-15 17:04:02 +0200

    Accept duplicate ids for now

--- polaris.shopify.com/src/components/Markdown/Markdown.tsx
+++ polaris.shopify.com/src/components/Markdown/Markdown.tsx
@@ -4,7 +4,7 @@ import Prism from "prismjs";
 import React from "react";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
-import { getIdGenerator, slugify } from "../../utils/various";
+import { slugify } from "../../utils/various";
 
 interface Props {
   text: string;
@@ -12,8 +12,6 @@ interface Props {
 }
 
 function Markdown({ text, skipH1 }: Props) {
-  const idGenerator = getIdGenerator();
-
   return (
     <ReactMarkdown
       remarkPlugins={[[remarkGfm, { tablePipeAlign: true }]]}
@@ -24,14 +22,14 @@ function Markdown({ text, skipH1 }: Props) {
         },
         h2: ({ children }) => {
           if (children.length === 1 && typeof children[0] === "string") {
-            return <h2 id={idGenerator.get(children[0])}>{children}</h2>;
+            return <h2 id={slugify(children[0])}>{children}</h2>;
           } else {
             return <h2>{children}</h2>;
           }
         },
         h3: ({ children }) => {
           if (children.length === 1 && typeof children[0] === "string") {
-            return <h3 id={idGenerator.get(children[0])}>{children}</h3>;
+            return <h3 id={slugify(children[0])}>{children}</h3>;
           } else {
             return <h3>{children}</h3>;
           }
