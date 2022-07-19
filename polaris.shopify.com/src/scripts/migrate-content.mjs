c02f48208aa47fe7deacaf3851d488f497770c1d
Alex Page <hi@alexpage.dev>
2022-07-19 11:56:46 -0700

    Update migrate-content.mjs

--- polaris.shopify.com/src/scripts/migrate-content.mjs
+++ polaris.shopify.com/src/scripts/migrate-content.mjs
@@ -13,23 +13,39 @@ const readmeFiles = glob.sync(
 
 const kebabize = (str) => {
   return str
-    .split("")
-    .map((letter, idx) => {
-      return letter.toUpperCase() === letter
-        ? `${idx !== 0 ? "-" : ""}${letter.toLowerCase()}`
-        : letter;
-    })
-    .join("");
+    .replace(/([a-z])([A-Z])/g, "$1-$2")
+    .replace(/\s+/g, "-")
+    .toLowerCase();
 };
 
 const migrateFile = async (filePath) => {
-  const fileName = `${kebabize(filePath.split("/").at(-2))}.md`;
+  const fileTitle = kebabize(filePath.split("/").at(-2));
+  const fileName = `${fileTitle}.md`;
   const fileDir = path.join(componentDir, fileName);
   const fileContent = fs.readFileSync(filePath, "utf-8");
-  const contentNoExample = fileContent
+  const exampleContent = fileContent
     .split("---")
-    .filter((fileChunk) => !fileChunk.startsWith("\n\n## Examples"))
-    .join("---");
+    .filter((fileChunk) => fileChunk.startsWith("\n\n## Examples"))[0]
+    .replace("\n\n## Examples", "");
+
+  let exampleFrontMatter = "examples:";
+  const examples = {};
+  exampleContent.split("\n\n### ").forEach((example) => {
+    const title = example.split("\n")[0].trim();
+    if (title === "") return;
+
+    const kebabTitle = kebabize(title);
+    const exampleId = `${fileTitle}-${kebabTitle}`;
+    const description = example.replace(title, "").split("```")[0].trim();
+    const code = example.split("```jsx").at(-1).replace("```", "").trim();
+
+    exampleFrontMatter += `\n  - fileName: ${exampleId}.tsx\n    title: ${title}\n    description:>-\n      ${description}`;
+    examples[`${exampleId}.tsx`] = code;
+  });
+
+  const contentNoExample = fileContent
+    .replace(`## Examples${exampleContent}---\n\n`, "")
+    .replace("\n---", `\n${exampleFrontMatter}\n---`);
 
   fs.writeFileSync(fileDir, contentNoExample);
 };
