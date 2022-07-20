792671ebd5fb9c7a92f7aed0ace8556b7e993725
Marten Bjork <marten.bjork@shopify.com>
2022-07-20 09:02:53 +0200

    Improve TS paring

--- polaris.shopify.com/src/scripts/get-props.ts
+++ polaris.shopify.com/src/scripts/get-props.ts
@@ -15,6 +15,7 @@ type Member = {
   name: string;
   type: string;
   description: string;
+  tags: { name: string; text: string }[];
 };
 export interface TypeOrInterfaceNode extends BaseNode {
   members: Member[];
@@ -34,6 +35,7 @@ function getProps(fileNames: string[], options: ts.CompilerOptions): void {
   for (const sourceFile of program.getSourceFiles()) {
     if (!sourceFile.isDeclarationFile) {
       ts.forEachChild(sourceFile, (child) => {
+        // console.log(sourceFile.fileName);
         visit(child);
       });
     }
@@ -42,67 +44,130 @@ function getProps(fileNames: string[], options: ts.CompilerOptions): void {
   const filePath = path.join(__dirname, "../../../data/props.json");
   fs.writeFileSync(filePath, JSON.stringify(tree, undefined, 2));
 
-  function visit(node: ts.Node, level = 0): string | undefined {
+  function visit(
+    node: ts.Node | ts.Declaration,
+    level = 0
+  ): string | undefined {
     if (level > 10) {
       return undefined;
     }
 
     if (node.kind === ts.SyntaxKind.InterfaceDeclaration) {
       const interfaceDeclaration = node as ts.InterfaceDeclaration;
+      const type = checker.getTypeAtLocation(interfaceDeclaration.name);
       const symbol = checker.getSymbolAtLocation(interfaceDeclaration.name);
-      if (symbol) {
-        const id = checker.getFullyQualifiedName(symbol);
-        if (id) {
-          interfaceDeclaration.members.forEach((member) => {
-            if (member.kind === ts.SyntaxKind.PropertySignature) {
-              const propertySignature = member as ts.PropertySignature;
-              if (propertySignature.type) {
-                console.log(propertySignature.name.getText());
-                console.log(propertySignature.type.getText());
-                console.log(ts.SyntaxKind[propertySignature.type.kind]);
-                const isLiteral = ts.isLiteralTypeNode(propertySignature.type);
-                console.log({ isLiteral });
-              }
-            }
-          });
 
-          const returnedNode: Node = {
-            kind: interfaceDeclaration.kind,
-            name: interfaceDeclaration.name.escapedText.toString(),
-            syntaxKind: ts.SyntaxKind[interfaceDeclaration.kind],
-            members: [],
-          };
-          tree[id] = returnedNode;
-          // ts.forEachChild(interfaceNode, (child) => {
-          //   visit(child, level + 1);
-          // });
+      if (symbol) {
+        const members: Member[] = [];
+        const fullyQualifiedName = checker.getFullyQualifiedName(symbol);
+
+        for (const prop of type.getProperties()) {
+          if (prop.valueDeclaration) {
+            const name = prop.getName();
+            const propType = checker.getTypeOfSymbolAtLocation(
+              prop,
+              prop.valueDeclaration
+            );
+            const type = checker.typeToString(propType);
+            const description = prop
+              .getDocumentationComment(checker)
+              .map((comment) => {
+                return comment.kind === "text" ? comment.text : "";
+              })
+              .join("\n");
+            const tags = prop.getJsDocTags().map((tag) => {
+              return {
+                name: tag.name,
+                text: tag.text
+                  ? tag.text.map((text) => text.text).join("")
+                  : "",
+              };
+            });
+
+            if (propType.aliasSymbol) {
+              console.log("alias symbol");
+              const x = propType.aliasSymbol.getName();
+              const y = propType.aliasSymbol.valueDeclaration;
+              const sis = checker.getSymbolsInScope(node, ts.SymbolFlags.Type);
+              console.log({
+                x,
+                y,
+                sis: sis.find((symbol) => symbol.name === name),
+              });
+            }
+
+            members.push({ name, type, description, tags });
+          }
+
+          // We only end up in this else branch for the Button component
+          // which extends another type in a really strange way.
+          // https://github.com/Shopify/polaris/blob/main/polaris-react/src/components/Button/Button.tsx#L57
+          else {
+            const declarations = prop.getDeclarations();
+            if (declarations) {
+              declarations.forEach((declaration) => {
+                console.log(">>", declaration.getText());
+                console.log(">>", declaration.kind);
+                visit(declaration, level + 1);
+              });
+            } else {
+              throw new Error("Expected interface member to have declarations");
+            }
+            // throw new Error("Expected value declaration");
+          }
         }
+
+        tree[fullyQualifiedName] = {
+          kind: node.kind,
+          syntaxKind: ts.SyntaxKind[node.kind],
+          name: interfaceDeclaration.name.escapedText.toString(),
+          members,
+        };
       } else {
-        console.warn(
-          "Expected interfaceDeclaration to have symbol",
-          interfaceDeclaration
-        );
+        throw new Error("Expected interface declaration to have symbol");
+      }
+    } else if (node.kind === ts.SyntaxKind.TypeAliasDeclaration) {
+      const typeAliasDeclaration = node as ts.TypeAliasDeclaration;
+      const type = checker.getTypeAtLocation(typeAliasDeclaration.name);
+      const symbol = checker.getSymbolAtLocation(typeAliasDeclaration.name);
+
+      if (symbol) {
+        const fullyQualifiedName = program
+          .getTypeChecker()
+          .getFullyQualifiedName(symbol);
+        const text = typeAliasDeclaration.getFullText();
+
+        if (typeAliasDeclaration.type.kind === ts.SyntaxKind.UnionType) {
+          const unionType = typeAliasDeclaration.type as ts.UnionTypeNode;
+          const unfurled = unionType.types
+            .map((type) => {
+              return type.getText();
+            })
+            .join(" | ");
+          if (fullyQualifiedName.includes("Alignment")) {
+            console.log({ fullyQualifiedName, text, unfurled });
+          }
+        } else if (
+          typeAliasDeclaration.type.kind === ts.SyntaxKind.IntersectionType
+        ) {
+          const unionType = typeAliasDeclaration.type as ts.UnionTypeNode;
+          const unfurled = unionType.types
+            .map((type) => {
+              return type.getText();
+            })
+            .join(" & ");
+          if (fullyQualifiedName.includes("Alignment")) {
+            console.log({ fullyQualifiedName, text, unfurled });
+          }
+        }
       }
     }
   }
-
-  // function getInterfaceMembers(
-  //   interfaceDeclaration: ts.InterfaceDeclaration
-  // ): Member[] {
-  //   return interfaceDeclaration.members.map((member) => {
-  //     return {
-  //       name: member.name ? member.name : "",
-  //       type,
-  //       description: "bar",
-  //     };
-  //   });
-  // }
 }
 
-globby("../polaris-react/src/**/*.tsx", {
-  ignore: ["*.test.tsx"],
-}).then((files) => {
-  getProps(files, {
+globby(["../polaris-react/src/**/*.tsx"]).then((files) => {
+  let filesWithoutTests = files.filter((file) => !file.endsWith("test.tsx"));
+  getProps(filesWithoutTests, {
     target: ts.ScriptTarget.ES5,
     module: ts.ModuleKind.CommonJS,
   });
