a766c83ee3147391ccb07c6c64b7dd743eee040b
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-07-21 10:11:53 -0400

    Add Text component examples

---
+++ polaris.shopify.com/src/pages/examples/text-font-weight-medium.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@shopify/polaris";
+import React from "react";
+import { withPolarisExample } from "../../components/PolarisExamplePage";
+
+function TextExample() {
+  return (
+    <Text variant="bodyMd" as="p" fontWeight="medium">
+      Sales this year
+    </Text>
+  );
+}
+
+export default withPolarisExample(TextExample);
