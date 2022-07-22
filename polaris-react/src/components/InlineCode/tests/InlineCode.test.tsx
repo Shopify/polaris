8349862b5089ee21548c5398c78546ab4851f673
Lo Kim <lo.kim@shopify.com>
2022-07-22 14:24:11 -0400

    [Typography foundations] Add tests for `InlineCode` component

---
+++ polaris-react/src/components/InlineCode/tests/InlineCode.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {mountWithApp} from 'tests/utilities';
+
+import {InlineCode} from '../InlineCode';
+
+describe('<Text />', () => {
+  const text = 'Hello world';
+
+  it('renders its children', () => {
+    const inlineCode = mountWithApp(<InlineCode>{text}</InlineCode>);
+    expect(inlineCode).toContainReactText(text);
+  });
+
+  it('renders its children inside a code tag', () => {
+    const inlineCode = mountWithApp(<InlineCode>{text}</InlineCode>);
+    expect(inlineCode).toContainReactComponent('code');
+  });
+
+  it('renders its children with code block styling', () => {
+    const inlineCode = mountWithApp(<InlineCode>{text}</InlineCode>);
+    expect(inlineCode).toContainReactComponent('code', {
+      className: expect.stringContaining('Code'),
+    });
+  });
+});
