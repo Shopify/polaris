b27d9f5efb60a03a2b94298b6d523524cd274480
Trish Ta <trish.ta@shopify.com>
2022-07-25 18:08:04 -0400

    Use lit

--- polaris-react/playground/WebComponentsManual.tsx
+++ polaris-react/playground/WebComponentsManual.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import {html, css, LitElement} from 'lit';
+// eslint-disable-next-line import/extensions
+import {customElement, property} from 'lit/decorators.js';
 
 // eslint-disable-next-line @shopify/strict-component-boundaries
 import styles from '../src/components/TextStyle/TextStyle.scss';
 
-export class UIText extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({mode: 'open'});
-  }
+// export class UIText extends HTMLElement {
+//   constructor() {
+//     super();
+//     this.attachShadow({mode: 'open'});
+//   }
 
+//   getVariationClass() {
+//     const variation = this.getAttribute('variation');
+
+//     if (!variation) {
+//       return undefined;
+//     }
+
+//     return styles[
+//       `variation${variation
+//         .slice(0, 1)
+//         .toUpperCase()
+//         .concat(variation.slice(1))}`
+//     ];
+//   }
+
+//   connectedCallback() {
+//     const {shadowRoot} = this;
+//     // This doesn't work with styles defined outside of the shadow dom
+//     shadowRoot.innerHTML = `<span class="${this.getVariationClass()}"><slot/></span>`;
+//   }
+// }
+
+// customElements.define('ui-text', UIText);
+
+@customElement('ui-text')
+export class UIText extends LitElement {
   getVariationClass() {
     const variation = this.getAttribute('variation');
 
@@ -24,14 +53,11 @@ export class UIText extends HTMLElement {
     ];
   }
 
-  connectedCallback() {
-    const {shadowRoot} = this;
+  render() {
     // This doesn't work with styles defined outside of the shadow dom
-    shadowRoot.innerHTML = `<span class="${this.getVariationClass()}"><slot/></span>`;
+    return html`<span class="${this.getVariationClass()}"><slot /></span>`;
   }
 }
-
-customElements.define('ui-text', UIText);
 
 export function WebComponentsManual() {
   return (
