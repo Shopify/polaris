7881471d1d1b433e6b05e454021024b37a2973f9
Alex Page <hi@alexpage.dev>
2022-07-26 16:28:22 -0700

    Bump storybook versions and clean up main.js

--- polaris-react/.storybook/main.js
+++ polaris-react/.storybook/main.js
@@ -2,24 +2,13 @@ const path = require('path');
 const spawn = require('child_process').spawn;
 const CreateFileWebpack = require('create-file-webpack');
 const postcssPlugins = require('../config/postcss-plugins');
-// Enabling docs means the preview panel takes an extra 2ish seconds to load
-// This usually isn't a big deal, except when we're running all of our stories
-// through our a11y tests, and a 2s delay over several hundred stories adds up.
-// This is an escape hatch to disable docs only wheile we're running a11y tests
-const enableDocs = !parseInt(process.env.STORYBOOK_DISABLE_DOCS || '0', 10);
 
 module.exports = {
   core: {
     builder: 'webpack5',
   },
-  // Added to work around https://github.com/storybookjs/storybook/issues/15336
-  // The line below can be removed in @storybook/react v6.5.0
-  typescript: {reactDocgen: false},
   stories: ['../playground/stories.tsx', '../src/components/**/*/README.md'],
-  addons: [
-    {name: '@storybook/addon-essentials', options: {docs: enableDocs}},
-    '@storybook/addon-a11y',
-  ],
+  addons: ['@storybook/addon-a11y'],
   webpackFinal: (config) => {
     const isProduction = config.mode === 'production';
 
@@ -73,15 +62,6 @@ module.exports = {
         ],
       },
     ];
-
-    config.plugins.push(
-      new CreateFileWebpack({
-        path: './build-internal/storybook/static/services/',
-        fileName: 'ping.html',
-        content:
-          '<!DOCTYPE html><html lang="en"><head></head><body>OK</body></html>',
-      }),
-    );
 
     config.module.rules = [
       // Strip out existing rules that apply to md files
