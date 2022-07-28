df32da7eb72aad5b4241eeb460541e0c46cbce2f
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-07-28 12:50:10 -0400

    Remove forced 'block' styling

--- polaris-react/src/components/Text/Text.tsx
+++ polaris-react/src/components/Text/Text.tsx
@@ -72,7 +72,6 @@ export const Text = ({
     styles.root,
     styles[variant],
     fontWeight ? styles[fontWeight] : styles[VariantFontWeightMapping[variant]],
-    (alignment || truncate) && as !== 'span' && styles.block,
     alignment && styles[alignment],
     color && styles[color],
     truncate && styles.truncate,
