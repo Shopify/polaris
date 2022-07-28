74053069222dca656907c4ec5512edee965592d4
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-07-28 08:44:23 -0700

    Remove Playground examples

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,35 +1,11 @@
 import React from 'react';
 
 import {Page} from '../src';
-import {useEventListener} from '../src/utilities/use-event-listener';
 
 export function Playground() {
-  const ref = React.useRef<HTMLParagraphElement>(null);
-
-  const [coords, setCoords] = React.useState({x: 0, y: 0});
-
-  useEventListener('mousemove', (event) =>
-    setCoords({
-      x: event.clientX,
-      y: event.clientY,
-    }),
-  );
-
-  useEventListener('click', (event) => event);
-  useEventListener('keydown', (event) => event);
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  useEventListener('copy', (event) => event);
-  useEventListener('copy', (event) => event, document);
-
-  // eslint-disable-next-line no-alert
-  useEventListener('dblclick', (_event) => alert('double clicked'), ref);
-
   return (
     <Page title="Playground">
-      <p ref={ref}>Double click me</p>
-      <p>Mouse X: {coords.x}</p>
-      <p>Mouse Y: {coords.y}</p>
+      {/* Add the code you want to test in here */}
     </Page>
   );
 }
