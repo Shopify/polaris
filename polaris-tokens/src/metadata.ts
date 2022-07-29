633916491d19a989042ffb2e3e242e3122837b41
Lo Kim <lo.kim@shopify.com>
2022-07-29 12:12:14 -0400

    Fix references to `font` tokens

--- polaris-tokens/src/metadata.ts
+++ polaris-tokens/src/metadata.ts
@@ -2,23 +2,23 @@ import type {Exact, Metadata} from './types';
 import {tokensToRems} from './utilities';
 import {breakpoints} from './token-groups/breakpoints';
 import {depth} from './token-groups/depth';
+import {font} from './token-groups/font';
 import {legacy} from './token-groups/legacy';
 import {colors} from './token-groups/colors';
 import {motion} from './token-groups/motion';
 import {shape} from './token-groups/shape';
 import {spacing} from './token-groups/spacing';
-import {font} from './token-groups/font';
 import {zIndex} from './token-groups/zIndex';
 
 export const metadata = createMetadata({
   breakpoints: tokensToRems(breakpoints),
   colors,
   depth,
+  font: tokensToRems(font),
   legacy: tokensToRems(legacy),
   motion,
   shape: tokensToRems(shape),
   spacing: tokensToRems(spacing),
-  font: tokensToRems(font),
   zIndex,
 });
 
