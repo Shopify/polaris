92d45381b34724641f4c09214945b43dd4aeee3c
Yuraima Estevez <yuraima.estevez@shopify.com>
2022-08-04 15:54:51 -0500

    updated ping api and fixed error on tokens api page (#6851)

---
+++ polaris.shopify.com/src/pages/api/ping/index.tsx
@@ -0,0 +1,7 @@
+import type {NextApiResponse, NextApiRequest} from "next";
+
+const handler = async (_: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).send('OK');
+}     
+
+export default handler
