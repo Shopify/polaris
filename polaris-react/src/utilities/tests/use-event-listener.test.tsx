79e5a58f0bac01a864294c8346699470f5584b36
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-08-08 11:50:59 -0700

    Add test for React ref support

--- polaris-react/src/utilities/tests/use-event-listener.test.tsx
+++ polaris-react/src/utilities/tests/use-event-listener.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {mount} from 'tests/utilities';
 
 import {useEventListener} from '../use-event-listener';
@@ -71,6 +71,22 @@ describe('useEventListener', () => {
     expect(spy).not.toHaveBeenCalled();
 
     document.dispatchEvent(new Event('click'));
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('adds a listener to a React ref', () => {
+    function MockComponent() {
+      const ref = useRef<HTMLParagraphElement>(null);
+
+      useEventListener('click', spy, ref);
+
+      return <p ref={ref} />;
+    }
+
+    const spy = jest.fn();
+    const eventListener = mount(<MockComponent />);
+
+    eventListener.find('p')!.domNode!.dispatchEvent(new Event('click'));
     expect(spy).toHaveBeenCalled();
   });
 });
