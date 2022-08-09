b964cca43f1dc2c9829a0dee1f5003eae39d135c
Sam Rose <sam.rose@shopify.com>
2022-08-09 08:44:00 -0600

    Get the next @shopify/polaris version from the changset info

--- polaris-react/scripts/readme-update-version.js
+++ polaris-react/scripts/readme-update-version.js
@@ -4,25 +4,39 @@ const {resolve} = require('path');
 const {execSync} = require('child_process');
 const {writeFileSync, readFileSync} = require('fs');
 
-const {version: newVersion} = require('../package.json');
+const getReleasePlan = require('@changesets/get-release-plan').default;
+
+const {name: pkgName} = require('../package.json');
 const {semverRegExp} = require('../scripts/utilities');
 
 const root = resolve(__dirname, '..');
 
 const readmes = Object.freeze(['README.md']);
 
-console.log(`🆕 Updating version in ${readmes.join(', ')}...`);
-readmes
-  .map((readme) => resolve(root, readme))
-  .forEach((file) => {
-    writeFileSync(
-      file,
-      readFileSync(file, 'utf8').replace(semverRegExp, newVersion),
-    );
-  });
+const run = async () => {
+  const {releases} = await getReleasePlan(resolve(process.cwd(), '../'));
+  const {newVersion} = releases.find((release) => release.name === pkgName);
 
-console.log(`🏃‍♂️ Running \`git add -A ${readmes.join(' ')}\`...`);
-const execOpts = {stdio: 'inherit'};
-execSync(`git add -A ${readmes.join(' ')}`, execOpts);
+  console.log(`🆕 Updating version in ${readmes.join(', ')}...`);
+  readmes
+    .map((readme) => resolve(root, readme))
+    .forEach((file) => {
+      writeFileSync(
+        file,
+        readFileSync(file, 'utf8').replace(semverRegExp, newVersion),
+      );
+    });
+
+  console.log(`🏃‍♂️ Running \`git add -A ${readmes.join(' ')}\`...`);
+  const execOpts = {stdio: 'inherit'};
+  execSync(`git add -A ${readmes.join(' ')}`, execOpts);
+};
+
+try {
+  run();
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
 
 module.exports = readmes;
