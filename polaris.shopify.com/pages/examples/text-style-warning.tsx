1e6b115c878be5b944298d777f06588b75bf8424
Lo Kim <lo.kim@shopify.com>
2022-08-23 14:49:54 -0400

    [Typography foundations] Deprecate existing typography components on polaris.shopify.com (#6915)

--- polaris.shopify.com/pages/examples/text-style-warning.tsx
+++
@@ -1,9 +0,0 @@
-import {TextStyle} from '@shopify/polaris';
-import React from 'react';
-import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
-
-function TextStyleExample() {
-  return <TextStyle variation="warning">Scheduled maintenance</TextStyle>;
-}
-
-export default withPolarisExample(TextStyleExample);
