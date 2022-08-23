f75fdd889ee5e6528546cb585f548764c1cfe2cd
Lo Kim <lo.kim@shopify.com>
2022-08-23 14:49:54 -0400

    [Typography foundations] Deprecate existing typography components on polaris.shopify.com (#6915)

--- polaris.shopify.com/pages/examples/subheading-default.tsx
+++
@@ -1,9 +0,0 @@
-import {Subheading} from '@shopify/polaris';
-import React from 'react';
-import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
-
-function Example() {
-  return <Subheading>Accounts</Subheading>;
-}
-
-export default withPolarisExample(Example);
