c96c62c5c27ed9c8766c988be2bb030e988d3bdf
Marten Bjork <marten.bjork@shopify.com>
2022-08-26 19:53:42 -0400

    Refactor prop table

--- polaris.shopify.com/src/components/PropsTable/PropsTable.tsx
+++ polaris.shopify.com/src/components/PropsTable/PropsTable.tsx
@@ -1,3 +1,4 @@
+import {createContext, useContext, useState} from 'react';
 import {TypeMeta} from '../../types';
 import StatusBadge from '../StatusBadge';
 import {Disclosure} from '@headlessui/react';
@@ -9,40 +10,27 @@ interface Props {
   typeMeta: TypeMeta[];
 }
 
-function highlightType(type: string, prev: string = ''): React.ReactNode {
-  if (
-    type === 'string' ||
-    type.match(/^['][^']+'$/) !== null ||
-    type.match(/^["][^"]+"$/) !== null
-  ) {
-    return <span className={styles.SyntaxString}>{type}</span>;
-  } else if (type === 'boolean') {
-    return <span className={styles.SyntaxBoolean}>{type}</span>;
-  } else if (type === 'number') {
-    return <span className={styles.SyntaxNumber}>{type}</span>;
-  } else if (type.endsWith('ReactNode') || type.endsWith('ReactElement')) {
-    return <span className={styles.SyntaxReactNode}>{type}</span>;
-  } else if (type === 'void') {
-    return <span className={styles.SyntaxReactNode}>{type}</span>;
-  } else if (type.match(/^[A-Z][A-Za-z]+$/) || type === 'any') {
-    return <span className={styles.SyntaxReactNode}>{type}</span>;
-  } else if (type.match(/^[a-z]+$/gi) !== null) {
-    return <span className={styles.SyntaxKeyword}>{type}</span>;
+function inlineTypeValue(
+  typeMeta: TypeMeta,
+): string | number | object | undefined {
+  if (typeMeta.members) {
+    return undefined;
   } else {
-    if (prev === type) {
-      return <>{type}</>;
-    }
-    const tokenRegex = /([^a-z0-9'"/-]+)/gi;
-    const tokens = type.split(tokenRegex);
-    return (
-      <>
-        {tokens.map((token) => {
-          return <>{highlightType(token, type)}</>;
-        })}
-      </>
-    );
+    return typeMeta.value;
   }
 }
+
+function getTypeWithName(
+  typeMeta: TypeMeta[],
+  name: string,
+): TypeMeta | undefined {
+  return typeMeta.find((type) => type.name === name);
+}
+
+const toPascalCase = (str: string) =>
+  (str.match(/[a-zA-Z0-9]+/g) || [])
+    .map((w) => `${w.charAt(0).toUpperCase()}${w.slice(1)}`)
+    .join('');
 
 function PropsTable({typeMeta, componentName}: Props) {
   const feedbackTitle = '[polaris.shopify.com] Props table feedback';
@@ -50,19 +38,14 @@ function PropsTable({typeMeta, componentName}: Props) {
     feedbackTitle,
   )}&amp;labels=polaris.shopify.com`;
 
-  console.log(typeMeta);
-  const propsForComponent = typeMeta.find(
-    (type) =>
-      type.name.toLowerCase() ===
-      `${componentName.replace(/\s/g, '').toLowerCase()}props`,
-  );
+  const propsName = `${toPascalCase(componentName).replace(/\s/g, '')}Props`;
+  const propsForComponent = getTypeWithName(typeMeta, propsName);
 
   if (!propsForComponent) {
     throw new Error('Could not find props for component');
   }
 
-  const propsAreDefinedUsingInterface =
-    propsForComponent && 'members' in propsForComponent;
+  const propsAreDefinedUsingInterface = !!propsForComponent.members;
 
   return (
     <div className={styles.PropsTable}>
@@ -85,6 +68,57 @@ function PropsTable({typeMeta, componentName}: Props) {
   );
 }
 
+function Highlighter({
+  type,
+  prev = '',
+}: {
+  type: string;
+  prev?: string;
+}): JSX.Element {
+  const {expandType} = useContext(ExpandedTypesContext);
+
+  if (
+    type === 'string' ||
+    type.match(/^['][^']+'$/) !== null ||
+    type.match(/^["][^"]+"$/) !== null
+  ) {
+    return <span className={styles.SyntaxString}>{type}</span>;
+  } else if (type === 'boolean') {
+    return <span className={styles.SyntaxBoolean}>{type}</span>;
+  } else if (type === 'number') {
+    return <span className={styles.SyntaxNumber}>{type}</span>;
+  } else if (type.endsWith('ReactNode') || type.endsWith('ReactElement')) {
+    return <span className={styles.SyntaxReactNode}>{type}</span>;
+  } else if (type === 'void') {
+    return <span className={styles.SyntaxReactNode}>{type}</span>;
+  } else if (type.match(/^[A-Z][A-Za-z]+$/) || type === 'any') {
+    return (
+      <span className={styles.SyntaxReactNode}>
+        <button onClick={() => expandType(type)}>{type}</button>
+      </span>
+    );
+  } else if (type.match(/^[a-z]+$/gi) !== null) {
+    return <span className={styles.SyntaxKeyword}>{type}</span>;
+  } else {
+    if (prev === type) {
+      return <>{type}</>;
+    }
+    const tokenRegex = /([^a-z0-9'"/-]+)/gi;
+    const tokens = type.split(tokenRegex);
+    return (
+      <>
+        {tokens.map((token) => {
+          return <Highlighter key={token} type={token} prev={type} />;
+        })}
+      </>
+    );
+  }
+}
+
+const ExpandedTypesContext = createContext<{
+  expandType: (typeName: string) => void;
+}>({expandType: () => undefined});
+
 function InterfaceList({
   typeMeta,
   level = 0,
@@ -92,6 +126,10 @@ function InterfaceList({
   typeMeta: TypeMeta;
   level?: number;
 }) {
+  const [expandedTypes, setExpandedTypes] = useState<
+    {memberName: string; typeName: string}[]
+  >([]);
+
   return (
     <div className={styles.InterfaceList}>
       <Disclosure defaultOpen={level === 0}>
@@ -111,51 +149,73 @@ function InterfaceList({
                 value,
               }) => {
                 return (
-                  <li key={name}>
-                    <span className={styles.KeyValue}>
-                      <span className={styles.Key}>
-                        {name}{' '}
-                        {isOptional ? (
-                          ''
-                        ) : (
-                          <>
-                            {' '}
-                            <StatusBadge
-                              status={{value: 'warning', message: 'Required'}}
-                            />
-                          </>
-                        )}
-                      </span>
-                      <span className={styles.Value}>
-                        <span className={styles.Description}>
-                          {description}
-                          {defaultValue && (
+                  <ExpandedTypesContext.Provider
+                    key={name}
+                    value={{
+                      expandType: (typeName) => {
+                        setExpandedTypes([
+                          ...expandedTypes,
+                          {typeName, memberName: name},
+                        ]);
+                      },
+                    }}
+                  >
+                    <li>
+                      <span className={styles.KeyValue}>
+                        <span className={styles.Key}>
+                          {name}{' '}
+                          {isOptional ? (
+                            ''
+                          ) : (
                             <>
-                              {'. Defaults to '}
-                              <span className={styles.Default}>
-                                {highlightType(defaultValue)}
-                              </span>
-                              .
+                              {' '}
+                              <StatusBadge
+                                status={{value: 'warning', message: 'Required'}}
+                              />
                             </>
                           )}
                         </span>
-
-                        {!members ? (
-                          <span className={styles.ValueText}>
-                            {highlightType(value)}
+                        <span className={styles.Value}>
+                          <span className={styles.Description}>
+                            {description}
+                            {defaultValue && (
+                              <>
+                                {'. Defaults to '}
+                                <span className={styles.Default}>
+                                  <Highlighter type={defaultValue} />
+                                </span>
+                                .
+                              </>
+                            )}
                           </span>
-                        ) : (
-                          <>
-                            {/* <InterfaceList
+
+                          {!members ? (
+                            <span className={styles.ValueText}>
+                              <Highlighter type={value.toString()} />
+                            </span>
+                          ) : (
+                            <>
+                              {/* <InterfaceList
                               typeMeta={members}
                               level={level + 1}
                             /> */}
-                            <p>I have members</p>
-                          </>
-                        )}
+                              <p>I have members</p>
+                            </>
+                          )}
+
+                          {expandedTypes
+                            .filter((expanded) => expanded.memberName === name)
+                            .map((expanded) => {
+                              return (
+                                <p key={expanded.typeName}>
+                                  {expanded.typeName}
+                                </p>
+                              );
+                            })}
+                        </span>
                       </span>
-                    </span>
-                  </li>
+                    </li>
+                  </ExpandedTypesContext.Provider>
                 );
               },
             )}
