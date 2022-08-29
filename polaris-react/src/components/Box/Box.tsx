20380ce65986ee79e11483ccf996acee18d3122a
Lo Kim <lo.kim@shopify.com>
2022-08-29 12:35:16 -0400

    [Layout foundations] Refactor and add `border` and `borderRadius` props

--- polaris-react/src/components/Box/Box.tsx
+++ polaris-react/src/components/Box/Box.tsx
@@ -36,9 +36,33 @@ type Shadow = 'default' | 'transparent' | 'faint' | 'deep';
 type ShapeTokenGroup = typeof shape;
 type ShapeTokenName = keyof ShapeTokenGroup;
 
-type BorderTokenScale = ShapeTokenName extends `border-${infer Scale}`
+type BorderShapeTokenScale = ShapeTokenName extends `border-${infer Scale}`
   ? Scale
   : never;
+
+type BorderTokenScale = Exclude<
+  BorderShapeTokenScale,
+  `radius-${string}` | `width-${string}`
+>;
+
+interface Border {
+  bottom: BorderTokenScale;
+  left: BorderTokenScale;
+  right: BorderTokenScale;
+  top: BorderTokenScale;
+}
+
+type BorderRadiusTokenScale = Extract<
+  BorderShapeTokenScale,
+  `radius-${string}`
+>;
+
+interface BorderRadius {
+  bottomLeft: BorderRadiusTokenScale | '';
+  bottomRight: BorderRadiusTokenScale | '';
+  topLeft: BorderRadiusTokenScale | '';
+  topRight: BorderRadiusTokenScale | '';
+}
 
 type SpacingTokenGroup = typeof spacing;
 type SpacingTokenName = keyof SpacingTokenGroup;
@@ -48,18 +72,36 @@ type SpacingTokenScale = SpacingTokenName extends `space-${infer Scale}`
   ? Scale
   : never;
 
-type Spacing = {
+interface Spacing {
   bottom: SpacingTokenScale | '';
   left: SpacingTokenScale | '';
   right: SpacingTokenScale | '';
   top: SpacingTokenScale | '';
-};
+}
 
 interface BoxBaseProps {
   /** Background color of the Box */
   background?: Background;
   /** Border styling of the Box */
   border?: BorderTokenScale;
+  /** Bottom border styling of the Box */
+  borderBottom?: BorderTokenScale;
+  /** Left border styling of the Box */
+  borderLeft?: BorderTokenScale;
+  /** Right border styling of the Box */
+  borderRight?: BorderTokenScale;
+  /** Top border styling of the Box */
+  borderTop?: BorderTokenScale;
+  /** Border radius of the Box */
+  borderRadius?: BorderRadiusTokenScale;
+  /** Bottom left border radius of the Box */
+  borderRadiusBottomLeft?: BorderRadiusTokenScale;
+  /** Bottom right border radius of the Box */
+  borderRadiusBottomRight?: BorderRadiusTokenScale;
+  /** Top left border radius of the Box */
+  borderRadiusTopLeft?: BorderRadiusTokenScale;
+  /** Top right border radius of the Box */
+  borderRadiusTopRight?: BorderRadiusTokenScale;
   /** Inner content of the Box */
   children: ReactNode;
   /** Spacing outside of the Box */
@@ -95,7 +137,16 @@ export const Box = forwardRef(
     {
       as: Component = 'div',
       background,
-      border,
+      border = '',
+      borderBottom = '',
+      borderLeft = '',
+      borderRight = '',
+      borderTop = '',
+      borderRadius = '',
+      borderRadiusBottomLeft = '',
+      borderRadiusBottomRight = '',
+      borderRadiusTopLeft = '',
+      borderRadiusTopRight = '',
       children,
       margin = '',
       marginBottom = '',
@@ -111,6 +162,24 @@ export const Box = forwardRef(
     },
     ref,
   ) => {
+    const borders = {
+      bottom: borderBottom ? borderBottom : border,
+      left: borderLeft ? borderLeft : border,
+      right: borderRight ? borderRight : border,
+      top: borderTop ? borderTop : border,
+    } as Border;
+
+    const borderRadiuses = {
+      bottomLeft: borderRadiusBottomLeft
+        ? borderRadiusBottomLeft
+        : borderRadius,
+      bottomRight: borderRadiusBottomRight
+        ? borderRadiusBottomRight
+        : borderRadius,
+      topLeft: borderRadiusTopLeft ? borderRadiusTopLeft : borderRadius,
+      topRight: borderRadiusTopRight ? borderRadiusTopRight : borderRadius,
+    } as BorderRadius;
+
     const margins = {
       bottom: marginBottom ? marginBottom : margin,
       left: marginLeft ? marginLeft : margin,
@@ -148,12 +217,35 @@ export const Box = forwardRef(
       '--pc-box-padding-top': paddings.top
         ? `var(--p-space-${paddings.top})`
         : '',
+      '--pc-box-border-bottom': borders.bottom
+        ? `var(--p-border-${borders.bottom})`
+        : '',
+      '--pc-box-border-left': borders.left
+        ? `var(--p-border-${borders.left})`
+        : '',
+      '--pc-box-border-right': borders.right
+        ? `var(--p-border-${borders.right})`
+        : '',
+      '--pc-box-border-top': borders.top
+        ? `var(--p-border-${borders.top})`
+        : '',
+      '--pc-box-border-radius-bottom-left': borderRadiuses.bottomLeft
+        ? `var(--p-border-${borderRadiuses.bottomLeft})`
+        : '',
+      '--pc-box-border-radius-bottom-right': borderRadiuses.bottomRight
+        ? `var(--p-border-${borderRadiuses.bottomRight})`
+        : '',
+      '--pc-box-border-radius-top-left': borderRadiuses.topLeft
+        ? `var(--p-border-${borderRadiuses.topLeft})`
+        : '',
+      '--pc-box-border-radius-top-right': borderRadiuses.topRight
+        ? `var(--p-border-${borderRadiuses.topRight})`
+        : '',
     } as React.CSSProperties;
 
     const className = classNames(
       styles.root,
       background && styles[background],
-      border && styles[border],
       shadow && styles[shadow],
     );
 
