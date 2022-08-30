26c71eaf450251696fa1e754a7f77cffb7e53feb
Rener Baffa <rener.baffa@shopify.com>
2022-08-30 12:22:52 +0200

    Add use case to Polaris documentation/website

---
+++ polaris.shopify.com/pages/examples/color-picker-with-hex-color-text-field.tsx
@@ -0,0 +1,16 @@
+import {ColorPicker} from '@shopify/polaris';
+import {useState} from 'react';
+import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
+
+function ColorPickerWithTransparentValueExample() {
+  const [color, setColor] = useState({
+    hue: 300,
+    brightness: 1,
+    saturation: 0.7,
+    alpha: 0.7,
+  });
+
+  return <ColorPicker onChange={setColor} color={color} showHexTextField />;
+}
+
+export default withPolarisExample(ColorPickerWithTransparentValueExample);
