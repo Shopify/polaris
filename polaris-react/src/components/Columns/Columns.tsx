08ec9604dec5d2a212195adb2ba73629ea92202a
Kyle Durand <kyledurand@users.noreply.github.com>
2022-08-31 11:46:30 -0400

    infer spacing scale

--- polaris-react/src/components/Columns/Columns.tsx
+++ polaris-react/src/components/Columns/Columns.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import type {spacing} from '@shopify/polaris-tokens';
 
 import styles from './Columns.scss';
 
 type Breakpoints = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+type SpacingTokenName = keyof typeof spacing;
+
+type SpacingTokenScale = SpacingTokenName extends `space-${infer Scale}`
+  ? Scale
+  : never;
 
 type Columns = {
   [Breakpoint in Breakpoints]?: number | string;
 };
 
 type Gap = {
-  [Breakpoint in Breakpoints]?: string;
+  [Breakpoint in Breakpoints]?: SpacingTokenScale;
 };
 
 interface ColumnsProps {
@@ -25,11 +32,11 @@ export function Columns({columns, children, gap}: ColumnsProps) {
     '--pc-columns-md': formatColumns(columns?.md),
     '--pc-columns-lg': formatColumns(columns?.lg),
     '--pc-columns-xl': formatColumns(columns?.xl),
-    '--pc-columns-gap-xs': gap?.xs,
-    '--pc-columns-gap-sm': gap?.sm,
-    '--pc-columns-gap-md': gap?.md,
-    '--pc-columns-gap-lg': gap?.lg,
-    '--pc-columns-gap-xl': gap?.xl,
+    '--pc-columns-gap-xs': gap?.xs ? `var(--p-space-${gap?.xs})` : undefined,
+    '--pc-columns-gap-sm': gap?.sm ? `var(--p-space-${gap?.sm})` : undefined,
+    '--pc-columns-gap-md': gap?.md ? `var(--p-space-${gap?.md})` : undefined,
+    '--pc-columns-gap-lg': gap?.lg ? `var(--p-space-${gap?.lg})` : undefined,
+    '--pc-columns-gap-xl': gap?.xl ? `var(--p-space-${gap?.xl})` : undefined,
   } as React.CSSProperties;
 
   return (
