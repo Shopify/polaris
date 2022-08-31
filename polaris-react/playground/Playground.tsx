27dae56c61bae7995628c8386223fcdbcfe5b658
Kyle Durand <kyledurand@users.noreply.github.com>
2022-08-31 11:13:28 -0400

    Simplify API

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
 
-import {Page} from '../src';
+import {Page, Columns, Stack} from '../src';
 
 export function Playground() {
   return (
     <Page title="Playground">
-      {/* Add the code you want to test in here */}
+      <Stack vertical>
+        <h2>Equal columns example</h2>
+        <Columns
+          columns={{xs: 2, sm: '1fr 1.5fr', md: 4, lg: 6}}
+          gap={{xs: 'var(--p-space-1)', lg: 'var(--p-space-4)'}}
+        >
+          <div style={{background: 'aquamarine'}}>one</div>
+          <div style={{background: 'aquamarine'}}>two</div>
+          <div style={{background: 'aquamarine'}}>three</div>
+          <div style={{background: 'aquamarine'}}>four</div>
+          <div style={{background: 'aquamarine'}}>five</div>
+          <div style={{background: 'aquamarine'}}>six</div>
+        </Columns>
+
+        <h2>Non equal columns example</h2>
+        <Columns
+          columns={{
+            xs: '1.5fr 0.5fr',
+            sm: '2fr 1fr',
+            md: '1fr 3fr auto 1fr',
+            lg: '1fr 4fr auto 2fr 3fr auto',
+          }}
+          gap={{xs: 20}}
+        >
+          <div style={{background: 'aquamarine'}}>one</div>
+          <div style={{background: 'aquamarine'}}>two</div>
+          <div style={{background: 'aquamarine'}}>three</div>
+          <div style={{background: 'aquamarine'}}>four</div>
+          <div style={{background: 'aquamarine'}}>five</div>
+          <div style={{background: 'aquamarine'}}>six</div>
+        </Columns>
+      </Stack>
     </Page>
   );
 }
--- polaris-react/src/components/Columns/Columns.tsx
+++ polaris-react/src/components/Columns/Columns.tsx
@@ -5,45 +5,31 @@ import styles from './Columns.scss';
 type Breakpoints = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
 type Columns = {
-  [Breakpoint in Breakpoints]?: number;
+  [Breakpoint in Breakpoints]?: number | string;
 };
 
 type Gap = {
   [Breakpoint in Breakpoints]?: string;
 };
 
-type Templates = {
-  [Breakpoint in Breakpoints]?: string;
-};
-
-interface NonMutuallyExclusiveProps {
+interface ColumnsProps {
   gap?: Gap;
+  columns?: Columns;
   children?: React.ReactNode;
 }
 
-export type ColumnsProps = NonMutuallyExclusiveProps &
-  (
-    | {columns?: Columns; templates?: undefined}
-    | {templates?: Templates; columns?: undefined}
-  );
-
-export function Columns({columns, templates, children, gap}: ColumnsProps) {
+export function Columns({columns, children, gap}: ColumnsProps) {
   const style = {
-    '--pc-columns-xs': columns?.xs,
-    '--pc-columns-sm': columns?.sm,
-    '--pc-columns-md': columns?.md,
-    '--pc-columns-lg': columns?.lg,
-    '--pc-columns-xl': columns?.xl,
+    '--pc-columns-xs': formatColumns(columns?.xs),
+    '--pc-columns-sm': formatColumns(columns?.sm),
+    '--pc-columns-md': formatColumns(columns?.md),
+    '--pc-columns-lg': formatColumns(columns?.lg),
+    '--pc-columns-xl': formatColumns(columns?.xl),
     '--pc-columns-gap-xs': gap?.xs,
     '--pc-columns-gap-sm': gap?.sm,
     '--pc-columns-gap-md': gap?.md,
     '--pc-columns-gap-lg': gap?.lg,
     '--pc-columns-gap-xl': gap?.xl,
-    '--pc-columns-templates-xs': templates?.xs,
-    '--pc-columns-templates-sm': templates?.sm,
-    '--pc-columns-templates-md': templates?.md,
-    '--pc-columns-templates-lg': templates?.lg,
-    '--pc-columns-templates-xl': templates?.xl,
   } as React.CSSProperties;
 
   return (
@@ -51,4 +37,12 @@ export function Columns({columns, templates, children, gap}: ColumnsProps) {
       {children}
     </div>
   );
+}
+
+function formatColumns(columns?: number | string) {
+  if (!columns) return undefined;
+
+  return typeof columns === 'number'
+    ? `repeat(${columns}, minmax(0, 1fr))`
+    : columns;
 }
