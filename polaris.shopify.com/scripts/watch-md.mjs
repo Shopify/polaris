9555bcdfdbec139583297fef6c7229bbb8f9310c
Alex Page <hi@alexpage.dev>
2022-09-01 18:26:13 -0700

    Update watch-md.mjs

--- polaris.shopify.com/scripts/watch-md.mjs
+++ polaris.shopify.com/scripts/watch-md.mjs
@@ -5,4 +5,8 @@ import genSiteJson from './gen-site-json.mjs';
 
 const mdPath = path.join(process.cwd(), 'content');
 
+// Run intially
+await genSiteJson();
+
+// Run whenever there is a change to a .md file
 chokidar.watch(mdPath).on('change', async () => await genSiteJson());
