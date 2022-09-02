d87416b246284b80aec592cbceae109f7c653f72
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-09-02 11:26:30 -0400

    Add `Tile` component

---
+++ polaris-react/src/components/Tile/Tile.tsx
@@ -0,0 +1,41 @@
+import React, {Children} from 'react';
+import type {spacing} from '@shopify/polaris-tokens';
+
+import {Box} from '../Box';
+import {classNames} from '../../utilities/css';
+
+import styles from './Tile.scss';
+
+type SpacingTokenGroup = typeof spacing;
+type SpacingTokenName = keyof SpacingTokenGroup;
+
+// TODO: Bring this logic into tokens
+type Spacing = SpacingTokenName extends `space-${infer Scale}` ? Scale : never;
+
+type Columns = '1' | '2' | '3' | '4' | '5' | '6';
+
+export interface TileProps {
+  /** Elements to display inside tile */
+  children: React.ReactNode;
+  /** Adjust spacing between elements */
+  spacing: Spacing;
+  /** Adjust number of columns */
+  columns: Columns;
+}
+
+export const Tile = ({children, spacing, columns}: TileProps) => {
+  const className = classNames(styles.root);
+
+  const style = {
+    '--pc-column-number': `repeat(${columns}, 1fr)`,
+    ...(spacing ? {'--pc-tile-spacing': `var(--p-space-${spacing})`} : {}),
+  } as React.CSSProperties;
+
+  return (
+    <div className={className} style={style}>
+      {Children.map(children, (child) => (
+        <Box>{child}</Box>
+      ))}
+    </div>
+  );
+};
