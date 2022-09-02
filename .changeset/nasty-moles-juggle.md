267e1a9bd8a920fde542ac8e9f1f9118749532ff
Alex Page <hi@alexpage.dev>
2022-09-02 11:45:55 -0700

    Add legacy-scss-api to stylelint test (#6890)
    When looking at `polaris-coverage` there are pages where there should be failures showing up. This didn't work as many values were missing from the deprecated scss API. `color` `spacing` etc.
    
    - [x] Add missing scss functions
    - [x] Add missing scss mixins
    - [x] Add missing global variables
    - [x]  **this is due to string test being an exact match**
      - Investigate why `color(` and `rem(` in screenshot below is not a failure
    - [x] Convert tests to regex as it exact matches strings
    
    
    <img width="614" alt="Screen Shot 2022-08-09 at 6 38 27 PM" src="https://user-images.githubusercontent.com/19199063/183791538-5b52636d-7699-4c79-9cad-a8cddb22a55c.png">
    
    [polaris-coverage website bug](https://polaris-coverage.shopifycloud.com/shopify/web/tree/main/app/components/DevUI/components/DevUITabs/components/Performance/Performance.scss)
    
    ---
    
    The outcome of this PR is a dramatic change in coverage.
    
    ![183802267-5d442b46-157e-4a78-bf5f-bbbd4d1ee877](https://user-images.githubusercontent.com/19199063/183802609-1f5c032a-5095-4561-9d6a-34285657e01d.png)
    
    Co-authored-by: Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>

---
+++ .changeset/nasty-moles-juggle.md
@@ -0,0 +1,5 @@
+---
+'@shopify/stylelint-polaris': minor
+---
+
+Add deprecated scss API to stylelint-polaris and use regex tests in the config
---
+++ stylelint-polaris/plugins/at-rule-disallowed-list/index.js
@@ -0,0 +1,107 @@
+const stylelint = require('stylelint');
+
+const {matchesStringOrRegExp, isRegExp, isString} = require('../../utils');
+
+const ruleName = 'stylelint-polaris/at-rule-disallowed-list';
+
+const messages = stylelint.utils.ruleMessages(ruleName, {
+  /**
+   * @type {stylelint.RuleMessageFunc}
+   */
+  rejected: (atRuleName, atRuleId, disallowedPattern) => {
+    return `Invalid @${atRuleName} rule${
+      atRuleId ? ` [${atRuleId}].` : ''
+    } Disallowed pattern [${disallowedPattern}]`;
+  },
+});
+
+/** @typedef {(string | RegExp)[]} DisallowedPatterns */
+
+/**
+ * @typedef {{[atRuleName: string]: DisallowedPatterns}} PrimaryOptions
+ */
+
+const {rule} = stylelint.createPlugin(
+  ruleName,
+  /** @param {PrimaryOptions} primary */
+  (primary) => {
+    return (root, result) => {
+      const validOptions = stylelint.utils.validateOptions(result, ruleName, {
+        actual: primary,
+        possible: validateAllowedValuesOption,
+      });
+
+      if (!validOptions) {
+        throw new Error(
+          `Invalid options were provided to the [${ruleName}] stylelint plugin.\n`,
+        );
+      }
+
+      root.walkAtRules((atRule) => {
+        const atRuleName = atRule.name;
+        const disallowedPatterns = primary[atRuleName];
+
+        if (!disallowedPatterns) return;
+
+        const atRuleId = atRule.params;
+
+        const found = disallowedPatterns.filter((test) => {
+          return matchesStringOrRegExp(atRuleId, test);
+        });
+
+        if (!found || !found.length) return;
+
+        found.forEach((disallowedPattern) => {
+          stylelint.utils.report({
+            ruleName,
+            result,
+            node: atRule,
+            message: messages.rejected(
+              atRuleName,
+              atRuleId,
+              isString(disallowedPattern)
+                ? disallowedPattern
+                : disallowedPattern.source,
+            ),
+          });
+        });
+      });
+    };
+  },
+);
+
+module.exports = {
+  rule,
+  ruleName,
+  messages,
+};
+
+/**
+ * Validates the input is an array of String or RegExp.
+ * @param {unknown} disallowedPatterns
+ * @returns {disallowedPatterns is DisallowedPatterns}
+ */
+function isDisallowedPatterns(disallowedPatterns) {
+  if (!Array.isArray(disallowedPatterns)) return false;
+
+  for (const pattern of disallowedPatterns) {
+    if (!(isString(pattern) || isRegExp(pattern))) return false;
+  }
+
+  return true;
+}
+
+/**
+ * @param {unknown} option - `primary` option.
+ */
+function validateAllowedValuesOption(option) {
+  if (typeof option !== 'object' || option === null) return false;
+
+  for (const [atRuleName, disallowedPatterns] of Object.entries(option)) {
+    if (!(isString(atRuleName) && isDisallowedPatterns(disallowedPatterns))) {
+      return false;
+    }
+  }
+
+  return true;
+}
