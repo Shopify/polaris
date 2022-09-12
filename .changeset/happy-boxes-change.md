edac52b6ef2a1e681f6575306e22eaaa21818881
Rener Baffa <rener.baffa@shopify.com>
2022-09-12 16:41:37 +0200

    Added an example of Hex code TextField to ColorPicker
    Co-authored-by: Kyle Durand <kyle.durand@shopify.com>

---
+++ .changeset/happy-boxes-change.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris': patch
+---
+
+Added an example of Hex code TextField to ColorPicker
--- polaris-react/src/components/ColorPicker/ColorPicker.stories.tsx
+++ polaris-react/src/components/ColorPicker/ColorPicker.stories.tsx
@@ -1,6 +1,13 @@
 import React, {useState} from 'react';
 import type {ComponentMeta} from '@storybook/react';
-import {ColorPicker} from '@shopify/polaris';
+import {
+  ColorPicker,
+  Stack,
+  TextField,
+  hsbToHex,
+  rgbToHsb,
+  hexToRgb,
+} from '@shopify/polaris';
 
 export default {
   component: ColorPicker,
@@ -36,4 +43,50 @@ export function WithTransparentValueFullWidth() {
   });
 
   return <ColorPicker fullWidth onChange={setColor} color={color} allowAlpha />;
+}
+
+export function WithHexTextField() {
+  const [color, setColor] = useState({
+    hue: 300,
+    brightness: 1,
+    saturation: 0.7,
+  });
+  const [hex, setHex] = useState(hsbToHex(color));
+
+  const handleBlur = React.useCallback(() => {
+    setColor(rgbToHsb(hexToRgb(hex)));
+  }, [hex]);
+
+  React.useEffect(() => {
+    setHex(hsbToHex(color));
+  }, [color]);
+
+  return (
+    <>
+      <ColorPicker onChange={setColor} color={color} />
+      <div style={{paddingTop: 8}}>
+        <Stack>
+          <div
+            style={{
+              width: 32,
+              height: 32,
+              borderRadius: 3,
+              border: '1px solid rgba(0, 0, 0, 0.2)',
+              backgroundColor: hsbToHex(color),
+            }}
+          />
+          <div style={{width: 143}}>
+            <TextField
+              label=""
+              prefix="#"
+              value={hex.replace('#', '')}
+              onChange={setHex}
+              onBlur={handleBlur}
+              autoComplete="off"
+            />
+          </div>
+        </Stack>
+      </div>
+    </>
+  );
 }
