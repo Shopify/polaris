3d97f9b57803815a60619eb2da6852dce89b9891
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-09-13 14:11:05 -0400

    Refactor `Tile` stories

--- polaris-react/src/components/Tile/Tile.stories.tsx
+++ polaris-react/src/components/Tile/Tile.stories.tsx
@@ -1,51 +1,33 @@
 import React from 'react';
 import type {ComponentMeta} from '@storybook/react';
-import {Tile, Text} from '@shopify/polaris';
+import {Box, Tile, Text} from '@shopify/polaris';
 
 export default {
   component: Tile,
 } as ComponentMeta<typeof Tile>;
 
 const styles = {
-  border: '1px solid grey',
-  padding: '5px',
+  background: 'var(--p-surface)',
+  border: 'var(--p-border-base)',
+  borderRadius: 'var(--p-border-radius-2)',
+  padding: 'var(--p-space-4)',
 };
+
+const children = Array.from(Array(4)).map((ele, index) => (
+  <div key={index} style={styles}>
+    <Text as="h2" variant="headingMd">
+      Sales
+    </Text>
+    <Text as="p" variant="bodyMd">
+      View a summary of your online store’s sales.
+    </Text>
+  </div>
+));
 
 export function Default() {
   return (
     <Tile spacing="1" columns="2">
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
+      {children}
     </Tile>
   );
 }
@@ -53,125 +35,25 @@ export function Default() {
 export function LargeSpacing() {
   return (
     <Tile spacing="10" columns="2">
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
+      {children}
     </Tile>
   );
 }
 
 export function ManyColumns() {
+  const children = Array.from(Array(10)).map((ele, index) => (
+    <div key={index} style={styles}>
+      <Text as="h2" variant="headingMd">
+        Sales
+      </Text>
+      <Text as="p" variant="bodyMd">
+        View a summary of your online store’s sales.
+      </Text>
+    </div>
+  ));
   return (
     <Tile spacing="3" columns="5">
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
-      <div style={styles}>
-        <Text as="h2" variant="headingMd">
-          Sales
-        </Text>
-        <Text as="p" variant="bodyMd">
-          View a summary of your online store’s sales.
-        </Text>
-      </div>
+      {children}
     </Tile>
   );
 }
