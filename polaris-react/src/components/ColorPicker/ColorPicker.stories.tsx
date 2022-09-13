b5e4b750d56a044eee755c6f7b0c77cafc6a3eb3
Kyle Durand <kyledurand@users.noreply.github.com>
2022-09-13 17:29:37 -0400

    Adjust layout

--- polaris-react/src/components/ColorPicker/ColorPicker.stories.tsx
+++ polaris-react/src/components/ColorPicker/ColorPicker.stories.tsx
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import type {ComponentMeta} from '@storybook/react';
 import {
   ColorPicker,
-  Stack,
   TextField,
   hsbToHex,
   rgbToHsb,
@@ -48,45 +47,60 @@ export function WithTransparentValueFullWidth() {
 export function WithHexTextField() {
   const [color, setColor] = useState({
     hue: 300,
-    brightness: 1,
     saturation: 0.7,
+    brightness: 1,
   });
+
   const [hex, setHex] = useState(hsbToHex(color));
 
-  const handleBlur = React.useCallback(() => {
+  const handleBlur = useCallback(() => {
     setColor(rgbToHsb(hexToRgb(hex)));
   }, [hex]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     setHex(hsbToHex(color));
   }, [color]);
 
+  const styles = {
+    Wrapper: {
+      display: 'grid',
+      gap: 'var(--p-space-2)',
+      gridTemplateColumns: 'auto 1fr',
+      width: 'fit-content',
+    },
+    Picker: {
+      gridColumn: '1 / 3',
+    },
+    Tile: {
+      minHeight: '100%',
+      aspectRatio: '1 / 1',
+      borderRadius: 'var(--p-border-radius-1)',
+      border: 'var(--p-border-divider)',
+      backgroundColor: hsbToHex(color),
+    },
+    TextField: {
+      maxWidth: 'fit-content',
+    },
+  };
+
   return (
-    <>
-      <ColorPicker onChange={setColor} color={color} />
-      <div style={{paddingTop: 8}}>
-        <Stack>
-          <div
-            style={{
-              width: 32,
-              height: 32,
-              borderRadius: 3,
-              border: '1px solid rgba(0, 0, 0, 0.2)',
-              backgroundColor: hsbToHex(color),
-            }}
-          />
-          <div style={{width: 143}}>
-            <TextField
-              label=""
-              prefix="#"
-              value={hex.replace('#', '')}
-              onChange={setHex}
-              onBlur={handleBlur}
-              autoComplete="off"
-            />
-          </div>
-        </Stack>
+    <div style={styles.Wrapper}>
+      <div style={styles.Picker}>
+        <ColorPicker onChange={setColor} color={color} />
       </div>
-    </>
+
+      <div style={styles.Tile} />
+
+      <div style={styles.TextField}>
+        <TextField
+          label=""
+          prefix="#"
+          value={hex.replace('#', '')}
+          onChange={setHex}
+          onBlur={handleBlur}
+          autoComplete="off"
+        />
+      </div>
+    </div>
   );
 }
