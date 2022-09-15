0211614ab07e0d1a4c052679f203e85c31090462
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-09-15 10:41:06 -0400

    Add `Bleed` stories

--- polaris-react/src/components/Bleed/Bleed.stories.tsx
+++ polaris-react/src/components/Bleed/Bleed.stories.tsx
@@ -6,13 +6,58 @@ export default {
   component: Bleed,
 } as ComponentMeta<typeof Bleed>;
 
+const styles = {
+  background: 'var(--p-surface-neutral-subdued-dark)',
+  borderRadius: 'var(--p-border-radius-05)',
+  padding: 'var(--p-space-4)',
+  height: 'var(--p-space-12)',
+};
+
 export function Default() {
   return (
-    <Box background="surface" padding="5">
+    <Box background="surface" padding="4">
       <Bleed>
-        <Box background="surface-dark" padding="5">
-          Bleed
-        </Box>
+        <div style={styles} />
+      </Bleed>
+    </Box>
+  );
+}
+
+export function WithVerticalDirection() {
+  return (
+    <Box background="surface" padding="4">
+      <Bleed vertical="6">
+        <div style={styles} />
+      </Bleed>
+    </Box>
+  );
+}
+
+export function WithHorizontalDirection() {
+  return (
+    <Box background="surface" padding="4">
+      <Bleed horizontal="6">
+        <div style={styles} />
+      </Bleed>
+    </Box>
+  );
+}
+
+export function WithSpecificDirection() {
+  return (
+    <Box background="surface" padding="4">
+      <Bleed top="6">
+        <div style={styles} />
+      </Bleed>
+    </Box>
+  );
+}
+
+export function WithAllDirection() {
+  return (
+    <Box background="surface" padding="4">
+      <Bleed space="6">
+        <div style={styles} />
       </Bleed>
     </Box>
   );
