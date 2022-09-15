984bcd0fcd44006c78d4acd0aa0844d12f303d49
Kyle Durand <kyledurand@users.noreply.github.com>
2022-09-15 15:40:42 -0400

    [Box] Set defaults

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-import {Page} from '../src';
+import {Page, Box} from '../src';
 
 export function Playground() {
+  const Card = ({children}: {children: React.ReactNode}) => (
+    <Box background="surface" padding="2" border="base" borderRadius="1">
+      {children}
+    </Box>
+  );
+
   return (
     <Page title="Playground">
-      {/* Add the code you want to test in here */}
+      <Card>
+        <Box>elemenet</Box>
+        <Box>elemenet</Box>
+      </Card>
     </Page>
   );
 }
