2d694702e6407b1987d888f51b5473b31ccb3899
aveline <aveline@users.noreply.github.com>
2022-09-22 14:16:06 -0700

    Create AlphaCard.stories.tsx

---
+++ polaris-react/src/components/AlphaCard/AlphaCard.stories.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import type {ComponentMeta} from '@storybook/react';
+import {AlphaCard, AlphaStack, Text} from '@shopify/polaris';
+
+export default {
+  component: AlphaCard,
+} as ComponentMeta<typeof AlphaCard>;
+
+export function Default() {
+  return (
+    <AlphaCard>
+      <AlphaStack spacing="5">
+        <Text as="h3" variant="headingMd">
+          Online store dashboard
+        </Text>
+        <p>View a summary of your online store’s performance.</p>
+      </AlphaStack>
+    </AlphaCard>
+  );
+}
+
+export function BackgroundSubdued() {
+  return (
+    <AlphaCard backgroundColor="surface-subdued">
+      <AlphaStack spacing="5">
+        <Text as="h3" variant="headingMd">
+          Online store dashboard
+        </Text>
+        <p>View a summary of your online store’s performance.</p>
+      </AlphaStack>
+    </AlphaCard>
+  );
+}
+
+export function BorderRadius() {
+  return (
+    <AlphaCard borderRadius="4">
+      <AlphaStack spacing="5">
+        <Text as="h3" variant="headingMd">
+          Online store dashboard
+        </Text>
+        <p>View a summary of your online store’s performance.</p>
+      </AlphaStack>
+    </AlphaCard>
+  );
+}
+
+export function BorderRadiusAbove() {
+  return (
+    <AlphaCard roundedAbove="sm">
+      <AlphaStack spacing="5">
+        <Text as="h3" variant="headingMd">
+          Online store dashboard
+        </Text>
+        <p>View a summary of your online store’s performance.</p>
+      </AlphaStack>
+    </AlphaCard>
+  );
+}
+
+export function Flat() {
+  return (
+    <AlphaCard elevation="transparent">
+      <AlphaStack spacing="5">
+        <Text as="h3" variant="headingMd">
+          Online store dashboard
+        </Text>
+        <p>View a summary of your online store’s performance.</p>
+      </AlphaStack>
+    </AlphaCard>
+  );
+}
