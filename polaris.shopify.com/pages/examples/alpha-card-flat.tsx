baf9ebad68231ea8a71e54d01bfbab724b2cb60e
aveline <aveline@users.noreply.github.com>
2022-09-22 14:59:23 -0700

    Add `AlphaCard` page to styleguide

---
+++ polaris.shopify.com/pages/examples/alpha-card-flat.tsx
@@ -0,0 +1,18 @@
+import {AlphaCard, Text, AlphaStack} from '@shopify/polaris';
+import React from 'react';
+import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
+
+function AlphaCardExample() {
+  return (
+    <AlphaCard elevation="transparent">
+      <AlphaStack spacing="5">
+        <Text as="h3" variant="headingMd">
+          Online store dashboard
+        </Text>
+        <p>View a summary of your online store’s performance.</p>
+      </AlphaStack>
+    </AlphaCard>
+  );
+}
+
+export default withPolarisExample(AlphaCardExample);
