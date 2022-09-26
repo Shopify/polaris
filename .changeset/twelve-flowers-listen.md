249934d001dd5e86801d85909ce2c4d9087d6323
Matt Gregg <matt.gregg@shopify.com>
2022-09-26 16:47:00 -0500

    Add changeset and remove playground code

---
+++ .changeset/twelve-flowers-listen.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris': minor
+---
+
+add onVisibilityChange callback for Tooltip component
--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 
-import {Page, Tooltip, TextStyle} from '../src';
+import {Page} from '../src';
 
 export function Playground() {
-  const callMonorailEventEmitter = (tooltipActivated: boolean) => {
-    if (tooltipActivated) console.log('Calling monorail event emitter!!');
-  };
-
   return (
     <Page title="Playground">
-      <Tooltip
-        active
-        content="This order has shipping labels."
-        onVisibilityChange={callMonorailEventEmitter}
-      >
-        <TextStyle variation="strong">Order #1001</TextStyle>
-      </Tooltip>
+      {/* Add the code you want to test in here */}
     </Page>
   );
 }
