9635813582030159f4c102242420fde3a6364808
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-09-26 10:59:56 -0700

    Update renameProps to avoid migrating nested component props (#7269)

--- polaris-migrator/src/migrations/rename-component-prop/tests/rename-component-prop.output.tsx
+++ polaris-migrator/src/migrations/rename-component-prop/tests/rename-component-prop.output.tsx
@@ -6,11 +6,18 @@ interface MyComponentProps {
   children?: React.ReactNode;
 }
 
+const Child = (props: {prop: string}) => <>{props.prop}</>;
+
 function MyComponent(props: MyComponentProps) {
   const value = props.newProp || props.prop;
   return <div data-prop={value}>{props.children}</div>;
 }
 
 export function App() {
-  return <MyComponent newProp="value">Hello</MyComponent>;
+  return (
+    <MyComponent newProp="value">
+      Hello
+      <Child prop="value" />
+    </MyComponent>
+  );
 }
