82a4e7b672269c4caf9c217fc05e0e65c6eba04d
Jess Telford <jess.telford@shopify.com>
2022-09-28 14:17:02 +1000

    Support all css sizes for icons on docs site

--- polaris.shopify.com/src/components/Icon/Icon.tsx
+++ polaris.shopify.com/src/components/Icon/Icon.tsx
@@ -2,8 +2,8 @@ import styles from './Icon.module.scss';
 
 interface Props {
   source: React.ElementType;
-  width?: number;
-  height?: number;
+  width?: number | string;
+  height?: number | string;
 }
 
 function Icon({source, height = 20, width = 20}: Props) {
