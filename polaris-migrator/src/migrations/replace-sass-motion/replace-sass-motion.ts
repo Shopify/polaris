d9f5d3c717fe70c8c70f2c564f37580c0f0b9242
Jess Telford <jess.telford@shopify.com>
2022-10-11 17:15:40 +1100

    Scaffold replace-sass-motion migration with set of sensible tests

---
+++ polaris-migrator/src/migrations/replace-sass-motion/replace-sass-motion.ts
@@ -0,0 +1,33 @@
+import type {FileInfo} from 'jscodeshift';
+import postcss, {Plugin} from 'postcss';
+import valueParser from 'postcss-value-parser';
+
+const processed = Symbol('processed');
+
+const plugin = (): Plugin => ({
+  postcssPlugin: 'replace-sass-motion',
+  Declaration(decl) {
+    // @ts-expect-error - Skip if processed so we don't process it again
+    if (decl[processed]) return;
+
+    // const prop = decl.prop;
+    const parsedValue = valueParser(decl.value);
+
+    parsedValue.walk((node) => {
+      if (!(node.type === 'function' && node.value === 'hello')) return;
+
+      node.value = 'world';
+    });
+
+    decl.value = parsedValue.toString();
+
+    // @ts-expect-error - Mark the declaration as processed
+    decl[processed] = true;
+  },
+});
+
+export default function replaceSassMotion(fileInfo: FileInfo) {
+  return postcss(plugin()).process(fileInfo.source, {
+    syntax: require('postcss-scss'),
+  }).css;
+}
