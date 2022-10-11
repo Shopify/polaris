349cc989dbd3ff8435eb73f83f1ee1d163394383
aveline <aveline@users.noreply.github.com>
2022-10-11 14:08:07 -0700

    Remove setting media width globally

--- polaris-react/tests/setup/tests.ts
+++ polaris-react/tests/setup/tests.ts
@@ -1,19 +1,6 @@
 import {destroyAll} from '@shopify/react-testing';
-import {matchMedia} from '@shopify/jest-dom-mocks';
 import '@shopify/react-testing/matchers';
 import './matchers';
-import {setMediaWidth} from 'tests/utilities/breakpoints';
-
-// eslint-disable-next-line jest/require-top-level-describe
-beforeAll(() => {
-  matchMedia.mock();
-  setMediaWidth('breakpoints-md');
-});
-
-// eslint-disable-next-line jest/require-top-level-describe
-afterAll(() => {
-  matchMedia.restore();
-});
 
 // eslint-disable-next-line jest/require-top-level-describe
 afterEach(() => {
