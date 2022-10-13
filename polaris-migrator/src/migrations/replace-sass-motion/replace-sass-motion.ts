f40484b406cca9ccdc5f1ad24cea3603083c27a1
Jess Telford <jess.telford@shopify.com>
2022-10-13 17:25:09 +1100

    WIP some structuring to the motion processing

--- polaris-migrator/src/migrations/replace-sass-motion/replace-sass-motion.ts
+++ polaris-migrator/src/migrations/replace-sass-motion/replace-sass-motion.ts
@@ -1,20 +1,25 @@
 import type {FileInfo} from 'jscodeshift';
 import postcss, {Plugin, Declaration, Helpers} from 'postcss';
-import valueParser from 'postcss-value-parser';
+import valueParser, {ParsedValue, Node} from 'postcss-value-parser';
+
+import {
+  NamespaceOptions,
+  namespace,
+  isSassFunction,
+} from '../../utilities/sass';
+import {isKeyOf} from '../../utilities/type-guards';
 
 const processed = Symbol('processed');
+const DEFAULT_DURATION = 'base';
 
-/*
-function mutateTransitionDurationProp(decl): void {}
-function mutateTransitionDelayProp(decl): void {}
-function mutateTransitionProp(decl): void {}
+const durationMap = {
+  // TODO
+  none: '--p-duration-0',
+};
 
-function mutateTransitionDurationValue(decl, node: unknown): void {}
-function mutateTransitionDelayValue(decl, node: unknown): void {}
-*/
 interface ParsedValueDeclaration extends Declaration {
   [processed]?: boolean;
-  parsedValue: valueParser.ParsedValue;
+  parsedValue: ParsedValue;
 }
 
 // postcss doesn't export this, so had to extract it to here
@@ -44,23 +49,48 @@ function withParsedValue(
   }) as DeclarationProcessor;
 }
 
-const plugin = (): Plugin => ({
-  postcssPlugin: 'replace-sass-motion',
-  Declaration: {
-    'transition-duration': withParsedValue((decl) => {
-      console.log('transition-duration');
-      // decl.parsedValue.walk((node) => {
-      //   console.log(JSON.stringify(node, null, 2));
-      // });
-    }),
-    'transition-delay': withParsedValue((decl) => {
-      console.log('transition-delay');
-    }),
-    transition: withParsedValue((decl) => {
-      console.log('transition');
-    }),
-  },
-});
+interface PluginOptions extends Options, NamespaceOptions {}
+
+const plugin = (options: PluginOptions = {}): Plugin => {
+  const durationFunc = namespace('duration', options);
+
+  function mutateTransitionDurationValue(
+    node: Node,
+    //decl: ParsedValueDeclaration,
+  ): void {
+    if (isSassFunction(durationFunc, node)) {
+      const duration = node.nodes[0]?.value ?? DEFAULT_DURATION;
+      if (!isKeyOf(durationMap, duration)) {
+        // TODO: Insert a comment that this function invocation has an invalid
+        // parameter
+      }
+    } else if (true /* isConstantValue */) {
+      // TODO
+    } else if (true /* isExpression */) {
+      // TODO
+    }
+  }
+
+  //function mutateTransitionDelayValue(decl, node: unknown): void {}
+
+  return {
+    postcssPlugin: 'replace-sass-motion',
+    Declaration: {
+      'transition-duration': withParsedValue((decl) => {
+        console.log('transition-duration');
+        decl.parsedValue.walk((node) => {
+          mutateTransitionDurationValue(node, decl);
+        });
+      }),
+      'transition-delay': withParsedValue((decl) => {
+        console.log('transition-delay');
+      }),
+      transition: withParsedValue((decl) => {
+        console.log('transition');
+      }),
+    },
+  };
+};
 
 export default function replaceSassMotion(fileInfo: FileInfo) {
   return postcss(plugin()).process(fileInfo.source, {
