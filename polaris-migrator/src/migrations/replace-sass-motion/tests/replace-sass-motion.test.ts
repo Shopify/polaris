3cceb0a36bf6418de8c018d7aa48f42a1dde4528
Jess Telford <jess.telford@shopify.com>
2022-10-14 17:42:40 +1100

    Working duration migration for motion

--- polaris-migrator/src/migrations/replace-sass-motion/tests/replace-sass-motion.test.ts
+++ polaris-migrator/src/migrations/replace-sass-motion/tests/replace-sass-motion.test.ts
@@ -1,12 +1,17 @@
 import {check} from '../../../utilities/testUtils';
 
 const migration = 'replace-sass-motion';
-const fixtures = ['replace-sass-duration', 'replace-sass-easing'];
+// const fixtures = ['debug'];
+// const fixtures = ['replace-sass-duration', 'replace-sass-easing'];
+const fixtures = ['replace-sass-duration'];
 
 for (const fixture of fixtures) {
   check(__dirname, {
     fixture,
     migration,
     extension: 'scss',
+    options: {
+      namespace: 'legacy-polaris-v8',
+    },
   });
 }
