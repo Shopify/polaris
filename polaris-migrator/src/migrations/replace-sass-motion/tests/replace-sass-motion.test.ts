cf6fedfa6aef4b7bdd0e53dbad993669ea6fa6ef
Jess Telford <jess.telford@shopify.com>
2022-10-14 17:52:46 +1100

    Add easing tests

--- polaris-migrator/src/migrations/replace-sass-motion/tests/replace-sass-motion.test.ts
+++ polaris-migrator/src/migrations/replace-sass-motion/tests/replace-sass-motion.test.ts
@@ -1,6 +1,7 @@
 import {check} from '../../../utilities/testUtils';
 
 const migration = 'replace-sass-motion';
+// const fixtures = ['replace-sass-duration', 'replace-sass-easing'];
 const fixtures = ['replace-sass-duration'];
 
 for (const fixture of fixtures) {
