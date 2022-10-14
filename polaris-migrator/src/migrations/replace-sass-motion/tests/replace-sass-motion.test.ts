922ef375ce9331eb9dec57a6204efe68458d6160
Jess Telford <jess.telford@shopify.com>
2022-10-14 17:51:45 +1100

    Remove easing from this branch

--- polaris-migrator/src/migrations/replace-sass-motion/tests/replace-sass-motion.test.ts
+++ polaris-migrator/src/migrations/replace-sass-motion/tests/replace-sass-motion.test.ts
@@ -1,8 +1,6 @@
 import {check} from '../../../utilities/testUtils';
 
 const migration = 'replace-sass-motion';
-// const fixtures = ['debug'];
-// const fixtures = ['replace-sass-duration', 'replace-sass-easing'];
 const fixtures = ['replace-sass-duration'];
 
 for (const fixture of fixtures) {
