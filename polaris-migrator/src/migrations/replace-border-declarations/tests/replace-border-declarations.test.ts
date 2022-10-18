0c4a8f9ad0473394d73cc3c09a6a706a2c98192e
Laura Griffee <laura@mailzone.com>
2022-10-18 10:22:30 -0600

    Add namespace to test fixtures

--- polaris-migrator/src/migrations/replace-border-declarations/tests/replace-border-declarations.test.ts
+++ polaris-migrator/src/migrations/replace-border-declarations/tests/replace-border-declarations.test.ts
@@ -1,12 +1,17 @@
 import {check} from '../../../utilities/testUtils';
 
 const migration = 'replace-border-declarations';
-const fixtures = ['replace-border-declarations'];
+const fixtures = ['replace-border-declarations', 'with-namespace'];
 
 for (const fixture of fixtures) {
   check(__dirname, {
     fixture,
     migration,
     extension: 'scss',
+    options: {
+      namespace: fixture.includes('with-namespace')
+        ? 'legacy-polaris-v8'
+        : undefined,
+    },
   });
 }
