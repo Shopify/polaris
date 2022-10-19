22b76ab9487b95c71c8848d38f014689c84f896c
Lo Kim <lo.kim@shopify.com>
2022-10-19 13:25:15 -0400

    [Modal.Header] Rebuild with layout primitives

--- polaris-react/src/components/Modal/components/Header/Header.tsx
+++ polaris-react/src/components/Modal/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import {DisplayText} from '../../../DisplayText';
+import {Box} from '../../../Box';
+import {Columns} from '../../../Columns';
+import {Inline} from '../../../Inline';
+import {Text} from '../../../Text';
 import {CloseButton} from '../CloseButton';
 
 import styles from './Header.scss';
@@ -13,16 +16,36 @@ export interface HeaderProps {
 }
 
 export function Header({id, titleHidden, children, onClose}: HeaderProps) {
-  return (
-    <div
-      className={titleHidden || !children ? styles.titleHidden : styles.Header}
-    >
-      <div id={id} className={styles.Title}>
-        <DisplayText element="h2" size="small">
-          {children}
-        </DisplayText>
-      </div>
-      <CloseButton titleHidden={titleHidden} onClick={onClose} />
+  const titleHiddenMarkup = (
+    <div className={styles.titleHidden}>
+      <Inline align="end">
+        <CloseButton titleHidden={titleHidden} onClick={onClose} />
+      </Inline>
     </div>
+  );
+
+  if (titleHidden || !children) {
+    return titleHiddenMarkup;
+  }
+
+  return (
+    <Box
+      paddingTop="4"
+      paddingBottom="4"
+      paddingLeft="5"
+      paddingRight="5"
+      borderBottom="divider"
+    >
+      <Columns columns={{xs: '1fr auto'}}>
+        <Inline alignY="center">
+          <Box id={id}>
+            <Text variant="headingLg" as="h2" fontWeight="regular" breakWord>
+              {children}
+            </Text>
+          </Box>
+        </Inline>
+        <CloseButton titleHidden={titleHidden} onClick={onClose} />
+      </Columns>
+    </Box>
   );
 }
