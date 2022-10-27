9bbf3b463502edf347e529f3b2571fdb5ab7ef54
Kyle Durand <kyledurand@users.noreply.github.com>
2022-10-27 14:53:16 -0400

    throw error on fail

--- polaris.shopify.com/scripts/get-styles.mjs
+++ polaris.shopify.com/scripts/get-styles.mjs
@@ -4,7 +4,9 @@ const builtStylesPath = '../polaris-react/build/esm/styles.css';
 const fileDestination = 'public/polaris-styles.css';
 
 fs.copyFile(builtStylesPath, fileDestination, (error) => {
-  if (error) console.log(`Error copying polaris styles: ${error}`);
+  if (error) {
+    throw new Error(`Error copying styles file: ${error}`);
+  }
 
   console.log('✅ Copied polaris styles');
 });
