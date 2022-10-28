c5e2cc12ae39d1a56efe73a1819c43db9c33bd97
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-10-28 15:06:08 +0200

    Update polaris.shopify.com/pages/examples/columns-with-set-number.tsx
    Co-authored-by: Lo Kim <lo.kim@shopify.com>

--- polaris.shopify.com/pages/examples/columns-with-set-number.tsx
+++ polaris.shopify.com/pages/examples/columns-with-set-number.tsx
@@ -48,7 +48,7 @@ const Placeholder = ({label = '', height = 'auto', width = 'auto'}) => {
         width: width ?? undefined,
       }}
     >
-      <Inline align={'center'} alignY={'center'}>
+      <Inline align="center" alignY="center">
         <div
           style={{
             color: '#FFFFFF',
