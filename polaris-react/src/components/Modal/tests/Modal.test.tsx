00b61115d9d099e410673134fc45ac1a3609a34c
Lo Kim <lo.kim@shopify.com>
2022-10-31 11:46:00 -0400

    [Modal] Rebuild with layout primitives (#7454)
    Resolves #7340.
    Refactors `Modal` and its child components to use our layout primitives.
    
    [Storybook
    URL](https://5d559397bae39100201eedc1-nrruulmwsh.chromatic.com/?path=/story/all-components-modal--default).
    Refactors `Modal`, `Modal.Section`, `Modal.Header`, and `Modal.Footer`
    to use our layout primitives and remove css.
    
    [Storybook
    URL](https://5d559397bae39100201eedc1-nrruulmwsh.chromatic.com/?path=/story/all-components-modal--default).
    I've tophatted all of our stories as well as the UI for all of our
    breakpoints.
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    - [x] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [x] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [x] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [ ] Updated the component's `README.md` with documentation changes
    - [ ] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris-react/src/components/Modal/tests/Modal.test.tsx
+++ polaris-react/src/components/Modal/tests/Modal.test.tsx
@@ -2,12 +2,14 @@ import React, {useRef} from 'react';
 import {animationFrame} from '@shopify/jest-dom-mocks';
 import {mountWithApp} from 'tests/utilities';
 
+import {Backdrop} from '../../Backdrop';
 import {Badge} from '../../Badge';
+import {Box} from '../../Box';
 import {Button} from '../../Button';
+import {Portal} from '../../Portal';
 import {Scrollable} from '../../Scrollable';
 import {Spinner} from '../../Spinner';
-import {Portal} from '../../Portal';
-import {Backdrop} from '../../Backdrop';
+import {Text} from '../../Text';
 import {Footer, Dialog, Header} from '../components';
 import {Modal} from '../Modal';
 import {WithinContentContext} from '../../../utilities/within-content-context';
@@ -302,9 +304,7 @@ describe('<Modal>', () => {
         <Modal onClose={jest.fn()} open title="foo" />,
       );
 
-      expect(modal.find(Header)).toContainReactComponent('div', {
-        className: 'Header',
-      });
+      expect(modal.find(Box)).toContainReactComponent(Text);
     });
 
     it('only renders a close button when titleHidden is present', () => {
@@ -315,6 +315,7 @@ describe('<Modal>', () => {
       expect(modal.find(Header)).toContainReactComponent('div', {
         className: 'titleHidden',
       });
+      expect(modal.find(Header)).not.toContainReactComponent(Box);
     });
   });
 
