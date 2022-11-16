10dd6f808023a4f1015b25231df73dda560f9ec6
Chloe Rice <chloe.rice@shopify.com>
2022-11-16 16:04:52 -0500

    Update coverage plugin to use customMessages and ruleMetadata

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,9 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
-  extends: ['@shopify/stylelint-plugin/prettier', './stylelint-polaris'],
-  // Disabling @shopify/stylelint-plugin/configs/core no-unknown-animations as stylelint
-  // is not aware of global Polaris keyframes
+  extends: [/*'@shopify/stylelint-plugin/prettier', */ './stylelint-polaris'],
+  // Disabling @shopify/stylelint-plugin/configs/core no-unknown-animations as stylelint is not aware of global Polaris keyframes
   // TODO: create custom plugin to ensure animation-names match Polaris keyframe names
+  customSyntax: 'postcss-scss',
   rules: {
     'no-unknown-animations': null,
     'value-keyword-case': ['lower', {camelCaseSvgKeywords: true}],
--- stylelint-polaris/index.js
+++ stylelint-polaris/index.js
@@ -4,17 +4,7 @@ const {getCustomPropertyNames, tokens} = require('@shopify/polaris-tokens');
 const stylelintPolarisCoverageOptions = {
   colors: {
     'color-named': 'never',
-    'color-no-hex': [
-      true,
-      {
-        // TODO: Add custom meta support in coverage.js
-        meta: {
-          url: 'https://polaris.shopify.com/tokens/color',
-        },
-        message: (hex) =>
-          `Unexpected use of hex code: value "${hex}" should be replaced with a Polaris color token.`,
-      },
-    ],
+    'color-no-hex': true,
     'scss/function-color-relative': true,
     'declaration-property-value-disallowed-list': {
       opacity: [/(?!0|1)\d$|^\d{2,}|^[1-9]+\.|^\d+\.\d+\.|^0\.\d{3,}/],
