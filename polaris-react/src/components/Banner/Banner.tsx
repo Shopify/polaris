Shopify/polaris

commit b3e73ee045ba5a90fbc930074b4e8255d079b0d5

Add defaults to bleed (#7644)
Bleed should do something by default. This offsets the default
horizontal padding from AlphaCard which I think will be the most common
use case

Part of https://github.com/Shopify/polaris/issues/7592

polaris-react/src/components/Bleed/Bleed.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type {ComponentMeta} from '@storybook/react';
-import {Bleed, Box} from '@shopify/polaris';
+import {AlphaCard, Bleed, Box, Text} from '@shopify/polaris';
 
 export default {
   component: Bleed,
@@ -15,11 +15,19 @@ const styles = {
 
 export function Default() {
   return (
-    <Box background="surface" padding="4">
+    <AlphaCard>
+      <Text as="p" variant="bodySm">
+        Section 01
+      </Text>
       <Bleed>
-        <div style={styles} />
+        <Box paddingBlockStart="2" paddingBlockEnd="2">
+          <Box borderBlockStart="base" />
+        </Box>
       </Bleed>
-    </Box>
+      <Text as="p" variant="bodySm">
+        Section 02
+      </Text>
+    </AlphaCard>
   );
 }
 

polaris-react/src/components/AlphaCard/AlphaCard.tsx
@@ -49,7 +49,7 @@ export const AlphaCard = ({
       background={background}
       padding={padding}
       shadow="card"
-      {...(hasBorderRadius && {borderRadius: defaultBorderRadius})}
+      borderRadius={hasBorderRadius ? defaultBorderRadius : undefined}
     >
       {children}
     </Box>

polaris-react/src/components/Bleed/Bleed.tsx
@@ -10,7 +10,9 @@ export interface BleedProps {
   children: React.ReactNode;
   /** Negative space around the element */
   spacing?: SpacingSpaceScale;
-  /** Negative horizontal space around the element */
+  /** Negative horizontal space around the element
+   * * @default '5'
+   */
   horizontal?: SpacingSpaceScale;
   /** Negative vertical space around the element */
   vertical?: SpacingSpaceScale;
@@ -26,7 +28,7 @@ export interface BleedProps {
 
 export const Bleed = ({
   spacing,
-  horizontal,
+  horizontal = '5',
   vertical,
   top,
   bottom,

polaris-react/src/components/Bleed/tests/Bleed.test.tsx
@@ -16,14 +16,19 @@ describe('<Bleed />', () => {
     expect(bleed).toContainReactComponent(Children);
   });
 
-  it('does not render custom properties by default', () => {
+  it('renders default horizontal custom properties', () => {
     const bleed = mountWithApp(
       <Bleed>
         <Children />
       </Bleed>,
     );
 
-    expect(bleed).toContainReactComponent('div', {style: undefined});
+    expect(bleed).toContainReactComponent('div', {
+      style: {
+        '--pc-bleed-margin-left': 'var(--p-space-5)',
+        '--pc-bleed-margin-right': 'var(--p-space-5)',
+      } as React.CSSProperties,
+    });
   });
 
   it('only renders the custom property that matches the property passed in', () => {
@@ -36,6 +41,7 @@ describe('<Bleed />', () => {
     expect(bleed).toContainReactComponent('div', {
       style: {
         '--pc-bleed-margin-left': 'var(--p-space-2)',
+        '--pc-bleed-margin-right': 'var(--p-space-5)',
       } as React.CSSProperties,
     });
   });

commit e1679bfbc35a4d44e8115501507cd60333c0957d

Fix `Bleed` regressions (#7705)
I'm still of the opinion that our layout components should have some
defaults and the ones we have on bleed may not be right but we need to
use it in more places to figure out what those defaults should be.

For now, this fixes regressions to Banner that were introduced in
https://github.com/Shopify/polaris/pull/7644

polaris-react/src/components/Banner/Banner.tsx
@@ -174,7 +174,7 @@ export const Banner = forwardRef<BannerHandles, BannerProps>(function Banner(
           <Icon source={iconName} color={iconColor} />
         </Box>
 
-        <Bleed top="05">
+        <Bleed horizontal="0" top="05">
           {headingMarkup}
           {contentMarkup}
         </Bleed>

polaris-react/src/components/Bleed/Bleed.stories.tsx
@@ -34,7 +34,7 @@ export function Default() {
 export function WithVerticalDirection() {
   return (
     <Box background="surface" padding="4">
-      <Bleed vertical="6">
+      <Bleed horizontal="0" vertical="6">
         <div style={styles} />
       </Bleed>
     </Box>
@@ -54,7 +54,7 @@ export function WithHorizontalDirection() {
 export function WithSpecificDirection() {
   return (
     <Box background="surface" padding="4">
-      <Bleed top="6">
+      <Bleed horizontal="0" top="6">
         <div style={styles} />
       </Bleed>
     </Box>

polaris.shopify.com/pages/examples/bleed-specific-direction.tsx
@@ -7,22 +7,22 @@ function BleedSpecificDirectionExample() {
   return (
     <AlphaStack spacing="6" fullWidth>
       <Box background="surface" border="base" padding="4">
-        <Bleed top="4">
+        <Bleed horizontal="0" top="4">
           <Placeholder label="Top" />
         </Bleed>
       </Box>
       <Box background="surface" border="base" padding="4">
-        <Bleed bottom="4">
+        <Bleed horizontal="0" bottom="4">
           <Placeholder label="Bottom" />
         </Bleed>
       </Box>
       <Box background="surface" border="base" padding="4">
-        <Bleed left="4">
+        <Bleed horizontal="0" left="4">
           <Placeholder label="Left" />
         </Bleed>
       </Box>
       <Box background="surface" border="base" padding="4">
-        <Bleed right="4">
+        <Bleed horizontal="0" right="4">
           <Placeholder label="Right" />
         </Bleed>
       </Box>

polaris.shopify.com/pages/examples/bleed-vertical.tsx
@@ -6,7 +6,7 @@ import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
 function BleedVerticalExample() {
   return (
     <Box background="surface" border="base" padding="4">
-      <Bleed vertical="4">
+      <Bleed horizontal="0" vertical="4">
         <Placeholder label="Vertical" />
       </Bleed>
     </Box>