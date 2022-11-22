3575833a338f370db691e541abe5d3a569f330ab
Chloe Rice <chloe.rice@shopify.com>
2022-11-21 21:09:37 -0500

    Add rule metadata

---
+++ stylelint-polaris/plugins/coverage/ruleMetadata.js
@@ -0,0 +1,45 @@
+module.exports = {
+  colors: {
+    // url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#colors',
+    url: 'https://polaris.shopify.com/tokens/colors',
+  },
+  motion: {
+    // url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#motion',
+    url: 'https://polaris.shopify.com/tokens/motion',
+  },
+  font: {
+    // url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#font',
+    url: 'https://polaris.shopify.com/tokens/font',
+  },
+  layout: {
+    url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#layout',
+  },
+  spacing: {
+    // url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#spacing',
+    url: 'https://polaris.shopify.com/tokens/spacing',
+  },
+  shape: {
+    // url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#shape',
+    url: 'https://polaris.shopify.com/tokens/shape',
+  },
+  depth: {
+    // url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#depth',
+    url: 'https://polaris.shopify.com/tokens/depth',
+  },
+  'z-index': {
+    // url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#z-index',
+    url: 'https://polaris.shopify.com/tokens/z-index',
+  },
+  conventions: {
+    url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#conventions',
+  },
+  breakpoints: {
+    // url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#breakpoints',
+    url: 'https://polaris.shopify.com/tokens/breakpoints',
+  },
+  legacy: {
+    // url: 'https://github.com/Shopify/polaris/tree/main/stylelint-polaris/plugins/coverage/README.md#legacy',
+    url: 'https://polaris.shopify.com/tokens/legacy',
+    deprecated: true,
+  },
+};
