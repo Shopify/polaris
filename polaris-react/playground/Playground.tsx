ea7e3b777e6414d1b702b0ebb3a824c7674caf28
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-11-23 18:14:33 -0500

    Clear `Playground.tsx`

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,63 +1,11 @@
 import React from 'react';
 
-import {
-  Card,
-  Layout,
-  Page,
-  SkeletonBodyText,
-  SkeletonDisplayText,
-  SkeletonPage,
-  TextContainer,
-} from '../src';
+import {Page} from '../src';
 
 export function Playground() {
   return (
     <Page title="Playground">
-      <SkeletonPage primaryAction narrowWidth>
-        <Layout>
-          <Layout.Section>
-            <Card sectioned>
-              <SkeletonBodyText />
-            </Card>
-            <Card sectioned>
-              <TextContainer>
-                <SkeletonDisplayText size="small" />
-                <SkeletonBodyText />
-              </TextContainer>
-            </Card>
-            <Card sectioned>
-              <TextContainer>
-                <SkeletonDisplayText size="small" />
-                <SkeletonBodyText />
-              </TextContainer>
-            </Card>
-          </Layout.Section>
-          <Layout.Section secondary>
-            <Card>
-              <Card.Section>
-                <TextContainer>
-                  <SkeletonDisplayText size="small" />
-                  <SkeletonBodyText lines={2} />
-                </TextContainer>
-              </Card.Section>
-              <Card.Section>
-                <SkeletonBodyText lines={1} />
-              </Card.Section>
-            </Card>
-            <Card subdued>
-              <Card.Section>
-                <TextContainer>
-                  <SkeletonDisplayText size="small" />
-                  <SkeletonBodyText lines={2} />
-                </TextContainer>
-              </Card.Section>
-              <Card.Section>
-                <SkeletonBodyText lines={2} />
-              </Card.Section>
-            </Card>
-          </Layout.Section>
-        </Layout>
-      </SkeletonPage>
+      {/* Add the code you want to test in here */}
     </Page>
   );
 }
