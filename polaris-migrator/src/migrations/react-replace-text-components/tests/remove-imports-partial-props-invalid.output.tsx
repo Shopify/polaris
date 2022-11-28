7d71066c071ed6bad65df8eb607ed1cd4fc147c6
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-11-28 14:27:43 -0800

    Add more invalid/unsupported prop test cases

--- polaris-migrator/src/migrations/react-replace-text-components/tests/remove-imports-partial-props-invalid.output.tsx
+++ polaris-migrator/src/migrations/react-replace-text-components/tests/remove-imports-partial-props-invalid.output.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import {
   /* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */
   /* Replace with: Text */
+  Caption,
+  /* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */
+  /* Replace with: Text */
   Heading,
   /* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */
   /* Replace with: Text */
   Subheading,
+  /* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */
+  /* Replace with: Text */
+  TextStyle,
   Text,
 } from '@shopify/polaris';
 
+declare const captionProps: {children: string};
 declare const element: string;
+declare function getVariation(): 'positive';
 
 export function App() {
   return (
@@ -17,6 +25,8 @@ export function App() {
       <Text variant="heading4xl" as="p">
         DisplayText
       </Text>
+      {/* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */}
+      <Caption {...captionProps} />
       <Text variant="headingMd" as="h2">
         Heading - Default
       </Text>
@@ -27,6 +37,8 @@ export function App() {
       </Text>
       {/* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */}
       <Subheading element={element}>Subheading - Unknown</Subheading>
+      {/* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */}
+      <TextStyle variation={getVariation()}>TextStyle - Unknown</TextStyle>
     </>
   );
 }
