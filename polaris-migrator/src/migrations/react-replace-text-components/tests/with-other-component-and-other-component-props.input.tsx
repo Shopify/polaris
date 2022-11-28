baea7243c1b59f28671956f10765ed5338910ac3
Lo Kim <lo.kim@shopify.com>
2022-11-28 13:45:27 -0500

    Add tests for `replace-other` step type checks

---
+++ polaris-migrator/src/migrations/react-replace-text-components/tests/with-other-component-and-other-component-props.input.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import type {HeadingProps} from '@shopify/polaris';
+import {Subheading} from '@shopify/polaris';
+
+const MyHeading = (_props: HeadingProps) => {
+  return null;
+};
+
+export function App() {
+  const headingProps: HeadingProps = {
+    element: 'h3',
+  };
+
+  return (
+    <>
+      <Subheading>Subheading</Subheading>
+      <MyHeading {...headingProps} />
+    </>
+  );
+}
