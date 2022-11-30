9d69c903d2b114ac067819f8b06d46e464886a38
Marc Thomas <marc.thomas@shopify.com>
2022-11-30 15:52:53 +0000

    Revert "[BulkActions] Update BulkActions and add in SelectAllActions" (#7811)
    Reverts Shopify/polaris#7290
    
    Reverting due to blocking a release of Polaris that needs to go out
    ASAP.

--- polaris-react/src/components/IndexTable/IndexTable.tsx
+++ polaris-react/src/components/IndexTable/IndexTable.tsx
@@ -1,5 +1,9 @@
 import React, {useRef, useState, useEffect, useCallback, useMemo} from 'react';
-import {SortAscendingMajor, SortDescendingMajor} from '@shopify/polaris-icons';
+import {
+  EnableSelectionMinor,
+  SortAscendingMajor,
+  SortDescendingMajor,
+} from '@shopify/polaris-icons';
 import {CSSTransition} from 'react-transition-group';
 import {tokens, toPx, motion} from '@shopify/polaris-tokens';
 
@@ -11,18 +15,14 @@ import {Checkbox as PolarisCheckbox} from '../Checkbox';
 import {EmptySearchResult} from '../EmptySearchResult';
 // eslint-disable-next-line import/no-deprecated
 import {EventListener} from '../EventListener';
-import {SelectAllActions} from '../SelectAllActions';
 import {Stack} from '../Stack';
 import {Sticky} from '../Sticky';
 import {Spinner} from '../Spinner';
 import {Text} from '../Text';
+import {Button} from '../Button';
 import {Tooltip} from '../Tooltip';
 import {UnstyledButton} from '../UnstyledButton';
-import {
-  BulkActions,
-  BulkActionsProps,
-  useIsBulkActionsSticky,
-} from '../BulkActions';
+import {BulkActions, BulkActionsProps} from '../BulkActions';
 import {classNames} from '../../utilities/css';
 import {
   useIndexValue,
@@ -150,9 +150,11 @@ function IndexTableBase({
   const condensedListElement = useRef<HTMLUListElement>(null);
 
   const [tableInitialized, setTableInitialized] = useState(false);
+  const [isSmallScreenSelectable, setIsSmallScreenSelectable] = useState(false);
   const [stickyWrapper, setStickyWrapper] = useState<HTMLElement | null>(null);
   const [hideScrollContainer, setHideScrollContainer] =
     useState<boolean>(false);
+  const [smallScreen, setSmallScreen] = useState(isBreakpointsXS());
 
   const tableHeadings = useRef<HTMLElement[]>([]);
   const stickyTableHeadings = useRef<HTMLElement[]>([]);
@@ -163,14 +165,6 @@ function IndexTableBase({
   const scrollContainerElement = useRef<HTMLDivElement>(null);
   const scrollingWithBar = useRef(false);
   const scrollingContainer = useRef(false);
-  const {
-    bulkActionsIntersectionRef,
-    tableMeasurerRef,
-    isBulkActionsSticky,
-    bulkActionsAbsoluteOffset,
-    bulkActionsMaxWidth,
-    bulkActionsOffsetLeft,
-  } = useIsBulkActionsSticky(selectMode);
 
   const tableBodyRef = useCallback(
     (node: Element | null) => {
@@ -180,6 +174,10 @@ function IndexTableBase({
     },
     [tableInitialized],
   );
+
+  const toggleIsSmallScreenSelectable = useCallback(() => {
+    setIsSmallScreenSelectable((value) => !value);
+  }, []);
 
   const handleSelectAllItemsInStore = useCallback(() => {
     handleSelectionChange(
@@ -300,6 +298,10 @@ function IndexTableBase({
     handleCanScrollRight();
   }, [handleCanScrollRight]);
 
+  const handleIsSmallScreen = useCallback(() => {
+    setSmallScreen(isBreakpointsXS());
+  }, []);
+
   const [canFitStickyColumn, setCanFitStickyColumn] = useState(true);
 
   const handleCanFitStickyColumn = useCallback(() => {
@@ -350,11 +352,13 @@ function IndexTableBase({
     resizeTableHeadings();
     debounceResizeTableScrollbar();
     handleCanScrollRight();
+    handleIsSmallScreen();
     handleCanFitStickyColumn();
   }, [
     resizeTableHeadings,
     debounceResizeTableScrollbar,
     handleCanScrollRight,
+    handleIsSmallScreen,
     handleCanFitStickyColumn,
   ]);
 
@@ -424,6 +428,12 @@ function IndexTableBase({
       condensed ? condensedListElement.current : tableElement.current,
     );
   }, [tableInitialized, resizeTableScrollBar, condensed]);
+
+  useEffect(() => {
+    if (!condensed && isSmallScreenSelectable) {
+      setIsSmallScreenSelectable(false);
+    }
+  }, [condensed, isSmallScreenSelectable]);
 
   const hasBulkActions = Boolean(
     (promotedBulkActions && promotedBulkActions.length > 0) ||
@@ -531,41 +541,8 @@ function IndexTableBase({
     condensed && styles['StickyTable-condensed'],
   );
 
-  const shouldShowBulkActions = bulkActionsSelectable && selectedItemsCount;
-
-  const bulkActionClassNames = classNames(
-    styles.BulkActionsWrapper,
-    isBulkActionsSticky && styles.BulkActionsWrapperSticky,
-  );
-
-  const shouldShowActions = !condensed || selectedItemsCount;
-  const promotedActions = shouldShowActions ? promotedBulkActions : [];
-  const actions = shouldShowActions ? bulkActions : [];
-
-  const bulkActionsMarkup =
-    shouldShowBulkActions && !condensed ? (
-      <div
-        className={bulkActionClassNames}
-        style={{
-          insetBlockStart: isBulkActionsSticky
-            ? undefined
-            : bulkActionsAbsoluteOffset,
-          width: bulkActionsMaxWidth,
-          insetInlineStart: isBulkActionsSticky
-            ? bulkActionsOffsetLeft
-            : undefined,
-        }}
-      >
-        <BulkActions
-          selectMode={selectMode}
-          promotedActions={promotedActions}
-          actions={actions}
-          onSelectModeToggle={condensed ? handleSelectModeToggle : undefined}
-          isSticky={isBulkActionsSticky}
-          width={bulkActionsMaxWidth}
-        />
-      </div>
-    ) : null;
+  const shouldShowBulkActions =
+    (bulkActionsSelectable && selectedItemsCount) || isSmallScreenSelectable;
 
   const stickyHeaderMarkup = (
     <div className={stickyTableClassNames} role="presentation">
@@ -576,44 +553,63 @@ function IndexTableBase({
             isSticky && styles['StickyTableHeader-isSticky'],
           );
 
-          const selectAllActionsClassName = classNames(
-            styles.SelectAllActionsWrapper,
+          const bulkActionClassNames = classNames(
+            styles.BulkActionsWrapper,
             condensed && styles['StickyTableHeader-condensed'],
             isSticky && styles['StickyTableHeader-isSticky'],
           );
 
-          const selectAllActionsMarkup =
-            shouldShowBulkActions && !condensed ? (
-              <div className={selectAllActionsClassName}>
-                <SelectAllActions
-                  label={i18n.translate('Polaris.IndexTable.selected', {
-                    selectedItemsCount: selectedItemsCountLabel,
-                  })}
-                  accessibilityLabel={bulkActionsAccessibilityLabel}
-                  selected={bulkSelectState}
-                  selectMode={selectMode}
-                  onToggleAll={handleTogglePage}
-                  paginatedSelectAllText={paginatedSelectAllText}
-                  paginatedSelectAllAction={paginatedSelectAllAction}
-                />
-                {loadingMarkup}
-              </div>
-            ) : null;
+          const shouldShowActions = !condensed || selectedItemsCount;
+          const promotedActions = shouldShowActions ? promotedBulkActions : [];
+          const actions = shouldShowActions ? bulkActions : [];
+
+          const bulkActionsMarkup = shouldShowBulkActions ? (
+            <div className={bulkActionClassNames} data-condensed={condensed}>
+              {loadingMarkup}
+              <BulkActions
+                smallScreen={smallScreen}
+                label={i18n.translate('Polaris.IndexTable.selected', {
+                  selectedItemsCount: selectedItemsCountLabel,
+                })}
+                accessibilityLabel={bulkActionsAccessibilityLabel}
+                selected={bulkSelectState}
+                selectMode={selectMode || isSmallScreenSelectable}
+                onToggleAll={handleTogglePage}
+                promotedActions={promotedActions}
+                actions={actions}
+                paginatedSelectAllText={paginatedSelectAllText}
+                paginatedSelectAllAction={paginatedSelectAllAction}
+                onSelectModeToggle={
+                  condensed ? handleSelectModeToggle : undefined
+                }
+              />
+            </div>
+          ) : null;
 
           const stickyColumnHeaderClassNames = classNames(
             styles.StickyTableColumnHeader,
             hasMoreLeftColumns && styles['StickyTableColumnHeader-isScrolling'],
           );
 
+          const selectButtonMarkup = (
+            <Button
+              icon={EnableSelectionMinor}
+              onClick={toggleIsSmallScreenSelectable}
+            >
+              {i18n.translate('Polaris.IndexTable.selectButtonText')}
+            </Button>
+          );
+
           const headerMarkup = condensed ? (
             <div
               className={classNames(
                 styles.HeaderWrapper,
-                (!selectable || condensed) && styles.unselectable,
+                !selectable && styles.unselectable,
               )}
             >
               {loadingMarkup}
               {sort}
+              {selectable && selectButtonMarkup}
             </div>
           ) : (
             <div
@@ -633,12 +629,13 @@ function IndexTableBase({
             </div>
           );
 
-          const stickyContent = selectAllActionsMarkup ?? headerMarkup;
+          const stickyContent = bulkActionsMarkup
+            ? bulkActionsMarkup
+            : headerMarkup;
 
           return stickyContent;
         }}
       </Sticky>
-      {bulkActionsMarkup}
     </div>
   );
 
@@ -710,7 +707,7 @@ function IndexTableBase({
     <>
       {sharedMarkup}
       <ul
-        data-selectmode={Boolean(selectMode)}
+        data-selectmode={Boolean(selectMode || isSmallScreenSelectable)}
         className={styles.CondensedList}
         ref={condensedListElement}
       >
@@ -740,20 +737,11 @@ function IndexTableBase({
       <div className={styles.EmptySearchResultWrapper}>{emptyStateMarkup}</div>
     );
 
-  const tableWrapperClassNames = classNames(
-    Boolean(bulkActionsMarkup) &&
-      selectMode &&
-      styles.IndexTableWithBulkActions,
-  );
-
   return (
     <>
       <div className={styles.IndexTable}>
-        <div className={tableWrapperClassNames} ref={tableMeasurerRef}>
-          {!shouldShowBulkActions && !condensed && loadingMarkup}
-          {tableContentMarkup}
-        </div>
-        <div ref={bulkActionsIntersectionRef} />
+        {!shouldShowBulkActions && !condensed && loadingMarkup}
+        {tableContentMarkup}
       </div>
       {scrollBarMarkup}
     </>
@@ -967,8 +955,9 @@ function IndexTableBase({
     };
   }
 
-  function handleSelectModeToggle() {
+  function handleSelectModeToggle(val: boolean) {
     handleSelectionChange(SelectionType.All, false);
+    setIsSmallScreenSelectable(val);
   }
 }
 
