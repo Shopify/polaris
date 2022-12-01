421c5d425e14e2eb6db75e8b56e26fdb3acc9cc6
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-12-01 11:54:46 -0500

    Update Divider.test.tsx

--- polaris-react/src/components/Divider/tests/Divider.test.tsx
+++ polaris-react/src/components/Divider/tests/Divider.test.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import {mountWithApp} from 'tests/utilities';
 
-import {Box} from '../../Box';
 import {Divider} from '../Divider';
 
 describe('<Divider />', () => {
-  it('renders with style', () => {
-    const divider = mountWithApp(<Divider color="dark" />);
+  it('renders custom properties with default values', () => {
+    const columns = mountWithApp(<Divider />);
 
-    expect(divider).toContainReactComponent(Box, {
-      borderBlockStart: 'dark',
+    expect(columns).toContainReactComponent('hr', {
+      style: {
+        '--pc-divider-color': 'var(--p-border-divider)',
+        '--pc-divider-width': 'var(--p-border-width-1)',
+      } as React.CSSProperties,
     });
   });
 
-  it('renders with width', () => {
+  it('renders custom color when passed in', () => {
+    const divider = mountWithApp(<Divider color="dark" />);
+
+    expect(divider).toContainReactComponent('hr', {
+      style: expect.objectContaining({
+        '--pc-divider-color': 'var(--p-border-dark)',
+      }) as React.CSSProperties,
+    });
+  });
+
+  it('renders custom width when passed in', () => {
     const divider = mountWithApp(<Divider width="5" />);
 
-    expect(divider).toContainReactComponent(Box, {
-      borderBlockStartWidth: '5',
+    expect(divider).toContainReactComponent('hr', {
+      style: expect.objectContaining({
+        '--pc-divider-width': 'var(--p-border-width-5)',
+      }) as React.CSSProperties,
     });
   });
 });
