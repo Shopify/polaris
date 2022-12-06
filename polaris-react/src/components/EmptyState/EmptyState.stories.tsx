efbe25d05f653a1959c20c00caf0193a86dd2cdd
Yuraima Estevez <yurm04@gmail.com>
2022-12-06 11:47:31 -0600

    uped chromatic

--- polaris-react/src/components/EmptyState/EmptyState.stories.tsx
+++ polaris-react/src/components/EmptyState/EmptyState.stories.tsx
@@ -7,7 +7,7 @@ export default {
   parameters: {
     // Sets a delay for EmptyState because there is a slight layout shift
     // when first rendered.
-    chromatic: {delay: 1000},
+    chromatic: {delay: 5000},
   },
 } as ComponentMeta<typeof EmptyState>;
 
