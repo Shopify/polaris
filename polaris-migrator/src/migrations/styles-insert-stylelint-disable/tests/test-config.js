6cd75fd9a089791a79fe8aa7e43f6b54add47a45
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2022-12-09 09:57:36 -0800

    Update `stylelint-polaris` to define a single entry point (#7868)

--- polaris-migrator/src/migrations/styles-insert-stylelint-disable/tests/test-config.js
+++ polaris-migrator/src/migrations/styles-insert-stylelint-disable/tests/test-config.js
@@ -1,7 +1,14 @@
+const path = require('path');
+
 module.exports = {
   customSyntax: 'postcss-scss',
   reportDescriptionlessDisables: true,
-  plugins: ['@shopify/stylelint-polaris/plugins/at-rule-disallowed-list'],
+  plugins: [
+    path.join(
+      __dirname,
+      '../../../../../stylelint-polaris/plugins/at-rule-disallowed-list',
+    ),
+  ],
   rules: {
     'unit-disallowed-list': ['rem'],
     'selector-pseudo-class-no-unknown': true,
