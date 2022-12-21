0a2328778495c8ede2f5897ebac84b7d334ca3da
Lo Kim <lo.kim@shopify.com>
2022-12-21 13:53:21 -0500

    [AlphaStack] Add stories for `blockAlign`

--- polaris-react/src/components/AlphaStack/AlphaStack.stories.tsx
+++ polaris-react/src/components/AlphaStack/AlphaStack.stories.tsx
@@ -39,6 +39,17 @@ export function WithResponsiveGap() {
   );
 }
 
+export function WithAlignStart() {
+  return (
+    <AlphaStack align="start">
+      <Badge>Paid</Badge>
+      <Badge>Processing</Badge>
+      <Badge>Fulfilled</Badge>
+      <Badge>Completed</Badge>
+    </AlphaStack>
+  );
+}
+
 export function WithAlignCenter() {
   return (
     <AlphaStack align="center">
@@ -58,6 +69,45 @@ export function WithAlignEnd() {
       <Badge>Fulfilled</Badge>
       <Badge>Completed</Badge>
     </AlphaStack>
+  );
+}
+
+export function WithBlockAlignStart() {
+  return (
+    <div style={{display: 'flex', height: '500px'}}>
+      <AlphaStack blockAlign="start">
+        <Badge>Paid</Badge>
+        <Badge>Processing</Badge>
+        <Badge>Fulfilled</Badge>
+        <Badge>Completed</Badge>
+      </AlphaStack>
+    </div>
+  );
+}
+
+export function WithBlockAlignCenter() {
+  return (
+    <div style={{display: 'flex', height: '500px'}}>
+      <AlphaStack blockAlign="center">
+        <Badge>Paid</Badge>
+        <Badge>Processing</Badge>
+        <Badge>Fulfilled</Badge>
+        <Badge>Completed</Badge>
+      </AlphaStack>
+    </div>
+  );
+}
+
+export function WithBlockAlignEnd() {
+  return (
+    <div style={{display: 'flex', height: '500px'}}>
+      <AlphaStack blockAlign="end">
+        <Badge>Paid</Badge>
+        <Badge>Processing</Badge>
+        <Badge>Fulfilled</Badge>
+        <Badge>Completed</Badge>
+      </AlphaStack>
+    </div>
   );
 }
 
