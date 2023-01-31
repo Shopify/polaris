a85231d6751c923a022bcddaaa35c21fb2857ceb
Yuraima Estevez <yurm04@gmail.com>
2023-01-31 11:58:14 -0600

    revert new props in banner

--- polaris.shopify.com/src/components/StatusBanner/StatusBanner.tsx
+++ polaris.shopify.com/src/components/StatusBanner/StatusBanner.tsx
@@ -8,7 +8,7 @@ interface Props {
   status: Status;
 }
 
-function StatusBanner({status: {value, message, linkText, url}}: Props) {
+function StatusBanner({status: {value, message}}: Props) {
   return (
     <div className={styles.StatusBanner} data-value={value.toLowerCase()}>
       <h2>{uppercaseFirst(value)}</h2>
