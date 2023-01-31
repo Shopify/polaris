aeafc05152459b77e5c59e2799405d09f4045ade
aveline <aveline@users.noreply.github.com>
2023-01-31 14:11:48 -0800

    Remove unused files

--- polaris-migrator/src/migrations/react-rename-component/tests/react-rename-component.input.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {Card} from '@shopify/polaris';
-
-export function App() {
-  return <Card>hello</Card>;
-}
--- polaris-migrator/src/migrations/react-rename-component/tests/react-rename-component.output.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {LegacyCard} from '@shopify/polaris';
-
-export function App() {
-  return <LegacyCard>hello</LegacyCard>;
-}
