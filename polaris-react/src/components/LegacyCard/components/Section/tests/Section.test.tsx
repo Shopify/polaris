2def5fc3581a5aad8f5b120316817dbcacbfc3e1
Lo Kim <lo.kim@shopify.com>
2023-02-01 13:16:59 -0500

    Add `LegacyCard` component

---
+++ polaris-react/src/components/LegacyCard/components/Section/tests/Section.test.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {mountWithApp} from 'tests/utilities';
+
+import {Badge} from '../../../../Badge';
+import {Button} from '../../../../Button';
+import {ButtonGroup} from '../../../../ButtonGroup';
+import {Text} from '../../../../Text';
+import {Section} from '../Section';
+
+describe('<LegacyCard.Section />', () => {
+  it('can have any valid react element as the card section title', () => {
+    const titleString = 'Online store';
+    const badgeString = 'I am a badge';
+    const titleMarkup = (
+      <h2>
+        {titleString}
+        <Badge>{badgeString}</Badge>
+      </h2>
+    );
+
+    const section = mountWithApp(<Section title={titleMarkup} />);
+    const headerMarkup = section.find('h2')!;
+
+    expect(headerMarkup).toContainReactText(titleString);
+    expect(headerMarkup.find(Badge)).toContainReactText(badgeString);
+  });
+
+  it('wraps plain string titles in a <Text />', () => {
+    const titleString = 'Online store';
+    const card = mountWithApp(<Section title={titleString} />);
+    expect(
+      card.find(Text, {variant: 'headingSm', as: 'h3'}),
+    ).toContainReactText(titleString);
+  });
+
+  describe('hideWhenPrinting prop', () => {
+    it('renders classname "Section Section-hideOnPrint" when prop is passed', () => {
+      const card = mountWithApp(<Section hideOnPrint />);
+
+      expect(card).toContainReactComponent('div', {
+        className: 'Section Section-hideOnPrint',
+      });
+    });
+
+    it('does not render classname "Section Section-hideOnPrint" when prop is not passed', () => {
+      const card = mountWithApp(<Section />);
+
+      expect(card).not.toContainReactComponent('div', {
+        className: 'Section Section-hideOnPrint',
+      });
+    });
+  });
+
+  describe('actions', () => {
+    const mockActions = [{content: 'Preview'}, {content: 'Open'}];
+
+    it('renders a button group when defined', () => {
+      const section = mountWithApp(<Section actions={mockActions} />);
+      expect(section).toContainReactComponent(ButtonGroup);
+    });
+
+    it('renders buttons for each action', () => {
+      const section = mountWithApp(<Section actions={mockActions} />);
+      expect(section).toContainReactComponentTimes(Button, 2);
+    });
+
+    it('does not render a button group when not defined', () => {
+      const section = mountWithApp(<Section />);
+      expect(section).not.toContainReactComponent(ButtonGroup);
+    });
+
+    it('renders both custom title markup and actions', () => {
+      const titleString = 'Online store';
+      const badgeString = 'I am a badge';
+      const titleMarkup = (
+        <h2>
+          {titleString}
+          <Badge>{badgeString}</Badge>
+        </h2>
+      );
+      const section = mountWithApp(
+        <Section actions={mockActions} title={titleMarkup} />,
+      );
+      expect(section).toContainReactComponentTimes(Button, 2);
+      expect(section).toContainReactText(titleString);
+      expect(section.find(Badge)).toContainReactText(badgeString);
+    });
+  });
+});
