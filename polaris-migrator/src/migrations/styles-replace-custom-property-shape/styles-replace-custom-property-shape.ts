120f2db747678b8fa0f9c8337649560b01b1e687
Laura Griffee <laura@mailzone.com>
2023-02-13 14:19:33 -0700

    Update border-radius value map

--- polaris-migrator/src/migrations/styles-replace-custom-property-shape/styles-replace-custom-property-shape.ts
+++ polaris-migrator/src/migrations/styles-replace-custom-property-shape/styles-replace-custom-property-shape.ts
@@ -5,10 +5,11 @@ import stylesReplaceCustomProperty from '../styles-replace-custom-property/style
 const replacementMaps = {
   '/.+/': {
     '--p-border-radius-base': '--p-border-radius-1',
-    '--p-border-radius-full': '--p-border-radius-half',
     '--p-border-radius-slim': '--p-border-radius-05',
     '--p-border-radius-wide': '--p-border-radius-2',
     '--p-text-field-focus-ring-border-radius': '7px',
+    '--p-border-radius-large': '--p-border-radius-2',
+    '--p-border-radius-half': '--p-border-radius-full',
   },
 };
 
