3defe85f5c5a5bf78813f9f0a750a2af2782322a
Yuraima Estevez <yurm04@gmail.com>
2023-02-16 17:46:11 -0600

    add migration for text container

---
+++ polaris-migrator/src/migrations/v11-react-replace-text-container-components/tests/v11-react-replace-text-container-components-loose.input.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {TextContainer} from '@shopify/polaris';
+
+export function App() {
+  return <TextContainer spacing="loose">hello</TextContainer>;
+}
---
+++ polaris-migrator/src/migrations/v11-react-replace-text-container-components/tests/v11-react-replace-text-container-components-tight.output.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {AlphaStack} from '@shopify/polaris';
+
+export function App() {
+  return <AlphaStack gap="2">hello</AlphaStack>;
+}
