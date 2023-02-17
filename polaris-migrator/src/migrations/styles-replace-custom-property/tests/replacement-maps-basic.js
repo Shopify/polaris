d891dc1a0a911fb66484236088b31d21c5583db0
Laura Griffee <laura@mailzone.com>
2023-02-17 09:53:17 -0700

    Remove duplicated files from merge

--- polaris-migrator/src/migrations/styles-replace-custom-property/tests/replacement-maps-basic.js
+++
@@ -1,9 +0,0 @@
-const colorMap = {
-  '--p-text': '--p-color-text',
-  '--p-black': '#000',
-};
-
-// eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export
-export default {
-  color: colorMap,
-};
