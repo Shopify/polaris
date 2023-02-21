96fcbf18076b289670e38f47bd361cb1c753c4a1
aveline <aveline@users.noreply.github.com>
2023-02-21 14:55:08 -0800

    Remove default value from `columns` prop

--- polaris-react/src/components/Columns/Columns.tsx
+++ polaris-react/src/components/Columns/Columns.tsx
@@ -48,7 +48,7 @@ export function Columns({children, columns, gap = '4'}: ColumnsProps) {
     ...getResponsiveValue(
       'columns',
       'grid-template-columns',
-      formatColumns(columns || 6),
+      formatColumns(columns),
     ),
     ...getResponsiveProps('columns', 'gap', 'space', gap),
   } as React.CSSProperties;
