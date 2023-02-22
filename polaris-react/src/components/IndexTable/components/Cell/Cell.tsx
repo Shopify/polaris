0d4cf7a34e900e0542ee8bd2b801e96c9592db6d
Rick Caplan <rick.caplan@shopify.com>
2023-02-22 21:48:11 +0000

    [IndexTable Cell] adds optional type prop for right-aligned, monospaced currency formatting

--- polaris-react/src/components/IndexTable/components/Cell/Cell.tsx
+++ polaris-react/src/components/IndexTable/components/Cell/Cell.tsx
@@ -7,17 +7,20 @@ export interface CellProps {
   children?: ReactNode;
   className?: string;
   flush?: boolean;
+  type?: 'currency';
 }
 
 export const Cell = memo(function Cell({
   children,
   className,
   flush,
+  type,
 }: CellProps) {
   const cellClassName = classNames(
     className,
     styles.TableCell,
     flush && styles['TableCell-flush'],
+    type === 'currency' && styles['TableCell-type-currency'],
   );
 
   return <td className={cellClassName}>{children}</td>;
