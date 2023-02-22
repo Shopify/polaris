d5aabaa5ba20b2e7755b116daf001d3cb36ed7b3
Charles Lee <charles.lee@shopify.com>
2023-02-23 09:57:47 +1100

    fix discussion link on PatternsPage

--- polaris.shopify.com/src/components/PatternsPage/index.tsx
+++ polaris.shopify.com/src/components/PatternsPage/index.tsx
@@ -25,7 +25,7 @@ export const PatternsPage = () => (
           </Longform>
           <UpdateBanner
             message={`We are making changes to the patterns section. Expect changes to
-              happen here and [join the discussion]("https://github.com/Shopify/polaris/discussions/categories/pattern-documentation)
+              happen here and [join the discussion](https://github.com/Shopify/polaris/discussions/categories/pattern-documentation)
               to make it better. [See old patterns here](/patterns-legacy).`}
             className={styles.UpdateBanner}
           />
