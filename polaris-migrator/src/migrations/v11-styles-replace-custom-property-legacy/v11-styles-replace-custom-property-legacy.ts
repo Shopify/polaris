490ce9c653aca49f8b3a2743d20607e16d57036b
Laura Griffee <laura@mailzone.com>
2023-02-23 11:32:02 -0700

    Ensure all replacement value units are in rem not px

--- polaris-migrator/src/migrations/v11-styles-replace-custom-property-legacy/v11-styles-replace-custom-property-legacy.ts
+++ polaris-migrator/src/migrations/v11-styles-replace-custom-property-legacy/v11-styles-replace-custom-property-legacy.ts
@@ -5,9 +5,9 @@ import stylesReplaceCustomProperty from '../styles-replace-custom-property/style
 const replacementMaps = {
   '/.+/': {
     '--p-override-loading-z-index': '--p-z-index-6',
-    '--p-choice-size': '20px',
-    '--p-icon-size-small': '8px',
-    '--p-icon-size-medium': '20px',
+    '--p-choice-size': '1.25rem',
+    '--p-icon-size-small': '0.5rem',
+    '--p-icon-size-medium': '1.25rem',
     '--p-choice-margin': '--p-space-025',
     '--p-control-border-width': '--p-border-radius-05',
     '--p-banner-border-default':
@@ -21,11 +21,11 @@ const replacementMaps = {
     '--p-banner-border-critical':
       'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-critical-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-critical-subdued)',
     '--p-thin-border-subdued': '--p-border-base',
-    '--p-text-field-spinner-offset': '2px',
-    '--p-text-field-focus-ring-offset': '-4px',
+    '--p-text-field-spinner-offset': '0.125rem',
+    '--p-text-field-focus-ring-offset': '-0.25rem',
     '--p-button-group-item-spacing': 'calc(-1 * var(--p-space-025))',
-    '--p-range-slider-thumb-size-base': '16px',
-    '--p-range-slider-thumb-size-active': '24px',
+    '--p-range-slider-thumb-size-base': '1rem',
+    '--p-range-slider-thumb-size-active': '1.5rem',
     '--p-frame-offset': '0',
   },
 };
