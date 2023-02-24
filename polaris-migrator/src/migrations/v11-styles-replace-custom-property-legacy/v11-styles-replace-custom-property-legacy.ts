029989a859c173610f486ee246f66fbe7fe6d45d
Laura Griffee <laura@mailzone.com>
2023-02-24 10:28:14 -0700

    Revert "Convert replacement values to px for snapshot"
    This reverts commit 508a408414874c43aaed153c5a82d0518f683b3d.

--- polaris-migrator/src/migrations/v11-styles-replace-custom-property-legacy/v11-styles-replace-custom-property-legacy.ts
+++ polaris-migrator/src/migrations/v11-styles-replace-custom-property-legacy/v11-styles-replace-custom-property-legacy.ts
@@ -5,9 +5,9 @@ import stylesReplaceCustomProperty from '../styles-replace-custom-property/style
 const replacementMaps = {
   '/.+/': {
     '--p-override-loading-z-index': '--p-z-index-6',
-    '--p-choice-size': '20px', // 20px
-    '--p-icon-size-small': '8px', // 8px
-    '--p-icon-size-medium': '20px', // 20px
+    '--p-choice-size': '1.25rem', // 20px
+    '--p-icon-size-small': '0.5rem', // 8px
+    '--p-icon-size-medium': '1.25rem', // 20px
     '--p-choice-margin': '--p-space-025',
     '--p-control-border-width': '--p-border-width-2',
     '--p-banner-border-default':
@@ -21,11 +21,11 @@ const replacementMaps = {
     '--p-banner-border-critical':
       'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-critical-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-critical-subdued)',
     '--p-thin-border-subdued': '--p-border-base',
-    '--p-text-field-spinner-offset': '2px', // 2px
-    '--p-text-field-focus-ring-offset': '-4px', // -4px
+    '--p-text-field-spinner-offset': '0.125rem', // 2px
+    '--p-text-field-focus-ring-offset': '-0.25rem', // -4px
     '--p-button-group-item-spacing': 'calc(-1 * var(--p-space-025))',
-    '--p-range-slider-thumb-size-base': '16px', // 16px
-    '--p-range-slider-thumb-size-active': '24px', // 24px
+    '--p-range-slider-thumb-size-base': '1rem', // 16px
+    '--p-range-slider-thumb-size-active': '1.5rem', // 24px
     '--p-frame-offset': '0',
   },
 };
