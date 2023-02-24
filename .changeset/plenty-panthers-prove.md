16f5a36380b51a1b13ff75089cde93fad19f72a1
Jess Telford <jess.telford@shopify.com>
2023-02-24 11:33:01 +1100

    Add caveats to code examples in Patterns

---
+++ .changeset/plenty-panthers-prove.md
@@ -0,0 +1,5 @@
+---
+'polaris.shopify.com': patch
+---
+
+Add caveats to code examples in Patterns
--- polaris.shopify.com/content/patterns/date-picking.ts
+++ polaris.shopify.com/content/patterns/date-picking.ts
@@ -1057,68 +1057,68 @@ const pattern: MultiVariantPattern = {
         )}
         `,
         code: `
-{(function DateListPicker () {
-  const ranges = [{
-      title: "No Date",
-      alias: "no-date",
-      period: null
-    },
-    {
-      title: "Today",
-      alias: "today",
-      period: {
-        since: "today",
-        until: "today"
-      }
-    },
-    {
-      title: "Yesterday",
-      alias: "yesterday",
-      period: {
-        since: "yesterday",
-        until: "yesterday"
-      }
-    },
-    {
-      title: "Last 7 days",
-      alias: "last7days",
-      period: {
-        since: "-7d",
-        until: "-1d"
-      }
-    }
-  ];
-  const [selected, setSelected] = useState(ranges[0]);
-  const [popoverActive, setPopoverActive] = useState(false);
-  return (
-    <Popover
-      autofocusTarget="none"
-      preferredAlignment="left"
-      preferInputActivator={false}
-      preferredPosition="below"
-      activator={
-        <Button
-          onClick={() => setPopoverActive(!popoverActive)}
-          icon={CalendarMinor}
-        >
-          {selected.title}
-        </Button>
-      }
-      active={popoverActive}
-    >
-      <OptionList
-        options={ranges.map((range) => ({
-          value: range.alias,
-          label: range.title
-        }))}
-        selected={selected.alias}
-        onChange={(value) => {
-          setSelected(ranges.find((range) => range.alias === value[0]));
-          setPopoverActive(false);
-        }}
-      />
-  </Popover>
-)})()}
+          {(function DateListPicker () {
+            const ranges = [{
+              title: "No Date",
+              alias: "no-date",
+              period: null
+            },
+            {
+              title: "Today",
+              alias: "today",
+              period: {
+                since: "today",
+                until: "today"
+              }
+            },
+            {
+              title: "Yesterday",
+              alias: "yesterday",
+              period: {
+                since: "yesterday",
+                until: "yesterday"
+              }
+            },
+            {
+              title: "Last 7 days",
+              alias: "last7days",
+              period: {
+                since: "-7d",
+                until: "-1d"
+              }
+            }
+          ];
+          const [selected, setSelected] = useState(ranges[0]);
+          const [popoverActive, setPopoverActive] = useState(false);
+          return (
+            <Popover
+              autofocusTarget="none"
+              preferredAlignment="left"
+              preferInputActivator={false}
+              preferredPosition="below"
+              activator={
+                <Button
+                  onClick={() => setPopoverActive(!popoverActive)}
+                  icon={CalendarMinor}
+                >
+                  {selected.title}
+                </Button>
+              }
+              active={popoverActive}
+            >
+              <OptionList
+                options={ranges.map((range) => ({
+                  value: range.alias,
+                  label: range.title
+                }))}
+                selected={selected.alias}
+                onChange={(value) => {
+                  setSelected(ranges.find((range) => range.alias === value[0]));
+                  setPopoverActive(false);
+                }}
+              />
+            </Popover>
+          )})()}
 `,
       },
     },
