df1c486dca50f4fc5d187cb0af714c7e60e7692f
Laura Griffee <laura@mailzone.com>
2023-02-28 10:39:27 -0700

    Convert replacement values to px and remove any color tokens for snapshot

--- polaris-migrator/src/migrations/v11-styles-replace-custom-property-legacy/v11-styles-replace-custom-property-legacy.ts
+++ polaris-migrator/src/migrations/v11-styles-replace-custom-property-legacy/v11-styles-replace-custom-property-legacy.ts
@@ -7,27 +7,27 @@ const allMap = {
   '--p-override-loading-z-index': '--p-z-index-6',
   '--p-choice-margin': '--p-space-025',
   '--p-control-border-width': '--p-border-width-2',
-  '--p-banner-border-default':
-    'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-strong), inset 0 0 0 (--p-border-width-1) var(--p-color-border-strong)',
-  '--p-banner-border-success':
-    'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-success-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-success-subdued)',
-  '--p-banner-border-highlight':
-    'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-info-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-info-subdued)',
-  '--p-banner-border-warning':
-    'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-caution-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-caution-subdued)',
-  '--p-banner-border-critical':
-    'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-critical-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-critical-subdued)',
-  '--p-thin-border-subdued':
-    'var(--p-border-width-1) solid var(--p-color-border-subdued)',
+  // '--p-banner-border-default':
+  //   'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-strong), inset 0 0 0 (--p-border-width-1) var(--p-color-border-strong)',
+  // '--p-banner-border-success':
+  //   'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-success-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-success-subdued)',
+  // '--p-banner-border-highlight':
+  //   'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-info-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-info-subdued)',
+  // '--p-banner-border-warning':
+  //   'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-caution-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-caution-subdued)',
+  // '--p-banner-border-critical':
+  //   'inset 0 (--p-border-width-1) 0 0 var(--p-color-border-critical-subdued), inset 0 0 0 (--p-border-width-1) var(--p-color-border-critical-subdued)',
+  // '--p-thin-border-subdued':
+  //   'var(--p-border-width-1) solid var(--p-color-border-subdued)',
   // The following custom properties are overridden in the spaceMap
   '--p-frame-offset': '0',
-  '--p-choice-size': '1.25rem', // 20px
-  '--p-icon-size-small': '0.5rem', // 8px
-  '--p-icon-size-medium': '1.25rem', // 20px
-  '--p-text-field-spinner-offset': '0.125rem', // 2px
-  '--p-text-field-focus-ring-offset': '-0.25rem', // -4px
-  '--p-range-slider-thumb-size-base': '1rem', // 16px
-  '--p-range-slider-thumb-size-active': '1.5rem', // 24px
+  '--p-choice-size': '20px', // 20px/1.25rem
+  '--p-icon-size-small': '8px', // 8px/0.5rem
+  '--p-icon-size-medium': '20px', // 20px/1.25rem
+  '--p-text-field-spinner-offset': '2px', // 2px/0.125rem
+  '--p-text-field-focus-ring-offset': '-4px', // -4px/-0.25rem
+  '--p-range-slider-thumb-size-base': '16px', // 16px/1rem
+  '--p-range-slider-thumb-size-active': '24px', // 24px/1.5rem
   '--p-button-group-item-spacing': 'calc(-1 * var(--p-space-025))',
 };
 
