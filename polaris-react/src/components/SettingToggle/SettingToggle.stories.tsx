6806212a11d0526bbbf4f43915360e39dc7d2715
Chloe Rice <chloe.rice@shopify.com>
2023-03-07 10:50:52 -0500

    Add default setting status; account for old API; move conditionals into variables
    Co-Authored-By: Oluwatimi <oluwatimi.owoturo@shopify.com>

--- polaris-react/src/components/SettingToggle/SettingToggle.stories.tsx
+++ polaris-react/src/components/SettingToggle/SettingToggle.stories.tsx
@@ -6,7 +6,29 @@ export default {
   component: SettingToggle,
 } as ComponentMeta<typeof SettingToggle>;
 
-export function Default() {
+export function WithOnlyChildContent() {
+  const [active, setActive] = useState(false);
+
+  const handleToggle = useCallback(() => setActive((active) => !active), []);
+
+  const contentStatus = active ? 'Turn off' : 'Turn on';
+
+  return (
+    <SettingToggle
+      action={{
+        content: contentStatus,
+        onAction: handleToggle,
+      }}
+      enabled={active}
+    >
+      <Text>
+        Allow customers to log in with an external customer account system.
+      </Text>
+    </SettingToggle>
+  );
+}
+
+export function WithTitleStatusAndDescription() {
   const [active, setActive] = useState(false);
 
   const handleToggle = useCallback(() => setActive((active) => !active), []);
@@ -17,10 +39,46 @@ export function Default() {
     <SettingToggle
       title="Multipass"
       description="Allow customers to log in with an external customer account system."
-      settingStatus={{
-        enabled: {content: 'On'},
-        disabled: {content: 'Off'},
+      action={{
+        content: contentStatus,
+        onAction: handleToggle,
       }}
+      enabled={active}
+    />
+  );
+}
+
+export function WithLongTitleAndDescription() {
+  const [active, setActive] = useState(false);
+
+  const handleToggle = useCallback(() => setActive((active) => !active), []);
+
+  const contentStatus = active ? 'Turn off' : 'Turn on';
+
+  return (
+    <SettingToggle
+      title="Multipass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass pass"
+      description="Allow customers to log in with an external customer account system. Allow customers to log in with an external customer account system. Allow customers to log in with an external customer account system."
+      action={{
+        content: contentStatus,
+        onAction: handleToggle,
+      }}
+      enabled={active}
+    />
+  );
+}
+
+export function WithAllOfItsElements() {
+  const [active, setActive] = useState(false);
+
+  const handleToggle = useCallback(() => setActive((active) => !active), []);
+
+  const contentStatus = active ? 'Turn off' : 'Turn on';
+
+  return (
+    <SettingToggle
+      title="Multipass"
+      description="Allow customers to log in with an external customer account system."
       action={{
         content: contentStatus,
         onAction: handleToggle,
--- polaris-react/src/components/SettingToggle/SettingToggle.tsx
+++ polaris-react/src/components/SettingToggle/SettingToggle.tsx
@@ -4,12 +4,13 @@ import type {ComplexAction} from '../../types';
 import {buttonFrom} from '../Button';
 import {globalIdGeneratorFactory} from '../../utilities/unique-id';
 import {Text} from '../Text';
-import {Badge, Status} from '../Badge';
+import {Badge, Status, StatusValue} from '../Badge';
 import {useMediaQuery} from '../../utilities/media-query';
 import {AlphaCard} from '../AlphaCard';
 import {AlphaStack} from '../AlphaStack';
 import {Inline} from '../Inline';
 import {Box} from '../Box';
+import {useI18n} from '../../utilities/i18n';
 import {WithinContentContext} from '../../utilities/within-content-context';
 
 interface SettingsToggleBadge {
@@ -20,21 +21,24 @@ interface SettingsToggleBadge {
 export interface SettingToggleProps {
   /** Inner content of the card */
   children?: React.ReactNode;
+  /** Card header actions */
+  action?: ComplexAction;
+  /** Sets toggle state to on or off */
+  enabled?: boolean;
   /** The title in the header of the settings toggle */
   title?: string;
   /** The description of the settings toggle */
   description?: string;
   /** The help link rendered in the settings toggle */
   helpLink?: React.ReactNode;
-  /** Card header actions */
-  action?: ComplexAction;
-  /** Sets toggle state to on or off */
-  enabled?: boolean;
   /** The content that should be rendered in the badge for setting status */
   settingStatus?: SettingsToggleBadge;
 }
 
 const getUniqueSettingToggleId = globalIdGeneratorFactory('SettingToggle');
+const getUniqueDescriptionId = globalIdGeneratorFactory(
+  'SettingToggleDescription',
+);
 
 export function SettingToggle({
   enabled,
@@ -45,56 +49,118 @@ export function SettingToggle({
   helpLink,
   settingStatus,
 }: SettingToggleProps) {
-  const id = useMemo(getUniqueSettingToggleId, []);
+  const toggleId = useMemo(getUniqueSettingToggleId, []);
+  const descriptionId = useMemo(getUniqueDescriptionId, []);
+
   const {isNavigationCollapsed} = useMediaQuery();
+
+  const i18n = useI18n();
 
   const actionMarkup = action
     ? buttonFrom(action, {
         role: 'switch',
-        id,
+        id: toggleId,
         ariaChecked: enabled ? 'true' : 'false',
+        ariaDescribedBy: description ? descriptionId : undefined,
       })
     : null;
 
-  const settingTitle = (
-    <Box width="100%">
-      <Inline align="space-between" wrap={false}>
-        <Inline gap="2">
+  // Accounts for the old API where the description was passed as a child
+  if (!title && !description) {
+    return (
+      <AlphaCard>
+        <WithinContentContext.Provider value>
+          <Box width="100%">
+            <Inline wrap gap="5" align="space-between" blockAlign="center">
+              <label htmlFor={toggleId}>{children}</label>
+              {actionMarkup}
+            </Inline>
+          </Box>
+        </WithinContentContext.Provider>
+      </AlphaCard>
+    );
+  }
+
+  const defaultSettingStatus = {
+    enabled: {
+      status: StatusValue.Success,
+      content: i18n.translate('Polaris.SettingToggle.status.enabled'),
+    },
+    disabled: {
+      content: i18n.translate('Polaris.SettingToggle.status.disabled'),
+    },
+  };
+
+  const badgeStatus: Status = enabled
+    ? settingStatus?.enabled.status || defaultSettingStatus.enabled.status
+    : settingStatus?.disabled.status;
+
+  const badgeContent = enabled
+    ? settingStatus?.enabled.content || defaultSettingStatus.enabled.content
+    : settingStatus?.disabled.content || defaultSettingStatus.disabled.content;
+
+  const settingStatusMarkup = (
+    <Badge
+      status={badgeStatus}
+      statusAndProgressLabelOverride={i18n.translate(
+        'Polaris.SettingToggle.status.accessibilityLabel',
+        {status: badgeContent},
+      )}
+    >
+      {badgeContent}
+    </Badge>
+  );
+
+  const settingTitle = title ? (
+    <Inline gap="2" wrap={false}>
+      <label htmlFor={toggleId}>
+        <Inline gap="2" align="start" blockAlign="baseline" wrap={false}>
           <Text variant="headingMd" as="h6">
             {title}
           </Text>
-          {settingStatus && (
-            <Badge
-              status={
-                enabled
-                  ? settingStatus?.enabled.status || 'success'
-                  : settingStatus?.disabled.status
-              }
-            >
-              {enabled
-                ? settingStatus?.enabled.content
-                : settingStatus?.disabled.content}
-            </Badge>
-          )}
-          {helpLink}
+          {settingStatusMarkup}
         </Inline>
-        <Inline align="end">
-          {!isNavigationCollapsed ? actionMarkup : null}
-        </Inline>
+      </label>
+      {helpLink}
+    </Inline>
+  ) : null;
+
+  const headerMarkup = (
+    <Box width="100%">
+      <Inline gap="12" align="space-between" blockAlign="start" wrap={false}>
+        {settingTitle}
+        {!isNavigationCollapsed ? (
+          <Box minWidth="fit-content">
+            <Inline align="end">{actionMarkup}</Inline>
+          </Box>
+        ) : null}
       </Inline>
     </Box>
   );
 
+  const descriptionMarkup = (
+    <AlphaStack gap="4">
+      <Text id={descriptionId} variant="bodyMd" as="p" color="subdued">
+        {description}
+      </Text>
+      {isNavigationCollapsed ? (
+        <Box width="100%">
+          <Inline align="start">{actionMarkup}</Inline>
+        </Box>
+      ) : null}
+    </AlphaStack>
+  );
+
   return (
     <AlphaCard>
-      <AlphaStack gap="5">
-        {settingTitle}
-        <AlphaStack gap="5">
-          <Text variant="bodyMd" as="p" color="subdued">
-            {description}
-          </Text>
-          {isNavigationCollapsed ? actionMarkup : null}
-        </AlphaStack>
+      <AlphaStack gap={{xs: '4', sm: '5'}}>
+        <Box width="100%">
+          <AlphaStack gap={{xs: '2', sm: '4'}}>
+            {headerMarkup}
+            {descriptionMarkup}
+          </AlphaStack>
+        </Box>
+
         <WithinContentContext.Provider value>
           {children}
         </WithinContentContext.Provider>
