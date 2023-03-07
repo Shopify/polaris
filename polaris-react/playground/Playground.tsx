5fadf571e976b67474922d587194339553012f5c
aveline <aveline@users.noreply.github.com>
2023-03-07 15:09:25 -0800

    Update Playground.tsx

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 
-import {Page, AlphaStack, Box} from '../src';
+import {Page} from '../src';
 
 export function Playground() {
   return (
     <Page title="Playground">
       {/* Add the code you want to test in here */}
-      <Box background="surface-subdued" border="dark">
-        <AlphaStack
-          gap="4"
-          paddingBlockStart={{sm: '2', md: '6'}}
-          paddingBlockEnd={{sm: '2', md: '6'}}
-        >
-          <Box background="surface-attention" padding="10" />
-          <Box background="surface-highlight" padding="10" />
-          <Box background="surface-attention" padding="10" />
-        </AlphaStack>
-      </Box>
     </Page>
   );
 }
