f632e1897bf27514448a268c48deec6d0252bdb9
Laura Griffee <laura@mailzone.com>
2023-03-07 11:11:32 -0700

    Add new depth tokens to v11 migration

--- polaris-migrator/src/migrations/v11-styles-replace-custom-property-depth/v11-styles-replace-custom-property-depth.ts
+++ polaris-migrator/src/migrations/v11-styles-replace-custom-property-depth/v11-styles-replace-custom-property-depth.ts
@@ -4,7 +4,21 @@ import stylesReplaceCustomProperty from '../styles-replace-custom-property/style
 
 const replacementMaps = {
   '/.+/': {
-    '--p-shadow-transparent': '0 0 0 0 transparent',
+    '--p-shadow-transparent': '--p-shadow-none',
+    '--p-shadow-faint': '--p-shadow-none',
+    '--p-shadow-base': '--p-shadow-xs',
+    '--p-shadow-deep': '--p-shadow-none',
+    '--p-shadow-button': '--p-shadow-sm',
+    '--p-shadow-top-bar': '--p-shadow-sm',
+    '--p-shadow-card': '--p-shadow-md',
+    '--p-shadow-popover': '--p-shadow-xl',
+    '--p-shadow-layer': '--p-shadow-none',
+    '--p-shadow-modal': '--p-shadow-2xl',
+    '--p-shadows-inset-button': '--p-shadow-none',
+    '--p-shadows-inset-button-pressed': '--p-shadow-inset-md',
+    '--p-shadow-color-picker': '--p-shadow-inset-sm',
+    '--p-shadow-color-picker-dragger': '--p-shadow-sm',
+    '--p-hint-from-direct-light': 'rgba(0, 0, 0, 0.15)',
   },
 };
 
