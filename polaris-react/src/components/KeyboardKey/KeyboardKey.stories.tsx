d27a361c25fd002937a1a6e7eea46225d12cad98
Reuben Dayo <reuben.dayo@shopify.com>
2023-03-10 06:34:53 -0800

    Add variant to KeyboardKey component (#8288)
    <!--
      ☝️How to write a good PR title:
    - Prefix it with [ComponentName] (if applicable), for example: [Button]
      - Start with a verb, for example: Add, Delete, Improve, Fix…
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ### WHY are these changes introduced?
    
    Part of the Online store editor Keyboard Shortcut enhancement -
    [Figma](https://www.figma.com/file/qRogDRYEpj1CokjAY06WKi/OSE-Keyboard-Shortcuts?node-id=1734%3A15442)
    
    Update styling of the default `KeyboardKey` component and adds optional
    `size` prop.
    
    <details open>
    <summary>Screenshots</summary>
    <br>
    
    Before:
    
    <img width="79" alt="image"
    src="https://user-images.githubusercontent.com/24706433/223430091-cbeefee6-4843-4c4d-8ffc-edf6b1dd01c5.png">
    
    ### Change
    
    Default:
    
    <img width="460" alt="image"
    src="https://user-images.githubusercontent.com/24706433/223429825-381e6be6-a7ff-42fb-a95e-6d9e9aa197e8.png">
    
    Small: 
    
    <img width="307" alt="image"
    src="https://user-images.githubusercontent.com/24706433/223895796-a835346f-9a42-448c-9b9c-0c408995da5d.png">
    
    </details>
    
    
    
    
    
    <!--
      Context about the problem that’s being addressed.
    
    -->
    
    ### WHAT is this pull request doing?
    
    <!--
      Summary of the changes committed.
    
    Before / after screenshots are appreciated for UI changes. Make sure to
    include alt text that describes the screenshot.
    
    If you include an animated gif showing your change, wrapping it in a
    details tag is recommended. Gifs usually autoplay, which can cause
    accessibility issues for people reviewing your PR:
    
        <details>
          <summary>Summary of your gif(s)</summary>
          <img src="..." alt="Description of what the gif shows">
        </details>
    -->
    
    Update `KeyboardKey` component to add an optional property `variant`,
    which can take `small` as a value. A `KeyboardKey` component with this
    variant will be used in tooltips to display the keyboard shortcut of an
    action.
    
    <!-- ℹ️ Delete the following for small / trivial changes -->
    
    ### How to 🎩
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    <!--
      Give as much information as needed to experiment with the component
      in the playground.
    -->
    
    <details>
    <summary>Copy-paste this code in
    <code>playground/Playground.tsx</code>:</summary>
    
    ```jsx
    import React from 'react';
    import {Page} from '../src';
    
    export function Playground() {
      return (
        <Page title="Playground">
          {/* Add the code you want to test in here */}
        </Page>
      );
    }
    ```
    
    </details>
    
    ### 🎩 checklist
    
    - [ ] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [ ] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [x] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [x] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris-react/src/components/KeyboardKey/KeyboardKey.stories.tsx
+++ polaris-react/src/components/KeyboardKey/KeyboardKey.stories.tsx
@@ -7,5 +7,64 @@ export default {
 } as ComponentMeta<typeof KeyboardKey>;
 
 export function Default() {
-  return <KeyboardKey>Ctrl</KeyboardKey>;
+  return (
+    <div style={{display: 'flex', gap: '20px'}}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey>⌘</KeyboardKey>
+          <KeyboardKey>s</KeyboardKey>
+        </div>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey>ctrl</KeyboardKey>
+          <KeyboardKey>s</KeyboardKey>
+        </div>
+      </div>
+
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey>⌘</KeyboardKey>
+          <KeyboardKey>/</KeyboardKey>
+        </div>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey>ctrl</KeyboardKey>
+          <KeyboardKey>/</KeyboardKey>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Small() {
+  return (
+    <div style={{display: 'flex', gap: '20px'}}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey size="small">⌘</KeyboardKey>
+          <KeyboardKey size="small">d</KeyboardKey>
+        </div>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey size="small">⌘</KeyboardKey>
+          <KeyboardKey size="small">h</KeyboardKey>
+        </div>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey size="small">⌘</KeyboardKey>
+          <KeyboardKey size="small">⌫</KeyboardKey>
+        </div>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey size="small">ctrl</KeyboardKey>
+          <KeyboardKey size="small">d</KeyboardKey>
+        </div>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey size="small">ctrl</KeyboardKey>
+          <KeyboardKey size="small">h</KeyboardKey>
+        </div>
+        <div style={{display: 'flex'}}>
+          <KeyboardKey size="small">ctrl</KeyboardKey>
+          <KeyboardKey size="small">⌫</KeyboardKey>
+        </div>
+      </div>
+    </div>
+  );
 }
