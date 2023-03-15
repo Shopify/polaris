37d455d72796ab9248f22230ed4c3964d2a26093
Richard Todd <40364429+rdott@users.noreply.github.com>
2023-03-15 17:28:32 +0000

    Added playground example

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Page} from '../src';
+import {AlphaCard, Page, TextField} from '../src';
 
 export function Playground() {
   return (
     <Page title="Playground">
-      {/* Add the code you want to test in here */}
+      <AlphaCard>
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+        <NumberInput />
+      </AlphaCard>
     </Page>
+  );
+}
+
+function NumberInput() {
+  const [value, setValue] = useState('');
+
+  function handleOnChange(newValue: string) {
+    setValue(newValue);
+  }
+
+  return (
+    <TextField
+      label="Number input"
+      value={value}
+      type="number"
+      autoComplete="off"
+      onChange={handleOnChange}
+    />
   );
 }
