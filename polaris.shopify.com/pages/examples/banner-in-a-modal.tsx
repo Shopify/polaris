8d7e45a8d289094f15eb7693d7fa22306a1127bc
Marten Bjork <marten.bjork@shopify.com>
2023-03-16 07:55:11 +0100

    Fix types in AlphaStack examples (#8674)

--- polaris.shopify.com/pages/examples/banner-in-a-modal.tsx
+++ polaris.shopify.com/pages/examples/banner-in-a-modal.tsx
@@ -1,5 +1,5 @@
 import {Button, Modal, TextContainer, Banner} from '@shopify/polaris';
-import {useState, useCallback} from 'react';
+import React, {useState, useCallback} from 'react';
 import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
 
 function BannerInModalExample() {
