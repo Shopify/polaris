f9504278cc0ea544a6c87755f367280b36092c93
Kyle Durand <kyledurand@users.noreply.github.com>
2023-03-19 14:14:03 -0400

    fix: text story semantics

--- polaris.shopify.com/pages/examples/text-inheritance.tsx
+++ polaris.shopify.com/pages/examples/text-inheritance.tsx
@@ -1,14 +1,13 @@
-import {LegacyStack, Text} from '@shopify/polaris';
+import {Text} from '@shopify/polaris';
 import React from 'react';
 import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
 
 function TextExample() {
   return (
     <Text as="p" variant="heading2xl" color="warning">
-      <LegacyStack vertical>
-        <Text as="span">This is a 2xl heading</Text>
-        <Text as="span">This is also a 2xl heading</Text>
-      </LegacyStack>
+      <Text as="span">This is a 2xl heading</Text>
+      <br />
+      <Text as="span">This is also a 2xl heading</Text>
     </Text>
   );
 }
