9ac3c66933e7a89a1692d86a19789f63f43626ed
Jess Telford <jess.telford@shopify.com>
2023-03-20 11:24:21 +1100

    Use next-mdx-remote to render markdown

--- polaris.shopify.com/src/components/ComponentExamples/index.ts
+++ polaris.shopify.com/src/components/ComponentExamples/index.ts
@@ -1,5 +1,8 @@
 import ComponentExamples from './ComponentExamples';
 
-export {type ComponentExample} from './ComponentExamples';
+export {
+  type ComponentExample,
+  type ComponentExampleSerialized,
+} from './ComponentExamples';
 
 export default ComponentExamples;
