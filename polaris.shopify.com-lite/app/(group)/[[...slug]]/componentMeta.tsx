24de4bb5b9ff7421997648a9b54fabaebe8e111e
Marten Bjork <marten.bjork@shopify.com>
2023-03-29 07:37:39 +0200

    Add a heading to examples

--- polaris.shopify.com-lite/app/(group)/[[...slug]]/componentMeta.tsx
+++ polaris.shopify.com-lite/app/(group)/[[...slug]]/componentMeta.tsx
@@ -59,6 +59,9 @@ export default function ComponentMeta({
         </div>
       )}
 
+      <h2 className={styles.ExamplesHeading} id="examples">
+        Examples
+      </h2>
       <Tabs
         tabs={pageMeta.examples.map((example) => example.title)}
         boxed={false}
