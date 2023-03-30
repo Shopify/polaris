e9354d2ff840ea0b906624f46d3c1e93fd471456
Eric Berndt <eric.berndt@shopify.com>
2023-03-30 16:32:37 -0700

    don't hide popover actions, use onDismiss instead

--- polaris-react/src/components/MediaCard/MediaCard.stories.tsx
+++ polaris-react/src/components/MediaCard/MediaCard.stories.tsx
@@ -149,6 +149,31 @@ export function WithDismissButton() {
         onAction: () => {},
       }}
       description="Discover how Shopify can power up your entrepreneurial journey."
+      onDismiss={() => console.log('clicked')}
+    >
+      <img
+        alt=""
+        width="100%"
+        height="100%"
+        style={{
+          objectFit: 'cover',
+          objectPosition: 'center',
+        }}
+        src="https://burst.shopifycdn.com/photos/business-woman-smiling-in-office.jpg?width=1850"
+      />
+    </MediaCard>
+  );
+}
+
+export function WithDismissButtonAndPopoverActions() {
+  return (
+    <MediaCard
+      title="Getting Started"
+      primaryAction={{
+        content: 'Learn about getting started',
+        onAction: () => {},
+      }}
+      description="Discover how Shopify can power up your entrepreneurial journey."
       popoverActions={[{content: 'Dismiss', onAction: () => {}}]}
       onDismiss={() => console.log('clicked')}
     >
