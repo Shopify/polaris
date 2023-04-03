5bf4f1db76ffc5fe3e3894aeb2c4c4171ed83429
Sophie Schneider <sophie.schneider@shopify.com>
2023-04-03 19:02:54 -0400

    Add jest, storybook, and rollup to prototype (#8835)

---
+++ polaris-patterns/scripts/utilities/retry.js
@@ -0,0 +1,27 @@
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function retry(functionToTry, maxAttempts = 3, delay = 1000) {
+  // TODO work out how to keep eslint happy
+  // SEE https://eslint.org/docs/rules/no-async-promise-executor
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise(async (resolve, reject) => {
+    let remainingAttempts = maxAttempts;
+    while (remainingAttempts > 0) {
+      remainingAttempts -= 1;
+      try {
+        functionToTry();
+        resolve();
+        break;
+      } catch (error) {
+        if (remainingAttempts === 0) {
+          reject(error);
+        }
+        await sleep(delay);
+      }
+    }
+  });
+}
+
+module.exports = retry;
