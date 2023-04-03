568fc571b356a6df5e11d263c87428070b13d9e1
Sophie Schneider <sophie.schneider@shopify.com>
2023-04-03 19:02:54 -0400

    Add jest, storybook, and rollup to prototype (#8835)

---
+++ polaris-patterns/src/components/AccountConnection/components/SettingAction/SettingAction.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './SettingAction.scss';
+
+export interface SettingActionProps {
+  action?: React.ReactNode;
+  children?: React.ReactNode;
+}
+
+export function SettingAction({action, children}: SettingActionProps) {
+  return (
+    <div className={styles.SettingAction}>
+      <div className={styles.Setting}>{children}</div>
+      <div className={styles.Action}>{action}</div>
+    </div>
+  );
+}
---
+++ polaris-patterns/src/index.ts
@@ -0,0 +1,2 @@
+export {AccountConnection} from './components/AccountConnection';
+export type {AccountConnectionProps} from './components/AccountConnection';
