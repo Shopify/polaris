7c43886581dbdd9e4bfa1d44fe1ff15a09a4b688
Sophie Schneider <sophie.schneider@shopify.com>
2023-04-06 18:29:10 -0400

    Add AppProvider to polaris-patterns (#8884)

--- polaris-patterns/src/index.ts
+++ polaris-patterns/src/index.ts
@@ -1,2 +1,7 @@
+import './configure';
+
 export {AccountConnection} from './components/AccountConnection';
 export type {AccountConnectionProps} from './components/AccountConnection';
+
+export {AppProvider} from './components/AppProvider';
+export type {AppProviderProps} from './components/AppProvider';
