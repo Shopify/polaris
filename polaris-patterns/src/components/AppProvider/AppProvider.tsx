0f4608fc4cd2e8245c7c3a3ae301c9c9a4af3255
Sophie Schneider <thesophieschneider@gmail.com>
2023-04-10 20:17:58 +0000

    remove app provider

--- polaris-patterns/src/components/AppProvider/AppProvider.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type {AppProviderProps as PolarisAppProviderProps} from '@shopify/polaris';
-import {AppProvider as PolarisAppProvider} from '@shopify/polaris';
-
-export function AppProvider({children, ...rest}: PolarisAppProviderProps) {
-  return <PolarisAppProvider {...rest}>{children}</PolarisAppProvider>;
-}
-
-export type AppProviderProps = PolarisAppProviderProps;
