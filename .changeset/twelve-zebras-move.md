e5b84e66d0dd98ad095cdff251c51a3c543e078a
Alex Page <hi@alexpage.dev>
2023-04-12 03:20:56 +1000

    Divider use new border tokens (#8909)
    ### WHY are these changes introduced?
    
    In version 11 the old tokens that divider use will be removed. This PR
    unblocks this by using the new approach now.
    
    ### WHAT is this pull request doing?
    
    Replacing `borderStyle` prop with `borderColor` and `borderWidth`
    
    **⚠️ Important note:** This is a breaking change to an alpha component.
    There are [9 uses
    of](https://coverage.polaris.shopify.io/usage/element?repo=shopify%2Fweb&type=components&element=%3CDivider%3E&props=borderStyle)
    `borderStyle` with `<Divider>` I don't think a migration is necessary
    for this change.
    
    ---------
    
    Co-authored-by: Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
    Co-authored-by: Lo Kim <lo.kim@shopify.com>

---
+++ .changeset/twelve-zebras-move.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris': minor
+---
+
+Replace borderStyle with borderColor and borderWidth in Divider
--- polaris.shopify.com/pages/examples/divider-with-border-color.tsx
+++ polaris.shopify.com/pages/examples/divider-with-border-color.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import type {ComponentMeta} from '@storybook/react';
-import {AlphaStack, Divider, Text} from '@shopify/polaris';
+import {Divider, Text, AlphaStack} from '@shopify/polaris';
 
-export default {
-  component: Divider,
-} as ComponentMeta<typeof Divider>;
+import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
 
-export function Default() {
-  return <Divider />;
-}
-
-export function WithBorderStyles() {
+function DividerWithBorderColorExample() {
   return (
     <AlphaStack gap="5">
       <Text as="h1" variant="headingXs">
@@ -32,3 +25,5 @@ export function WithBorderStyles() {
     </AlphaStack>
   );
 }
+
+export default withPolarisExample(DividerWithBorderColorExample);
