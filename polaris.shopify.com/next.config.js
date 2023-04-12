75566299fb4f4d192de71aa46a1cc1c241ecb97f
Lo Kim <lo.kim@shopify.com>
2023-04-12 18:41:44 -0400

    [Layout foundations] Update redirects for renamed layout components (#8948)
    ### WHY are these changes introduced?
    
    Resolves #8943.
    We renamed:
    - AlphaStack to VerticalStack
    - Inline to HorizontalStack
    - Columns to HorizontalGrid
    
    Updates the routing for the old component names to redirect to the new
    names for links outside of the polaris repo.
    
    ### WHAT is this pull request doing?
    
    Adds redirects for renamed layout components.
    
    ### How to 🎩
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    ### 🎩 checklist
    
    - [ ] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [ ] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [ ] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [ ] Updated the component's `README.md` with documentation changes
    - [x] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris.shopify.com/next.config.js
+++ polaris.shopify.com/next.config.js
@@ -331,18 +331,8 @@ const layoutAndStructure = [
     permanent: false,
   },
   {
-    source: '/components/structure/card',
-    destination: '/components/layout-and-structure/alpha-card',
-    permanent: false,
-  },
-  {
     source: '/components/layout-and-structure/card',
     destination: '/components/layout-and-structure/alpha-card',
-    permanent: false,
-  },
-  {
-    source: '/components/vertical-stack',
-    destination: '/components/layout-and-structure/vertical-stack',
     permanent: false,
   },
   {
@@ -361,7 +351,7 @@ const layoutAndStructure = [
     permanent: false,
   },
   {
-    source: '/components/horizontal-grid',
+    source: '/components/columns',
     destination: '/components/layout-and-structure/horizontal-grid',
     permanent: false,
   },
@@ -396,7 +386,7 @@ const layoutAndStructure = [
     permanent: false,
   },
   {
-    source: '/components/horizontal-stack',
+    source: '/components/inline',
     destination: '/components/layout-and-structure/horizontal-stack',
     permanent: false,
   },
@@ -436,12 +426,12 @@ const layoutAndStructure = [
     permanent: false,
   },
   {
-    source: '/components/stack',
-    destination: '/components/layout-and-structure/stack',
+    source: '/components/layout-and-structure/stack',
+    destination: '/components/layout-and-structure/vertical-stack',
     permanent: false,
   },
   {
-    source: '/components/layout-and-structure/stack',
+    source: '/components/layout-and-structure/alpha-stack',
     destination: '/components/layout-and-structure/vertical-stack',
     permanent: false,
   },
