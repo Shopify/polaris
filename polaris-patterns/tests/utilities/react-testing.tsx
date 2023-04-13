d462e712881d62980a6b8c1caef905f540fcd6b9
Sophie Schneider <sophie.schneider@shopify.com>
2023-04-13 11:09:36 -0400

    [Polaris Patterns][Prototype] Add single date picker code (#8930)
    Closes https://github.com/Shopify/polaris/issues/8925
    
    Should we allow translations within the `polaris-patterns` library? or
    make consumers pass translations as a prop?
    
    <img width="713" alt="Screenshot 2023-04-12 at 6 53 39 PM"
    src="https://user-images.githubusercontent.com/20652326/231603572-69a4037f-aeef-44bb-acfa-1178c5ec3242.png">

--- polaris-patterns/tests/utilities/react-testing.tsx
+++ polaris-patterns/tests/utilities/react-testing.tsx
@@ -7,6 +7,8 @@ import {
 } from '@shopify/react-testing';
 import {PolarisTestProvider} from '@shopify/polaris';
 import type {WithPolarisTestProviderOptions} from '@shopify/polaris';
+import polarisEnTranslations from '@shopify/polaris/locales/en.json';
+import {I18nContext, I18nManager} from '@shopify/react-i18n';
 
 export {createMount, mount, ReactTestingElement, CustomRoot};
 
@@ -18,10 +20,18 @@ export const mountWithApp = createMount<
     return options;
   },
   render(element, context) {
+    const i18nManager = new I18nManager({
+      locale: 'en',
+      currency: 'usd',
+      country: 'CA',
+    });
+
     return (
-      <PolarisTestProvider i18n={{}} {...context}>
-        {element}
-      </PolarisTestProvider>
+      <I18nContext.Provider value={i18nManager}>
+        <PolarisTestProvider i18n={{...polarisEnTranslations}} {...context}>
+          {element}
+        </PolarisTestProvider>
+      </I18nContext.Provider>
     );
   },
 });
