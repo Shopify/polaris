86a84faf6c5b4bd3324ead44aed5f0ec0b3cc379
Sophie Schneider <sophie.schneider@shopify.com>
2023-04-13 11:09:36 -0400

    [Polaris Patterns][Prototype] Add single date picker code (#8930)
    Closes https://github.com/Shopify/polaris/issues/8925
    
    Should we allow translations within the `polaris-patterns` library? or
    make consumers pass translations as a prop?
    
    <img width="713" alt="Screenshot 2023-04-12 at 6 53 39 PM"
    src="https://user-images.githubusercontent.com/20652326/231603572-69a4037f-aeef-44bb-acfa-1178c5ec3242.png">

--- polaris-patterns/.storybook/preview.js
+++ polaris-patterns/.storybook/preview.js
@@ -1,13 +1,23 @@
 import React from 'react';
-
-import {AppProvider as PolarisAppProvider} from '@shopify/polaris';
 import {breakpoints} from '@shopify/polaris-tokens';
+import {I18nContext, I18nManager} from '@shopify/react-i18n';
+import {AppProvider} from '@shopify/polaris';
+
+import polarisEnTranslations from '../../polaris-react/locales/en.json';
 
 function AppProviderDecorator(Story, context) {
+  const i18nManager = new I18nManager({
+    locale: 'en',
+    currency: 'usd',
+    country: 'CA',
+  });
+
   return (
-    <PolarisAppProvider>
-      <Story {...context} />
-    </PolarisAppProvider>
+    <I18nContext.Provider value={i18nManager}>
+      <AppProvider i18n={{...polarisEnTranslations}}>
+        <Story {...context} />
+      </AppProvider>
+    </I18nContext.Provider>
   );
 }
 
