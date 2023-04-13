d05de7b92c12a413044ab7dd40b2ef658959c65d
Sophie Schneider <sophie.schneider@shopify.com>
2023-04-13 11:09:36 -0400

    [Polaris Patterns][Prototype] Add single date picker code (#8930)
    Closes https://github.com/Shopify/polaris/issues/8925
    
    Should we allow translations within the `polaris-patterns` library? or
    make consumers pass translations as a prop?
    
    <img width="713" alt="Screenshot 2023-04-12 at 6 53 39 PM"
    src="https://user-images.githubusercontent.com/20652326/231603572-69a4037f-aeef-44bb-acfa-1178c5ec3242.png">

---
+++ polaris-patterns/src/components/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,139 @@
+import React, {useCallback, useState} from 'react';
+import type {Meta} from '@storybook/react';
+
+import {DatePicker} from './DatePicker';
+
+export default {
+  component: DatePicker,
+} as Meta<typeof DatePicker>;
+
+export function Default() {
+  const [{month, year}, setDate] = useState({month: 1, year: 2018});
+  const [selectedDates, setSelectedDates] = useState({
+    start: new Date('Wed Feb 07 2018 00:00:00 GMT-0500 (EST)'),
+    end: new Date('Wed Feb 07 2018 00:00:00 GMT-0500 (EST)'),
+  });
+
+  const handleMonthChange = useCallback(
+    (month, year) => setDate({month, year}),
+    [],
+  );
+
+  return (
+    <DatePicker
+      month={month}
+      year={year}
+      onChange={setSelectedDates}
+      onMonthChange={handleMonthChange}
+      selected={selectedDates}
+    />
+  );
+}
+
+export function Ranged() {
+  const [{month, year}, setDate] = useState({month: 1, year: 2018});
+  const [selectedDates, setSelectedDates] = useState({
+    start: new Date('Wed Feb 07 2018 00:00:00 GMT-0500 (EST)'),
+    end: new Date('Sat Feb 10 2018 00:00:00 GMT-0500 (EST)'),
+  });
+
+  const handleMonthChange = useCallback(
+    (month, year) => setDate({month, year}),
+    [],
+  );
+
+  return (
+    <DatePicker
+      month={month}
+      year={year}
+      onChange={setSelectedDates}
+      onMonthChange={handleMonthChange}
+      selected={selectedDates}
+      allowRange
+    />
+  );
+}
+
+export function MultiMonthRanged() {
+  const [{month, year}, setDate] = useState({month: 1, year: 2018});
+  const [selectedDates, setSelectedDates] = useState({
+    start: new Date('Wed Feb 07 2018 00:00:00 GMT-0500 (EST)'),
+    end: new Date('Mon Mar 12 2018 00:00:00 GMT-0500 (EST)'),
+  });
+
+  const handleMonthChange = useCallback(
+    (month, year) => setDate({month, year}),
+    [],
+  );
+
+  return (
+    <DatePicker
+      month={month}
+      year={year}
+      onChange={setSelectedDates}
+      onMonthChange={handleMonthChange}
+      selected={selectedDates}
+      multiMonth
+      allowRange
+    />
+  );
+}
+
+export function WithDisabledDateRanges() {
+  const [{month, year}, setDate] = useState({month: 1, year: 2018});
+  const [selectedDates, setSelectedDates] = useState({
+    start: new Date('Wed Feb 07 2018 00:00:00 GMT-0500 (EST)'),
+    end: new Date('Sat Feb 10 2018 00:00:00 GMT-0500 (EST)'),
+  });
+
+  const handleMonthChange = useCallback(
+    (month, year) => setDate({month, year}),
+    [],
+  );
+
+  return (
+    <DatePicker
+      month={month}
+      year={year}
+      onChange={setSelectedDates}
+      onMonthChange={handleMonthChange}
+      selected={selectedDates}
+      disableDatesBefore={new Date('Sat Feb 03 2018 00:00:00 GMT-0500 (EST)')}
+      disableDatesAfter={new Date('Sun Feb 18 2018 00:00:00 GMT-0500 (EST)')}
+      allowRange
+    />
+  );
+}
+
+export function WithSpecificDisabledDates() {
+  const [{month, year}, setDate] = useState({month: 1, year: 2018});
+  const [selectedDates, setSelectedDates] = useState(
+    new Date('Wed Feb 07 2018 00:00:00 GMT-0500 (EST)'),
+  );
+
+  const handleMonthChange = useCallback(
+    (month, year) => setDate({month, year}),
+    [],
+  );
+
+  const browserTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  const disableSpecificDates = [
+    new Date('Mon Feb 12 2018 00:00:00 GMT-0500 (EST)'),
+    new Date('Sat Feb 10 2018 00:00:00 GMT-0500 (EST)'),
+    new Date('Wed Feb 21 2018 00:00:00 GMT-0500 (EST)'),
+  ];
+
+  return (
+    <DatePicker
+      month={month}
+      year={year}
+      onChange={setSelectedDates}
+      onMonthChange={handleMonthChange}
+      selected={selectedDates}
+      disableDatesBefore={new Date('Sat Feb 03 2018 00:00:00 GMT-0500 (EST)')}
+      disableDatesAfter={new Date('Sun Feb 25 2018 00:00:00 GMT-0500 (EST)')}
+      disableSpecificDates={disableSpecificDates}
+    />
+  );
+}
---
+++ polaris-patterns/src/components/DatePickerTextField/DatePickerTextField.stories.tsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import type {Meta, StoryFn} from '@storybook/react';
+
+import {DatePickerTextField} from './DatePickerTextField';
+import type {Props} from './DatePickerTextField';
+
+const meta: Meta = {
+  component: DatePickerTextField,
+};
+
+export default meta;
+
+const Template: StoryFn<Props> = (args) => {
+  const [value, setValue] = useState(args.value);
+
+  return <DatePickerTextField {...args} value={value} onChange={setValue} />;
+};
+
+const TemplateBlur: StoryFn<Props> = (args) => {
+  const [value, setValue] = useState(args.value);
+  const [result, setResult] = useState('');
+
+  return (
+    <>
+      <DatePickerTextField
+        {...args}
+        value={value}
+        onChange={setValue}
+        onBlur={setResult}
+      />
+      <div>Value: {result}</div>
+    </>
+  );
+};
+
+const defaultArgs = {
+  value: '2022-07-15',
+  datePlaceholder: 'YYYY-MM-DD',
+  label: 'Date',
+  onChange: () => {},
+  onBlur: () => {},
+};
+
+export const Basic = Template.bind({});
+export const FormatOnBlur = Template.bind({});
+export const HelpText = Template.bind({});
+export const NoDatePicker = Template.bind({});
+export const ReturnOnBlur = TemplateBlur.bind({});
+
+Basic.args = {
+  ...defaultArgs,
+};
+FormatOnBlur.args = {
+  ...defaultArgs,
+  formatDateOnBlur: true,
+};
+HelpText.args = {
+  ...defaultArgs,
+  helpText: 'Please enter a date in YYYY-MM-DD format',
+};
+NoDatePicker.args = {
+  ...defaultArgs,
+  hideDatePicker: true,
+};
+ReturnOnBlur.args = {
+  ...defaultArgs,
+  formatDateOnBlur: true,
+};
