32a4e385535a2d410fdf8d1761f6770ef156f141
Sophie Schneider <thesophieschneider@gmail.com>
2023-04-21 23:17:52 +0000

    bug in playground

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import type {ReactNode} from 'react';
+import React, {useState} from 'react';
 
-import {Page} from '../src';
+import {Popover, UnstyledButton} from '../src';
 
 export function Playground() {
+  return <DefinitionPopover definition="hello world" title="hello world" />;
+}
+
+export interface Props {
+  /** A title to be displayed in the popover */
+  title: string;
+  /** The content to be displayed in the popover */
+  definition: ReactNode;
+}
+
+export function DefinitionPopover({title, definition}: Props) {
+  const [hoverActive, setHoverActive] = useState(false);
+
+  function handleClose() {
+    setHoverActive(false);
+  }
+
+  function handleActivatorMouseEnter() {
+    console.log('handleActivatorMouseEnter');
+    setHoverActive(true);
+  }
+
+  function handleActivatorMouseLeave() {
+    console.log('handleActivatorMouseLeave');
+    setHoverActive(false);
+  }
+
   return (
-    <Page title="Playground">
-      {/* Add the code you want to test in here */}
-    </Page>
+    <Popover
+      active={hoverActive}
+      activator={
+        <UnstyledButton
+          aria-describedby="hi"
+          type="button"
+          onMouseEnter={handleActivatorMouseEnter}
+          onMouseLeave={handleActivatorMouseLeave}
+        >
+          {title}
+        </UnstyledButton>
+      }
+      onClose={handleClose}
+    >
+      {definition}
+    </Popover>
   );
 }
