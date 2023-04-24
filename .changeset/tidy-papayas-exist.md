32dae86382534f8418ff52f713742cbc81eb72e7
Jess Telford <jess.telford@shopify.com>
2023-04-24 14:13:26 +1000

    Merge /patterns-legacy index page into /patterns to ease discovery
    Fixes #8949

---
+++ .changeset/tidy-papayas-exist.md
@@ -0,0 +1,5 @@
+---
+'polaris.shopify.com': patch
+---
+
+Merge /patterns-legacy index page into /patterns to ease discovery (includes a redirect so old links continue to work)
--- polaris.shopify.com/src/components/PatternsPage/index.tsx
+++ polaris.shopify.com/src/components/PatternsPage/index.tsx
@@ -3,12 +3,14 @@ import PageMeta from '../PageMeta';
 import Longform from '../Longform';
 import {Grid, GridItem} from '../Grid';
 import {Stack} from '../Stack';
+import {Heading} from '../Heading';
 import {Status} from '../../types';
 import styles from './PatternsPage.module.scss';
 import Preview from '../ThumbnailPreview';
-import {patterns} from '../../utils/various';
+import {patterns, legacyPatterns} from '../../utils/various';
 import UpdateBanner from '../UpdateBanner';
 import {FoundationsProps} from '../FoundationsPage/FoundationsPage';
+import FoundationsThumbnail from '../FoundationsThumbnail';
 
 export const PatternsPage = ({title, description}: FoundationsProps) => (
   <>
@@ -18,11 +20,13 @@ export const PatternsPage = ({title, description}: FoundationsProps) => (
       <Stack gap={'8'}>
         <div>
           <Longform>
-            <h1 className={styles.Header}>Patterns </h1>
+            <Heading as="h1" className={styles.Header}>
+              Patterns
+            </Heading>
             <p>{description}</p>
           </Longform>
           <UpdateBanner
-            message={`Our pattern documentation is evolving. [Join our discussions](https://github.com/Shopify/polaris/discussions/categories/pattern-documentation) to make it better. The [legacy patterns documentation](/patterns-legacy) can still be used.`}
+            message={`Our pattern documentation is evolving. [Join our discussions](https://github.com/Shopify/polaris/discussions/categories/pattern-documentation) to make it better. The [legacy patterns documentation](#legacy) can still be used.`}
             className={styles.UpdateBanner}
           />
         </div>
@@ -37,6 +41,34 @@ export const PatternsPage = ({title, description}: FoundationsProps) => (
                 url={pattern.url ?? ''}
                 renderPreview={() => (
                   <Preview alt={pattern.title} src={pattern.previewImg} />
+                )}
+                status={pattern.status as Status}
+              />
+            ))}
+        </Grid>
+        <Longform>
+          <Heading as="h1" id="legacy" className={styles.Header}>
+            Legacy Patterns
+          </Heading>
+          <p>
+            Previously documented patterns are considered legacy and should not
+            be used for new projects. Use at your own risk.
+          </p>
+        </Longform>
+        <Grid>
+          {Object.values(legacyPatterns)
+            .filter(({frontMatter: {draft}}) => !draft)
+            .map(({frontMatter: pattern}, index) => (
+              <GridItem
+                key={index}
+                title={pattern.title}
+                description={pattern.description ?? ''}
+                url={pattern.url ?? ''}
+                renderPreview={() => (
+                  <FoundationsThumbnail
+                    icon={pattern.icon!}
+                    category={pattern.title.toLowerCase()}
+                  />
                 )}
                 status={pattern.status as Status}
               />
