e9871563ff2f6b5b3f8a78a8d8032068fdf15ac4
Zakaria Warsame <zakaria.warsame@shopify.com>
2023-04-26 14:53:53 -0400

    review: remove unused vals

--- polaris-react/src/components/ActionList/components/Item/Item.tsx
+++ polaris-react/src/components/ActionList/components/Item/Item.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 
 import {classNames} from '../../../../utilities/css';
 import type {ActionListItemDescriptor} from '../../../../types';
