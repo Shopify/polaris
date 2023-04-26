c09654ddfd1c8e06aea7614839507df464e93461
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2023-04-26 11:17:59 -0700

    Remove `Polaris.VERSION` from the global window object (#9091)

---
+++ .changeset/light-steaks-promise.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris': major
+---
+
+Remove `Polaris.VERSION` from the global window object
--- polaris-react/scripts/build-validate.js
+++ polaris-react/scripts/build-validate.js
@@ -118,10 +118,7 @@ function validateVersionReplacement() {
   assert.strictEqual(fileBuckets.includesTemplateString.length, 0);
 
   assert.deepStrictEqual(fileBuckets.includesVersion, [
-    './build/cjs/configure.js',
-    './build/esm/configure.js',
     './build/esm/styles.css',
-    './build/esnext/configure.esnext',
     './build/esnext/components/AppProvider/AppProvider.css',
   ]);
 }
