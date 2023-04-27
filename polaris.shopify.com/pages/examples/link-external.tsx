7ae2d2f0b3329cb69918127e949a8247fbd04c3f
aveline <aveline@users.noreply.github.com>
2023-04-27 14:21:57 -0700

    Update link-external.tsx

--- polaris.shopify.com/pages/examples/link-external.tsx
+++ polaris.shopify.com/pages/examples/link-external.tsx
@@ -4,7 +4,7 @@ import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
 
 function LinkExample() {
   return (
-    <Link href="https://help.shopify.com/manual" external>
+    <Link href="https://help.shopify.com/manual" target="_blank">
       Shopify Help Center
     </Link>
   );
