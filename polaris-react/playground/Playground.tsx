5294c28c5cb1a1e0f73a5889cf136d7de10282e5
Kyle Durand <kyledurand@users.noreply.github.com>
2023-04-28 15:21:03 -0400

    fix build

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -7,10 +7,16 @@ console.log(atoms);
 export function Playground() {
   return (
     <Page title="Playground">
-      <HorizontalGrid columns={3} gap="5">
+      <HorizontalGrid columns={3} gap="1">
         <div style={{background: 'red'}}>1</div>
         <div style={{background: 'red'}}>2</div>
         <div style={{background: 'red'}}>3</div>
+        <div
+          className={atoms({padding: 'space-3'})}
+          style={{background: 'red'}}
+        >
+          padded atom
+        </div>
       </HorizontalGrid>
     </Page>
   );
