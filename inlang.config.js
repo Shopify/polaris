53a7ee4480f176f42fb186e259ba0278da7025e3
Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
2023-04-28 15:34:17 +0200

    Create inlang.config.js

---
+++ inlang.config.js
@@ -0,0 +1,27 @@
+
+/**
+ * @type { import("@inlang/core/config").DefineConfig }
+ */
+export async function defineConfig(env) {
+  const plugin = await env.$import(
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+  );
+
+  const { standardLintRules } = await env.$import(
+    "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js"
+  );
+
+  const pluginConfig = {
+    pathPattern: "./polaris-react/locales/{language}.json",
+  };
+
+  return {
+    referenceLanguage: "main",
+    languages: await plugin.getLanguages({ ...env, pluginConfig }),
+    readResources: (args) => plugin.readResources({ ...args, ...env, pluginConfig }),
+    writeResources: (args) => plugin.writeResources({ ...args, ...env, pluginConfig }),
+    lint: {
+      rules: [standardLintRules()],
+    },
+  };
+}
