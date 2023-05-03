e1607f21cdcbc9e99635749b8d1415df5c6ac436
Sophie Schneider <sophie.schneider@shopify.com>
2023-05-03 17:04:48 -0400

    Remove date picking from `polaris-patterns` (#9158)
    Decision log entry here:
    https://vault.shopify.io/gsd/projects/33833/decisions/2291

--- polaris-patterns/src/components/PolarisPatternsProvider/i18n.ts
+++ polaris-patterns/src/components/PolarisPatternsProvider/i18n.ts
@@ -1,14 +1,9 @@
-import {createContext} from 'react';
 import {createInstance} from 'i18next';
 import {initReactI18next} from 'react-i18next';
 
 import enTranslations from '../../../locales/en.json';
 
 import {DEFAULT_I18N_DETAILS} from './constants';
-import type {I18nDetails} from './PolarisPatternsProvider';
-
-export const I18nDetailsContext =
-  createContext<I18nDetails>(DEFAULT_I18N_DETAILS);
 
 export const i18next = createInstance({
   fallbackLng: DEFAULT_I18N_DETAILS.locale,
