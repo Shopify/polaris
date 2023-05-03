62153165444e99ec80cfdbe83747bc5cf5623cdb
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2023-05-03 11:34:29 -0400

    Update test

--- polaris-migrator/src/migrations/v12-react-replace-link-components/tests/v12-react-replace-link-components.output.tsx
+++ polaris-migrator/src/migrations/v12-react-replace-link-components/tests/v12-react-replace-link-components.output.tsx
@@ -2,18 +2,15 @@ import {Link} from '@shopify/polaris';
 import React from 'react';
 
 export function App() {
-  const x = false;
+  const condition = false;
 
   return (
     <>
       <Link href="https://help.shopify.com/">Click Me</Link>
-      <Link href={x || `https://help.shopify.com/`}>Click Me</Link>
+      <Link href={condition || `https://help.shopify.com/`}>Click Me</Link>
       <Link tone="inherit">Click Me</Link>
-      {/* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */}
-      <Link monochrome={false}>Click Me</Link>
-      <Link underline="none">Click Me</Link>
-      {/* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */}
-      <Link removeUnderline={false}>Click Me</Link>
+      <Link>Click Me</Link>
+      <Link target="_blank">Click Me</Link>
     </>
   );
 }
--- polaris-migrator/src/migrations/v12-react-replace-link-components/tests/v12-react-replace-link-components.test.ts
+++ polaris-migrator/src/migrations/v12-react-replace-link-components/tests/v12-react-replace-link-components.test.ts
@@ -1,7 +1,10 @@
 import {check} from '../../../utilities/testUtils';
 
 const migration = 'v12-react-replace-link-components';
-const fixtures = ['v12-react-replace-link-components'];
+const fixtures = [
+  'v12-react-replace-link-components',
+  'v12-react-replace-link-components-invalid',
+];
 
 for (const fixture of fixtures) {
   check(__dirname, {
