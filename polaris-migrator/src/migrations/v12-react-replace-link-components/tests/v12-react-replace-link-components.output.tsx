70ab4f43147457639eb8e0220ca06f1d98a5ccf0
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2023-05-12 10:07:21 -0400

    Refactor migration

--- polaris-migrator/src/migrations/v12-react-replace-link-components/tests/v12-react-replace-link-components.output.tsx
+++ polaris-migrator/src/migrations/v12-react-replace-link-components/tests/v12-react-replace-link-components.output.tsx
@@ -6,10 +6,6 @@ export function App() {
 
   return (
     <>
-      <Link href="https://help.shopify.com/">Click Me</Link>
-      <Link href={condition || `https://help.shopify.com/`}>Click Me</Link>
-      <Link tone="inherit">Click Me</Link>
-      <Link>Click Me</Link>
       <Link target="_blank">Click Me</Link>
     </>
   );
