ffe96a05fea3a49dd3989b30b9182b7be9ad45e0
Yuraima Estevez <yuraima.estevez@shopify.com>
2023-05-15 15:37:07 -0500

    Add analytics call with new `service` endpoint (#9202)
    <!--
      ☝️How to write a good PR title:
    - Prefix it with [ComponentName] (if applicable), for example: [Button]
      - Start with a verb, for example: Add, Delete, Improve, Fix…
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ### WHY are these changes introduced?
    
    Fixes https://github.com/Shopify/polaris/issues/9084 <!-- link to issue
    if one exists -->
    
    <!--
      Context about the problem that’s being addressed.
    -->
    
    ### WHAT is this pull request doing?
    
    * Adds a new next API endpoint `/service` that makes calls to the
    backend service
    * Replaces the Google Analytics search events with calls to the new
    `/service` endpoint
    
    <!--
      Summary of the changes committed.
    
    Before / after screenshots are appreciated for UI changes. Make sure to
    include alt text that describes the screenshot.
    
    If you include an animated gif showing your change, wrapping it in a
    details tag is recommended. Gifs usually autoplay, which can cause
    accessibility issues for people reviewing your PR:
    
        <details>
          <summary>Summary of your gif(s)</summary>
          <img src="..." alt="Description of what the gif shows">
        </details>
    -->
    
    <!-- ℹ️ Delete the following for small / trivial changes -->
    
    ### How to 🎩
    
    Run dev server and check the server console logs for `Analytics event
    logged` along with the event data captured
    
    <img width="543" alt="Screenshot 2023-05-12 at 12 00 49 PM"
    src="https://github.com/Shopify/polaris/assets/4642404/869f5744-0b72-454c-b04d-d01e025aa343">
    
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    <!--
      Give as much information as needed to experiment with the component
      in the playground.
    -->
    
    <details>
    <summary>Copy-paste this code in
    <code>playground/Playground.tsx</code>:</summary>
    
    ```jsx
    import React from 'react';
    import {Page} from '../src';
    
    export function Playground() {
      return (
        <Page title="Playground">
          {/* Add the code you want to test in here */}
        </Page>
      );
    }
    ```
    
    </details>
    
    ### 🎩 checklist
    
    - [ ] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [ ] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [ ] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [ ] Updated the component's `README.md` with documentation changes
    - [ ] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

---
+++ polaris.shopify.com/pages/api/service/index.tsx
@@ -0,0 +1,94 @@
+import type {NextApiResponse, NextApiRequest} from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
+const enableLogs = process.env.DEBUG_ANALYTICS === 'true';
+const ANALYTICS_URL = isProd
+  ? 'https://polaris.sfe.shopifyinternal.com/analytics'
+  : 'http://localhost:3001/analytics'; // local service
+
+type Event = {
+  id: string;
+  payload: any;
+};
+
+type AnalyticsProducerFunction = (event: Event) => void;
+
+type AnalyticsProducerType = 'log' | 'http';
+
+class AnalyticsProducer {
+  constructor(producer: AnalyticsProducerType) {
+    this.produce =
+      producer === 'http' ? this.produceHttpRequest : this.produceLog;
+  }
+
+  public produce: AnalyticsProducerFunction;
+
+  private produceLog(event: Event) {
+    if (!isProd && enableLogs) {
+      console.log('Analytics event logged', event);
+    }
+  }
+
+  private async produceHttpRequest(event: Event) {
+    if (isProd) {
+      try {
+        const response = await fetch(ANALYTICS_URL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(event),
+        });
+        const data = await response.json();
+
+        if (response.status !== 200 || data.error) {
+          logError(`Analytics call failed. Error: ${data.error}`);
+        }
+      } catch (error) {
+        logError(`Analytics call failed. Error: ${error}`);
+      }
+    }
+  }
+}
+
+function logError(error: any) {
+  if (enableLogs) {
+    console.error(error);
+  }
+}
+
+export async function captureAnalyticsEvent(id: string, payload: any) {
+  const event = {
+    id,
+    payload,
+  };
+
+  const producerType = isProd ? 'http' : 'log';
+
+  const analysisProducer = new AnalyticsProducer(producerType);
+
+  analysisProducer.produce(event);
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {body} = req;
+
+  if (!body || !body.id || !body.payload) {
+    return res.status(400).send({});
+  }
+
+  const event = {
+    id: body.id,
+    payload: body.payload,
+  };
+
+  const producerType = isProd ? 'http' : 'log';
+
+  const analytics = new AnalyticsProducer(producerType);
+
+  analytics.produce(event);
+
+  return res.status(200).send({});
+};
+
+export default handler;
