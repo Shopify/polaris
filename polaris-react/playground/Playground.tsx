6749199e8356b321f7313c6bdfed48512a0e6910
Sara Hill <sarashill22@gmail.com>
2023-05-16 13:19:32 -0700

    uplift button styles first pass

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,11 +1,78 @@
 import React from 'react';
+import {LocationsMinor} from '@shopify/polaris-icons';
 
-import {Page} from '../src';
+import {Button, HorizontalStack, Page, VerticalStack} from '../src';
 
 export function Playground() {
   return (
     <Page title="Playground">
-      {/* Add the code you want to test in here */}
+      <VerticalStack gap="8">
+        <HorizontalStack gap="4" blockAlign="end">
+          <Button>Label</Button>
+          <Button disabled>Label</Button>
+          <Button pressed>Label</Button>
+          <Button icon={LocationsMinor}>Label</Button>
+          <Button disclosure>Label</Button>
+          {/* <Button
+            connectedDisclosure={{
+              accessibilityLabel: 'Other save actions',
+              actions: [{content: 'Action'}],
+            }}
+          >
+            Label
+          </Button> */}
+        </HorizontalStack>
+
+        <HorizontalStack gap="4" blockAlign="end">
+          <Button primary>Label</Button>
+          <Button primary disabled>
+            Label
+          </Button>
+          <Button primary pressed>
+            Label
+          </Button>
+          <Button primary icon={LocationsMinor}>
+            Label
+          </Button>
+          <Button primary disclosure>
+            Label
+          </Button>
+          {/* <Button
+            primary
+            connectedDisclosure={{
+              accessibilityLabel: 'Other save actions',
+              actions: [{content: 'Action'}],
+            }}
+          >
+            Label
+          </Button> */}
+        </HorizontalStack>
+
+        <HorizontalStack gap="4" blockAlign="end">
+          <Button destructive>Label</Button>
+          <Button destructive disabled>
+            Label
+          </Button>
+          <Button destructive pressed>
+            Label
+          </Button>
+          <Button destructive icon={LocationsMinor}>
+            Label
+          </Button>
+          <Button destructive disclosure>
+            Label
+          </Button>
+          {/* <Button
+            destructive
+            connectedDisclosure={{
+              accessibilityLabel: 'Other save actions',
+              actions: [{content: 'Action'}],
+            }}
+          >
+            Label
+          </Button> */}
+        </HorizontalStack>
+      </VerticalStack>
     </Page>
   );
 }
