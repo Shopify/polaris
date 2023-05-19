66c8ff429b0f39728fea1d93f2855f083850d8cd
Kyle Durand <kyledurand@users.noreply.github.com>
2023-05-19 10:00:35 -0400

    Update polaris-react/src/utilities/features/context.ts
    Co-authored-by: Lo Kim <lo.kim@shopify.com>

--- polaris-react/src/utilities/features/context.ts
+++ polaris-react/src/utilities/features/context.ts
@@ -2,7 +2,7 @@ import {createContext} from 'react';
 
 import type {FeaturesConfig} from './types';
 
-export const summerEditions2023ClassName = 'Polaris-summer-editions-2023';
+export const summerEditions2023ClassName = 'Polaris-Summer-Editions-2023';
 
 export const FeaturesContext = createContext<FeaturesConfig | undefined>(
   undefined,
