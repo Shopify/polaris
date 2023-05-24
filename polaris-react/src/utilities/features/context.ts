377a45600b59a6028da4d2364ec0731a8b1f3132
Yuraima Estevez <yuraima.estevez@shopify.com>
2023-05-24 11:16:02 -0500

    Revert "Add global class and scss helper (#9238)"
    This reverts commit 35ee2189f7169fa02aaf8f120b1c24f8c0332ba7.

--- polaris-react/src/utilities/features/context.ts
+++ polaris-react/src/utilities/features/context.ts
@@ -2,8 +2,6 @@ import {createContext} from 'react';
 
 import type {FeaturesConfig} from './types';
 
-export const summerEditions2023ClassName = 'Polaris-Summer-Editions-2023';
-
 export const FeaturesContext = createContext<FeaturesConfig | undefined>(
   undefined,
 );
