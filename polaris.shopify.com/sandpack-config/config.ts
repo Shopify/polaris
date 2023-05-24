28412a14536cbadc6b2d3b968fce1182e5e23263
Jess Telford <jess.telford@shopify.com>
2023-05-24 14:20:53 +1000

    WIP: add config.ts

---
+++ polaris.shopify.com/sandpack-config/config.ts
@@ -0,0 +1,12 @@
+export default {
+  files: {
+    '/App.js': {
+      code: `// frame component`,
+      readOnly: true,
+      hidden: true,
+    },
+    '/code.js': {
+      code: `// the app developer's actual code`,
+    },
+  },
+};
