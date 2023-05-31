7cc8d6554f7c8e2ec4c2f7f773ceb47dc9cbe135
kyledurand <kyle.durand@shopify.com>
2023-05-31 13:08:47 -0400

    experimental position

--- polaris-tokens/src/token-groups/font.ts
+++ polaris-tokens/src/token-groups/font.ts
@@ -12,7 +12,7 @@ export type FontSizeScale =
   | '600'
   | '700';
 
-export type FontSizeExperimentalScale = '70' | '80';
+export type FontExperimentalSizeScale = '70' | '80';
 
 export type FontLineHeightScale = '1' | '2' | '3' | '4' | '5' | '6' | '7';
 
@@ -21,7 +21,7 @@ export type FontWeightAlias = 'regular' | 'medium' | 'semibold' | 'bold';
 export type FontTokenName =
   | `font-family-${FontFamilyAlias}`
   | `font-size-${FontSizeScale}`
-  | `font-size-experimental-${FontSizeExperimentalScale}`
+  | `font-experimental-size-${FontExperimentalSizeScale}`
   | `font-weight-${FontWeightAlias}`
   | `font-line-height-${FontLineHeightScale}`;
 
@@ -40,13 +40,13 @@ export const font: {
     value:
       "ui-monospace, SFMono-Regular, 'SF Mono', Consolas, 'Liberation Mono', Menlo, monospace",
   },
-  'font-size-experimental-70': {
+  'font-experimental-size-70': {
     value: '11px',
   },
   'font-size-75': {
     value: '12px',
   },
-  'font-size-experimental-80': {
+  'font-experimental-size-80': {
     value: '13px',
   },
   'font-size-100': {
