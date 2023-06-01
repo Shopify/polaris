26bd4b566d898327163eabff8352e6f4fcb5dc17
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2023-06-01 09:50:15 -0700

    Add new line-height token

--- polaris-tokens/src/token-groups/font.ts
+++ polaris-tokens/src/token-groups/font.ts
@@ -1,6 +1,10 @@
 import type {MetadataProperties} from '../types';
 
+type Experimental<T extends string> = `${T}-experimental`;
+
 type FontFamilyAlias = 'sans' | 'mono';
+
+type FontSizeScaleExperimental = Experimental<'70' | '80'>;
 
 export type FontSizeScale =
   | '75'
@@ -10,18 +14,26 @@ export type FontSizeScale =
   | '400'
   | '500'
   | '600'
-  | '700';
+  | '700'
+  | FontSizeScaleExperimental;
 
-type FontExperimentalSizeScale = '70' | '80';
+type FontLineHeightScaleExperimental = Experimental<'075'>;
 
-export type FontLineHeightScale = '1' | '2' | '3' | '4' | '5' | '6' | '7';
+export type FontLineHeightScale =
+  | '1'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | FontLineHeightScaleExperimental;
 
 export type FontWeightAlias = 'regular' | 'medium' | 'semibold' | 'bold';
 
 export type FontTokenName =
   | `font-family-${FontFamilyAlias}`
   | `font-size-${FontSizeScale}`
-  | `font-experimental-size-${FontExperimentalSizeScale}`
   | `font-weight-${FontWeightAlias}`
   | `font-line-height-${FontLineHeightScale}`;
 
@@ -40,13 +52,13 @@ export const font: {
     value:
       "ui-monospace, SFMono-Regular, 'SF Mono', Consolas, 'Liberation Mono', Menlo, monospace",
   },
-  'font-experimental-size-70': {
+  'font-size-70-experimental': {
     value: '11px',
   },
   'font-size-75': {
     value: '12px',
   },
-  'font-experimental-size-80': {
+  'font-size-80-experimental': {
     value: '13px',
   },
   'font-size-100': {
@@ -83,6 +95,9 @@ export const font: {
   },
   'font-weight-bold': {
     value: '700',
+  },
+  'font-line-height-075-experimental': {
+    value: '12px',
   },
   'font-line-height-1': {
     value: '16px',
