82955fb3d54d770d21fa7ca454b96cfaaf601473
aveline <aveline@users.noreply.github.com>
2023-06-05 10:22:10 -0700

    Rename `replace-link` migration for v11

--- polaris-migrator/src/migrations/v11-react-replace-link-external-property/tests/v11-react-replace-link-external-property.test.ts
+++ polaris-migrator/src/migrations/v11-react-replace-link-external-property/tests/v11-react-replace-link-external-property.test.ts
@@ -1,9 +1,9 @@
 import {check} from '../../../utilities/testUtils';
 
-const migration = 'v12-react-replace-link-external-property';
+const migration = 'v11-react-replace-link-external-property';
 const fixtures = [
-  'v12-react-replace-link-external-property',
-  'v12-react-replace-link-external-property-invalid',
+  'v11-react-replace-link-external-property',
+  'v11-react-replace-link-external-property-invalid',
 ];
 
 for (const fixture of fixtures) {
