3fa710a8502f3a14448d6547e583da559f5c26d7
aveline <aveline@users.noreply.github.com>
2023-06-05 10:56:05 -0700

    Add relative tests

--- polaris-migrator/src/migrations/v11-react-replace-link-external-property/tests/v11-react-replace-link-external-property.test.ts
+++ polaris-migrator/src/migrations/v11-react-replace-link-external-property/tests/v11-react-replace-link-external-property.test.ts
@@ -2,13 +2,24 @@ import {check} from '../../../utilities/testUtils';
 
 const migration = 'v11-react-replace-link-external-property';
 const fixtures = [
-  'v11-react-replace-link-external-property',
-  'v11-react-replace-link-external-property-invalid',
+  {
+    name: 'v11-react-replace-link-external-property',
+  },
+  {
+    name: 'v11-react-replace-link-external-property-invalid',
+  },
+  {
+    name: 'with-relative-link',
+    options: {
+      relative: true,
+    },
+  },
 ];
 
 for (const fixture of fixtures) {
   check(__dirname, {
-    fixture,
+    fixture: fixture.name,
     migration,
+    options: fixture.options,
   });
 }
---
+++ polaris-migrator/src/migrations/v11-react-replace-link-external-property/tests/with-relative-link.output.tsx
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import React from 'react';
+
+import {Link} from '../Link';
+
+export function App() {
+  return <Link target="_blank">Link</Link>;
+}
