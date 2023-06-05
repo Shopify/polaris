bf5aa2445dd64fff89e1465d55357a90369bcb03
aveline <aveline@users.noreply.github.com>
2023-06-05 11:32:27 -0700

    Insert comments on all identifiers
    Co-Authored-By: Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>

--- polaris-migrator/src/migrations/v11-react-replace-link-external-property/tests/v11-react-replace-link-external-property-invalid.input.tsx
+++ polaris-migrator/src/migrations/v11-react-replace-link-external-property/tests/v11-react-replace-link-external-property-invalid.input.tsx
@@ -1,13 +1,21 @@
+import type {LinkProps} from '@shopify/polaris';
 import {Link} from '@shopify/polaris';
 import React from 'react';
 
 export function App() {
   const condition = false;
 
+  const linkProps: LinkProps = {
+    external: true,
+  };
+
+  const MyLink = Link;
+
   return (
     <>
       <Link external={false}>Click Me</Link>
       <Link external={condition}>Click Me</Link>
+      <MyLink external>My link</MyLink>
     </>
   );
 }
--- polaris-migrator/src/migrations/v11-react-replace-link-external-property/tests/v11-react-replace-link-external-property-invalid.output.tsx
+++ polaris-migrator/src/migrations/v11-react-replace-link-external-property/tests/v11-react-replace-link-external-property-invalid.output.tsx
@@ -1,8 +1,18 @@
+import type {LinkProps} from '@shopify/polaris';
 import {Link} from '@shopify/polaris';
 import React from 'react';
 
 export function App() {
   const condition = false;
+
+  const linkProps: /* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */
+  LinkProps = {
+    external: true,
+  };
+
+  const MyLink =
+    /* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */
+    Link;
 
   return (
     <>
@@ -10,6 +20,7 @@ export function App() {
       <Link external={false}>Click Me</Link>
       {/* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */}
       <Link external={condition}>Click Me</Link>
+      <MyLink external>My link</MyLink>
     </>
   );
 }
