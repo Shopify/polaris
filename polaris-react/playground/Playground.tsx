51491e8c319efba49d2823ad05957a56e8e9559b
Rose Cass <rose.cass@shopify.com>
2023-06-06 17:53:33 -0700

    Start updating column prop to accept string

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-import {Page} from '../src';
+import {Page, Grid, Text, LegacyStack, Button} from '../src';
 
 export function Playground() {
   return (
     <Page title="Playground">
-      {/* Add the code you want to test in here */}
+      {/* <Grid columns={{ xs: 10, sm: 10, md: 10, lg: 10, xl: 10 }}> */}
+      <Grid columns={10}>
+        <Grid.Cell columnSpan={{ xs: 2, md: 1 }}>
+          <Text as="h1">First columns</Text>
+        </Grid.Cell>
+        <Grid.Cell columnSpan={{ xs: 4, md: 6 }}>
+          <LegacyStack vertical spacing="none">
+            <Text variant="bodyMd" as="span" fontWeight="semibold">
+              TITLE
+            </Text>
+            <Text variant="bodyMd" as="span" color="subdued">
+              Variant Title
+            </Text>
+          </LegacyStack>
+        </Grid.Cell>
+        <Grid.Cell columnSpan={{ xs: 3, md: 2 }}>
+          <Text variant="bodyMd" as="span" color="subdued">
+            Variant Title
+          </Text>
+        </Grid.Cell>
+        <Grid.Cell columnSpan={{ xs: 1, md: 1 }}>
+          <Button> Click here </Button>
+        </Grid.Cell>
+      </Grid>
     </Page>
   );
 }
--- polaris-react/src/components/Grid/Grid.tsx
+++ polaris-react/src/components/Grid/Grid.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
-import {Cell} from './components';
+import { Cell } from './components';
 import styles from './Grid.scss';
 
+import {
+  ResponsiveValue,
+  ResponsiveProp,
+  getResponsiveValue,
+  sanitizeCustomProperties,
+} from '../../utilities/css';
+
 type Breakpoints = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+type ColumnsType = number | string;
 
 type Areas = {
   [Breakpoint in Breakpoints]?: string[];
 };
 
-type Columns = {
-  [Breakpoint in Breakpoints]?: number;
-};
+type Columns = ResponsiveProp<ColumnsType>;
 
 type Gap = {
   [Breakpoint in Breakpoints]?: string;
@@ -23,7 +29,11 @@ export interface GridProps {
    * @deprecated Use nested layout components instead
    */
   areas?: Areas;
-  /* Number of columns */
+  /** The number of columns to display. Accepts either a single value or an object of values for different screen sizes.
+   * @example
+   * columns={6}
+   * columns={{xs: 1, sm: 1, md: 3, lg: 6, xl: 6}}
+   */
   columns?: Columns;
   /* Grid gap */
   gap?: Gap;
@@ -31,18 +41,19 @@ export interface GridProps {
 }
 export const Grid: React.FunctionComponent<GridProps> & {
   Cell: typeof Cell;
-} = function Grid({gap, areas, children, columns}: GridProps) {
+} = function Grid({ gap, areas, children, columns }: GridProps) {
   const style = {
+    ...getResponsiveValue('grid', 'columns', formatColumns(columns)),
     '--pc-grid-gap-xs': gap?.xs,
     '--pc-grid-gap-sm': gap?.sm,
     '--pc-grid-gap-md': gap?.md,
     '--pc-grid-gap-lg': gap?.lg,
     '--pc-grid-gap-xl': gap?.xl,
-    '--pc-grid-columns-xs': columns?.xs,
-    '--pc-grid-columns-sm': columns?.sm,
-    '--pc-grid-columns-md': columns?.md,
-    '--pc-grid-columns-lg': columns?.lg,
-    '--pc-grid-columns-xl': columns?.xl,
+    // '--pc-grid-columns-xs': columns?.xs,
+    // '--pc-grid-columns-sm': columns?.sm,
+    // '--pc-grid-columns-md': columns?.md,
+    // '--pc-grid-columns-lg': columns?.lg,
+    // '--pc-grid-columns-xl': columns?.xl,
     '--pc-grid-areas-xs': formatAreas(areas?.xs),
     '--pc-grid-areas-sm': formatAreas(areas?.sm),
     '--pc-grid-areas-md': formatAreas(areas?.md),
@@ -51,11 +62,33 @@ export const Grid: React.FunctionComponent<GridProps> & {
   } as React.CSSProperties;
 
   return (
-    <div className={styles.Grid} style={style}>
+    <div className={styles.Grid} style={sanitizeCustomProperties(style)}>
       {children}
     </div>
   );
 };
+
+function formatColumns(columns?: Columns): ResponsiveValue {
+  if (typeof columns === 'object' && columns !== null) {
+    return Object.fromEntries(
+      Object.entries(columns).map(([breakpointAlias, breakpointGrid]) => [
+        breakpointAlias,
+        getColumnValue(breakpointGrid),
+      ]),
+    );
+  }
+  return getColumnValue(columns);
+}
+
+function getColumnValue(columns?: ColumnsType) {
+  if (!columns) return undefined;
+
+  if (typeof columns === 'number' || !isNaN(Number(columns))) {
+    return `repeat(${Number(columns)}, minmax(0, 1fr))`;
+  }
+
+  return columns;
+}
 
 export function formatAreas(areas?: string[]) {
   if (!areas) return;
