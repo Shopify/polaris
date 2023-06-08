4620b059009a8f702966f39076c52a7522772ad3
Laura Griffee <laura@mailzone.com>
2023-06-08 15:30:37 -0600

    Update `Divider` (#9414)
    ### WHY are these changes introduced?
    
    Resolves https://github.com/Shopify/polaris-summer-editions/issues/211
    
    
    ### WHAT is this pull request doing?
    
    - Updates Divider default border color
    
    
    ### How to 🎩
    
    Review on
    [Storybook](https://5d559397bae39100201eedc1-bdhoxdcfvu.chromatic.com/?path=/story/all-components-divider--with-border-styles&globals=polarisSummerEditions2023:true)
    - Ensure beta flagged Divider styles render as expected
    - Ensure no regressions to the existing Divider styles
    
    ### 🎩 checklist
    
    - [x] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [x] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [x] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    
    ---------
    
    Co-authored-by: Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>

--- polaris-react/src/components/Divider/Divider.tsx
+++ polaris-react/src/components/Divider/Divider.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import type {BorderWidthScale, ColorBorderAlias} from '@shopify/polaris-tokens';
 
+import {useFeatures} from '../../utilities/features';
+
 import styles from './Divider.scss';
 
 export interface DividerProps {
@@ -20,10 +22,16 @@ export const Divider = ({
   borderColor = 'border-subdued',
   borderWidth = '1',
 }: DividerProps) => {
+  const {polarisSummerEditions2023} = useFeatures();
+
   const borderColorValue =
     borderColor === 'transparent'
       ? borderColor
-      : `var(--p-color-${borderColor})`;
+      : `var(--p-color-${
+          polarisSummerEditions2023 && borderColor === 'border-subdued'
+            ? 'border-faint-experimental'
+            : borderColor
+        })`;
 
   return (
     <hr
