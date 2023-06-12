b679f2e4c924545418b55025a7d72b69998ca2c6
Lo Kim <lo.kim@shopify.com>
2023-06-12 10:16:46 -0400

    Update Listbox.Action style in with search story

--- polaris-react/src/components/Popover/Popover.stories.tsx
+++ polaris-react/src/components/Popover/Popover.stories.tsx
@@ -664,7 +664,13 @@ export function WithSearchableListbox() {
 
   const showAllMarkup = showFooterAction ? (
     <Listbox.Action value={actionValue}>
-      <span style={{color: 'var(--p-color-text-interactive)'}}>
+      <span
+        style={{
+          color: polarisSummerEditions2023
+            ? 'var(--p-color-text-subdued)'
+            : 'var(--p-color-text-interactive)',
+        }}
+      >
         Show all 111 segments
       </span>
     </Listbox.Action>
