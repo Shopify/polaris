19d04d79bc648fc0ef5f0efe0769402aa9259aea
aveline <aveline@users.noreply.github.com>
2023-06-20 15:25:14 -0700

    Icon `primary` and `plain` button updates (#9461)
    ### WHY are these changes introduced?
    
    Fixes https://github.com/Shopify/polaris-summer-editions/issues/317
    
    ### WHAT is this pull request doing?
    
    - Adds some negative margin for cases of Buttons with the combination of
    `primary` `plain` where the content is only an icon so that the button
    aligns to the icon/content rather than the button block.
    - Update colors of `plain` Buttons with only icon content from active
    blue to icon color
    
    ### How to 🎩
    
    - Review
    [Storybook](https://5d559397bae39100201eedc1-hlcnwluung.chromatic.com/?path=/story/all-components-button--plain-primary&globals=polarisSummerEditions2023:true)

--- polaris-react/src/components/Button/Button.stories.tsx
+++ polaris-react/src/components/Button/Button.stories.tsx
@@ -231,37 +231,7 @@ export function All() {
 
         <VerticalStack gap="2">
           <Text as="h2">plain</Text>
-          <HorizontalStack gap="5" blockAlign="end">
-            <Button plain>Label</Button>
-            <Button plain disabled>
-              Label
-            </Button>
-            <Button plain icon={PlusMinor}>
-              Label
-            </Button>
-            <Button plain disabled icon={PlusMinor}>
-              Label
-            </Button>
-            <Button plain disclosure>
-              Label
-            </Button>
-            <Button plain disclosure disabled>
-              Label
-            </Button>
-            <Button
-              plain
-              icon={CancelSmallMinor}
-              onClick={() => {}}
-              accessibilityLabel="Dismiss"
-            />
-            <Button
-              plain
-              disabled
-              icon={CancelSmallMinor}
-              onClick={() => {}}
-              accessibilityLabel="Dismiss"
-            />
-          </HorizontalStack>
+          <Plain />
         </VerticalStack>
 
         <VerticalStack gap="2">
@@ -360,7 +330,41 @@ export function OutlineMonochrome() {
 }
 
 export function Plain() {
-  return <Button plain>View shipping settings</Button>;
+  return (
+    <Box padding="4">
+      <HorizontalStack gap="5" blockAlign="end">
+        <Button plain>Label</Button>
+        <Button plain disabled>
+          Label
+        </Button>
+        <Button plain icon={PlusMinor}>
+          Label
+        </Button>
+        <Button plain disabled icon={PlusMinor}>
+          Label
+        </Button>
+        <Button plain disclosure>
+          Label
+        </Button>
+        <Button plain disclosure disabled>
+          Label
+        </Button>
+        <Button
+          plain
+          icon={CancelSmallMinor}
+          onClick={() => {}}
+          accessibilityLabel="Dismiss"
+        />
+        <Button
+          plain
+          disabled
+          icon={CancelSmallMinor}
+          onClick={() => {}}
+          accessibilityLabel="Dismiss"
+        />
+      </HorizontalStack>
+    </Box>
+  );
 }
 
 export function PlainPrimary() {
@@ -383,21 +387,6 @@ export function PlainPrimary() {
           <Button primary plain disclosure>
             Label
           </Button>
-          <Button
-            primary
-            plain
-            icon={CancelSmallMinor}
-            onClick={() => {}}
-            accessibilityLabel="Dismiss"
-          />
-          <Button
-            disabled
-            primary
-            plain
-            icon={EditMajor}
-            onClick={() => {}}
-            accessibilityLabel="Dismiss"
-          />
         </HorizontalStack>
       </Box>
       <Card>
@@ -417,6 +406,10 @@ export function PlainPrimary() {
           <Button primary plain disclosure>
             Label
           </Button>
+        </HorizontalStack>
+      </Card>
+      <Card>
+        <HorizontalStack gap="5" blockAlign="end">
           <Button
             primary
             plain
