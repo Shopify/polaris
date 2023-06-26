b4db546d16bbeb2bf54ce894e7879f8d36179d8e
Matt Kubej <86790511+mattkubej@users.noreply.github.com>
2023-06-26 14:36:49 -0400

    [Card] wrap with WithinContent.Provider, enable inset banner styling (#9538)
    ### WHY are these changes introduced?
    
    Addresses https://github.com/Shopify/polaris-summer-editions/issues/97
    
    Wraps the `Card` component with the WithinContentContext.Provider, which
    the `Banner` depends on to set it's inset styling (i.e. flat style).
    This also puts the `Card` component in line with the `LegacyCard`
    component.
    
    
    ### WHAT is this pull request doing?
    
    `Banner` children within the `Card` component will share the same style
    as `Banner` children within the `LegacyCard` component, which results in
    a flat style with a colored background.
    
    ### How to 🎩
    
    1. Validate the [Banner In A
    Card](https://storybook.web.polaris-card-withincontent.matt-kubej.us.spin.dev/?path=/story/all-components-banner--in-a-card&globals=polarisSummerEditions2023:true)
    story renders the `Banner` with the inset style (i.e. flat)
    2. Validate no regressions with the
    [Card](https://storybook.web.polaris-card-withincontent.matt-kubej.us.spin.dev/?path=/story/all-components-card--default)
    stories
    
    ### 🎩 checklist
    
    - [x] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [x] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [x] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [x] Updated the component's `README.md` with documentation changes
    - [x] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris-react/src/components/Card/tests/Card.test.tsx
+++ polaris-react/src/components/Card/tests/Card.test.tsx
@@ -3,6 +3,7 @@ import {mountWithApp} from 'tests/utilities';
 import {matchMedia} from '@shopify/jest-dom-mocks';
 import {setMediaWidth} from 'tests/utilities/breakpoints';
 
+import {WithinContentContext} from '../../../utilities/within-content-context';
 import {Card} from '..';
 
 const heading = <p>Online store dashboard</p>;
@@ -15,6 +16,27 @@ describe('Card', () => {
 
   afterEach(() => {
     matchMedia.restore();
+  });
+
+  it('has a child with prop withinContentContainer set to true', () => {
+    function TestComponent(_: {withinContentContainer: any}) {
+      return null;
+    }
+
+    const card = mountWithApp(
+      <Card>
+        <WithinContentContext.Consumer>
+          {(withinContentContext) => {
+            return (
+              <TestComponent withinContentContainer={withinContentContext} />
+            );
+          }}
+        </WithinContentContext.Consumer>
+      </Card>,
+    );
+    expect(card).toContainReactComponent(TestComponent, {
+      withinContentContainer: true,
+    });
   });
 
   it('renders children', () => {
