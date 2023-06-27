5bff04246e6559f73118c334388762e90a4478ef
Charles Lee <charles.lee@shopify.com>
2023-06-27 16:08:46 +1000

    Replace SecondaryAction markup with one sans css overrides

--- polaris-react/src/components/Page/Page.stories.tsx
+++ polaris-react/src/components/Page/Page.stories.tsx
@@ -328,3 +328,52 @@ export function WithDivider() {
     </Page>
   );
 }
+
+export function WithDisabledSecondaryAction() {
+  return (
+    <Page
+      backAction={{content: 'Products', url: '#'}}
+      title="3/4 inch Leather pet collar"
+      titleMetadata={<Badge status="success">Paid</Badge>}
+      subtitle="Perfect for any pet"
+      compactTitle
+      primaryAction={{content: 'Save', disabled: true}}
+      secondaryActions={[
+        {
+          content: 'Delete',
+          destructive: true,
+          disabled: true,
+          icon: DeleteMinor,
+          accessibilityLabel: 'Delete action label',
+          onAction: () => console.log('Delete action'),
+        },
+        {
+          content: 'View on your store',
+          icon: ViewMinor,
+          onAction: () => console.log('View on your store action'),
+        },
+      ]}
+      actionGroups={[
+        {
+          title: 'Promote',
+          icon: MobileVerticalDotsMajor,
+          actions: [
+            {
+              content: 'Share on Facebook',
+              accessibilityLabel: 'Individual action label',
+              onAction: () => console.log('Share on Facebook action'),
+            },
+          ],
+        },
+      ]}
+      pagination={{
+        hasPrevious: true,
+        hasNext: true,
+      }}
+    >
+      <LegacyCard title="Credit card" sectioned>
+        <p>Credit card information</p>
+      </LegacyCard>
+    </Page>
+  );
+}
