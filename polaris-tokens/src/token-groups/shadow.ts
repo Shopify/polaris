5a2805038140928b1e265012abed09b63c60abee
Charles Lee <charles.lee@shopify.com>
2023-06-28 09:49:10 +1000

    fix unitless decl in shadow token change

--- polaris-tokens/src/token-groups/shadow.ts
+++ polaris-tokens/src/token-groups/shadow.ts
@@ -78,7 +78,7 @@ export const shadow: {
   },
   'shadow-card-md-experimental': {
     value:
-      '0 3 1 -1 rgba(0, 0, 0, 0.07),  0 -1 0 0 rgba(0, 0, 0, 0.16),  1 0 0 0 rgba(227, 227, 227, 1),  -1 0 0 0 rgba(227, 227, 227, 1),  0 1 0 0 rgba(227, 227, 227, 1)',
+      '0px 3px 1px -1px rgba(0, 0, 0, 0.07),  0px -1px 0px 0px rgba(0, 0, 0, 0.16),  1px 0px 0px 0px rgba(227, 227, 227, 1),  -1px 0px 0px 0px rgba(227, 227, 227, 1),  0px 1px 0px 0px rgba(227, 227, 227, 1)',
   },
   'shadow-card-lg-experimental': {
     value:
