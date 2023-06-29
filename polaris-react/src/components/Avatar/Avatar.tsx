5f2d98e4e5d47deeef501106a0319fed3b56f284
aveline <aveline@users.noreply.github.com>
2023-06-29 12:13:27 -0700

    Update avatar icon

--- polaris-react/src/components/Avatar/Avatar.tsx
+++ polaris-react/src/components/Avatar/Avatar.tsx
@@ -2,6 +2,10 @@ import React, {useState, useCallback, useEffect} from 'react';
 
 import type {Experimental} from '../../types';
 import {classNames, variationName} from '../../utilities/css';
+import {
+  summerEditions2023ClassName,
+  useFeatures,
+} from '../../utilities/features';
 import {useI18n} from '../../utilities/i18n';
 import {useIsAfterInitialMount} from '../../utilities/use-is-after-initial-mount';
 import {Image} from '../Image';
@@ -24,6 +28,15 @@ enum Status {
 }
 
 export const STYLE_CLASSES = ['one', 'two', 'three', 'four', 'five'] as const;
+
+const avatarStrokeWidth: {[S in Size]: string} = {
+  extraSmall: '1.5',
+  small: '1.5',
+  medium: '2',
+  large: '2',
+  'xl-experimental': '2',
+  '2xl-experimental': '2',
+};
 
 /**
  * Computes a rudimentary hash from a string by xoring the character codes
@@ -84,6 +97,8 @@ export function Avatar({
   const isAfterInitialMount = useIsAfterInitialMount();
 
   const [status, setStatus] = useState<Status>(Status.Pending);
+
+  const {polarisSummerEditions2023} = useFeatures();
 
   // If the source changes, set the status back to pending
   useEffect(() => {
@@ -153,12 +168,33 @@ export function Avatar({
   // Use `dominant-baseline: central` instead of `dy` when Edge supports it.
   const verticalOffset = '0.35em';
 
+  const avatarPath = polarisSummerEditions2023 ? (
+    <>
+      <path
+        fill="none"
+        d="M25.5 13.5C25.5 16.5376 23.0376 19 20 19C16.9624 19 14.5 16.5376 14.5 13.5C14.5 10.4624 16.9624 8 20 8C23.0376 8 25.5 10.4624 25.5 13.5Z"
+        stroke="currentColor"
+        stroke-width={avatarStrokeWidth[size]}
+      />
+      <path
+        fill="none"
+        d="M10.3433 29.682L9.47 31.254C9.03481 32.0373 9.60125 33 10.4974 33H29.5026C30.3988 33 30.9652 32.0373 30.53 31.254L29.6567 29.682C27.7084 26.175 24.0119 24 20 24C15.9882 24 12.2916 26.175 10.3433 29.682Z"
+        stroke="currentColor"
+        stroke-width={avatarStrokeWidth[size]}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </>
+  ) : (
+    <path
+      fill="currentColor"
+      d="M8.28 27.5A14.95 14.95 0 0120 21.8c4.76 0 8.97 2.24 11.72 5.7a14.02 14.02 0 01-8.25 5.91 14.82 14.82 0 01-6.94 0 14.02 14.02 0 01-8.25-5.9zM13.99 12.78a6.02 6.02 0 1112.03 0 6.02 6.02 0 01-12.03 0z"
+    />
+  );
+
   const avatarBody =
     customer || !initials ? (
-      <path
-        fill="currentColor"
-        d="M8.28 27.5A14.95 14.95 0 0120 21.8c4.76 0 8.97 2.24 11.72 5.7a14.02 14.02 0 01-8.25 5.91 14.82 14.82 0 01-6.94 0 14.02 14.02 0 01-8.25-5.9zM13.99 12.78a6.02 6.02 0 1112.03 0 6.02 6.02 0 01-12.03 0z"
-      />
+      avatarPath
     ) : (
       <text
         className={textClassName}
