289528f65d71c244b15f2becc18f8a5c2578d4ec
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2023-07-06 15:33:30 -0700

    Add colored examples

--- polaris-react/src/components/ShadowBevel/ShadowBevel.stories.tsx
+++ polaris-react/src/components/ShadowBevel/ShadowBevel.stories.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import type {ComponentMeta} from '@storybook/react';
+import type {BoxProps} from '@shopify/polaris';
 import {Box, VerticalStack, InlineCode} from '@shopify/polaris';
+
+import {useFeatures} from '../../utilities/features';
 
 import {ShadowBevel} from './ShadowBevel';
 
@@ -9,6 +12,27 @@ export default {
 } as ComponentMeta<typeof ShadowBevel>;
 
 export function Default() {
+  const {polarisSummerEditions2023} = useFeatures();
+
+  const colors: BoxProps[] = [
+    {
+      background: 'bg-success-strong',
+      color: 'text-on-color',
+    },
+    {
+      background: 'bg-info-strong',
+      color: 'text-info-strong',
+    },
+    {
+      background: 'bg-warning-strong-experimental',
+      color: 'text-warning-strong',
+    },
+    {
+      background: 'bg-critical-strong',
+      color: 'text-on-color',
+    },
+  ];
+
   return (
     <VerticalStack gap="5">
       <ShadowBevel boxShadow="md" borderRadius="3">
@@ -49,6 +73,21 @@ export function Default() {
           With <InlineCode>as: article</InlineCode>
         </Box>
       </ShadowBevel>
+
+      {polarisSummerEditions2023 &&
+        colors.map(({background, color}) => (
+          <ShadowBevel
+            key={`${background}-${color}`}
+            boxShadow="md"
+            borderRadius="3"
+          >
+            <Box background={background} color={color} padding="4">
+              {background}
+            </Box>
+          </ShadowBevel>
+        ))}
+
+      <br />
     </VerticalStack>
   );
 }
