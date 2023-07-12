3656c91340acc90493d584231100e5ed53aa87c4
Jess Telford <jess.telford@shopify.com>
2023-07-12 11:59:04 +1000

    [Polaris Sandbox] Support passing feature=X URL params to <AppProvider>

--- polaris.shopify.com/playroom/FrameComponent.tsx
+++ polaris.shopify.com/playroom/FrameComponent.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {AppProvider} from '@shopify/polaris';
 import '@shopify/polaris/build/esm/styles.css';
 import enTranslations from '@shopify/polaris/locales/en.json';
@@ -10,11 +10,25 @@ export default function FrameComponent({
   theme: any;
   children: React.ReactNode;
 }) {
+  const [features, setFeatures] =
+    useState<React.ComponentProps<typeof AppProvider>['features']>();
   useEffect(() => {
     updateGrowFrameHeight(`${document.body.scrollHeight}px`);
   });
+  useEffect(() => {
+    const urlFeatures = new URLSearchParams(window.location.search).get(
+      'features',
+    );
+    if (typeof urlFeatures === 'string') {
+      setFeatures(
+        Object.fromEntries(
+          urlFeatures.split(',').map((feature) => [feature.trim(), true]),
+        ),
+      );
+    }
+  }, []);
   return (
-    <AppProvider i18n={theme || enTranslations}>
+    <AppProvider i18n={theme || enTranslations} features={features}>
       <div id="polaris-sandbox-wrapper">{children}</div>
     </AppProvider>
   );
