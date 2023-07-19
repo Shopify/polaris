d0477410d90946916e9dadcaf5cef2461be1909d
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2023-07-19 15:39:28 -0700

    Rename `shadow-bevel` opt out class name (#9767)

--- polaris-react/src/utilities/features/context.ts
+++ polaris-react/src/utilities/features/context.ts
@@ -5,7 +5,7 @@ import type {FeaturesConfig} from './types';
 export const classNamePolarisSummerEditions2023 =
   'Polaris-Summer-Editions-2023';
 export const classNamePolarisSummerEditions2023ShadowBevelOptOut =
-  'Polaris-Summer-Editions-2023-Shadow-Bevel-Opt-Out';
+  'Polaris-SE23-Shadow-Bevel-Opt-Out';
 
 export const FeaturesContext = createContext<FeaturesConfig | undefined>(
   undefined,
