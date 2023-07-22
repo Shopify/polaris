506c696f7b66346698440587075f2b8dd1932e5b
Dominik Wilkowski <dominik.wilkowski@shopify.com>
2023-07-22 14:31:18 +1000

    Added new design language page

---
+++ polaris.shopify.com/pages/new-design-language/index.tsx
@@ -0,0 +1,51 @@
+import type {GetStaticProps} from 'next';
+import fs from 'fs';
+import path from 'path';
+import Longform from '../../src/components/Longform';
+import Markdown from '../../src/components/Markdown';
+import Page from '../../src/components/Page';
+import PageMeta from '../../src/components/PageMeta';
+import {parseMarkdown} from '../../src/utils/markdown.mjs';
+
+export interface ContentProps {
+  title: string;
+  description: string;
+  content: string;
+}
+
+const NewDesignLanguage = ({title, description, content}: ContentProps) => {
+  return (
+    <>
+      <PageMeta description={description} />
+      <Page isContentPage>
+        <Longform>
+          <h1>{title}</h1>
+          <p>{description}</p>
+          <Markdown>{content}</Markdown>
+        </Longform>
+      </Page>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps<ContentProps> = async () => {
+  const markdownPath = path.resolve(
+    process.cwd(),
+    `content/new-design-language/index.md`,
+  );
+  const markdown = fs.readFileSync(markdownPath, 'utf-8');
+  const {
+    frontMatter: {title, description},
+    readme: content,
+  }: MarkdownFile = parseMarkdown(markdown);
+
+  return {
+    props: {
+      title,
+      description,
+      content,
+    },
+  };
+};
+
+export default NewDesignLanguage;
