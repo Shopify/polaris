5961dd20b50f4baf409c0c045e91a87165ebd2bc
Dominik Wilkowski <dominik.wilkowski@shopify.com>
2023-07-23 08:35:59 +1000

    disabled content again

--- polaris.shopify.com/pages/new-design-language/index.tsx
+++ polaris.shopify.com/pages/new-design-language/index.tsx
@@ -20,9 +20,9 @@ const NewDesignLanguage = ({/*title, description, content*/}: ContentProps) => {
       <PageMeta description={/*description*/''} />
       <Page isContentPage>
         <Longform>
-          <h1>{title}</h1>
+          {/*<h1>{title}</h1>
           <p>{description}</p>
-          <Markdown>{content}</Markdown>
+          <Markdown>{content}</Markdown>*/}
         </Longform>
       </Page>
     </>
