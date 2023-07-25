Shopify/polaris

commit f4bf5072adc7851fcd13ca01b572331e3f517a11

[Scrollable] shadow overlays scrollable content correctly.

polaris-react/src/components/Scrollable/Scrollable.scss
@@ -1,12 +1,14 @@
 .Scrollable {
   // stylelint-disable -- Polaris component custom properties
   --pc-scrollable-shadow-size: var(--p-space-5);
+  --pc-scrollable-shadow-color: rgba(0, 0, 0, 0.15);
   --pc-scrollable-shadow-bottom: inset 0
     calc(-1 * var(--pc-scrollable-shadow-size)) var(--pc-scrollable-shadow-size)
-    calc(-1 * var(--pc-scrollable-shadow-size)) rgba(0, 0, 0, 0.15);
+    calc(-1 * var(--pc-scrollable-shadow-size))
+    var(--pc-scrollable-shadow-color);
   --pc-scrollable-shadow-top: inset 0 var(--pc-scrollable-shadow-size)
     var(--pc-scrollable-shadow-size) calc(-1 * var(--pc-scrollable-shadow-size))
-    rgba(0, 0, 0, 0.15);
+    var(--pc-scrollable-shadow-color);
   --pc-scrollable-max-height: none;
   // stylelint-enable
   -webkit-overflow-scrolling: touch;
@@ -21,6 +23,62 @@
       var(--p-color-border-interactive-focus);
     outline-offset: var(--p-space-05);
   }
+
+  &.hasTopShadow,
+  &.hasBottomShadow {
+    /* Why not combine the box shadows onto one element? Because then it would
+     * need to be height: 100%, but that doesn't work when there's no explicit
+     * height set on the parent element (eg; for the Modal component which uses
+     * min-height: 80vh instead). */
+    &::before,
+    &::after {
+      content: '';
+      pointer-events: none;
+
+      /* Using CSS sticky to position the pseudo elements relative to the scroll
+       * container insead of relative to the contents */
+      position: sticky;
+
+      /* left/float/margin-right ensures it does not take up any static space,
+       * but still abides by position: sticky rules (ie; sorta position: fixed,
+       * but relative to the scroll container). */
+      left: 0;
+      float: left;
+      // stylelint-disable-next-line -- Can't use a token value here
+      margin-right: -100%;
+
+      /* Full width, but only as high as the shadow needs to be */
+      width: 100%;
+      // stylelint-disable-next-line -- Using custom variable here for the height
+      height: var(--pc-scrollable-shadow-size);
+
+      /* Arbitrarily picked this because it sorta sits above other elements most
+      * of the time without obscuring modals that might be above, etc.
+      * See https://github.com/Shopify/polaris/pull/9668/files#diff-b4fd34abf294f8d6bdcf784136be2ac82883085b8b0f91e450b2210eb6caaef9R70 */
+      // stylelint-disable-next-line -- See above
+      z-index: 32;
+    }
+
+    &::before {
+      // First element ticks to the top
+      top: 0;
+    }
+
+    &::after {
+      // Last element sticks to the bottom
+      bottom: 0;
+    }
+  }
+
+  &.hasTopShadow::before {
+    // stylelint-disable-next-line -- generated by polaris-migrator DO NOT COPY
+    box-shadow: var(--pc-scrollable-shadow-top);
+  }
+
+  &.hasBottomShadow::after {
+    // stylelint-disable-next-line -- generated by polaris-migrator DO NOT COPY
+    box-shadow: var(--pc-scrollable-shadow-bottom);
+  }
 }
 
 .horizontal {
@@ -29,21 +87,4 @@
 
 .vertical {
   overflow-y: auto;
-}
-
-.hasTopShadow {
-  // stylelint-disable-next-line -- generated by polaris-migrator DO NOT COPY
-  box-shadow: var(--pc-scrollable-shadow-top);
-}
-
-.hasBottomShadow {
-  // stylelint-disable-next-line -- generated by polaris-migrator DO NOT COPY
-  box-shadow: var(--pc-scrollable-shadow-bottom);
-}
-
-.hasTopShadow.hasBottomShadow {
-  // stylelint-disable -- generated by polaris-migrator DO NOT COPY
-  box-shadow: var(--pc-scrollable-shadow-top),
-    var(--pc-scrollable-shadow-bottom);
-  // stylelint-enable
 }

polaris-react/src/components/Scrollable/Scrollable.stories.tsx
@@ -1,6 +1,16 @@
 import React, {ComponentRef, useRef} from 'react';
 import type {ComponentMeta} from '@storybook/react';
-import {Button, LegacyCard, Scrollable} from '@shopify/polaris';
+import {
+  Button,
+  LegacyCard,
+  Scrollable,
+  Banner,
+  Box,
+  Text,
+  VerticalStack,
+  FormLayout,
+  TextField,
+} from '@shopify/polaris';
 import type {ScrollableRef} from '@shopify/polaris';
 
 export default {
@@ -1155,5 +1165,134 @@ export function UsingInstantScrollToFromRef() {
         <Button onClick={handleOnClick}>Scroll to top</Button>
       </Scrollable>
     </LegacyCard>
+  );
+}
+
+export function WithShadowOverComplexChildren() {
+  return (
+    <VerticalStack gap="4">
+      <Text as="p" variant="bodyLg" fontWeight="bold">
+        NOTE: Red shadow is for demo/debug purposes only.
+        <br />
+        DO NOT do this in production.
+      </Text>
+      <Scrollable
+        shadow
+        style={{
+          height: '200px',
+          maxWidth: '40rem',
+          backgroundColor: 'var(--p-color-bg)',
+          // Setting red here so the storybook is really obvious that the shadow
+          // overlays the content (particularly the banner)
+          /* @ts-expect-error TS doesn't understand CSS vars */
+          '--pc-scrollable-shadow-color': 'rgba(255, 0, 0, 0.85)',
+        }}
+      >
+        <Box padding="4">
+          <Banner title="Payment details">
+            <p>Last updated on: September 6, 2022</p>
+
+            <p>
+              Welcome to Shopify! By signing up for a Shopify Account (as
+              defined in Section 1) or by using any Shopify Services (as defined
+              below), you are agreeing to be bound by the following terms and
+              conditions (the “<strong>Terms of Service</strong>”).
+            </p>
+
+            <p>
+              As used in these Terms of Service, “<strong>we</strong>”, “
+              <strong>us</strong>”, “<strong>our</strong>” and “
+              <strong>Shopify</strong>” means the applicable Shopify Contracting
+              Party (as defined in Section 13 below), and “<strong>you</strong>”
+              means the Shopify User (if registering for or using a Shopify
+              Service as an individual), or the business employing the Shopify
+              User (if registering for or using a Shopify Service as a business)
+              and any of its affiliates.
+            </p>
+          </Banner>
+
+          <FormLayout>
+            <FormLayout.Group>
+              <TextField label="First name" autoComplete="off" />
+              <TextField label="Last name" autoComplete="off" />
+            </FormLayout.Group>
+
+            <FormLayout.Group>
+              <TextField label="Address" autoComplete="off" />
+              <TextField label="Apartment, suite, etc." autoComplete="off" />
+            </FormLayout.Group>
+
+            <FormLayout.Group>
+              <TextField label="City" autoComplete="off" />
+              <TextField label="Province" autoComplete="off" />
+              <TextField label="Postal code" autoComplete="off" />
+            </FormLayout.Group>
+          </FormLayout>
+
+          <p>
+            Shopify provides a complete commerce platform that enables merchants
+            to unify their commerce activities. Among other features, this
+            platform includes a range of tools for merchants to build and
+            customize online stores, sell in multiple places (including web,
+            mobile, social media, online marketplaces and other online locations
+            (“<strong>Online Services</strong>”) and in person (“
+            <strong>POS Services</strong>”)), manage products, inventory,
+            payments, fulfillment, shipping, business operations, marketing and
+            advertising, and engage with existing and potential customers. Any
+            such service or services offered by Shopify are referred to in these
+            Terms of Services as the “<strong>Service(s)</strong>”. Any new
+            features or tools which are added to the current Services will also
+            be subject to the Terms of Service. You can review the current
+            version of the Terms of Service at any time at
+            <a href="https://www.shopify.com/legal/terms">
+              https://www.shopify.com/legal/terms
+            </a>
+            .
+          </p>
+          <p>
+            You must read, agree with and accept all of the terms and conditions
+            contained or expressly referenced in these Terms of Service,
+            including Shopify’s
+            <a href="https://www.shopify.com/legal/aup">
+              Acceptable Use Policy
+            </a>
+            (“<strong>AUP</strong>”) and
+            <a href="https://www.shopify.com/legal/privacy">Privacy Policy</a>,
+            and, if applicable, the
+            <a href="https://www.shopify.com/legal/eu-terms">
+              Supplementary Terms of Service for E.U. Merchants
+            </a>
+            (“<strong>EU Terms</strong>”), the Shopify
+            <a href="https://www.shopify.com/legal/api-terms">
+              API License and Terms of Use
+            </a>
+            (“<strong>API Terms</strong>”) and the Shopify
+            <a href="https://www.shopify.com/legal/dpa">
+              Data Processing Addendum
+            </a>
+            (“<strong>DPA</strong>”) before you may sign up for a Shopify
+            Account or use any Shopify Service. Additionally, if you offer goods
+            or services in relation to COVID-19, you must read, acknowledge and
+            agree to the
+            <a href="/legal/rules-of-engagement-covid19">
+              Rules of Engagement for Sale of COVID-19 Related Products
+            </a>
+            .
+          </p>
+
+          <p>
+            <strong>
+              Everyday language summaries are provided for convenience only and
+              appear in bold near each section, but these summaries are not
+              legally binding. Please read the Terms of Service, including any
+              document referred to in these Terms of Service, for the complete
+              picture of your legal requirements. By using Shopify or any
+              Shopify services, you are agreeing to these terms. Be sure to
+              occasionally check back for updates.
+            </strong>
+          </p>
+        </Box>
+      </Scrollable>
+    </VerticalStack>
   );
 }

commit 74b768225ccd9d0e51e9351d117e539088179833

[Scrollable][Filters] Remove unwanted height in Scrollable Filters (#9773)
Fixes a bug introduced in https://github.com/Shopify/polaris/pull/9752

- Before https://github.com/Shopify/polaris/pull/9752:
-
![scrollable-bug-before](https://github.com/Shopify/polaris/assets/612020/222d975f-f37c-4d44-9876-caff262fbff9)
-
[Storybook](https://5d559397bae39100201eedc1-rxdryckmpi.chromatic.com/?path=/story/all-components-filters--with-a-resource-list&globals=polarisSummerEditions2023:true)
- After https://github.com/Shopify/polaris/pull/9752:
-
![scrollable-bug-after](https://github.com/Shopify/polaris/assets/612020/4af71673-d5ed-46e8-bdcb-83e46553ae5f)
-
[Storybook](https://5d559397bae39100201eedc1-evnicxtjes.chromatic.com/?path=/story/all-components-filters--with-a-resource-list&globals=polarisSummerEditions2023:true)
- This PR:
-
![scrollable-bug-fixed](https://github.com/Shopify/polaris/assets/612020/69ed976d-b4cb-42aa-aef5-5e1aadad81aa)
-
[Storybook](https://5d559397bae39100201eedc1-fpskayihlk.chromatic.com/?path=/story/all-components-filters--with-a-resource-list&globals=polarisSummerEditions2023:true)

---------

Co-authored-by: Kyle Durand <kyledurand@users.noreply.github.com>

polaris-react/src/components/Popover/Popover.scss
@@ -60,6 +60,22 @@ $vertical-motion-offset: -5px;
   }
 }
 
+.ContentContainer {
+  position: relative;
+  overflow: hidden;
+  background: var(--p-color-bg);
+  border-radius: var(--p-space-2);
+
+  /* Prevent Scrollable's box shadows overflowing the rounded corners of this
+   * element on Safari prior to tech preview version 156.
+   * See: https://bugs.webkit.org/show_bug.cgi?id=68196 */
+  isolation: isolate;
+
+  #{$se23} & {
+    border-radius: var(--p-space-3);
+  }
+}
+
 .Content {
   position: relative;
   display: flex;

polaris-react/src/components/Popover/components/PopoverOverlay/PopoverOverlay.tsx
@@ -19,8 +19,6 @@ import type {PaneProps} from '../Pane';
 import styles from '../../Popover.scss';
 import {PortalsManagerContext} from '../../../../utilities/portals';
 import type {PortalsContainerElement} from '../../../../utilities/portals';
-import {Box} from '../../../Box';
-import {UseFeatures} from '../../../../utilities/features';
 
 export enum PopoverCloseSource {
   Click,
@@ -232,18 +230,6 @@ export class PopoverOverlay extends PureComponent<PopoverOverlayProps, State> {
       fluidContent && styles['Content-fluidContent'],
     );
 
-    const content = (
-      <div
-        id={id}
-        tabIndex={autofocusTarget === 'none' ? undefined : -1}
-        className={contentClassNames}
-        style={contentStyles}
-        ref={this.contentNode}
-      >
-        {renderPopoverContent(children, {captureOverscroll, sectioned})}
-      </div>
-    );
-
     return (
       <div className={className} {...overlay.props}>
         <EventListener event="click" handler={this.handleClick} />
@@ -255,19 +241,17 @@ export class PopoverOverlay extends PureComponent<PopoverOverlayProps, State> {
           tabIndex={0}
           onFocus={this.handleFocusFirstItem}
         />
-        <UseFeatures>
-          {(features) => (
-            <Box
-              position="relative"
-              overflowX="hidden"
-              overflowY="hidden"
-              background="bg"
-              borderRadius={features.polarisSummerEditions2023 ? '3' : '2'}
-            >
-              {content}
-            </Box>
-          )}
-        </UseFeatures>
+        <div className={styles.ContentContainer}>
+          <div
+            id={id}
+            tabIndex={autofocusTarget === 'none' ? undefined : -1}
+            className={contentClassNames}
+            style={contentStyles}
+            ref={this.contentNode}
+          >
+            {renderPopoverContent(children, {captureOverscroll, sectioned})}
+          </div>
+        </div>
         <div
           className={styles.FocusTracker}
           // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex

polaris-react/src/components/Scrollable/Scrollable.scss
@@ -2,12 +2,11 @@
   // stylelint-disable -- Polaris component custom properties
   --pc-scrollable-shadow-size: var(--p-space-5);
   --pc-scrollable-shadow-color: rgba(0, 0, 0, 0.15);
-  --pc-scrollable-shadow-bottom: inset 0
-    calc(-1 * var(--pc-scrollable-shadow-size)) var(--pc-scrollable-shadow-size)
-    calc(-1 * var(--pc-scrollable-shadow-size))
+  --pc-scrollable-shadow-bottom: 0 var(--pc-scrollable-shadow-size)
+    var(--pc-scrollable-shadow-size) var(--pc-scrollable-shadow-size)
     var(--pc-scrollable-shadow-color);
-  --pc-scrollable-shadow-top: inset 0 var(--pc-scrollable-shadow-size)
-    var(--pc-scrollable-shadow-size) calc(-1 * var(--pc-scrollable-shadow-size))
+  --pc-scrollable-shadow-top: 0 calc(-1 * var(--pc-scrollable-shadow-size))
+    var(--pc-scrollable-shadow-size) var(--pc-scrollable-shadow-size)
     var(--pc-scrollable-shadow-color);
   --pc-scrollable-max-height: none;
   // stylelint-enable
@@ -17,6 +16,11 @@
   max-height: var(--pc-scrollable-max-height);
   overflow-x: hidden;
   overflow-y: hidden;
+
+  /* Prevent Scrollable's box shadows overflowing the rounded corners of this
+   * element on Safari prior to tech preview version 156.
+   * See: https://bugs.webkit.org/show_bug.cgi?id=68196 */
+  isolation: isolate;
 
   &:focus {
     outline: var(--p-border-width-2) solid
@@ -33,24 +37,26 @@
     &::before,
     &::after {
       content: '';
-      pointer-events: none;
 
       /* Using CSS sticky to position the pseudo elements relative to the scroll
        * container insead of relative to the contents */
       position: sticky;
 
-      /* left/float/margin-right ensures it does not take up any static space,
-       * but still abides by position: sticky rules (ie; sorta position: fixed,
-       * but relative to the scroll container). */
       left: 0;
-      float: left;
-      // stylelint-disable-next-line -- Can't use a token value here
-      margin-right: -100%;
 
-      /* Full width, but only as high as the shadow needs to be */
+      // Ensure we can give it an explicit height of 0 later
+      display: block;
+
+      // No one likes clicking on shadows!
+      pointer-events: none;
+
+      /* height: 0 ensures it does not take up any static space, but still
+       * abides by position: sticky rules (ie; sorta position: fixed, but
+       * relative to the scroll container). */
+      height: var(--p-space-0);
+
+      /* Full width to cover the entire scrollable container */
       width: 100%;
-      // stylelint-disable-next-line -- Using custom variable here for the height
-      height: var(--pc-scrollable-shadow-size);
 
       /* Arbitrarily picked this because it sorta sits above other elements most
       * of the time without obscuring modals that might be above, etc.
@@ -60,12 +66,12 @@
     }
 
     &::before {
-      // First element ticks to the top
+      // First element ticks to the top left
       top: 0;
     }
 
     &::after {
-      // Last element sticks to the bottom
+      // Last element sticks to the bottom left
       bottom: 0;
     }
   }