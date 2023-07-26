c5878f0928ade667c69b130a07afe3512edf5e5a
Natacha Levesque <natacha.levesque@shopify.com>
2023-07-26 20:24:59 +0000

    rough color picker

--- polaris-react/src/components/ColorPicker/ColorPicker.stories.tsx
+++ polaris-react/src/components/ColorPicker/ColorPicker.stories.tsx
@@ -44,63 +44,44 @@ export function WithTransparentValueFullWidth() {
   return <ColorPicker fullWidth onChange={setColor} color={color} allowAlpha />;
 }
 
-export function WithHexTextField() {
+export function WithTextFields() {
   const [color, setColor] = useState({
-    hue: 300,
-    saturation: 0.7,
+    hue: 120,
     brightness: 1,
+    saturation: 1,
   });
 
-  const [hex, setHex] = useState(hsbToHex(color));
+  return <ColorPicker onChange={setColor} color={color} textEditor />;
+}
 
-  const handleBlur = useCallback(() => {
-    setColor(rgbToHsb(hexToRgb(hex)));
-  }, [hex]);
-
-  useEffect(() => {
-    setHex(hsbToHex(color));
-  }, [color]);
-
-  const styles = {
-    Wrapper: {
-      display: 'grid',
-      gap: 'var(--p-space-2)',
-      gridTemplateColumns: 'auto 1fr',
-      maxWidth: 'fit-content',
-    },
-    Picker: {
-      gridColumn: '1 / 3',
-    },
-    Tile: {
-      minHeight: 'calc(100% - 0.125rem)',
-      aspectRatio: '1 / 1',
-      borderRadius: 'var(--p-border-radius-1)',
-      border: 'var(--p-border-width-1) solid var(--p-color-border-subdued)',
-      backgroundColor: hsbToHex(color),
-    },
-    TextField: {
-      maxWidth: 'fit-content',
-    },
-  };
+export function WithTransparentValueAndTextFields() {
+  const [color, setColor] = useState({
+    hue: 300,
+    brightness: 1,
+    saturation: 0.7,
+    alpha: 0.7,
+  });
 
   return (
-    <div style={styles.Wrapper}>
-      <div style={styles.Picker}>
-        <ColorPicker onChange={setColor} color={color} />
-      </div>
+    <ColorPicker onChange={setColor} color={color} allowAlpha textEditor />
+  );
+}
 
-      <div style={styles.Tile} />
+export function WithTransparentValueAndTextFieldsFullWidth() {
+  const [color, setColor] = useState({
+    hue: 300,
+    brightness: 1,
+    saturation: 0.7,
+    alpha: 0.7,
+  });
 
-      <div style={styles.TextField}>
-        <TextField
-          label=""
-          prefix="#"
-          value={hex.replace('#', '')}
-          onChange={setHex}
-          onBlur={handleBlur}
-          autoComplete="off"
-        />
-      </div>
-    </div>
+  return (
+    <ColorPicker
+      onChange={setColor}
+      color={color}
+      allowAlpha
+      fullWidth
+      textEditor
+    />
   );
 }
