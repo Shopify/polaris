0fc1cce54c49b68ed6188d8ee75d5049837accda
Lo Kim <lo.kim@shopify.com>
2023-08-11 10:49:59 -0400

    [Breadcrumbs] Consolidate se23 logic and styles (#10030)
    ### WHY are these changes introduced?
    
    Resolves #9920.
    
    ### WHAT is this pull request doing?
    
    Consolidates se23 beta logic styles for `Breadcrumbs`.
    
    ### How to 🎩
    
    
    [Storybook](https://5d559397bae39100201eedc1-eghgntjqfq.chromatic.com/?path=/story/all-components-page--with-back-action-on-action)
    [Prod
    Storybook](https://storybook.polaris.shopify.com/?path=/story/all-components-page--with-back-action-on-action&globals=polarisSummerEditions2023:true)
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    ### 🎩 checklist
    
    - [x] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [x] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [ ] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [ ] Updated the component's `README.md` with documentation changes
    - [ ] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris-react/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ polaris-react/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import {ArrowLeftMinor} from '@shopify/polaris-icons';
 
-import {Icon} from '../Icon';
-import {UnstyledLink} from '../UnstyledLink';
 import type {CallbackAction, LinkAction} from '../../types';
 import {handleMouseUpByBlurring} from '../../utilities/focus';
-import {Text} from '../Text';
 import {Button} from '../Button';
-import {useFeatures} from '../../utilities/features';
-
-import styles from './Breadcrumbs.scss';
 
 export interface BreadcrumbsProps {
   /** Back action link */
@@ -18,44 +12,8 @@ export interface BreadcrumbsProps {
 
 export function Breadcrumbs({backAction}: BreadcrumbsProps) {
   const {content} = backAction;
-  const {polarisSummerEditions2023} = useFeatures();
 
-  const contentMarkup = (
-    <>
-      <span className={styles.Icon}>
-        <Icon source={ArrowLeftMinor} />
-      </span>
-      <Text as="span" visuallyHidden>
-        {content}
-      </Text>
-    </>
-  );
-
-  const breadcrumbMarkup =
-    'url' in backAction ? (
-      <UnstyledLink
-        key={content}
-        url={backAction.url}
-        className={styles.Breadcrumb}
-        onMouseUp={handleMouseUpByBlurring}
-        aria-label={backAction.accessibilityLabel}
-      >
-        {contentMarkup}
-      </UnstyledLink>
-    ) : (
-      <button
-        key={content}
-        className={styles.Breadcrumb}
-        onClick={backAction.onAction}
-        onMouseUp={handleMouseUpByBlurring}
-        type="button"
-        aria-label={backAction.accessibilityLabel}
-      >
-        {contentMarkup}
-      </button>
-    );
-
-  const summerEditionsBreadcrumbMarkup = (
+  const breadcrumbMarkup = (
     <Button
       key={content}
       url={'url' in backAction ? backAction.url : undefined}
@@ -66,11 +24,5 @@ export function Breadcrumbs({backAction}: BreadcrumbsProps) {
     />
   );
 
-  return (
-    <nav role="navigation">
-      {polarisSummerEditions2023
-        ? summerEditionsBreadcrumbMarkup
-        : breadcrumbMarkup}
-    </nav>
-  );
+  return <nav role="navigation">{breadcrumbMarkup}</nav>;
 }
