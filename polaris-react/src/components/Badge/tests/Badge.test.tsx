ddcb8db243e72870630164c4d06979028d2e6615
Sophie Schneider <sophie.schneider@shopify.com>
2023-08-11 14:38:54 -0400

    [Badge] Consolidate se23 logic and styles (#10004)
    ### WHY are these changes introduced?
    
    Fixes https://github.com/Shopify/polaris/issues/9918
    
    ### WHAT is this pull request doing?
    
    Removing flagged se23 code from badge
    
    ### How to 🎩
    
    Compare and make sure they are the same:
    
    * This PR's
    [Storybook](https://5d559397bae39100201eedc1-fqebvmxjcw.chromatic.com/?path=/story/all-components-badge--all&globals=polarisSummerEditions2023:false)
    * [Production
    Storybook](https://storybook.polaris.shopify.com/?path=/story/all-components-badge--all&globals=polarisSummerEditions2023:true)

--- polaris-react/src/components/Badge/tests/Badge.test.tsx
+++ polaris-react/src/components/Badge/tests/Badge.test.tsx
@@ -34,16 +34,12 @@ describe('<Badge />', () => {
   });
 
   it('does not add pip styles when progress is not provided', () => {
-    const badge = mountWithApp(<Badge status="attention" />, {
-      features: {polarisSummerEditions2023: true},
-    });
+    const badge = mountWithApp(<Badge status="attention" />);
     expect(badge).not.toContainReactComponent(Icon);
   });
 
   it('renders with pip styles when progress is provided', () => {
-    const badge = mountWithApp(<Badge progress="incomplete" />, {
-      features: {polarisSummerEditions2023: true},
-    });
+    const badge = mountWithApp(<Badge progress="incomplete" />);
 
     expect(badge).toContainReactComponent(Icon);
   });
@@ -169,29 +165,6 @@ describe('<Badge.Pip />', () => {
     expect(badge).toContainReactComponent(Text, {
       children: 'Complete',
       visuallyHidden: true,
-    });
-  });
-
-  // se23 -- css pip replaced with icon pip
-  describe('polarisSummerEditions2023 false', () => {
-    it('does not add pip styles when progress is not provided', () => {
-      const badge = mountWithApp(<Badge status="attention" />, {
-        features: {polarisSummerEditions2023: false},
-      });
-
-      expect(badge).not.toContainReactComponent('span', {
-        className: 'Pip',
-      });
-    });
-
-    it('renders with pip styles when progress is provided', () => {
-      const badge = mountWithApp(<Badge progress="incomplete" />, {
-        features: {polarisSummerEditions2023: false},
-      });
-
-      expect(badge).toContainReactComponent('span', {
-        className: 'Pip progressIncomplete',
-      });
     });
   });
 });
