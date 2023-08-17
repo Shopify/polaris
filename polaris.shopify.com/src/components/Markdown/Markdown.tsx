b4b8a2dca001def58936ad0433f06707adc0d788
Jess Telford <jess.telford@shopify.com>
2023-08-17 16:50:13 +1000

    [Docs] Consolidate different markdown rendering into single component

--- polaris.shopify.com/src/components/Markdown/Markdown.tsx
+++ polaris.shopify.com/src/components/Markdown/Markdown.tsx
@@ -1,14 +1,17 @@
-import React, {type ComponentProps} from 'react';
+import React, {cloneElement, useState, type ComponentProps} from 'react';
 import {MDXRemote} from 'next-mdx-remote';
 
 import styles from './Markdown.module.scss';
-import Code from '../Code';
+import Code, {InlineCode} from '../../components/Code';
+import {Box} from '../../components/Box';
+import {Stack} from '../../components/Stack';
 import StatusBanner from '../../components/StatusBanner';
 import {Lede} from '../../components/Lede';
 import UpdateBanner from '../../components/UpdateBanner';
 import {SideBySide} from './components/SideBySide';
 import {DoDont} from './components/DoDont';
 import {Heading} from '../../components/Heading';
+import PatternsExample from '../../components/PatternsExample';
 
 function Markdown(props: ComponentProps<typeof MDXRemote>) {
   return (
@@ -16,58 +19,144 @@ function Markdown(props: ComponentProps<typeof MDXRemote>) {
       {...props}
       components={{
         h1: ({id, children}) => (
-          <Heading as="h1" id={id}>
+          <Heading as="h1" id={id} className={styles.Heading1}>
             {children}
           </Heading>
         ),
         h2: ({id, children}) => (
-          <Heading as="h2" id={id}>
+          <Heading as="h2" id={id} className={styles.Heading2}>
             {children}
           </Heading>
         ),
         h3: ({id, children}) => (
-          <Heading as="h3" id={id}>
+          <Heading as="h3" id={id} className={styles.Heading3}>
             {children}
           </Heading>
         ),
         h4: ({id, children}) => (
-          <Heading as="h4" id={id}>
+          <Heading as="h4" id={id} className={styles.Heading4}>
             {children}
           </Heading>
         ),
         h5: ({id, children}) => (
-          <Heading as="h5" id={id}>
+          <Heading as="h5" id={id} className={styles.Heading5}>
             {children}
           </Heading>
         ),
-        h5: ({id, children}) => (
-          <Heading as="h5" id={id}>
+        h6: ({id, children}) => (
+          <Heading as="h6" id={id} className={styles.Heading5}>
             {children}
           </Heading>
         ),
-        img: ({src, alt}) =>
+        ol: ({children}) => (
+          <Stack as="ol" gap="2" className={[styles.List, styles.OrderedList]}>
+            {children}
+          </Stack>
+        ),
+        ul: ({children}) => (
+          <Stack
+            as="ul"
+            className={[styles.List, styles.UnorderedList]}
+            gap="2"
+          >
+            {children}
+          </Stack>
+        ),
+        li: (props) => <Box as="li" className={styles.ListItem} {...props} />,
+        p: (props) => <Box as="p" {...props} className={styles.Paragraph} />,
+        hr: (props) => (
+          <Box as="hr" {...props} className={styles.HorizontalRule} />
+        ),
+        strong: (props) => (
+          <Box as="strong" {...props} className={styles.Strong} />
+        ),
+        img: ({src, alt, style}) =>
           src ? (
             // eslint-disable-next-line @next/next/no-img-element
             <img
               src={src}
               alt={alt ?? ''}
-              className={styles.MarkdownImage}
+              className={styles.Image}
               style={style}
             />
           ) : null,
-        pre: ({children, className}) => (
-          <Code
-            code={{
-              className,
-              title: 'Example',
-              code: children?.toString() ?? '',
-            }}
-          />
-        ),
-        table: ({children}) => (
-          <div className="table-wrapper">
-            <table>{children}</table>
-          </div>
+        // `pre` is only for fenced code blocks, so we set the flag here which
+        // is then passed to `code`
+        pre: ({children}) =>
+          cloneElement(
+            React.Children.only(children) as React.ReactElement<any>,
+            {fenced: true},
+          ),
+        // `code` could be an inline code element, or a fanced code block coming
+        // from `pre`
+        code: function MDXCode({
+          children,
+          className,
+          // @ts-expect-error Unsure how to tell react-markdown this prop is
+          // being injected by a plugin
+          fenced,
+          // @ts-expect-error Unsure how to tell react-markdown this prop is
+          // being injected by a plugin
+          meta,
+        }) {
+          let type,
+            previewContext,
+            sandboxContext,
+            title,
+            isCodeVisible,
+            isActionsVisible;
+
+          try {
+            ({
+              title,
+              type,
+              previewContext,
+              sandboxContext,
+              isCodeVisible,
+              isActionsVisible,
+            } = JSON.parse(meta ?? '{}'));
+          } catch (error) {
+            console.warn(`code block meta is not parsable JSON: ${meta}`);
+          }
+
+          const [showCode, setShowCode] = useState<boolean>(isCodeVisible);
+
+          if (!fenced) {
+            return (
+              <InlineCode className={styles.InlineCode}>{children}</InlineCode>
+            );
+          }
+
+          if (type === 'livePreview') {
+            return (
+              <PatternsExample
+                example={{
+                  code: (children as string) ?? '',
+                  previewContext,
+                  sandboxContext,
+                }}
+                isCodeVisible={showCode}
+                isActionsVisible={isActionsVisible}
+                onCodeVisibilityToggle={() => setShowCode(!showCode)}
+                patternName={'Pattern Name'}
+              />
+            );
+          }
+
+          return (
+            <Code
+              code={{
+                className,
+                title: title ?? 'Example',
+                code: (children as string)?.toString() ?? '',
+              }}
+            />
+          );
+        },
+        table: (props) => (
+          <Box className={styles.TableWrapper}>
+            <Box as="table" {...props} className={styles.Table} />
+          </Box>
         ),
         SideBySide,
         DoDont,
--- polaris.shopify.com/src/components/Markdown/serialize.ts
+++ polaris.shopify.com/src/components/Markdown/serialize.ts
@@ -4,7 +4,7 @@ import remarkUnwrapImages from 'remark-unwrap-images';
 import {VFile, type Data} from 'vfile';
 import remarkGfm from 'remark-gfm';
 import remarkSlug from 'remark-slug';
-import {visit} from 'unist-util-visit';
+import {visit, type Node, type Parent} from 'unist-util-visit';
 import type {Plugin} from 'unified';
 
 import remarkExtractFirstParagraph, {
@@ -18,9 +18,83 @@ export type SerializedMdx<
   TScope = DefaultScope,
 > = MDXRemoteSerializeResult<TScope, TFrontmatter>;
 
-// rehype-raw will strip the non-HTML-standard `meta` field from the node when
-// converting the parsed markdown to HTML, so we have to capture it in a
-// different property which wont be stripped out.
+function codeAsContext(): Plugin {
+  return (tree) => {
+    // Gather up all the code elements
+    const codes: {
+      node: Node;
+      parent: Parent;
+      index: number;
+      meta: Record<string, any>;
+    }[] = [];
+    visit(tree, 'code', (node, index, parent) => {
+      console.log('visiting code:', JSON.stringify(node, null, 2));
+      if (node.meta) {
+        try {
+          codes.push({
+            node,
+            index: index!,
+            parent,
+            meta: JSON.parse(node.meta),
+          });
+        } catch (error) {
+          // Just ignore this block
+        }
+      }
+    });
+
+    // Iterate over all the code elements, matching wrappers with ids
+    codes
+      // Ignore anything which doesn't self-identify as wrapping another
+      .filter(
+        ({meta}) =>
+          ['previewContext', 'sandboxContext'].includes(meta.type) && meta.for,
+      )
+      // sort descending so when we splice these nodes out of their parents, all
+      // following indexes are still valid
+      .sort((a, b) => b.index - a.index)
+      .forEach(({node, meta, index, parent}) => {
+        if (meta.for === meta.id) {
+          console.warn(
+            `Code block specifies { for: "${meta.for}", id: "${meta.id}" }, which would cause an infinite loop.`,
+          );
+          return;
+        }
+
+        const forCode = codes.find(
+          (otherNode) => otherNode.meta.id === meta.for,
+        );
+
+        if (!forCode) {
+          console.warn(
+            `Code block specifies { for: "${meta.for}" }, but could not find matching { id: "${meta.for}" }`,
+          );
+          return;
+        }
+
+        // @ts-expect-error Yes, it does exist Typescript. Shhhhh
+        forCode.meta[meta.type] = node.value;
+
+        // Delete this code block from the tree
+        parent.children.splice(index, 1);
+      });
+
+    // For all the code blocks who might now be wrapped, re-encode the modified
+    // meta and stick it back on the node
+    codes
+      .filter(({meta}) => meta.id)
+      .forEach(({node, meta}) => {
+        // @ts-expect-error Yes, it does exist Typescript. Shhhhh
+        node.meta = JSON.stringify(meta);
+      });
+  };
+}
+
+// next-mdx-remote#serialize will strip the non-HTML-standard `meta` field from
+// pre nodes (```) when  converting the parsed markdown to HTML, so we have to
+// capture it in a different property which wont be stripped out. The
+// `.data.hProperties` will be accessible in the `code` component renderer as
+// props.
 function codeMetaAsDataAttribute(): Plugin {
   return (tree) => {
     visit(tree, 'code', (node) => {
@@ -67,6 +141,7 @@ export const serializeMdx = async <
         remarkUnwrapImages,
         remarkSlug,
         ...(mdxOptions?.remarkPlugins ?? []),
+        codeAsContext,
         codeMetaAsDataAttribute,
         [
           remarkExtractFirstParagraph,
