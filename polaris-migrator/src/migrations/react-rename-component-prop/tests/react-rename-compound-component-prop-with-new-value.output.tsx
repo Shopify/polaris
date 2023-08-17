4e11e7a7d5b6b9beb770523491ae0b21129f2ea0
Kyle Durand <kyledurand@users.noreply.github.com>
2023-08-17 18:50:56 -0400

    Allow migrator to target subcomponents and change prop values (#10071)
    Closes https://github.com/Shopify/polaris/issues/10084
    
    First pass at allowing prop values to be changed through migrations. 
    Also needed to allow support for finding compound components in the
    rename prop migration
    
    **Question**
    Should this be merged into main? Or left in next until it's tested?

---
+++ polaris-migrator/src/migrations/react-rename-component-prop/tests/react-rename-compound-component-prop-with-new-value.output.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface MyComponentProps {
+  prop?: string;
+  newProp?: string;
+  children?: React.ReactNode;
+}
+
+const Child = (props: {prop: string}) => <>{props.prop}</>;
+
+function MyComponent(props: MyComponentProps) {
+  const value = props.newProp ?? props.prop;
+  return <div data-prop={value}>{props.children}</div>;
+}
+
+function SubComponent({...props}: any) {
+  return <div {...props} />;
+}
+
+export function App() {
+  return (
+    <MyComponent>
+      <MyComponent.SubComponent newProp="new value" />
+      Hello
+      <Child prop="value" />
+    </MyComponent>
+  );
+}
+
+MyComponent.SubComponent = SubComponent;
