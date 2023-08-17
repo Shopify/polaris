68994878f5b8245e439795e3f31f549db6927923
Jess Telford <jess.telford@shopify.com>
2023-08-17 16:41:37 +1000

    [Docs] Fix race conditions when resizing code example iframes

--- polaris.shopify.com/playroom/FrameComponent.tsx
+++ polaris.shopify.com/playroom/FrameComponent.tsx
@@ -1,8 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {AppProvider} from '@shopify/polaris';
 import '@shopify/polaris/build/esm/styles.css';
 import enTranslations from '@shopify/polaris/locales/en.json';
 import {updateGrowFrameHeight} from '../src/components/GrowFrame';
+import {ResizeObserver} from '@juggle/resize-observer';
+
 export default function FrameComponent({
   theme = enTranslations,
   children,
@@ -10,26 +12,37 @@ export default function FrameComponent({
   theme: any;
   children: React.ReactNode;
 }) {
-  const [features, setFeatures] =
-    useState<React.ComponentProps<typeof AppProvider>['features']>();
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+
+  // Tell parent frame of the initial size on first render
   useEffect(() => {
-    updateGrowFrameHeight(`${document.body.scrollHeight}px`);
-  });
-  useEffect(() => {
-    const urlFeatures = new URLSearchParams(window.location.search).get(
-      'features',
-    );
-    if (typeof urlFeatures === 'string') {
-      setFeatures(
-        Object.fromEntries(
-          urlFeatures.split(',').map((feature) => [feature.trim(), true]),
-        ),
-      );
+    if (!wrapperRef.current) {
+      return;
     }
+    var {height} = wrapperRef.current.getBoundingClientRect();
+    updateGrowFrameHeight(`${Math.ceil(height)}px`);
   }, []);
+
+  // Watch for changes in size (screen resizing, interacting, etc);
+  useEffect(() => {
+    if (!wrapperRef.current) {
+      return;
+    }
+    const observer = new ResizeObserver((entries) => {
+      const {blockSize: height} = entries[0].contentBoxSize[0];
+      updateGrowFrameHeight(`${Math.ceil(height)}px`);
+    });
+    observer.observe(wrapperRef.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <AppProvider i18n={theme || enTranslations} features={features}>
-      <div id="polaris-sandbox-wrapper">{children}</div>
+    <AppProvider i18n={theme || enTranslations}>
+      <div id="polaris-sandbox-wrapper" ref={wrapperRef}>
+        {children}
+      </div>
     </AppProvider>
   );
 }
