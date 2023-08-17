2d142bbf6708bdb965d9306b0fb3b901bffcc15f
Sam Rose <11774595+samrose3@users.noreply.github.com>
2023-08-17 14:39:30 -0400

    Remove `polaris-cli` and `polaris-codemods` packages (#10101)
    Use only the `@shopify/polaris-migrator` package.
    
    This also removes the documentation for the Polaris CLI on the [Tools
    page](https://polaris.shopify.com/tools/polaris-cli).
    
    Removing these packages will
    
    * **Improve our CI times** by removing extra packages from our build,
    test, and lint workflows
    * **Reduce confusion** on where to add new migrations or update previous
    migrations
    * **Clean up things** such as ESLint configs, changelog entries on
    Version Package PRs, and package dep bumps

---
+++ polaris-migrator/codeshift.config.js
@@ -0,0 +1,65 @@
+function resolve(transform) {
+  return require.resolve(`./dist/cjs/migrations/${transform}/transform`);
+}
+
+module.exports = {
+  presets: {
+    'react-rename-component': resolve('react-rename-component'),
+    'react-rename-component-prop': resolve('react-rename-component-prop'),
+    'scss-remove-unused-at-use': resolve('scss-remove-unused-at-use'),
+    'styles-insert-stylelint-disable': resolve(
+      'styles-insert-stylelint-disable',
+    ),
+    'styles-replace-custom-property': resolve('styles-replace-custom-property'),
+    'v11-react-update-page-breadcrumbs': resolve(
+      'v11-react-update-page-breadcrumbs',
+    ),
+    'v11-styles-replace-custom-property-border': resolve(
+      'v11-styles-replace-custom-property-border',
+    ),
+    'v11-styles-replace-custom-property-depth': resolve(
+      'v11-styles-replace-custom-property-depth',
+    ),
+    'v11-styles-replace-custom-property-legacy': resolve(
+      'v11-styles-replace-custom-property-legacy',
+    ),
+    'v11-styles-replace-custom-property-z-index': resolve(
+      'v11-styles-replace-custom-property-z-index',
+    ),
+    'v10-react-replace-text-components': resolve(
+      'v10-react-replace-text-components',
+    ),
+    'v9-scss-replace-border': resolve('v9-scss-replace-border'),
+    'v9-scss-replace-border-radius': resolve('v9-scss-replace-border-radius'),
+    'v9-scss-replace-border-width': resolve('v9-scss-replace-border-width'),
+    'v9-scss-replace-breakpoints': resolve('v9-scss-replace-breakpoints'),
+    'v9-scss-replace-color': resolve('v9-scss-replace-color'),
+    'v9-scss-replace-duration': resolve('v9-scss-replace-duration'),
+    'v9-scss-replace-easing': resolve('v9-scss-replace-easing'),
+    'v9-scss-replace-font-family': resolve('v9-scss-replace-font-family'),
+    'v9-scss-replace-font-size': resolve('v9-scss-replace-font-size'),
+    'v9-scss-replace-line-height': resolve('v9-scss-replace-line-height'),
+    'v9-scss-replace-spacing': resolve('v9-scss-replace-spacing'),
+    'v9-scss-replace-text-emphasis': resolve('v9-scss-replace-text-emphasis'),
+    'v9-scss-replace-z-index': resolve('v9-scss-replace-z-index'),
+    'v9-styles-replace-custom-property-border': resolve(
+      'v9-styles-replace-custom-property-border',
+    ),
+    'v9-styles-replace-custom-property-depth': resolve(
+      'v9-styles-replace-custom-property-depth',
+    ),
+    'v9-styles-replace-custom-property-font': resolve(
+      'v9-styles-replace-custom-property-font',
+    ),
+    'v9-styles-replace-custom-property-legacy': resolve(
+      'v9-styles-replace-custom-property-legacy',
+    ),
+    'v9-styles-replace-custom-property-motion': resolve(
+      'v9-styles-replace-custom-property-motion',
+    ),
+    'v9-styles-tokenize-font': resolve('v9-styles-tokenize-font'),
+    'v9-styles-tokenize-motion': resolve('v9-styles-tokenize-motion'),
+    'v9-styles-tokenize-shape': resolve('v9-styles-tokenize-shape'),
+    'v9-styles-tokenize-space': resolve('v9-styles-tokenize-space'),
+  },
+};
--- polaris-migrator/src/cli.ts
+++ polaris-migrator/src/cli.ts
@@ -1,8 +1,66 @@
+import type {Exact} from 'type-fest';
 import meow from 'meow';
 import type {AnyFlag} from 'meow';
 
 import {migrate} from './migrate';
-import {cliConfig} from './constants';
+import type {Alias} from './utilities/types';
+import type {MigrateOptions} from './migrate';
+
+interface Arg {
+  name: string;
+  description: string;
+}
+
+interface Flag {
+  type: 'boolean' | 'string' | 'number';
+  alias?: Alias;
+  description: string;
+}
+
+type Flags = {[O in keyof MigrateOptions]: Flag};
+
+interface Config {
+  description: string;
+  args: Arg[];
+  flags: Flags;
+}
+
+const createCLIConfig = <T extends Exact<Config, T>>(config: T): T => config;
+
+export const cliConfig = createCLIConfig({
+  description: 'Code migrations for updating Polaris apps.',
+  args: [
+    {
+      name: 'migration',
+      description:
+        'One of the choices from https://polaris.shopify.com/docs/advanced-features/migrations',
+    },
+    {
+      name: 'path',
+      description:
+        'Files or directory to transform. Can be a glob like src/**.scss',
+    },
+  ],
+  flags: {
+    dry: {
+      alias: 'd',
+      description: 'Dry run (no changes are made to files)',
+      type: 'boolean',
+    },
+    print: {
+      alias: 'p',
+      description: 'Print transformed files to your terminal',
+      type: 'boolean',
+    },
+    force: {
+      alias: 'f',
+      description: 'Bypass Git safety checks and forcibly run migrations',
+      type: 'boolean',
+    },
+  },
+});
+
+export type CLIConfig = typeof cliConfig;
 
 const help = `
 Usage
