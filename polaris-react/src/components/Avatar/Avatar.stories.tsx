5f64495f8ace7d18ba0d932963db79c421ce815d
aveline <aveline@users.noreply.github.com>
2023-08-18 11:38:12 -0700

    Remove `shape` from `Avatar` component

--- polaris-react/src/components/Avatar/Avatar.stories.tsx
+++ polaris-react/src/components/Avatar/Avatar.stories.tsx
@@ -20,15 +20,6 @@ export default {
   component: Avatar,
 } as ComponentMeta<typeof Avatar>;
 
-const shapes: {
-  [S in NonNullable<AvatarProps['shape']>]: string;
-} = {
-  round: 'Round',
-  square: 'Square',
-};
-
-const shapeEntries = Object.entries(shapes) as Entries<typeof shapes>;
-
 const sizes: {
   [S in NonNullable<AvatarProps['size']>]: string;
 } = {
@@ -75,124 +66,68 @@ const styleInitialsLongEntries = Object.entries(styleInitialsLong) as Entries<
 export function All() {
   return (
     <BlockStack gap="4">
-      {shapeEntries.map(([shape, shapeLabel]) => (
-        <Box key={shape} paddingBlockEnd="2">
-          <BlockStack gap="3">
-            <Text as="h2" variant="headingXl">
-              Shape: {shapeLabel}
+      <Box paddingBlockEnd="2">
+        <BlockStack gap="3">
+          <BlockStack gap="2">
+            <Text as="h2" variant="headingXs">
+              Default
+            </Text>
+            <InlineStack gap="2" blockAlign="center">
+              {sizeEntries.map(([size]) => (
+                <Avatar key={size} size={size} />
+              ))}
+            </InlineStack>
+          </BlockStack>
+          <BlockStack gap="2">
+            <Text as="h2" variant="headingXs">
+              With customer
+            </Text>
+            <InlineStack gap="2" blockAlign="center">
+              {sizeEntries.map(([size]) => (
+                <Avatar key={size} size={size} customer />
+              ))}
+            </InlineStack>
+          </BlockStack>
+          <BlockStack gap="2">
+            <Text as="h2" variant="headingXs">
+              With image
+            </Text>
+            <InlineStack gap="2" blockAlign="center">
+              <Image />
+            </InlineStack>
+          </BlockStack>
+          <BlockStack gap="2">
+            <Text as="h2" variant="headingXs">
+              With icon (all styles)
+            </Text>
+            <IconColorsSizes />
+          </BlockStack>
+          <BlockStack gap="2">
+            <Text as="h2" variant="headingXs">
+              With default initials (all styles)
+            </Text>
+            <InitialsColorsSizes />
+          </BlockStack>
+          <BlockStack gap="2">
+            <Text as="h2" variant="headingXs">
+              With long initials (all styles)
+            </Text>
+            <InitialsLong />
+          </BlockStack>
+          <BlockStack gap="2">
+            <Text as="h2" variant="headingXs">
+              With long and wide initials
             </Text>
             <BlockStack gap="2">
-              <Text as="h2" variant="headingXs">
-                Default
-              </Text>
               <InlineStack gap="2" blockAlign="center">
                 {sizeEntries.map(([size]) => (
-                  <Avatar key={size} shape={shape} size={size} />
+                  <Avatar key={size} size={size} initials="WWW" />
                 ))}
               </InlineStack>
-            </BlockStack>
-            <BlockStack gap="2">
-              <Text as="h2" variant="headingXs">
-                With customer
-              </Text>
-              <InlineStack gap="2" blockAlign="center">
-                {sizeEntries.map(([size]) => (
-                  <Avatar key={size} shape={shape} size={size} customer />
-                ))}
-              </InlineStack>
-            </BlockStack>
-            <BlockStack gap="2">
-              <Text as="h2" variant="headingXs">
-                With image
-              </Text>
-              <InlineStack gap="2" blockAlign="center">
-                {sizeEntries.map(([size]) => (
-                  <Avatar
-                    key={size}
-                    shape={shape}
-                    size={size}
-                    source="https://burst.shopifycdn.com/photos/woman-dressed-in-pale-colors.jpg"
-                  />
-                ))}
-              </InlineStack>
-            </BlockStack>
-            <BlockStack gap="2">
-              <Text as="h2" variant="headingXs">
-                With name (all styles)
-              </Text>
-              <BlockStack gap="2">
-                {styleInitialsDefaultEntries.map(([style, initials]) => (
-                  <InlineStack key={style} gap="2" blockAlign="center">
-                    {sizeEntries.map(([size]) => (
-                      <Avatar
-                        key={size}
-                        shape={shape}
-                        name={initials}
-                        size={size}
-                      />
-                    ))}
-                  </InlineStack>
-                ))}
-              </BlockStack>
-            </BlockStack>
-            <BlockStack gap="2">
-              <Text as="h2" variant="headingXs">
-                With default initials (all styles)
-              </Text>
-              <BlockStack gap="2">
-                {styleInitialsDefaultEntries.map(([style, initials]) => (
-                  <InlineStack key={style} gap="2" blockAlign="center">
-                    {sizeEntries.map(([size]) => (
-                      <Avatar
-                        key={size}
-                        shape={shape}
-                        initials={initials}
-                        size={size}
-                      />
-                    ))}
-                  </InlineStack>
-                ))}
-              </BlockStack>
-            </BlockStack>
-            <BlockStack gap="2">
-              <Text as="h2" variant="headingXs">
-                With long initials (all styles)
-              </Text>
-              <BlockStack gap="2">
-                {styleInitialsLongEntries.map(([style, initialsLong]) => (
-                  <InlineStack key={style} gap="2" blockAlign="center">
-                    {sizeEntries.map(([size]) => (
-                      <Avatar
-                        key={size}
-                        shape={shape}
-                        initials={initialsLong}
-                        size={size}
-                      />
-                    ))}
-                  </InlineStack>
-                ))}
-              </BlockStack>
-            </BlockStack>
-            <BlockStack gap="2">
-              <Text as="h2" variant="headingXs">
-                With long and wide initials
-              </Text>
-              <BlockStack gap="2">
-                <InlineStack gap="2" blockAlign="center">
-                  {sizeEntries.map(([size]) => (
-                    <Avatar
-                      key={size}
-                      shape={shape}
-                      size={size}
-                      initials="WWW"
-                    />
-                  ))}
-                </InlineStack>
-              </BlockStack>
             </BlockStack>
           </BlockStack>
-        </Box>
-      ))}
+        </BlockStack>
+      </Box>
     </BlockStack>
   );
 }
@@ -201,89 +136,49 @@ export function Default() {
   return <Avatar />;
 }
 
-export function CircleIconColorsSizes() {
+export function IconColorsSizes() {
   return (
-    <BlockStack gap="4">
-      <InlineStack gap="4">
-        <Avatar customer size="extraSmall" />
-        <Avatar name="AG" size="extraSmall" />
-        <Avatar name="AA" size="extraSmall" />
-        <Avatar name="AC" size="extraSmall" />
-        <Avatar name="AB" size="extraSmall" />
-        <Avatar name="AE" size="extraSmall" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar customer size="small" />
-        <Avatar name="AG" size="small" />
-        <Avatar name="AA" size="small" />
-        <Avatar name="AC" size="small" />
-        <Avatar name="AB" size="small" />
-        <Avatar name="AE" size="small" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar customer size="medium" />
-        <Avatar name="AG" size="medium" />
-        <Avatar name="AA" size="medium" />
-        <Avatar name="AC" size="medium" />
-        <Avatar name="AB" size="medium" />
-        <Avatar name="AE" size="medium" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar customer size="large" />
-        <Avatar name="AG" size="large" />
-        <Avatar name="AA" size="large" />
-        <Avatar name="AC" size="large" />
-        <Avatar name="AB" size="large" />
-        <Avatar name="AE" size="large" />
-      </InlineStack>
+    <BlockStack gap="2">
+      {styleInitialsDefaultEntries.map(([style, initials]) => (
+        <InlineStack key={style} gap="2" blockAlign="center">
+          {sizeEntries.map(([size]) => (
+            <Avatar key={size} name={initials} size={size} />
+          ))}
+        </InlineStack>
+      ))}
     </BlockStack>
   );
 }
 
-export function CircleInitialsColorsSizes() {
+export function InitialsColorsSizes() {
   return (
-    <BlockStack gap="4">
-      <InlineStack gap="4">
-        <Avatar initials="AG" size="extraSmall" />
-        <Avatar initials="AA" size="extraSmall" />
-        <Avatar initials="AC" size="extraSmall" />
-        <Avatar initials="AB" size="extraSmall" />
-        <Avatar initials="AE" size="extraSmall" />
-        <Avatar initials="WW" size="extraSmall" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar initials="AG" size="small" />
-        <Avatar initials="AA" size="small" />
-        <Avatar initials="AC" size="small" />
-        <Avatar initials="AB" size="small" />
-        <Avatar initials="AE" size="small" />
-        <Avatar initials="WW" size="small" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar initials="AG" size="medium" />
-        <Avatar initials="AA" size="medium" />
-        <Avatar initials="AC" size="medium" />
-        <Avatar initials="AB" size="medium" />
-        <Avatar initials="AE" size="medium" />
-        <Avatar initials="WW" size="medium" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar initials="AG" size="large" />
-        <Avatar initials="AA" size="large" />
-        <Avatar initials="AC" size="large" />
-        <Avatar initials="AB" size="large" />
-        <Avatar initials="AE" size="large" />
-        <Avatar initials="WW" size="large" />
-      </InlineStack>
+    <BlockStack gap="2">
+      {styleInitialsDefaultEntries.map(([style, initials]) => (
+        <InlineStack key={style} gap="2" blockAlign="center">
+          {sizeEntries.map(([size]) => (
+            <Avatar key={size} initials={initials} size={size} />
+          ))}
+        </InlineStack>
+      ))}
     </BlockStack>
   );
 }
 
-export function CircleInitialsLong() {
-  return <Avatar initials="WWW" name="Woluwayemisi Wolu Weun-Jung" />;
+export function InitialsLong() {
+  return (
+    <BlockStack gap="2">
+      {styleInitialsLongEntries.map(([style, initialsLong]) => (
+        <InlineStack key={style} gap="2" blockAlign="center">
+          {sizeEntries.map(([size]) => (
+            <Avatar key={size} initials={initialsLong} size={size} />
+          ))}
+        </InlineStack>
+      ))}
+    </BlockStack>
+  );
 }
 
-export function CircleExtraSmallInContext() {
+export function ExtraSmallInContext() {
   const [active, setActive] = useState(true);
   const toggleActive = useCallback(() => setActive((active) => !active), []);
   const activator = (
@@ -314,89 +209,18 @@ export function CircleExtraSmallInContext() {
   );
 }
 
-export function CircleImage() {
+export function Image() {
   return (
-    <Avatar
-      name="Image"
-      source="https://burst.shopifycdn.com/photos/woman-dressed-in-pale-colors.jpg"
-    />
-  );
-}
-
-export function SquareIconColorsSizes() {
-  return (
-    <BlockStack gap="4">
-      <InlineStack gap="4">
-        <Avatar customer size="extraSmall" shape="square" />
-        <Avatar name="AG" size="extraSmall" shape="square" />
-        <Avatar name="AA" size="extraSmall" shape="square" />
-        <Avatar name="AC" size="extraSmall" shape="square" />
-        <Avatar name="AB" size="extraSmall" shape="square" />
-        <Avatar name="AE" size="extraSmall" shape="square" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar customer size="small" shape="square" />
-        <Avatar name="AG" size="small" shape="square" />
-        <Avatar name="AA" size="small" shape="square" />
-        <Avatar name="AC" size="small" shape="square" />
-        <Avatar name="AB" size="small" shape="square" />
-        <Avatar name="AE" size="small" shape="square" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar customer size="medium" shape="square" />
-        <Avatar name="AG" size="medium" shape="square" />
-        <Avatar name="AA" size="medium" shape="square" />
-        <Avatar name="AC" size="medium" shape="square" />
-        <Avatar name="AB" size="medium" shape="square" />
-        <Avatar name="AE" size="medium" shape="square" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar customer size="large" shape="square" />
-        <Avatar name="AG" size="large" shape="square" />
-        <Avatar name="AA" size="large" shape="square" />
-        <Avatar name="AC" size="large" shape="square" />
-        <Avatar name="AB" size="large" shape="square" />
-        <Avatar name="AE" size="large" shape="square" />
-      </InlineStack>
-    </BlockStack>
-  );
-}
-
-export function SquareInitialsColorsSizes() {
-  return (
-    <BlockStack gap="4">
-      <InlineStack gap="4">
-        <Avatar initials="AG" size="extraSmall" shape="square" />
-        <Avatar initials="AA" size="extraSmall" shape="square" />
-        <Avatar initials="AC" size="extraSmall" shape="square" />
-        <Avatar initials="AB" size="extraSmall" shape="square" />
-        <Avatar initials="AE" size="extraSmall" shape="square" />
-        <Avatar initials="WW" size="extraSmall" shape="square" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar initials="AG" size="small" shape="square" />
-        <Avatar initials="AA" size="small" shape="square" />
-        <Avatar initials="AC" size="small" shape="square" />
-        <Avatar initials="AB" size="small" shape="square" />
-        <Avatar initials="AE" size="small" shape="square" />
-        <Avatar initials="WW" size="small" shape="square" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar initials="AG" size="medium" shape="square" />
-        <Avatar initials="AA" size="medium" shape="square" />
-        <Avatar initials="AC" size="medium" shape="square" />
-        <Avatar initials="AB" size="medium" shape="square" />
-        <Avatar initials="AE" size="medium" shape="square" />
-        <Avatar initials="WW" size="medium" shape="square" />
-      </InlineStack>
-      <InlineStack gap="4">
-        <Avatar initials="AG" size="large" shape="square" />
-        <Avatar initials="AA" size="large" shape="square" />
-        <Avatar initials="AC" size="large" shape="square" />
-        <Avatar initials="AB" size="large" shape="square" />
-        <Avatar initials="AE" size="large" shape="square" />
-        <Avatar initials="WW" size="large" shape="square" />
-      </InlineStack>
-    </BlockStack>
+    <InlineStack gap="2" blockAlign="center">
+      {sizeEntries.map(([size]) => (
+        <Avatar
+          key={size}
+          size={size}
+          initials="WWW"
+          name="Image"
+          source="https://burst.shopifycdn.com/photos/woman-dressed-in-pale-colors.jpg"
+        />
+      ))}
+    </InlineStack>
   );
 }
