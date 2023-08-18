359614cf835973ae742082ff6fdc17ff37c27fc6
Kyle Durand <kyledurand@users.noreply.github.com>
2023-08-18 08:48:44 -0400

    Textfield add variant prop (#10036)
    <!--
      ☝️How to write a good PR title:
    - Prefix it with [ComponentName] (if applicable), for example: [Button]
      - Start with a verb, for example: Add, Delete, Improve, Fix…
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ### WHY are these changes introduced?
    
    Fixes https://github.com/Shopify/polaris/issues/10033
    
    <!--
      Context about the problem that’s being addressed.
    -->
    
    ### WHAT is this pull request doing?
    
    Deprecates `borderless` prop in favor of `variant='borderless'`

--- polaris-react/src/components/Filters/components/SearchField/SearchField.tsx
+++ polaris-react/src/components/Filters/components/SearchField/SearchField.tsx
@@ -105,7 +105,7 @@ export function SearchField({
       clearButton
       onClearButtonClick={handleClear}
       disabled={disabled}
-      borderless={borderlessQueryField}
+      variant={borderlessQueryField ? 'borderless' : undefined}
     />
   );
 }
