90b731d6c651af17743d3d2feb3cd0bfd3bcd2cf
Jess Telford <jess.telford@shopify.com>
2023-08-18 19:13:50 +1000

    Reinstate things lost during rebase

--- polaris.shopify.com/playroom/FrameComponent.tsx
+++ polaris.shopify.com/playroom/FrameComponent.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {AppProvider} from '@shopify/polaris';
 import '@shopify/polaris/build/esm/styles.css';
 import enTranslations from '@shopify/polaris/locales/en.json';
@@ -12,6 +12,8 @@ export default function FrameComponent({
   theme: any;
   children: React.ReactNode;
 }) {
+  const [features, setFeatures] =
+    useState<React.ComponentProps<typeof AppProvider>['features']>();
   const wrapperRef = useRef<HTMLDivElement | null>(null);
 
   // Tell parent frame of the initial size on first render
@@ -38,8 +40,21 @@ export default function FrameComponent({
     };
   }, []);
 
+  useEffect(() => {
+    const urlFeatures = new URLSearchParams(window.location.search).get(
+      'features',
+    );
+    if (typeof urlFeatures === 'string') {
+      setFeatures(
+        Object.fromEntries(
+          urlFeatures.split(',').map((feature) => [feature.trim(), true]),
+        ),
+      );
+    }
+  }, []);
+
   return (
-    <AppProvider i18n={theme || enTranslations}>
+    <AppProvider i18n={theme || enTranslations} features={features}>
       <div id="polaris-sandbox-wrapper" ref={wrapperRef}>
         {children}
       </div>
