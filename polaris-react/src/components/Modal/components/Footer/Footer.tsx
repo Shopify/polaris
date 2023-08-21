310b34bc96e409fc617eb8b3dd9991c9086b89b7
Lo Kim <lo.kim@shopify.com>
2023-08-21 12:53:04 -0400

    [Modal] Consolidate se23 styles and logic (#10139)
    ### WHY are these changes introduced?
    
    Resolves #9951.
    
    ### WHAT is this pull request doing?
    
    Consolidates se23 beta styles and logic for `Modal`.
    
    ### How to 🎩
    
    [Storybook](https://5d559397bae39100201eedc1-tqhardweqn.chromatic.com/?path=/story/all-components-modal--default)
    [Prod
    Storybook](https://storybook.polaris.shopify.com/?path=/story/all-components-modal--default&globals=polarisSummerEditions2023:true)
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    ### 🎩 checklist
    
    - [x] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [x] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [ ] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [ ] Updated the component's `README.md` with documentation changes
    - [ ] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris-react/src/components/Modal/components/Footer/Footer.tsx
+++ polaris-react/src/components/Modal/components/Footer/Footer.tsx
@@ -4,7 +4,6 @@ import type {ComplexAction} from '../../../../types';
 import {buttonsFrom} from '../../../Button';
 import {Box} from '../../../Box';
 import {InlineStack} from '../../../InlineStack';
-import {useFeatures} from '../../../../utilities/features';
 
 export interface FooterProps {
   /** Primary action */
@@ -20,8 +19,6 @@ export function Footer({
   secondaryActions,
   children,
 }: FooterProps) {
-  const {polarisSummerEditions2023} = useFeatures();
-
   const primaryActionButton =
     (primaryAction && buttonsFrom(primaryAction, {primary: true})) || null;
   const secondaryActionButtons =
@@ -37,12 +34,9 @@ export function Footer({
   return (
     <InlineStack gap="4" blockAlign="center">
       <Box
-        borderColor={polarisSummerEditions2023 ? 'border' : 'border-subdued'}
+        borderColor="border"
         borderBlockStartWidth="1"
-        minHeight={polarisSummerEditions2023 ? undefined : 'var(--p-space-16)'}
         padding="4"
-        paddingInlineStart={polarisSummerEditions2023 ? undefined : '5'}
-        paddingInlineEnd={polarisSummerEditions2023 ? undefined : '5'}
         width="100%"
       >
         <InlineStack gap="4" blockAlign="center" align="space-between">
