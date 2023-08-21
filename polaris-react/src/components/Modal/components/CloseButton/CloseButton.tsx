fcaa40c8d36eec3dcd57240a09a74976455700b9
Lo Kim <lo.kim@shopify.com>
2023-08-21 11:41:05 -0400

    [Modal] Consolidate se23 styles and logic

--- polaris-react/src/components/Modal/components/CloseButton/CloseButton.tsx
+++ polaris-react/src/components/Modal/components/CloseButton/CloseButton.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import {MobileCancelMajor, CancelMajor} from '@shopify/polaris-icons';
+import {CancelMajor} from '@shopify/polaris-icons';
 
-import {classNames} from '../../../../utilities/css';
 import {useI18n} from '../../../../utilities/i18n';
-import {useFeatures} from '../../../../utilities/features';
-import {Icon} from '../../../Icon';
 import {Button} from '../../../Button';
-
-import styles from './CloseButton.scss';
 
 export interface CloseButtonProps {
   pressed?: boolean;
-  titleHidden?: boolean;
   onClick(): void;
 }
 
-export function CloseButton({
-  pressed,
-  titleHidden = false,
-  onClick,
-}: CloseButtonProps) {
+export function CloseButton({pressed, onClick}: CloseButtonProps) {
   const i18n = useI18n();
-  const {polarisSummerEditions2023} = useFeatures();
 
-  return polarisSummerEditions2023 ? (
+  return (
     <Button
       primary
       plain
@@ -32,17 +21,5 @@ export function CloseButton({
       onClick={onClick}
       accessibilityLabel={i18n.translate('Polaris.Common.close')}
     />
-  ) : (
-    <button
-      onClick={onClick}
-      className={classNames(
-        styles.CloseButton,
-        titleHidden && styles.titleHidden,
-        pressed && styles.pressed,
-      )}
-      aria-label={i18n.translate('Polaris.Common.close')}
-    >
-      <Icon source={MobileCancelMajor} color="base" />
-    </button>
   );
 }
