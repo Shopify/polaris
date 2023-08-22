ef7d60e829fec275c9bc08f47620db946060c5d5
Yuraima Estevez <yuraima.estevez@shopify.com>
2023-08-22 13:31:15 +0100

    Components page component automatically handles title, lede, and status banners.

--- polaris.shopify.com/src/components/StatusBanner/StatusBanner.tsx
+++ polaris.shopify.com/src/components/StatusBanner/StatusBanner.tsx
@@ -1,18 +1,20 @@
 import type {PropsWithChildren} from 'react';
-import {StatusName} from '../../types';
+import {Status} from '../../types';
 import {uppercaseFirst} from '../../utils/various';
 import styles from './StatusBanner.module.scss';
 
 interface Props extends PropsWithChildren {
-  status: StatusName;
+  status: Status;
 }
 
-function StatusBanner({status, children}: Props) {
+function StatusBanner({status: {value, message}}: Props) {
   return (
-    <div className={styles.StatusBanner} data-value={status.toLowerCase()}>
-      <h2>{uppercaseFirst(status)}</h2>
-      {children}
-    </div>
+    value && (
+      <div className={styles.StatusBanner} data-value={value.toLowerCase()}>
+        <h2>{uppercaseFirst(value)}</h2>
+        {message}
+      </div>
+    )
   );
 }
 
