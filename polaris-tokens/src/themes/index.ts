0b8b9d58f1e0b8293ed993190422083a9601eb06
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2023-08-23 15:22:58 -0700

    Expose theme-agnostic vars for use in JavaScript

--- polaris-tokens/src/themes/index.ts
+++ polaris-tokens/src/themes/index.ts
@@ -1,6 +1,10 @@
+import {createVar} from '../utilities';
+
 import type {Themes, ThemesPartials} from './types';
 import {themeLight} from './light';
 import {themeLightUplift, themeLightUpliftPartial} from './light-uplift';
+import type {ThemeBase} from './base';
+import {themeBase} from './base';
 
 export {themeDefault} from './constants';
 
@@ -12,3 +16,45 @@ export const themes: Themes = {
 export const themesPartials: ThemesPartials = {
   'Polaris-Summer-Editions-2023': themeLightUpliftPartial,
 };
+
+export type ThemeVars = {
+  [TokenGroupName in keyof ThemeBase]: {
+    [TokenName in keyof ThemeBase[TokenGroupName]]: string;
+  };
+};
+
+/**
+ * Theme object where values represent token names as CSS variables.
+ *
+ * Useful for applying theme-agnostic tokens in JavaScript.
+ *
+ * @example
+ * const themeVars = {
+ *   color: { 'color-bg': 'var(--p-color-bg)' }
+ *   font: { 'font-family': 'var(--p-font-family)' }
+ *   // etc.
+ * }
+ *
+ * @example
+ * // ✅ Do
+ * import {themeVars} from '@shopify/polaris-tokens';
+ *
+ * themeVars.color['color-bg']; // var(--p-color-bg)
+ *
+ * // ❌ Don't
+ * import * as themes from '@shopify/polaris-tokens/themes';
+ *
+ * themes.light.color['color-bg']; // #123
+ * themes.dark.color['color-bg']; // #456
+ */
+export const themeVars = Object.fromEntries(
+  Object.entries(themeBase).map(([tokenGroupName, tokenGroup]) => [
+    tokenGroupName,
+    Object.fromEntries(
+      Object.keys(tokenGroup).map((tokenName) => [
+        tokenName,
+        `var(${createVar(tokenName)})`,
+      ]),
+    ),
+  ]),
+) as ThemeVars;
