662966b15e491cad0f20154ad5aec7ee69571672
Jess Telford <jess.telford@shopify.com>
2023-08-23 16:09:50 +1000

    [Docs] Remove unused Grid#deepLinks prop

--- polaris.shopify.com/src/utils/foundations.ts
+++ polaris.shopify.com/src/utils/foundations.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import {MarkdownFile} from '../types';
 import {parseMarkdown} from './markdown.mjs';
-import {slugify, uppercaseFirst} from './various';
+import {uppercaseFirst} from './various';
 import {GetStaticProps} from 'next';
 import {FoundationsProps} from '../components/FoundationsPage/FoundationsPage';
 
@@ -34,7 +34,7 @@ export const getStaticPropsForFoundations = (category: string) => {
       .forEach((markdownFilePath) => {
         if (fs.existsSync(markdownFilePath)) {
           const markdown = fs.readFileSync(markdownFilePath, 'utf-8');
-          const {frontMatter, readme}: MarkdownFile = parseMarkdown(markdown);
+          const {frontMatter}: MarkdownFile = parseMarkdown(markdown);
           const {
             title,
             description,
@@ -50,20 +50,11 @@ export const getStaticPropsForFoundations = (category: string) => {
               .replace('/index', '')
               .replace(/\.md$/, '');
 
-          const headings = (readme.match(/\n## [^\n]+/gi) || []).map(
-            (heading) => heading.replace(/^\n## /, '').trim(),
-          );
-          const deepLinks = headings.map((heading) => ({
-            url: `${url.replace(/\/$/, '')}#${slugify(heading)}`,
-            text: heading,
-          }));
-
           items.push({
             title,
             description: description || '',
             icon: icon || '',
             url,
-            deepLinks,
             order: !isNaN(parseInt(order)) ? order : 1000,
           });
         }
