e17a5129e548898a361509e57ba31b1a0205ed4a
Yuraima Estevez <yuraima.estevez@shopify.com>
2023-08-24 17:22:30 +0100

    fix status banners on component pages by rendering mdx for the status message

--- polaris.shopify.com/src/components/StatusBanner/StatusBanner.tsx
+++ polaris.shopify.com/src/components/StatusBanner/StatusBanner.tsx
@@ -2,17 +2,18 @@ import type {PropsWithChildren} from 'react';
 import {Status} from '../../types';
 import {uppercaseFirst} from '../../utils/various';
 import styles from './StatusBanner.module.scss';
+import Markdown from '../Markdown';
 
 interface Props extends PropsWithChildren {
   status: Status;
 }
 
-function StatusBanner({status: {value, message}}: Props) {
+function StatusBanner({status: {value, message, mdx}}: Props) {
   return (
     value && (
       <div className={styles.StatusBanner} data-value={value.toLowerCase()}>
         <h2>{uppercaseFirst(value)}</h2>
-        {message}
+        {mdx ? <Markdown {...mdx} /> : message}
       </div>
     )
   );
