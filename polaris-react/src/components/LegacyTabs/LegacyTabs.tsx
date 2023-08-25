776f28a86dd41e614b877da8313b4fb048aff362
Lo Kim <lo.kim@shopify.com>
2023-08-25 10:48:01 -0400

    [LegacyTabs] Consolidate se23 styles and logic (#10231)
    ### WHY are these changes introduced?
    
    Resolves #9946.
    
    ### WHAT is this pull request doing?
    
    Consolidates se23 beta styles and logic for `LegacyTabs`.
    
    ### How to 🎩
    
    [Storybook](https://5d559397bae39100201eedc1-yxlghuvhen.chromatic.com/?path=/story/all-components-legacytabs--all)
    [Prod
    Storybook](https://storybook.polaris.shopify.com/?path=/story/all-components-legacytabs--all&globals=polarisSummerEditions2023:true)
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    ### 🎩 checklist
    
    - [x] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [x] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [ ] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [ ] Updated the component's `README.md` with documentation changes
    - [ ] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris-react/src/components/LegacyTabs/LegacyTabs.tsx
+++ polaris-react/src/components/LegacyTabs/LegacyTabs.tsx
@@ -1,16 +1,11 @@
 import React, {PureComponent} from 'react';
-import {
-  HorizontalDotsMinor,
-  CaretDownMinor,
-  ChevronDownMinor,
-} from '@shopify/polaris-icons';
+import {HorizontalDotsMinor, ChevronDownMinor} from '@shopify/polaris-icons';
 
 import {Box} from '../Box';
 import {Icon} from '../Icon';
 import {Popover} from '../Popover';
 import {classNames} from '../../utilities/css';
 import {useI18n} from '../../utilities/i18n';
-import {UseFeatures} from '../../utilities/features';
 
 import type {TabDescriptor} from './types';
 import {getVisibleAndHiddenTabIndices} from './utilities';
@@ -135,16 +130,7 @@ class TabsInner extends PureComponent<CombinedProps, State> {
     const disclosureButtonContent = hasCustomDisclosure ? (
       <>
         {disclosureText}
-        <UseFeatures>
-          {({polarisSummerEditions2023}) => (
-            <Icon
-              source={
-                polarisSummerEditions2023 ? ChevronDownMinor : CaretDownMinor
-              }
-              tone="subdued"
-            />
-          )}
-        </UseFeatures>
+        <Icon source={ChevronDownMinor} tone="subdued" />
       </>
     ) : (
       <Icon source={HorizontalDotsMinor} tone="subdued" />
