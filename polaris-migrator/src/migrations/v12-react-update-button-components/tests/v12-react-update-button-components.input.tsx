70bc2897faa2175f06f678f8d8cccf2bc6c061f9
aveline <aveline@users.noreply.github.com>
2023-08-30 14:39:09 -0700

    Add more test cases

--- polaris-migrator/src/migrations/v12-react-update-button-components/tests/v12-react-update-button-components.input.tsx
+++ polaris-migrator/src/migrations/v12-react-update-button-components/tests/v12-react-update-button-components.input.tsx
@@ -3,6 +3,10 @@ import {Button} from '@shopify/polaris';
 import {PhoneMajor} from '@shopify/polaris-icons';
 
 export function App() {
+  const hasFormError = false;
+  const polarisSummerEditions2023Enabled = true;
+  const disabled = false;
+  const primary = true;
   return (
     <>
       <Button destructive outline disabled>
@@ -15,11 +19,40 @@ export function App() {
       <Button monochrome>Monochrome</Button>
       <Button outline>Outline</Button>
       <Button destructive>Destructive</Button>
+      <Button primarySuccess>Primary success</Button>
       <Button destructive outline>
         Destructive outline
       </Button>
       <Button destructive plain>
         Destructive plain
+      </Button>
+      <Button
+        disclosure="select"
+        fullWidth
+        textAlign="left"
+        outline={hasFormError}
+        destructive={hasFormError}
+        disabled={disabled}
+        size={polarisSummerEditions2023Enabled ? 'large' : undefined}
+      >
+        Selected branch
+      </Button>
+      <Button
+        icon={PhoneMajor}
+        removeUnderline
+        plain={!primary}
+        monochrome={!primary}
+        disabled={disabled}
+        primarySuccess={primary}
+      >
+        Content
+      </Button>
+      <Button
+        outline={!polarisSummerEditions2023Enabled}
+        plain={polarisSummerEditions2023Enabled}
+        primary={polarisSummerEditions2023Enabled}
+      >
+        Button
       </Button>
     </>
   );
--- polaris-migrator/src/migrations/v12-react-update-button-components/tests/v12-react-update-button-components.output.tsx
+++ polaris-migrator/src/migrations/v12-react-update-button-components/tests/v12-react-update-button-components.output.tsx
@@ -3,6 +3,10 @@ import {Button} from '@shopify/polaris';
 import {PhoneMajor} from '@shopify/polaris-icons';
 
 export function App() {
+  const hasFormError = false;
+  const polarisSummerEditions2023Enabled = true;
+  const disabled = false;
+  const primary = true;
   return (
     <>
       <Button disabled tone="critical">
@@ -17,9 +21,43 @@ export function App() {
       <Button variant="primary" tone="critical">
         Destructive
       </Button>
+      <Button variant="primary" tone="success">
+        Primary success
+      </Button>
       <Button tone="critical">Destructive outline</Button>
       <Button variant="plain" tone="critical">
         Destructive plain
+      </Button>
+      {/* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */}
+      <Button
+        disclosure="select"
+        fullWidth
+        textAlign="left"
+        outline={hasFormError}
+        destructive={hasFormError}
+        disabled={disabled}
+        size={polarisSummerEditions2023Enabled ? 'large' : undefined}
+      >
+        Selected branch
+      </Button>
+      {/* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */}
+      <Button
+        icon={PhoneMajor}
+        removeUnderline
+        plain={!primary}
+        monochrome={!primary}
+        disabled={disabled}
+        primarySuccess={primary}
+      >
+        Content
+      </Button>
+      {/* polaris-migrator: Unable to migrate the following expression. Please upgrade manually. */}
+      <Button
+        outline={!polarisSummerEditions2023Enabled}
+        plain={polarisSummerEditions2023Enabled}
+        primary={polarisSummerEditions2023Enabled}
+      >
+        Button
       </Button>
     </>
   );
