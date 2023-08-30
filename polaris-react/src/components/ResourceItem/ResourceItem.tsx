76d9c8abc192c4ca008e5e0e80d4828d5cc9531e
Lo Kim <lo.kim@shopify.com>
2023-08-30 08:15:48 -0400

    [ResourceItem] Fix focus styles for BulkActions

--- polaris-react/src/components/ResourceItem/ResourceItem.tsx
+++ polaris-react/src/components/ResourceItem/ResourceItem.tsx
@@ -225,6 +225,7 @@ class BaseResourceItem extends Component<CombinedProps, State> {
       styles.ListItem,
       focused && !focusedInner && styles.focused,
       hasBulkActions && styles.hasBulkActions,
+      selected && styles.selected,
       selectable && styles.selectable,
     );
 
