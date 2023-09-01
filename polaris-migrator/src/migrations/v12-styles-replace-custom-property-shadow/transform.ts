a8caec5e85529dd165667adf0e993db476f06cc9
Laura Griffee <laura@mailzone.com>
2023-09-01 12:46:43 -0600

    Scaffold migration

---
+++ polaris-migrator/src/migrations/v12-styles-replace-custom-property-shadow/transform.ts
@@ -0,0 +1,13 @@
+import type {FileInfo, API} from 'jscodeshift';
+
+import stylesReplaceCustomProperty from '../styles-replace-custom-property/transform';
+
+export default function transformer(fileInfo: FileInfo, _: API) {
+  return stylesReplaceCustomProperty(fileInfo, _, {replacementMaps});
+}
+
+const replacementMaps = {
+  '/.+/': {
+    '--p-shadow-XX': '--p-shadow-XX',
+  },
+};
