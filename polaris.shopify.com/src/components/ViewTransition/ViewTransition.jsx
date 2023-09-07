95c3414fc76e5aecfcbbba47bbfb607fe11c230e
Chloe Rice <chloe.rice@shopify.com>
2023-09-07 18:35:26 -0400

    Alright Next, you win.

--- polaris.shopify.com/src/components/ViewTransition/ViewTransition.jsx
+++ polaris.shopify.com/src/components/ViewTransition/ViewTransition.jsx
@@ -2,23 +2,25 @@ import React, {useEffect} from 'react';
 import {useRouter} from 'next/router';
 
 export default function ViewTransition({children}) {
-  const {beforePopState, isReady} = useRouter();
+  const router = useRouter();
 
   useEffect(() => {
-    beforePopState(() => {
-      if (isReady && document.startViewTransition !== undefined) {
+    const handler = () => {
+      if (document.startViewTransition !== undefined) {
         document.startViewTransition(async () => {
-          console.log('Calling startViewTransition');
-
           return await new Promise((resolve) => {
             return resolve(true);
           });
         });
       }
+    };
 
-      return true;
-    });
-  });
+    router.events.on('routeChangeStart', handler);
+
+    return () => {
+      router.events.off('routeChangeStart', handler);
+    };
+  }, [router.events]);
 
   return children;
 }
