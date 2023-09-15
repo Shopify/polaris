84eea45a5e6084ee1a13f1ba2177de5713182b7d
Jess Telford <jess.telford@shopify.com>
2023-09-15 18:22:08 +1000

    Standardize how we map over responsive props within components

--- polaris-react/src/utilities/tests/is-object.test.ts
+++ polaris-react/src/utilities/tests/is-object.test.ts
@@ -1,8 +1,32 @@
-import {isObject} from '../is-object';
+import {isObjectishOrFunction, isObject} from '../is-object';
+
+describe('isObjectishOrFunction', () => {
+  it('returns false for nullish', () => {
+    expect(isObjectishOrFunction(null)).toBe(false);
+    expect(isObjectishOrFunction(undefined)).toBe(false);
+  });
+
+  it('returns false for primitive values', () => {
+    expect(isObjectishOrFunction(4)).toBe(false);
+  });
+
+  it('returns true for objects', () => {
+    expect(isObjectishOrFunction({})).toBe(true);
+  });
+
+  it('returns true for functions', () => {
+    expect(isObjectishOrFunction(() => {})).toBe(true);
+  });
+
+  it('returns true for arrays', () => {
+    expect(isObjectishOrFunction([])).toBe(true);
+  });
+});
 
 describe('isObject', () => {
-  it('returns false for null', () => {
+  it('returns false for nullish', () => {
     expect(isObject(null)).toBe(false);
+    expect(isObject(undefined)).toBe(false);
   });
 
   it('returns false for primitive values', () => {
@@ -13,7 +37,11 @@ describe('isObject', () => {
     expect(isObject({})).toBe(true);
   });
 
-  it('returns true for functions', () => {
-    expect(isObject(() => {})).toBe(true);
+  it('returns false for functions', () => {
+    expect(isObject(() => {})).toBe(false);
+  });
+
+  it('returns false for arrays', () => {
+    expect(isObject([])).toBe(false);
   });
 });
