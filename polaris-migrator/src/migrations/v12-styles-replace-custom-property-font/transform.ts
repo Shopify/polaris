235ef9712d190f585f3e11eb40e78ac65561a510
Laura Griffee <laura@mailzone.com>
2023-09-15 11:53:03 -0600

    Add v12 migration for `font` custom properties

--- polaris-migrator/src/migrations/v12-styles-replace-custom-property-font/transform.ts
+++ polaris-migrator/src/migrations/v12-styles-replace-custom-property-font/transform.ts
@@ -8,6 +8,24 @@ export default function transformer(fileInfo: FileInfo, _: API) {
 
 const replacementMaps = {
   '/.+/': {
-    '--p-font-XX': '--p-font-XX',
+    '--p-font-size-70-experimental': '--p-font-size-275',
+    '--p-font-size-75': '--p-font-size-300',
+    '--p-font-size-80-experimental': '--p-font-size-325',
+    '--p-font-size-100': '--p-font-size-350',
+    '--p-font-size-200': '--p-font-size-400',
+    // The following migrations need to be run on the next branch with the v12 launch
+    // '--p-font-size-300': '--p-font-size-500',
+    // '--p-font-size-400': '--p-font-size-600',
+    // '--p-font-size-500': '--p-font-size-750',
+    // '--p-font-size-600': '--p-font-size-900',
+    '--p-font-size-700': '--p-font-size-1000',
+    '--p-font-line-height-075-experimental': '--p-font-line-height-300',
+    '--p-font-line-height-1': '--p-font-line-height-400',
+    '--p-font-line-height-2': '--p-font-line-height-500',
+    '--p-font-line-height-3': '--p-font-line-height-600',
+    '--p-font-line-height-4': '--p-font-line-height-700',
+    '--p-font-line-height-5': '--p-font-line-height-800',
+    '--p-font-line-height-6': '--p-font-line-height-1000',
+    '--p-font-line-height-7': '--p-font-line-height-1200',
   },
 };
