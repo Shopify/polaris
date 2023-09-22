205635b52705af82a3146d4d2f466c59292c2147
Lo Kim <lo.kim@shopify.com>
2023-09-22 12:30:50 -0400

    [Docs] Fix hydration errors
    Co-authored-by: Yuraima Estevez <yuraima.estevez@shopify.com>

--- polaris.shopify.com/src/components/Lede/index.tsx
+++ polaris.shopify.com/src/components/Lede/index.tsx
@@ -2,13 +2,13 @@ import {forwardRef} from 'react';
 import styles from './Lede.module.scss';
 import {Box, type WithAsProp} from '../Box';
 
-export const Lede = forwardRef(({as = 'p', className, ...props}, ref) => (
+export const Lede = forwardRef(({as = 'div', className, ...props}, ref) => (
   <Box
     ref={ref}
     as={as}
     className={[styles.LedeParagraph, className]}
     {...props}
   />
-)) as WithAsProp<{}, typeof Box, 'p'>;
+)) as WithAsProp<{}, typeof Box, 'div'>;
 
 Lede.displayName = 'Lede';
