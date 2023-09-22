01b63b4a6f02b9ca0112061634fab75a04891039
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2023-09-22 16:58:03 -0700

    Initial resolve theme refs exploration

--- polaris-tokens/src/themes/utils.ts
+++ polaris-tokens/src/themes/utils.ts
@@ -11,6 +11,7 @@ import type {
   MetaThemePartialShape,
   MetaTokenGroupShape,
   ThemeName,
+  Theme,
 } from './types';
 import {themeNameLightUplift} from './constants';
 import {metaThemeBase} from './base';
@@ -53,4 +54,33 @@ export function extractMetaThemeValues<T extends MetaThemeShape>(metaTheme: T) {
       > => [tokenGroupName, extractMetaTokenGroupValues(metaTokenGroup)],
     ),
   ) as ExtractMetaThemeValues<T>;
+}
+
+/**
+ * Private utility to resolve references within the theme object by:
+ * 1. Traversing each token group.
+ * 2. Identifying token values starting with `var(--p-`.
+ * 3. Replacing these values with a getter function that returns the resolved value.
+ * 4. Returning the mutated theme object.
+ */
+export function resolveThemeRefs<T extends Theme>(theme: T): T {
+  Object.keys(theme).forEach((tokenGroupName) => {
+    const tokenGroupRef = theme[tokenGroupName as keyof Theme];
+
+    Object.entries(tokenGroupRef).forEach(([tokenName, tokenValue]) => {
+      if (tokenValue.startsWith('var(--p-')) {
+        const tokenNameRef = tokenValue.slice(8, -1);
+        const tokenGroupNameRef = tokenNameRef.split('-')[0];
+
+        Object.defineProperty(tokenGroupRef, tokenName, {
+          get() {
+            return (theme as any)[tokenGroupNameRef][tokenNameRef] as string;
+          },
+          enumerable: true,
+        });
+      }
+    });
+  });
+
+  return theme;
 }
