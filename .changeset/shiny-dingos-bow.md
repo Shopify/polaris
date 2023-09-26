63d198b69a81de7b3a94c985bf4304210c456d9a
Sophie Schneider <sophie.schneider@shopify.com>
2023-09-26 13:15:01 -0400

    Merge `main` at `1c4bea3` into `next` (#10690)
    Merge `main` at `1c4bea36cc067d227d157cc4247297ae66daf7f7` into `next`
    following [this
    process](https://github.com/Shopify/polaris/issues/10594)
    
    ## Commits merged from main
    -
    https://github.com/Shopify/polaris/commit/1c4bea36cc067d227d157cc4247297ae66daf7f7
    @sam-b-rose
    -
    https://github.com/Shopify/polaris/commit/629b2ec170f31b330d3e5192ecd42444d52eb733
    @sam-b-rose
    
    ---------
    
    Co-authored-by: Sam Rose <11774595+sam-b-rose@users.noreply.github.com>

---
+++ .changeset/shiny-dingos-bow.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris': minor
+---
+
+Updated CSS custom properties with safe mapping to new border design tokens for v12
--- polaris-react/src/components/Tooltip/Tooltip.tsx
+++ polaris-react/src/components/Tooltip/Tooltip.tsx
@@ -13,7 +13,10 @@ import styles from './Tooltip.scss';
 
 export type Width = 'default' | 'wide';
 export type Padding = 'default' | Extract<SpaceScale, '4' | '400'>;
-export type BorderRadius = Extract<BorderRadiusScale, '1' | '2'>;
+export type BorderRadius = Extract<
+  BorderRadiusScale,
+  '1' | '2' | '100' | '200'
+>;
 
 export interface TooltipProps {
   /** The element that will activate to tooltip */
