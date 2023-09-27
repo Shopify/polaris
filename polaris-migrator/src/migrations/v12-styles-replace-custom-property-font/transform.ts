63df19dec716400a76ef2cf00fa7824b73e3bd07
Laura Griffee <laura@mailzone.com>
2023-09-27 12:03:58 -0600

    Finalize migration

--- polaris-migrator/src/migrations/v12-styles-replace-custom-property-font/transform.ts
+++ polaris-migrator/src/migrations/v12-styles-replace-custom-property-font/transform.ts
@@ -9,16 +9,14 @@ export default function transformer(fileInfo: FileInfo, _: API) {
 const replacementMaps = {
   '/.+/': {
     '--p-font-size-70-experimental': '--p-font-size-275',
-    // The following migration needs to be run on the next branch with the v12 launch
-    // '--p-font-size-75': '--p-font-size-300',
+    '--p-font-size-75': '--p-font-size-300',
     '--p-font-size-80-experimental': '--p-font-size-325',
     '--p-font-size-100': '--p-font-size-350',
-    // The following migrations need to be run on the next branch with the v12 launch
-    // '--p-font-size-200': '--p-font-size-400',
-    // '--p-font-size-300': '--p-font-size-500',
-    // '--p-font-size-400': '--p-font-size-600',
-    // '--p-font-size-500': '--p-font-size-750',
-    // '--p-font-size-600': '--p-font-size-900',
+    '--p-font-size-200': '--p-font-size-400',
+    '--p-font-size-300': '--p-font-size-500',
+    '--p-font-size-400': '--p-font-size-600',
+    '--p-font-size-500': '--p-font-size-750',
+    '--p-font-size-600': '--p-font-size-900',
     '--p-font-size-700': '--p-font-size-1000',
     '--p-font-line-height-075-experimental': '--p-font-line-height-300',
     '--p-font-line-height-1': '--p-font-line-height-400',
