32b4338fdbc0db8253bb851fcc55cc07e22a3747
Lo Kim <lo.kim@shopify.com>
2023-09-28 20:30:17 -0400

    Manually migrate `shadow` custom properties from v11 to v12

--- polaris-react/src/components/ShadowBevel/tests/ShadowBevel.test.tsx
+++ polaris-react/src/components/ShadowBevel/tests/ShadowBevel.test.tsx
@@ -36,7 +36,7 @@ describe('ShadowBevel', () => {
 
     expect(shadowBevel).toContainReactComponent('div', {
       style: expect.objectContaining({
-        '--pc-shadow-bevel-box-shadow-xs': 'var(--p-shadow-md)',
+        '--pc-shadow-bevel-box-shadow-xs': 'var(--p-shadow-300)',
         '--pc-shadow-bevel-border-radius-xs': 'var(--p-border-radius-300)',
         '--pc-shadow-bevel-content-xs': '""',
       }),
@@ -55,7 +55,7 @@ describe('ShadowBevel', () => {
     expect(shadowBevel).toContainReactComponent('div', {
       style: expect.objectContaining({
         '--pc-shadow-bevel-box-shadow-xs': 'none',
-        '--pc-shadow-bevel-box-shadow-sm': 'var(--p-shadow-md)',
+        '--pc-shadow-bevel-box-shadow-sm': 'var(--p-shadow-300)',
         '--pc-shadow-bevel-box-shadow-lg': 'none',
         '--pc-shadow-bevel-border-radius-xs': 'var(--p-border-radius-0)',
         '--pc-shadow-bevel-border-radius-sm': 'var(--p-border-radius-300)',
