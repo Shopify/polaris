15d8d95b76aaa018d754a3aab8f2a7f1e041b7f1
Charles Lee <charles.lee@shopify.com>
2023-10-04 16:17:50 +1100

    `Docs` Fix gen-colors script

--- polaris.shopify.com/scripts/gen-colors.ts
+++ polaris.shopify.com/scripts/gen-colors.ts
@@ -6,4 +6,5 @@ import * as colors from '../../polaris-tokens/src/colors';
 const cacheDir = path.join(process.cwd(), '.cache');
 const colorJsonFile = path.join(cacheDir, 'colors.json');
 
+fs.mkdirSync(cacheDir, {recursive: true});
 fs.writeFileSync(colorJsonFile, JSON.stringify(colors, null, 2));
