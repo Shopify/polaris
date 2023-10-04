2471ce9dd962787d20017212f866a9968aa2ff9a
Sam Rose <11774595+sam-b-rose@users.noreply.github.com>
2023-10-04 11:38:54 -0400

    Add support for `@custom-media` for using breakpoints in CSS (#10804)
    Adds support for `@custom-media` using the
    [postcss-global-data](https://github.com/csstools/postcss-plugins/blob/main/plugins/postcss-global-data/README.md)
    and
    [postcss-custom-media](https://github.com/csstools/postcss-plugins/blob/main/plugins/postcss-custom-media/README.md)
    plugins.
    
    See `Text` component example of how this will migrate our breakpoints
    away from the current SCSS method:
    
    <img width="630" alt="Screenshot 2023-10-02 at 3 05 03 PM"
    src="https://github.com/Shopify/polaris/assets/11774595/613b1f32-eb96-4a14-8dc8-188536493efe">

--- polaris-react/config/postcss-plugins.js
+++ polaris-react/config/postcss-plugins.js
@@ -1,7 +1,20 @@
+const path = require('path');
+
 const postcssShopify = require('@shopify/postcss-plugin');
 const pxtorem = require('postcss-pxtorem');
+const postcssCustomMedia = require('postcss-custom-media');
+const postcssGlobalData = require('@csstools/postcss-global-data');
+
+const mediaQueriesCssPath = path.resolve(
+  __dirname,
+  '../../node_modules/@shopify/polaris-tokens/dist/css/media-queries.css',
+);
 
 module.exports = [
+  postcssGlobalData({
+    files: [mediaQueriesCssPath],
+  }),
+  postcssCustomMedia(),
   postcssShopify,
   pxtorem({
     rootValue: 16,
