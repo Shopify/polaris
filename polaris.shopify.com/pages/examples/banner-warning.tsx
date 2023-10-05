0c56fd9da018526ef5f06560756207bf5850a955
Jess Telford <jess.telford@shopify.com>
2023-10-05 13:16:05 +1100

    Revert "Fix Banner hydration error (#10882)"
    This reverts commit 16ba0a422cd53787bd307256f70474441c33dc2a.

--- polaris.shopify.com/pages/examples/banner-warning.tsx
+++ polaris.shopify.com/pages/examples/banner-warning.tsx
@@ -1,25 +1,6 @@
-import {List} from '@shopify/polaris';
-import dynamic from 'next/dynamic';
+import {Banner, List} from '@shopify/polaris';
 import React from 'react';
 import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
-
-/**
- * For some reason when the Banner component is hydrated on the client the css
- * variables are incorrect, causing the top border-radius of the component to
- * not get set correctly. Disabling SSR via the documentation below resolves
- * this hydration issue, but I'm not sure what the root cause of this problem is.
- * This is a temporary fix until we can dig into things furthur
- *
- * To see the hydration error, remove lines 17-20 and import the Banner component
- * as you normally would. Then load the page with the banner component example
- * and open up the dev console.
- *
- * https://nextjs.org/docs/messages/react-hydration-error#solution-2-disabling-ssr-on-specific-components
- */
-const Banner = dynamic(
-  () => import('@shopify/polaris').then((polaris) => polaris.Banner),
-  {ssr: false},
-);
 
 function BannerExample() {
   return (
