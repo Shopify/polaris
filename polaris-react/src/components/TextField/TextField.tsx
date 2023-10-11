3f92d7021487724544b5ad6ef7d0c8c3a0572e54
Carlos Matallin <carlos.matallin@shopify.com>
2023-10-11 20:15:19 +0000

    [TextField] Add styles for AI generated values

--- polaris-react/src/components/TextField/TextField.tsx
+++ polaris-react/src/components/TextField/TextField.tsx
@@ -52,6 +52,8 @@ type InputMode =
   | 'email'
   | 'url';
 
+type Tone = 'magic';
+
 interface SelectSuggestion {
   suggestion?: string;
 }
@@ -175,6 +177,8 @@ interface NonMutuallyExclusiveProps {
   onFocus?: (event?: React.FocusEvent) => void;
   /** Callback fired when input is blurred */
   onBlur?(event?: React.FocusEvent): void;
+  /** Indicates the tone of the text field */
+  tone?: Tone;
 }
 
 export type MutuallyExclusiveSelectionProps =
@@ -241,6 +245,7 @@ export function TextField({
   onSpinnerChange,
   onFocus,
   onBlur,
+  tone,
 }: TextFieldProps) {
   const i18n = useI18n();
   const [height, setHeight] = useState<number | null>(null);
@@ -290,6 +295,7 @@ export function TextField({
     disabled && styles.disabled,
     readOnly && styles.readOnly,
     error && styles.error,
+    tone && tone === 'magic' && styles.magic,
     multiline && styles.multiline,
     focus && !disabled && styles.focus,
     variant !== 'inherit' && styles[variant],
