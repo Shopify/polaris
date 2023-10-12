c644ef52a2db81f00b064c90baee6d1d523ad28a
Yuraima Estevez <yuraima.estevez@shopify.com>
2023-10-12 16:20:59 -0500

    refactor and make it accessible

--- polaris.shopify.com/src/components/ComponentExamples/ComponentExamples.tsx
+++ polaris.shopify.com/src/components/ComponentExamples/ComponentExamples.tsx
@@ -1,11 +1,15 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import type {SerializedMdx} from '../../types';
 import styles from './ComponentExamples.module.scss';
 import CodesandboxButton from '../CodesandboxButton';
+import {className as classNames} from '../../utils/various';
 import Code from '../Code';
 import {Tab} from '@headlessui/react';
 import {className} from '../../utils/various';
 import Markdown from '../Markdown';
+import {useRouter} from 'next/router';
+import {useSearchParams} from 'next/navigation';
+import style from 'react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark';
 
 const exampleIframeId = 'example-iframe';
 const iframePadding = 192;
@@ -49,9 +53,10 @@ function formatHTML(html: string): string {
 }
 
 const ComponentExamples = ({examples}: Props) => {
-  const [selectedIndex, setSelectedIndex] = useState(0);
   const [htmlCode, setHTMLCode] = useState('');
   const [iframeHeight, setIframeHeight] = useState(400);
+  const router = useRouter();
+  const params = useSearchParams();
 
   const handleExampleLoad = () => {
     let attempts = 0;
@@ -81,61 +86,165 @@ const ComponentExamples = ({examples}: Props) => {
     return () => clearInterval(waitForExampleContentToRender);
   };
 
+  const defaultExampleName = examples[0].fileName.replace('.tsx', '');
+  let exampleName = params.get('example') || defaultExampleName;
+  let exampleFilename =
+    (exampleName ? exampleName : defaultExampleName) + '.tsx';
+  let exampleIndex = examples.findIndex(
+    (obj) => obj.fileName === exampleFilename,
+  );
+
+  const updateSelectedExample = (index: number) => {
+    exampleName = examples[index].fileName.replace('.tsx', '');
+    router.replace(
+      {
+        pathname: router.pathname,
+        query: {...router.query, example: exampleName},
+      },
+      undefined,
+      {shallow: true},
+    );
+  };
+
+  const examplesMarkup = examples.map(
+    ({fileName, description, code}, index) => {
+      const exampleUrl = `/examples/${fileName.replace('.tsx', '')}`;
+      const isSelected = index === exampleIndex ? true : false;
+      const classes = classNames(
+        styles.Tab,
+        isSelected ? styles.selected : undefined,
+      );
+
+      return (
+        <div
+          id={`tabpanel-${index}`}
+          key={fileName}
+          role="tabpanel"
+          tabIndex={0}
+          className={classes}
+          aria-labelledby={`tab-${index}`}
+        >
+          {description ? <Markdown {...description} /> : null}
+          <div className={styles.ExampleFrame}>
+            <iframe
+              src={exampleUrl}
+              height={iframeHeight}
+              onLoad={handleExampleLoad}
+              id={exampleIframeId}
+            />
+            <div className={className(styles.Buttons, 'light-mode')}>
+              <CodesandboxButton
+                className={styles.CodesandboxButton}
+                code={code}
+              />
+            </div>
+          </div>
+
+          <Code
+            code={[
+              {title: 'React', code: code.trim()},
+              {title: 'HTML', code: htmlCode},
+            ]}
+          />
+        </div>
+      );
+    },
+  );
+
+  const tablistId = 'component-examples-tablist';
+
+  const examplesLength = examples.length - 1;
+
+  const setSelectedToPreviousTab = (event) => {
+    if (exampleIndex === 0) {
+      setSelectedTab(examplesLength);
+    } else {
+      const index = exampleIndex - 1;
+      setSelectedTab(index);
+    }
+  };
+
+  const setSelectedToNextTab = (event) => {
+    if (exampleIndex === examplesLength) {
+      setSelectedTab(0);
+    } else {
+      const index = exampleIndex + 1;
+      setSelectedTab(index);
+    }
+  };
+
+  const setSelectedTab = (index: number) => {
+    exampleIndex = index;
+    updateSelectedExample(index);
+  };
+
+  const onKeyDownHandler = (event) => {
+    switch (event.key) {
+      case 'ArrowLeft':
+        setSelectedToPreviousTab(event);
+        break;
+
+      case 'ArrowRight':
+        setSelectedToNextTab(event);
+        break;
+
+      case 'Home':
+        setSelectedTab(0);
+        break;
+
+      case 'End':
+        setSelectedTab(examplesLength);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const buttonRefs = useRef([]);
+
   useEffect(() => {
-    setSelectedIndex(0);
+    buttonRefs.current = buttonRefs.current.slice(0, examples.length);
   }, [examples]);
 
+  useEffect(() => {
+    const currentButtonRef = buttonRefs.current[exampleIndex];
+    if (currentButtonRef) {
+      currentButtonRef.focus();
+    }
+  }, [exampleIndex]);
+
+  const tabButtons = examples.map((example, index) => {
+    return (
+      <button
+        id={`tab-${index}`}
+        key={example.fileName}
+        onClick={() => updateSelectedExample(index)}
+        aria-controls={`tabpanel-${index}`}
+        tabIndex={index === exampleIndex ? 0 : -1}
+        ref={(el) => (buttonRefs.current[index] = el)}
+        {...(index === exampleIndex ? {'aria-selected': 'true'} : {})}
+      >
+        <span>{example.title}</span>
+      </button>
+    );
+  });
+
   return (
-    <Tab.Group
-      defaultIndex={0}
-      selectedIndex={selectedIndex}
-      onChange={setSelectedIndex}
-    >
-      <Tab.List>
-        <div className={styles.ExamplesList} id="examples">
-          {examples.map((example) => {
-            return (
-              <Tab key={example.fileName}>
-                <span>{example.title}</span>
-              </Tab>
-            );
-          })}
+    <>
+      <h3 id={tablistId}>Component examples</h3>
+      <div className="tabs" aria-labelledby={tablistId}>
+        <div
+          className={styles.ExamplesList}
+          id="examples"
+          role="tablist"
+          onKeyDown={onKeyDownHandler}
+        >
+          {tabButtons}
         </div>
-      </Tab.List>
-
-      <Tab.Panels>
-        {examples.map(({fileName, description, code}) => {
-          const exampleUrl = `/examples/${fileName.replace('.tsx', '')}`;
-
-          return (
-            <Tab.Panel key={fileName}>
-              {description ? <Markdown {...description} /> : null}
-              <div className={styles.ExampleFrame}>
-                <iframe
-                  src={exampleUrl}
-                  height={iframeHeight}
-                  onLoad={handleExampleLoad}
-                  id={exampleIframeId}
-                />
-                <div className={className(styles.Buttons, 'light-mode')}>
-                  <CodesandboxButton
-                    className={styles.CodesandboxButton}
-                    code={code}
-                  />
-                </div>
-              </div>
-
-              <Code
-                code={[
-                  {title: 'React', code: code.trim()},
-                  {title: 'HTML', code: htmlCode},
-                ]}
-              />
-            </Tab.Panel>
-          );
-        })}
-      </Tab.Panels>
-    </Tab.Group>
+      </div>
+      {examplesMarkup[exampleIndex]}
+    </>
   );
 };
 
