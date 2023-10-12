f4bb85961c310eaaa59e440aba8def332f5543ea
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2023-10-12 15:03:52 -0700

    Update tests

--- polaris-migrator/src/migrations/react-update-component-prop/tests/with-replacement-maps.input.tsx
+++ polaris-migrator/src/migrations/react-update-component-prop/tests/with-replacement-maps.input.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-// @ts-expect-error
-import {MyComponent1, MyComponent2 as CustomComponent} from '@shopify/polaris';
+import {
+  // @ts-expect-error
+  MyComponentA,
+  // @ts-expect-error
+  MyComponentB as MyComponentBAlias,
+} from '@shopify/polaris';
 
 declare function Child(props: any): JSX.Element;
 
-const MyComponentWrapper = MyComponent1;
+const MyComponentAWrapper = MyComponentA;
 
 export function App() {
   return (
     <>
-      <MyComponent1 prop1="value-1" foo="bar">
+      <MyComponentA prop1="value-1" prop2 foo="bar">
         Hello
-        <Child prop1="value-1" />
-        <MyComponentWrapper />
-      </MyComponent1>
-      <CustomComponent prop2="value-2" foo="bar">
+        <Child prop1="value-1" prop2 />
+        <MyComponentAWrapper />
+      </MyComponentA>
+      <MyComponentBAlias prop3="value-3" foo="bar">
         Hello
-        <Child prop2="value-2" />
-        <MyComponentWrapper />
-      </CustomComponent>
+        <Child prop3="value-3" />
+        <MyComponentAWrapper />
+      </MyComponentBAlias>
     </>
   );
 }
