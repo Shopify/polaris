ddf045f51720541ddf432369edc34f306c683d6a
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2023-10-13 12:10:41 -0700

    Optimize the implementation to perform a single AST traversal

--- polaris-migrator/src/migrations/react-update-component-prop/transform.ts
+++ polaris-migrator/src/migrations/react-update-component-prop/transform.ts
@@ -85,23 +85,77 @@ export default function transformer(
     return;
   }
 
-  for (const replacementMapEntry of Object.entries(replacementMaps)) {
-    const [componentName, replacementOptions] = replacementMapEntry;
+  interface LocalElementConfig {
+    componentNames: string[];
+    replacementOptions: ReplacementOptions[];
+  }
 
-    const componentNames = componentName.split('.');
-    const targetComponentName = componentNames[0];
+  const localElementConfigs = Object.fromEntries(
+    Object.entries(replacementMaps)
+      .map((replacementMapEntry): null | [string, LocalElementConfig] => {
+        const [componentName, replacementOptions] = replacementMapEntry;
 
-    const sourcePaths = normalizeImportSourcePaths(j, source, {
-      relative: options.relative,
-      from: targetComponentName,
-      to: targetComponentName,
-    });
+        const componentNames = componentName.split('.');
+        const targetComponentName = componentNames[0];
+
+        const sourcePaths = normalizeImportSourcePaths(j, source, {
+          relative: options.relative,
+          from: targetComponentName,
+          to: targetComponentName,
+        });
+
+        if (
+          !sourcePaths ||
+          !hasImportSpecifier(j, source, targetComponentName, sourcePaths.from)
+        ) {
+          return null;
+        }
+
+        const localElementName =
+          getImportSpecifierName(
+            j,
+            source,
+            targetComponentName,
+            sourcePaths.from,
+          ) || targetComponentName;
+
+        componentNames[0] = localElementName;
+
+        return [
+          localElementName,
+          {
+            componentNames,
+            replacementOptions,
+          },
+        ];
+      })
+      .filter(Boolean) as [string, LocalElementConfig][],
+  );
+
+  source.find(j.JSXElement).forEach((element) => {
+    const elementNames = getElementNames(element.node.openingElement.name);
+
+    const localElementConfig = localElementConfigs[elementNames[0]];
+
+    if (!localElementConfig) return;
+
+    const {componentNames, replacementOptions} = localElementConfig;
 
     if (
-      !sourcePaths ||
-      !hasImportSpecifier(j, source, targetComponentName, sourcePaths.from)
+      elementNames.length !== componentNames.length ||
+      !componentNames.every((name, index) => name === elementNames[index])
     ) {
-      continue;
+      return;
+    }
+
+    const allAttributes = element.node.openingElement.attributes ?? [];
+
+    if (
+      // Early exit on spread operators
+      allAttributes.some((attribute) => attribute.type !== 'JSXAttribute')
+    ) {
+      insertJSXComment(j, element, POLARIS_MIGRATOR_COMMENT);
+      return;
     }
 
     for (const replacementOption of replacementOptions) {
@@ -112,117 +166,81 @@ export default function transformer(
         fromValue,
         toValue,
       } = replacementOption;
+      const jsxAttributes = allAttributes as JSXAttribute[];
 
-      const localElementName =
-        getImportSpecifierName(
-          j,
-          source,
-          targetComponentName,
-          sourcePaths.from,
-        ) || targetComponentName;
+      const fromPropAttribute = jsxAttributes.find(
+        (attribute) => attribute.name.name === fromProp,
+      );
 
-      componentNames[0] = localElementName;
+      if (
+        fromPropAttribute &&
+        (fromPropType === 'boolean'
+          ? fromPropAttribute.value !== null
+          : fromPropAttribute.value?.type !== 'StringLiteral')
+      ) {
+        insertJSXComment(j, element, POLARIS_MIGRATOR_COMMENT);
+        return;
+      }
 
-      source.find(j.JSXElement).forEach((element) => {
+      jsxAttributes.forEach((jsxAttribute) => {
         if (
-          element.node.openingElement.name.type === 'JSXIdentifier' &&
-          element.node.openingElement.name.name !== localElementName
+          !(
+            jsxAttribute.type === 'JSXAttribute' &&
+            jsxAttribute.name.name === fromProp
+          )
+        ) {
+          return jsxAttribute;
+        }
+
+        const attributeValueValue =
+          jsxAttribute.value?.type === 'StringLiteral'
+            ? jsxAttribute.value.value
+            : null;
+
+        if (
+          toProp &&
+          toProp !== fromProp &&
+          (!fromValue || fromValue === attributeValueValue)
+        ) {
+          jsxAttribute.name.name = toProp;
+        }
+
+        if (fromValue && fromValue !== attributeValueValue) return jsxAttribute;
+
+        jsxAttribute.value = toValue
+          ? j.stringLiteral(toValue)
+          : jsxAttribute.value;
+      });
+    }
+  });
+
+  Object.keys(localElementConfigs).forEach((localElementName) => {
+    source
+      .find(j.Identifier)
+      .filter((path) => path.node.name === localElementName)
+      .forEach((path) => {
+        if (path.node.type !== 'Identifier') return;
+
+        if (
+          path.parent.value.type === 'ImportSpecifier' ||
+          path.parent.value.type === 'MemberExpression'
         ) {
           return;
         }
 
-        const nameChain = getNameChain(element.node.openingElement.name);
-
-        if (
-          nameChain.length === componentNames.length &&
-          componentNames.every((name, index) => name === nameChain[index])
-        ) {
-          const allAttributes = element.node.openingElement.attributes ?? [];
-
-          if (
-            // Early exit on spread operators
-            allAttributes.some((attribute) => attribute.type !== 'JSXAttribute')
-          ) {
-            insertJSXComment(j, element, POLARIS_MIGRATOR_COMMENT);
-            return;
-          }
-
-          const jsxAttributes = allAttributes as JSXAttribute[];
-
-          const fromPropAttribute = jsxAttributes.find(
-            (attribute) => attribute.name.name === fromProp,
-          );
-
-          if (
-            fromPropAttribute &&
-            (fromPropType === 'boolean'
-              ? fromPropAttribute.value !== null
-              : fromPropAttribute.value?.type !== 'StringLiteral')
-          ) {
-            insertJSXComment(j, element, POLARIS_MIGRATOR_COMMENT);
-            return;
-          }
-
-          jsxAttributes.forEach((jsxAttribute) => {
-            if (
-              !(
-                jsxAttribute.type === 'JSXAttribute' &&
-                jsxAttribute.name.name === fromProp
-              )
-            ) {
-              return jsxAttribute;
-            }
-
-            const attributeValueValue =
-              jsxAttribute.value?.type === 'StringLiteral'
-                ? jsxAttribute.value.value
-                : null;
-
-            if (
-              toProp &&
-              toProp !== fromProp &&
-              (!fromValue || fromValue === attributeValueValue)
-            ) {
-              jsxAttribute.name.name = toProp;
-            }
-
-            if (fromValue && fromValue !== attributeValueValue)
-              return jsxAttribute;
-
-            jsxAttribute.value = toValue
-              ? j.stringLiteral(toValue)
-              : jsxAttribute.value;
-          });
-        }
+        insertCommentBefore(j, path, POLARIS_MIGRATOR_COMMENT);
       });
-
-      source
-        .find(j.Identifier)
-        .filter((path) => path.node.name === localElementName)
-        .forEach((path) => {
-          if (path.node.type !== 'Identifier') return;
-
-          if (
-            path.parent.value.type === 'ImportSpecifier' ||
-            path.parent.value.type === 'MemberExpression'
-          ) {
-            return;
-          }
-
-          insertCommentBefore(j, path, POLARIS_MIGRATOR_COMMENT);
-        });
-    }
-  }
+  });
 
   return source.toSource();
 }
 
-function getNameChain(name: JSXOpeningElement['name']): string[] {
+function getElementNames(name: JSXOpeningElement['name']): string[] {
   if (name.type === 'JSXIdentifier') {
     return [name.name];
   }
   if (name.type === 'JSXMemberExpression') {
-    return [...getNameChain(name.object), name.property.name];
+    return [...getElementNames(name.object), name.property.name];
   }
   return [];
 }
