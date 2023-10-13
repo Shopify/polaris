78b534831c4509880e551fffedb3af8fd1cd3c4c
Oksana Azarova <104942025+oksanashopify@users.noreply.github.com>
2023-10-13 12:31:26 -0600

    fixed background-color for unselectable tabel  first-child cell (#10960)
    ### WHY are these changes introduced?
    After @shopify/polaris@12.0.0 [has been
    shipped](https://shopify.slack.com/archives/C4Y8N30KD/p1696832186682759)
    I noticed that on `main` Polaris branch IndexTable with Sub Headers does
    not have proper `background-color` for unselectable table first-child
    cell.
    <img width="1498" alt="image"
    src="https://github.com/Shopify/polaris/assets/104942025/e9d0a920-c74b-4b1f-baf6-204d6d8f7144">
    
    
    
    ### WHAT is this pull request doing?
    
    I added new story "With Sub Headers Non Selectable" and fixed styles in
    this PR
    
    How it was before:
    
    ![image](https://github.com/Shopify/polaris/assets/104942025/3a69331f-3b67-49aa-8ea6-bf4bbd43b1f2)
    
    
    How it looks now:
    <img width="1501" alt="image"
    src="https://github.com/Shopify/polaris/assets/104942025/0e315958-467d-4643-84a4-dda88b7cff2a">
    
    [Storybook
    link](https://5d559397bae39100201eedc1-kabdrxeayi.chromatic.com/?path=/story/all-components-indextable--with-sub-headers-non-selectable)
    
    
    <!-- ℹ️ Delete the following for small / trivial changes -->
    
    ### How to 🎩
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    <!--
      Give as much information as needed to experiment with the component
      in the playground.
    -->
    
    <details>
    <summary>Copy-paste this code in
    <code>playground/Playground.tsx</code>:</summary>
    
    ```jsx
    import React from 'react';
    import {Page} from '../src';
    
    export function Playground() {
      return (
        <Page title="Playground">
          {/* Add the code you want to test in here */}
        </Page>
      );
    }
    ```
    
    </details>
    
    ### 🎩 checklist
    
    - [x] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [x] N/A Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [x] N/A Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [x] N/A Updated the component's `README.md` with documentation changes
    - [x] N/A [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris-react/src/components/IndexTable/IndexTable.stories.tsx
+++ polaris-react/src/components/IndexTable/IndexTable.stories.tsx
@@ -4002,3 +4002,213 @@ export function WithSubHeaders() {
     </LegacyCard>
   );
 }
+
+export function WithSubHeadersNonSelectable() {
+  const rows = [
+    {
+      id: '3411',
+      url: '#',
+      name: 'Mae Jemison',
+      location: 'Decatur, USA',
+      orders: 11,
+      amountSpent: '$2,400',
+      lastOrderDate: 'May 31, 2023',
+    },
+    {
+      id: '2562',
+      url: '#',
+      name: 'Ellen Ochoa',
+      location: 'Los Angeles, USA',
+      orders: 30,
+      amountSpent: '$975',
+      lastOrderDate: 'May 31, 2023',
+    },
+    {
+      id: '4102',
+      url: '#',
+      name: 'Colm Dillane',
+      location: 'New York, USA',
+      orders: 27,
+      amountSpent: '$2885',
+      lastOrderDate: 'May 31, 2023',
+    },
+    {
+      id: '2564',
+      url: '#',
+      name: 'Al Chemist',
+      location: 'New York, USA',
+      orders: 19,
+      amountSpent: '$1,209',
+      lastOrderDate: 'April 4, 2023',
+      disabled: true,
+    },
+    {
+      id: '2563',
+      url: '#',
+      name: 'Larry June',
+      location: 'San Francisco, USA',
+      orders: 22,
+      amountSpent: '$1,400',
+      lastOrderDate: 'March 19, 2023',
+    },
+  ];
+
+  const columnHeadings = [
+    {title: 'Name', id: 'column-header--name'},
+    {title: 'Location', id: 'column-header--location'},
+    {
+      alignment: 'end',
+      id: 'column-header--order-count',
+      title: 'Order count',
+    },
+    {
+      alignment: 'end',
+      hidden: false,
+      id: 'column-header--amount-spent',
+      title: 'Amount spent',
+    },
+  ];
+
+  const groupRowsBy = (groupKey: string, resolveId: (groupVal) => string) => {
+    let position = -1;
+    const groups = rows.reduce((groups, customer) => {
+      const groupVal = customer[groupKey];
+      if (!groups[groupVal]) {
+        position += 1;
+
+        groups[groupVal] = {
+          position,
+          customers: [],
+          id: resolveId(groupVal),
+        };
+      }
+
+      groups[groupVal].customers.push({
+        ...customer,
+        position: position + 1,
+      });
+
+      position += 1;
+      return groups;
+    }, {});
+
+    return groups;
+  };
+
+  const resourceName = {
+    singular: 'customer',
+    plural: 'customers',
+  };
+
+  const {selectedResources, allResourcesSelected, handleSelectionChange} =
+    useIndexResourceState(rows, {resourceFilter: ({disabled}) => !disabled});
+
+  const orders = groupRowsBy(
+    'lastOrderDate',
+    (date) => `last-order-date--${date.replace(',', '').split(' ').join('-')}`,
+  );
+
+  const rowMarkup = Object.keys(orders).map((orderDate, index) => {
+    const {customers, position, id: subheaderId} = orders[orderDate];
+    let selected: IndexTableRowProps['selected'] = false;
+
+    const someCustomersSelected = customers.some(({id}) =>
+      selectedResources.includes(id),
+    );
+
+    const allCustomersSelected = customers.every(({id}) =>
+      selectedResources.includes(id),
+    );
+
+    if (allCustomersSelected) {
+      selected = true;
+    } else if (someCustomersSelected) {
+      selected = 'indeterminate';
+    }
+
+    const selectableRows = rows.filter(({disabled}) => !disabled);
+    const rowRange: IndexTableRowProps['selectionRange'] = [
+      selectableRows.findIndex((row) => row.id === customers[0].id),
+      selectableRows.findIndex(
+        (row) => row.id === customers[customers.length - 1].id,
+      ),
+    ];
+
+    const disabled = customers.every(({disabled}) => disabled);
+
+    return (
+      <Fragment key={subheaderId}>
+        <IndexTable.Row
+          rowType="subheader"
+          selectionRange={rowRange}
+          id={`Subheader-${index}`}
+          position={position}
+          selected={selected}
+          disabled={disabled}
+          accessibilityLabel={`Select all customers whose last order was placed on ${orderDate}`}
+        >
+          <IndexTable.Cell scope="col" as="th" id={subheaderId}>
+            {`Last order placed: ${orderDate}`}
+          </IndexTable.Cell>
+          <IndexTable.Cell as="th" />
+          <IndexTable.Cell as="th" />
+          <IndexTable.Cell as="th" />
+        </IndexTable.Row>
+        {customers.map(
+          (
+            {id, name, location, orders, amountSpent, position, disabled},
+            rowIndex,
+          ) => {
+            return (
+              <IndexTable.Row
+                key={rowIndex}
+                id={id}
+                position={position}
+                selected={selectedResources.includes(id)}
+                disabled={disabled}
+              >
+                <IndexTable.Cell
+                  as="th"
+                  scope="row"
+                  headers={`${columnHeadings[0].id} ${subheaderId}`}
+                >
+                  <Text variant="bodyMd" fontWeight="semibold" as="span">
+                    {name}
+                  </Text>
+                </IndexTable.Cell>
+                <IndexTable.Cell>{location}</IndexTable.Cell>
+                <IndexTable.Cell>
+                  <Text as="span" alignment="end" numeric>
+                    {orders}
+                  </Text>
+                </IndexTable.Cell>
+                <IndexTable.Cell>
+                  <Text as="span" alignment="end" numeric>
+                    {amountSpent}
+                  </Text>
+                </IndexTable.Cell>
+              </IndexTable.Row>
+            );
+          },
+        )}
+      </Fragment>
+    );
+  });
+
+  return (
+    <LegacyCard>
+      <IndexTable
+        onSelectionChange={handleSelectionChange}
+        selectedItemsCount={
+          allResourcesSelected ? 'All' : selectedResources.length
+        }
+        resourceName={resourceName}
+        itemCount={rows.length}
+        headings={columnHeadings as IndexTableProps['headings']}
+        selectable={false}
+      >
+        {rowMarkup}
+      </IndexTable>
+    </LegacyCard>
+  );
+}
