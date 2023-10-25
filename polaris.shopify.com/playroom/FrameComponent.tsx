72f63e401089d232484d37687a4845492b9f08ba
Jess Telford <jess.telford@shopify.com>
2023-10-25 21:44:39 +1100

    [Sandbox] Only render sandbox preview when within sandboxed iframe.

--- polaris.shopify.com/playroom/FrameComponent.tsx
+++ polaris.shopify.com/playroom/FrameComponent.tsx
@@ -38,6 +38,14 @@ const mockFrameContext = {
   },
 };
 
+function inIframe() {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+}
+
 export default function FrameComponent({
   theme = enTranslations,
   children,
@@ -47,6 +55,9 @@ export default function FrameComponent({
 }) {
   const [features, setFeatures] =
     useState<React.ComponentProps<typeof AppProvider>['features']>();
+  // Don't render anything until we've done our client-side check for iframe
+  // security.
+  const [content, setContent] = useState(null);
   const wrapperRef = useRef<HTMLDivElement | null>(null);
 
   // Tell parent frame of the initial size on first render
@@ -86,11 +97,35 @@ export default function FrameComponent({
     }
   }, []);
 
+  // Don't render anything on the server, only render on the client.
+  useEffect(() => {
+    // window.origin is set to "null" when the page is loaded in a sandboxed iframe without access to the same origin
+    if (window.origin === 'null') {
+      setContent(children);
+    } else {
+      if (!inIframe()) {
+        // Raw window, not embedded in an iframe at all, so redirect to the correct page (useful for old links before we enforced iframe sandboxing)
+        const url = new URL(window.location.href);
+        url.pathname = '/sandbox/preview';
+        window.location.replace(url.toString());
+      } else if (window.frameElement) {
+        // Embedded on our own domain, but with insecure sandbox settings (frameElement only has a value on same-origin iframes).
+        console.error(
+          'Refusing to load example on same origin with allow-same-origin enabled. Hint: ensure the iframe has a "sandbox" attribute WITHOUT "allow-same-origin" set.',
+        );
+      } else {
+        // Embedded on a cross-origin domain, so throw an error
+        console.error('Unable to load example on cross-origin domain');
+      }
+      setContent(null);
+    }
+  }, [children]);
+
   return (
     <AppProvider i18n={theme || enTranslations} features={features}>
       <FrameContext.Provider value={mockFrameContext}>
         <div id="polaris-sandbox-wrapper" ref={wrapperRef}>
-          {children}
+          {content}
         </div>
       </FrameContext.Provider>
     </AppProvider>
