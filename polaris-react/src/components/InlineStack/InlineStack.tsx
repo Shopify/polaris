4e30c4163fcdfa9d17838a4f1c4ec5196665d2df
Lo Kim <lo.kim@shopify.com>
2023-10-30 11:01:31 -0400

    [InlineStack] Update example for `gap` prop (#11057)
    ### WHY are these changes introduced?
    
    Updates examples for `gap` prop to use values that align with updated
    space tokens.
    
    ### WHAT is this pull request doing?
    
    <!--
      Summary of the changes committed.
    
    Before / after screenshots are appreciated for UI changes. Make sure to
    include alt text that describes the screenshot.
    
    If you include an animated gif showing your change, wrapping it in a
    details tag is recommended. Gifs usually autoplay, which can cause
    accessibility issues for people reviewing your PR:
    
        <details>
          <summary>Summary of your gif(s)</summary>
          <img src="..." alt="Description of what the gif shows">
        </details>
    -->
    
    <!-- ℹ️ Delete the following for small / trivial changes -->
    
    ### How to 🎩
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    <!--
      Give as much information as needed to experiment with the component
      in the playground.
    -->
    
    <details>
    <summary>Copy-paste this code in
    <code>playground/Playground.tsx</code>:</summary>
    
    ```jsx
    import React from 'react';
    import {Page} from '../src';
    
    export function Playground() {
      return (
        <Page title="Playground">
          {/* Add the code you want to test in here */}
        </Page>
      );
    }
    ```
    
    </details>
    
    ### 🎩 checklist
    
    - [ ] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [ ] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [ ] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [ ] Updated the component's `README.md` with documentation changes
    - [ ] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris-react/src/components/InlineStack/InlineStack.tsx
+++ polaris-react/src/components/InlineStack/InlineStack.tsx
@@ -25,8 +25,8 @@ export interface InlineStackProps extends React.AriaAttributes {
   blockAlign?: BlockAlign;
   /** The spacing between elements. Accepts a spacing token or an object of spacing tokens for different screen sizes.
    * @example
-   * gap='2'
-   * gap={{xs: '2', sm: '3', md: '4', lg: '5', xl: '6'}}
+   * gap='200'
+   * gap={{xs: '200', sm: '300', md: '400', lg: '500', xl: '600'}}
    */
   gap?: Gap;
   /** Wrap stack elements to additional rows as needed on small screens
