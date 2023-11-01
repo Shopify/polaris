00868639e32294bb44ab03ceb5ca5d10cc7710fc
Charles Lee <charles.lee@shopify.com>
2023-11-01 15:25:27 +1100

    update new Box styles and port over css utilities

--- polaris.shopify.com/src/components/Cube/index.tsx
+++ polaris.shopify.com/src/components/Cube/index.tsx
@@ -1,14 +1,22 @@
 import style from './style.module.scss';
 import {PropsWithChildren} from 'react';
+import decamelize from 'decamelize';
 import * as CSS from 'csstype';
+import {getResponsiveProps} from '../../utils/various';
 
 function getCssProps(styleProps: Record<string, any>) {
   // camelCase to kebabCase of styleProps
   // intiailise responsive values from valid styleProps
-  return {};
+  return Object.entries(styleProps).reduce((acc, [key, value]) => {
+    const decamelizedPropKey = decamelize(key, {separator: '-'});
+    return {
+      ...getResponsiveProps('box', decamelizedPropKey, null, value),
+      ...acc,
+    };
+  }, {});
 }
 
-export function Box({
+export function Cube({
   children,
   ...styleProps
 }: PropsWithChildren<CSS.Properties & {as?: Element}>) {
