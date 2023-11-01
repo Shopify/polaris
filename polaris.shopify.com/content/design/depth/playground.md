856ec326216917c28b37425391e2ce0270324c13
Charles Lee <charles.lee@shopify.com>
2023-11-01 15:37:14 +1100

    add box playground

---
+++ polaris.shopify.com/content/design/depth/playground.md
@@ -0,0 +1,7 @@
+---
+title: Box playground
+icon: VariantMajor
+showTOC: true
+---
+
+<Cube gap="400">Hello</Cube>
--- polaris.shopify.com/src/components/Markdown/Markdown.tsx
+++ polaris.shopify.com/src/components/Markdown/Markdown.tsx
@@ -26,6 +26,7 @@ import {DoDont, Do, Dont} from './components/DoDont';
 import {Heading} from '../../components/Heading';
 import PatternsExample from '../../components/PatternsExample';
 import {Card} from '../../components/Card';
+import {Cube} from '../../components/Cube';
 import RichCardGrid from '../RichCardGrid';
 import Tooltip from '../Tooltip';
 import Icon from '../Icon';
