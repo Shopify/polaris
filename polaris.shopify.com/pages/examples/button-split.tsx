ba4123bf0f915eca0692828f85821953c60a0984
Chloe Rice <chloerice@users.noreply.github.com>
2023-10-31 21:22:46 -0400

    [Button] Fix tertiary button focus state (#11075)
    ### WHAT is this pull request doing?
    
    Fixes: 
    
    - Split button example in the website Button docs
    - Tertiary icon only Button focus state having unwanted box-shadow
    
    |Before|After|
    |---|---|
    |<img width="53" alt="Screenshot 2023-10-31 at 8 27 13 PM"
    src="https://github.com/Shopify/polaris/assets/18447883/7ab08f63-3382-4249-9acc-3c10e03515c6">|<img
    width="34" alt="Screenshot 2023-10-31 at 8 23 11 PM"
    src="https://github.com/Shopify/polaris/assets/18447883/635d1da0-777a-4e75-ab1d-ed7b579482b5">|
    
    ### How to 🎩
    
    🖥 [Local development
    instructions](https://github.com/Shopify/polaris/blob/main/README.md#local-development)
    🗒 [General tophatting
    guidelines](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md)
    📄 [Changelog
    guidelines](https://github.com/Shopify/polaris/blob/main/.github/CONTRIBUTING.md#changelog)
    
    <!--
      Give as much information as needed to experiment with the component
      in the playground.
    -->
    
    <details>
    <summary>Copy-paste this code in
    <code>playground/Playground.tsx</code>:</summary>
    
    ```jsx
    import React from 'react';
    import {Page} from '../src';
    
    export function Playground() {
      return (
        <Page title="Playground">
          {/* Add the code you want to test in here */}
        </Page>
      );
    }
    ```
    
    </details>
    
    ### 🎩 checklist
    
    - [ ] Tested on
    [mobile](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting.md#cross-browser-testing)
    - [ ] Tested on [multiple
    browsers](https://help.shopify.com/en/manual/shopify-admin/supported-browsers)
    - [ ] Tested for
    [accessibility](https://github.com/Shopify/polaris/blob/main/documentation/Accessibility%20testing.md)
    - [ ] Updated the component's `README.md` with documentation changes
    - [ ] [Tophatted
    documentation](https://github.com/Shopify/polaris/blob/main/documentation/Tophatting%20documentation.md)
    changes in the style guide

--- polaris.shopify.com/pages/examples/button-split.tsx
+++ polaris.shopify.com/pages/examples/button-split.tsx
@@ -1,57 +1,70 @@
-import {ActionList, Button, ButtonGroup, Popover} from '@shopify/polaris';
+import {
+  ActionList,
+  InlineStack,
+  Button,
+  ButtonGroup,
+  Popover,
+} from '@shopify/polaris';
 import React from 'react';
 import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
 import {ChevronDownMinor} from '@shopify/polaris-icons';
 
 function ButtonExample() {
-  const [active, setActive] = React.useState(false);
+  const [active, setActive] = React.useState<string | null>(null);
+
+  const toggleActive = (id: string) => () => {
+    setActive((activeId) => (activeId !== id ? id : null));
+  };
   return (
     <div style={{height: '100px'}}>
-      <ButtonGroup variant="segmented">
-        <Button variant="primary">Save</Button>
-        <div style={{width: '3px'}} />
-        <Popover
-          active={active}
-          preferredAlignment="right"
-          activator={
-            <Button
-              variant="primary"
-              onClick={() => setActive(true)}
-              icon={ChevronDownMinor}
-              accessibilityLabel="Other save actions"
-            />
-          }
-          autofocusTarget="first-node"
-          onClose={() => setActive(false)}
-        >
-          <ActionList
-            actionRole="menuitem"
-            items={[{content: 'Save as draft'}]}
-          />
-        </Popover>
-      </ButtonGroup>
+      <InlineStack gap="500">
+        <ButtonGroup variant="segmented">
+          <Button variant="primary">Save</Button>
 
-      <ButtonGroup variant="segmented">
-        <Button>Save</Button>
-        <Popover
-          active={active}
-          preferredAlignment="right"
-          activator={
-            <Button
-              onClick={() => setActive(true)}
-              icon={ChevronDownMinor}
-              accessibilityLabel="Other save actions"
+          <Popover
+            active={active === 'popover1'}
+            preferredAlignment="right"
+            activator={
+              <Button
+                variant="primary"
+                onClick={toggleActive('popover1')}
+                icon={ChevronDownMinor}
+                accessibilityLabel="Other save actions"
+              />
+            }
+            autofocusTarget="first-node"
+            onClose={toggleActive('popover1')}
+          >
+            <ActionList
+              actionRole="menuitem"
+              items={[{content: 'Save as draft'}]}
             />
-          }
-          autofocusTarget="first-node"
-          onClose={() => setActive(false)}
-        >
-          <ActionList
-            actionRole="menuitem"
-            items={[{content: 'Save as draft'}]}
-          />
-        </Popover>
-      </ButtonGroup>
+          </Popover>
+        </ButtonGroup>
+
+        <ButtonGroup variant="segmented">
+          <Button>Save</Button>
+
+          <Popover
+            active={active === 'popover2'}
+            preferredAlignment="right"
+            activator={
+              <Button
+                onClick={toggleActive('popover2')}
+                icon={ChevronDownMinor}
+                accessibilityLabel="Other save actions"
+              />
+            }
+            autofocusTarget="first-node"
+            onClose={toggleActive('popover2')}
+          >
+            <ActionList
+              actionRole="menuitem"
+              items={[{content: 'Save as draft'}]}
+            />
+          </Popover>
+        </ButtonGroup>
+      </InlineStack>
     </div>
   );
 }
