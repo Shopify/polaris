3f7c4e37c570fae199ef603a505747447e76205e
Jess Telford <jess.telford@shopify.com>
2023-11-02 18:27:07 +1100

    Remove unnecessary CSS props on Cube

--- polaris.shopify.com/scripts/get-style-props.mjs
+++ polaris.shopify.com/scripts/get-style-props.mjs
@@ -17,19 +17,109 @@ const file = await fs.open(
   'w+',
 );
 
+// @webref/css conveniently curates down all the available specs for us. Some
+// specs are defined as "Delta" where they add / modify some properties, those
+// specs are suffixed with `-<number>`
+const stripDeltaSpecSuffix = (specShortName) =>
+  specShortName.replace(/-\d$/, '');
+
+// Note, we don't list the "Delta" specs here (those ending in `-<number>`),
+// but they will be included if found.
+const supportedSpecifications = [
+  'CSS',
+  'compositing',
+  'css-align',
+  'css-animations',
+  'css-backgrounds',
+  'css-borders',
+  'css-box',
+  'css-break',
+  'css-color',
+  'css-display',
+  'css-flexbox',
+  'css-fonts',
+  'css-grid',
+  'css-images',
+  'css-inline',
+  'css-line-grid',
+  'css-link-params',
+  'css-lists',
+  'css-logical',
+  'css-multicol',
+  'css-nav',
+  'css-overflow',
+  'css-overscroll',
+  'css-position',
+  'css-scroll-snap',
+  'css-scrollbars',
+  'css-shapes',
+  'css-size-adjust',
+  'css-sizing',
+  'css-speech',
+  'css-tables',
+  'css-text',
+  'css-text-decor',
+  'css-transforms',
+  'css-transitions',
+  'css-ui',
+  'css-view-transitions',
+  'css-will-change',
+  'css-writing-modes',
+  'filter-effects',
+  'motion',
+  'scroll-animations',
+];
+
+const writeProperty = async (file, property) => {
+  if (property.inherited) {
+    await file.write(`\n    (prop: '${property.name}', default: 'inherit'),`);
+  } else {
+    await file.write(`\n    '${property.name}',`);
+  }
+};
+
+// An array as order is important
+const properties = [];
+
+const getOrPushProperty = (list, name) => {
+  let prop = list.find((existingProp) => existingProp.name === name);
+  if (!prop) {
+    prop = {name, inherited: false};
+    list.push(prop);
+  }
+  return prop;
+};
+
 await file.write('/* THIS FILE IS AUTO GENERATED, DO NOT TOUCH */');
 await file.write("\n@import '../../styles/mixins';");
 await file.write('\n.Box {');
 await file.write(`\n  @include responsive-props('box', (`);
+
+// Do a pass to gather up properties and group them by shorthand -> longhand
 for (let [shortname, data] of Object.entries(parsedFiles)) {
-  for (let i = 0; i < data.properties.length; i++) {
-    const property = data.properties[i];
-    if (property.inherited === 'yes') {
-      await file.write(`\n    (prop: '${property.name}', default: 'inherit'),`);
-    } else {
-      await file.write(`\n    '${property.name}',`);
-    }
+  // Treat delta specs the same as their "full" spec name. The data in
+  // @webref/css is ordered, so deltas will always come after full specs.
+  shortname = stripDeltaSpecSuffix(shortname);
+  if (!supportedSpecifications.includes(shortname)) {
+    continue;
   }
+
+  for (let i = 0; i < data.properties.length; i++) {
+    const propertySpec = data.properties[i];
+    // TODO: This could be a long-hand version of another property, such as
+    // `margin-left` -> `margin` as indicated by .logicalPropertyGroup. However,
+    // the .logicalPropertyGroup value does not always seem map to a CSS
+    // property name, so it's not possible to know which longhand the shorthand
+    // should come after. We need to figure this out somehow so we can ensure
+    // specificity is maintained.
+    const prop = getOrPushProperty(properties, propertySpec.name);
+    prop.inherited = propertySpec.inherited === 'yes';
+  }
+}
+
+// Now write them all out in the correct order
+for (let property of properties) {
+  await writeProperty(file, property);
 }
 await file.write(`\n  ));`);
 await file.write('\n}');
