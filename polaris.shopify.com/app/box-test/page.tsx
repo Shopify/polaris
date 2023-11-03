4b95c5daadda80559e90b1fc3cd6310df9c123f0
Jess Telford <jess.telford@shopify.com>
2023-11-03 19:23:37 +1100

    Add demo of badly ordered CSS vars (flex-direction overridden by flex-flow)

--- polaris.shopify.com/app/box-test/page.tsx
+++ polaris.shopify.com/app/box-test/page.tsx
@@ -8,6 +8,15 @@ export default function Page() {
       {[...Array(1).keys()].map((id) => (
         <Cube key={id}>A cube</Cube>
       ))}
+      <Cube display="flex" flexDirection="column" gap="10px">
+        <Cube height="40px" padding="4px" backgroundColor="#aaf0d1" />
+        <Cube height="40px" padding="4px" backgroundColor="#ca9bf7">
+          <Cube display="flex" flexDirection="column">
+            <Cube height="40px" padding="4px" backgroundColor="#b2fba5" />
+            <Cube height="40px" padding="4px" backgroundColor="#aa9499" />
+          </Cube>
+        </Cube>
+      </Cube>
     </div>
   );
 }
