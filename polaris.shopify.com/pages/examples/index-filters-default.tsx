734249eca706b78c967bfb25a931c38de925adad
Kyle Durand <kyledurand@users.noreply.github.com>
2023-11-03 13:05:02 -0400

    [Filters] Fix `onQueryClear` examples (#11102)
    Fixes: https://github.com/Shopify/polaris/issues/11090

--- polaris.shopify.com/pages/examples/index-filters-default.tsx
+++ polaris.shopify.com/pages/examples/index-filters-default.tsx
@@ -334,7 +334,7 @@ function IndexFiltersDefaultExample() {
         queryValue={queryValue}
         queryPlaceholder="Searching in all"
         onQueryChange={handleFiltersQueryChange}
-        onQueryClear={() => {}}
+        onQueryClear={() => setQueryValue('')}
         onSort={setSortSelected}
         primaryAction={primaryAction}
         cancelAction={{
