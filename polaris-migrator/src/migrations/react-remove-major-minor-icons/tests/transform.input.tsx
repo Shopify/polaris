b4c818d763e8d2416ea1f479722aa6f02516aaba
Sam Rose <11774595+sam-b-rose@users.noreply.github.com>
2023-11-07 16:33:10 -0500

    WIP: Major and Minor icon migration

---
+++ polaris-migrator/src/migrations/react-remove-major-minor-icons/tests/transform.input.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Icon} from '@shopify/polaris';
+import {AppsMajor} from '@shopify/polaris-icons';
+
+export function App() {
+  return (
+    <>
+      <Icon source={AppsMajor} />
+    </>
+  );
+}
