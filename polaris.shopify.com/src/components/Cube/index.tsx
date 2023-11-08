6e883f8a2ecb005ba269c0a134a9f56171862a68
Jess Telford <jess.telford@shopify.com>
2023-11-08 17:45:15 +1100

    Aliases of style props

--- polaris.shopify.com/src/components/Cube/index.tsx
+++ polaris.shopify.com/src/components/Cube/index.tsx
@@ -2,46 +2,78 @@ import style from './style.module.scss';
 import {forwardRef} from 'react';
 import decamelize from 'decamelize';
 import {designTokenStyleProps} from '@shopify/polaris-tokens';
-import type {CubeProps} from './types';
 import type * as Polymorphic from '@radix-ui/react-polymorphic';
-import {getResponsiveValue, isObject} from '../../utils/various';
+import type {Entries} from 'type-fest';
+import {getResponsiveValue, getResponsiveProps} from '../../utils/various';
+import {type CubeProps, stylePropAliases} from './generated-data';
 
-function getCssProps(styleProps: Record<string, any>) {
+// Extract a unique set of just the alias names
+const allAliases = Array.from(new Set(Object.values(stylePropAliases).flat()));
+
+const stylePropAliasesEntries = Object.entries(stylePropAliases) as Entries<
+  Required<typeof stylePropAliases>
+>;
+
+function convertStylePropsToCSSProperties(styleProps: CubeProps) {
+  const stylePropsWithExpandedAliases: CubeProps = {...styleProps};
+
+  // Ensure constituent styles are given fallback values even when they're not
+  // passed in as an explicit style prop.
+  stylePropAliasesEntries.forEach(([styleProp, aliases]) => {
+    for (
+      let index = 0;
+      // Stop looping if there are no more fallbacks
+      index < aliases.length &&
+      // or if a value is already set (either passed in, or from an earlier
+      // fallback)
+      typeof stylePropsWithExpandedAliases[styleProp] === 'undefined';
+      index++
+    ) {
+      // TODO: How might we remove the 'as any' here (used to fix the
+      // 'Expression produces a union type that is too complex to represent.'
+      // TS error)?
+      (stylePropsWithExpandedAliases[styleProp] as any) =
+        styleProps[aliases[index]];
+    }
+  });
+
+  // Then delete the aliases as they're no longer needed.
+  for (let alias of allAliases) {
+    delete stylePropsWithExpandedAliases[alias];
+  }
+
   // camelCase to kebabCase of styleProps
   // intiailise responsive values from valid styleProps
-  return Object.entries(styleProps).reduce((acc, [key, value]) => {
-    const decamelizedPropKey = decamelize(key, {separator: '-'});
-    let filteredValue = value;
-    if (designTokenStyleProps.includes(key as any)) {
-      filteredValue = `var(--p-${value})`;
-      if (isObject(value)) {
-        filteredValue = Object.entries(value).reduce((acc, [key, value]) => {
-          return {
-            ...acc,
-            [key]: `var(--p-${value})`,
-          };
-        }, {});
-      }
+  return (
+    Object.entries(stylePropsWithExpandedAliases) as Entries<
+      typeof stylePropsWithExpandedAliases
+    >
+  ).reduce((acc, [key, value]) => {
+    // Skip undefined values (can happen when explicit 'undefined' is passed
+    // in, or from expanding aliases above.
+    if (typeof value === 'undefined') {
+      return acc;
     }
+
+    const decamelizedPropKey = decamelize(key, {separator: '-'});
+
     return {
-      // TODO: Use `getResponsiveProps` for properties that must match to token
-      // We need a way to differentiate properties that require token values from those that do not
-      // values only.
-      ...getResponsiveValue('box', decamelizedPropKey, filteredValue),
       ...acc,
+      ...(designTokenStyleProps.includes(key as any)
+        ? // no token subgroup because it's defined as part of the value
+          getResponsiveProps('box', decamelizedPropKey, null, value)
+        : getResponsiveValue('box', decamelizedPropKey, value)),
     };
   }, {});
 }
 
-type PolymorphicCube = Polymorphic.ForwardRefComponent<any, Partial<CubeProps>>;
+type PolymorphicCube = Polymorphic.ForwardRefComponent<any, CubeProps>;
 
 export const Cube = forwardRef(function Cube(
   {as: Tag = 'div', children, ...styleProps},
   forwardedRef,
 ) {
-  const styles = {
-    ...getCssProps(styleProps),
-  };
+  const styles = convertStylePropsToCSSProperties(styleProps);
   return (
     <Tag ref={forwardedRef} style={styles} className={style.Box}>
       {children}
