2a94aec026f56a8c27139b1383d829e18f731265
Sam Rose <11774595+sam-b-rose@users.noreply.github.com>
2023-11-09 17:19:24 -0500

    Update icons rename migration

--- polaris-migrator/src/migrations/icons-v8-update-names/tests/transform.test.ts
+++ polaris-migrator/src/migrations/icons-v8-update-names/tests/transform.test.ts
@@ -1,7 +1,7 @@
 import {check} from '../../../utilities/check';
 
 const transform = 'icons-v8-update-names';
-const fixtures = ['transform'];
+const fixtures = ['transform', 'big-transform'];
 
 for (const fixture of fixtures) {
   check(__dirname, {
