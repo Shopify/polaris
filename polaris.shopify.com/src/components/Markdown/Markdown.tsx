3eabc606074135accbea145b4527ff2625b1a215
Sam Rose <11774595+sam-b-rose@users.noreply.github.com>
2023-11-22 08:41:51 -0500

    Remove Row/Column layout components

--- polaris.shopify.com/src/components/Markdown/Markdown.tsx
+++ polaris.shopify.com/src/components/Markdown/Markdown.tsx
@@ -14,7 +14,6 @@ import {Grid, InlineGrid} from '@shopify/polaris';
 import styles from './Markdown.module.scss';
 import Code, {InlineCode} from '../../components/Code';
 import {DirectiveCard} from './components/DirectiveCard';
-import {Section, Row, Column} from './components/Mdx';
 import {Box, type WithAsProp} from '../../components/Box';
 import {Stack} from '../../components/Stack';
 import StatusBanner from '../../components/StatusBanner';
@@ -245,9 +244,6 @@ function Markdown<
         SideBySide,
         Grid,
         Card,
-        Section,
-        Row,
-        Column,
         DirectiveCard,
         FeaturedCardGrid,
         YoutubeVideo,
