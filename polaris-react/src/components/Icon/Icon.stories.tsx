b63784eb5e55419af50092f77db3d80415a127d3
Alex Page <alex.page@shopify.com>
2023-11-27 10:50:40 +1100

    Add missing icon stories

--- polaris-react/src/components/Icon/Icon.stories.tsx
+++ polaris-react/src/components/Icon/Icon.stories.tsx
@@ -82,6 +82,14 @@ export function Colored() {
         Text Magic tone
       </Text>
       <Icon source={CirclePlusMinor} tone="textMagic" />
+      <Text as="p" variant="bodyMd" alignment="center">
+        Placeholder
+      </Text>
+      <Icon source="placeholder" />
+      <Text as="p" variant="bodyMd" alignment="center">
+        External icon
+      </Text>
+      <Icon source="https://polaris.shopify.com/images/shopify-logo.svg" />
     </BlockStack>
   );
 }
