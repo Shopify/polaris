957d4a7d3f59164e584d8791f05f52b44aa2faec
Yuraima Estevez <yuraima.estevez@shopify.com>
2023-11-27 16:43:15 -0600

    remove anchor in url during client side render

--- polaris.shopify.com/src/components/Subnav/Subnav.tsx
+++ polaris.shopify.com/src/components/Subnav/Subnav.tsx
@@ -9,13 +9,28 @@ import {useRouter} from 'next/router';
 import {Icon} from '@shopify/polaris';
 import * as polarisIcons from '@shopify/polaris-icons';
 import icons from '../../icons';
+import {useEffect, useState} from 'react';
 
 type PolarisIcon = keyof typeof polarisIcons;
 const nav = navJSON.children as NavJSON;
 
+interface NavObject {
+  [key: string]: NavItem;
+}
+
+type NavObjects = NavObject | undefined;
+
 function Subnav() {
   const {asPath} = useRouter();
-  const navItems = getNavItems(asPath);
+  const [navItems, setNavItems] = useState<NavObjects>();
+
+  /**
+   * We need to run this on the client side because anchor links are not passed
+   * to the server
+   */
+  useEffect(() => {
+    setNavItems(getNavItems(asPath));
+  }, [asPath]);
 
   if (!navItems) {
     console.warn('No subnav items found for path:', asPath);
@@ -57,6 +72,13 @@ function Subnav() {
 }
 
 function getNavItems(path: string): {[key: string]: NavItem} | undefined {
+  const anchor = path.indexOf('#');
+
+  // remove the anchor link from path if exists
+  if (anchor >= 0) {
+    path = path.substring(0, anchor);
+  }
+
   const paths = path.split('/').filter((segment) => segment);
 
   const navItemPath = paths.join('.children.');
