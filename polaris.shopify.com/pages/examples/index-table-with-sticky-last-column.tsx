61cacbfbb4a59d97bd48124ee86c7b066be5b091
Laura Aubin <laura.aubin@shopify.com>
2023-11-29 18:23:01 -0500

    [IndexTable, IndexFilters] Fix total column alignment in the Style Guide (#11260)
    Fix IndexTable and IndexFilters total column alignment

--- polaris.shopify.com/pages/examples/index-table-with-sticky-last-column.tsx
+++ polaris.shopify.com/pages/examples/index-table-with-sticky-last-column.tsx
@@ -65,7 +65,11 @@ function StickyLastCellIndexTableExample() {
         </IndexTable.Cell>
         <IndexTable.Cell>{date}</IndexTable.Cell>
         <IndexTable.Cell>{customer}</IndexTable.Cell>
-        <IndexTable.Cell>{total}</IndexTable.Cell>
+        <IndexTable.Cell>
+          <Text as="span" alignment="end" numeric>
+            {total}
+          </Text>
+        </IndexTable.Cell>
         <IndexTable.Cell>{paymentStatus}</IndexTable.Cell>
         <IndexTable.Cell>{fulfillmentStatus}</IndexTable.Cell>
       </IndexTable.Row>
