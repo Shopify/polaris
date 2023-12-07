f1d3f384f553b30aa1a550042f6c59f2f5c1c88d
Alex Page <hi@alexpage.dev>
2023-12-07 12:32:27 +1100

    Update icons so that polaris-icons/icons/*.svg matches NPM package (#11271)
    ### WHY are these changes introduced?
    
    Align our icons in the `polaris-icons/icons/*.svg` directory to be the
    same as the ones deployed in the NPM package.
    
    ### WHAT is this pull request doing?
    
    - [x] Remove `fill` as we already remove it as a plugin with rollup
    - [x] Fix `svgo` config as the `convertPathData` plugin was not
    correctly running with the overridden option
    - [x] Add a new test to make sure that optimized svg code matches the
    code in the `.svg` file
    - [x] Remove unnecessary tests that are resolved with `svgo` config and
    `yarn optimize`
    - [x] Reduce icons library SVG size by `383.75kb`

--- polaris-icons/svgo.config.js
+++ polaris-icons/svgo.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: [
     {
-      /**
-       * removeDimensions is set to true to remove width and height attributes from SVGs.
-       * This allows the SVG to scale to the size of its container, making it more responsive.
-       */
-      name: 'removeDimensions',
-      active: true,
-    },
-    {
       name: 'preset-default',
       params: {
         overrides: {
@@ -33,18 +25,58 @@ module.exports = {
           mergePaths: false,
 
           convertPathData: {
+            applyTransforms: true,
+            applyTransformsStroked: true,
+            makeArcs: {
+              threshold: 2.5,
+              tolerence: 0.5,
+            },
+            straightCurves: true,
+            lineShorthands: true,
+            convertToZ: true,
+            curveSmoothShorthands: true,
+            floatPrecision: 3,
+            transformPrecision: 5,
+            removeUseless: true,
+            collapseRepeated: true,
             /**
              * Mixing absolute and relative path commands can lead to rendering
              * issues on some platforms. This disables converting some path data to
              * absolute if it is shorter, keeping all path data relative. Using
-             * relative paths means that data points are relative  to the current
+             * relative paths means that data points are relative to the current
              * point at the start of the path command, which does not greatly
              * increase the quantity of path data.
              */
             utilizeAbsolute: false,
+            leadingZero: true,
+            negativeExtraSpace: true,
+            noSpaceAfterFlags: false,
+            forceAbsolutePath: false,
           },
         },
       },
+    },
+
+    /**
+     * removeAttrs is set to true to remove fill, width and height attributes from SVGs.
+     * This allows the SVG to scale to the size of its container, making it more responsive.
+     * This ensures that SVG colors are always set to default values.
+     */
+    {
+      name: 'removeAttrs',
+      params: {
+        attrs: ['*:fill', 'svg:(width|height)'],
+      },
+    },
+
+    /**
+     * Sorts attributes in all elements in the document.
+     * This does not reduce the size of the SVG, but improves readability
+     * and may improve how compression algorithms perform on it.
+     */
+    {
+      name: 'sortAttrs',
+      active: true,
     },
   ],
 };
