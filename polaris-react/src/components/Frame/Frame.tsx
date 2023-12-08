d6e1e8db94ecb49cfb2555689a031b4425bca032
Lo Kim <lo.kim@shopify.com>
2023-12-08 09:08:16 -0500

    [Frame] Add View Transition support

--- polaris-react/src/components/Frame/Frame.tsx
+++ polaris-react/src/components/Frame/Frame.tsx
@@ -55,6 +55,7 @@ export interface FrameProps {
    * @default false
    */
   sidebar?: boolean;
+  viewTransition?: boolean;
 }
 
 type CombinedProps = FrameProps & {
@@ -118,6 +119,7 @@ class FrameInner extends PureComponent<CombinedProps, State> {
       i18n,
       sidebar,
       mediaQuery: {isNavigationCollapsed},
+      viewTransition = false,
     } = this.props;
     const navClassName = classNames(
       styles.Navigation,
@@ -242,6 +244,11 @@ class FrameInner extends PureComponent<CombinedProps, State> {
       sidebar && styles.hasSidebar,
     );
 
+    const frameContentClassName = classNames(
+      styles.Content,
+      viewTransition && styles.hasViewTransition,
+    );
+
     const contextualSaveBarMarkup = (
       <CSSAnimation
         in={showContextualSaveBar}
@@ -292,7 +299,7 @@ class FrameInner extends PureComponent<CombinedProps, State> {
             id={APP_FRAME_MAIN}
             data-has-global-ribbon={Boolean(globalRibbon)}
           >
-            <div className={styles.Content}>{children}</div>
+            <div className={frameContentClassName}>{children}</div>
           </main>
           <ToastManager toastMessages={toastMessages} />
           {globalRibbonMarkup}
