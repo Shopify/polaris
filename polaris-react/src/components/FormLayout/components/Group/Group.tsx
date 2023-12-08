f971ae88e293325285e99a18c6272d92c20e9c1b
Chloe Rice <chloe.rice@shopify.com>
2023-12-08 21:15:40 +0000

    [FormLayout.Group] Add support for vertical layout

--- polaris-react/src/components/FormLayout/components/Group/Group.tsx
+++ polaris-react/src/components/FormLayout/components/Group/Group.tsx
@@ -3,18 +3,33 @@ import React, {Children, useId} from 'react';
 import {wrapWithComponent} from '../../../../utilities/components';
 import {Box} from '../../../Box';
 import {InlineStack} from '../../../InlineStack';
+import {BlockStack} from '../../../BlockStack';
 import {Text} from '../../../Text';
 import {Item} from '../Item';
 import styles from '../../FormLayout.scss';
 
 export interface GroupProps {
+  /** The child inputs to group */
   children?: React.ReactNode;
+  /** Whether to reduce the min-width of child inputs by 50%. Use for inline groups of inputs with short character length, like numeric dimensions. */
   condensed?: boolean;
+  /** Label describing the input group */
   title?: string;
+  /** Helpful text for the input group */
   helpText?: React.ReactNode;
+  /** Flex direction of the input group
+   * @default 'inline'
+   */
+  variant?: 'inline' | 'block';
 }
 
-export function Group({children, condensed, title, helpText}: GroupProps) {
+export function Group({
+  children,
+  condensed,
+  title,
+  helpText,
+  variant = 'inline',
+}: GroupProps) {
   const id = useId();
 
   let helpTextElement = null;
@@ -44,6 +59,8 @@ export function Group({children, condensed, title, helpText}: GroupProps) {
     wrapWithComponent(child, Item, {condensed}),
   );
 
+  const InputWrapper = variant === 'inline' ? InlineStack : BlockStack;
+
   return (
     <div
       role="group"
@@ -52,7 +69,7 @@ export function Group({children, condensed, title, helpText}: GroupProps) {
       aria-describedby={helpTextId}
     >
       {titleElement}
-      <InlineStack gap="300">{itemsMarkup}</InlineStack>
+      <InputWrapper gap="300">{itemsMarkup}</InputWrapper>
       {helpTextElement}
     </div>
   );
