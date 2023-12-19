444000b66d430c752390efd0d64ca6c282034c87
Jess Telford <jess.telford@shopify.com>
2023-12-19 15:00:05 +1100

    Commit the shit otu of this

--- polaris-migrator/src/migrations/react-css-module-to-style-props/tests/simple-class.input.tsx
+++ polaris-migrator/src/migrations/react-css-module-to-style-props/tests/simple-class.input.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import styles, {Bar} from './simple-class.input.module.scss';
+import styles, {Bar, Bar as Zip} from './simple-class.input.module.scss';
 
 declare function MyComponent(props: any): JSX.Element;
 export function App() {
-  return <MyComponent className={[styles.Foo, Bar]}>Hello world</MyComponent>;
+  return (
+    <MyComponent className={[styles.Foo, Zip, Bar]}>Hello world</MyComponent>
+  );
 }
