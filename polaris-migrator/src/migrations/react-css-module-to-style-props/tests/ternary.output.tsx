0e1462dadd7bab882b21a566bbd423d2319ee2ae
Jess Telford <jess.telford@shopify.com>
2023-12-20 01:39:34 +1100

    Working commenter

--- polaris-migrator/src/migrations/react-css-module-to-style-props/tests/ternary.output.tsx
+++ polaris-migrator/src/migrations/react-css-module-to-style-props/tests/ternary.output.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import classNames from 'classnames';
+
+import styles from './ternary.input.module.scss';
+import styles2 from './ternary.input.module.scss?2';
 
 declare function MyComponent(props: any): JSX.Element;
 export function App() {
   const useFoo = true;
   return (
+    /* Couldn't merge all styles:
     <MyComponent
-      {...(useFoo
-        ? {
-            display: 'flex',
-            color: 'red',
-            fontWeight: 'bold',
-          }
-        : {
-            display: 'block',
-            color: 'green',
-            borderRadius: '4px',
-          })}
+      className={useFoo ? {
+        display: "block",
+        color: "red",
+        fontWeight: "bold"
+      } : {
+        display: "block"
+      }}
+    >
+      Hello world
+    </MyComponent> */
+    <MyComponent
+      className={useFoo ? classNames(styles.Foo, styles.Bar) : styles2.Bar}
     >
       Hello world
     </MyComponent>
