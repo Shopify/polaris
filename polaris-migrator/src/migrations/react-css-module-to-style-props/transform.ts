f56fc74997e0d354f198b2b7a0311c147ddcd960
Jess Telford <jess.telford@shopify.com>
2023-12-19 15:00:05 +1100

    Commit the shit otu of this

--- polaris-migrator/src/migrations/react-css-module-to-style-props/transform.ts
+++ polaris-migrator/src/migrations/react-css-module-to-style-props/transform.ts
@@ -8,12 +8,20 @@ import type {
   FileInfo,
   Options,
   Collection,
+  ObjectExpression,
 } from 'jscodeshift';
 import postcss from 'postcss';
 
-import {insertJSXAttribute, removeJSXAttributes} from '../../utilities/jsx';
+import {
+  insertJSXAttribute,
+  removeJSXAttributes,
+  insertJSXComment,
+} from '../../utilities/jsx';
 import {removeImportDeclaration} from '../../utilities/imports';
 
+type Property = ObjectExpression['properties'] extends Array<infer T>
+  ? T
+  : never;
 type Path = any;
 type Node = any;
 interface StyleObject {
@@ -48,48 +56,6 @@ const processCSSFile = (importPath: string, file: FileInfo): StyleFile => {
   return postcssJS.objectify(cssAST);
 };
 
-const walkExpressions = (
-  j: JSCodeshift,
-  source: Collection<any>,
-  file: FileInfo,
-  expression: any,
-  importCache: ImportCache,
-) => {
-  debugger;
-  const memberExpression = expression
-    .find(j.MemberExpression, (node) => {})
-    .get();
-  console.log(memberExpression);
-  if (expression.type === 'MemberExpression') {
-    // className = {styles.Foo};
-    const styleObject = getStyleObjectFromNode(
-      j,
-      source,
-      file,
-      expression,
-      importCache,
-    );
-    console.log(styleObject);
-    replaceExpressionWithObjectExpression(j, expression, styleObject);
-  } else if (expression.type === 'CallExpression') {
-    // className = classNames(styles.Foo, styles.Bar);
-    expression.arguments.forEach((expression: any) =>
-      walkExpressions(j, source, file, expression, importCache),
-    );
-  } else if (expression.type === 'ArrayExpression') {
-    // className={[styles.Foo, styles.Bar]}
-    expression.elements.forEach((expression: any) =>
-      walkExpressions(j, source, file, expression, importCache),
-    );
-    // } else if (expression.type === 'ConditionalExpression') {
-    //   // Ternary
-    //   addMemberExpression(expression);
-    //   // className={[styles.Foo, styles.Bar]}
-    //   walkExpressions(j, expression.consequent, importCache);
-    //   walkExpressions(j, expression.alternate, importCache);
-  }
-};
-
 const getStyleObjectFromNode = (
   j: JSCodeshift,
   file: FileInfo,
@@ -97,14 +63,38 @@ const getStyleObjectFromNode = (
   importCache: ImportCache,
 ): StyleObject => {
   const importIdentifier = path.value.object?.name ?? path.value.name;
-  const className = path.value.property?.name ?? path.value.name;
-  const importNode = getImportNodeForIdentifier(j, importIdentifier, path);
-  let CSSObject = importCache.get(importNode);
+  // In case the import is something like:
+  // import {Foo as Bar} from './styles.scss';
+  const className =
+    path.value.property?.name ??
+    getImportedNameForIdentifier(j, importIdentifier, path);
+  const importNodePath = getImportNodeForIdentifier(j, importIdentifier, path);
+  let CSSObject = importCache.get(importNodePath);
   if (!CSSObject) {
-    CSSObject = processCSSFile(importNode.source.value as string, file);
-    importCache.set(importNode, CSSObject!);
+    CSSObject = processCSSFile(
+      importNodePath.value.source.value as string,
+      file,
+    );
+    importCache.set(importNodePath, CSSObject!);
   }
   return CSSObject![`.${className}`];
+};
+
+const findUp = (
+  nodePath: Path,
+  fn: (nodePath: Path) => boolean,
+): Path | undefined => {
+  if (!nodePath) {
+    return undefined;
+  }
+  let curNodePath = nodePath;
+  while (curNodePath.parentPath) {
+    curNodePath = curNodePath.parentPath;
+    if (fn(curNodePath)) {
+      return curNodePath;
+    }
+  }
+  return undefined;
 };
 
 const createObjectExpression = (j: JSCodeshift, obj: {[x: string]: any}) => {
@@ -115,18 +105,40 @@ const createObjectExpression = (j: JSCodeshift, obj: {[x: string]: any}) => {
   );
 };
 
-const replaceExpressionWithObjectExpression = (
+const insertPropertyIntoObjectExpression = (
   j: JSCodeshift,
-  path: Path,
-  styleObject: StyleObject,
+  expression: ObjectExpression,
+  property: Property,
 ) => {
-  path.replace(
-    j.objectExpression(
-      Object.entries(styleObject).map(([key, value]) =>
-        j.property('init', j.identifier(key), j.literal(value)),
-      ),
-    ),
-  );
+  let index = -1;
+  if (property.type === 'Property') {
+    index = expression.properties.findIndex(
+      (prop) => prop.type === 'Property' && prop.key.name === property.key.name,
+    );
+  }
+  if (index === -1) {
+    index = expression.properties.length;
+  }
+  expression.properties.splice(index, 1, property);
+};
+
+const mergeObjectExpressions = (
+  j: JSCodeshift,
+  target: ObjectExpression,
+  ...expressions: ObjectExpression[]
+) => {
+  expressions.forEach((expression) => {
+    expression.properties.forEach((property) =>
+      insertPropertyIntoObjectExpression(j, target, property),
+    );
+  });
+};
+
+const fromObjectExpression = (expression: ObjectExpression) => {
+  return expression.properties.reduce((acc, property) => {
+    acc[property.key.name] = property.value.value;
+    return acc;
+  }, {});
 };
 
 const insertStyleProps = (
@@ -140,7 +152,7 @@ const insertStyleProps = (
   removeJSXAttributes(j, element, 'className');
 };
 
-const getImportNodeForIdentifier = (
+const getImportedNameForIdentifier = (
   j: JSCodeshift,
   identifier: string,
   path: Path,
@@ -149,13 +161,26 @@ const getImportNodeForIdentifier = (
   // This avoids issues with variable shadowing
   const importNode = path.scope.lookup(identifier)?.getBindings()?.[
     identifier
-  ][0].parent.parent.value;
+  ][0].parent;
 
-  if (j.ImportDeclaration.check(importNode)) {
-    return importNode;
+  if (j.ImportDeclaration.check(importNode.parent.value)) {
+    return importNode.value.imported.name;
   }
 
   return undefined;
+};
+
+const getImportNodeForIdentifier = (
+  j: JSCodeshift,
+  identifier: string,
+  path: Path,
+): Node | undefined => {
+  // Use scope to find this variable's declaration binding.
+  // This avoids issues with variable shadowing
+  return findUp(
+    path.scope.lookup(identifier)?.getBindings()?.[identifier][0],
+    (nodePath) => j.ImportDeclaration.check(nodePath.value),
+  );
 };
 
 const IS_CSS_FILE_PATH = /\.s?css$/;
@@ -166,8 +191,17 @@ const isReferenceToCSSImport = (
   path: Path,
 ): boolean => {
   return IS_CSS_FILE_PATH.test(
-    getImportNodeForIdentifier(j, identifier, path)?.source.value,
+    getImportNodeForIdentifier(j, identifier, path)?.value.source.value,
   );
+};
+
+const isLocalVariableReference = (
+  j: JSCodeshift,
+  identifier: string,
+  path: Path,
+): boolean => {
+  // TODO
+  return false;
 };
 
 export default function transformer(
@@ -194,16 +228,16 @@ export default function transformer(
       }
     });
 
-  classNameCollection
-    .find(j.Identifier)
-    .filter(
-      (path) =>
-        // For some reason, a JSXIdentifier matches a `j.Identifier`, so we want
-        // to filter that out
-        !j.JSXIdentifier.check(path.value) &&
-        // Ignore identifiers that are part of an expression (eg; `styles.Foo`)
-        !j.Expression.check(path.parentPath.value),
-    )
+  const identifierCollection = classNameCollection.find(j.Identifier).filter(
+    (path) =>
+      // For some reason, a JSXIdentifier matches a `j.Identifier`, so we want
+      // to filter that out
+      !j.JSXIdentifier.check(path.value) &&
+      // Ignore identifiers that are part of an expression (eg; `styles.Foo`)
+      !j.Expression.check(path.parentPath.value),
+  );
+
+  identifierCollection
     .filter((path) => isReferenceToCSSImport(j, path.value.name, path))
     .forEach((path) => {
       const styleObject = getStyleObjectFromNode(j, file, path, importCache);
@@ -214,40 +248,124 @@ export default function transformer(
       }
     });
 
-  classNameCollection.find(j.ConditionalExpression).forEach((path) => {
-    // TODO;
-    debugger;
-  });
+  // When we have styles reassigned to a variable, we have to switch over to
+  // that assignment and do all the same dance with converting to POJOs, etc
+  identifierCollection
+    .filter((path) => isLocalVariableReference(j, path.value.name, path))
+    .forEach((path) => {
+      // TODO: Recurse on this whole process
+    });
+
+  const bailouts: Path[] = [];
+
+  // Is it a call to classNames()?
+  const functionCalls = classNameCollection
+    .find(j.FunctionExpression)
+    .filter((path) => path.parentPath.value.type === 'JSXExpressionContainer');
 
   // Is it just an array of values?
   const arrayOfValues = classNameCollection
     .find(j.ArrayExpression)
-    .filter((path) => path.parentPath.value.type === 'JSXExpressionContainer');
+    .filter((path) => path.parentPath.value.type === 'JSXExpressionContainer')
+    .forEach((path) => {
+      let canFullyMerge = true;
+      const elements = path.value.elements;
+      // Reset the elements so we can re-insert runs of ObjectExpressions as
+      // merged objects.
+      path.value.elements = [];
 
-  if (arrayOfValues.size() > 0) {
-    // TODO Flatten the array down to a single ObjcetExpressionn
-    debugger;
-  }
+      let index = 0;
+
+      // Scan the entire array looking for runs of ObjectExpressions we can
+      // merge togethe
+      while (index < elements.length) {
+        const acc = createObjectExpression(j, {});
+        // Accumulate a run of ObjectExpressions using JS object merge order t
+        // override earlier
+        while (
+          index < elements.length &&
+          elements[index]?.type === 'ObjectExpression'
+        ) {
+          mergeObjectExpressions(j, acc, elements[index] as ObjectExpression);
+          index++;
+        }
+
+        if (Object.keys(acc).length) {
+          // push acc into array if it has keys
+          path.value.elements.push(acc);
+        }
+
+        if (
+          index < elements.length &&
+          elements[index]?.type !== 'ObjectExpression'
+        ) {
+          canFullyMerge = false;
+          path.value.elements.push(elements[index]);
+          // push element into array
+        }
+
+        index++;
+      }
+
+      if (canFullyMerge) {
+        // Replace the whole array expression with the single POJO
+        path.replace(path.value.elements[0]);
+      } else {
+        const jsxElement = findUp(path, (parentPath) =>
+          j.JSXElement.check(parentPath.value),
+        );
+        // Unable to process this fully, create a comment with the current
+        // state, then replace it with the original node
+        bailouts.push(jsxElement);
+      }
+    });
 
   // Is it a POJO?
-  const objectValue = classNameCollection
-    .find(j.ObjectExpressioon)
-    .filter((path) => path.parentPath.value.type === 'JSXExpressionContainer');
+  classNameCollection
+    .find(j.ObjectExpression)
+    .filter((path) => {
+      return path.parentPath.value.type === 'JSXExpressionContainer';
+    })
+    .forEach((path) => {
+      const pojo = fromObjectExpression(path.value);
+      const jsxElement = findUp(path, (parentPath) =>
+        j.JSXElement.check(parentPath.value),
+      );
+      insertStyleProps(j, jsxElement, pojo);
+    });
 
-  if (objectValue.size() > 0) {
-    // TODO convert to style props on the object, then delete `className`
-    debugger;
-  }
+  // Inject comments
+  j(bailouts).forEach((path) => {
+    const comment = `Couldn't merge all styles:\n${j(path).toSource()}`;
+    insertJSXComment(j, path, comment, 'before');
+    // Restore nodes to their original
+    path.replace(path.value.original);
+  });
 
-  // Is it a ternary?
-  const ternaryValue = classNameCollection
-    .find(j.ConditionalExpression)
-    .filter((path) => path.parentPath.value.type === 'JSXExpressionContainer');
+  const importPaths = Array.from(importCache.keys());
 
-  if (ternaryValue.size() > 0) {
-    // TODO Convert to an object spread, then delete `className`
-    debugger;
-  }
+  const removableImportPaths = importPaths.filter((importPath) => {
+    const identifiers = importPath.value.specifiers.map(
+      (specifier) => specifier.local.name,
+    );
+
+    const referencesToImport = source
+      .find(j.Identifier, (value) => identifiers.includes(value.name))
+      .filter(
+        (path) =>
+          // Don't match the identifiers in the import statement itself
+          !findUp(path, (nodePath) =>
+            j.ImportDeclaration.check(nodePath.value),
+          ) &&
+          // Ensure it's associated with the import we want to remove
+          getImportNodeForIdentifier(j, path.value.name, path) === importPath,
+      )
+      .size();
+
+    return referencesToImport === 0;
+  });
+
+  j(removableImportPaths).remove();
 
   /*
     .replaceWith((path) => {
