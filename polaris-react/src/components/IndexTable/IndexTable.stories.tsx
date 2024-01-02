Shopify/polaris

commit 812a04e25ace23e78c073de1b6f481d0eda868c9

[IndexTable] Fix sticky header position (#11342)
<!--
  ☝️How to write a good PR title:
- Prefix it with [ComponentName] (if applicable), for example: [Button]
  - Start with a verb, for example: Add, Delete, Improve, Fix…
  - Give as much context as necessary and as little as possible
  - Open it as a draft if it’s a work in progress
-->

### WHY are these changes introduced?

Closes #9591

As shown in [this
example](https://codesandbox.io/s/compassionate-goldstine-xrwcnh?file=/App.tsx:488-2148)
when you scroll in an IndexTable the sticky header shifts. The heading
cells should remain in place. This happens as the minWidth styles are
applied to the wrong index, i.e the minWith of the first cell is applied
to the second and so on.

Recording of the fix:


https://github.com/Shopify/polaris/assets/10282220/27ab07f6-4da0-4223-bde1-4498e2487f43

<!--
  Context about the problem that’s being addressed.
-->

### WHAT is this pull request doing?

This PR changes the index that the minWidth styles are applied to.

I've also added the example from codesandbox to Storybook for easy
testing.

<!--
  Summary of the changes committed.

Before / after screenshots are appreciated for UI changes. Make sure to
include alt text that describes the screenshot.

  Include a video if your changes include interactive content.

If you include an animated gif showing your change, wrapping it in a
details tag is recommended. Gifs usually autoplay, which can cause
accessibility issues for people reviewing your PR:

  <details>
    <summary>Summary of your gif(s)</summary>
    <img src="..." alt="Description of what the gif shows">
  </details>
-->

### How to 🎩

- Checkout this branch
- Run `yarn && yarn build`
- [Open this
component](http://localhost:6006/?path=/story/all-components-indextable--with-long-data-set)
in Storybook

🖥 [Local development

polaris-react/src/components/IndexTable/IndexTable.stories.tsx
@@ -18,6 +18,7 @@ import {
   useIndexResourceState,
   Thumbnail,
   Box,
+  Badge,
 } from '@shopify/polaris';
 
 import {IndexTable} from './IndexTable';
@@ -6085,6 +6086,75 @@ export function WithNestedRowsWithThumbnailsOneCellNonSelectable() {
         resourceName={resourceName}
         itemCount={rows.length}
         headings={columnHeadings as IndexTableProps['headings']}
+        selectable={false}
+      >
+        {rowMarkup}
+      </IndexTable>
+    </LegacyCard>
+  );
+}
+
+export function WithLongDataSet() {
+  const orders = Array.from(Array(100).keys()).map((i) => ({
+    id: `${i}`,
+    order: i,
+    date: 'Jul 20 at 4:34pm',
+    customer: 'Jaydon Stanton',
+    total: `$969.44${i}`,
+    paymentStatus: <Badge progress="complete">Paid</Badge>,
+    fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+  }));
+
+  const resourceName = {
+    singular: 'order',
+    plural: 'orders',
+  };
+
+  const {selectedResources, allResourcesSelected, handleSelectionChange} =
+    useIndexResourceState(orders);
+
+  const rowMarkup = orders.map(
+    (
+      {id, order, date, customer, total, paymentStatus, fulfillmentStatus},
+      index,
+    ) => (
+      <IndexTable.Row
+        id={id}
+        key={id}
+        selected={selectedResources.includes(id)}
+        position={index}
+      >
+        <IndexTable.Cell>
+          <Text variant="bodyMd" fontWeight="bold" as="span">
+            {order}
+          </Text>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{date}</IndexTable.Cell>
+        <IndexTable.Cell>{customer}</IndexTable.Cell>
+        <IndexTable.Cell>{total}</IndexTable.Cell>
+        <IndexTable.Cell>{paymentStatus}</IndexTable.Cell>
+        <IndexTable.Cell>{fulfillmentStatus}</IndexTable.Cell>
+      </IndexTable.Row>
+    ),
+  );
+
+  return (
+    <LegacyCard>
+      <IndexTable
+        resourceName={resourceName}
+        itemCount={orders.length}
+        selectedItemsCount={
+          allResourcesSelected ? 'All' : selectedResources.length
+        }
+        onSelectionChange={handleSelectionChange}
+        headings={[
+          {title: 'Order'},
+          {title: 'Date'},
+          {title: 'Customer'},
+          {title: 'Total', alignment: 'end'},
+          {title: 'Payment status'},
+          {title: 'Fulfillment status'},
+        ]}
         selectable={false}
       >
         {rowMarkup}

polaris-react/src/components/IndexTable/IndexTable.tsx
@@ -1083,10 +1083,9 @@ function IndexTableBase({
   }
 
   function renderStickyHeading(heading: IndexTableHeading, index: number) {
-    const position = index + 1;
     const headingStyle =
-      tableHeadingRects.current && tableHeadingRects.current.length > position
-        ? {minWidth: tableHeadingRects.current[position].offsetWidth}
+      tableHeadingRects.current && tableHeadingRects.current.length > index
+        ? {minWidth: tableHeadingRects.current[index].offsetWidth}
         : undefined;
     const headingAlignment = heading.alignment || 'start';
 

commit a3df9194c28661174e606243361e41ba0dfce302

Fixes an issue introduced in #11342 that causes the sticky header position to be one out of place when a selectable table is used
This change checks if theselectable is set, if so then add one to the index, otherwise use the index

polaris-react/src/components/IndexTable/IndexTable.stories.tsx
@@ -6094,7 +6094,64 @@ export function WithNestedRowsWithThumbnailsOneCellNonSelectable() {
   );
 }
 
-export function WithLongDataSet() {
+export function WithLongDataSetNonSelectable() {
+  const orders = Array.from(Array(100).keys()).map((i) => ({
+    id: `${i}`,
+    order: i,
+    date: 'Jul 20 at 4:34pm',
+    customer: 'Jaydon Stanton',
+    total: `$969.44${i}`,
+    paymentStatus: <Badge progress="complete">Paid</Badge>,
+    fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+  }));
+
+  const resourceName = {
+    singular: 'order',
+    plural: 'orders',
+  };
+
+  const rowMarkup = orders.map(
+    (
+      {id, order, date, customer, total, paymentStatus, fulfillmentStatus},
+      index,
+    ) => (
+      <IndexTable.Row id={id} key={id} position={index}>
+        <IndexTable.Cell>
+          <Text variant="bodyMd" fontWeight="bold" as="span">
+            {order}
+          </Text>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{date}</IndexTable.Cell>
+        <IndexTable.Cell>{customer}</IndexTable.Cell>
+        <IndexTable.Cell>{total}</IndexTable.Cell>
+        <IndexTable.Cell>{paymentStatus}</IndexTable.Cell>
+        <IndexTable.Cell>{fulfillmentStatus}</IndexTable.Cell>
+      </IndexTable.Row>
+    ),
+  );
+
+  return (
+    <LegacyCard>
+      <IndexTable
+        resourceName={resourceName}
+        itemCount={orders.length}
+        headings={[
+          {title: 'Order'},
+          {title: 'Date'},
+          {title: 'Customer'},
+          {title: 'Total', alignment: 'end'},
+          {title: 'Payment status'},
+          {title: 'Fulfillment status'},
+        ]}
+        selectable={false}
+      >
+        {rowMarkup}
+      </IndexTable>
+    </LegacyCard>
+  );
+}
+
+export function WithLongDataSetSelectable() {
   const orders = Array.from(Array(100).keys()).map((i) => ({
     id: `${i}`,
     order: i,
@@ -6138,6 +6195,21 @@ export function WithLongDataSet() {
     ),
   );
 
+  const bulkActions = [
+    {
+      content: 'Add tags',
+      onAction: () => console.log('Todo: implement bulk add tags'),
+    },
+    {
+      content: 'Remove tags',
+      onAction: () => console.log('Todo: implement bulk remove tags'),
+    },
+    {
+      content: 'Delete customers',
+      onAction: () => console.log('Todo: implement bulk delete'),
+    },
+  ];
+
   return (
     <LegacyCard>
       <IndexTable
@@ -6148,14 +6220,15 @@ export function WithLongDataSet() {
         }
         onSelectionChange={handleSelectionChange}
         headings={[
-          {title: 'Order'},
+          {title: 'Order', hidden: true},
           {title: 'Date'},
           {title: 'Customer'},
           {title: 'Total', alignment: 'end'},
           {title: 'Payment status'},
           {title: 'Fulfillment status'},
         ]}
-        selectable={false}
+        bulkActions={bulkActions}
+        selectable
       >
         {rowMarkup}
       </IndexTable>

polaris-react/src/components/IndexTable/IndexTable.tsx
@@ -1083,9 +1083,10 @@ function IndexTableBase({
   }
 
   function renderStickyHeading(heading: IndexTableHeading, index: number) {
+    const position = selectable ? index + 1 : index;
     const headingStyle =
-      tableHeadingRects.current && tableHeadingRects.current.length > index
-        ? {minWidth: tableHeadingRects.current[index].offsetWidth}
+      tableHeadingRects.current && tableHeadingRects.current.length > position
+        ? {minWidth: tableHeadingRects.current[position].offsetWidth}
         : undefined;
     const headingAlignment = heading.alignment || 'start';
 