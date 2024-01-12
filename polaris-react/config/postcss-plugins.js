ff46c9524c85a1335083ada0a12e42172ef55388
Jess Telford <jess.telford@shopify.com>
2024-01-12 16:25:04 +1100

    Add postcss-nesting to support CSS-spec compliant selector nesting with backwards compat for SASS

--- polaris-react/config/postcss-plugins.js
+++ polaris-react/config/postcss-plugins.js
@@ -5,6 +5,7 @@ const pxtorem = require('postcss-pxtorem');
 const postcssCustomMedia = require('postcss-custom-media');
 const postcssGlobalData = require('@csstools/postcss-global-data');
 const postcssAdvancedVariables = require('postcss-advanced-variables');
+const postcssNesting = require('postcss-nesting');
 
 const mediaQueriesCssPath = path.resolve(
   __dirname,
@@ -14,6 +15,16 @@ const mediaQueriesCssPath = path.resolve(
 module.exports = [
   // SASS-ish-support
   postcssAdvancedVariables(),
+
+  postcssNesting({
+    // The way native CSS nesting & SASS nesting behave with complex selectors
+    // differ; SASS expands out every selector into a comma separated list, but
+    // native CSS wraps the complex selectors in an `:is()` which can result in
+    // a different specificity. We favour the SASS convention here to ensure
+    // compatibility with our ported-in SASS styles.
+    // See: https://sass-lang.com/blog/sass-and-native-nesting/
+    noIsPseudoSelector: true,
+  }),
   postcssGlobalData({
     files: [mediaQueriesCssPath],
   }),
