49c9fa3c6af127c6c2741deaa7bffd23715a7cc6
Kyle Durand <kyledurand@users.noreply.github.com>
2024-01-30 09:02:21 -0500

    [Scrollable] Add new scrollbar properties (#11508)
    <!--
      ☝️How to write a good PR title:
    - Prefix it with [ComponentName] (if applicable), for example: [Button]
      - Start with a verb, for example: Add, Delete, Improve, Fix…
      - Give as much context as necessary and as little as possible
      - Open it as a draft if it’s a work in progress
    -->
    
    ### WHY are these changes introduced?
    
    Part of https://github.com/Shopify/polaris-internal/issues/1193
    
    scrollbar-width was just introduced to chrome / edge v121 but it's been
    available in firefox for some time now. We can make use of the feature
    as it rolls out to the remaining browsers.
    
    Example Navigation:
    
    Before | After
    ---|---
    
    ![image](https://github.com/Shopify/polaris/assets/6844391/1e0d1d13-47ca-49a2-abbb-29f07d4666b9)
    |
    ![image](https://github.com/Shopify/polaris/assets/6844391/ae311bca-3ecd-46c3-b374-e1d5551d3702)

---
+++ .changeset/silver-timers-lick.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris': minor
+---
+
+Added scrollbarWidth and scrollbarGutter properties to Scrollable
--- polaris-react/src/components/Scrollable/Scrollable.tsx
+++ polaris-react/src/components/Scrollable/Scrollable.tsx
@@ -8,7 +8,7 @@ import React, {
 } from 'react';
 
 import {debounce} from '../../utilities/debounce';
-import {classNames} from '../../utilities/css';
+import {classNames, variationName} from '../../utilities/css';
 import {
   StickyManager,
   StickyManagerContext,
@@ -41,6 +41,10 @@ export interface ScrollableProps extends React.HTMLProps<HTMLDivElement> {
   hint?: boolean;
   /** Adds a tabIndex to scrollable when children are not focusable */
   focusable?: boolean;
+  /** Browser determined scrollbar width */
+  scrollbarWidth?: 'thin' | 'none';
+  /** Adds space to one or both sides to prevent content shift when scrolling is necessary */
+  scrollbarGutter?: 'stable' | 'stable both-edges';
   /** Called when scrolled to the bottom of the scroll area */
   onScrolledToBottom?(): void;
 }
@@ -63,6 +67,8 @@ const ScrollableComponent = forwardRef<ScrollableRef, ScrollableProps>(
       shadow,
       hint,
       focusable,
+      scrollbarWidth,
+      scrollbarGutter,
       onScrolledToBottom,
       ...rest
     }: ScrollableProps,
@@ -144,6 +150,11 @@ const ScrollableComponent = forwardRef<ScrollableRef, ScrollableProps>(
       horizontal && styles.horizontal,
       shadow && topShadow && styles.hasTopShadow,
       shadow && bottomShadow && styles.hasBottomShadow,
+      scrollbarWidth && styles[variationName('scrollbarWidth', scrollbarWidth)],
+      scrollbarGutter &&
+        styles[
+          variationName('scrollbarGutter', scrollbarGutter.replace(' ', ''))
+        ],
     );
 
     return (
