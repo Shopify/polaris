293166c703d8414314918d76209820325ffbe461
Matt Gregg <matt.gregg@shopify.com>
2024-02-05 14:39:58 -0600

    Adjust documentation example

--- polaris.shopify.com/pages/examples/inline-stack-with-direction.tsx
+++ polaris.shopify.com/pages/examples/inline-stack-with-direction.tsx
@@ -6,7 +6,7 @@ import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
 function InlineWithDirectionExample() {
   return (
     <BlockStack gap="1600">
-      <InlineStack direction="row">
+      <InlineStack direction="row" align="center">
         <Placeholder width="106px" label="1" />
         <Placeholder width="106px" height="20px" showBorder />
         <Placeholder width="106px" height="20px" showBorder />
@@ -16,14 +16,14 @@ function InlineWithDirectionExample() {
         <Placeholder width="106px" height="20px" showBorder />
       </InlineStack>
       <Divider />
-      <InlineStack direction="row-reverse">
-        <Placeholder width="106px" label="1" />
+      <InlineStack direction="row-reverse" align="center">
+        <Placeholder width="106px" label="1" showBorder />
         <Placeholder width="106px" height="20px" showBorder />
         <Placeholder width="106px" height="20px" showBorder />
         <Placeholder width="106px" height="20px" showBorder />
         <Placeholder width="106px" height="20px" showBorder />
         <Placeholder width="106px" height="20px" showBorder />
-        <Placeholder width="106px" height="20px" showBorder />
+        <Placeholder width="106px" height="20px" />
       </InlineStack>
     </BlockStack>
   );
