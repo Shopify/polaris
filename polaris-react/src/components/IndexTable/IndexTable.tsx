1f946bef017f0783da7c8b241b4fb03a05c33245
Marc Thomas <marc.thomas@shopify.com>
2024-02-09 15:55:05 +0000

    chore: testing

--- polaris-react/src/components/IndexTable/IndexTable.tsx
+++ polaris-react/src/components/IndexTable/IndexTable.tsx
@@ -653,6 +653,14 @@ function IndexTableBase({
   const promotedActions = shouldShowActions ? promotedBulkActions : [];
   const actions = shouldShowActions ? bulkActions : [];
 
+  // eslint-disable-next-line no-console
+  console.log({
+    selectAllActionsOffsetBottom,
+    isSelectAllActionsSticky,
+    tableMeasurerRef,
+    selectAllActionsIntersectionRef,
+  });
+
   const selectAllActionsMarkup =
     shouldShowActions && !condensed ? (
       <div
