0a0f3c69a8001e5f308ed5b0dd84209b6f7a1fb1
Laura Griffee <laura@mailzone.com>
2024-02-13 09:12:58 -0700

    Add v13 primitive font token migration

---
+++ polaris-migrator/src/migrations/v13-styles-replace-custom-property-font/transform.ts
@@ -0,0 +1,20 @@
+import type {FileInfo, API} from 'jscodeshift';
+
+import stylesReplaceCustomProperty from '../styles-replace-custom-property/transform';
+
+export default function transformer(fileInfo: FileInfo, _: API) {
+  return stylesReplaceCustomProperty(fileInfo, _, {replacementMaps});
+}
+
+const replacementMaps = {
+  '/.+/': {
+    '--p-font-size-750': '--p-font-size-600',
+    '--p-font-size-800': '--p-font-size-600',
+    '--p-font-size-900': '--p-font-size-600',
+    '--p-font-size-1000': '--p-font-size-600',
+    '--p-font-letter-spacing-denser': '--p-font-letter-spacing-dense',
+    '--p-font-letter-spacing-densest': '--p-font-letter-spacing-dense',
+    '--p-font-line-height-1000': '--p-font-line-height-800',
+    '--p-font-line-height-1200': '--p-font-line-height-800',
+  },
+};
