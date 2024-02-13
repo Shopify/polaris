55ca59f9f63c3edbf985ba6abc7a66117667a8f6
Laura Griffee <laura@mailzone.com>
2024-02-13 12:28:46 -0700

    Add migrations for composite text tokens

--- polaris-migrator/src/migrations/v13-styles-replace-custom-property-font/transform.ts
+++ polaris-migrator/src/migrations/v13-styles-replace-custom-property-font/transform.ts
@@ -16,5 +16,19 @@ const replacementMaps = {
     '--p-font-letter-spacing-densest': '--p-font-letter-spacing-dense',
     '--p-font-line-height-1000': '--p-font-line-height-800',
     '--p-font-line-height-1200': '--p-font-line-height-800',
+    '--p-text-heading-3xl-font-family': '--p-text-heading-xl-font-family',
+    '--p-text-heading-3xl-font-size': '--p-text-heading-xl-font-size',
+    '--p-text-heading-3xl-font-weight': '--p-text-heading-xl-font-weight',
+    '--p-text-heading-3xl-font-letter-spacing':
+      '--p-text-heading-xl-font-letter-spacing',
+    '--p-text-heading-3xl-font-line-height':
+      '--p-text-heading-xl-font-line-height',
+    '--p-text-heading-2xl-font-family': '--p-text-heading-xl-font-family',
+    '--p-text-heading-2xl-font-size': '--p-text-heading-xl-font-size',
+    '--p-text-heading-2xl-font-weight': '--p-text-heading-xl-font-weight',
+    '--p-text-heading-2xl-font-letter-spacing':
+      '--p-text-heading-xl-font-letter-spacing',
+    '--p-text-heading-2xl-font-line-height':
+      '--p-text-heading-xl-font-line-height',
   },
 };
