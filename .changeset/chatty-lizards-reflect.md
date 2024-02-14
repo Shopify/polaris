2c53d64762ff545f56787e932e0b0935c45b5fdc
Laura Griffee <laura@mailzone.com>
2024-02-14 10:52:36 -0700

    Update v13-styles-replace-custom-property-font migration (#11603)
    Reverting the incorrect removal of the `font-size-800` ➡️
    `font-size-600` migration

---
+++ .changeset/chatty-lizards-reflect.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris-migrator': patch
+---
+
+Updated migration to replace deprecated `font` custom properties in polaris-react v13.0.0
--- polaris-migrator/src/migrations/v13-styles-replace-custom-property-font/transform.ts
+++ polaris-migrator/src/migrations/v13-styles-replace-custom-property-font/transform.ts
@@ -9,6 +9,7 @@ export default function transformer(fileInfo: FileInfo, _: API) {
 const replacementMaps = {
   '/.+/': {
     '--p-font-size-750': '--p-font-size-600',
+    '--p-font-size-800': '--p-font-size-600',
     '--p-font-size-900': '--p-font-size-600',
     '--p-font-size-1000': '--p-font-size-600',
     '--p-font-letter-spacing-denser': '--p-font-letter-spacing-dense',
