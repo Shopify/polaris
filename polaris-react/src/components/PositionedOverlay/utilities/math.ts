390b08a2dad376b10132b759c06d64fb96c1ea71
Chloe Rice <chloe.rice@shopify.com>
2024-02-14 04:26:10 -0500

    remove logs

--- polaris-react/src/components/PositionedOverlay/utilities/math.ts
+++ polaris-react/src/components/PositionedOverlay/utilities/math.ts
@@ -129,7 +129,7 @@ export function calculateHorizontalPosition(
   containerRect: Rect,
   overlayMargins: Margins,
   preferredAlignment: PreferredAlignment,
-  scrollableContainerRect: Rect,
+  _scrollableContainerRect: Rect,
   preferredHorizontalPosition?: 'left' | 'right',
   overlayMinWidth = 0,
 ) {
@@ -151,37 +151,6 @@ export function calculateHorizontalPosition(
     (overlayMinWidth || desiredWidth) + minimumSurroundingSpace;
 
   if (!preferredHorizontalPosition) {
-    console.table([
-      {variable: 'maximumWidth', value: maximumWidth},
-      {variable: 'overlayMinWidth', value: overlayMinWidth},
-      {variable: 'activatorRect.width', value: activatorRect.width},
-      {variable: 'overlayRect.width', value: overlayRect.width},
-      {variable: 'containerRect.width', value: containerRect.width},
-      {
-        variable: 'activatorRect.left',
-        value: activatorRect.left,
-      },
-      {
-        variable: 'activatorRect.right',
-        value: activatorRect.right,
-      },
-      {
-        variable: 'enoughSpaceFromLeftEdge',
-        value: enoughSpaceFromLeftEdge,
-      },
-      {
-        variable: 'enoughSpaceFromRightEdge',
-        value: enoughSpaceFromRightEdge,
-      },
-      {
-        variable: 'distanceToLeftEdge',
-        value: distanceToLeftEdge,
-      },
-      {
-        variable: 'distanceToRightEdge',
-        value: distanceToRightEdge,
-      },
-    ]);
     if (preferredAlignment === 'left') {
       return {
         left: Math.min(
@@ -212,76 +181,6 @@ export function calculateHorizontalPosition(
     const widthIfRight = enoughSpaceFromRightEdge
       ? desiredWidth
       : Math.min(distanceToRightEdge - minimumSurroundingSpace, desiredWidth);
-
-    console.table([
-      {variable: 'maximumWidth', value: maximumWidth},
-      {variable: 'overlayMinWidth', value: overlayMinWidth},
-      {variable: 'activatorRect.width', value: activatorRect.width},
-      {variable: 'overlayRect.width', value: overlayRect.width},
-      {variable: 'containerRect.width', value: containerRect.width},
-      {
-        variable: 'scrollableContainerRect.width',
-        value: scrollableContainerRect.width,
-      },
-      {
-        variable: 'widthIfLeft',
-        value: widthIfLeft,
-      },
-      {
-        variable: 'widthIfRight',
-        value: widthIfRight,
-      },
-
-      {
-        variable: 'activatorRect.left',
-        value: activatorRect.left,
-      },
-      {
-        variable: 'containerRect.left',
-        value: containerRect.left,
-      },
-      {
-        variable: 'scrollableContainerRect.left',
-        value: scrollableContainerRect.left,
-      },
-      {
-        variable: 'activatorRect.right',
-        value: activatorRect.right,
-      },
-      {
-        variable: 'containerRect.right',
-        value: containerRect.right,
-      },
-      {
-        variable: 'scrollableContainerRect.right',
-        value: scrollableContainerRect.right,
-      },
-      {
-        variable: 'enoughSpaceFromLeftEdge',
-        value: enoughSpaceFromLeftEdge,
-      },
-      {
-        variable: 'enoughSpaceFromRightEdge',
-        value: enoughSpaceFromRightEdge,
-      },
-      {
-        variable: 'distanceToLeftEdge',
-        value: distanceToLeftEdge,
-      },
-      {
-        variable: 'distanceToRightEdge',
-        value: distanceToRightEdge,
-      },
-
-      {
-        variable: 'positionIfLeft',
-        value: positionIfLeft,
-      },
-      {
-        variable: 'positionIfRight',
-        value: positionIfRight,
-      },
-    ]);
 
     if (preferredHorizontalPosition === 'right') {
       return enoughSpaceFromRightEdge
