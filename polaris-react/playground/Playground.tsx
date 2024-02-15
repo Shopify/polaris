9b848f9ad7f9d97efe46f7419ed0247742a005ce
Sam Rose <11774595+sam-b-rose@users.noreply.github.com>
2024-02-16 09:33:27 +1300

    Playground example

--- polaris-react/playground/Playground.tsx
+++ polaris-react/playground/Playground.tsx
@@ -1,11 +1,68 @@
 import React from 'react';
 
-import {Page} from '../src';
+import {
+  ChartStackedIcon,
+  CollectionFeaturedIcon,
+  CollectionFilledIcon,
+  CollectionIcon,
+  CollectionReferenceIcon,
+  DiscountCodeIcon,
+  DuplicateIcon,
+  EnterIcon,
+  ExitIcon,
+  EyeDropperIcon,
+  GiftCardFilledIcon,
+  GiftCardIcon,
+  ImagesIcon,
+  LayoutBuyButtonHorizontalIcon,
+  LayoutBuyButtonIcon,
+  LayoutBuyButtonVerticalIcon,
+  LayoutColumn1Icon,
+  LayoutColumns2Icon,
+  LayoutColumns3Icon,
+  LayoutPopupIcon,
+  LayoutSectionIcon,
+  MeasurementSizeIcon,
+  MoneyFilledIcon,
+  MoneyIcon,
+  MoneyNoneIcon,
+  VariantIcon,
+  WandIcon,
+  WifiIcon,
+} from '../../polaris-icons/dist';
+import {Page, Icon} from '../src';
 
 export function Playground() {
   return (
     <Page title="Playground">
-      {/* Add the code you want to test in here */}
+      <Icon source={ChartStackedIcon} />
+      <Icon source={CollectionFeaturedIcon} />
+      <Icon source={CollectionFilledIcon} />
+      <Icon source={CollectionIcon} />
+      <Icon source={CollectionReferenceIcon} />
+      <Icon source={DiscountCodeIcon} />
+      <Icon source={DuplicateIcon} />
+      <Icon source={EnterIcon} />
+      <Icon source={ExitIcon} />
+      <Icon source={EyeDropperIcon} />
+      <Icon source={GiftCardFilledIcon} />
+      <Icon source={GiftCardIcon} />
+      <Icon source={ImagesIcon} />
+      <Icon source={LayoutBuyButtonHorizontalIcon} />
+      <Icon source={LayoutBuyButtonIcon} />
+      <Icon source={LayoutBuyButtonVerticalIcon} />
+      <Icon source={LayoutColumn1Icon} />
+      <Icon source={LayoutColumns2Icon} />
+      <Icon source={LayoutColumns3Icon} />
+      <Icon source={LayoutPopupIcon} />
+      <Icon source={LayoutSectionIcon} />
+      <Icon source={MeasurementSizeIcon} />
+      <Icon source={MoneyFilledIcon} />
+      <Icon source={MoneyIcon} />
+      <Icon source={MoneyNoneIcon} />
+      <Icon source={VariantIcon} />
+      <Icon source={WandIcon} />
+      <Icon source={WifiIcon} />
     </Page>
   );
 }
