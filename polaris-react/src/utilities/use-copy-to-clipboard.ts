f50e41bd77c4f1f749eec46387cd5f5084d338ad
Aaron Casanova <aaron.casanova@shopify.com>
2024-02-22 12:14:43 -0800

    Initial Copy To Clipboard recipe

---
+++ polaris-react/src/utilities/use-copy-to-clipboard.ts
@@ -0,0 +1,43 @@
+import React from 'react';
+
+type Status = 'inactive' | 'copied' | 'failed';
+
+interface UseCopyToClipboardOptions {
+  defaultValue?: string;
+  timeout?: number;
+}
+
+/**
+ * Copy text to the native clipboard using the `navigator.clipboard` API
+ * Adapted from https://www.benmvp.com/blog/copy-to-clipboard-react-custom-hook
+ */
+export function useCopyToClipboard(options: UseCopyToClipboardOptions = {}) {
+  const {defaultValue = '', timeout = 2500} = options;
+
+  const [status, setStatus] = React.useState<Status>('inactive');
+
+  const copy = React.useCallback(
+    (value?: string) => {
+      navigator.clipboard
+        .writeText(typeof value === 'string' ? value : defaultValue)
+        .then(
+          () => setStatus('copied'),
+          () => setStatus('failed'),
+        )
+        .catch((error) => {
+          throw error;
+        });
+    },
+    [defaultValue],
+  );
+
+  React.useEffect(() => {
+    if (status === 'inactive') return;
+
+    const timeoutId = setTimeout(() => setStatus('inactive'), timeout);
+
+    return () => clearTimeout(timeoutId);
+  }, [status, timeout]);
+
+  return [copy, status] as const;
+}
