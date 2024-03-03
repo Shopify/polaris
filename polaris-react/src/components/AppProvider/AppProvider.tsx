d174a4c5e8e46292caba75b72e722612b0f5f223
Charles Lee <charles.lee@shopify.com>
2024-03-04 10:47:18 +1100

    Rename AppProvider.scss to global.scss

--- polaris-react/src/components/AppProvider/AppProvider.tsx
+++ polaris-react/src/components/AppProvider/AppProvider.tsx
@@ -25,7 +25,7 @@ import type {LinkLikeComponent} from '../../utilities/link';
 import {FeaturesContext} from '../../utilities/features';
 import type {FeaturesConfig} from '../../utilities/features';
 
-import './AppProvider.scss';
+import './global.scss';
 
 const MAX_SCROLLBAR_WIDTH = 20;
 const SCROLLBAR_TEST_ELEMENT_PARENT_SIZE = 30;
