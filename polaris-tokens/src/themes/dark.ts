c56e03b31d6024785da5a50d9871e1e30e50dc00
Aaron Casanova <aaron.casanova@shopify.com>
2024-03-05 12:39:33 -0800

    Refine dark theme

--- polaris-tokens/src/themes/dark.ts
+++ polaris-tokens/src/themes/dark.ts
@@ -5,12 +5,15 @@ import {createMetaTheme, createMetaThemePartial} from './utils';
 export const metaThemeDarkPartial = createMetaThemePartial({
   color: {
     'color-bg': {value: colors.gray[16]},
-    'color-bg-inverse': {value: colors.gray[16]},
     'color-bg-surface': {value: colors.gray[15]},
+    'color-bg-fill-disabled': {value: colors.gray[15]},
+    'color-bg-fill-secondary': {value: colors.gray[13]},
     'color-text': {value: colors.gray[3]},
     'color-text-secondary': {value: colors.gray[6]},
     'color-text-disabled': {value: colors.gray[13]},
     'color-text-brand': {value: colors.blue[14]},
+    'color-icon': {value: colors.gray[11]},
+    'color-icon-disabled': {value: colors.gray[13]},
   },
 });
 
