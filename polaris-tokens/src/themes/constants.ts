e5cde368954176bb9af3fa6da53d7b5e5243ca63
Aaron Casanova <aaron.casanova@shopify.com>
2024-03-07 10:58:50 -0800

    Refine constants to use themeNameDefault

--- polaris-tokens/src/themes/constants.ts
+++ polaris-tokens/src/themes/constants.ts
@@ -2,16 +2,16 @@ export const themeNameLight = 'light';
 export const themeNameDefault = themeNameLight;
 
 export const themeNames = [
-  themeNameLight,
+  themeNameDefault,
   'light-mobile',
   'light-high-contrast-experimental',
   'dark',
 ] as const;
 
 export const themeNamesGlobal = [
-  themeNameLight,
+  themeNameDefault,
   'light-mobile',
   'light-high-contrast-experimental',
 ] as const;
 
-export const themeNamesLocal = [themeNameLight, 'dark'] as const;
+export const themeNamesLocal = [themeNameDefault, 'dark'] as const;
