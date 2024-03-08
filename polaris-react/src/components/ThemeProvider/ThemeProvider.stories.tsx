3063f922eb65e8ba247db9f2fe8811af8a2e30cb
Aaron Casanova <aaron.casanova@shopify.com>
2024-03-08 11:36:29 -0800

    Update storybook entry

--- polaris-react/src/components/ThemeProvider/ThemeProvider.stories.tsx
+++ polaris-react/src/components/ThemeProvider/ThemeProvider.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useState} from 'react';
 import type {ComponentMeta} from '@storybook/react';
 import {
   Button,
@@ -21,8 +21,39 @@ export default {
   component: ThemeProvider,
 } as ComponentMeta<typeof ThemeProvider>;
 
+const sections = [
+  {
+    title: 'File options',
+    items: [
+      {
+        active: true,
+        content: 'Import file',
+        icon: ImportIcon,
+      },
+      {content: 'Export file', icon: ExportIcon},
+      {
+        destructive: true,
+        content: 'Delete file',
+        icon: DeleteIcon,
+      },
+    ],
+  },
+];
+
 export function Default() {
   const breakpoints = useBreakpoints();
+
+  const [lightThemeLightPopoverActive, setLightThemeLightPopoverActive] =
+    useState(false);
+
+  const [darkThemeDarkPopoverActive, setDarkThemeDarkPopoverActive] =
+    useState(false);
+
+  const [lightThemeDarkPopoverActive, setLightThemeDarkPopoverActive] =
+    useState(false);
+
+  const [darkThemeLightPopoverActive, setDarkThemeLightPopoverActive] =
+    useState(false);
 
   return (
     <div
@@ -32,96 +63,152 @@ export function Default() {
         gap: 20,
       }}
     >
-      {Array.from({length: 5}, (_, index) => (
-        <Card key={index}>
-          <div style={{display: 'grid', gap: 'var(--p-space-300)'}}>
-            <div
-              style={{
-                display: 'grid',
-                gridTemplateColumns: '1fr auto',
-                gap: 'var(--p-space-200)',
-              }}
+      <CustomerCard
+        moreActions={
+          <Popover
+            active={lightThemeLightPopoverActive}
+            autofocusTarget="first-node"
+            onClose={() => setLightThemeLightPopoverActive((active) => !active)}
+            activator={
+              <Button
+                variant="tertiary"
+                icon={MenuHorizontalIcon}
+                onClick={() =>
+                  setLightThemeLightPopoverActive((active) => !active)
+                }
+              />
+            }
+          >
+            <ActionList actionRole="menuitem" sections={sections} />
+          </Popover>
+        }
+      />
+      <ThemeProvider theme="dark">
+        <CustomerCard
+          moreActions={
+            <Popover
+              active={darkThemeDarkPopoverActive}
+              autofocusTarget="first-node"
+              onClose={() => setDarkThemeDarkPopoverActive((active) => !active)}
+              activator={
+                <Button
+                  variant="tertiary"
+                  icon={MenuHorizontalIcon}
+                  onClick={() =>
+                    setDarkThemeDarkPopoverActive((active) => !active)
+                  }
+                />
+              }
             >
-              <Text as="h2" variant="headingSm">
-                Customer
-              </Text>
-              <MoreActionsMenu />
-              <Text as="p" variant="bodyMd">
-                John Smith
-              </Text>
-            </div>
-            <div
-              style={{
-                display: 'grid',
-                gridTemplateColumns: '1fr auto',
-              }}
+              <ActionList actionRole="menuitem" sections={sections} />
+            </Popover>
+          }
+        />
+      </ThemeProvider>
+      <CustomerCard
+        moreActions={
+          <ThemeProvider theme="dark">
+            <Popover
+              active={lightThemeDarkPopoverActive}
+              autofocusTarget="first-node"
+              onClose={() =>
+                setLightThemeDarkPopoverActive((active) => !active)
+              }
+              activator={
+                <ThemeProvider theme="light">
+                  <Button
+                    variant="tertiary"
+                    icon={MenuHorizontalIcon}
+                    onClick={() =>
+                      setLightThemeDarkPopoverActive((active) => !active)
+                    }
+                  />
+                </ThemeProvider>
+              }
             >
-              <Text as="h3" variant="headingSm" fontWeight="medium">
-                Contact Information!
-              </Text>
-              <Button icon={ClipboardIcon} variant="tertiary" />
-              <Text as="p" variant="bodyMd">
-                john.smith@example.com
-              </Text>
-              <span />
-            </div>
-            <div>
-              <Text as="h3" variant="headingSm" fontWeight="medium">
-                Address
-              </Text>
-              <div />
-              <Text as="p" variant="bodyMd">
-                1234 Street
-                <br />
-                Anytown, CA, 12345
-              </Text>
-            </div>
-          </div>
-        </Card>
-      ))}
+              <ActionList actionRole="menuitem" sections={sections} />
+            </Popover>
+          </ThemeProvider>
+        }
+      />
+      <ThemeProvider theme="dark">
+        <CustomerCard
+          moreActions={
+            <ThemeProvider theme="light">
+              <Popover
+                active={darkThemeLightPopoverActive}
+                autofocusTarget="first-node"
+                onClose={() =>
+                  setDarkThemeLightPopoverActive((active) => !active)
+                }
+                activator={
+                  <ThemeProvider theme="dark">
+                    <Button
+                      variant="tertiary"
+                      icon={MenuHorizontalIcon}
+                      onClick={() =>
+                        setDarkThemeLightPopoverActive((active) => !active)
+                      }
+                    />
+                  </ThemeProvider>
+                }
+              >
+                <ActionList actionRole="menuitem" sections={sections} />
+              </Popover>
+            </ThemeProvider>
+          }
+        />
+      </ThemeProvider>
     </div>
   );
 }
 
-function MoreActionsMenu() {
-  const [active, setActive] = useState(false);
-
-  const toggleActive = useCallback(() => setActive((active) => !active), []);
-
+function CustomerCard(props: {moreActions: React.ReactNode}) {
   return (
-    <Popover
-      active={active}
-      autofocusTarget="first-node"
-      onClose={toggleActive}
-      activator={
-        <Button
-          variant="tertiary"
-          icon={MenuHorizontalIcon}
-          onClick={toggleActive}
-        />
-      }
-    >
-      <ActionList
-        actionRole="menuitem"
-        sections={[
-          {
-            title: 'File options',
-            items: [
-              {
-                active: true,
-                content: 'Import file',
-                icon: ImportIcon,
-              },
-              {content: 'Export file', icon: ExportIcon},
-              {
-                destructive: true,
-                content: 'Delete file',
-                icon: DeleteIcon,
-              },
-            ],
-          },
-        ]}
-      />
-    </Popover>
+    <Card>
+      <div style={{display: 'grid', gap: 'var(--p-space-300)'}}>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr auto',
+            gap: 'var(--p-space-200)',
+          }}
+        >
+          <Text as="h2" variant="headingSm">
+            Customer
+          </Text>
+          {props.moreActions}
+          <Text as="p" variant="bodyMd">
+            John Smith!
+          </Text>
+        </div>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr auto',
+          }}
+        >
+          <Text as="h3" variant="headingSm" fontWeight="medium">
+            Contact Information!
+          </Text>
+          <Button icon={ClipboardIcon} variant="tertiary" />
+          <Text as="p" variant="bodyMd">
+            john.smith@example.com
+          </Text>
+          <span />
+        </div>
+        <div>
+          <Text as="h3" variant="headingSm" fontWeight="medium">
+            Address
+          </Text>
+          <div />
+          <Text as="p" variant="bodyMd">
+            1234 Street
+            <br />
+            Anytown, CA, 12345
+          </Text>
+        </div>
+      </div>
+    </Card>
   );
 }
