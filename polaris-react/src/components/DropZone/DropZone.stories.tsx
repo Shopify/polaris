5df6c82eb82cc0c13c9c698d82dc8803ffdf32b5
Chloe Rice <chloe.rice@shopify.com>
2024-03-11 14:25:01 -0400

    Changes requested in PR review

--- polaris-react/src/components/DropZone/DropZone.stories.tsx
+++ polaris-react/src/components/DropZone/DropZone.stories.tsx
@@ -1,6 +1,7 @@
 import React, {useCallback, useState} from 'react';
 import type {ComponentMeta} from '@storybook/react';
 import {
+  Box,
   Banner,
   Text,
   LegacyCard,
@@ -14,14 +15,14 @@ import {
   Modal,
   Icon,
 } from '@shopify/polaris';
-import {ImageAddIcon, NoteIcon, PlusCircleIcon} from '@shopify/polaris-icons';
+import {ImageAddIcon, NoteIcon} from '@shopify/polaris-icons';
 
 export default {
   component: DropZone,
 } as ComponentMeta<typeof DropZone>;
 
 export function Default() {
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState<File[]>([]);
 
   const handleDropZoneDrop = useCallback(
     (_dropFiles, acceptedFiles, _rejectedFiles) =>
@@ -75,8 +76,8 @@ export function WithALabel() {
 }
 
 export function WithImageFileUpload() {
-  const [files, setFiles] = useState([]);
-  const [rejectedFiles, setRejectedFiles] = useState([]);
+  const [files, setFiles] = useState<File[]>([]);
+  const [rejectedFiles, setRejectedFiles] = useState<File[]>([]);
   const hasError = rejectedFiles.length > 0;
 
   const handleDrop = useCallback(
@@ -89,30 +90,29 @@ export function WithImageFileUpload() {
 
   const fileUpload = !files.length && <DropZone.FileUpload />;
   const uploadedFiles = files.length > 0 && (
-    <BlockStack gap="400">
-      {files.map((file, index) => (
-        <InlineStack gap="400" align="center" key={index}>
-          <Thumbnail
-            size="small"
-            alt={file.name}
-            source={window.URL.createObjectURL(file)}
-          />
-          <div>
-            {file.name}{' '}
-            <Text variant="bodySm" as="p">
-              {file.size} bytes
-            </Text>
-          </div>
-        </InlineStack>
-      ))}
-    </BlockStack>
+    <Box width="100%" padding="400">
+      <BlockStack gap="400">
+        {files.map((file, index) => (
+          <InlineStack gap="400" align="start" key={index}>
+            <Thumbnail
+              size="small"
+              alt={file.name}
+              source={window.URL.createObjectURL(file)}
+            />
+            <div>
+              {file.name}{' '}
+              <Text variant="bodySm" as="p" tone="subdued">
+                {file.size} bytes
+              </Text>
+            </div>
+          </InlineStack>
+        ))}
+      </BlockStack>
+    </Box>
   );
 
   const errorMessage = hasError && (
-    <Banner
-      title="The following images couldn’t be uploaded:"
-      status="critical"
-    >
+    <Banner title="The following images couldn’t be uploaded:" tone="critical">
       <List type="bullet">
         {rejectedFiles.map((file, index) => (
           <List.Item key={index}>
@@ -124,7 +124,7 @@ export function WithImageFileUpload() {
   );
 
   return (
-    <BlockStack gap="400">
+    <BlockStack gap="400" inlineAlign="stretch" align="center">
       {errorMessage}
       <DropZone accept="image/*" type="image" onDrop={handleDrop}>
         {uploadedFiles}
@@ -135,7 +135,7 @@ export function WithImageFileUpload() {
 }
 
 export function WithSingleFileUpload() {
-  const [file, setFile] = useState();
+  const [file, setFile] = useState<File>();
 
   const handleDropZoneDrop = useCallback(
     (_dropFiles, acceptedFiles, _rejectedFiles) =>
@@ -175,7 +175,7 @@ export function WithSingleFileUpload() {
 }
 
 export function WithDropOnPage() {
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState<File[]>([]);
 
   const handleDropZoneDrop = useCallback(
     (dropFiles, _acceptedFiles, _rejectedFiles) =>
@@ -213,7 +213,7 @@ export function WithDropOnPage() {
 
   return (
     <Page
-      backAction={{content: 'Products'}}
+      backAction={{content: 'Products', url: '#'}}
       title="Jar With Lock-Lid"
       primaryAction={{content: 'Save', disabled: true}}
       secondaryActions={[
@@ -234,8 +234,8 @@ export function WithDropOnPage() {
 }
 
 export function AcceptsOnlySVGFiles() {
-  const [files, setFiles] = useState([]);
-  const [rejectedFiles, setRejectedFiles] = useState([]);
+  const [files, setFiles] = useState<File[]>([]);
+  const [rejectedFiles, setRejectedFiles] = useState<File[]>([]);
   const hasError = rejectedFiles.length > 0;
 
   const handleDropZoneDrop = useCallback(
@@ -267,10 +267,7 @@ export function AcceptsOnlySVGFiles() {
   );
 
   const errorMessage = hasError && (
-    <Banner
-      title="The following images couldn’t be uploaded:"
-      status="critical"
-    >
+    <Banner title="The following images couldn’t be uploaded:" tone="critical">
       <List type="bullet">
         {rejectedFiles.map((file, index) => (
           <List.Item key={index}>
@@ -297,7 +294,7 @@ export function AcceptsOnlySVGFiles() {
 }
 
 export function Nested() {
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState<File[]>([]);
 
   const handleDrop = useCallback((dropFiles) => {
     setFiles((files) => [...files, dropFiles]);
@@ -427,8 +424,155 @@ export function SmallSized() {
   );
 }
 
+export function SmallSizedWithCustomFilePicker() {
+  const [activeModal, setActiveModal] = useState(false);
+  const [files, setFiles] = useState<File[]>([]);
+  const [rejectedFiles, setRejectedFiles] = useState<File[]>([]);
+  const hasError = rejectedFiles.length > 0;
+
+  const handleThumbnailDrop = useCallback(
+    (_droppedFiles: File[], acceptedFiles: File[], rejectedFiles: File[]) => {
+      setFiles((files) => [...files, acceptedFiles[0]]);
+      setRejectedFiles(rejectedFiles);
+    },
+    [],
+  );
+
+  const handleModalToggle = () => setActiveModal((activeModal) => !activeModal);
+
+  const handleKeypress = (event: React.KeyboardEvent) => {
+    event.stopPropagation();
+    if (event.key === 'Enter' || event.key === 'Space') {
+      handleModalToggle();
+    }
+  };
+
+  const thumbnailStyle = {
+    background: 'none',
+    border: 'none',
+    padding: 0,
+    borderRadius: 'var(--p-border-radius-200)',
+    outlineOffset: 'var(--p-border-width-050)',
+    cursor: 'pointer',
+    overflow: 'hidden',
+    minHeight: '0',
+    minWidth: '0',
+    display: 'flex',
+  };
+
+  const fileUploadThumbnail = (
+    <div style={{width: '40px', height: '40px'}}>
+      <DropZone
+        dragAndDropOnly
+        error={hasError}
+        allowMultiple={false}
+        onClick={handleModalToggle}
+        onDrop={handleThumbnailDrop}
+      >
+        <UnstyledButton style={thumbnailStyle} onKeyUp={handleKeypress}>
+          <Thumbnail
+            alt="Add variant image"
+            size="small"
+            source={ImageAddIcon}
+          />
+        </UnstyledButton>
+      </DropZone>
+    </div>
+  );
+
+  const uploadImageModal = !files.length ? (
+    <div style={{height: '500px'}}>
+      <Modal
+        activator={fileUploadThumbnail}
+        size="small"
+        open={activeModal}
+        onClose={handleModalToggle}
+        title="Upload images from store gallery"
+        primaryAction={{
+          content: 'Save',
+          onAction: handleModalToggle,
+        }}
+        secondaryActions={[
+          {
+            content: 'Cancel',
+            onAction: handleModalToggle,
+          },
+        ]}
+      >
+        <Modal.Section>
+          <ImageFileUpload />
+        </Modal.Section>
+      </Modal>
+    </div>
+  ) : null;
+
+  return uploadImageModal;
+
+  function ImageFileUpload() {
+    const [files, setFiles] = useState<File[]>([]);
+    const [rejectedFiles, setRejectedFiles] = useState<File[]>([]);
+    const hasError = rejectedFiles.length > 0;
+
+    const handleDrop = useCallback(
+      (_droppedFiles, acceptedFiles, rejectedFiles) => {
+        setFiles((files) => [...files, ...acceptedFiles]);
+        setRejectedFiles(rejectedFiles);
+      },
+      [],
+    );
+
+    const fileUpload = !files.length && <DropZone.FileUpload />;
+    const uploadedFiles = files.length > 0 && (
+      <Box width="100%" padding="400">
+        <BlockStack gap="400">
+          {files.map((file, index) => (
+            <InlineStack gap="400" align="start" key={index}>
+              <Thumbnail
+                size="small"
+                alt={file.name}
+                source={window.URL.createObjectURL(file)}
+              />
+              <div>
+                {file.name}{' '}
+                <Text variant="bodySm" as="p" tone="subdued">
+                  {file.size} bytes
+                </Text>
+              </div>
+            </InlineStack>
+          ))}
+        </BlockStack>
+      </Box>
+    );
+
+    const errorMessage = hasError && (
+      <Banner
+        title="The following images couldn’t be uploaded:"
+        tone="critical"
+      >
+        <List type="bullet">
+          {rejectedFiles.map((file, index) => (
+            <List.Item key={index}>
+              {`"${file.name}" is not supported. File type must be .gif, .jpg, .png or .svg.`}
+            </List.Item>
+          ))}
+        </List>
+      </Banner>
+    );
+
+    return (
+      <BlockStack gap="400" inlineAlign="stretch" align="center">
+        {errorMessage}
+        <DropZone accept="image/*" type="image" onDrop={handleDrop}>
+          {uploadedFiles}
+          {fileUpload}
+        </DropZone>
+      </BlockStack>
+    );
+  }
+}
+
 export function WithCustomFileUploadText() {
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState<File[]>([]);
 
   const handleDropZoneDrop = useCallback(
     (_dropFiles, acceptedFiles, _rejectedFiles) =>
@@ -475,7 +619,7 @@ export function WithCustomFileUploadText() {
 }
 
 export function WithCustomFileDialogTrigger() {
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState<File[]>([]);
   const [openFileDialog, setOpenFileDialog] = useState(false);
 
   const handleDropZoneDrop = useCallback(
@@ -536,110 +680,8 @@ export function WithCustomFileDialogTrigger() {
   );
 }
 
-export function WithCustomModalAndDragAndDropOnly() {
-  const [files, setFiles] = useState([]);
-  const [activeModal, setActiveModal] = useState(false);
-
-  const handleDrop = useCallback((dropFiles) => {
-    setFiles((files) => [...files, dropFiles]);
-  }, []);
-
-  const handleOpenModal = useCallback(
-    () =>
-      setActiveModal((activeModal) => {
-        console.log(activeModal);
-        return !activeModal;
-      }),
-    [],
-  );
-
-  const handleOpenModalFromChildElement = useCallback(
-    () => (event) => {
-      event.stopPropagation();
-      setActiveModal((activeModal) => !activeModal);
-    },
-    [],
-  );
-
-  const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-  const fileUpload = !files.length && (
-    <UnstyledButton
-      onClick={handleOpenModalFromChildElement}
-      style={{width: 40, height: 40, border: 0, cursor: 'pointer'}}
-      accessibilityLabel="Upload image for product Green T-shirt size Small"
-    >
-      <Icon source={ImageAddIcon} tone="interactive" />
-    </UnstyledButton>
-  );
-
-  const uploadImageModal = (
-    <div style={{height: '500px'}}>
-      <Modal
-        size="small"
-        open={activeModal}
-        onClose={handleOpenModal}
-        title="Upload images from store gallery"
-        primaryAction={{
-          content: 'Save',
-          onAction: handleOpenModal,
-        }}
-        secondaryActions={[
-          {
-            content: 'Cancel',
-            onAction: handleOpenModal,
-          },
-        ]}
-      >
-        <Modal.Section>
-          <DropZone accept=".png" type="file" onDrop={() => {}}>
-            <DropZone.FileUpload />
-          </DropZone>
-        </Modal.Section>
-      </Modal>
-    </div>
-  );
-
-  const uploadedFiles = files.length > 0 && (
-    <BlockStack gap="400">
-      {files.map((file, index) => (
-        <InlineStack gap="400" align="center" key={index}>
-          <Thumbnail
-            size="small"
-            alt={file.name}
-            source={
-              validImageTypes.includes(file.type)
-                ? window.URL.createObjectURL(file)
-                : NoteIcon
-            }
-          />
-        </InlineStack>
-      ))}
-    </BlockStack>
-  );
-
-  return (
-    <BlockStack gap="200">
-      <Text as="h2" variant="headingMd">
-        Small sized Drop zone that functions as a drop zone when using a mouse
-        and has a customized modal that opens when clicked. When navigating with
-        a keyboard, the child element serves as the modal activator and provides
-        accessibility and custom desight features.
-      </Text>
-      <div style={{width: 40, height: 40}}>
-        <DropZone onDrop={handleDrop} onClick={handleOpenModal} dragAndDropOnly>
-          <BlockStack align="center" inlineAlign="center" gap="400">
-            {uploadedFiles}
-            {fileUpload}
-          </BlockStack>
-        </DropZone>
-      </div>
-      {uploadImageModal}
-    </BlockStack>
-  );
-}
-
 export function Error() {
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState<File[]>([]);
 
   const handleDropZoneDrop = useCallback(
     (_dropFiles, acceptedFiles, _rejectedFiles) =>
