ee000f713096193ab7ec7db7b6a102408af0eb06
Charles Lee <charles.lee@shopify.com>
2024-03-11 12:34:32 +1100

    Fix lint errors in css

--- polaris-react/postcss-mixins/range-track-selectors.js
+++ polaris-react/postcss-mixins/range-track-selectors.js
@@ -2,6 +2,7 @@
   This mixin exists because we can't have comma separated selector lists for browser prefixes
   REASON UNKNOWN
 */
+
 module.exports = () => ({
   '&::-ms-track': {
     '@mixin-content': {},
