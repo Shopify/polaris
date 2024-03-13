4c5c99fb8f13e635575d3a5e57ee0bfbcf48f136
Kyle Durand <kyledurand@users.noreply.github.com>
2024-03-13 15:19:41 -0400

    fix: iOS 16 font patch for web views (#11725)

---
+++ .changeset/lemon-ties-grow.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris': patch
+---
+
+Fixed a bug where iOS 16 font patch wasn't added for mobile app web views
--- polaris-react/src/components/AppProvider/AppProvider.tsx
+++ polaris-react/src/components/AppProvider/AppProvider.tsx
@@ -113,7 +113,13 @@ export class AppProvider extends Component<AppProviderProps, State> {
           navigator.userAgent.includes('Version/16.2') ||
           navigator.userAgent.includes('Version/16.3'));
 
-      if (isSafari16) {
+      const isMobileApp16 =
+        navigator.userAgent.includes('Shopify Mobile/iOS') &&
+        (navigator.userAgent.includes('OS 16_1') ||
+          navigator.userAgent.includes('OS 16_2') ||
+          navigator.userAgent.includes('OS 16_3'));
+
+      if (isSafari16 || isMobileApp16) {
         document.documentElement.classList.add(
           'Polaris-Safari-16-Font-Optical-Sizing-Patch',
         );
