ef296fbc9247cd5ae29e6b5a2d9bc5c5cb058c8c
Aaron Casanova <aaron.casanova@shopify.com>
2024-03-14 21:51:53 -0700

    [Tooltip] Clear timeouts on open state changes

--- polaris-react/src/components/Tooltip/Tooltip.tsx
+++ polaris-react/src/components/Tooltip/Tooltip.tsx
@@ -152,6 +152,13 @@ export function Tooltip({
     };
   }, []);
 
+  useEffect(() => {
+    if (open && hoverDelayTimeout.current) {
+      clearTimeout(hoverDelayTimeout.current);
+      hoverDelayTimeout.current = null;
+    }
+  }, [open]);
+
   const handleOpen = useCallback(() => {
     setShouldAnimate(!presenceList.tooltip && !active);
     onOpen?.();
@@ -161,10 +168,11 @@ export function Tooltip({
   const handleClose = useCallback(() => {
     onClose?.();
     setShouldAnimate(false);
+    if (open) return;
     hoverOutTimeout.current = setTimeout(() => {
       removePresence('tooltip');
     }, HOVER_OUT_TIMEOUT);
-  }, [removePresence, onClose]);
+  }, [open, removePresence, onClose]);
 
   const handleKeyUp = useCallback(
     (event: React.KeyboardEvent) => {
