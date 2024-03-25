3cd475476fe85617ceb2fdcbbe987ec1365e83a2
Sophie Schneider <thesophieschneider@gmail.com>
2024-03-25 14:18:32 +0000

    Make border gradient tokens experimental and add tooltip tail tokens

--- polaris-tokens/src/themes/dark.ts
+++ polaris-tokens/src/themes/dark.ts
@@ -4,9 +4,7 @@ import {createMetaTheme, createMetaThemePartial} from './utils';
 
 export const metaThemeDarkPartial = createMetaThemePartial({
   color: {
-    // todo
     'color-bg': {value: colors.gray[16]},
-    // from figma
     'color-bg-surface': {value: colors.gray[15]},
     'color-bg-fill': {value: colors.gray[15]},
     'color-icon': {value: colors.gray[8]},
@@ -30,17 +28,18 @@ export const metaThemeDarkPartial = createMetaThemePartial({
     'color-bg-fill-brand-active': {value: colors.gray[4]},
     'color-bg-surface-secondary-active': {value: colors.gray[13]},
     'color-bg-surface-secondary-hover': {value: colors.gray[14]},
+    'color-bg-surface-brand-selected': {value: colors.gray[14]},
     'color-border-secondary': {value: colors.gray[13]},
-    'color-border-gradient': {
+    'color-border-gradient-experimental': {
       value: `linear-gradient(to bottom, ${colors.whiteAlpha[9]}, ${colors.whiteAlpha[4]})`,
     },
-    'color-border-gradient-hover': {
+    'color-border-gradient-hover-experimental': {
       value: `linear-gradient(to bottom, ${colors.whiteAlpha[9]}, ${colors.whiteAlpha[4]})`,
     },
-    'color-border-gradient-selected': {
+    'color-border-gradient-selected-experimental': {
       value: `linear-gradient(to bottom, ${colors.blackAlpha[10]}, ${colors.whiteAlpha[10]})`,
     },
-    'color-border-gradient-active': {
+    'color-border-gradient-active-experimental': {
       value: `linear-gradient(to bottom, ${colors.whiteAlpha[10]}, ${colors.whiteAlpha[4]})`,
     },
   },
