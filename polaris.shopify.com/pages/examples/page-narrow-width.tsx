849e79aeeb354930e433c7c07a6e8720d3d0d770
Matt Gregg <matt.gregg@shopify.com>
2024-03-25 13:17:15 -0500

    Add spacing to Card
    Co-authored-by: Lo Kim <lo.kim@shopify.com>

--- polaris.shopify.com/pages/examples/page-narrow-width.tsx
+++ polaris.shopify.com/pages/examples/page-narrow-width.tsx
@@ -1,4 +1,4 @@
-import {Page, PageActions, Card, Text} from '@shopify/polaris';
+import {Page, PageActions, Card, Text, BlockStack} from '@shopify/polaris';
 import React from 'react';
 import {withPolarisExample} from '../../src/components/PolarisExampleWrapper';
 import {DeleteIcon} from '@shopify/polaris-icons';
@@ -12,12 +12,14 @@ function PageExample() {
       primaryAction={{content: 'Save', disabled: true}}
     >
       <Card>
-        <Text as="h2" variant="headingSm">
-          Credit card
-        </Text>
-        <Text as="p" variant="bodyMd">
-          Credit card information
-        </Text>
+        <BlockStack gap="200">
+          <Text as="h2" variant="headingSm">
+            Credit card
+          </Text>
+          <Text as="p" variant="bodyMd">
+            Credit card information
+          </Text>
+        </BlockStack>
       </Card>
       <PageActions
         primaryAction={{content: 'Save', disabled: true}}
