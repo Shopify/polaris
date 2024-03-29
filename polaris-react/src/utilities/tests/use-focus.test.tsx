28288ac97ddc973cf138f6d890e30dbdcff4546b
Aaron Casanova <aaron.casanova@shopify.com>
2024-03-28 17:06:40 -0700

    Add tests for useFocus and useFocusIn hooks

---
+++ polaris-react/src/utilities/tests/use-focus.test.tsx
@@ -0,0 +1,139 @@
+import React, {useRef} from 'react';
+import {mount} from 'tests/utilities';
+
+import {useFocus, useFocusIn} from '../use-focus';
+
+describe('useFocus', () => {
+  it('returns false by default', () => {
+    function App() {
+      const ref = useRef(null);
+      const isFocused = useFocus(ref);
+      return <div ref={ref}>{String(isFocused)}</div>;
+    }
+
+    const app = mount(<App />);
+
+    expect(app).toContainReactText('false');
+  });
+
+  it('returns true on focus', () => {
+    function App() {
+      const ref = useRef(null);
+      const isFocused = useFocus(ref);
+      return <div ref={ref}>{String(isFocused)}</div>;
+    }
+
+    const app = mount(<App />);
+    const div = app.find('div')!.domNode!;
+
+    app.act(() => {
+      div.dispatchEvent(new Event('focus'));
+    });
+
+    expect(app).toContainReactText('true');
+  });
+
+  it('returns false on focus and blur', () => {
+    function App() {
+      const ref = useRef(null);
+      const isFocused = useFocus(ref);
+      return <div ref={ref}>{String(isFocused)}</div>;
+    }
+
+    const app = mount(<App />);
+    const div = app.find('div')!.domNode!;
+
+    app.act(() => {
+      div.dispatchEvent(new Event('focus'));
+      div.dispatchEvent(new Event('blur'));
+    });
+
+    expect(app).toContainReactText('false');
+  });
+});
+
+describe('useFocusIn', () => {
+  it('returns false by default', () => {
+    function App() {
+      const ref = useRef(null);
+      const isFocusedIn = useFocusIn(ref);
+      return <div ref={ref}>{String(isFocusedIn)}</div>;
+    }
+
+    const app = mount(<App />);
+
+    expect(app).toContainReactText('false');
+  });
+
+  it('returns true on focusin', () => {
+    function App() {
+      const ref = useRef(null);
+      const isFocusedIn = useFocusIn(ref);
+      return <div ref={ref}>{String(isFocusedIn)}</div>;
+    }
+
+    const app = mount(<App />);
+    const div = app.find('div')!.domNode!;
+
+    app.act(() => {
+      div.dispatchEvent(new Event('focusin'));
+    });
+
+    expect(app).toContainReactText('true');
+  });
+
+  it('returns false on focusin and focusout', () => {
+    jest.useFakeTimers();
+
+    function App() {
+      const ref = useRef(null);
+      const isFocusedIn = useFocusIn(ref);
+      return <div ref={ref}>{String(isFocusedIn)}</div>;
+    }
+
+    const app = mount(<App />);
+    const div = app.find('div')!.domNode!;
+
+    app.act(() => {
+      div.dispatchEvent(new Event('focusin'));
+      div.dispatchEvent(new Event('focusout'));
+    });
+
+    // Should remain true for one tick to prevent flashing (See next test)
+    expect(app).toContainReactText('true');
+
+    app.act(() => jest.advanceTimersByTime(1));
+
+    expect(app).toContainReactText('false');
+  });
+
+  it('returns true on focusin between children', () => {
+    jest.useFakeTimers();
+
+    function App() {
+      const ref = useRef(null);
+      const isFocusedIn = useFocusIn(ref);
+      return <div ref={ref}>{String(isFocusedIn)}</div>;
+    }
+
+    const app = mount(<App />);
+    const div = app.find('div')!.domNode!;
+
+    app.act(() => {
+      div.dispatchEvent(new Event('focusin'));
+      div.dispatchEvent(new Event('focusout'));
+    });
+
+    // Should remain true for one tick to prevent flashing
+    // when moving focus between child elements
+    expect(app).toContainReactText('true');
+
+    app.act(() => {
+      div.dispatchEvent(new Event('focusin'));
+    });
+
+    app.act(() => jest.advanceTimersByTime(1));
+
+    expect(app).toContainReactText('true');
+  });
+});
