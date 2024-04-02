0bce3da67e04983798bc1502c095e4b15f6bbeca
Laura Griffee <laura@mailzone.com>
2024-04-02 15:33:23 -0600

    Remove `Text` `heading2xl` and associated tokens from v14 font migration (#11813)
    ### WHY are these changes introduced?
    
    Part of https://github.com/Shopify/polaris-internal/issues/1529
    
    ### WHAT is this pull request doing?
    This PR updates the migration for deprecated `font` custom properties
    based on the undeprecation of heading2xl.
    
    #### v14-styles-replace-custom-property-font
    | Deprecated CSS Custom Property | Replacement Value |
    | -- | -- | 
    | ~`--p-font-size-750`~  | ~`--p-font-size-600`~ | 
    | `--p-font-size-800`  | ~`--p-font-size-600`~<br>`--p-font-size-750` | 
    | `--p-font-size-900` | ~`--p-font-size-600`~<br>`--p-font-size-750` | 
    | `--p-font-size-1000` | ~`--p-font-size-600`~<br>`--p-font-size-750` | 
    | ~`--p-font-letter-spacing-denser`~ | ~`--p-font-letter-spacing-dense`~
    |
    | `--p-font-letter-spacing-densest` |
    ~`--p-font-letter-spacing-dense`~<br>`--p-font-letter-spacing-denser` |
    | ~`--p-font-line-height-1000`~ | ~`--p-font-line-height-800`~ | 
    | `--p-font-line-height-1200` |
    ~`--p-font-line-height-800`~<br>`--p-font-line-height-1000` |
    |`--p-text-heading-3xl-font-family`|
    ~`--p-text-heading-xl-font-family`~<br>`--p-text-heading-2xl-font-family`|
    |`--p-text-heading-3xl-font-size`|
    ~`--p-text-heading-xl-font-size`~<br>`--p-text-heading-2xl-font-size`|
    | `--p-text-heading-3xl-font-weight`|
    ~`--p-text-heading-xl-font-weight`~<br>`--p-text-heading-2xl-font-weight`|
    |`--p-text-heading-3xl-font-letter-spacing`|
    ~`--p-text-heading-xl-font-letter-spacing`~<br>`--p-text-heading-2xl-font-letter-spacing`|
    | `--p-text-heading-3xl-font-line-height`|
    ~`--p-text-heading-xl-font-line-height`~<br>`--p-text-heading-2xl-font-line-height`|
    |~`--p-text-heading-2xl-font-family`~|
    ~`--p-text-heading-xl-font-family`~|
    | ~`--p-text-heading-2xl-font-size`~| ~`--p-text-heading-xl-font-size`~|
    | ~`--p-text-heading-2xl-font-weight`~|
    ~`--p-text-heading-xl-font-weight`~|
    | ~`--p-text-heading-2xl-font-letter-spacing`~|
    ~`--p-text-heading-xl-font-letter-spacing`~|
    |~`--p-text-heading-2xl-font-line-height`~|
    ~`--p-text-heading-xl-font-line-height`~|
    
    >[!NOTE]
    >Major version upgrade guidance is updated [on a separate PR on the v13
    branch](https://github.com/Shopify/polaris/pull/11815).

--- polaris-migrator/src/migrations/v14-styles-replace-custom-property-font/transform.ts
+++ polaris-migrator/src/migrations/v14-styles-replace-custom-property-font/transform.ts
@@ -8,27 +8,17 @@ export default function transformer(fileInfo: FileInfo, _: API) {
 
 const replacementMaps = {
   '/.+/': {
-    '--p-font-size-750': '--p-font-size-600',
-    '--p-font-size-800': '--p-font-size-600',
-    '--p-font-size-900': '--p-font-size-600',
-    '--p-font-size-1000': '--p-font-size-600',
-    '--p-font-letter-spacing-denser': '--p-font-letter-spacing-dense',
-    '--p-font-letter-spacing-densest': '--p-font-letter-spacing-dense',
-    '--p-font-line-height-1000': '--p-font-line-height-800',
-    '--p-font-line-height-1200': '--p-font-line-height-800',
-    '--p-text-heading-3xl-font-family': '--p-text-heading-xl-font-family',
-    '--p-text-heading-3xl-font-size': '--p-text-heading-xl-font-size',
-    '--p-text-heading-3xl-font-weight': '--p-text-heading-xl-font-weight',
+    '--p-font-size-800': '--p-font-size-750',
+    '--p-font-size-900': '--p-font-size-750',
+    '--p-font-size-1000': '--p-font-size-750',
+    '--p-font-letter-spacing-densest': '--p-font-letter-spacing-denser',
+    '--p-font-line-height-1200': '--p-font-line-height-1000',
+    '--p-text-heading-3xl-font-family': '--p-text-heading-2xl-font-family',
+    '--p-text-heading-3xl-font-size': '--p-text-heading-2xl-font-size',
+    '--p-text-heading-3xl-font-weight': '--p-text-heading-2xl-font-weight',
     '--p-text-heading-3xl-font-letter-spacing':
-      '--p-text-heading-xl-font-letter-spacing',
+      '--p-text-heading-2xl-font-letter-spacing',
     '--p-text-heading-3xl-font-line-height':
-      '--p-text-heading-xl-font-line-height',
-    '--p-text-heading-2xl-font-family': '--p-text-heading-xl-font-family',
-    '--p-text-heading-2xl-font-size': '--p-text-heading-xl-font-size',
-    '--p-text-heading-2xl-font-weight': '--p-text-heading-xl-font-weight',
-    '--p-text-heading-2xl-font-letter-spacing':
-      '--p-text-heading-xl-font-letter-spacing',
-    '--p-text-heading-2xl-font-line-height':
-      '--p-text-heading-xl-font-line-height',
+      '--p-text-heading-2xl-font-line-height',
   },
 };
