49caee89c53d6485d0b135d6bddfd7c43d142d2a
Lucien Bénié <lucien.benie@gmail.com>
2024-04-03 15:09:12 -0400

    fix

--- polaris-react/src/components/IndexProvider/tests/IndexProvider.test.tsx
+++ polaris-react/src/components/IndexProvider/tests/IndexProvider.test.tsx
@@ -1,3 +1,4 @@
+import type {ComponentProps} from 'react';
 import React from 'react';
 import {mountWithApp} from 'tests/utilities';
 
@@ -5,10 +6,11 @@ import {IndexProvider} from '../IndexProvider';
 import {IndexRowContext} from '../../../utilities/index-provider';
 
 describe('<IndexProvider />', () => {
-  const defaultProps = {
+  const defaultProps: ComponentProps<typeof IndexProvider> = {
     itemCount: 0,
     selectedItemsCount: 0,
     onSelectionChange: () => {},
+    onClearSelection: () => {},
   };
 
   it('renders a IndexRowContextProvider', () => {
