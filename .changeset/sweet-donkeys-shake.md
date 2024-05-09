c98a08506a831dfc99cd5a8e52b5c4b6d32d128c
Ash <siavash.etemadieh@shopify.com>
2024-05-09 15:09:41 +0100

    [Prototype] Mobile Inline Input labels

---
+++ .changeset/sweet-donkeys-shake.md
@@ -0,0 +1,5 @@
+---
+'@shopify/polaris': major
+---
+
+Introduce Inline Labels for Mobile to TextField & Select
--- polaris-react/src/components/Label/Label.tsx
+++ polaris-react/src/components/Label/Label.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import {Text} from '../Text';
 import {classNames} from '../../utilities/css';
+import type {TextProps} from '../Text';
+import {Text} from '../Text';
 
 import styles from './Label.module.css';
 
@@ -14,13 +15,25 @@ export interface LabelProps {
   hidden?: boolean;
   /** Visual required indicator for the label */
   requiredIndicator?: boolean;
+  /** Text variant */
+  variant?: TextProps['variant'];
+
+  /** Text tone*/
+  tone?: TextProps['tone'];
 }
 
 export function labelID(id: string) {
   return `${id}Label`;
 }
 
-export function Label({children, id, hidden, requiredIndicator}: LabelProps) {
+export function Label({
+  children,
+  id,
+  hidden,
+  requiredIndicator,
+  variant = 'bodyMd',
+  tone,
+}: LabelProps) {
   const className = classNames(styles.Label, hidden && styles.hidden);
 
   return (
@@ -33,7 +46,7 @@ export function Label({children, id, hidden, requiredIndicator}: LabelProps) {
           requiredIndicator && styles.RequiredIndicator,
         )}
       >
-        <Text as="span" variant="bodyMd">
+        <Text as="span" variant={variant} tone={tone}>
           {children}
         </Text>
       </label>
