65d7a359180837b86ed7940e54df408d4b8efe99
Jess Telford <jess.telford@shopify.com>
2024-06-21 16:54:24 +1000

    Update `useBreakpoints` to use a single set of event listeners (#12287)
    Co-authored-by: Aaron Casanova <aaron.casanova@shopify.com>

--- polaris-react/tests/setup/tests.ts
+++ polaris-react/tests/setup/tests.ts
@@ -3,16 +3,22 @@ import {matchMedia} from '@shopify/jest-dom-mocks';
 import '@shopify/react-testing/matchers';
 import './matchers';
 
+// Mock once before test files are imported so uses of `window.matchMedia`
+// outside of components still works.
+if (typeof window !== 'undefined' && !matchMedia.isMocked()) {
+  matchMedia.mock();
+}
+
 // eslint-disable-next-line jest/require-top-level-describe
 beforeEach(() => {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && !matchMedia.isMocked()) {
     matchMedia.mock();
   }
 });
 
 // eslint-disable-next-line jest/require-top-level-describe
 afterEach(() => {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && matchMedia.isMocked()) {
     matchMedia.restore();
   }
   destroyAll();
