51ee41a36b27988c003ef43bb7a77e9d67942208
oksanashopify <oksana.azarova@shopify.com>
2024-10-10 21:09:36 -0600

    added story fith filters within the Modal

--- polaris-react/src/components/Modal/Modal.stories.tsx
+++ polaris-react/src/components/Modal/Modal.stories.tsx
@@ -1,6 +1,7 @@
 import React, {useCallback, useRef, useState} from 'react';
 import type {Meta} from '@storybook/react';
 import {within} from '@storybook/test';
+import type {FiltersProps} from '@shopify/polaris';
 import {
   Banner,
   Button,
@@ -15,6 +16,10 @@ import {
   TextField,
   Frame,
   FrameContext,
+  ResourceList,
+  Filters,
+  RangeSlider,
+  Avatar,
 } from '@shopify/polaris';
 import {transitionsAllSettled} from 'transitions-all-settled';
 
@@ -58,6 +63,235 @@ export const Default = {
                 people. Let shoppers buy from your store without leaving
                 Instagram.
               </TextContainer>
+            </Modal.Section>
+          </Modal>
+        </div>
+      </Frame>
+    );
+  },
+};
+
+export const DefaultWithFilters = {
+  play: async ({canvasElement}) => {
+    await transitionsAllSettled(canvasElement);
+  },
+  render() {
+    const [active, setActive] = useState(true);
+
+    const handleChange = useCallback(() => setActive(!active), [active]);
+
+    const activator = <Button onClick={handleChange}>Open</Button>;
+
+    const [accountStatus, setAccountStatus] = useState(null);
+    const [moneySpent, setMoneySpent] = useState(null);
+    const [taggedWith, setTaggedWith] = useState('');
+    const [queryValue, setQueryValue] = useState('');
+
+    const handleAccountStatusChange = useCallback(
+      (value) => setAccountStatus(value),
+      [],
+    );
+    const handleMoneySpentChange = useCallback(
+      (value) => setMoneySpent(value),
+      [],
+    );
+    const handleTaggedWithChange = useCallback(
+      (value) => setTaggedWith(value),
+      [],
+    );
+    const handleFiltersQueryChange = useCallback(
+      (value) => setQueryValue(value),
+      [],
+    );
+    const handleAccountStatusRemove = useCallback(
+      () => setAccountStatus(null),
+      [],
+    );
+    const handleMoneySpentRemove = useCallback(() => setMoneySpent(null), []);
+    const handleTaggedWithRemove = useCallback(() => setTaggedWith(''), []);
+    const handleQueryValueRemove = useCallback(() => setQueryValue(''), []);
+    const handleFiltersClearAll = useCallback(() => {
+      handleAccountStatusRemove();
+      handleMoneySpentRemove();
+      handleTaggedWithRemove();
+      handleQueryValueRemove();
+    }, [
+      handleAccountStatusRemove,
+      handleMoneySpentRemove,
+      handleQueryValueRemove,
+      handleTaggedWithRemove,
+    ]);
+
+    const filters = [
+      {
+        key: 'accountStatus',
+        label: 'Account status',
+        filter: (
+          <ChoiceList
+            title="Account status"
+            titleHidden
+            choices={[
+              {label: 'Enabled', value: 'enabled'},
+              {label: 'Not invited', value: 'not invited'},
+              {label: 'Invited', value: 'invited'},
+              {label: 'Declined', value: 'declined'},
+            ]}
+            selected={accountStatus || []}
+            onChange={handleAccountStatusChange}
+            allowMultiple
+          />
+        ),
+        shortcut: true,
+        pinned: true,
+      },
+      {
+        key: 'taggedWith',
+        label: 'Tagged with',
+        filter: (
+          <TextField
+            label="Tagged with"
+            value={taggedWith}
+            onChange={handleTaggedWithChange}
+            autoComplete="off"
+            labelHidden
+          />
+        ),
+        shortcut: true,
+        pinned: true,
+      },
+      {
+        key: 'moneySpent',
+        label: 'Money spent',
+        filter: (
+          <RangeSlider
+            label="Money spent is between"
+            labelHidden
+            value={moneySpent || [0, 500]}
+            prefix="$"
+            output
+            min={0}
+            max={2000}
+            step={1}
+            onChange={handleMoneySpentChange}
+          />
+        ),
+      },
+    ];
+
+    const appliedFilters: FiltersProps['appliedFilters'] = [];
+    if (!isEmpty(accountStatus)) {
+      const key = 'accountStatus';
+      appliedFilters.push({
+        key,
+        label: disambiguateLabel(key, accountStatus),
+        onRemove: handleAccountStatusRemove,
+      });
+    }
+    if (!isEmpty(moneySpent)) {
+      const key = 'moneySpent';
+      appliedFilters.push({
+        key,
+        label: disambiguateLabel(key, moneySpent),
+        onRemove: handleMoneySpentRemove,
+      });
+    }
+    if (!isEmpty(taggedWith)) {
+      const key = 'taggedWith';
+      appliedFilters.push({
+        key,
+        label: disambiguateLabel(key, taggedWith),
+        onRemove: handleTaggedWithRemove,
+      });
+    }
+    function disambiguateLabel(key, value) {
+      switch (key) {
+        case 'moneySpent':
+          return `Money spent is between $${value[0]} and $${value[1]}`;
+        case 'taggedWith':
+          return `Tagged with ${value}`;
+        case 'accountStatus':
+          return value.map((val) => `Customer ${val}`).join(', ');
+        default:
+          return value;
+      }
+    }
+
+    function isEmpty(value) {
+      if (Array.isArray(value)) {
+        return value.length === 0;
+      } else {
+        return value === '' || value == null;
+      }
+    }
+
+    return (
+      <Frame>
+        <div style={{height: '500px'}}>
+          <Modal
+            activator={activator}
+            open={active}
+            onClose={handleChange}
+            title="Reach more shoppers with Instagram product tags"
+            primaryAction={{
+              content: 'Add Instagram',
+              onAction: handleChange,
+            }}
+            secondaryActions={[
+              {
+                content: 'Learn more',
+                onAction: handleChange,
+              },
+            ]}
+          >
+            <Modal.Section>
+              <ResourceList
+                resourceName={{singular: 'customer', plural: 'customers'}}
+                filterControl={
+                  <Filters
+                    queryValue={queryValue}
+                    queryPlaceholder="Searching in all"
+                    filters={filters}
+                    appliedFilters={appliedFilters}
+                    onQueryChange={handleFiltersQueryChange}
+                    onQueryClear={handleQueryValueRemove}
+                    onClearAll={handleFiltersClearAll}
+                    closeOnChildOverlayClick
+                  />
+                }
+                flushFilters
+                items={[
+                  {
+                    id: '341',
+                    url: '#',
+                    name: 'Mae Jemison',
+                    location: 'Decatur, USA',
+                  },
+                  {
+                    id: '256',
+                    url: '#',
+                    name: 'Ellen Ochoa',
+                    location: 'Los Angeles, USA',
+                  },
+                ]}
+                renderItem={(item) => {
+                  const {id, url, name, location} = item;
+                  const media = <Avatar customer size="md" name={name} />;
+
+                  return (
+                    <ResourceList.Item
+                      id={id}
+                      url={url}
+                      media={media}
+                      accessibilityLabel={`View details for ${name}`}
+                    >
+                      <Text as="h3" fontWeight="bold">
+                        {name}
+                      </Text>
+                      <div>{location}</div>
+                    </ResourceList.Item>
+                  );
+                }}
+              />
             </Modal.Section>
           </Modal>
         </div>
