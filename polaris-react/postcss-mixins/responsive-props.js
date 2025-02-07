b011266017edfcc27fdbfa9a2f60615f685a4364
simonporter007 <simonporter007@users.noreply.github.com>
2025-02-07 09:34:39 +0000

    remove errant semicolons from mixins

--- polaris-react/postcss-mixins/responsive-props.js
+++ polaris-react/postcss-mixins/responsive-props.js
@@ -29,7 +29,7 @@ module.exports = (mixin, componentName, componentProp, declarationProp) => {
             var(--pc-${componentName}-${componentProp}-xs)
           )
         )
-      );`,
+      )`,
     },
     '@media (--p-breakpoints-xl-up)': {
       [declarationProp]: `var(
@@ -44,7 +44,7 @@ module.exports = (mixin, componentName, componentProp, declarationProp) => {
             )
           )
         )
-      );`,
+      )`,
     },
   };
 };
