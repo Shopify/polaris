9cac694a396d26b55b69e34c40b637b4c7a8310d
Lo Kim <lo.kim@shopify.com>
2025-05-07 08:06:43 -1000

    [Breadcrumbs] Deprecate `backAction` prop

--- polaris-react/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ polaris-react/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -6,7 +6,10 @@ import {handleMouseUpByBlurring} from '../../utilities/focus';
 import {Button} from '../Button';
 
 export interface BreadcrumbsProps {
-  /** Back action link */
+  /**
+   * @deprecated Back action link
+   * Use `breadcrumbs` prop instead as documented [here](https://shopify.dev/docs/api/app-bridge/previous-versions/actions/titlebar#using-titlebar-with-polaris)
+   */
   backAction: CallbackAction | LinkAction;
 }
 
